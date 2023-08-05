d0c25b5fd74219dfadc945e068556d5630c036ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:17 +0000

    Add package imag3/1.2.3/imag3-1.2.3.tar.gz

---
+++ packages/imag3/imag3-1.2.3.tar.gz/imag3-1.2.3/imag3/classes/config/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+import os, sys, ast, json, glob, platform, subprocess, random, time
+
+# inc imports.
+import syst3m
+from dev0s import *
+
+# source.
+ALIAS = "imag3"
+SOURCE_PATH = Defaults.source_path(__file__, back=3)
+OS = Defaults.operating_system(supported=["macos", "linux"])
+Defaults.alias(alias=ALIAS, executable=f"{SOURCE_PATH}")
+
+# universal variables.
+OWNER = os.environ.get("USER")
+GROUP = "root"
+HOME_BASE = "/home/"
+HOME = f"/home/{os.environ.get('USER')}/"
+MEDIA = f"/media/{os.environ.get('USER')}/"
+if OS in ["macos"]: 
+	HOME_BASE = "/Users/"
+	HOME = f"/Users/{os.environ.get('USER')}/"
+	MEDIA = f"/Volumes/"
+	GROUP = "wheel"
