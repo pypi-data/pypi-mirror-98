630048a0e242c0cc0325723fb1d5749314ed6462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:42 +0000

    Add package unimport/0.8.1/unimport-0.8.1.tar.gz

---
+++ packages/unimport/unimport-0.8.1.tar.gz/unimport-0.8.1/unimport/config.py
@@ -0,0 +1,104 @@
+import configparser
+from ast import literal_eval
+from pathlib import Path
+from typing import List, NamedTuple
+
+import toml
+
+from unimport import constants as C
+from unimport import utils
+
+__all__ = ["CONFIG_FILES", "Config", "DefaultConfig"]
+
+CONFIG_FILES = {"setup.cfg": "unimport", "pyproject.toml": "tool.unimport"}
+
+
+class DefaultConfig(NamedTuple):
+    sources: List[Path] = [Path(".")]
+    include: str = C.INCLUDE_REGEX_PATTERN
+    exclude: str = C.EXCLUDE_REGEX_PATTERN
+    requirements: bool = False
+    gitignore: bool = False
+    remove: bool = False
+    diff: bool = False
+    include_star_import: bool = False
+    permission: bool = False
+    check: bool = False
+    ignore_init: bool = False
+
+    def merge(self, **kwargs):
+        diff_dict = set(kwargs) - set(self._asdict())
+        # delete keys that are not available.
+        for invalid_key in diff_dict:
+            del kwargs[invalid_key]
+        # delete items if they are the same as default values
+        for key, value in kwargs.copy().items():
+            if getattr(self, key) == value:
+                del kwargs[key]
+        config = self._replace(**kwargs)
+        diff = kwargs.get("diff") or kwargs.get("permission")
+        config = config._replace(
+            diff=diff or any((config.diff, config.permission))
+        )
+        config = config._replace(
+            check=kwargs.get("check") or not any((config.diff, config.remove))
+        )
+        if config.gitignore:
+            gitignore_exclude = utils.get_exclude_list_from_gitignore()
+            config = config._replace(
+                exclude="|".join([config.exclude] + gitignore_exclude)
+            )
+        if config.ignore_init:
+            config = config._replace(
+                exclude="|".join([config.exclude, C.INIT_FILE_IGNORE_REGEX])
+            )
+        return config
+
+
+class Config:
+
+    default_config = DefaultConfig()
+
+    def __init__(self, config_file: Path) -> None:
+        self.config_file = config_file
+        self.section = CONFIG_FILES[config_file.name]
+
+    def parse(self) -> DefaultConfig:
+        return getattr(self, f"parse_{self.config_file.suffix.strip('.')}")()
+
+    def parse_cfg(self) -> DefaultConfig:
+        parser = configparser.ConfigParser(allow_no_value=True)
+        parser.read(self.config_file)
+        if parser.has_section(self.section):
+
+            def get_config_as_list(name: str) -> List[str]:
+                return literal_eval(
+                    parser.get(
+                        self.section,
+                        name,
+                        fallback=getattr(self.default_config, name),
+                    )
+                )
+
+            cfg_context = {}
+            config_annotations = self.default_config.__annotations__
+            for key, value in parser[self.section].items():
+                key_type = config_annotations[key]
+                if key_type == bool:
+                    cfg_context[key] = parser.getboolean(self.section, key)
+                elif key_type == str:
+                    cfg_context[key] = value  # type: ignore
+                elif key_type == List[Path]:
+                    cfg_context[key] = [  # type: ignore
+                        Path(p) for p in get_config_as_list(key)
+                    ]
+            return self.default_config._replace(**cfg_context)  # type: ignore
+        else:
+            return self.default_config
+
+    def parse_toml(self) -> DefaultConfig:
+        parsed_toml = toml.loads(self.config_file.read_text())
+        config = parsed_toml.get("tool", {}).get("unimport", {})
+        sources = config.get("sources", self.default_config.sources)
+        config["sources"] = [Path(path) for path in sources]
+        return self.default_config._replace(**config)
---
+++ packages/unimport/unimport-0.8.1.tar.gz/unimport-0.8.1/unimport/refactor.py
@@ -0,0 +1,162 @@
+from typing import List, Optional, Sequence, TypeVar, Union, cast
+
+import libcst as cst
+import libcst.matchers as m
+from libcst.metadata import CodeRange, PositionProvider
+
+from unimport import constants as C
+from unimport.statement import Import, ImportFrom
+
+__all__ = ["refactor_string"]
+
+
+class _RemoveUnusedImportTransformer(cst.CSTTransformer):
+    METADATA_DEPENDENCIES = (PositionProvider,)
+
+    def __init__(
+        self, unused_imports: List[Union[Import, ImportFrom]]
+    ) -> None:
+        self.unused_imports = unused_imports
+
+    @staticmethod
+    def get_import_name_from_attr(attr_node: cst.Attribute) -> str:
+        name = [attr_node.attr.value]  # last value
+        node = attr_node.value
+        while m.matches(node, m.OneOf(m.Name(), m.Attribute())):
+            if isinstance(node, cst.Attribute):
+                name.append(node.attr.value)
+                node = node.value
+            else:
+                name.append(cst.ensure_type(node, cst.Name).value)
+                break
+        name.reverse()
+        return ".".join(name)
+
+    def is_import_used(
+        self, import_name: str, column: int, location: CodeRange
+    ) -> bool:
+        for imp in self.unused_imports:
+            if all(
+                (
+                    imp.name == import_name,
+                    imp.lineno == location.start.line,
+                    imp.column == column,
+                )
+            ):
+                return False
+        return True
+
+    def get_location(
+        self, node: Union[cst.Import, cst.ImportFrom]
+    ) -> CodeRange:
+        return self.get_metadata(cst.metadata.PositionProvider, node)
+
+    @staticmethod
+    def get_rpar(
+        rpar: Optional[cst.RightParen], location: CodeRange
+    ) -> Optional[cst.RightParen]:
+        if not rpar or location.start.line == location.end.line:
+            return rpar
+        else:
+            return cst.RightParen(
+                whitespace_before=cst.ParenthesizedWhitespace()
+            )
+
+    def leave_import_alike(
+        self,
+        original_node: C.CSTImportT,
+        updated_node: C.CSTImportT,
+    ) -> Union[cst.RemovalSentinel, C.CSTImportT]:
+        names_to_keep = []
+        names = cast(Sequence[cst.ImportAlias], updated_node.names)
+        # already handled by leave_ImportFrom
+        for column, import_alias in enumerate(names):
+            if isinstance(import_alias.name, cst.Attribute):
+                import_name = self.get_import_name_from_attr(
+                    attr_node=import_alias.name
+                )
+            else:
+                raw_import = import_alias.asname or import_alias
+                raw_import_name = cst.ensure_type(raw_import.name, cst.Name)
+                import_name = raw_import_name.value
+            if self.is_import_used(
+                import_name, column + 1, self.get_location(original_node)
+            ):
+                names_to_keep.append(import_alias)
+        if not names_to_keep:
+            return cst.RemoveFromParent()
+        elif len(names) == len(names_to_keep):
+            return updated_node
+        else:
+            names_to_keep[-1] = names_to_keep[-1].with_changes(
+                comma=cst.MaybeSentinel.DEFAULT
+            )
+            return cast(
+                C.CSTImportT, updated_node.with_changes(names=names_to_keep)
+            )
+
+    @staticmethod
+    def leave_StarImport(
+        updated_node: cst.ImportFrom,
+        imp: ImportFrom,
+    ) -> Union[cst.ImportFrom, cst.RemovalSentinel]:
+        if imp.suggestions:
+            names_to_suggestions = [
+                cst.ImportAlias(cst.Name(module)) for module in imp.suggestions
+            ]
+            return updated_node.with_changes(names=names_to_suggestions)
+        else:
+            return cst.RemoveFromParent()
+
+    def leave_Import(
+        self, original_node: cst.Import, updated_node: cst.Import
+    ) -> Union[cst.RemovalSentinel, cst.Import]:
+        return self.leave_import_alike(original_node, updated_node)
+
+    def leave_ImportFrom(
+        self, original_node: cst.ImportFrom, updated_node: cst.ImportFrom
+    ) -> Union[cst.RemovalSentinel, cst.ImportFrom]:
+        if isinstance(updated_node.names, cst.ImportStar):
+
+            def get_star_imp() -> Optional[ImportFrom]:
+                if isinstance(updated_node.module, cst.Attribute):
+                    import_name = self.get_import_name_from_attr(
+                        attr_node=updated_node.module
+                    )
+                else:
+                    import_name = updated_node.module.value
+                location = self.get_location(original_node)
+                for imp in self.unused_imports:
+                    if (
+                        isinstance(imp, ImportFrom)
+                        and imp.name == import_name
+                        and imp.lineno == location.start.line
+                    ):
+                        return imp
+                else:
+                    return None
+
+            imp = get_star_imp()
+            if imp:
+                return self.leave_StarImport(updated_node, imp)
+            else:
+                return original_node
+        rpar = self.get_rpar(
+            updated_node.rpar, self.get_location(original_node)
+        )
+        return self.leave_import_alike(
+            original_node, updated_node.with_changes(rpar=rpar)
+        )
+
+
+def refactor_string(
+    source: str,
+    unused_imports: List[Union[Import, ImportFrom]],
+) -> str:
+    if unused_imports:
+        wrapper = cst.MetadataWrapper(cst.parse_module(source))
+        fixed_module = wrapper.visit(
+            _RemoveUnusedImportTransformer(unused_imports)
+        )
+        return fixed_module.code
+    return source
