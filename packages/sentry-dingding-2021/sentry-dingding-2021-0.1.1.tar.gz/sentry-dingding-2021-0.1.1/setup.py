dafa57f5de5145863ea2835e1f966f7d99fa2c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:08 +0000

    Add package sentry-dingding-2021/0.1.1/sentry-dingding-2021-0.1.1.tar.gz

---
+++ packages/sentry-dingding-2021/sentry-dingding-2021-0.1.1.tar.gz/sentry-dingding-2021-0.1.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sentry-dingding-2021",
+    version='0.1.1',
+    author='hsw10151',
+    author_email='forsakenhsw1015@gmail.com',
+    description='A Sentry extension which send errors stats to DingDing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry dingding',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'sentry_dingding = sentry_dingding.plugin:DingDingPlugin'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
+)
