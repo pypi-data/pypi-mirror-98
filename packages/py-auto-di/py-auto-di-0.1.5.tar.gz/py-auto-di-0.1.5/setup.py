ea671371332acb4246ffa2e729e95606dbf59e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package py-auto-di/0.1.5/py-auto-di-0.1.5.tar.gz

---
+++ packages/py-auto-di/py-auto-di-0.1.5.tar.gz/py-auto-di-0.1.5/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README_CONTENT = (CURRENT_DIR / "README.md").read_text()
+
+setup(
+    name='py-auto-di',
+    version='0.1.5',
+    packages=["PyAutoDI"],
+    include_package_data=True,
+    author="Chris Stead",
+    author_email="cmstead@gmail.com",
+    url="https://github.com/cmstead/py-auto-di#readme",
+    platforms=['any'],
+    description="Lightweight, automatic Python DI library",
+    license='MIT',
+    long_description=README_CONTENT,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
