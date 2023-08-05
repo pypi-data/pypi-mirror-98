78889dc10aa36e7e621fa5af906c39153fee5b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package orbis-plugin-aggregation-weblyzard-harvest/2.2.4/orbis_plugin_aggregation_weblyzard_harvest-2.2.4-py3-none-any.whl

---
+++ packages/orbis-plugin-aggregation-weblyzard-harvest/orbis_plugin_aggregation_weblyzard_harvest-2.2.4-py3-none-any.whl/orbis_plugin_aggregation_weblyzard_harvest/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__name__ = "orbis_plugin_aggregation_weblyzard_harvest"
+__version__ = "2.2.4"
+__author__ = "fabod"
+__year__ = "2019"
+__description__ = "The Weblyzard Harvest aggregation plugin for Orbis"
+__license__ = "GPL2"
+__min_python_version__ = "3.6"
+__requirements_file__ = "requirements.txt"
+__url__ = "https://github.com/orbis-eval/orbis_plugin_aggregation_weblyzard_harvest"
+__type__ = "plugin"
+
+from .main import Main
