2a23047fc3d0fd5bf911f9d5b7e3e1ab5e056699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package DicomRTTool/0.3.13/DicomRTTool-0.3.13-py3-none-any.whl

---
+++ packages/DicomRTTool/DicomRTTool-0.3.13-py3-none-any.whl/DicomRTTool/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Brian M Anderson'
+# Created on 10/28/2020
+from .ReaderWriter import DicomReaderWriter, Dicom_to_Imagestack, plot_scroll_Image, sitk
+
+
+if __name__ == '__main__':
+    xxx = 1
