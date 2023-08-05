cf7a165eead78606a93a7120a24ce243f6c614ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:25 +0000

    Add package hagworm/4.2.4/hagworm-4.2.4.tar.gz

---
+++ packages/hagworm/hagworm-4.2.4.tar.gz/hagworm-4.2.4/example/fastapi_demo/controller/home.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from service.base import DataSource
+
+from hagworm.frame.fastapi.base import APIRouter, Request, ErrorResponse
+
+
+router = APIRouter()
+
+
+@router.get(r'/')
+async def default(request: Request):
+
+    return request.client_ip
+
+
+@router.get(r'/error')
+async def error(request: Request):
+
+    raise ErrorResponse(-1, request.client_ip, 400)
+
+
+@router.get(r'/health')
+async def health():
+
+    return await DataSource().health()
