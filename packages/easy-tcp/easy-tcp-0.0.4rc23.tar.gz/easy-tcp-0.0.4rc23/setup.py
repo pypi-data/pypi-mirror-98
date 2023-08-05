c68873a43cacb781cd6fc7fb1603d84d9b97c6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package easy-tcp/0.0.4rc23/easy-tcp-0.0.4rc23.tar.gz

---
+++ packages/easy-tcp/easy-tcp-0.0.4rc23.tar.gz/easy-tcp-0.0.4rc23/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, Extension
+import sys
+
+if "--et_comp" in sys.argv:
+    print("Compiling...")
+    from Cython.Build import cythonize
+    sys.argv.remove("--et_comp")
+    ext_modules = cythonize(
+        Extension(
+            "easy_tcp",
+            sources=[
+                "easy_tcp/process.c",
+                "easy_tcp/server.c",
+                "easy_tcp/cython_src/server.pyx"
+            ],
+        )
+    )
+else:
+    ext_modules = [
+        Extension(
+            "easy_tcp.cython_src",
+            [
+                "easy_tcp/cython_src/server.c",
+            ],
+        )
+    ]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+        name="easy-tcp",
+        version="0.0.4rc23",
+        description="Python TCP WSGI Server",
+        packages=["easy_tcp", "easy_tcp.cython_src"],
+        install_requires=[
+            "cython"
+    ],
+    zip_safe=False,
+    ext_modules=ext_modules,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Unix",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joegasewicz/easy-tcp",
+    author="Joe Gasewicz",
+    author_email="joegasewicz@gmail.com",
+)
