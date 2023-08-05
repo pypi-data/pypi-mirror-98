0dc7730d764e4175f54a464c1e8daaae7c3ed02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package djmicrosip-clasificadores/1.0.3/djmicrosip_clasificadores-1.0.3.zip

---
+++ packages/djmicrosip-clasificadores/djmicrosip_clasificadores-1.0.3.zip/djmicrosip_clasificadores-1.0.3/setup.py
@@ -0,0 +1,39 @@
+import os, time, compileall, sys
+from setuptools import setup, find_packages
+
+if "sdist" in sys.argv:
+    path = os.path.abspath(os.path.dirname(__file__))+"\\djmicrosip_clasificadores\\"
+    compileall.compile_dir(path, force=True)
+
+# README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='djmicrosip_clasificadores',
+    version='1.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='djmicrosip_clasificadores',
+    long_description='README',
+    url='',
+    author='Servicios de Ingenieria Computacional',
+    author_email='desarrollo@siccomputacion.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=REQUIREMENTS,
+)
