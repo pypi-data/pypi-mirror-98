0bb98ab68c5acfd08156744a50c8cc97f93d2e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:40 +0000

    Add package jittor/1.2.2.42/jittor-1.2.2.42.tar.gz

---
+++ packages/jittor/jittor-1.2.2.42.tar.gz/jittor-1.2.2.42/python/jittor/src/ops/arg_reduce_op.h
@@ -0,0 +1,31 @@
+// ***************************************************************
+// Copyright (c) 2021 Jittor. All Rights Reserved. 
+// Maintainers: 
+//     Guoye Yang <498731903@qq.com>
+//     Dun Liang <randonlang@gmail.com>. 
+// 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ***************************************************************
+#pragma once
+#include "op.h"
+
+
+namespace jittor {
+
+struct ArgReduceOp : Op {
+    Var* x, * y, * y_key;
+    NanoString op;
+    int dim;
+    bool keepdims;
+    // @attrs(multiple_outputs)
+    ArgReduceOp(Var* x, NanoString op, int dim, bool keepdims);
+    VarPtr grad(Var* out, Var* dout, Var* v, int v_index) override;
+    static VarPtr get_grad(Var* out, Var* dout, Var* v, int v_index, int dim, Var* y);
+    void infer_shape() override;
+    
+    const char* name() const override { return "arg_reduce"; }
+    DECLARE_jit_run;
+};
+
+} // jittor
