03451fce957015f9c098268903d019901412fca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:10 +0000

    Add package AirobotLibrary/1.1.5/AirobotLibrary-1.1.5.tar.gz

---
+++ packages/AirobotLibrary/AirobotLibrary-1.1.5.tar.gz/AirobotLibrary-1.1.5/src/airobotLibrary/__init__.py
@@ -0,0 +1,7 @@
+from .air_selenium import AirSelenium
+from DatabaseLibrary import DatabaseLibrary as AirDatabase
+from AppiumLibrary import AppiumLibrary as AirAppium
+from DiffLibrary import DiffLibrary as AirDiff
+from RequestsLibrary import RequestsLibrary as AirRequests
+from ArchiveLibrary import ArchiveLibrary as AirArchive
+from FtpLibrary import FtpLibrary as AirFtp
