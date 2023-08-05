9e4b86262954d97440e2995a970a4dd344e154b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package pps-tweet/2.0/pps_tweet-2.0.tar.gz

---
+++ packages/pps-tweet/pps_tweet-2.0.tar.gz/pps_tweet-2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='pps_tweet',
+      author='kerota',
+      author_email='hifuna2727@gmail.com',
+      url='https://twitter.com',
+      version='2.0',
+      description='Python tweet',
+      
+      
+     
+     )
