f9f735123a656d2774980e98c05011852160c877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:23 +0000

    Add package quickpython/0.1.4/quickpython-0.1.4-py3-none-any.whl

---
+++ packages/quickpython/quickpython-0.1.4-py3-none-any.whl/quickpython/__init__.py
@@ -0,0 +1,6 @@
+"""**quickpython**
+
+A retro interactive coding environment powered by Python and nostalgia
+"""
+
+__version__ = "0.1.4"
---
+++ packages/quickpython/quickpython-0.1.4-py3-none-any.whl/quickpython/examples/__init__.py
@@ -0,0 +1,25 @@
+from quickpython.examples import (
+    connect,
+    eightpuzzle,
+    hangman,
+    memory,
+    minesweeper,
+    simon,
+    tictactoe,
+    towers,
+    uno,
+    zigzag,
+)
+
+__all__ = [
+    "connect",
+    "eightpuzzle",
+    "hangman",
+    "memory",
+    "minesweeper",
+    "simon",
+    "tictactoe",
+    "towers",
+    "uno",
+    "zigzag",
+]
