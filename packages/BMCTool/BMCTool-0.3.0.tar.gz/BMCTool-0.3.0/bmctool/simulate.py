dbb2fc261dde967b09df4fef4ec12fa1fcda16d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:19 +0000

    Add package BMCTool/0.3.0/BMCTool-0.3.0.tar.gz

---
+++ packages/BMCTool/BMCTool-0.3.0.tar.gz/BMCTool-0.3.0/bmctool/simulate.py
@@ -0,0 +1,43 @@
+"""
+simulate.py
+    Script to run the BMCTool simulation based on a seq-file and a *.yaml config file.
+"""
+
+from os import path
+from pathlib import Path
+from bmctool.bmc_tool import BMCTool
+from bmctool.utils.eval import plot_z
+from bmctool.set_params import load_params
+
+
+def simulate(config_file: (str, Path) = None, seq_file: (str, Path) = None):
+    """
+    Function to run the BMCTool simulation based on a seq-file and a *.yaml config file..
+    :param config_file: Path of the config file (can be of type str or Path)
+    :param seq_file: Path of the seq file (can be of type str or Path)
+    """
+
+    if config_file is None:
+        config_file = Path(path.dirname(__file__)) / 'library' / 'sim-library' / 'config_example.yaml'
+
+    if seq_file is None:
+        seq_file = Path(path.dirname(__file__)) / 'library' / 'seq-library' / 'seq_example.seq'
+
+    # load config file(s)
+    sim_params = load_params(config_file)
+
+    # create BMCTool object and run simulation
+    sim = BMCTool(sim_params, seq_file)
+    sim.run()
+
+    # extract and plot z-spectrum
+    offsets, mz = sim.get_zspec()
+    fig = plot_z(mz=mz,
+                 offsets=offsets,
+                 invert_ax=True,
+                 plot_mtr_asym=True,
+                 title='Example WASABI spectrum')
+
+
+if __name__ == '__main__':
+    simulate()
---
+++ packages/BMCTool/BMCTool-0.3.0.tar.gz/BMCTool-0.3.0/bmctool/utils/seq/read.py
@@ -0,0 +1,43 @@
+"""
+read.py
+    Auxiliary functions for reading of seq files and seq file entries.
+"""
+from os import remove
+from pathlib import Path
+from pypulseq.Sequence.sequence import Sequence
+from bmctool.utils.seq.conversion import convert_seq_13_to_12
+
+
+def get_minor_version(seq_file: (str, Path)) -> int:
+    """
+    Reads minor version from a seq file.
+    :param seq_file: path to the sequence file to read into the Sequence object
+    :return version: version from the sequence file
+    """
+    with open(seq_file) as file:
+        for line in file:
+            if line.startswith('minor'):
+                return int(line[len('minor '):])
+
+
+def read_any_version(seq_file: (str, Path),
+                     seq: Sequence = None) \
+        -> Sequence:
+    """
+    Reads a sequence file (seq_file) independent of the (py)pulseq version.
+    :param seq_file: path to the sequence file to read into the Sequence object
+    :param seq: the sequence to read the seq file into. If not provided, a new Sequence object is instantiated
+    :return seq: Sequence object
+    """
+    version = get_minor_version(seq_file)
+    if not seq:
+        seq = Sequence()
+    if version == 2:
+        seq.read(seq_file)
+    elif version == 3:
+        tmp_file = convert_seq_13_to_12(seq_file, temp=True)
+        seq.read(tmp_file)
+        remove(tmp_file)
+    else:
+        raise ValueError('Version', version, 'can not be converted.')
+    return seq
