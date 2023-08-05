91ccc8c9fd13737e7826796e65d9d0e3ae2fd0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:07 +0000

    Add package django-read-only-admin/0.14.20/django_read_only_admin-0.14.20-py3-none-any.whl

---
+++ packages/django-read-only-admin/django_read_only_admin-0.14.20-py3-none-any.whl/read_only_admin/apps.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# django-read-only-admin
+# read_only_admin/apps.py
+
+
+from typing import List  # pylint: disable=W0611
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+__all__ = ["DjangoReadOnlyAdminConfig"]  # type: List[str]
+
+
+class DjangoReadOnlyAdminConfig(AppConfig):
+    """
+    Application config.
+    """
+
+    name = "read_only_admin"  # type: str
+    verbose_name = _("Django read only admin")  # type: str
