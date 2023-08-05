1bc192613fffcd2025b939b6049ec3d6dfb18865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package appsync-router/0.0.2/appsync_router-0.0.2-py2.py3-none-any.whl

---
+++ packages/appsync-router/appsync_router-0.0.2-py2.py3-none-any.whl/appsync_router/templates/resolver/__init__.py
@@ -0,0 +1,12 @@
+from os import path
+from pkgutil import walk_packages
+from appsync_router import Router
+
+
+module_dir = path.dirname(__file__)
+routes = Router()
+
+resolvers = [x.name for x in walk_packages(path=["resolvers"])]
+
+for x in resolvers:
+    exec(f"from .{x} import *")
