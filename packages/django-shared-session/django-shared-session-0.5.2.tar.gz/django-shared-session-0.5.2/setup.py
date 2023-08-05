4bdf721dfc0165323913dec26c4b150d31d112c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:22 +0000

    Add package django-shared-session/0.5.2/django-shared-session-0.5.2.tar.gz

---
+++ packages/django-shared-session/django-shared-session-0.5.2.tar.gz/django-shared-session-0.5.2/setup.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-shared-session',
+    version='0.5.2',
+    packages=['shared_session', 'shared_session.templatetags'],
+    include_package_data=True,
+    license='MPL',
+    description='Django session sharing across multiple domains running same application',
+    url='https://github.com/ViktorStiskala/django-shared-session',
+    author='Viktor Stískala',
+    author_email='viktor@stiskala.cz',
+    install_requires=[
+        'setuptools-git',
+        'django>=1.8',
+        'python-dateutil>=2.5',
+        'PyNaCl>=1.0.0',
+        'six>=1.11'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
