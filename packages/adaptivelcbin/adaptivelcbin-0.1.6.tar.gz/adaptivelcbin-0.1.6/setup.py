74c2ed700cb6d3a74382f3d7ba53922d006226e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package adaptivelcbin/0.1.6/adaptivelcbin-0.1.6.tar.gz

---
+++ packages/adaptivelcbin/adaptivelcbin-0.1.6.tar.gz/adaptivelcbin-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import sys
+
+setup_requires = ['setuptools >= 30.3.3']
+
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+
+
+setup(description="adaptivelcbin",
+      long_description=open('README.md').read(),
+      version='0.1.6',
+      include_package_data=True,
+      setup_requires=setup_requires)
