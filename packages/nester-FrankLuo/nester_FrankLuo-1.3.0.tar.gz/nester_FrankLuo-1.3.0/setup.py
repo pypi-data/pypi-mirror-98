30c04ce5be6c2f3bda6e66cc650987af3b1e7446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package nester-FrankLuo/1.3.0/nester_FrankLuo-1.3.0.tar.gz

---
+++ packages/nester-FrankLuo/nester_FrankLuo-1.3.0.tar.gz/nester_FrankLuo-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	 name         = 'nester_FrankLuo', 
+	  version      = '1.3.0',         
+	  py_modules   = ['nester'],        
+	  author       = 'hfpython',        
+	  author_email = 'FrankLuo666@gmail.com',        
+	  url          = 'http://www.headfirstlabs.com',        
+	  description  = 'A simple printer of nested lists', 
+)
