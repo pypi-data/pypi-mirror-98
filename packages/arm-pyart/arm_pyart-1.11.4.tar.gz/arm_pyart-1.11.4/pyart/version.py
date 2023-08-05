a5ef32f038096463c306b232ce4b06abebf26fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:53 +0000

    Add package arm-pyart/1.11.4/arm_pyart-1.11.4.tar.gz

---
+++ packages/arm-pyart/arm_pyart-1.11.4.tar.gz/arm_pyart-1.11.4/pyart/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYART SETUP.PY
+short_version = '1.11.4'
+version = '1.11.4'
+full_version = '1.11.4'
+git_revision = '27d5c9c425832d1044fbf34aca6d1fa9f42d99f9'
+release = True
+
+if not release:
+    version = full_version
