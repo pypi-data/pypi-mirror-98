51ef70b8ef5119185fa05583e56b0f6383ef7f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:14 +0000

    Add package bosch-thermostat-client/0.11.0/bosch-thermostat-client-0.11.0.tar.gz

---
+++ packages/bosch-thermostat-client/bosch-thermostat-client-0.11.0.tar.gz/bosch-thermostat-client-0.11.0/bosch_thermostat_client/const_old.py
@@ -0,0 +1,69 @@
+"""Constants used in Bosch thermostat."""
+
+
+DATA = "data"
+SUBMIT = "submit"
+
+
+
+
+DICT = "dict"
+
+# PRESETS = "presets"
+
+
+
+
+""" New refs scheme. """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DAYOFWEEK = "dayOfWeek"
+
+START = "start"
+STOP = "stop"
+
+
+OPEN = "open"
+SHORT = "short"
+
+
+CIRCUITS = "circuits"
+
+
+""" Section of gateway info consts. """
+
+
+
+
+
+
+
+
+SYSTEM_INTERFACES = "systemInterfaces"
+
+
+# SCHEDULE
+
+
+
+
+
+
+
+SENSORS_LIST = ["outdoor_t1", "hotWater_t2", "supply_t1_setpoint", "supply_t1",
+                "return", "healthStatus", "actualPower", "actualModulation",
+                "CHpumpModulation"]
+
+
