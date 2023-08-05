81e8cd3f02d52aff5a323f39dc3d473f32e4113e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:36 +0000

    Add package aioipfs/0.5.5/aioipfs-0.5.5.tar.gz

---
+++ packages/aioipfs/aioipfs-0.5.5.tar.gz/aioipfs-0.5.5/README.rst
@@ -0,0 +1,106 @@
+=======
+aioipfs
+=======
+
+:info: Asynchronous IPFS_ client library
+
+**aioipfs** is a python3 library providing an asynchronous API for IPFS_.
+Supported python versions: *3.6*, *3.7*, *3.8*, *3.9*
+
+.. image:: https://github.com/pinnaculum/aioipfs/workflows/aioipfs-build/badge.svg
+    :target: https://github.com/pinnaculum/aioipfs
+
+Installation
+============
+
+.. code-block:: shell
+
+    pip install aioipfs
+
+Usage examples
+==============
+
+Get an IPFS resource
+--------------------
+
+.. code-block:: python
+
+    import sys
+    import asyncio
+
+    import aioipfs
+
+    async def get(ipfshash):
+        client = aioipfs.AsyncIPFS()
+        await client.get(ipfshash, dstdir='.')
+        await client.close()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get(sys.argv[1]))
+    loop.close()
+
+Add some files
+--------------
+
+This example will import all files and directories specified on the command
+line. Note that the **add** API function is an asynchronous generator and
+therefore needs to be used with the *async for* syntax.
+
+.. code-block:: python
+
+    import sys
+    import asyncio
+
+    import aioipfs
+
+    async def add_files(files):
+        client = aioipfs.AsyncIPFS()
+
+        async for added_file in client.add(*files, recursive=True):
+            print('Imported file {0}, CID: {1}'.format(
+                added_file['Name'], added_file['Hash']))
+
+        await client.close()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(add_files(sys.argv[1:]))
+    loop.close()
+
+Pubsub service
+--------------
+
+.. code-block:: python
+
+    async def pubsub_serve(topic):
+        async with aioipfs.AsyncIPFS() as cli:
+            async for message in cli.pubsub.sub(topic):
+                print('Received message from', message['from'])
+                await cli.pubsub.pub(topic, message['data'])
+
+Features
+========
+
+Async file writing on get operations
+------------------------------------
+
+The **aiofiles** library is used to asynchronously write data retrieved from
+the IPFS daemon when using the */api/v0/get* API call, to avoid blocking the
+event loop. TAR extraction is done in asyncio's threadpool.
+
+Requirements
+============
+
+- Python >= 3.6, <= 3.9
+- aiohttp_
+- aiofiles_
+- yarl_
+
+.. _aiohttp: https://pypi.python.org/pypi/aiohttp
+.. _aiofiles: https://pypi.python.org/pypi/aiofiles
+.. _yarl: https://pypi.python.org/pypi/yarl
+.. _IPFS: https://ipfs.io
+
+License
+=======
+
+**aioipfs** is offered under the GNU Lesser GPL3 (LGPL3) license.
---
+++ packages/aioipfs/aioipfs-0.5.5.tar.gz/aioipfs-0.5.5/tests/test_client.py
@@ -0,0 +1,546 @@
+import pytest
+
+import tempfile
+import random
+import time
+import subprocess
+import os
+import os.path
+import json
+from pathlib import Path
+
+import asyncio
+import aioipfs
+
+
+def ipfs_getconfig_var(var):
+    sp_getconfig = subprocess.Popen(['ipfs', 'config',
+                                     var], stdout=subprocess.PIPE)
+    stdout, stderr = sp_getconfig.communicate()
+    return stdout.decode()
+
+
+@pytest.fixture
+def smalltar():
+    import tarfile
+    tpath = tempfile.mkstemp()[1]
+    tar = tarfile.open(tpath, 'w|')
+    tar.add(__file__)
+    tar.close()
+    yield tar, tpath
+    os.unlink(tpath)
+
+
+@pytest.fixture
+def testfile1(tmpdir):
+    filep = tmpdir.join('testfile1.txt')
+    filep.write('POIEKJDOOOPIDMWOPIMPOWE()=ds129084bjcy')
+    return filep
+
+
+@pytest.fixture
+def testfile2(tmpdir):
+    r = random.Random()
+    filep = tmpdir.join('testfile2.txt')
+    for i in range(0, 128):
+        filep.write(str(r.randint(i, i * 2)))
+    return filep
+
+
+@pytest.fixture
+def hiddendirs(tmpdir):
+    root = Path(str(tmpdir))
+    root.joinpath('a/b/.c').mkdir(parents=True)
+    root.joinpath('a/b/.c/file0').touch()
+    root.joinpath('d/.e/f').mkdir(parents=True)
+    root.joinpath('d/.e/f/.file3').touch()
+    root.joinpath('file1').touch()
+    root.joinpath('.file2').touch()
+    return str(root)
+
+
+@pytest.fixture
+def ignoredirs(tmpdir):
+    root = Path(str(tmpdir))
+    root.joinpath('a/b/.c').mkdir(parents=True)
+    root.joinpath('d/.e/f').mkdir(parents=True)
+    root.joinpath('d/.e/f/.file3').touch()
+    root.joinpath('file1').touch()
+    root.joinpath('README.txt').touch()
+    root.joinpath('README2.txt').touch()
+    root.joinpath('.file2').touch()
+    ign = root.joinpath('.gitignore')
+    ign.touch()
+    ign.write_text("README.txt\n.file2\na**\n\nd/.e/*/*\nd/.e/f\n")
+    return str(root)
+
+
+def ipfs_config(param, value):
+    os.system('ipfs config {0} "{1}"'.format(param, value))
+
+
+def ipfs_config_json(param, value):
+    os.system('ipfs config --json {0} "{1}"'.format(
+        param, json.dumps(value)))
+
+
+apiport = 9001
+gwport = 9080
+swarmport = 9002
+
+
+@pytest.fixture(scope='module')
+def ipfsdaemon():
+    # Starts a daemon on high port and temporary directory, yield it
+    # when started and shut it down on fixture's exit
+
+    tmpdir = tempfile.mkdtemp()
+
+    # Setup IPFS_PATH and initialize the repository
+    os.putenv('IPFS_PATH', tmpdir)
+    os.system('ipfs init -e')
+    ipfs_config('Addresses.API',
+                '/ip4/127.0.0.1/tcp/{0}'.format(apiport))
+    ipfs_config('Addresses.Gateway',
+                '/ip4/127.0.0.1/tcp/{0}'.format(gwport))
+    ipfs_config_json('Addresses.Swarm',
+                     '["/ip4/127.0.0.1/tcp/{0}"]'.format(swarmport))
+
+    # Empty bootstrap so we're not bothered
+    ipfs_config_json('Bootstrap', '[]')
+    ipfs_config_json('Experimental.Libp2pStreamMounting', 'true')
+    ipfs_config_json('Experimental.FilestoreEnabled', 'true')
+
+    # Run the daemon and wait a bit
+    sp = subprocess.Popen(['ipfs', 'daemon'],
+                          stdout=subprocess.PIPE)
+    time.sleep(1)
+
+    yield tmpdir, sp
+
+    time.sleep(0.5)
+    # Cleanup
+    sp.terminate()
+
+
+@pytest.fixture
+def ipfs_peerid(ipfsdaemon):
+    return ipfs_getconfig_var('Identity.PeerID').strip()
+
+
+@pytest.fixture()
+def iclient(event_loop):
+    return aioipfs.AsyncIPFS(port=apiport, loop=event_loop)
+
+
+class TestAsyncIPFS:
+    @pytest.mark.asyncio
+    async def test_basic(self, event_loop, ipfsdaemon, iclient):
+        tmpdir, sp = ipfsdaemon
+        await iclient.id()
+        await iclient.core.version()
+        await iclient.commands()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_bootstrap(self, event_loop, ipfsdaemon, iclient):
+        tmpdir, sp = ipfsdaemon
+        await iclient.bootstrap.list()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_swarm(self, event_loop, ipfsdaemon, iclient):
+        await iclient.swarm.peers()
+        await iclient.swarm.addrs()
+        await iclient.swarm.addrs_local()
+        await iclient.swarm.addrs_listen()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_refs(self, event_loop, ipfsdaemon, iclient):
+        async for refobj in iclient.refs.local():
+            assert 'Ref' in refobj
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_block1(self, event_loop, ipfsdaemon, iclient, testfile1):
+        reply = await iclient.block.put(testfile1)
+        data = await iclient.block.get(reply['Key'])
+        assert data.decode() == testfile1.read()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_add(self, event_loop, ipfsdaemon, iclient, testfile1,
+                       testfile2):
+        count = 0
+        async for added in iclient.add(str(testfile1)):
+            assert 'Hash' in added
+            count += 1
+
+        assert count == 1
+        count = 0
+        all = [[str(testfile1), str(testfile2)]]
+
+        async for added in iclient.add(*all):
+            assert 'Hash' in added
+            count += 1
+
+        assert count == 2
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_hidden(self, event_loop, ipfsdaemon, iclient, hiddendirs):
+        async for added in iclient.add(hiddendirs, hidden=False):
+            parts = added['Name'].split('/')
+            for part in parts:
+                assert not part.startswith('.')
+
+        names = []
+        async for added in iclient.add(hiddendirs, hidden=True):
+            names.append(added['Name'])
+
+        assert 'test_hidden0/d/.e/f/.file3' in names
+        assert 'test_hidden0/a/b/.c' in names
+
+    @pytest.mark.asyncio
+    async def test_ignorerules(self, event_loop, ipfsdaemon, iclient,
+                               ignoredirs):
+        names = []
+        async for added in iclient.add(ignoredirs,
+                                       ignore_rules_path='.gitignore',
+                                       hidden=True):
+            names.append(added['Name'])
+
+        assert 'test_ignorerules0/.gitignore' in names
+        assert 'test_ignorerules0/.file2' not in names
+        assert 'test_ignorerules0/a' not in names
+        assert 'test_ignorerules0/d/.e/f' not in names
+        assert 'test_ignorerules0/README.txt' not in names
+        assert 'test_ignorerules0/README2.txt' in names
+
+        names = []
+        async for added in iclient.add(ignoredirs,
+                                       ignore_rules_path='.gitignore',
+                                       hidden=False):
+            names.append(added['Name'])
+
+        assert 'test_ignorerules0/.gitignore' in names
+
+    @pytest.mark.asyncio
+    async def test_addtar(self, event_loop, ipfsdaemon, iclient,
+                          tmpdir, smalltar):
+        tar, tarpath = smalltar
+        reply = await iclient.tar.add(tarpath)
+        tarhash = reply['Hash']
+        fetched = await iclient.tar.cat(tarhash)
+        f = tmpdir.join('new.tar')
+        f.write(fetched)
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('order', ['gin', 'tonic'])
+    @pytest.mark.parametrize('second', ['beer', 'wine'])
+    async def test_addjson(self, event_loop, ipfsdaemon, iclient,
+                           order, second):
+        json1 = {
+            'random': 'stuff',
+            'order': order,
+            'second': second
+        }
+
+        reply = await iclient.add_json(json1)
+        h = reply['Hash']
+
+        data = await iclient.cat(h)
+        assert data.decode() == json.dumps(json1)
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('data', [b'234098dsfkj2doidf0'])
+    async def test_addbytes(self, event_loop, ipfsdaemon, iclient, data):
+        reply = await iclient.add_bytes(data, cid_version=1, hash='sha2-256')
+        assert reply['Hash'] == \
+            'bafkreiewqrl3s3cgd4ll3wybtrxv7futfksuylocfxzlugbjparmyyt6eq'
+
+        catD = await iclient.cat(reply['Hash'])
+        assert catD == data
+
+        reply = await iclient.add_bytes(data, cid_version=1, hash='sha2-512')
+        assert reply['Hash'] == 'bafkrgqdao6vujlzh4z6o7mzgv3jnydftv2of5jy32yufswk7bnvwaq7oyaizo6gnditr4okfphi2cguz2cack27rsjfzuybm57knagzjl6m34'  # noqa
+
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('data', [b'234098dsfkj2doidf0'])
+    async def test_dag(self, event_loop, ipfsdaemon, iclient, tmpdir, data):
+        # More tests needed here
+        entry = await iclient.add_bytes(data)
+        jsondag = {'dag': {'/': entry['Hash']}}
+        filedag = tmpdir.join('jsondag.txt')
+        filedag.write(json.dumps(jsondag))
+
+        reply = await iclient.dag.put(filedag)
+        assert 'Cid' in reply
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('data', [b'234098dsfkj2doidf0'])
+    async def test_car(self, event_loop, ipfsdaemon, iclient, tmpdir, data):
+        entry = await iclient.add_bytes(data)
+        jsondag = {'dag': {'/': entry['Hash']}}
+        filedag = tmpdir.join('jsondag.txt')
+        filedag.write(json.dumps(jsondag))
+
+        reply = await iclient.dag.put(filedag)
+        assert 'Cid' in reply
+
+        export = await iclient.dag.car_export(reply['Cid']['/'])
+        assert isinstance(export, bytes)
+
+        imported = await iclient.dag.car_import(export)
+        assert imported['Root']['Cid']['/'] is not None
+        assert reply['Cid']['/'] == imported['Root']['Cid']['/']
+
+        filecar = tempfile.mkstemp()[1]
+        with open(filecar, 'wb') as fd:
+            fd.write(export)
+
+        imported = await iclient.dag.car_import(filecar)
+        assert imported['Root']['Cid']['/'] is not None
+        assert reply['Cid']['/'] == imported['Root']['Cid']['/']
+
+        os.unlink(filecar)
+
+    @pytest.mark.asyncio
+    async def test_diag(self, event_loop, ipfsdaemon, iclient, tmpdir):
+        reply = await iclient.diag.sys()
+        assert 'diskinfo' in reply
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('data', [b'0123456789'])
+    async def test_catoffset(self, event_loop, ipfsdaemon, iclient,
+                             tmpdir, data):
+        entry = await iclient.add_bytes(data)
+        raw = await iclient.cat(entry['Hash'], offset=4)
+        assert raw.decode() == '456789'
+        raw = await iclient.cat(entry['Hash'], offset=2, length=3)
+        assert raw.decode() == '234'
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_multiget(self, event_loop, ipfsdaemon,
+                            iclient, testfile2, tmpdir):
+        hashes = []
+
+        # Create 16 variations of testfile2 and add them to the node
+        for idx in range(0, 16):
+            testfile2.write('ABCD' + str(idx))
+            async for reply in iclient.add(str(testfile2)):
+                hashes.append(reply['Hash'])
+
+        # Get them all back concurrently
+        tasks = [iclient.get(hash, dstdir=tmpdir) for hash in hashes]
+        await asyncio.gather(*tasks)
+
+        for hash in hashes:
+            async for result in iclient.getgen(hash, dstdir=tmpdir):
+                status, read, clength = result
+                assert status in [0, 1]
+
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_pubsub(self, event_loop, ipfsdaemon, iclient):
+        # because we don't have pubsub enabled in the daemon with
+        # --enable-pubsub-experiment this should raise an exception
+        with pytest.raises(aioipfs.APIError):
+            await iclient.pubsub.ls()
+            await iclient.pubsub.peers()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_stats(self, event_loop, ipfsdaemon, iclient):
+        await iclient.stats.bw()
+        await iclient.stats.bitswap()
+        await iclient.stats.repo()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('protocol', ['/x/test'])
+    @pytest.mark.parametrize('address', ['/ip4/127.0.0.1/tcp/10000'])
+    async def test_p2p(self, event_loop, ipfsdaemon, iclient, protocol,
+                       address):
+        await iclient.p2p.listen(protocol, address)
+        listeners = await iclient.p2p.listener_ls(headers=True)
+        assert len(listeners['Listeners']) > 0
+
+        listener = listeners['Listeners'].pop()
+        assert listener['Protocol'] == protocol
+
+        if 'Address' in listener:
+            # Pre 0.4.18
+            assert listener['Address'] == address
+        elif 'TargetAddress' in listener:
+            # Post 0.4.18
+            assert listener['TargetAddress'] == address
+
+        await iclient.p2p.listener_close(protocol)
+        listeners = await iclient.p2p.listener_ls()
+        assert listeners['Listeners'] is None
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('keyname', ['key1'])
+    @pytest.mark.parametrize('keysize', [2048, 4096])
+    async def test_keys(self, event_loop, ipfsdaemon, iclient,
+                        keyname, keysize):
+        reply = await iclient.key.gen(keyname, size=keysize)
+        assert reply['Name'] == keyname
+        key_hash = reply['Id']
+
+        reply = await iclient.key.list()
+        # initial peer key + the one we just made
+        assert len(reply['Keys']) == 2
+
+        removed = await iclient.key.rm(keyname)
+        assert removed['Keys'].pop()['Id'] == key_hash
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_bitswap(self, event_loop, ipfsdaemon, iclient):
+        await iclient.bitswap.wantlist()
+        stats = await iclient.bitswap.stat()
+        assert 'Wantlist' in stats
+        assert 'DataSent' in stats
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_filestore(self, event_loop, ipfsdaemon, iclient):
+        await iclient.filestore.dups()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('obj', [b'0123456789'])
+    async def test_files_rw(self, event_loop, ipfsdaemon, iclient, obj,
+                            testfile1, testfile2):
+        # Write obj (bytes) to /test1
+        await iclient.files.write('/test1', obj, create=True)
+        data = await iclient.files.read('/test1')
+        assert data == obj
+
+        # Write testfile1 to /test2
+        await iclient.files.write('/test2', str(testfile1), create=True)
+        data = await iclient.files.read('/test2')
+        filedata = testfile1.read()
+        assert data.decode() == filedata
+
+        # Write testfile2 to /test3, then write 123 at some offset
+        # and read the file again starting from that offset
+        await iclient.files.write('/test3', str(testfile2), create=True)
+        otro = b'123'
+        await iclient.files.write('/test3', otro, create=True,
+                                  offset=5)
+        data = await iclient.files.read('/test3', offset=5, count=3)
+        assert data == otro
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('obj1', [b'0123456789'])
+    @pytest.mark.parametrize('obj2', [b'0a1b2c3d4e5'])
+    async def test_object(self, event_loop, ipfsdaemon, iclient, obj1, obj2,
+                          testfile2):
+        """ Unsure if this is correct """
+        obj1Ent = await iclient.add_bytes(obj1)
+        obj2Ent = await iclient.add_bytes(obj2)
+        obj = await iclient.object.new()
+        r1 = await iclient.object.patch.add_link(obj['Hash'], 'obj1',
+                                                 obj1Ent['Hash'])
+        r2 = await iclient.object.patch.add_link(r1['Hash'], 'obj2',
+                                                 obj2Ent['Hash'])
+        diff = await iclient.object.diff(r2['Hash'], obj['Hash'], verbose=True)
+        assert 'Changes' in diff
+        assert len(diff['Changes']) == 2
+
+        dag = await iclient.object.get(r2['Hash'])
+        assert len(dag['Links']) == 2
+        data1 = await iclient.cat(dag['Links'][0]['Hash'])
+        data2 = await iclient.cat(dag['Links'][1]['Hash'])
+
+        assert data1 == obj1
+        assert data2 == obj2
+
+        with pytest.raises(aioipfs.APIError):
+            await iclient.object.patch.rm_link(obj['Hash'], 'obj1')
+
+        rm = await iclient.object.patch.rm_link(r2['Hash'], 'obj1')
+        dag = await iclient.object.get(rm['Hash'])
+        assert len(dag['Links']) == 1
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_config(self, event_loop, ipfsdaemon, iclient, tmpdir):
+        conf = await iclient.config.show()
+        assert 'API' in conf
+        sameconf = tmpdir.join('config.json')
+        sameconf.write(json.dumps(conf))
+        await iclient.config.replace(str(sameconf))
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    async def test_cidapi(self, event_loop, ipfsdaemon, iclient, testfile1):
+        async for added in iclient.add(str(testfile1), cid_version=1):
+            multihash = added['Hash']
+            reply = await iclient.cid.base32(multihash)
+            assert reply['CidStr'] == multihash
+            assert 'Formatted' in reply
+
+            await iclient.cid.format(multihash, version=0)
+
+        await iclient.cid.codecs()
+        await iclient.cid.bases()
+        await iclient.cid.hashes()
+        await iclient.close()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('srvname', ['mysrv1'])
+    @pytest.mark.parametrize('srvendpoint', ['http://localhost:9580'])
+    async def _no_test_pin_remote(self, event_loop, ipfsdaemon, iclient,
+                              srvname, srvendpoint):
+        res = await iclient.pin.remote.service.add(
+            srvname,
+            srvendpoint,
+            'mykey'
+        )
+
+        res = await iclient.pin.remote.service.ls()
+        assert 'RemoteServices' in res
+        service = res['RemoteServices'].pop()
+        assert service['Service'] == srvname
+        assert service['ApiEndpoint'] == srvendpoint
+
+        entry = await iclient.core.add_bytes(b'ABCD')
+
+        # Try a remote pin (will fail, service does not exist)
+        try:
+            res = await iclient.pin.remote.add(
+                srvname,
+                f'/ipfs/{entry["Hash"]}'
+            )
+        except aioipfs.APIError:
+            pass
+
+        try:
+            async for entry in iclient.pin.remote.ls(
+                srvname,
+                status=['queued']
+            ):
+                print(entry)
+        except aioipfs.APIError as e:
+            print(e.message)
+
+        await iclient.pin.remote.service.rm(srvname)
+        res = await iclient.pin.remote.service.ls()
+        assert len(res['RemoteServices']) == 0
+
+        await iclient.close()
