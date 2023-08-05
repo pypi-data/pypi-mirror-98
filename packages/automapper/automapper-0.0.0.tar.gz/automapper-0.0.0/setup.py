da0e72f648d77e063f2e5f7ca65e81f52fe7e379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:59 +0000

    Add package automapper/0.0.0/automapper-0.0.0.tar.gz

---
+++ packages/automapper/automapper-0.0.0.tar.gz/automapper-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='automapper',
+    packages=["automapper"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
