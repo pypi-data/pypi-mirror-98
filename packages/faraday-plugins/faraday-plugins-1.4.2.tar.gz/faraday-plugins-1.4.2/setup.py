67d6414a9a5f7c270e73285e7efe90d4f0e66547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:16 +0000

    Add package faraday-plugins/1.4.2/faraday-plugins-1.4.2.tar.gz

---
+++ packages/faraday-plugins/faraday-plugins-1.4.2.tar.gz/faraday-plugins-1.4.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from re import search
+
+with open('faraday_plugins/__init__.py', 'rt', encoding='utf8') as f:
+    version = search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+
+install_requires = [
+    'Click',
+    'simplejson',
+    'requests',
+    'lxml',
+    'html2text',
+    'beautifulsoup4',
+    'pytz',
+    'python-dateutil',
+    'colorama'
+]
+
+
+setup(
+    name='faraday-plugins',
+    version=version,
+    packages=find_packages(include=['faraday_plugins', 'faraday_plugins.*']),
+    url='',
+    license='',
+    author='Faradaysec',
+    author_email='devel@faradaysec.com',
+    description='Faraday plugins package',
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+            'console_scripts': [
+                'faraday-plugins=faraday_plugins.commands:cli',
+            ],
+        },
+)
