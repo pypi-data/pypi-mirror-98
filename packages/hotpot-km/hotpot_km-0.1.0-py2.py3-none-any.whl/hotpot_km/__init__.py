a7229f569a0223724bba848ae5865aca78962453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package hotpot-km/0.1.0/hotpot_km-0.1.0-py2.py3-none-any.whl

---
+++ packages/hotpot-km/hotpot_km-0.1.0-py2.py3-none-any.whl/hotpot_km/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+# Copyright (c) Vidar Tonaas Fauske.
+# Distributed under the terms of the Modified BSD License.
+"""Hotpot - Jupyter kernel manager helpers
+"""
+
+from ._version import __version__
+
+from .limited import MaximumKernelsException, LimitedKernelManager
+from .pooled import PooledKernelManager
+
+__all__ = [
+    '__version__',
+    'MaximumKernelsException',
+    'PooledKernelManager',
+    'LimitedKernelManager',
+]
+
+try:
+    from .mapping import PooledMappingKernelManager
+    __all__.append('PooledMappingKernelManager')
+except ImportError:
+    pass
