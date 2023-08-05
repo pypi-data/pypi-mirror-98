8b934b545d71941414460b13b017a0f002074065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:45 +0000

    Add package isabelle-client/0.1.4/isabelle_client-0.1.4-py3-none-any.whl

---
+++ packages/isabelle-client/isabelle_client-0.1.4-py3-none-any.whl/isabelle_client/isabelle__client.py
@@ -0,0 +1,361 @@
+"""
+Copyright 2021 Boris Shminke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import asyncio
+import json
+from logging import Logger
+from typing import Dict, List, Optional, Union
+
+from isabelle_client.socket_communication import (
+    IsabelleResponse,
+    get_final_message,
+)
+
+
+class IsabelleClient:
+    """ a TCP client for an ``isabelle`` server """
+
+    def __init__(
+        self,
+        address: str,
+        port: int,
+        password: str,
+        logger: Optional[Logger] = None,
+    ):
+        """
+        :param address: IP or a domain name
+        :param port: a port number on which the server listens
+        :param password: a password to access the server through TCP
+        """
+        self.address = address
+        self.port = port
+        self.password = password
+        self.logger = logger
+
+    async def execute_command(
+        self,
+        command: str,
+        asynchronous: bool = True,
+    ) -> IsabelleResponse:
+        """
+        executes a command and waits for results
+
+        >>> from unittest.mock import Mock, patch, AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> res = AsyncMock(return_value=IsabelleResponse(
+        ...    "FINISHED", '{"session_id": "session_id__42"}', 42
+        ... ))
+        >>> test_writer = Mock()
+        >>> test_writer.drain = AsyncMock()
+        >>> connection = AsyncMock(return_value=(Mock(), test_writer))
+        >>> with (
+        ...     patch("asyncio.open_connection", connection),
+        ...     patch(
+        ...         "isabelle_client.isabelle__client.get_final_message",
+        ...         res
+        ...     )
+        ... ):
+        ...    test_response = asyncio.run(
+        ...     isabelle_client.execute_command("test")
+        ... )
+        >>> print(test_response.response_type)
+        FINISHED
+        >>> print(test_response.response_body)
+        {"session_id": "session_id__42"}
+        >>> print(test_response.response_length)
+        42
+        >>> print(connection.mock_calls)
+        [call('localhost', 1000)]
+        >>> print(test_writer.write.mock_calls)
+        [call(b'test\\ntest\\n')]
+
+        :param command: a full text of a command to ``isabelle``
+        :param asynchronous: if ``False``, waits for ``OK``; else waits for
+            ``FINISHED``
+        :returns: ``isabelle`` server response
+        """
+        final_message = (
+            {"FINISHED", "FAILED", "ERROR"}
+            if asynchronous
+            else {"OK", "ERROR"}
+        )
+        reader, writer = await asyncio.open_connection(self.address, self.port)
+        writer.write(f"{self.password}\n{command}\n".encode("utf-8"))
+        await writer.drain()
+        response = await get_final_message(reader, final_message, self.logger)
+        return response
+
+    def session_build(
+        self,
+        session: str,
+        dirs: List[str] = None,
+        options: List[str] = None,
+    ) -> IsabelleResponse:
+        """
+        build a session from ROOT file
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...     "FINISHED", '{"ok": true}', None
+        ...     )
+        ... )
+        >>> print(isabelle_client.session_build(
+        ...     session="test_session", dirs=["."], options=[]
+        ... ))
+        FINISHED {"ok": true}
+
+        :param session: a name of the session from ROOT file
+        :param dirs: where to look for ROOT files
+        :param options: additional options
+        :returns: an ``isabelle`` response
+        """
+        arguments: Dict[str, Union[str, List[str]]] = {"session": session}
+        if dirs is not None:
+            arguments["dirs"] = dirs
+        if options is not None:
+            arguments["options"] = options
+        response = asyncio.run(
+            self.execute_command(f"session_build {json.dumps(arguments)}")
+        )
+        return response
+
+    def session_start(self, session_image: str = "HOL") -> str:
+        """
+        start a new session
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...     "FINISHED", '{"session_id": "test_session"}', None
+        ...     )
+        ... )
+        >>> print(isabelle_client.session_start())
+        test_session
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse("OK", "OK")
+        ... )
+        >>> print(isabelle_client.session_start())
+        Traceback (most recent call last):
+            ...
+        ValueError: Unexpected response type: OK
+
+        :param session_image: a name of a session image
+        :returns: a ``session_id``
+        """
+        arguments = json.dumps({"session": session_image})
+        response = asyncio.run(
+            self.execute_command(f"session_start {arguments}")
+        )
+        if response.response_type == "FINISHED":
+            return json.loads(response.response_body)["session_id"]
+        raise ValueError(f"Unexpected response type: {response.response_type}")
+
+    def session_stop(self, session_id: str) -> IsabelleResponse:
+        """
+        stop session with given ID
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse("FAILED", "")
+        ... )
+        >>> test_response = isabelle_client.session_stop("test")
+        >>> print(test_response.response_type)
+        FAILED
+
+        :param session_id: a string ID of a session
+        :returns: ``isabelle`` server response
+        """
+        arguments = json.dumps({"session_id": session_id})
+        response = asyncio.run(
+            self.execute_command(f"session_stop {arguments}")
+        )
+        return response
+
+    def use_theories(
+        self,
+        theories: List[str],
+        session_id: Optional[str] = None,
+        master_dir: Optional[str] = None,
+        watchdog_timeout: Optional[int] = None,
+    ) -> IsabelleResponse:
+        """
+        run the engine on theory files
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "FINISHED", '{"session_id": "test"}'
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.use_theories(
+        ...     ["test"], master_dir="test", watchdog_timeout=0
+        ... )
+        >>> print(test_response.response_type)
+        FINISHED
+
+        :param theories: names of theory files (without extensions!)
+        :param session_id: an ID of a session; if ``None``, a new session is
+            created and then destroyed after trying to process theories
+        :param master_dir: where to look for theory files; if ``None``, uses a
+            temp folder of the session
+        :param watchdog_timeout: for how long to wait a response from server
+        :returns: ``isabelle`` server response
+        """
+        new_session_id = (
+            self.session_start() if session_id is None else session_id
+        )
+        arguments: Dict[str, Union[List[str], int, str]] = {
+            "session_id": new_session_id,
+            "theories": theories,
+        }
+        if watchdog_timeout is not None:
+            arguments["watchdog_timeout"] = watchdog_timeout
+        if master_dir is not None:
+            arguments["master_dir"] = master_dir
+        response = asyncio.run(
+            self.execute_command(f"use_theories {json.dumps(arguments)}")
+        )
+        if session_id is None:
+            self.session_stop(new_session_id)
+        return response
+
+    def echo(self, message: str) -> IsabelleResponse:
+        """
+        asks a server to echo a message
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "OK", json.dumps("test message")
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.echo("test_message")
+        >>> print(test_response.response_body)
+        "test message"
+
+        :param message: any text
+        :returns: ``isabelle`` server response
+        """
+        response = asyncio.run(
+            self.execute_command(
+                f"echo {json. dumps(message)}", asynchronous=False
+            )
+        )
+        return response
+
+    def help(self) -> IsabelleResponse:
+        """
+        asks a server to display the list of available commands
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "OK", json.dumps(["help", "echo"])
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.help()
+        >>> print(test_response.response_body)
+        ["help", "echo"]
+
+        :returns: ``isabelle`` server response
+        """
+        response = asyncio.run(
+            self.execute_command("help", asynchronous=False)
+        )
+        return response
+
+    def purge_theories(
+        self, session_id: str, theories: List[str]
+    ) -> IsabelleResponse:
+        """
+        asks a server to purge listed theories from it
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "OK", json.dumps({"purged": [], "retained": []})
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.purge_theories("test", [])
+        >>> print(test_response.response_body)
+        {"purged": [], "retained": []}
+
+        :param session_id: an ID of the session from which to purge theories
+        :param theories: a list of theory names to purge from the server
+        :returns: ``isabelle`` server response
+        """
+        arguments = {"session_id": session_id, "theories": theories}
+        response = asyncio.run(
+            self.execute_command(
+                f"purge_theories {json.dumps(arguments)}", asynchronous=False
+            )
+        )
+        return response
+
+    def cancel(self, task: str) -> IsabelleResponse:
+        """
+        asks a server to try to cancel a task with a given ID
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "OK", ""
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.cancel("test_task")
+        >>> print(test_response.response_body)
+        <BLANKLINE>
+
+        :param task: a task ID
+        :returns: ``isabelle`` server response
+        """
+        arguments = {"task": task}
+        response = asyncio.run(
+            self.execute_command(
+                f"cancel {json.dumps(arguments)}", asynchronous=False
+            )
+        )
+        return response
+
+    def shutdown(self) -> IsabelleResponse:
+        """
+        asks a server to shutdown immediately
+
+        >>> from unittest.mock import AsyncMock
+        >>> isabelle_client = IsabelleClient("localhost", 1000, "test")
+        >>> isabelle_client.execute_command = AsyncMock(
+        ...     return_value=IsabelleResponse(
+        ...         "OK", ""
+        ...     )
+        ... )
+        >>> test_response = isabelle_client.shutdown()
+        >>> print(test_response.response_body)
+        <BLANKLINE>
+
+        :returns: ``isabelle`` server response
+        """
+        response = asyncio.run(
+            self.execute_command("shutdown", asynchronous=False)
+        )
+        return response
---
+++ packages/isabelle-client/isabelle_client-0.1.4-py3-none-any.whl/isabelle_client/utils.py
@@ -0,0 +1,95 @@
+"""
+Copyright 2021 Boris Shminke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import asyncio
+import re
+from typing import Tuple
+
+from isabelle_client.isabelle__client import IsabelleClient
+
+
+def get_isabelle_client(server_info: str) -> IsabelleClient:
+    """
+    get an instance of ``IsabelleClient`` from server info
+
+    >>> server_inf = 'server "test" = 127.0.0.1:10000 (password "pass")'
+    >>> print(get_isabelle_client(server_inf).port)
+    10000
+    >>> get_isabelle_client("wrong")
+    Traceback (most recent call last):
+        ...
+    ValueError: Unexpected server info: wrong
+
+    :param server_info: a line returned by a server on start
+    :returns: an ``isabelle`` client
+    """
+    match = re.compile(
+        r"server \".*\" = (.*):(.*) \(password \"(.*)\"\)"
+    ).match(server_info)
+    if match is None:
+        raise ValueError(f"Unexpected server info: {server_info}")
+    address, port, password = match.groups()
+    isabelle_client = IsabelleClient(address, int(port), password)
+    return isabelle_client
+
+
+async def async_start_isabelle_server() -> Tuple[
+    str, asyncio.subprocess.Process
+]:
+    """
+    a technical function
+
+    >>> from unittest.mock import patch, AsyncMock, Mock
+    >>> server = Mock()
+    >>> server.stdout = Mock()
+    >>> server.stdout.readline = AsyncMock(return_value=b"test_info")
+    >>> server_builder = AsyncMock(return_value=server)
+    >>> with patch("asyncio.create_subprocess_exec", server_builder):
+    ...     print(asyncio.run(async_start_isabelle_server())[0])
+    test_info
+    >>> server_builder.assert_awaited_once()
+    >>> server.stdout.readline.assert_awaited_once()
+    >>> server.stdout = None
+    >>> server_builder = AsyncMock(return_value=server)
+    >>> with patch("asyncio.create_subprocess_exec", server_builder):
+    ...     print(asyncio.run(async_start_isabelle_server())[0])
+    Traceback (most recent call last):
+        ...
+    ValueError: Failed to start server
+    """
+    isabelle_server = await asyncio.create_subprocess_exec(
+        "isabelle", "server", stdout=asyncio.subprocess.PIPE
+    )
+    if isabelle_server.stdout is None:
+        raise ValueError("Failed to start server")
+    server_info = (await isabelle_server.stdout.readline()).decode("utf-8")
+    return server_info, isabelle_server
+
+
+def start_isabelle_server() -> Tuple[str, asyncio.subprocess.Process]:
+    """
+    start ``isabelle`` server
+
+    >>> from unittest.mock import patch, AsyncMock
+    >>> with patch(
+    ...     "isabelle_client.utils.async_start_isabelle_server",
+    ...     AsyncMock(return_value="TestIsabelleServer")
+    ... ):
+    ...     print(start_isabelle_server())
+    TestIsabelleServer
+
+    :return: a line of server info and server process
+    """
+    return asyncio.run(async_start_isabelle_server())
