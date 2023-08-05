e5bcf695cb51143392c336dc2c4400e2e1c24061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package swifttools/1.8/swifttools-1.8.tar.gz

---
+++ packages/swifttools/swifttools-1.8.tar.gz/swifttools-1.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PKGNAME = "swifttools"
+
+setuptools.setup(
+    name=PKGNAME,
+    version="1.8",
+    author="Phil Evans",
+    author_email="pae9@leicester.ac.uk",
+    description="Tools for users of the Swift satellite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/DrPhilEvans/swifttools",
+    packages=[PKGNAME] + [f'{PKGNAME}.{p}' for p in setuptools.find_packages(where=PKGNAME)],
+    install_requires=['numpy', 'requests', 'python-jose', 'pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
