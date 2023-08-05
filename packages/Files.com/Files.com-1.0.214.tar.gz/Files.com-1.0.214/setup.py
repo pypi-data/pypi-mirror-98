ca8f4aab8294f3a0abf51c3c50c2663b7e3cd683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:17 +0000

    Add package Files.com/1.0.214/Files.com-1.0.214.tar.gz

---
+++ packages/Files.com/Files.com-1.0.214.tar.gz/Files.com-1.0.214/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('_VERSION') as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='Files.com',
+    version=version,
+    license='MIT',
+    description="Python bindings for the Files.com API",
+    packages = find_packages(exclude = ('tests', 'tests.*', 'doc')),
+    install_requires=[
+        'requests >= 2.20; python_version >= "3.0"',
+    ],
+    python_requires="!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+)
