4653f7ff6c0c1fc91459bbd011a92f465d4f8ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:17 +0000

    Add package netw0rk/1.11.3/netw0rk-1.11.3.tar.gz

---
+++ packages/netw0rk/netw0rk-1.11.3.tar.gz/netw0rk-1.11.3/netw0rk/classes/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from netw0rk.classes.firewall import firewall
+from netw0rk.classes.network import network
+from netw0rk.classes import curl
