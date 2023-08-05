9108e8fab28832b452481247808259329d0261d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:56 +0000

    Add package jupyterlab-fasta/3.1.0/jupyterlab_fasta-3.1.0-py3-none-any.whl

---
+++ packages/jupyterlab-fasta/jupyterlab_fasta-3.1.0-py3-none-any.whl/jupyterlab_fasta-3.1.0.data/data/share/jupyter/labextensions/@jupyterlab/fasta-extension/static/style_index_js.7fe23c0c57f18a91dbb0.js
@@ -0,0 +1,92 @@
+(self["webpackChunk_jupyterlab_fasta_extension"] = self["webpackChunk_jupyterlab_fasta_extension"] || []).push([["style_index_js"],{
+
+/***/ "../../node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!******************************************************************!*\
+  !*** ../../node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \******************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "../../node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/getUrl.js */ "../../node_modules/css-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _msa_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./msa.svg */ "./style/msa.svg");
+/* harmony import */ var _msa_svg__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_msa_svg__WEBPACK_IMPORTED_MODULE_3__);
+// Imports
+
+
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()((_msa_svg__WEBPACK_IMPORTED_MODULE_3___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/**\n  Copyright (c) Jupyter Development Team.\n  Distributed under the terms of the Modified BSD License.\n*/\n\n/* Add CSS variables to :root */\n:root {\n  --jp-icon-msa: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\n}\n\n/* Base styles */\n.jp-RenderedMSA {\n  /*width: 100%;\n  height: 100%;*/\n  margin: 10px;\n  overflow-y: auto;\n}\n\n/* Document styles */\n.jp-MimeDocument .jp-RenderedMSA {\n}\n\n/* Output styles */\n.jp-OutputArea .jp-RenderedMSA {\n}\n\n/* Document icon */\n.jp-MSAIcon {\n  background-image: var(--jp-icon-msa);\n  /*background-size: 24px;\n  background-position: center !important;*/\n}\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;;;CAGC;;AAED,+BAA+B;AAC/B;EACE,sDAA+B;AACjC;;AAEA,gBAAgB;AAChB;EACE;gBACc;EACd,YAAY;EACZ,gBAAgB;AAClB;;AAEA,oBAAoB;AACpB;AACA;;AAEA,kBAAkB;AAClB;AACA;;AAEA,kBAAkB;AAClB;EACE,oCAAoC;EACpC;0CACwC;AAC1C","sourcesContent":["/**\n  Copyright (c) Jupyter Development Team.\n  Distributed under the terms of the Modified BSD License.\n*/\n\n/* Add CSS variables to :root */\n:root {\n  --jp-icon-msa: url('./msa.svg');\n}\n\n/* Base styles */\n.jp-RenderedMSA {\n  /*width: 100%;\n  height: 100%;*/\n  margin: 10px;\n  overflow-y: auto;\n}\n\n/* Document styles */\n.jp-MimeDocument .jp-RenderedMSA {\n}\n\n/* Output styles */\n.jp-OutputArea .jp-RenderedMSA {\n}\n\n/* Document icon */\n.jp-MSAIcon {\n  background-image: var(--jp-icon-msa);\n  /*background-size: 24px;\n  background-position: center !important;*/\n}\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../../node_modules/css-loader/dist/cjs.js!./base.css */ "../../node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default, options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});
+
+/***/ }),
+
+/***/ "./style/msa.svg":
+/*!***********************!*\
+  !*** ./style/msa.svg ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = "data:image/svg+xml,%3Csvg width='12px' height='12px' viewBox='0 0 12 12' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E %3C!-- Generator: Sketch 46.2 (44496) - http://www.bohemiancoding.com/sketch --%3E %3Ctitle%3Efasta%3C/title%3E %3Cdesc%3ECreated with Sketch.%3C/desc%3E %3Cdefs%3E%3C/defs%3E %3Cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E %3Cg id='fasta' transform='translate(-6.000000, -6.000000)'%3E %3Crect id='Rectangle' fill='%2364FF62' x='6' y='6' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-3' fill='%23FFA460' x='10' y='6' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-6' fill='%23665FFF' x='14' y='6' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy' fill='%23DFFF59' x='6' y='10' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-4' fill='%23FFFFFF' x='10' y='10' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-4' fill='%23A1A1A1' x='11.5' y='11.75' width='1' height='1'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-7' fill='%23FF60E2' x='14' y='10' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-2' fill='%23FF8561' x='6' y='14' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-5' fill='%23A460FF' x='10' y='14' width='4' height='4'%3E%3C/rect%3E %3Crect id='Rectangle-Copy-8' fill='%23E160FF' x='14' y='14' width='4' height='4'%3E%3C/rect%3E %3Cpolygon id='Shape' points='0 0 24 0 24 24 0 24'%3E%3C/polygon%3E %3C/g%3E %3C/g%3E %3C/svg%3E"
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.7fe23c0c57f18a91dbb0.js.map
