ecdd7a32c908ffdbb85838295a48707a24632766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package vineyard/0.1.12/vineyard-0.1.12-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/vineyard/vineyard-0.1.12-cp39-cp39-macosx_10_9_x86_64.whl/vineyard/data/utils.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import json
+import platform
+
+import numpy as np
+
+import pickle
+
+if pickle.HIGHEST_PROTOCOL < 5:
+    import pickle5 as pickle
+
+
+def normalize_dtype(dtype, dtype_meta=None):
+    ''' Normalize a descriptive C++ type to numpy.dtype.
+    '''
+    if isinstance(dtype, np.dtype):
+        return dtype
+    if dtype in ['i32', 'int', 'int32', 'int32_t']:
+        return np.dtype('int32')
+    if dtype in ['u32', 'uint', 'uint_t', 'uint32', 'uint32_t']:
+        return np.dtype('uint32')
+    if dtype in [int, 'i64', 'int64', 'long long', 'int64_t']:
+        return np.dtype('int64')
+    if dtype in ['u64', 'uint64', 'uint64_t']:
+        return np.dtype('uint64')
+    if dtype in ['float', 'float32']:
+        return np.dtype('float')
+    if dtype in [float, 'double', 'float64']:
+        return np.dtype('double')
+    if dtype.startswith('str'):
+        return np.dtype(dtype_meta)
+    return dtype
+
+
+def normalize_cpptype(dtype):
+    if dtype.name == 'int32':
+        return 'int'
+    if dtype.name == 'uint32':
+        return 'unsigned int'
+    if dtype.name == 'int64':
+        if platform.system() == 'Linux':
+            return 'long'
+        else:
+            return 'long long'
+    if dtype.name == 'uint64':
+        if platform.system() == 'Linux':
+            return 'unsigned long'
+        else:
+            return 'unsigned long long'
+    if dtype.name == 'float32':
+        return 'float'
+    if dtype.name == 'float64':
+        return 'double'
+    return dtype.name
+
+
+def build_buffer(client, address, size):
+    if size == 0:
+        return client.create_empty_blob()
+    buffer = client.create_blob(size)
+    buffer.copy(0, address, size)
+    return buffer.seal(client)
+
+
+def build_numpy_buffer(client, array):
+    if array.dtype.name != 'object':
+        if not array.flags['C_CONTIGUOUS']:
+            array = np.ascontiguousarray(array)
+        address, _ = array.__array_interface__['data']
+        return build_buffer(client, address, array.nbytes)
+    else:
+        payload = pickle.dumps(array, protocol=5)
+        buffer = client.create_blob(len(payload))
+        buffer.copy(0, payload)
+        return buffer.seal(client)
+
+
+def default_json_encoder(value):
+    if isinstance(value, (np.integer, np.floating)):
+        return value.item()
+    raise TypeError
+
+
+def to_json(value):
+    return json.dumps(value, default=default_json_encoder)
+
+
+def from_json(string):
+    return json.loads(string)
+
+
+__all__ = ['normalize_dtype', 'normalize_cpptype', 'build_buffer', 'build_numpy_buffer', 'to_json', 'from_json']
---
+++ packages/vineyard/vineyard-0.1.12-cp39-cp39-macosx_10_9_x86_64.whl/vineyard/io/stream.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+import traceback
+from urllib.parse import urlparse
+
+import vineyard
+from ..core.driver import registerize
+
+logger = logging.getLogger('vineyard')
+
+
+@registerize
+def read(path, *args, **kwargs):
+    ''' Open a path and read it as a single stream.
+
+        Parameters
+        ----------
+        path: str
+            Path to read, the last reader registered for the scheme of the path will be used.
+        vineyard_ipc_socket: str
+            The local or remote vineyard's IPC socket location that the remote readers will
+            use to establish connections with the vineyard server.
+        vineyard_endpoint: str, optional
+            An optional address of vineyard's RPC socket, which will be used for retrieving server's
+            information on the client side. If not provided, the `vineyard_ipc_socket` will be used,
+            or it will tries to discovery vineyard's IPC or RPC endpoints from environment variables.
+    '''
+    parsed = urlparse(path)
+    if read.__factory and read.__factory.get(parsed.scheme):
+        errors = []
+        for reader in read.__factory[parsed.scheme][::-1]:
+            try:
+                proc_kwargs = kwargs.copy()
+                r = reader(path, proc_kwargs.pop('vineyard_ipc_socket'), *args, **proc_kwargs)
+                if r is not None:
+                    return r
+            except Exception as e:
+                errors.append('%s: %s' % (reader.__name__, traceback.format_exc()))
+        raise RuntimeError('Unable to find a proper IO driver for %s, potential causes are:\n %s' %
+                           (path, '\n'.join(errors)))
+    else:
+        raise ValueError("No IO driver registered for %s" % path)
+
+
+@registerize
+def write(path, stream, *args, **kwargs):
+    ''' Write the stream to a given path.
+
+        Parameters
+        ----------
+        path: str
+            Path to write, the last writer registered for the scheme of the path will be used.
+        stream: vineyard stream
+            Stream that produces the data to write.
+        vineyard_ipc_socket: str
+            The local or remote vineyard's IPC socket location that the remote readers will
+            use to establish connections with the vineyard server.
+        vineyard_endpoint: str, optional
+            An optional address of vineyard's RPC socket, which will be used for retrieving server's
+            information on the client side. If not provided, the `vineyard_ipc_socket` will be used,
+            or it will tries to discovery vineyard's IPC or RPC endpoints from environment variables.
+    '''
+    parsed = urlparse(path)
+    if write.__factory and write.__factory.get(parsed.scheme):
+        errors = []
+        for writer in write.__factory[parsed.scheme][::-1]:
+            try:
+                proc_kwargs = kwargs.copy()
+                writer(path, stream, proc_kwargs.pop('vineyard_ipc_socket'), *args, **proc_kwargs)
+            except Exception as e:
+                errors.append('%s: %s' % (writer.__name__, traceback.format_exc()))
+                continue
+            else:
+                return
+        raise RuntimeError('Unable to find a proper IO driver for %s, potential causes are:\n %s' %
+                           (path, '\n'.join(errors)))
+    else:
+        raise ValueError("No IO driver registered for %s" % path)
+
+
+def open(path, *args, mode='r', **kwargs):
+    ''' Open a path as a reader or writer, depends on the parameter :code:`mode`. If :code:`mode`
+        is :code:`r`, it will open a stream for read, and open a stream for write when :code:`mode`
+        is :code:`w`.
+
+        Parameters
+        ----------
+        path: str
+            Path to open.
+        mode: char
+            Mode about how to open the path, :code:`r` is for read and :code:`w` for write.
+        vineyard_ipc_socket: str
+            Vineyard's IPC socket location.
+        vineyard_endpoint: str
+            Vineyard's RPC socket address.
+
+        See Also
+        --------
+        vineyard.io.read
+        vineyard.io.write
+    '''
+    parsed = urlparse(path)
+    if not parsed.scheme:
+        path = 'file://' + path
+
+    if mode == 'r':
+        return read(path, *args, **kwargs)
+
+    if mode == 'w':
+        return write(path, *args, **kwargs)
+
+    raise RuntimeError('Opening %s with mode %s is not supported' % (path, mode))
+
+
+__all__ = ['open', 'read', 'write']
