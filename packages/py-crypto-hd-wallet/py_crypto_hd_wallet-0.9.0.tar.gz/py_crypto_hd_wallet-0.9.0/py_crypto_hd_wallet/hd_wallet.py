ec5e7e58fc121f6b89ac959702bd6a94c174de12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:11 +0000

    Add package py-crypto-hd-wallet/0.9.0/py_crypto_hd_wallet-0.9.0.tar.gz

---
+++ packages/py-crypto-hd-wallet/py_crypto_hd_wallet-0.9.0.tar.gz/py_crypto_hd_wallet-0.9.0/py_crypto_hd_wallet/hd_wallet.py
@@ -0,0 +1,236 @@
+# Copyright (c) 2020 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Imports
+import json
+from bip_utils       import Bip44Levels
+from .hd_wallet_addr import HdWalletAddresses
+from .hd_wallet_keys import HdWalletKeys
+from .hd_wallet_enum import *
+from .               import utils
+
+
+class HdWalletConst:
+    """ Class container for HD wallet constants. """
+
+    # Map data types to dictionary key
+    DATA_TYPE_TO_DICT_KEY = \
+        {
+            HdWalletDataTypes.WALLET_NAME : "wallet_name",
+            HdWalletDataTypes.COIN_NAME   : "coin_name",
+            HdWalletDataTypes.SPEC_NAME   : "spec_name",
+            HdWalletDataTypes.MNEMONIC    : "mnemonic",
+            HdWalletDataTypes.PASSPHRASE  : "passphrase",
+            HdWalletDataTypes.SEED_BYTES  : "seed_bytes",
+            HdWalletDataTypes.ACCOUNT_IDX : "account_idx",
+            HdWalletDataTypes.CHANGE_IDX  : "change_idx",
+            HdWalletDataTypes.MASTER_KEY  : "master_key",
+            HdWalletDataTypes.PURPOSE_KEY : "purpose_key",
+            HdWalletDataTypes.COIN_KEY    : "coin_key",
+            HdWalletDataTypes.ACCOUNT_KEY : "account_key",
+            HdWalletDataTypes.CHANGE_KEY  : "change_key",
+            HdWalletDataTypes.ADDRESSES   : "addresses",
+        }
+
+
+class HdWallet:
+    """ HD wallet class. It basically wraps the bip_utils, allowing to generate a complete wallet. """
+
+    #
+    # Public methods
+    #
+
+    def __init__(self, wallet_name, bip_obj, mnemonic = "", passphrase = "", seed_bytes = b""):
+        """ Construct class.
+
+        Args:
+            wallet_name (str)         : Wallet name
+            bip_obj (Bip object)      : BIP object
+            mnemonic (str, optional)  : Mnemonic, empty if not specified
+            passphrase (str, optional): Passphrase, empty if not specified
+            seed_bytes (str, optional): Seed_bytes, empty if not specified
+        """
+
+        # Initialize members
+        self.m_bip_obj     = bip_obj
+        self.m_wallet_data = {}
+
+        # Initialize data
+        self.__InitData(wallet_name, mnemonic, passphrase, seed_bytes)
+
+    def Generate(self, account_idx = 0, change_idx = HdWalletChanges.CHAIN_EXT, addr_num = 20):
+        """ Generate wallet keys and addresses.
+
+        Args:
+            account_idx (int, optional)           : Account index, 0 by default
+            change_idx (HdWalletChanges, optional): Change index, must a HdWalletChanges enum, external chain by default
+            addr_num (int, optional)              : Number of addresses to be generated, 20 by default
+        """
+
+        # Check parameters
+        if not isinstance(change_idx, HdWalletChanges):
+            raise TypeError("Change index is not an enumerative of HdWalletChanges")
+        if addr_num < 0:
+            raise ValueError("Address number shall be greater than zero")
+
+        # Save the BIP object
+        bip_obj = self.m_bip_obj
+
+        # Set master keys and derive purpose if correct level
+        if bip_obj.IsLevel(Bip44Levels.MASTER):
+            self.__SetKeys(HdWalletDataTypes.MASTER_KEY, bip_obj)
+            bip_obj = bip_obj.Purpose()
+        # Set purpose keys and derive coin if correct level
+        if bip_obj.IsLevel(Bip44Levels.PURPOSE):
+            self.__SetKeys(HdWalletDataTypes.PURPOSE_KEY, bip_obj)
+            bip_obj = bip_obj.Coin()
+        # Set coin keys and derive account if correct level
+        if bip_obj.IsLevel(Bip44Levels.COIN):
+            self.__SetKeys(HdWalletDataTypes.COIN_KEY, bip_obj)
+            self.__SetData(HdWalletDataTypes.ACCOUNT_IDX, account_idx)
+            bip_obj = bip_obj.Account(account_idx)
+        # Set account keys and derive change if correct level
+        if bip_obj.IsLevel(Bip44Levels.ACCOUNT):
+            self.__SetKeys(HdWalletDataTypes.ACCOUNT_KEY, bip_obj)
+            self.__SetData(HdWalletDataTypes.CHANGE_IDX, int(change_idx))
+            bip_obj = bip_obj.Change(change_idx.ToBip44Change())
+
+        # Set change keys and derive addresses if correct level
+        if bip_obj.IsLevel(Bip44Levels.CHANGE):
+            self.__SetKeys(HdWalletDataTypes.CHANGE_KEY, bip_obj)
+            self.__SetData(HdWalletDataTypes.ADDRESSES, HdWalletAddresses.FromBipObj(bip_obj, addr_num))
+        # In this case, the wallet was created from an address index extended key, so there is only one address to generate
+        else:
+            self.__SetData(HdWalletDataTypes.ADDRESSES, HdWalletAddresses.FromBipObj(bip_obj, 1))
+
+    def IsWatchOnly(self):
+        """ Get if the wallet is watch-only.
+
+        Returns :
+            bool: True if watch-only, false otherwise
+        """
+        return self.m_bip_obj.IsPublicOnly()
+
+    def ToDict(self):
+        """ Get wallet data as a dictionary.
+
+        Returns:
+            dict: Wallet data as a dictionary
+        """
+        wallet_dict = {}
+
+        # Convert to dictionary the instances of HdWalletKeys or HdWalletAddresses classes
+        for key, value in self.m_wallet_data.items():
+            if isinstance(value, HdWalletKeys) or isinstance(value, HdWalletAddresses):
+                wallet_dict[key] = value.ToDict()
+            else:
+                wallet_dict[key] = value
+
+        return wallet_dict
+
+    def ToJson(self, json_indent = 4):
+        """ Get wallet data as string in JSON format.
+
+        Args:
+            json_indent (int, optional): Indent for JSON format, 4 by default
+
+        Returns:
+            str: Wallet data as string in JSON format
+        """
+        return json.dumps(self.ToDict(), indent = json_indent)
+
+    def HasData(self, data_type):
+        """ Get if the wallet data of the specified type is present.
+
+        Args:
+            data_type (HdWalletDataTypes): Data type, shall be of HdWalletDataTypes enum
+
+        Returns:
+            bool: True if present, false otherwise
+        """
+        if not isinstance(data_type, HdWalletDataTypes):
+            raise TypeError("Data type is not an enumerative of HdWalletDataTypes")
+
+        dict_key = HdWalletConst.DATA_TYPE_TO_DICT_KEY[data_type]
+        return dict_key in self.m_wallet_data
+
+    def GetData(self, data_type):
+        """ Get wallet data of the specified type.
+
+        Args:
+            data_type (HdWalletDataTypes): Data type, shall be of HdWalletDataTypes enum
+
+        Returns (str, dict or None):
+            str or HdWalletKeys or HdWalletAddresses: Wallet data
+            None: If not found
+        """
+        if self.HasData(data_type):
+            return self.m_wallet_data[HdWalletConst.DATA_TYPE_TO_DICT_KEY[data_type]]
+        else:
+            return None
+
+    #
+    # Private methods
+    #
+
+    def __InitData(self, wallet_name, mnemonic, passphrase, seed_bytes):
+        """ Initialize data.
+
+        Args:
+            wallet_name (str): Wallet name
+            mnemonic (str)   : Mnemonic
+            passphrase (str) : Passphrase
+            seed_bytes (str) : Seed_bytes
+        """
+
+        # Set wallet name
+        self.__SetData(HdWalletDataTypes.WALLET_NAME, wallet_name)
+        # Set specification name
+        self.__SetData(HdWalletDataTypes.SPEC_NAME, self.m_bip_obj.SpecName())
+        # Set coin name
+        coin_names = self.m_bip_obj.CoinClass().CoinNames()
+        self.__SetData(HdWalletDataTypes.COIN_NAME, "%s (%s)" % (coin_names.Name(), coin_names.Abbreviation()))
+
+        # Set optional data if specified
+        if mnemonic != "":
+            self.__SetData(HdWalletDataTypes.MNEMONIC, mnemonic)
+            self.__SetData(HdWalletDataTypes.PASSPHRASE, passphrase)
+        if seed_bytes != b"":
+            self.__SetData(HdWalletDataTypes.SEED_BYTES, utils.BytesToString(seed_bytes))
+
+    def __SetData(self, data_type, data_value):
+        """ Set wallet data.
+
+        Args:
+            data_type (HdWalletDataTypes): Data type, shall be of HdWalletDataTypes enum
+            data_value (str)             : Data value
+        """
+        dict_key = HdWalletConst.DATA_TYPE_TO_DICT_KEY[data_type]
+        self.m_wallet_data[dict_key] = data_value
+
+    def __SetKeys(self, data_type, bip_obj):
+        """ Add keys to wallet data.
+
+        Args:
+            data_type (HdWalletDataTypes): Data type, shall be of HdWalletDataTypes enum
+            bip_obj (Bip object)         : BIP object
+        """
+        self.__SetData(data_type, HdWalletKeys.FromBipObj(bip_obj))
---
+++ packages/py-crypto-hd-wallet/py_crypto_hd_wallet-0.9.0.tar.gz/py_crypto_hd_wallet-0.9.0/py_crypto_hd_wallet/hd_wallet_enum.py
@@ -0,0 +1,134 @@
+# Copyright (c) 2020 Emanuele Bellocchia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Imports
+from enum      import Enum, IntEnum, auto, unique
+from bip_utils import Bip44Changes, Bip44Coins
+
+
+@unique
+class HdWalletWordsNum(IntEnum):
+    """ Words number enumeratives. """
+
+    WORDS_NUM_12 = 12,
+    WORDS_NUM_15 = 15,
+    WORDS_NUM_18 = 18,
+    WORDS_NUM_21 = 21,
+    WORDS_NUM_24 = 24,
+
+
+@unique
+class HdWalletChanges(IntEnum):
+    """ Alias for hiding Bip44Changes. """
+
+    CHAIN_EXT = Bip44Changes.CHAIN_EXT,
+    CHAIN_INT = Bip44Changes.CHAIN_INT,
+
+    def ToBip44Change(enum_val):
+        """ Convert to Bip44Changes type.
+
+        Returns:
+            Bip44Changes: Bip44Changes value
+        """
+        return Bip44Changes(enum_val)
+
+
+@unique
+class HdWalletCoins(Enum):
+    """ Alias for hiding Bip44Coins. """
+
+    # Main nets
+    BITCOIN              = Bip44Coins.BITCOIN,
+    BITCOIN_CASH         = Bip44Coins.BITCOIN_CASH,
+    BITCOIN_SV           = Bip44Coins.BITCOIN_SV,
+    LITECOIN             = Bip44Coins.LITECOIN,
+    DOGECOIN             = Bip44Coins.DOGECOIN,
+    DASH                 = Bip44Coins.DASH,
+    ZCASH                = Bip44Coins.ZCASH,
+    ETHEREUM             = Bip44Coins.ETHEREUM,
+    ETHEREUM_CLASSIC     = Bip44Coins.ETHEREUM_CLASSIC,
+    RIPPLE               = Bip44Coins.RIPPLE,
+    TRON                 = Bip44Coins.TRON,
+    VECHAIN              = Bip44Coins.VECHAIN,
+    COSMOS               = Bip44Coins.COSMOS,
+    BAND_PROTOCOL        = Bip44Coins.BAND_PROTOCOL,
+    KAVA                 = Bip44Coins.KAVA,
+    IRIS_NET             = Bip44Coins.IRIS_NET,
+    BINANCE_CHAIN        = Bip44Coins.BINANCE_CHAIN,
+    BINANCE_SMART_CHAIN  = Bip44Coins.BINANCE_SMART_CHAIN,
+    # Test nets
+    BITCOIN_TESTNET      = Bip44Coins.BITCOIN_TESTNET,
+    BITCOIN_CASH_TESTNET = Bip44Coins.BITCOIN_CASH_TESTNET,
+    BITCOIN_SV_TESTNET   = Bip44Coins.BITCOIN_SV_TESTNET,
+    LITECOIN_TESTNET     = Bip44Coins.LITECOIN_TESTNET,
+    DOGECOIN_TESTNET     = Bip44Coins.DOGECOIN_TESTNET,
+    DASH_TESTNET         = Bip44Coins.DASH_TESTNET,
+    ZCASH_TESTNET        = Bip44Coins.ZCASH_TESTNET,
+
+    def ToBip44Coin(enum_val):
+        """ Convert to Bip44Coins type.
+
+        Returns:
+            Bip44Coins: Bip44Coins value
+        """
+        return Bip44Coins(enum_val.value[0])
+
+
+@unique
+class HdWalletSpecs(Enum):
+    """ Enumerative for wallet specifications. """
+
+    BIP44 = auto(),
+    BIP49 = auto(),
+    BIP84 = auto(),
+
+
+@unique
+class HdWalletDataTypes(Enum):
+    """ Enumerative for wallet data types. """
+
+    WALLET_NAME = auto(),
+    COIN_NAME   = auto(),
+    SPEC_NAME   = auto(),
+    MNEMONIC    = auto(),
+    PASSPHRASE  = auto(),
+    SEED_BYTES  = auto(),
+    ACCOUNT_IDX = auto(),
+    CHANGE_IDX  = auto(),
+    MASTER_KEY  = auto(),
+    PURPOSE_KEY = auto(),
+    COIN_KEY    = auto(),
+    ACCOUNT_KEY = auto(),
+    CHANGE_KEY  = auto(),
+    ADDRESSES   = auto(),
+
+
+@unique
+class HdWalletKeyTypes(Enum):
+    """ Enumerative for wallet key types. """
+
+    EX_PRIV         = auto(),
+    RAW_PRIV        = auto(),
+    WIF_PRIV        = auto(),
+    EX_PUB          = auto(),
+    RAW_COMPR_PUB   = auto(),
+    RAW_UNCOMPR_PUB = auto(),
+    ADDRESS         = auto(),
