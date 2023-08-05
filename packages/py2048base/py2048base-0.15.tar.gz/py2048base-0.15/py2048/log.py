5a1e02525695a288eae6340e4b9067867fdf6f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package py2048base/0.15/py2048base-0.15.tar.gz

---
+++ packages/py2048base/py2048base-0.15.tar.gz/py2048base-0.15/py2048/log.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# log.py
+#
+# This file is part of py2048.
+#
+# py2048 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# py2048 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with py2048.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+from pathlib import Path
+import logging
+
+from py2048 import APPNAME, DATA_DIR
+
+
+__all__ = ["logger", "setup_logger"]
+
+
+# setup log folder and log file path
+DATA_DIR.mkdir(exist_ok=True)
+fpath = str(DATA_DIR.resolve() / f"{APPNAME}.log")
+
+# setup the default handlers
+HANDLERS = (
+    logging.FileHandler(filename=fpath, mode="w"),
+    logging.StreamHandler(stream=sys.stderr),
+)
+HANDLERS[-1].setLevel(logging.ERROR)  # will override the logger's level
+
+# set up a formatter (to be used for all handlers)
+FORMATTER = logging.Formatter(
+    fmt="\t".join(
+        [
+            # "%(asctime)s,%(msecs)d",  # human-readable timestamp
+            "%(asctime)s",  # human-readable timestamp
+            # "%(name)s @ %(module)s",  # logger name @ module
+            "%(funcName)s @ %(module)s",  # calling function @ module
+            "(%(levelname)s) %(message)s",  # (level name) message
+        ]
+    ),
+    datefmt="%H:%M:%S",
+)
+
+
+def setup_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    for h in HANDLERS:
+        h.setFormatter(FORMATTER)
+        logger.addHandler(h)
+        logger.info("Loaded handler: '%s'.", h)
+    logger.info("Created '%s'.", logger)
+    return logger
+
+
+# set up the main logger and equip it
+logger = setup_logger(APPNAME)
