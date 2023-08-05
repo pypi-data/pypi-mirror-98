47f4e730bbf29afbc87daacd89bb9b27efd11e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:08 +0000

    Add package oceansat/0.1.0/oceansat-0.1.0.tar.gz

---
+++ packages/oceansat/oceansat-0.1.0.tar.gz/oceansat-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['oceansat']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.20.1,<2.0.0', 'scipy>=1.6.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['satver = oceansat.commandline:version']}
+
+setup_kwargs = {
+    'name': 'oceansat',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Bror Jonsson',
+    'author_email': 'brorfred@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
