746c3ae3c44bece0a38da872140f72084fd93cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:32 +0000

    Add package datasette-ripgrep/0.7/datasette-ripgrep-0.7.tar.gz

---
+++ packages/datasette-ripgrep/datasette-ripgrep-0.7.tar.gz/datasette-ripgrep-0.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+
+VERSION = "0.7"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-ripgrep",
+    description="Web interface for searching your code using ripgrep, built as a Datasette plugin",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-ripgrep",
+    project_urls={
+        "Issues": "https://github.com/simonw/datasette-ripgrep/issues",
+        "CI": "https://github.com/simonw/datasette-ripgrep/actions",
+        "Changelog": "https://github.com/simonw/datasette-ripgrep/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["datasette_ripgrep"],
+    entry_points={"datasette": ["ripgrep = datasette_ripgrep"]},
+    package_data={"datasette_ripgrep": ["templates/*.html"]},
+    install_requires=["datasette"],
+    extras_require={"test": ["pytest", "pytest-asyncio", "httpx"]},
+    tests_require=["datasette-ripgrep[test]"],
+    python_requires=">=3.6",
+)
