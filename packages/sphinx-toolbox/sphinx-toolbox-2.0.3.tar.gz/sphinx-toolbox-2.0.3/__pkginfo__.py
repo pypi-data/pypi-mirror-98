4f55ad624f21cae1bc4b3cf0af2636f9e3c4207f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package sphinx-toolbox/2.0.3/sphinx-toolbox-2.0.3.tar.gz

---
+++ packages/sphinx-toolbox/sphinx-toolbox-2.0.3.tar.gz/sphinx-toolbox-2.0.3/__pkginfo__.py
@@ -0,0 +1,45 @@
+#  This file is managed by 'repo_helper'. Don't edit it directly.
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This file is distributed under the same license terms as the program it came with.
+#  There will probably be a file called LICEN[S/C]E in the same directory as this file.
+#
+#  In any case, this program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+#
+
+# stdlib
+import pathlib
+
+__all__ = [
+		"__copyright__",
+		"__version__",
+		"repo_root",
+		"install_requires",
+		"extras_require",
+		]
+
+__copyright__ = """
+2020-2021 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+"""
+
+__version__ = "2.0.3"
+repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
+extras_require = {
+		"testing": [
+				"domdf-python-tools[testing]>=1.5.0",
+				"pygments>=2.7.4",
+				"pytest>=6.0.0",
+				"pytest-regressions>=2.0.3"
+				],
+		"all": [
+				"domdf-python-tools[testing]>=1.5.0",
+				"pygments>=2.7.4",
+				"pytest>=6.0.0",
+				"pytest-regressions>=2.0.3"
+				]
+		}
