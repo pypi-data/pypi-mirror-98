19f3e782eaa10ecbf5005edfa4ba90dafc37787c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package field-slicer/0.1.9/field-slicer-0.1.9.tar.gz

---
+++ packages/field-slicer/field-slicer-0.1.9.tar.gz/field-slicer-0.1.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    scripts=[
+        "bin/field-slicer",
+        "bin/field-slicer-services"
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'labbox==0.1.22'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
