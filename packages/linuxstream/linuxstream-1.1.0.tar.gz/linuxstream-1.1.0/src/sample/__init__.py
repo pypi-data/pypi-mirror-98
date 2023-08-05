6ccfdbaf14580301db32fb90209f298244698d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package linuxstream/1.1.0/linuxstream-1.1.0.tar.gz

---
+++ packages/linuxstream/linuxstream-1.1.0.tar.gz/linuxstream-1.1.0/src/sample/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/ python
+# -*- coding: utf-8 -*-
+########################################################################
+#
+# Copyright (c) 2021 Baidu.com, Inc. All Rights Reserved
+#
+########################################################################
+
+"""
+    File: __init__.py
+    Author: dushuangshuang(dushuangshuang@baidu.com)
+    Date:   2021/03/10 下午17:12
+"""
+
+import os
+import numpy as np
+import openpyxl
+import chardet
+import requests
+import certifi
+
+
+if __name__ == '__main__':
+    """三方库安装
+    """
+    print('Start import ...')
+    print('OK!')
+    
