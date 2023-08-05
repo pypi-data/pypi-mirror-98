cba238217f2b2c2e08fcd9c8cedcda1419c8a816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:56 +0000

    Add package selenium-firefox/1.0.31/selenium_firefox-1.0.31.tar.gz

---
+++ packages/selenium-firefox/selenium_firefox-1.0.31.tar.gz/selenium_firefox-1.0.31/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium_firefox",
+    version="1.0.31",
+    author="Kovács Kristóf-Attila",
+    description="selenium_firefox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_firefox",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'geckodriver-autoinstaller>=0.1.0',
+        'noraise>=0.0.10',
+        'selenium>=3.141.0',
+        'tldextract>=3.1.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
