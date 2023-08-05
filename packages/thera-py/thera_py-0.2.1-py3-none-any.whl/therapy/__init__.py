ba2de14930fdbdc612468639b7f7d516a0c18be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:36 +0000

    Add package thera-py/0.2.1/thera_py-0.2.1-py3-none-any.whl

---
+++ packages/thera-py/thera_py-0.2.1-py3-none-any.whl/therapy/__init__.py
@@ -0,0 +1,20 @@
+"""The VICC library for normalizing therapies."""
+from pathlib import Path
+import logging
+
+PROJECT_ROOT = Path(__file__).resolve().parents[0]
+logging.basicConfig(
+    filename='therapy.log',
+    format='[%(asctime)s] %(levelname)s : %(message)s')
+logger = logging.getLogger('therapy')
+logger.setLevel(logging.DEBUG)
+
+__version__ = "0.2.1"
+
+
+class DownloadException(Exception):
+    """Exception for failures relating to source file downloads."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize exception."""
+        super().__init__(*args, **kwargs)
