b6fa681088850ceda6f1f8f6e97f8470b4e5569f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package lightly/1.1.0/lightly-1.1.0-py3-none-any.whl

---
+++ packages/lightly/lightly-1.1.0-py3-none-any.whl/lightly/loss/__init__.py
@@ -0,0 +1,7 @@
+"""The lightly.loss package provides loss functions for self-supervised learning. """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.loss.ntx_ent_loss import NTXentLoss
+from lightly.loss.sym_neg_cos_sim_loss import SymNegCosineSimilarityLoss
