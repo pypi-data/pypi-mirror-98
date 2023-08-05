bb1cf2b102feebb4b4432c59b4438edbb5385053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:19 +0000

    Add package TWP/40/TWP-40.tar.gz

---
+++ packages/TWP/TWP-40.tar.gz/TWP-40/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='TWP',
+      version='40',
+      description='tweet with picture',
+      author='Kerota',
+      author_email='hifuna2727@gmail.com',
+      url='https://twitter.com',
+      
+
+     
+     )
