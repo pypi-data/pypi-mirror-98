f8375dbe11fb1c4fe75db776ba9613bf99085294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package modbus_client/0.0.1/modbus_client-0.0.1.tar.gz

---
+++ packages/modbus_client/modbus_client-0.0.1.tar.gz/modbus_client-0.0.1/modbus_client/__init__.py
@@ -0,0 +1,3 @@
+""" Easy-to-use ModbusTCP Client in pure Python """
+
+__version__ = "0.0.1"
---
+++ packages/modbus_client/modbus_client-0.0.1.tar.gz/modbus_client-0.0.1/modbus_client/modbus_client.py
@@ -0,0 +1,7 @@
+import struct
+import socket
+
+
+class Client:
+    def __init__(self, host="localhost", port="520"):
+        pass
