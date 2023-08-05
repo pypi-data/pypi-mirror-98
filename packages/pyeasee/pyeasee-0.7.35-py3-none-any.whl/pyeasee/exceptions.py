2e64f016f4b76d82a4bc726d903250dc8aa49497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:44 +0000

    Add package pyeasee/0.7.35/pyeasee-0.7.35-py3-none-any.whl

---
+++ packages/pyeasee/pyeasee-0.7.35-py3-none-any.whl/pyeasee/exceptions.py
@@ -0,0 +1,14 @@
+class AuthorizationFailedException(Exception):
+    pass
+
+
+class NotFoundException(Exception):
+    pass
+
+
+class TooManyRequestsException(Exception):
+    pass
+
+
+class ServerFailureException(Exception):
+    pass
