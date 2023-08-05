597bf63b70808724ee5debe4a7a816ea6d753a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:54 +0000

    Add package PyFlume/0.6.4/PyFlume-0.6.4.tar.gz

---
+++ packages/PyFlume/PyFlume-0.6.4.tar.gz/PyFlume-0.6.4/setup.py
@@ -0,0 +1,28 @@
+"""PyFlume setuptools for PyPi."""
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyFlume',
+    version='0.6.4',
+    author='ChrisMandich',
+    author_email='Chris@Mandich.net',
+    description='Package to integrate with Flume Sensor',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/ChrisMandich/PyFlume',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pyjwt==2.0.1',
+        'ratelimit==2.2.1',
+        'pytz==2019.2',
+        'requests==2.24.0',
+    ],
+)
