eaabe401f2415bc486251202b2a4ec6d59b2b083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:58 +0000

    Add package iog-randomizer/4.3.6/iog-randomizer-4.3.6.tar.gz

---
+++ packages/iog-randomizer/iog-randomizer-4.3.6.tar.gz/iog-randomizer-4.3.6/randomizer/models/enums/difficulty.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Difficulty(Enum):
+    EASY = 0
+    NORMAL = 1
+    HARD = 2
+    EXTREME = 3
---
+++ packages/iog-randomizer/iog-randomizer-4.3.6.tar.gz/iog-randomizer-4.3.6/setup.py
@@ -0,0 +1,22 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='iog-randomizer',
+    version='4.3.6',
+    description='The Illusion of Gaia Randomizer',
+    author='dontbagume,bryon_w,raeven0',
+    packages=setuptools.find_packages(),
+    package_data={'': [
+        'bin/*.bin',
+        'bin/plugins/**/*.bin',
+        'bin/plugins/**/**/*.bin'
+    ]},
+    install_requires=['bitstring', 'graphviz'],
+    python_requires='>=3.7'
+)
