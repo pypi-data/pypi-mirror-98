57023eede486c127a5a6da05f19c1d2f0cd49fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:00 +0000

    Add package viajen/0.1.0/viajen-0.1.0.tar.gz

---
+++ packages/viajen/viajen-0.1.0.tar.gz/viajen-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='viajen',
+      version='0.1.0',
+      description=('View Images as Animation in Jupyter and Equivalent'
+                   ' Notebooks'),
+      url='https://github.com/brunomsantiago/viajen',
+      author='Bruno Santiago',
+      author_email='not_a_real_email_4523fd786fs5@gmail.com',
+      license='MIT',
+      python_requires='>=3.6.0',
+      packages=['viajen'])
