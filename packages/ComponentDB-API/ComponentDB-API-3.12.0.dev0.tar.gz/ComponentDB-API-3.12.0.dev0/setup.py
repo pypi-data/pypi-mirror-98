c5d791442d5efc61df4a976a21c2d8b08e8a1652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:25 +0000

    Add package ComponentDB-API/3.12.0.dev0/ComponentDB-API-3.12.0.dev0.tar.gz

---
+++ packages/ComponentDB-API/ComponentDB-API-3.12.0.dev0.tar.gz/ComponentDB-API-3.12.0.dev0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+"""
+DEV NOTE: To publish API
+# Update version in this file
+python3 setup.py sdist
+twine upload dist/(specific version file)
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='ComponentDB-API',
+      version='3.12.0.dev0',
+      packages=find_packages(),
+      py_modules=["CdbApiFactory"],
+      install_requires=['python-dateutil', 
+          'urllib3',
+          'six'],
+      license='Copyright (c) UChicago Argonne, LLC. All rights reserved.',
+      description='Python APIs used to communicate with java hosted ComponentDB API.',
+      maintainer='Dariusz Jarosz',
+      maintainer_email='djarosz@aps.anl.gov',
+      url='https://github.com/AdvancedPhotonSource/ComponentDB',
+      scripts=[
+          'cdbCli/service/cli/CDBcli.py',
+          'cdbCli/service/cli/addLogToItemByIdCli.py', 
+          'cdbCli/service/cli/getItemByIdCli.py'
+          ],
+      entry_points={
+        'console_scripts': [
+          'cdb-python-client-test = CdbApiFactory:run_command',
+          'cdb-get-item-by-id = cdbCli.service.cli.getItemByIdCli:get_item_by_id',
+          'cdb-add-log-to-item-by-id = cdbCli.service.cli.addLogToItemByIdCli:add_log_to_item_by_id',
+          'cdb-cli-tool = cdbCli.service.cli.CDBcli:main'
+        ]
+      })
