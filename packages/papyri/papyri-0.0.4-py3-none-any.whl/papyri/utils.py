ea83c4e3177edbfc77bff39fb33157d1de5f9b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package papyri/0.0.4/papyri-0.0.4-py3-none-any.whl

---
+++ packages/papyri/papyri-0.0.4-py3-none-any.whl/papyri/utils.py
@@ -0,0 +1,101 @@
+import time
+from datetime import timedelta
+from textwrap import dedent
+from typing import Tuple
+
+from rich.progress import BarColumn, Progress, ProgressColumn, Task, TextColumn
+from rich.text import Text
+
+
+class TimeElapsedColumn(ProgressColumn):
+
+    # Only refresh twice a second to prevent jitter
+    max_refresh = 0.5
+
+    def __init__(self, *args, **kwargs):
+        self.avg = None
+        super().__init__(*args, **kwargs)
+
+    def render(self, task: "Task"):
+        # task.completed
+        # task.total
+        elapsed = task.elapsed
+        if elapsed is None:
+            return Text("-:--:--", style="progress.elapsed")
+        elapsed_delta = timedelta(seconds=int(elapsed))
+        if task.time_remaining is not None:
+            if self.avg is None:
+                self.avg = elapsed_delta + timedelta(seconds=int(task.time_remaining))
+            else:
+                self.avg = (
+                    99 * self.avg
+                    + elapsed_delta
+                    + timedelta(seconds=int(task.time_remaining))
+                ) / 100
+            finish_delta = str(self.avg).split(".")[0]
+        else:
+            finish_delta = "--:--:--"
+        return Text(
+            str(elapsed_delta) + "/" + str(finish_delta), style="progress.elapsed"
+        )
+
+
+def progress(iterable, *, description="Progress", transient=True):
+    items = list(iterable)
+    p = Progress(
+        TextColumn("[progress.description]{task.description:15}", justify="left"),
+        BarColumn(bar_width=None),
+        "[progress.percentage]{task.completed}/{task.total}",
+        TimeElapsedColumn(),
+        transient=transient,
+    )
+    p.start()
+    task = p.add_task(description, total=len(items), ee=0)
+    it = iter(items)
+    now = time.monotonic()
+
+    def gen():
+        try:
+            c = 0
+            while True:
+                p.update(task, ee=time.monotonic() - now)
+                p.advance(task)
+                yield p, next(it)
+                c += 1
+        except StopIteration:
+            p.stop()
+            if transient:
+                print(
+                    description,
+                    f"Done {c} items in {time.monotonic() - now:.2f} seconds",
+                )
+            return
+        except BaseException:
+            p.stop()
+            raise
+
+    return gen()
+
+
+def dedent_but_first(text):
+    """
+    simple version of `inspect.cleandoc` that does not trim empty lines
+    """
+    assert isinstance(text, str)
+    a, *b = text.split("\n")
+    return dedent(a) + "\n" + dedent("\n".join(b))
+
+
+def pos_to_nl(script: str, pos: int) -> Tuple[int, int]:
+    """
+    Convert pigments position to Jedi col/line
+    """
+    rest = pos
+    ln = 0
+    for line in script.splitlines():
+        if len(line) < rest:
+            rest -= len(line) + 1
+            ln += 1
+        else:
+            return ln, rest
+    raise RuntimeError
