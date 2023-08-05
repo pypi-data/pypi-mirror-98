eb9c21e42bc38e94cfe8d01e0397e98eac0b99d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:10 +0000

    Add package fusionauth-client/1.25.0/fusionauth-client-1.25.0.tar.gz

---
+++ packages/fusionauth-client/fusionauth-client-1.25.0.tar.gz/fusionauth-client-1.25.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fusionauth-client",
+    version="1.25.0",
+    author="Tyler Scott",
+    author_email="dev@fusionauth.io",
+    description="A client library for FusionAuth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FusionAuth/fusionauth-python-client",
+    packages=find_packages(where='src/main/python'),
+    namespace_packages=["fusionauth"],
+    package_dir={'': 'src/main/python'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+    install_requires=[
+        'deprecated', 'requests',
+    ]
+)
