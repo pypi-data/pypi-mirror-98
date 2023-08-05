3d7ec2d2afefd11ab403ec97ed6813f2c300de1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:49 +0000

    Add package merlinwf/2.0.0/merlinwf-2.0.0.tar.gz

---
+++ packages/merlinwf/merlinwf-2.0.0.tar.gz/merlinwf-2.0.0/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+version = "2.0.0"
+DEPRECATION_MESSAGE = "The 'merlinwf' package has been deprecated and replaced by the 'merlin' package."
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="merlinwf",
+    author="Merlin Dev team",
+    author_email="merlin@llnl.gov",
+    version=version,
+    description=DEPRECATION_MESSAGE,
+    long_description=DEPRECATION_MESSAGE,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    url="https://github.com/LLNL/merlin",
+    license="MIT",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    entry_points={
+        "console_scripts": [
+            "merlin=main:main",
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
