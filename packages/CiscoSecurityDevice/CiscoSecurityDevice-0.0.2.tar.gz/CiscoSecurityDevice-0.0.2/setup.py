b26ccde05d02d1e43d1ef874518d185726176fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:23 +0000

    Add package CiscoSecurityDevice/0.0.2/CiscoSecurityDevice-0.0.2.tar.gz

---
+++ packages/CiscoSecurityDevice/CiscoSecurityDevice-0.0.2.tar.gz/CiscoSecurityDevice-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+with open("/Users/madewang/PycharmProjects/Input/CiscoSecurityDevice/README.md",'r') as fh:
+    long_description = fh.read()
+setup(
+    name='CiscoSecurityDevice',
+    py_modules=['CiscoSecurityDevice'],
+    version='0.0.2',
+    description='This wrapper is designed to provide a unique interface to interact with ISE and FMC api.',
+    author='Madhuri Dewangan',
+    author_email='madewang@cisco.com',
+    package_dir={'':'CiscoSecurityDevice'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests","ISE","fireREST"],
+    classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7"]
+
+)
