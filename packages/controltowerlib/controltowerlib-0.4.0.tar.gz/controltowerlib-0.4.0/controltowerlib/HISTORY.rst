cd51f94fb4cb2fe206a25c576a6b17a279f8841d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package controltowerlib/0.4.0/controltowerlib-0.4.0.tar.gz

---
+++ packages/controltowerlib/controltowerlib-0.4.0.tar.gz/controltowerlib-0.4.0/controltowerlib/HISTORY.rst
@@ -0,0 +1,45 @@
+.. :changelog:
+
+History
+-------
+
+0.0.1 (18-02-2020)
+---------------------
+
+* First code creation
+
+
+0.1.0 (12-01-2021)
+------------------
+
+* First public release.
+
+
+0.1.1 (19-01-2021)
+------------------
+
+* Working around the OU pagination until the implementation fixes that properly.
+
+
+0.1.2 (19-01-2021)
+------------------
+
+* Fixed pagination bug properly.
+
+
+0.2.0 (28-01-2021)
+------------------
+
+* Implemented control tower versions and account update capabilites.
+
+
+0.3.0 (26-02-2021)
+------------------
+
+* Implemented control tower update, identifying the deployment status and locking when busy, along with exposing some more data like guard rails and drift messages.
+
+
+0.4.0 (11-03-2021)
+------------------
+
+* Made account deletion not depending on the suspended OU existance and account factory retrieval checking for CT being deployed.
---
+++ packages/controltowerlib/controltowerlib-0.4.0.tar.gz/controltowerlib-0.4.0/controltowerlib/resources/configuration.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: __init__.py
+#
+# Copyright 2020 Costas Tyfoxylos
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#
+
+"""
+configuration module.
+
+Import all parts from configuration here
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+"""
+
+import logging
+
+__author__ = '''Costas Tyfoxylos <ctyfoxylos@schubergphilis.com>'''
+__docformat__ = '''google'''
+__date__ = '''18-02-2020'''
+__copyright__ = '''Copyright 2020, Costas Tyfoxylos'''
+__license__ = '''MIT'''
+__maintainer__ = '''Costas Tyfoxylos'''
+__email__ = '''<ctyfoxylos@schubergphilis.com>'''
+__status__ = '''Development'''  # "Prototype", "Development", "Production".
+
+
+LOGGER_BASENAME = '''controltowerlib'''
+LOGGER = logging.getLogger(LOGGER_BASENAME)
+LOGGER.addHandler(logging.NullHandler())
+
+CREATING_ACCOUNT_ERROR_MESSAGE = 'Package is in state CREATING, but must be in state AVAILABLE'
+
+
+class LoggerMixin:  # pylint: disable=too-few-public-methods
+    """Logger."""
+
+    @property
+    def logger(self):
+        """Exposes the logger to be used by objects using the Mixin.
+
+        Returns:
+            logger (logger): The properly named logger.
+
+        """
+        return logging.getLogger(f'{LOGGER_BASENAME}.{self.__class__.__name__}')
