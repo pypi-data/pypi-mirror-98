d7e241bf2da29160d6c5730e58ba6bcd50d00f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:52 +0000

    Add package datamodel-code-generator/0.9.2/datamodel_code_generator-0.9.2-py3-none-any.whl

---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.9.2-py3-none-any.whl/datamodel_code_generator/model/__init__.py
@@ -0,0 +1,5 @@
+from ..types import StrictTypes
+from .base import ConstraintsBase, DataModel, DataModelFieldBase
+from .pydantic.dataclass import DataClass
+
+__all__ = ['ConstraintsBase', 'DataModel', 'DataModelFieldBase']
