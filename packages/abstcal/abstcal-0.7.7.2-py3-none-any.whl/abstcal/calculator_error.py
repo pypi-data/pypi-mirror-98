3053d406bd8bc3e62fdce0e4531a7b739823537e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package abstcal/0.7.7.2/abstcal-0.7.7.2-py3-none-any.whl

---
+++ packages/abstcal/abstcal-0.7.7.2-py3-none-any.whl/abstcal/calculator_error.py
@@ -0,0 +1,31 @@
+"""
+AbstinenceCalculatorError
+---------
+A data model for raising exceptions in the abstinence calculation
+"""
+
+import warnings
+
+
+class AbstinenceCalculatorError(Exception):
+    pass
+
+
+class FileExtensionError(AbstinenceCalculatorError):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def __str__(self):
+        return f"The file {self.file_path} isn't a tab-delimited text file, CSV or Excel file."
+
+
+class FileFormatError(AbstinenceCalculatorError):
+    pass
+
+
+class InputArgumentError(AbstinenceCalculatorError):
+    pass
+
+
+def _show_warning(warning_message):
+    warnings.warn(warning_message)
