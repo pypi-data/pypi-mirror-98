c9dc11ff6f35b7d51d9761c8f35257046269df96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:29 +0000

    Add package pretix-vacc-utils/1.0.0/pretix-vacc-utils-1.0.0.tar.gz

---
+++ packages/pretix-vacc-utils/pretix-vacc-utils-1.0.0.tar.gz/pretix-vacc-utils-1.0.0/pretix_vacc_autosched/__init__.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.0"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_vacc_autosched"
+    verbose_name = "Vaccination: Automatic scheduling of second dose"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Vaccination: Automatic scheduling of second dose")
+        author = "pretix team"
+        description = gettext_lazy("Automatic scheduling of second dose after checkin")
+        visible = True
+        version = __version__
+        category = "FEATURE"
+        compatibility = "pretix>=3.15.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_vacc_autosched.PluginApp"
