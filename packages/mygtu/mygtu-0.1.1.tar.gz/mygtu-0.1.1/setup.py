33ec856fb492690725f099967f08d0cc0c10dc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package mygtu/0.1.1/mygtu-0.1.1.tar.gz

---
+++ packages/mygtu/mygtu-0.1.1.tar.gz/mygtu-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as setup:
+    long_description = setup.read()
+
+setuptools.setup(
+    name="mygtu",
+    version="0.1.1",
+    author="Dhruv Maaniya (@midnightmadwalk)",
+    author_email="midnightmadwalk@gmail.com",
+    description="pyhton interaction with gtu website.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mygtu/mygtu",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['datetime', 'pytz', 'ujson', 'aiofiles', 'aiohttp', 'tqdm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
