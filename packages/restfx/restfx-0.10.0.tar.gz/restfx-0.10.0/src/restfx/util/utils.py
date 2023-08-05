eadc1f0ac40289a1b9746fe3962a0456dfff2286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package restfx/0.10.0/restfx-0.10.0.tar.gz

---
+++ packages/restfx/restfx-0.10.0.tar.gz/restfx-0.10.0/src/restfx/util/utils.py
@@ -0,0 +1,30 @@
+import inspect
+
+
+def load_module(module_name: str, level=0):
+    """
+    加载模块
+    :param level:
+    :param module_name:
+    :return:
+    """
+    # __import__ 自带缓存
+    module = __import__(module_name, level=level, globals=globals())
+    temp = module_name.split('.')
+    if len(temp) > 1:
+        for seg in temp[1:]:
+            module = getattr(module, seg)
+    return module
+
+
+def get_func_info(func):
+    source_lines = inspect.getsourcelines(func)
+    line = source_lines[1]
+    return 'File "%s", line %d, in %s' % (
+        inspect.getmodule(func).__file__,
+        # 此处的 line 是从 0 开始计算的，
+        # 而在编辑器中，或者说从开发者角度来看，应该是从 1 开始计算
+        # 所以 +1 以修正行号的正确性
+        line + 1,
+        func.__name__
+    )
