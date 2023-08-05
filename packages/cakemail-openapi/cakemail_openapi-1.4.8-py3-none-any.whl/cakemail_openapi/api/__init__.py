0b66027da23dfed18e677b94048bdffb813328e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:36 +0000

    Add package cakemail-openapi/1.4.8/cakemail_openapi-1.4.8-py3-none-any.whl

---
+++ packages/cakemail-openapi/cakemail_openapi-1.4.8-py3-none-any.whl/cakemail_openapi/api/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cakemail_openapi.api.account_api import AccountApi
+from cakemail_openapi.api.action_api import ActionApi
+from cakemail_openapi.api.campaign_api import CampaignApi
+from cakemail_openapi.api.campaign_blueprint_api import CampaignBlueprintApi
+from cakemail_openapi.api.contact_api import ContactApi
+from cakemail_openapi.api.custom_attribute_api import CustomAttributeApi
+from cakemail_openapi.api.domain_api import DomainApi
+from cakemail_openapi.api.form_api import FormApi
+from cakemail_openapi.api.list_api import ListApi
+from cakemail_openapi.api.log_api import LogApi
+from cakemail_openapi.api.logo_api import LogoApi
+from cakemail_openapi.api.report_api import ReportApi
+from cakemail_openapi.api.segment_api import SegmentApi
+from cakemail_openapi.api.sender_api import SenderApi
+from cakemail_openapi.api.sub_account_api import SubAccountApi
+from cakemail_openapi.api.suppressed_email_api import SuppressedEmailApi
+from cakemail_openapi.api.template_api import TemplateApi
+from cakemail_openapi.api.token_api import TokenApi
+from cakemail_openapi.api.transactional_email_api import TransactionalEmailApi
+from cakemail_openapi.api.user_api import UserApi
+from cakemail_openapi.api.workflow_api import WorkflowApi
+from cakemail_openapi.api.workflow_blueprint_api import WorkflowBlueprintApi
