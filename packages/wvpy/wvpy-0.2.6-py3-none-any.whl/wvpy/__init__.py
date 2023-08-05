f3944d786b6b3598fe7954db2b4c99dbe5ce2939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:39 +0000

    Add package wvpy/0.2.6/wvpy-0.2.6-py3-none-any.whl

---
+++ packages/wvpy/wvpy-0.2.6-py3-none-any.whl/wvpy/__init__.py
@@ -0,0 +1,7 @@
+__docformat__ = "restructuredtext"
+__version__ = "0.2.6"
+
+__doc__ = """
+This<https://github.com/WinVector/wvpy> is a package of example files for teaching data science.
+It is not a replacement for scikit learn procedures.
+"""
