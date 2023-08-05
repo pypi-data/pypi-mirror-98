93c21e7b599b26dbc8bfa7587f6275759830e3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:06 +0000

    Add package blackfox/4.0.2/blackfox-4.0.2.tar.gz

---
+++ packages/blackfox/blackfox-4.0.2.tar.gz/blackfox-4.0.2/README.md
@@ -0,0 +1,61 @@
+# BlackFoxPython
+
+Python package for communication with BlackFoxWebAPI
+
+- API version: v1
+- Package version: 0.0.11
+
+## Requirements.
+
+Python 2.7 and 3.4+
+
+## Installation & Usage
+
+### pip install
+
+```sh
+pip install blackfox
+```
+
+### pip install tar.gz
+
+Download [blackfox-0.0.11.tar.gz](https://github.com/vodena/BlackFoxPython/raw/master/dist/blackfox-0.0.11.tar.gz)
+
+```sh
+pip install blackfox-0.0.11.tar.gz
+```
+
+You can install directly from Github
+
+```sh
+pip install git+https://github.com/vodena/BlackFoxPython.git
+```
+(you may need to run `pip` with root permission: `sudo pip install git+https://github.com/vodena/BlackFoxPython.git`)
+Please note that blackfox depends on [BlackFoxApi](https://github.com/vodena/BlackFoxRestApiPython) which must be available for blackfox to work. This means that you may need to install it manually if pip or others doesn't install it automatically.
+
+Then import the package:
+```python
+import blackfox 
+```
+
+### Setuptools
+
+Install via [Setuptools](http://pypi.python.org/pypi/setuptools).
+
+```sh
+python setup.py install --user
+```
+(or `sudo python setup.py install` to install the package for all users)
+
+Then import the package:
+```python
+import blackfox
+```
+
+## Getting Started
+
+Please follow the [installation procedure](#installation--usage) and then run examples
+
+
+
+
---
+++ packages/blackfox/blackfox-4.0.2.tar.gz/blackfox-4.0.2/blackfox/csv_log_writer.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import sys
+
+
+class CsvLogWriter(object):
+
+    def __init__(self, file=sys.stdout, only_change=True, clear_file=True):
+        self.log_file = file
+        self.write_string(
+            'Time,Status,Generation,Total generations,Validation set error,Training set error,Generation time [s],StartTime,End time,Optimization Id, Epoch', clear_file)
+        self.only_change = only_change
+        self.previous_generation = -1
+
+    def write_neural_network_statues(self, id, statuses):
+        if len(statuses) >= 2:
+            status = statuses[-2]
+            if self.only_change == False or self.previous_generation != status.generation:
+                self.__write_nn_status(id, status)
+                if statuses[-1].state != 'Active':
+                    self.__write_nn_status(id, statuses[-1])
+                self.previous_generation = status.generation
+
+    def __write_nn_status(self, id, status):
+        msg = ("%s,%s,%d,%d,%f,%f,%d,%s,%s,%s,%d") % (
+            datetime.now(),
+            status.state,
+            status.generation,
+            status.total_generations,
+            status.validation_set_error,
+            status.training_set_error,
+            status.generation_seconds,
+            status.start_date_time,
+            status.estimated_date_time,
+            id,
+            status.epoch
+        )
+        self.write_string(msg)
+
+    def write_random_forest_statues(self, id, statuses):
+        if len(statuses) >= 2:
+            status = statuses[-2]
+            if self.only_change == False or self.previous_generation != status.generation:
+                self.__write_rf_status(id, status)
+                if statuses[-1].state != 'Active':
+                    self.__write_rf_status(id, statuses[-1])
+                self.previous_generation = status.generation
+
+    def __write_rf_status(self, id, status):
+        msg = ("%s,%s,%d,%d,%f,%f,%d,%s,%s,%s") % (
+            datetime.now(),
+            status.state,
+            status.generation,
+            status.total_generations,
+            status.validation_set_error,
+            status.training_set_error,
+            status.generation_seconds,
+            status.start_date_time,
+            status.estimated_date_time,
+            id
+        )
+        self.write_string(msg)
+
+    def write_xgboost_statues(self, id, statuses):
+        self.write_random_forest_statues(id, statuses)
+
+    def write_string(self, msg, clear=False):
+        if isinstance(self.log_file, str):
+            mode = 'w' if clear else 'a'
+            with open(self.log_file, mode=mode, encoding='utf-8', buffering=1) as f:
+                f.write(msg+'\n')
+        else:
+            self.log_file.write(msg+'\n')
+            self.log_file.flush()
