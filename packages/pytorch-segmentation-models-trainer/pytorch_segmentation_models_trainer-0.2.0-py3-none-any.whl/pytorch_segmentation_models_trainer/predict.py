444e83d17fb1b1bce6374ff0b8e5ca536e83f784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:12 +0000

    Add package pytorch-segmentation-models-trainer/0.2.0/pytorch_segmentation_models_trainer-0.2.0-py3-none-any.whl

---
+++ packages/pytorch-segmentation-models-trainer/pytorch_segmentation_models_trainer-0.2.0-py3-none-any.whl/pytorch_segmentation_models_trainer/predict.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ pytorch_segmentation_models_trainer
+                              -------------------
+        begin                : 2021-03-02
+        git sha              : $Format:%H$
+        copyright            : (C) 2021 by Philipe Borba - Cartographic Engineer
+                                                            @ Brazilian Army
+        email                : philipeborba at gmail dot com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ****
+"""
+import hydra
+from omegaconf import DictConfig
+from pytorch_lightning import Trainer
+
+
+@hydra.main(config_path="conf", config_name="config")
+def predict(cfg: DictConfig) -> Trainer:
+    print("Not implemented yet.")
+    pass
+
+if __name__=="__main__":
+    predict()
