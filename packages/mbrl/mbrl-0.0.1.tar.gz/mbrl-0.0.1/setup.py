dbcb01a3afb0a36c10329d8c12836794233c433a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package mbrl/0.0.1/mbrl-0.0.1.tar.gz

---
+++ packages/mbrl/mbrl-0.0.1.tar.gz/mbrl-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup(
+    name='mbrl',
+    version=VERSION,
+    url='',
+    license='MIT',
+    author='Roberto Calandra',
+    author_email='rcalandra@fb.com',
+    description='Python library',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
+
