e4eb0fbb20b81b7998af86426b52f9d1abff1570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:46 +0000

    Add package probfit/1.2.0/probfit-1.2.0-cp36-cp36m-win32.whl

---
+++ packages/probfit/probfit-1.2.0-cp36-cp36m-win32.whl/probfit/decorator.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# decorators
+# @normalized_function(xmin,xmax)
+# def function_i_have_no_idea_how_to_normalize(x,y,z)
+#   return complicated_function(x,y,z)
+#
+from .functor import Extended, Normalized
+
+
+class normalized:
+    """
+    Normalized decorator
+
+    **Arguments**
+
+        - **bound** normalized bound
+        - **nint** option number of integral pieces. Default 1000.
+
+    .. seealso::
+
+        :class:`probfit.functor.Normalized`
+
+    """
+
+    def __init__(self, bound, nint=1000):
+        self.bound = bound
+        self.nint = nint
+
+    def __call__(self, f):
+        return Normalized(f, self.bound, self.nint)
+
+
+class extended:
+    """
+    Extended decorator
+
+    **Arguments**
+
+        - **extname** extended parameter name. Default 'N'
+
+    .. seealso::
+
+        :class:`probfit.functor.Extended`
+
+    """
+
+    def __init__(self, extname="N"):
+        self.extname = extname
+
+    def __call__(self, f):
+        return Extended(f, extname=self.extname)
