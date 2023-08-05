59cdf61b43a4ce2f13b9ab3ec133fcc76561e3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package slackbot-queue/0.3.4/slackbot-queue-0.3.4.tar.gz

---
+++ packages/slackbot-queue/slackbot-queue-0.3.4.tar.gz/slackbot-queue-0.3.4/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst', format='md')
+except (IOError, ImportError) as e:
+    print(str(e))
+    long_description = ''
+
+setup(
+    name='slackbot-queue',
+    packages=['slackbot_queue'],
+    version='0.3.4',
+    description='Slackbot with a celery queue for long running tasks',
+    long_description=long_description,
+    author='Eddy Hintze',
+    author_email="eddy@hintze.co",
+    url="https://github.com/xtream1101/slackbot-queue",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['celery==4.1.0',
+                      'slackclient==1.1.2'],
+)
