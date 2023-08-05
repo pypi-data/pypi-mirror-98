9dd759b1d33a8f92a6bd798aa95c69b3a7432d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package ruotvet/1.3.1/ruotvet-1.3.1-py3-none-any.whl

---
+++ packages/ruotvet/ruotvet-1.3.1-py3-none-any.whl/ruotvet/clients/__init__.py
@@ -0,0 +1,6 @@
+from .superresheba import SuperResheba
+from .chemequations import ChemEquations
+from .yandexq import YandexQ
+from .brainly import Brainly
+from .mailru import MailRu
+from .gdz import GDZ
