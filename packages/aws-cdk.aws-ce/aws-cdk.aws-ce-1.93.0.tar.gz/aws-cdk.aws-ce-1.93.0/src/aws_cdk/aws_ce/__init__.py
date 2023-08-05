98356aae55ffd4b2575e2a030e7fcf62220160dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package aws-cdk.aws-ce/1.93.0/aws-cdk.aws-ce-1.93.0.tar.gz

---
+++ packages/aws-cdk.aws-ce/aws-cdk.aws-ce-1.93.0.tar.gz/aws-cdk.aws-ce-1.93.0/src/aws_cdk/aws_ce/__init__.py
@@ -0,0 +1,230 @@
+'''
+# AWS::CE Construct Library
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![cfn-resources: Stable](https://img.shields.io/badge/cfn--resources-stable-success.svg?style=for-the-badge)
+
+> All classes with the `Cfn` prefix in this module ([CFN Resources](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_lib)) are always stable and safe to use.
+
+---
+<!--END STABILITY BANNER-->
+
+This module is part of the [AWS Cloud Development Kit](https://github.com/aws/aws-cdk) project.
+
+```python
+# Example automatically generated. See https://github.com/aws/jsii/issues/826
+import aws_cdk.aws_ce as ce
+```
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import aws_cdk.core
+
+
+@jsii.implements(aws_cdk.core.IInspectable)
+class CfnCostCategory(
+    aws_cdk.core.CfnResource,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-cdk/aws-ce.CfnCostCategory",
+):
+    '''A CloudFormation ``AWS::CE::CostCategory``.
+
+    :cloudformationResource: AWS::CE::CostCategory
+    :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html
+    '''
+
+    def __init__(
+        self,
+        scope: aws_cdk.core.Construct,
+        id: builtins.str,
+        *,
+        name: builtins.str,
+        rules: builtins.str,
+        rule_version: builtins.str,
+    ) -> None:
+        '''Create a new ``AWS::CE::CostCategory``.
+
+        :param scope: - scope in which this resource is defined.
+        :param id: - scoped id of the resource.
+        :param name: ``AWS::CE::CostCategory.Name``.
+        :param rules: ``AWS::CE::CostCategory.Rules``.
+        :param rule_version: ``AWS::CE::CostCategory.RuleVersion``.
+        '''
+        props = CfnCostCategoryProps(name=name, rules=rules, rule_version=rule_version)
+
+        jsii.create(CfnCostCategory, self, [scope, id, props])
+
+    @jsii.member(jsii_name="inspect")
+    def inspect(self, inspector: aws_cdk.core.TreeInspector) -> None:
+        '''(experimental) Examines the CloudFormation resource and discloses attributes.
+
+        :param inspector: - tree inspector to collect and process attributes.
+
+        :stability: experimental
+        '''
+        return typing.cast(None, jsii.invoke(self, "inspect", [inspector]))
+
+    @jsii.member(jsii_name="renderProperties")
+    def _render_properties(
+        self,
+        props: typing.Mapping[builtins.str, typing.Any],
+    ) -> typing.Mapping[builtins.str, typing.Any]:
+        '''
+        :param props: -
+        '''
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "renderProperties", [props]))
+
+    @jsii.python.classproperty # type: ignore[misc]
+    @jsii.member(jsii_name="CFN_RESOURCE_TYPE_NAME")
+    def CFN_RESOURCE_TYPE_NAME(cls) -> builtins.str:
+        '''The CloudFormation resource type name for this resource class.'''
+        return typing.cast(builtins.str, jsii.sget(cls, "CFN_RESOURCE_TYPE_NAME"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="attrArn")
+    def attr_arn(self) -> builtins.str:
+        '''
+        :cloudformationAttribute: Arn
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "attrArn"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="attrEffectiveStart")
+    def attr_effective_start(self) -> builtins.str:
+        '''
+        :cloudformationAttribute: EffectiveStart
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "attrEffectiveStart"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="cfnProperties")
+    def _cfn_properties(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.get(self, "cfnProperties"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="name")
+    def name(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.Name``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-name
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "name"))
+
+    @name.setter
+    def name(self, value: builtins.str) -> None:
+        jsii.set(self, "name", value)
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="rules")
+    def rules(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.Rules``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-rules
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "rules"))
+
+    @rules.setter
+    def rules(self, value: builtins.str) -> None:
+        jsii.set(self, "rules", value)
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="ruleVersion")
+    def rule_version(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.RuleVersion``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-ruleversion
+        '''
+        return typing.cast(builtins.str, jsii.get(self, "ruleVersion"))
+
+    @rule_version.setter
+    def rule_version(self, value: builtins.str) -> None:
+        jsii.set(self, "ruleVersion", value)
+
+
+@jsii.data_type(
+    jsii_type="@aws-cdk/aws-ce.CfnCostCategoryProps",
+    jsii_struct_bases=[],
+    name_mapping={"name": "name", "rules": "rules", "rule_version": "ruleVersion"},
+)
+class CfnCostCategoryProps:
+    def __init__(
+        self,
+        *,
+        name: builtins.str,
+        rules: builtins.str,
+        rule_version: builtins.str,
+    ) -> None:
+        '''Properties for defining a ``AWS::CE::CostCategory``.
+
+        :param name: ``AWS::CE::CostCategory.Name``.
+        :param rules: ``AWS::CE::CostCategory.Rules``.
+        :param rule_version: ``AWS::CE::CostCategory.RuleVersion``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html
+        '''
+        self._values: typing.Dict[str, typing.Any] = {
+            "name": name,
+            "rules": rules,
+            "rule_version": rule_version,
+        }
+
+    @builtins.property
+    def name(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.Name``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-name
+        '''
+        result = self._values.get("name")
+        assert result is not None, "Required property 'name' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def rules(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.Rules``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-rules
+        '''
+        result = self._values.get("rules")
+        assert result is not None, "Required property 'rules' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def rule_version(self) -> builtins.str:
+        '''``AWS::CE::CostCategory.RuleVersion``.
+
+        :link: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ce-costcategory.html#cfn-ce-costcategory-ruleversion
+        '''
+        result = self._values.get("rule_version")
+        assert result is not None, "Required property 'rule_version' is missing"
+        return typing.cast(builtins.str, result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "CfnCostCategoryProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "CfnCostCategory",
+    "CfnCostCategoryProps",
+]
+
+publication.publish()
