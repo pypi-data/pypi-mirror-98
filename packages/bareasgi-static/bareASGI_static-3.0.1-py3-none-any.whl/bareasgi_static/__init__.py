d056aadb53cd8a6c46f9c2ec86f5ac6b8356c387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:08 +0000

    Add package bareasgi-static/3.0.1/bareASGI_static-3.0.1-py3-none-any.whl

---
+++ packages/bareasgi-static/bareASGI_static-3.0.1-py3-none-any.whl/bareasgi_static/__init__.py
@@ -0,0 +1,11 @@
+"""Static file support for bareASGI"""
+
+from .static_files_provider import StaticFilesProvider, add_static_file_provider
+from .file_streaming import file_response, file_writer
+
+__all__ = [
+    "add_static_file_provider",
+    "StaticFilesProvider",
+    "file_response",
+    "file_writer"
+]
