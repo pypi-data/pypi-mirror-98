347ff554f80332f6d8049b8905961be7c02b38d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package aioaria2/1.3.1/aioaria2-1.3.1-py3-none-any.whl

---
+++ packages/aioaria2/aioaria2-1.3.1-py3-none-any.whl/aioaria2/exceptions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+本模块存放异常
+"""
+from typing import Optional
+
+
+class Aria2rpcException(Exception):
+    """
+    Base exception raised by this module.
+    """
+
+    def __init__(self, msg: str, connection_error: Optional[bool] = False):
+        super().__init__(msg)
+        self.msg = msg
+        self.connection_error = connection_error
+
+    def __str__(self):
+        return '{}: {}'.format(self.__class__.__name__, self.msg)
---
+++ packages/aioaria2/aioaria2-1.3.1-py3-none-any.whl/aioaria2/typing.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+支持类型注释
+"""
+from typing import (Callable,
+                    Dict,
+                    Awaitable,
+                    Any,
+                    TypeVar,
+                    TYPE_CHECKING)
+
+Aria2WebsocketTrigger = TypeVar("Aria2WebsocketTrigger", bound="Aria2WebsocketTrigger")
+
+if TYPE_CHECKING:
+    from aioaria2 import Aria2WebsocketTrigger
+
+CallBack = Callable[[Aria2WebsocketTrigger, Dict[str, Any]], Awaitable[Any]]
+"""
+Websocket事件的回调函数
+"""
+IdFactory = Callable[[], int]
+"""
+产生随机id的工厂函数 如果一定要参数可以用functools.partial
+"""
+
+# TODO 完成typing
