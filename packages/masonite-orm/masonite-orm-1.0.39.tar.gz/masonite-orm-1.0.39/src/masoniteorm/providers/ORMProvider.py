1176cbf3df10e2c6cd7fc1db53ec1560418f3d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:47 +0000

    Add package masonite-orm/1.0.39/masonite-orm-1.0.39.tar.gz

---
+++ packages/masonite-orm/masonite-orm-1.0.39.tar.gz/masonite-orm-1.0.39/src/masoniteorm/providers/ORMProvider.py
@@ -0,0 +1,34 @@
+from masonite.provider import ServiceProvider
+from masonite.helpers import config
+
+from masoniteorm.connections import ConnectionResolver
+from masoniteorm.commands import (
+    MakeMigrationCommand,
+    MakeSeedCommand,
+    MakeObserverCommand,
+    MigrateCommand,
+    MigrateRefreshCommand,
+    MigrateRollbackCommand,
+    SeedRunCommand,
+)
+
+
+class ORMProvider(ServiceProvider):
+    """Masonite ORM database provider to be used inside
+    Masonite based projects."""
+
+    wsgi = False
+
+    def register(self):
+        self.commands(
+            MakeMigrationCommand(),
+            MakeSeedCommand(),
+            MakeObserverCommand(),
+            MigrateCommand(),
+            MigrateRefreshCommand(),
+            MigrateRollbackCommand(),
+            SeedRunCommand(),
+        )
+
+    def boot(self):
+        pass
