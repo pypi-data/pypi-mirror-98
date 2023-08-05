7b5b563abd7736cbd24d546a18051b4b1e2545e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:38 +0000

    Add package research-utils/0.0.1/research-utils-0.0.1.tar.gz

---
+++ packages/research-utils/research-utils-0.0.1.tar.gz/research-utils-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['research_utils', 'research_utils.sqlite', 'research_utils.torch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytorch-lightning>=1.2.2,<2.0.0', 'sqlite-utils>=3.6,<4.0']
+
+setup_kwargs = {
+    'name': 'research-utils',
+    'version': '0.0.1',
+    'description': 'some utils for my research',
+    'long_description': None,
+    'author': 'Rainforest Cheng',
+    'author_email': 'r08521610@ntu.edu.tw',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
