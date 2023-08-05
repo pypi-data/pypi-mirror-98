f0eb739320123640d2ee5d1d64fed9d956f2c245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package defectdojo-cli/0.3.2/defectdojo_cli-0.3.2.tar.gz

---
+++ packages/defectdojo-cli/defectdojo_cli-0.3.2.tar.gz/defectdojo_cli-0.3.2/README.md
@@ -0,0 +1,17 @@
+## DefectDojo CLI
+[![License](https://img.shields.io/badge/license-MIT-_red.svg)](https://opensource.org/licenses/MIT)
+
+A CLI wrapper for [DefectDojo](https://github.com/DefectDojo/django-DefectDojo)
+
+## Installation
+
+Simply run:
+```
+$ python3 -m pip install defectdojo_cli
+```
+
+## Usage
+
+```
+$ defectdojo --help
+```
---
+++ packages/defectdojo-cli/defectdojo_cli-0.3.2.tar.gz/defectdojo_cli-0.3.2/defectdojo_cli/__main__.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+from defectdojo_cli import Findings
+from defectdojo_cli import Engagements
+from defectdojo_cli import __version__
+
+# Multilevel argparse based on https://chase-seibert.github.io/blog/2014/03/21/python-multilevel-argparse.html
+class DefectDojoCLI(object):
+    def parse_cli_args(self):
+        parser = argparse.ArgumentParser(
+                description='CLI wrapper for DefectDojo using APIv2',
+                usage='''defectdojo <command> [<args>]
+
+    You can use the following commands:
+            findings        Operations related to findings (findings --help for more details)
+            engagements     Operations related to engagements (engagements --help for more details)
+        ''')
+        parser.add_argument('command', help='Command to run', choices=['findings', 'engagements'])
+        parser.add_argument('-v', '--version', action='version', version='%(prog)s_cli v' + __version__)
+        # Parse_args defaults to [1:] for args, but you need to
+        # exclude the rest of the args too, or validation will fail
+        args = parser.parse_args(sys.argv[1:2])
+        # Use dispatch pattern to invoke method with same name (that starts with _)
+        getattr(self, '_'+args.command)()
+
+    def _findings(self):
+        Findings().parse_cli_args()
+
+    def _engagements(self):
+        Engagements().parse_cli_args()
+
+def main():
+    DefectDojoCLI().parse_cli_args()
+
+if __name__ == '__main__':
+    main()
