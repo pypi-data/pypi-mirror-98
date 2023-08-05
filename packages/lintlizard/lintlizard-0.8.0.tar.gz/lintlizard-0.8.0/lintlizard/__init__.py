67bd5ada51b2cce82aea86bba8b0114daf020f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:06 +0000

    Add package lintlizard/0.8.0/lintlizard-0.8.0.tar.gz

---
+++ packages/lintlizard/lintlizard-0.8.0.tar.gz/lintlizard-0.8.0/lintlizard/__init__.py
@@ -0,0 +1,133 @@
+import argparse
+import subprocess
+from typing import Iterable, Optional, Tuple
+
+from attr import attrib, attrs
+
+__version__ = '0.8.0'
+
+
+Command = Tuple[str, ...]
+
+
+@attrs(frozen=True)
+class CommandTool:
+    executable: str = attrib()
+    run_params: Command = attrib(default=())
+    fix_params: Optional[Command] = attrib(default=None)
+
+    # if set, supports running with individually specified files.
+    # Otherwise, always runs without specifying files.
+    default_files: Optional[Command] = attrib(default=None)
+
+    def run_command(self) -> Command:
+        return (self.executable,) + self.run_params
+
+    @property
+    def fixable(self):
+        return self.fix_params is not None
+
+    def fix_command(self) -> Command:
+        if self.fix_params is None:
+            raise RuntimeError('Command is not fixable')
+        return (self.executable,) + self.fix_params
+
+    def version_command(self) -> Command:
+        return self.executable, '--version'
+
+
+TOOLS = [
+    CommandTool('flake8', default_files=()),
+    CommandTool(
+        'isort', run_params=('-c',), fix_params=tuple(), default_files=('.',)
+    ),
+    CommandTool('mypy'),
+    CommandTool(
+        'black',
+        run_params=('--check',),
+        fix_params=tuple(),
+        default_files=('.',),
+    ),
+]
+
+
+def execute_tools(fix: bool, files: Tuple[str, ...]) -> Iterable[bool]:
+    tools = TOOLS
+    if fix:
+        tools = [tool for tool in tools if tool.fixable]
+    for tool in tools:
+        print('*' * 79)
+        try:
+            subprocess.run(args=tool.version_command(), check=True)
+            cmd = (
+                tool.fix_command()
+                if fix and tool.fix_params is not None
+                else tool.run_command()
+            )
+            if tool.default_files is not None:
+                cmd = cmd + (files or tool.default_files)
+            subprocess.run(args=cmd, check=True)
+        except subprocess.CalledProcessError:
+            yield False
+        else:
+            yield True
+
+
+def get_changed_files() -> Iterable[str]:
+    try:
+        subprocess.run(args=['git', '--version'], check=True)
+        result = subprocess.run(
+            args=[
+                "git",
+                "diff",
+                "--name-only",
+                "--cached",
+                "--diff-filter=d",
+                "HEAD",
+                "--",
+                "*.py",
+            ],
+            stdout=subprocess.PIPE,
+        )
+        # if no files are changed, don't return ['']
+        return [
+            i for i in result.stdout.decode('utf8').strip().split('\n') if i
+        ]
+    except subprocess.CalledProcessError:
+        raise Exception('Error encountered when determining changed files.')
+
+
+def main() -> None:
+    args = make_arg_parser().parse_args()
+    if args.version:
+        print(f"lintlizard v{__version__}")
+        return
+
+    files = list(args.files) or []
+
+    if args.changed:
+        changed_files = get_changed_files()
+        if not changed_files and not files:
+            # ran as `lintlizard --changed` but nothing changed, don't run
+            # anything
+            return
+
+        files.extend(changed_files)
+
+    tool_results = list(execute_tools(fix=args.fix, files=tuple(files)))
+    if not all(tool_results):
+        exit(1)
+
+
+def make_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fix', action='store_true')
+    parser.add_argument('--changed', action='store_true')
+    parser.add_argument('--version', action='store_true')
+    parser.add_argument('files', nargs='*', default=None)
+
+    return parser
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/lintlizard/lintlizard-0.8.0.tar.gz/lintlizard-0.8.0/setup.py
@@ -0,0 +1,52 @@
+import io
+import re
+
+import setuptools
+
+VERSION_FILE = "lintlizard/__init__.py"
+with io.open(VERSION_FILE, "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = ([\'"])(.*?)\1', f.read()).group(2)
+
+setuptools.setup(
+    name="lintlizard",
+    version=version,
+    author="Vyacheslav Tverskoy",
+    author_email="v@close.com",
+    description="Run various linters to ensure a common code quality baseline",
+    url="https://github.com/closeio/lintlizard",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'attrs',
+        'black==20.8b1',
+        'flake8==3.8.4',
+        'flake8-bugbear==21.3.2',
+        'flake8-docstrings==1.5.0',
+        'flake8-future-import==0.4.6',
+        'flake8-no-u-prefixed-strings==0.2',
+        'flake8-polyfill==1.0.2',
+        'flake8-tidy-imports==4.2.1',
+        'isort==5.7.0',
+        'mccabe==0.6.1',
+        'mypy==0.812',
+        'mypy-extensions==0.4.3',
+        'pathspec==0.8.1',
+        'pep8==1.7.1',
+        'pep8-naming==0.11.1',
+        'pycodestyle==2.6.0',
+        'pydocstyle==5.1.1',
+        'pyflakes==2.2.0',
+        'snowballstemmer==2.1.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'lintlizard=lintlizard:main',
+        ],
+    },
+)
