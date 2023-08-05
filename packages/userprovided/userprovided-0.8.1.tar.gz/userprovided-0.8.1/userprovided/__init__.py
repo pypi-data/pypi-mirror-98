e7805381b42903f4f2f9c6299a53360afc80ea31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package userprovided/0.8.1/userprovided-0.8.1.tar.gz

---
+++ packages/userprovided/userprovided-0.8.1.tar.gz/userprovided-0.8.1/userprovided/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+userprovided library
+~~~~~~~~~~~~~~~~~~~~~
+Source: https://github.com/RuedigerVoigt/userprovided
+(c) 2020-2021 Rüdiger Voigt
+Released under the Apache License 2.0
+"""
+
+from userprovided import cloud
+from userprovided import date
+from userprovided import hash
+from userprovided import mail
+from userprovided import parameters
+from userprovided import port
+from userprovided import url
+
+
+name = "userprovided"
+__version__ = "0.8.0"
+__author__ = "Rüdiger Voigt"
