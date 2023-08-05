7eed1a8128c3a441970371214544b452054792db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:00 +0000

    Add package TWP/20/TWP-20.tar.gz

---
+++ packages/TWP/TWP-20.tar.gz/TWP-20/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='TWP',
+      version='20',
+      description='tweet with picture',
+      author='Kerota',
+      author_email='hifuna2727@gmail.com',
+      url='https://twitter.com',
+
+     
+     )
