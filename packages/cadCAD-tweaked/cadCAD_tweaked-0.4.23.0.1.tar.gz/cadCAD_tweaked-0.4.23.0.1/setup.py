7273c05dfba08cc666674f6c4cdd61c86c11939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:09 +0000

    Add package cadCAD-tweaked/0.4.23.0.1/cadCAD_tweaked-0.4.23.0.1.tar.gz

---
+++ packages/cadCAD-tweaked/cadCAD_tweaked-0.4.23.0.1.tar.gz/cadCAD_tweaked-0.4.23.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+short_description = "cadCAD: tweaked version"
+
+long_description = """
+This is a tweaked version of cadCAD, use at your own peril.
+"""
+
+setup(name='cadCAD_tweaked',
+      version='0.4.23.0.1',
+      description=short_description,
+      long_description=long_description,
+      url='https://github.com/danlessa/cadCAD-tweaked',
+      author='Danilo Lessa Bernardineli',
+      author_email='danilo.lessa@gmail.com',
+      license='LICENSE.txt',
+      packages=find_packages(),
+      install_requires=[
+            "pandas",
+            "pathos",
+            "fn",
+            "funcy"
+      ],
+      python_requires='>=3.6.8'
+)
