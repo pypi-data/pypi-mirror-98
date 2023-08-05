2f0786c3fe0d4367676f7f5c32436c841dfb9d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:22 +0000

    Add package sparkautomapper.fhir/0.2.0/sparkautomapper.fhir-0.2.0-py3-none-any.whl

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.2.0-py3-none-any.whl/spark_auto_mapper_fhir/extensions/custom/base_extension_item.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.date_time import FhirDateTime
+from spark_auto_mapper_fhir.fhir_types.string import FhirString
+
+
+class BaseExtensionItem(ExtensionBase):
+    # noinspection PyPep8Naming
+    def __init__(
+        self,
+        url: str,
+        valueString: Optional[FhirString] = None,
+        valueDateTime: Optional[FhirDateTime] = None,
+        include_null_properties: bool = True
+    ) -> None:
+        super().__init__(
+            url=url, valueString=valueString, valueDateTime=valueDateTime
+        )
+        super().include_null_properties(include_null_properties=True)
