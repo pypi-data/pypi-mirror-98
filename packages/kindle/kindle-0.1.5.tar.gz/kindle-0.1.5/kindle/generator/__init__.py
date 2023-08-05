740adeac1f9098492111871f51ae4b6d91c92611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:49 +0000

    Add package kindle/0.1.5/kindle-0.1.5.tar.gz

---
+++ packages/kindle/kindle-0.1.5.tar.gz/kindle-0.1.5/kindle/generator/__init__.py
@@ -0,0 +1,36 @@
+"""PyTorch Module Generator for parsing model yaml file.
+
+- Author: Jongkuk Lim
+- Contact: lim.jeikei@gmail.com
+"""
+
+from kindle.generator.add import AddGenerator
+from kindle.generator.base_generator import GeneratorAbstract, ModuleGenerator
+from kindle.generator.bottleneck import BottleneckGenerator
+from kindle.generator.concat import ConcatGenerator
+from kindle.generator.conv import ConvGenerator
+from kindle.generator.custom_yaml_module import YamlModuleGenerator
+from kindle.generator.dwconv import DWConvGenerator
+from kindle.generator.flatten import FlattenGenerator
+from kindle.generator.linear import LinearGenerator
+from kindle.generator.poolings import (AvgPoolGenerator,
+                                       GlobalAvgPoolGenerator,
+                                       MaxPoolGenerator)
+from kindle.generator.upsample import UpSampleGenerator
+
+__all__ = [
+    "ModuleGenerator",
+    "GeneratorAbstract",
+    "BottleneckGenerator",
+    "ConcatGenerator",
+    "ConvGenerator",
+    "DWConvGenerator",
+    "FlattenGenerator",
+    "LinearGenerator",
+    "AvgPoolGenerator",
+    "GlobalAvgPoolGenerator",
+    "MaxPoolGenerator",
+    "YamlModuleGenerator",
+    "AddGenerator",
+    "UpSampleGenerator",
+]
