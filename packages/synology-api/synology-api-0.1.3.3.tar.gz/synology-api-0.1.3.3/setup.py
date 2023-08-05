128640888c4d06bef5062c750560d7623c07c06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:02 +0000

    Add package synology-api/0.1.3.3/synology-api-0.1.3.3.tar.gz

---
+++ packages/synology-api/synology-api-0.1.3.3.tar.gz/synology-api-0.1.3.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='synology-api',
+    version='0.1.3.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Python Synology API Wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['requests'],
+    url='https://github.com/N4S4/synology-api',
+    author='Renato Visaggio',
+    author_email='synology.python.api@gmail.com'
+)
