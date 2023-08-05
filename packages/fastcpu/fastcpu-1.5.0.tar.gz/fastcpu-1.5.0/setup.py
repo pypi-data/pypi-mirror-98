37e6a3d5d08f6e6105917bd25a5c60344bb3f1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:45 +0000

    Add package fastcpu/1.5.0/fastcpu-1.5.0.tar.gz

---
+++ packages/fastcpu/fastcpu-1.5.0.tar.gz/fastcpu-1.5.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# https://packaging.python.org/guides/distributing-packages-using-setuptools/#console-scripts
+
+setuptools.setup(
+    name="fastcpu",
+    version="1.5.0",
+    author="Tawanda Minya",
+    author_email="tminya@gmail.com",
+    description="A queue service for quickly running scripts sequentially to use all your CPUs efficiently",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tawanda/fastcpu",
+    project_urls={
+        "Bug Tracker": "https://github.com/tawanda/fastcpu/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.0",
+    install_requires=[
+        'fastcore',
+        'psutil',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fastcpu_poll=fastcpu.cli:fastcpu_poll',
+        ],
+    },
+)
