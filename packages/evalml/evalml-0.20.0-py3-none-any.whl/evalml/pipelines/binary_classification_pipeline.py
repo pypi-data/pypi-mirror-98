6e9b05853926ca30b5ffefc224ca701f402bbd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:16 +0000

    Add package evalml/0.20.0/evalml-0.20.0-py3-none-any.whl

---
+++ packages/evalml/evalml-0.20.0-py3-none-any.whl/evalml/pipelines/binary_classification_pipeline.py
@@ -0,0 +1,62 @@
+from evalml.objectives import get_objective
+from evalml.pipelines.classification_pipeline import ClassificationPipeline
+from evalml.problem_types import ProblemTypes
+from evalml.utils import infer_feature_types
+
+
+class BinaryClassificationPipeline(ClassificationPipeline):
+    """Pipeline subclass for all binary classification pipelines."""
+    _threshold = None
+    problem_type = ProblemTypes.BINARY
+
+    @property
+    def threshold(self):
+        """Threshold used to make a prediction. Defaults to None."""
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, value):
+        self._threshold = value
+
+    def _predict(self, X, objective=None):
+        """Make predictions using selected features.
+
+        Arguments:
+            X (ww.DataTable, pd.DataFrame): Data of shape [n_samples, n_features]
+            objective (Object or string): The objective to use to make predictions
+
+        Returns:
+            ww.DataColumn: Estimated labels
+        """
+
+        if objective is not None:
+            objective = get_objective(objective, return_instance=True)
+            if not objective.is_defined_for_problem_type(self.problem_type):
+                raise ValueError("You can only use a binary classification objective to make predictions for a binary classification pipeline.")
+
+        if self.threshold is None:
+            return self._component_graph.predict(X)
+        ypred_proba = self.predict_proba(X).to_dataframe()
+        ypred_proba = ypred_proba.iloc[:, 1]
+        if objective is None:
+            return infer_feature_types(ypred_proba > self.threshold)
+        return infer_feature_types(objective.decision_function(ypred_proba, threshold=self.threshold, X=X))
+
+    def predict_proba(self, X):
+        """Make probability estimates for labels. Assumes that the column at index 1 represents the positive label case.
+
+        Arguments:
+            X (ww.DataTable, pd.DataFrame or np.ndarray): Data of shape [n_samples, n_features]
+
+        Returns:
+            ww.DataTable: Probability estimates
+        """
+        return super().predict_proba(X)
+
+    @staticmethod
+    def _score(X, y, predictions, objective):
+        """Given data, model predictions or predicted probabilities computed on the data, and an objective, evaluate and return the objective score.
+        """
+        if predictions.ndim > 1:
+            predictions = predictions.iloc[:, 1]
+        return ClassificationPipeline._score(X, y, predictions, objective)
---
+++ packages/evalml/evalml-0.20.0-py3-none-any.whl/evalml/tests/pipeline_tests/classification_pipeline_tests/test_binary_classification.py
@@ -0,0 +1,68 @@
+from unittest.mock import patch
+
+import pandas as pd
+import pytest
+import woodwork as ww
+
+
+@patch('evalml.pipelines.ClassificationPipeline._decode_targets', return_value=[0, 1])
+@patch('evalml.objectives.BinaryClassificationObjective.decision_function', return_value=pd.Series([1, 0]))
+@patch('evalml.pipelines.components.Estimator.predict_proba', return_value=ww.DataTable(pd.DataFrame([[0.1, 0.2], [0.1, 0.2]])))
+@patch('evalml.pipelines.components.Estimator.predict', return_value=ww.DataColumn(pd.Series([1, 0])))
+def test_binary_classification_pipeline_predict(mock_predict, mock_predict_proba,
+                                                mock_obj_decision, mock_decode,
+                                                X_y_binary, dummy_binary_pipeline_class):
+    mock_objs = [mock_decode, mock_predict]
+    X, y = X_y_binary
+    binary_pipeline = dummy_binary_pipeline_class(parameters={"Logistic Regression Classifier": {"n_jobs": 1}})
+    # test no objective passed and no custom threshold uses underlying estimator's predict method
+    binary_pipeline.fit(X, y)
+    binary_pipeline.predict(X)
+    for mock_obj in mock_objs:
+        mock_obj.assert_called()
+        mock_obj.reset_mock()
+
+    # test objective passed but no custom threshold uses underlying estimator's predict method
+    binary_pipeline.predict(X, 'precision')
+    for mock_obj in mock_objs:
+        mock_obj.assert_called()
+        mock_obj.reset_mock()
+
+    mock_objs = [mock_decode, mock_predict_proba]
+    # test custom threshold set but no objective passed
+    binary_pipeline.threshold = 0.6
+    binary_pipeline._encoder.classes_ = [0, 1]
+    binary_pipeline.predict(X)
+    for mock_obj in mock_objs:
+        mock_obj.assert_called()
+        mock_obj.reset_mock()
+    mock_obj_decision.assert_not_called()
+    mock_predict.assert_not_called()
+
+    # test custom threshold set but no objective passed
+    binary_pipeline.threshold = 0.6
+    binary_pipeline.predict(X)
+    for mock_obj in mock_objs:
+        mock_obj.assert_called()
+        mock_obj.reset_mock()
+    mock_obj_decision.assert_not_called()
+    mock_predict.assert_not_called()
+
+    # test custom threshold set and objective passed
+    binary_pipeline.threshold = 0.6
+    binary_pipeline.predict(X, 'precision')
+    for mock_obj in mock_objs:
+        mock_obj.assert_called()
+        mock_obj.reset_mock()
+    mock_predict.assert_not_called()
+    mock_obj_decision.assert_called()
+
+
+@patch('evalml.pipelines.ComponentGraph._compute_features')
+def test_binary_predict_pipeline_objective_mismatch(mock_transform, X_y_binary, dummy_binary_pipeline_class):
+    X, y = X_y_binary
+    binary_pipeline = dummy_binary_pipeline_class(parameters={"Logistic Regression Classifier": {"n_jobs": 1}})
+    binary_pipeline.fit(X, y)
+    with pytest.raises(ValueError, match="You can only use a binary classification objective to make predictions for a binary classification pipeline."):
+        binary_pipeline.predict(X, "precision micro")
+    mock_transform.assert_called()
