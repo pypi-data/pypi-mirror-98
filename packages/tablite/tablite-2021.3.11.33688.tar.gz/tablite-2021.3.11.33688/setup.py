22fcb0b3caf1bdc0ffb32775360e77d4049dd83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:09 +0000

    Add package tablite/2021.3.11.33688/tablite-2021.3.11.33688.tar.gz

---
+++ packages/tablite/tablite-2021.3.11.33688.tar.gz/tablite-2021.3.11.33688/setup.py
@@ -0,0 +1,58 @@
+"""
+tablite
+"""
+build_tag = "887ebc5447cb248b321dec0ac3c3a19f1e4501a34db9a645234da74f0eae5c45"
+from setuptools import setup
+from pathlib import Path
+
+
+folder = Path(__file__).parent
+file = "README.md"
+readme = folder / file
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+keywords = list({
+    'tablite', 'tables', 'csv', 'txt', 'excel', 'xlsx', 'ods', 'zip', 'log',
+    'any', 'all', 'filter', 'column', 'columns', 'rows', 'from', 'json', 'to',
+    'inner join', 'outer join', 'left join', 'groupby', 'pivot', 'pivot tablite',
+    'sort', 'is sorted', 'show', 'use disk', 'out-of-memory', 'list on disk',
+    'stored list', 'min', 'max', 'sum', 'first', 'last', 'count', 'unique',
+    'average', 'standard deviation', 'median', 'mode', 'in-memory', 'index'
+})
+
+keywords.sort(key=lambda x: x.lower())
+
+with open('requirements.txt', 'r') as fi:
+    requirements = [v.rstrip('\n') for v in fi.readlines()]
+
+
+setup(
+    name="tablite",
+    version="2021.3.11.33688",
+    url="https://github.com/root-11/tablite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A tablite crunching library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=keywords,
+    packages=["tablite"],
+    include_package_data=True,
+    data_files=[(".", ["LICENSE", "README.md", "requirements.txt"])],
+    platforms="any",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
+
+
---
+++ packages/tablite/tablite-2021.3.11.33688.tar.gz/tablite-2021.3.11.33688/tablite/__init__.py
@@ -0,0 +1,8 @@
+from tablite.core import Table, GroupBy
+
+__all__ = [
+    Table, GroupBy
+]
+
+
+
