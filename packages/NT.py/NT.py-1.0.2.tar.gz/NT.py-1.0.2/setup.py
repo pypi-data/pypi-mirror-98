cd2b74e994fdd69ca46d8b7258e3e93acb665e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:13 +0000

    Add package NT.py/1.0.2/NT.py-1.0.2.tar.gz

---
+++ packages/NT.py/NT.py-1.0.2.tar.gz/NT.py-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NT.py",
+    version="1.0.2",
+    author="adl212",
+    author_email="emailforpythoncoding@gmail.com",
+    description="A package to use the nitrotype api and get player or team stats",
+    long_description=long_description, # don't touch this, this is your README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/adl212/NT.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
