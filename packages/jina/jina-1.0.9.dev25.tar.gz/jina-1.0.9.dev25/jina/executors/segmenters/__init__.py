09d9db3ee9907be6b6fc18594a084212e45885e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:01 +0000

    Add package jina/1.0.9.dev25/jina-1.0.9.dev25.tar.gz

---
+++ packages/jina/jina-1.0.9.dev25.tar.gz/jina-1.0.9.dev25/jina/executors/segmenters/__init__.py
@@ -0,0 +1,27 @@
+import inspect
+from typing import Dict, List
+
+from .. import BaseExecutor
+from ...helper import typename
+
+
+class BaseSegmenter(BaseExecutor):
+    """:class:`BaseSegmenter` works on doc-level,
+    it chunks Documents into set of Chunks"""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor."""
+        super().__init__(*args, **kwargs)
+        self.required_keys = {
+            k for k in inspect.getfullargspec(self.segment).args if k != 'self'
+        }
+        if not self.required_keys:
+            self.logger.warning(
+                f'{typename(self)} works on keys, but no keys are specified'
+            )
+
+    def segment(self, *args, **kwargs) -> List[Dict]:
+        """
+        :return: a list of chunks-level info represented by a dict
+        """
+        raise NotImplementedError
