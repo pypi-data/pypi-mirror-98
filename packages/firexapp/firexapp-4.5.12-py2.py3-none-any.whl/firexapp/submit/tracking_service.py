be48a8a875f49cc6872b057810b4cf9f507e771d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:22 +0000

    Add package firexapp/4.5.12/firexapp-4.5.12-py2.py3-none-any.whl

---
+++ packages/firexapp/firexapp-4.5.12-py2.py3-none-any.whl/firexapp/submit/tracking_service.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+from firexapp.discovery import get_firex_tracking_services_entry_points, prune_duplicate_module_entry_points
+from firexapp.submit.install_configs import FireXInstallConfigs
+
+_services = None
+
+
+def get_tracking_services() -> ():
+    global _services
+    if _services is None:
+        entry_pts = get_firex_tracking_services_entry_points()
+        entry_objects = [e.load() for e in entry_pts]
+        _services = tuple([point() for point in entry_objects])
+    return _services
+
+
+def has_flame():
+    # Unfortunate coupling, but just too many things vary depending on presence of flame. Will eventually bring
+    # flame in to firexapp.
+    return 'FlameLauncher' in get_tracking_services()
+
+
+class TrackingService(ABC):
+
+    install_configs: FireXInstallConfigs
+
+    def extra_cli_arguments(self, arg_parser):
+        pass
+
+    @abstractmethod
+    def start(self, args, install_configs: FireXInstallConfigs, **kwargs) -> {}:
+        self.install_configs = install_configs
+
+    def ready_for_tasks(self, **kwargs) -> bool:
+        return True
+
+    def ready_release_console(self, **kwargs) -> bool:
+        return True
+
+    def get_version(self) -> str:
+        return None
+
+
+def get_service_name(service: TrackingService) -> str:
+    return service.__class__.__name__
