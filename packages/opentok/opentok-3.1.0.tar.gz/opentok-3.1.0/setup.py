7d75e45a352e879ef939eea30561db9f75f31a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:47 +0000

    Add package opentok/3.1.0/opentok-3.1.0.tar.gz

---
+++ packages/opentok/opentok-3.1.0.tar.gz/opentok-3.1.0/setup.py
@@ -0,0 +1,87 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+# Get the long description from the relevant file
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requires = [
+    'enum34  ; python_version < "3.4"',
+    'requests',
+    'six',
+    'pytz',
+    'python-jose'
+]
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 4:
+    install_requires.append('enum34')
+
+if sys.version_info[0] < 3:
+    install_requires.append('rsa<=4.0')
+
+setup(
+    name = 'opentok',
+    version = find_version('opentok', 'version.py'),
+    description = 'OpenTok server-side SDK',
+    long_description_content_type='text/x-rst',
+    url='https://github.com/opentok/Opentok-Python-SDK/',
+    long_description=long_description,
+
+    author='TokBox, Inc.',
+    author_email='support@tokbox.com',
+    license='LICENSE.txt',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Conferencing',
+        'Topic :: Multimedia :: Video :: Capture',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+
+    keywords = 'video chat tokbox tok opentok python media webrtc archiving realtime',
+
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+
+    install_requires=install_requires,
+
+    include_package_data = True,
+)
