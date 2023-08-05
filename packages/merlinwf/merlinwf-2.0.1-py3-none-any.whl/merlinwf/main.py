ad9de3682f48d44c45effdc1ba5e1dc62ce62835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:10 +0000

    Add package merlinwf/2.0.1/merlinwf-2.0.1-py3-none-any.whl

---
+++ packages/merlinwf/merlinwf-2.0.1-py3-none-any.whl/merlinwf/main.py
@@ -0,0 +1,7 @@
+
+def main():
+    DEPRECATION_MESSAGE = "The 'merlinwf' package has been deprecated and replaced by the 'merlin' package."
+    print(DEPRECATION_MESSAGE)
+
+if __name__ == '__main__':
+    main()
