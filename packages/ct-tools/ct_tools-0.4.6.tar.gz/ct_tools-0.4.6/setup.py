706595fc517359eb68edcfb36c40074683fe18e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:47 +0000

    Add package ct-tools/0.4.6/ct_tools-0.4.6.tar.gz

---
+++ packages/ct-tools/ct_tools-0.4.6.tar.gz/ct_tools-0.4.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+setup(
+	name = "ct_tools",
+	version = "0.4.6",
+	description = "ct tools",
+	author = "Vvegetables",
+	author_email = "hardwork_fight@163.com",
+	license = "Public domain",
+	url = "https://github.com/Vvegetables/ct_tools",
+	packages = find_packages(),
+	platforms = ["all"],
+	install_requires = [
+		"Django >= 1.11.0, < 2.0",
+		"PyMysql >= 0.9.2",
+		"openpyxl"
+	],
+	classifiers = [
+		"Development Status :: 2 - Pre-Alpha",
+		"Environment :: Web Environment",
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+	include_package_data=True,    
+	exclude_package_date={'':['.gitignore']}
+)
