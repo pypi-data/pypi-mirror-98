26dd908cf1eb59c6b826f11835dac0d2cfa787f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:25 +0000

    Add package appmap/0.6.0/appmap-0.6.0.tar.gz

---
+++ packages/appmap/appmap-0.6.0.tar.gz/appmap-0.6.0/appmap/__init__.py
@@ -0,0 +1,11 @@
+"""AppMap recorder for Python"""
+
+
+from ._implementation.env import Env  # noqa: F401
+from ._implementation.recording import Recording, instrument_module  # noqa: F401
+from ._implementation import generation  # noqa: F401
+from ._implementation.labels import labels  # noqa: F401
+
+
+def enabled():
+    return Env.current.enabled
