285108afb7ac858961b014eb52f2625cfaea02f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package nationality-predictor/1.2.1/nationality_predictor-1.2.1.tar.gz

---
+++ packages/nationality-predictor/nationality_predictor-1.2.1.tar.gz/nationality_predictor-1.2.1/README.md
@@ -0,0 +1,38 @@
+# nationality-predictor
+
+[![npm](https://img.shields.io/pypi/v/nationality-predictor.svg)](https://pypi.org/project/nationality-predictor/)
+
+This engine attempts to predict the nationality of a person's name.
+
+## Installation
+
+Run the following to install:
+
+```console
+pip install nationality-predictor
+```
+
+## Usage
+
+### Predict
+
+```python
+predict()
+```
+
+Params:
+
+```
+Name: String ? The name that has to be predicted.
+```
+
+Demo:
+
+```python
+from nationality_predictor import predict
+
+predict("Thomas")
+```
+
+# License
+[MIT](https://github.com/dewittethomas/nationality-predictor/blob/master/LICENSE)
---
+++ packages/nationality-predictor/nationality_predictor-1.2.1.tar.gz/nationality_predictor-1.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name = "nationality_predictor",
+    version = "1.2.1",
+    license = "MIT",
+    url = "https://github.com/dewittethomas/nationality-predictor",
+    
+    description = "An engine that predicts the nationality of a person's name",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+
+    package_dir = {"nationality_predictor": "nationality_predictor"},
+    install_requires = [
+        "requests>=2.22.0", 
+        "pycountry>=19.8.18"
+    ],
+
+    packages = find_packages(),
+
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+
+    keywords = "name nationality API person predict predictor"
+)
