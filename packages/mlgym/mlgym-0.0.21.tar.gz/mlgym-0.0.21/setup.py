9cde892156f1d28ee6a7dd950d939fe88d8945a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package mlgym/0.0.21/mlgym-0.0.21.tar.gz

---
+++ packages/mlgym/mlgym-0.0.21.tar.gz/mlgym-0.0.21/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mlgym',
+    version='0.0.21',
+    author='Max Luebbering, Rajkumar Ramamurthy',
+    description="MLgym, a python framework for distributed machine learning model training in research.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "pytest",
+        "pytest-cov",
+        "torch",
+        "h5py",
+        "tqdm",
+        "pyyaml",
+        "datastack",
+        "scipy",
+        "dashifyML",
+        "scikit-learn"
+    ],
+    python_requires=">=3.7",
+    url="https://github.com/le1nux/mlgym"
+)
