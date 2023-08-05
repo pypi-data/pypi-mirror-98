d69c1bb1b873a5b1840d6936e9ef85a207946a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package sparkautomapper.fhir/0.2.1/sparkautomapper.fhir-0.2.1-py3-none-any.whl

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.2.1-py3-none-any.whl/spark_auto_mapper_fhir/fhir_types/decimal.py
@@ -0,0 +1,29 @@
+from typing import Union, Optional
+
+from pyspark.sql import DataFrame, Column
+from pyspark.sql.functions import round
+from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
+from spark_auto_mapper.data_types.text_like_base import AutoMapperTextLikeBase
+
+
+class FhirDecimal(AutoMapperTextLikeBase):
+    """
+    Cleans up the text for an id column
+    """
+    def __init__(
+        self, column: Union[AutoMapperDataTypeColumn, AutoMapperTextLikeBase]
+    ):
+        super().__init__()
+
+        self.column: Union[AutoMapperDataTypeColumn,
+                           AutoMapperTextLikeBase] = column
+
+    def get_column_spec(
+        self, source_df: Optional[DataFrame], current_column: Optional[Column]
+    ) -> Column:
+        column_spec = round(
+            self.column.get_column_spec(
+                source_df=source_df, current_column=current_column
+            ).cast("float"), 6
+        )
+        return column_spec
---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.2.1-py3-none-any.whl/spark_auto_mapper_fhir/fhir_types/id.py
@@ -0,0 +1,34 @@
+from typing import Union, Optional
+
+from pyspark.sql import DataFrame, Column
+from pyspark.sql.functions import regexp_replace, substring
+from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
+from spark_auto_mapper.data_types.text_like_base import AutoMapperTextLikeBase
+
+
+class FhirId(AutoMapperTextLikeBase):
+    """
+    Cleans up the text for an id column
+    """
+    def __init__(
+        self, column: Union[AutoMapperDataTypeColumn, AutoMapperTextLikeBase]
+    ):
+        super().__init__()
+
+        self.column: Union[AutoMapperDataTypeColumn,
+                           AutoMapperTextLikeBase] = column
+
+    def get_column_spec(
+        self, source_df: Optional[DataFrame], current_column: Optional[Column]
+    ) -> Column:
+        # https://hl7.org/FHIR/datatypes.html#id
+        column_spec = substring(
+            regexp_replace(
+                str=self.column.get_column_spec(
+                    source_df=source_df, current_column=current_column
+                ),
+                pattern=r'[^A-Za-z0-9\-\.]',
+                replacement="-"
+            ), 0, 63
+        )
+        return column_spec
