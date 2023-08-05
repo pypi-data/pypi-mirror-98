3533ee414083fbd223ac438e98fa0e63d38262b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:59 +0000

    Add package hidrocomp/1.2.1/hidrocomp-1.2.1.tar.gz

---
+++ packages/hidrocomp/hidrocomp-1.2.1.tar.gz/hidrocomp-1.2.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+rootpath = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    return open(os.path.join(rootpath, *parts), 'r').read()
+
+
+long_description = '{}'.format(read('README.md'))
+
+with open('requirements.txt') as f:
+    require = f.readlines()
+install_requires = [r.strip() for r in require]
+
+setup(
+    name='hidrocomp',
+    version='1.2.1',
+    include_package_data=True,
+    long_description=long_description,
+    classifiers=['Development Status :: 1 - Planning',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Education',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Education',
+                 ],
+    url='https://github.com/clebsonpy/HidroComp',
+    license='MIT License',
+    author='Clebson Farias',
+    author_email='clebson2007.farias@gmail.com',
+    keywords='hydrology statistic eflow flow',
+    description='Developed for hydrological studies',
+    install_requires=install_requires,
+    packages=['hidrocomp'],
+)
