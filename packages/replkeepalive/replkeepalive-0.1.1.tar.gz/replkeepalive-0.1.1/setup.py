ee22938a72f835312326dec4c592b91f2b72a0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:47 +0000

    Add package replkeepalive/0.1.1/replkeepalive-0.1.1.tar.gz

---
+++ packages/replkeepalive/replkeepalive-0.1.1.tar.gz/replkeepalive-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='replkeepalive',
+    version='0.1.1',
+    description='Keep repls alive.',
+    long_description='Keep a repl.it (Soon to be replit.com) repl online 24/7.\nUseful for discord.py bots.',
+    packages=['replkeepalive']
+    )
