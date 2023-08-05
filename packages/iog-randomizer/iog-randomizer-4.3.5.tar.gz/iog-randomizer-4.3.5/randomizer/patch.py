b3694ffaa660205a131e1439ac21283a10de378f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:23 +0000

    Add package iog-randomizer/4.3.5/iog-randomizer-4.3.5.tar.gz

---
+++ packages/iog-randomizer/iog-randomizer-4.3.5.tar.gz/iog-randomizer-4.3.5/randomizer/patch.py
@@ -0,0 +1,32 @@
+import tempfile
+
+
+class Patch:
+    step = 0
+
+    def __init__(self, rom_data, logger):
+        self.patch_data = []
+        self.logger = logger
+        self.temp = tempfile.TemporaryFile()
+        self.temp.write(rom_data)
+
+    def __del__(self):
+        self.temp.close()
+
+    def seek(self, position: int) -> None:
+        self.temp.seek(position)
+
+    def write(self, data: bytes) -> None:
+        arr = [x for x in data]
+        address = int(self.temp.tell())
+
+        self.patch_data.append({'index': self.step, 'address': address, 'data': arr})
+        self.temp.write(data)
+        self.step += 1
+
+    def read(self, n: int = None):
+        return self.temp.read(n)
+
+    def find(self, sub, start=None, end=None):
+        d = self.read()
+        return d.find(sub, start, end)
