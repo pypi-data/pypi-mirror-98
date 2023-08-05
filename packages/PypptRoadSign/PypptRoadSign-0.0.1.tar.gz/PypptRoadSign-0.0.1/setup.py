362bb313dc74c5eacf023a7c778aebc20c402f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:18 +0000

    Add package PypptRoadSign/0.0.1/PypptRoadSign-0.0.1.tar.gz

---
+++ packages/PypptRoadSign/PypptRoadSign-0.0.1.tar.gz/PypptRoadSign-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='PypptRoadSign',
+    packages=['PypptRoadSign'],
+    version='0.0.1',
+    license='MIT',
+    description=' QXWZ Power Point Road Sign Project',
+    author='Haosong Liu',
+    author_email='haosongliuofficial@gmail.com',
+    url='https://github.com/liulhs/PypptRoadSign_Git',
+    download_url='https://github.com/liulhs/PypptRoadSign_Git/archive/v_0.0.1.tar.gz',
+    keywords=['Roadsign', 'PowerPoint', 'Genertor'],
+    install_requires=[
+        'python-pptx'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
