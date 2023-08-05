6be41ad8b820b7df0f475b3672039118bb294eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:46 +0000

    Add package pythondata-misc-opentitan/0.0.post5354/pythondata_misc_opentitan-0.0.post5354-py2-none-any.whl

---
+++ packages/pythondata-misc-opentitan/pythondata_misc_opentitan-0.0.post5354-py2-none-any.whl/pythondata_misc_opentitan/resources/hw/ip/edn/dv/env/seq_lib/edn_smoke_vseq.sv
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class edn_smoke_vseq extends edn_base_vseq;
+  `uvm_object_utils(edn_smoke_vseq)
+
+  `uvm_object_new
+
+  push_pull_host_seq#(edn_pkg::FIPS_ENDPOINT_BUS_WIDTH)   m_endpoint_pull_seq;
+
+  task body();
+    super.body();
+
+    // Enable edn
+    csr_wr(.csr(ral.ctrl), .value(1'b1));
+
+    m_endpoint_pull_seq = push_pull_host_seq#(edn_pkg::FIPS_ENDPOINT_BUS_WIDTH)::type_id::
+        create("m_endpoint_pull_seq");
+    m_endpoint_pull_seq.start(p_sequencer.endpoint_sequencer_h[edn_env_pkg::NUM_ENDPOINTS-1]);
+
+    // TODO: Compare to genbits from csrng_device_seq
+    assert (cfg.m_endpoint_agent_cfg[edn_env_pkg::NUM_ENDPOINTS-1].vif.d_data == {1'b0, 32'hdeadbeef}) else
+      `uvm_error(`gfn, $sformatf("endpoint_data (%0h) != fips_genbits_data (0x0_deadbeef)", cfg.m_endpoint_agent_cfg[edn_env_pkg::NUM_ENDPOINTS-1].vif.d_data))
+  endtask
+
+endclass
