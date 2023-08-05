c9fe3bc8c4319c6c7d65f89a79384b4036e492cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:48 +0000

    Add package probfit/1.2.0/probfit-1.2.0-cp39-cp39-win32.whl

---
+++ packages/probfit/probfit-1.2.0-cp39-cp39-win32.whl/probfit/probfit_warnings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+class ProbfitWarning(RuntimeWarning):
+    pass
+
+
+class SmallIntegralWarning(ProbfitWarning):
+    pass
+
+
+class SmallDivisionWarning(ProbfitWarning):
+    pass
+
+
+class SharedExtendeeExtenderParameter(ProbfitWarning):
+    pass
+
+
+class LogWarning(ProbfitWarning):
+    pass
