5c0e177c7c8b2cf32a8cce4f6104ae206b76e3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:14 +0000

    Add package wildgram/0.2.2/wildgram-0.2.2.tar.gz

---
+++ packages/wildgram/wildgram-0.2.2.tar.gz/wildgram-0.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wildgram",
+    version="0.2.2",
+    author="Grace Turner",
+    author_email="gracekatherineturner@gmail.com",
+    description="wildgram tokenizes and seperates tokens into ngrams of varying size based on the natural language breaks in the text.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/gracekatherineturner/wildgram",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
