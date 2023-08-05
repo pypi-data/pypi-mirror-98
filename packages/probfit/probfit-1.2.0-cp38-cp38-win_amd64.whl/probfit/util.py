77e028726cbf7d00ec61323bcfbb8a486074d552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package probfit/1.2.0/probfit-1.2.0-cp38-cp38-win_amd64.whl

---
+++ packages/probfit/probfit-1.2.0-cp38-cp38-win_amd64.whl/probfit/util.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from iminuit.util import describe
+
+
+def parse_arg(f, kwd, offset=0):
+    """
+    convert dictionary of keyword argument and value to positional argument
+    equivalent to::
+
+        vnames = describe(f)
+        return tuple([kwd[k] for k in vnames[offset:]])
+
+    """
+    vnames = describe(f)
+    return tuple([kwd[k] for k in vnames[offset:]])
+
+
+def remove_prefix(s, prefix):
+    if prefix is None:
+        return s
+    if s.startswith(prefix + "_"):
+        l = len(prefix) + 1
+        return s[l:]
+    elif s.startswith(prefix):
+        l = len(prefix)
+        return s[l:]
+    else:
+        return s
