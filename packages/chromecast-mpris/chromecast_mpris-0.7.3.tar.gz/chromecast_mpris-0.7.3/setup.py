a40b44c18ae89b4a3b27cbd2bb73d6fa147869be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:29 +0000

    Add package chromecast-mpris/0.7.3/chromecast_mpris-0.7.3.tar.gz

---
+++ packages/chromecast-mpris/chromecast_mpris-0.7.3.tar.gz/chromecast_mpris-0.7.3/setup.py
@@ -0,0 +1,41 @@
+__author__ = "Alex DeLorenzo"
+from setuptools import setup
+from pathlib import Path
+
+
+NAME = "chromecast_mpris"
+VERSION = "0.7.3"
+LICENSE = "AGPL-3.0"
+
+# potential new name
+NEW_NAME = "cast_control"
+
+ENTRY_POINTS = {
+  "console_scripts": [
+    f"{NAME} = {NAME}.command:cmd",
+    f"{NEW_NAME} = {NAME}.command:cmd"
+  ]
+}
+
+REQS = Path('requirements.txt') \
+  .read_text() \
+  .split('\n')
+
+README = Path('README.md').read_text()
+
+
+setup(
+  name=NAME,
+  version=VERSION,
+  description="📺 Control Chromecasts from Linux and D-Bus",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  url="https://github.com/alexdelorenzo/chromecast_mpris",
+  author=__author__,
+  license=LICENSE,
+  packages=[NAME],
+  zip_safe=True,
+  install_requires=REQS,
+  entry_points=ENTRY_POINTS,
+  python_requires='>=3.6',
+)
