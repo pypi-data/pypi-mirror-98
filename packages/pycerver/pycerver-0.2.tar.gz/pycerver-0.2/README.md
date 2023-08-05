288c5ef71dee0f1ba3af2899f1b147bac4abecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:22 +0000

    Add package pycerver/0.2/pycerver-0.2.tar.gz

---
+++ packages/pycerver/pycerver-0.2.tar.gz/pycerver-0.2/README.md
@@ -0,0 +1,11 @@
+# PyCerver
+
+PyCerver is a pure Python wrapper around the cerver framework. It uses the built-in ctypes module to interface with cerver, and provides simple Python classes and wrappers for common cerver functionality.
+
+## Trying out
+
+You can test out PyCerver without actually installing it. You just need to set up your PYTHONPATH to point to the location of the source distribution package.
+
+```
+export PYTHONPATH=/path/to/py-cerver:$PYTHONPATH
+```
---
+++ packages/pycerver/pycerver-0.2.tar.gz/pycerver-0.2/cerver/http.py
@@ -0,0 +1,37 @@
+from ctypes import c_void_p, c_char_p
+
+from .lib import lib
+
+from .alg import jwt_alg_t
+
+# main
+http_cerver_get = lib.http_cerver_get
+http_cerver_get.argtypes = [c_void_p]
+http_cerver_get.restype = c_void_p
+
+# stats
+http_cerver_all_stats_print = lib.http_cerver_all_stats_print
+http_cerver_all_stats_print.argtypes = [c_void_p]
+
+# public
+http_cerver_static_path_add = lib.http_cerver_static_path_add
+http_cerver_static_path_add.argtypes = [c_void_p, c_char_p]
+http_cerver_static_path_add.restype = c_void_p
+
+# routes
+http_cerver_route_register = lib.http_cerver_route_register
+http_cerver_route_register.argtypes = [c_void_p, c_void_p]
+
+# auth
+# sets the jwt algorithm used for encoding & decoding jwt tokens
+# the default value is JWT_ALG_HS256
+http_cerver_route_register = lib.http_cerver_route_register
+http_cerver_route_register.argtypes = [c_void_p, jwt_alg_t]
+
+# sets the filename from where the jwt private key will be loaded
+http_cerver_auth_set_jwt_priv_key_filename = lib.http_cerver_auth_set_jwt_priv_key_filename
+http_cerver_auth_set_jwt_priv_key_filename.argtypes = [c_void_p, c_char_p]
+
+# sets the filename from where the jwt public key will be loaded
+http_cerver_auth_set_jwt_pub_key_filename = lib.http_cerver_auth_set_jwt_pub_key_filename
+http_cerver_auth_set_jwt_pub_key_filename.argtypes = [c_void_p, c_char_p]
