b99a223fcd10cd784bb983fc8ab65e9f0a728f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:14 +0000

    Add package ocrd-utils/2.23.2/ocrd_utils-2.23.2.tar.gz

---
+++ packages/ocrd-utils/ocrd_utils-2.23.2.tar.gz/ocrd_utils-2.23.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+install_requires = open('requirements.txt').read().split('\n')
+
+setup(
+    name='ocrd_utils',
+    version='2.23.2',
+    description='OCR-D framework - shared code, helpers, constants',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Konstantin Baierer',
+    author_email='unixprog@gmail.com',
+    url='https://github.com/OCR-D/core',
+    license='Apache License 2.0',
+    packages=['ocrd_utils'],
+    install_requires=install_requires,
+    package_data={'': ['*.json', '*.yml', '*.xml']},
+    keywords=['OCR', 'OCR-D']
+)
