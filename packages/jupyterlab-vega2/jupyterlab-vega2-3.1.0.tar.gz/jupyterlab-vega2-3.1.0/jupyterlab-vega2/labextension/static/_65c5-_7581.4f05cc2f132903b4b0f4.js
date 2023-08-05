fbe07a5272068f8bd07f04ce053527a90931398a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:00 +0000

    Add package jupyterlab-vega2/3.1.0/jupyterlab-vega2-3.1.0.tar.gz

---
+++ packages/jupyterlab-vega2/jupyterlab-vega2-3.1.0.tar.gz/jupyterlab-vega2-3.1.0/jupyterlab-vega2/labextension/static/_65c5-_7581.4f05cc2f132903b4b0f4.js
@@ -0,0 +1,24 @@
+(self["webpackChunk_jupyterlab_vega2_extension"] = self["webpackChunk_jupyterlab_vega2_extension"] || []).push([["_65c5-_7581"],{
+
+/***/ "?65c5":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?7581":
+/*!****************************!*\
+  !*** node-fetch (ignored) ***!
+  \****************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=_65c5-_7581.4f05cc2f132903b4b0f4.js.map
