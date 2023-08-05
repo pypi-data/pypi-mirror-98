721a6a548ca69f68cc6d0d288365fe0e152da4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:28 +0000

    Add package MeUtils/2021.3.10.19.51.55/MeUtils-2021.3.10.19.51.55-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2021.3.10.19.51.55-py3-none-any.whl/meutils/decorators.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : DeepNN.
+# @File         : decorator_utils
+# @Time         : 2020/4/30 10:46 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+"""
+@wrapt.decorator
+def noargs(wrapped, instance, args, kwargs):
+    logger.info(f'noargs decorator')
+
+    return wrapped(*args, **kwargs)
+
+
+def withargs(myarg1, myarg2):
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        logger.info(f'withargs decorator: {myarg1}, {myarg2}')
+        return wrapped(*args, **kwargs)
+
+    return wrapper
+"""
+import wrapt
+
+
+class Singleton:
+    def __init__(self, cls):
+        self.cls = cls
+        self.instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self.instance is None:
+            self.instance = self.cls(*args, **kwargs)
+        return self.instance
+
+
+@wrapt.decorator
+def opener(wrapped, instance, args, kwargs):
+    """
+    opener(yaml.load)("conf.yaml")
+    opener(json.load)("conf.json")
+
+    """
+    path = args[0]
+    with open(path) as f:
+        return wrapped(f)
+
+
+if __name__ == '__main__':
+    class A:
+
+        def __init__(self, ):
+            print('A实例化')
+
+
+    @Singleton
+    class B:
+        def __init__(self, ):
+            print('B实例化')
+
+
+    for _ in range(3):
+        print('\n', _)
+        A()
+        B()
