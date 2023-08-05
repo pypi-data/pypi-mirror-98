8fdd5246c950b1d40656efb613defcf164973471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:06 +0000

    Add package agave/0.1.3.dev5/agave-0.1.3.dev5.tar.gz

---
+++ packages/agave/agave-0.1.3.dev5.tar.gz/agave-0.1.3.dev5/agave/models/helpers.py
@@ -0,0 +1,15 @@
+import uuid
+from base64 import urlsafe_b64encode
+
+
+def uuid_field(prefix: str = ''):
+    def base64_uuid_func() -> str:
+        return prefix + urlsafe_b64encode(uuid.uuid4().bytes).decode()[:-2]
+
+    return base64_uuid_func
+
+
+def uuid_field_account(account_number: str, bank_code: str, user_id: str):
+    text = f"{bank_code}{account_number}{user_id}"
+    uuid5 = uuid.uuid5(uuid.NAMESPACE_DNS, text)
+    return 'AC' + urlsafe_b64encode(uuid5.bytes).decode()[:-2]
