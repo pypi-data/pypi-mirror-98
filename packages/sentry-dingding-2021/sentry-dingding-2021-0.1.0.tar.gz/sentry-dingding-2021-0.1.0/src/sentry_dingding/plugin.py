0abc2ba5e894914c32411b5b46ac2724129c01cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:06 +0000

    Add package sentry-dingding-2021/0.1.0/sentry-dingding-2021-0.1.0.tar.gz

---
+++ packages/sentry-dingding-2021/sentry-dingding-2021-0.1.0.tar.gz/sentry-dingding-2021-0.1.0/src/sentry_dingding/plugin.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+import json
+
+import requests
+from sentry.plugins.bases.notify import NotificationPlugin
+
+import sentry_dingding
+from .forms import DingDingOptionsForm
+
+DingTalk_API = "https://oapi.dingtalk.com/robot/send?access_token={token}"
+
+
+class DingDingPlugin(NotificationPlugin):
+    """
+    Sentry plugin to send error counts to DingDing.
+    """
+    author = 'hsw10151'
+    version = sentry_dingding.VERSION
+    description = 'upgraded DingDing integrations for sentry. '
+    slug = 'DingDing'
+    title = 'DingDing'
+    conf_key = slug
+    conf_title = title
+    project_conf_form = DingDingOptionsForm
+
+    def is_configured(self, project):
+        """
+        Check if plugin is configured.
+        """
+        return bool(self.get_option('access_token', project))
+
+    def notify_users(self, group, event, *args, **kwargs):
+        self.post_process(group, event, *args, **kwargs)
+
+    def post_process(self, group, event, *args, **kwargs):
+        """
+        Process error.
+        """
+        if not self.is_configured(group.project):
+            return
+
+        if group.is_ignored():
+            return
+
+        access_token = self.get_option('access_token', group.project)
+        send_url = DingTalk_API.format(token=access_token)
+        title = u">>>>>发现异常: \n [$Sentry Alert from {}]".format(event.project.slug)
+
+        data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "title": title,
+                "text": u"#### {title} \n > {message} [href]({url})  \n  Environment: {env} \n Project: {proj} \n Id: {id} \n RequestFrom:{requestFrom}".format(
+                    title=title,
+                    message=event.message,
+                    url=u"{}events/{}/".format(group.get_absolute_url(), event.event_id),
+                    env=event.environment,
+                    proj=event.tags.project,
+                    id=event.event_id,
+                    requestFrom=event.request.url
+                )
+            }
+        }
+        requests.post(
+            url=send_url,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(data).encode("utf-8")
+        )
