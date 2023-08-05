111e9caad79bde648792946132cd1202416db7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:55 +0000

    Add package w3bsite/4.31.6/w3bsite-4.31.6.tar.gz

---
+++ packages/w3bsite/w3bsite-4.31.6.tar.gz/w3bsite-4.31.6/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='w3bsite',
+	version='4.31.6',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/w3bsite',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=[
+            'wheel',
+            'asgiref',
+            'certifi',
+            'chardet',
+            'idna',
+            'sqlparse',
+            'urllib3',
+            'gunicorn',
+            'whitenoise',
+            'psutil',
+            'uwsgi',
+            'xmltodict',
+            'stripe',
+            'encrypti0n',
+            'syst3m',
+            'dev0s',
+            'netw0rk',
+            'ssht00ls',
+        ])
