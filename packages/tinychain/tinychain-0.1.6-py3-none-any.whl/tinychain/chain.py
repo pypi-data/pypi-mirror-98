0d580ae2e7657f24649c7c30fc8092b9e8d9be1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:00 +0000

    Add package tinychain/0.1.6/tinychain-0.1.6-py3-none-any.whl

---
+++ packages/tinychain/tinychain-0.1.6-py3-none-any.whl/tinychain/chain.py
@@ -0,0 +1,32 @@
+"""Data structures responsible for keeping track of mutations to a :class:`Value`"""
+
+from .ref import OpRef
+from .reflect import Meta
+from .state import Scalar, State
+from .util import *
+
+
+class Chain(State, metaclass=Meta):
+    """
+    Data structure responsible for keeping track of mutations to a :class:`Value`.
+    """
+
+    __uri__ = uri(State) + "/chain"
+
+    def set(self, value):
+        """Update the value of this `Chain`."""
+
+        return OpRef.Put(uri(self), None, value)
+
+
+class SyncChain(Chain):
+    """
+    A :class:`Chain` which keeps track of only the current transaction's operations,
+    in order to recover from a transaction failure (e.g. if the host crashes).
+    """
+
+    __uri__ = uri(Chain) + "/sync"
+
+
+Chain.Sync = SyncChain
+
