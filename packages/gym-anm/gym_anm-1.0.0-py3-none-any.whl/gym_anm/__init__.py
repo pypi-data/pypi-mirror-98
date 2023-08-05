98be5d242cd5dc854ad8b4bc912eeed7e7a8de47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:45 +0000

    Add package gym-anm/1.0.0/gym_anm-1.0.0-py3-none-any.whl

---
+++ packages/gym-anm/gym_anm-1.0.0-py3-none-any.whl/gym_anm/__init__.py
@@ -0,0 +1,11 @@
+"""A package for designing RL ANM tasks in power grids."""
+
+from gym.envs.registration import register
+
+from .agents import MPCAgent, MPCAgentANM6Easy
+from .envs import ANMEnv
+
+register(
+    id='ANM6Easy-v0',
+    entry_point='gym_anm.envs:ANM6Easy',
+)
