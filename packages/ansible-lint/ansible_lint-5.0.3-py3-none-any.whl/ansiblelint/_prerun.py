d9e7e56937f057aa6f2b8b403a73ae43ab93786a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:13 +0000

    Add package ansible-lint/5.0.3/ansible_lint-5.0.3-py3-none-any.whl

---
+++ packages/ansible-lint/ansible_lint-5.0.3-py3-none-any.whl/ansiblelint/_prerun.py
@@ -0,0 +1,294 @@
+import os
+import pathlib
+import re
+import subprocess
+import sys
+from functools import lru_cache
+from typing import List, Optional, Tuple
+
+from packaging import version
+
+from ansiblelint.config import (
+    ansible_collections_path,
+    collection_list,
+    options,
+    parse_ansible_version,
+)
+from ansiblelint.constants import (
+    ANSIBLE_DEFAULT_ROLES_PATH,
+    ANSIBLE_MIN_VERSION,
+    ANSIBLE_MISSING_RC,
+    ANSIBLE_MOCKED_MODULE,
+    INVALID_CONFIG_RC,
+    INVALID_PREREQUISITES_RC,
+)
+from ansiblelint.loaders import yaml_from_file
+
+
+def check_ansible_presence(exit_on_error: bool = False) -> Tuple[str, str]:
+    """Assures we stop execution if Ansible is missing or outdated.
+
+    Returne found version and an optional exception if something wrong
+    was detected.
+    """
+
+    @lru_cache()
+    def _get_ver_err() -> Tuple[str, str]:
+
+        err = ""
+        failed = False
+        ver = ""
+        result = subprocess.run(
+            args=["ansible", "--version"],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+            check=False,
+        )
+        if result.returncode != 0:
+            return (
+                ver,
+                "FATAL: Unable to retrieve ansible cli version: %s" % result.stdout,
+            )
+
+        ver, error = parse_ansible_version(result.stdout)
+        if error is not None:
+            return "", error
+        try:
+            # pylint: disable=import-outside-toplevel
+            from ansible.release import __version__ as ansible_module_version
+
+            if version.parse(ansible_module_version) < version.parse(
+                ANSIBLE_MIN_VERSION
+            ):
+                failed = True
+        except (ImportError, ModuleNotFoundError) as e:
+            failed = True
+            ansible_module_version = "none"
+            err += f"{e}\n"
+        if failed:
+            err += (
+                "FATAL: ansible-lint requires a version of Ansible package"
+                " >= %s, but %s was found. "
+                "Please install a compatible version using the same python interpreter. See "
+                "https://docs.ansible.com/ansible/latest/installation_guide"
+                "/intro_installation.html#installing-ansible-with-pip"
+                % (ANSIBLE_MIN_VERSION, ansible_module_version)
+            )
+
+        elif ver != ansible_module_version:
+            err = (
+                f"FATAL: Ansible CLI ({ver}) and python module"
+                f" ({ansible_module_version}) versions do not match. This "
+                "indicates a broken execution environment."
+            )
+        return ver, err
+
+    ver, err = _get_ver_err()
+    if exit_on_error and err:
+        print(err, file=sys.stderr)
+        sys.exit(ANSIBLE_MISSING_RC)
+    return ver, err
+
+
+def prepare_environment() -> None:
+    """Make dependencies available if needed."""
+    if not options.offline and os.path.exists("requirements.yml"):
+
+        cmd = [
+            "ansible-galaxy",
+            "role",
+            "install",
+            "--roles-path",
+            f"{options.project_dir}/.cache/roles",
+            "-vr",
+            "requirements.yml",
+        ]
+
+        print("Running %s" % " ".join(cmd), file=sys.stderr)
+        run = subprocess.run(
+            cmd,
+            universal_newlines=True,
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        if run.returncode != 0:
+            print(run.stdout, file=sys.stderr)
+            sys.exit(run.returncode)
+
+        # Run galaxy collection install works on v2 requirements.yml
+        if "collections" in yaml_from_file("requirements.yml"):
+
+            cmd = [
+                "ansible-galaxy",
+                "collection",
+                "install",
+                "-p",
+                f"{options.project_dir}/.cache/collections",
+                "-vr",
+                "requirements.yml",
+            ]
+
+            print("Running %s" % " ".join(cmd), file=sys.stderr)
+            run = subprocess.run(
+                cmd,
+                universal_newlines=True,
+                check=False,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            if run.returncode != 0:
+                print(run.stdout, file=sys.stderr)
+                sys.exit(run.returncode)
+
+    _install_galaxy_role()
+    _perform_mockings()
+    _prepare_ansible_paths()
+
+
+def _install_galaxy_role() -> None:
+    """Detect standalone galaxy role and installs it."""
+    if not os.path.exists("meta/main.yml"):
+        return
+    yaml = yaml_from_file("meta/main.yml")
+    if 'galaxy_info' not in yaml:
+        return
+    role_name = yaml['galaxy_info'].get('role_name', None)
+    role_namespace = yaml['galaxy_info'].get('namespace', None)
+    if not role_namespace:
+        role_namespace = yaml['galaxy_info'].get('author', None)
+    if not role_name:
+        role_name = pathlib.Path(".").absolute().name
+        role_name = re.sub(r'^{0}'.format(re.escape('ansible-role-')), '', role_name)
+    fqrn = f"{role_namespace}.{role_name}"
+    if not re.match(r"[a-z0-9][a-z0-9_]+\.[a-z][a-z0-9_]+$", fqrn):
+        print(
+            f"""\
+Computed fully qualified role name of {fqrn} is not valid.
+Please edit meta/main.yml and assure we can correctly determine full role name:
+
+galaxy_info:
+  role_name: my_name  # if absent directory name hosting role is used instead
+  namespace: my_galaxy_namespace  # if absent, author is used instead
+
+Namespace: https://galaxy.ansible.com/docs/contributing/namespaces.html#galaxy-namespace-limitations
+Role: https://galaxy.ansible.com/docs/contributing/creating_role.html#role-names
+""",
+            file=sys.stderr,
+        )
+        sys.exit(INVALID_PREREQUISITES_RC)
+    p = pathlib.Path(f"{options.project_dir}/.cache/roles")
+    p.mkdir(parents=True, exist_ok=True)
+    link_path = p / f"{role_namespace}.{role_name}"
+    # despite documentation stating that is_file() reports true for symlinks,
+    # it appears that is_dir() reports true instead, so we rely on exits().
+    if not link_path.exists():
+        link_path.symlink_to(pathlib.Path("../..", target_is_directory=True))
+    print(
+        f"Using {link_path} symlink to current repository in order to enable Ansible to find the role using its expected full name.",
+        file=sys.stderr,
+    )
+
+
+def _prepare_ansible_paths() -> None:
+    """Configure Ansible environment variables."""
+    library_paths: List[str] = []
+    roles_path: List[str] = []
+
+    for path_list, path in (
+        (library_paths, "plugins/modules"),
+        (library_paths, f"{options.project_dir}/.cache/modules"),
+        (collection_list, f"{options.project_dir}/.cache/collections"),
+        (roles_path, "roles"),
+        (roles_path, f"{options.project_dir}/.cache/roles"),
+    ):
+        if path not in path_list and os.path.exists(path):
+            path_list.append(path)
+
+    _update_env('ANSIBLE_LIBRARY', library_paths)
+    _update_env(ansible_collections_path(), collection_list)
+    _update_env('ANSIBLE_ROLES_PATH', roles_path, default=ANSIBLE_DEFAULT_ROLES_PATH)
+
+
+def _make_module_stub(module_name: str) -> None:
+    # a.b.c is treated a collection
+    if re.match(r"\w+\.\w+\.\w+", module_name):
+        namespace, collection, module_file = module_name.split(".")
+        path = f"{ options.project_dir }/.cache/collections/ansible_collections/{ namespace }/{ collection }/plugins/modules"
+        os.makedirs(path, exist_ok=True)
+        _write_module_stub(
+            filename=f"{path}/{module_file}.py",
+            name=module_file,
+            namespace=namespace,
+            collection=collection,
+        )
+    elif "." in module_name:
+        print(
+            "Config error: %s is not a valid module name." % module_name,
+            file=sys.stderr,
+        )
+        sys.exit(INVALID_CONFIG_RC)
+    else:
+        os.makedirs(f"{options.project_dir}/.cache/modules", exist_ok=True)
+        _write_module_stub(
+            filename=f"{options.project_dir}/.cache/modules/{module_name}.py",
+            name=module_name,
+        )
+
+
+def _write_module_stub(
+    filename: str,
+    name: str,
+    namespace: Optional[str] = None,
+    collection: Optional[str] = None,
+) -> None:
+    """Write module stub to disk."""
+    body = ANSIBLE_MOCKED_MODULE.format(
+        name=name, collection=collection, namespace=namespace
+    )
+    with open(filename, "w") as f:
+        f.write(body)
+
+
+def _update_env(varname: str, value: List[str], default: str = "") -> None:
+    """Update colon based environment variable if needed. by appending."""
+    if value:
+        value = [*os.environ.get(varname, default=default).split(':'), *value]
+        value_str = ":".join(value)
+        if value_str != os.environ.get(varname, ""):
+            os.environ[varname] = value_str
+            print("Added %s=%s" % (varname, value_str), file=sys.stderr)
+
+
+def _perform_mockings() -> None:
+    """Mock modules and roles."""
+    for role_name in options.mock_roles:
+        if re.match(r"\w+\.\w+\.\w+", role_name):
+            namespace, collection, role_dir = role_name.split(".")
+            path = f".cache/collections/ansible_collections/{ namespace }/{ collection }/roles/{ role_dir }/"
+        else:
+            path = f".cache/roles/{role_name}"
+        os.makedirs(path, exist_ok=True)
+
+    if options.mock_modules:
+        for module_name in options.mock_modules:
+            _make_module_stub(module_name)
+
+    # if inside a collection repo, symlink it to simulate its installed state
+    if not os.path.exists("galaxy.yml"):
+        return
+    yaml = yaml_from_file("galaxy.yml")
+    if not yaml:
+        # ignore empty galaxy.yml file
+        return
+    namespace = yaml.get('namespace', None)
+    collection = yaml.get('name', None)
+    if not namespace or not collection:
+        return
+    p = pathlib.Path(
+        f"{options.project_dir}/.cache/collections/ansible_collections/{ namespace }"
+    )
+    p.mkdir(parents=True, exist_ok=True)
+    link_path = p / collection
+    if not link_path.exists():
+        link_path.symlink_to(pathlib.Path("../../../..", target_is_directory=True))
---
+++ packages/ansible-lint/ansible_lint-5.0.3-py3-none-any.whl/ansiblelint/config.py
@@ -0,0 +1,128 @@
+"""Store configuration options as a singleton."""
+import os
+import re
+import subprocess
+import sys
+from argparse import Namespace
+from functools import lru_cache
+from typing import Dict, List, Optional, Tuple
+
+from packaging.version import Version
+
+from ansiblelint.constants import ANSIBLE_MISSING_RC
+
+DEFAULT_KINDS = [
+    # Do not sort this list, order matters.
+    {"requirements": "**/meta/requirements.yml"},  # v1 only
+    {"reno": "**/releasenotes/*/*.{yaml,yml}"},  # reno release notes
+    {"playbook": "**/playbooks/*.{yml,yaml}"},
+    {"playbook": "**/*playbook*.{yml,yaml}"},
+    {"role": "**/roles/*/"},
+    {"tasks": "**/tasks/**/*.{yaml,yml}"},
+    {"handlers": "**/handlers/*.{yaml,yml}"},
+    {"vars": "**/{host_vars,group_vars,vars,defaults}/**/*.{yaml,yml}"},
+    {"meta": "**/meta/main.{yaml,yml}"},
+    {"yaml": ".config/molecule/config.{yaml,yml}"},  # molecule global config
+    {
+        "requirements": "**/molecule/*/{collections,requirements}.{yaml,yml}"
+    },  # molecule old collection requirements (v1), ansible 2.8 only
+    {"yaml": "**/molecule/*/{base,molecule}.{yaml,yml}"},  # molecule config
+    {"requirements": "**/requirements.yml"},  # v2 and v1
+    {"playbook": "**/molecule/*/*.{yaml,yml}"},  # molecule playbooks
+    {"yaml": "**/*.{yaml,yml}"},
+    {"yaml": "**/.*.{yaml,yml}"},
+]
+
+options = Namespace(
+    colored=True,
+    cwd=".",
+    display_relative_path=True,
+    exclude_paths=[],
+    lintables=[],
+    listrules=False,
+    listtags=False,
+    parseable=False,
+    parseable_severity=False,
+    quiet=False,
+    rulesdirs=[],
+    skip_list=[],
+    tags=[],
+    verbosity=False,
+    warn_list=[],
+    kinds=DEFAULT_KINDS,
+    mock_modules=[],
+    mock_roles=[],
+    loop_var_prefix=None,
+    offline=False,
+    project_dir=None,
+    extra_vars=None,
+    enable_list=[],
+    skip_action_validation=True,
+)
+
+# Used to store detected tag deprecations
+used_old_tags: Dict[str, str] = {}
+
+# Used to store collection list paths (with mock paths if needed)
+collection_list: List[str] = []
+
+
+@lru_cache()
+def ansible_collections_path() -> str:
+    """Return collection path variable for current version of Ansible."""
+    # respect Ansible behavior, which is to load old name if present
+    for env_var in ["ANSIBLE_COLLECTIONS_PATHS", "ANSIBLE_COLLECTIONS_PATH"]:
+        if env_var in os.environ:
+            return env_var
+
+    # https://github.com/ansible/ansible/pull/70007
+    if ansible_version() >= ansible_version("2.10.0.dev0"):
+        return "ANSIBLE_COLLECTIONS_PATH"
+    return "ANSIBLE_COLLECTIONS_PATHS"
+
+
+def parse_ansible_version(stdout: str) -> Tuple[str, Optional[str]]:
+    """Parse output of 'ansible --version'."""
+    # ansible-core 2.11+: 'ansible [core 2.11.3]'
+    match = re.match(r"^ansible \[(?:core|base) ([^\]]+)\]", stdout)
+    if match:
+        return match.group(1), None
+    # ansible-base 2.10 and Ansible 2.9: 'ansible 2.x.y'
+    match = re.match(r"^ansible ([^\s]+)", stdout)
+    if match:
+        return match.group(1), None
+    return "", "FATAL: Unable parse ansible cli version: %s" % stdout
+
+
+@lru_cache()
+def ansible_version(version: str = "") -> Version:
+    """Return current Version object for Ansible.
+
+    If version is not mentioned, it returns current version as detected.
+    When version argument is mentioned, it return converts the version string
+    to Version object in order to make it usable in comparisons.
+    """
+    if not version:
+        proc = subprocess.run(
+            ["ansible", "--version"],
+            universal_newlines=True,
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if proc.returncode == 0:
+            version, error = parse_ansible_version(proc.stdout)
+            if error is not None:
+                print(error)
+                sys.exit(ANSIBLE_MISSING_RC)
+        else:
+            print(
+                "Unable to find a working copy of ansible executable.",
+                proc,
+            )
+            sys.exit(ANSIBLE_MISSING_RC)
+    return Version(version)
+
+
+if ansible_collections_path() in os.environ:
+    collection_list = os.environ[ansible_collections_path()].split(':')
