716e05a2fb444b65e1423dd622caa0cdba40242c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package netw0rk/1.10.8/netw0rk-1.10.8.tar.gz

---
+++ packages/netw0rk/netw0rk-1.10.8.tar.gz/netw0rk-1.10.8/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='netw0rk',
+	version='1.10.8',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/netw0rk',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+    install_requires=[
+            'dev0s',
+        ])
