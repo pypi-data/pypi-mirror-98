9a5a5e3f94a586b123d1aa2f8f6a60bf868a6ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package fastestimator-nightly/1.1.3.dev202103111236/fastestimator-nightly-1.1.3.dev202103111236.tar.gz

---
+++ packages/fastestimator-nightly/fastestimator-nightly-1.1.3.dev202103111236.tar.gz/fastestimator-nightly-1.1.3.dev202103111236/fastestimator/dataset/op_dataset.py
@@ -0,0 +1,71 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from copy import deepcopy
+from typing import Any, List, Mapping
+
+import numpy as np
+from torch.utils.data import Dataset
+
+from fastestimator.dataset import BatchDataset
+from fastestimator.op.numpyop.numpyop import NumpyOp, forward_numpyop
+from fastestimator.util.traceability_util import traceable
+from fastestimator.util.util import pad_batch
+
+
+@traceable()
+class OpDataset(Dataset):
+    """A wrapper for datasets which allows operators to be applied to them in a pipeline.
+
+    This class should not be directly instantiated by the end user. The fe.Pipeline will automatically wrap datasets
+    within an Op dataset as needed.
+
+    Args:
+        dataset: The base dataset to wrap.
+        ops: A list of ops to be applied after the base `dataset` `__getitem__` is invoked.
+        mode: What mode the system is currently running in ('train', 'eval', 'test', or 'infer').
+    """
+    def __init__(self, dataset: Dataset, ops: List[NumpyOp], mode: str) -> None:
+        self.dataset = dataset
+        if isinstance(self.dataset, BatchDataset):
+            self.dataset.reset_index_maps()
+        self.ops = ops
+        self.mode = mode
+
+    def __getitem__(self, index: int) -> Mapping[str, Any]:
+        """Fetch a data instance at a specified index, and apply transformations to it.
+
+        Args:
+            index: Which datapoint to retrieve.
+
+        Returns:
+            The data dictionary from the specified index, with transformations applied.
+        """
+        items = deepcopy(self.dataset[index])  # Deepcopy to prevent ops from overwriting values in datasets
+        if isinstance(self.dataset, BatchDataset):
+            # BatchDataset may randomly sample the same elements multiple times, so need to avoid reprocessing
+            unique_samples = set()
+            for item in items:
+                if id(item) not in unique_samples:
+                    forward_numpyop(self.ops, item, {'mode': self.mode})
+                    unique_samples.add(id(item))
+            if self.dataset.pad_value is not None:
+                pad_batch(items, self.dataset.pad_value)
+            items = {key: np.array([item[key] for item in items]) for key in items[0]}
+        else:
+            forward_numpyop(self.ops, items, {'mode': self.mode})
+        return items
+
+    def __len__(self):
+        return len(self.dataset)
---
+++ packages/fastestimator-nightly/fastestimator-nightly-1.1.3.dev202103111236.tar.gz/fastestimator-nightly-1.1.3.dev202103111236/fastestimator/trace/adapt/lr_scheduler.py
@@ -0,0 +1,74 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import inspect
+from typing import Callable, Union
+
+import numpy as np
+import tensorflow as tf
+import torch
+
+from fastestimator.backend.get_lr import get_lr
+from fastestimator.backend.set_lr import set_lr
+from fastestimator.summary.system import System
+from fastestimator.trace.trace import Trace
+from fastestimator.util.data import Data
+from fastestimator.util.traceability_util import traceable
+
+
+@traceable()
+class LRScheduler(Trace):
+    """Learning rate scheduler trace that changes the learning rate while training.
+
+    This class requires an input function which takes either 'epoch' or 'step' as input:
+    ```python
+    s = LRScheduler(model=model, lr_fn=lambda step: fe.schedule.cosine_decay(step, cycle_length=3750, init_lr=1e-3))
+    fe.Estimator(..., traces=[s])  # Learning rate will change based on step
+    s = LRScheduler(model=model, lr_fn=lambda epoch: fe.schedule.cosine_decay(epoch, cycle_length=3750, init_lr=1e-3))
+    fe.Estimator(..., traces=[s])  # Learning rate will change based on epoch
+    ```
+
+    Args:
+        model: A model instance compiled with fe.build.
+        lr_fn: A lr scheduling function that takes either 'epoch' or 'step' as input.
+
+    Raises:
+        AssertionError: If the `lr_fn` is not configured properly.
+    """
+    system: System
+
+    def __init__(self, model: Union[tf.keras.Model, torch.nn.Module], lr_fn: Callable[[int], float]) -> None:
+        self.model = model
+        self.lr_fn = lr_fn
+        assert hasattr(lr_fn, "__call__"), "lr_fn must be a function"
+        arg = list(inspect.signature(lr_fn).parameters.keys())
+        assert len(arg) == 1 and arg[0] in {"step", "epoch"}, "the lr_fn input arg must be either 'step' or 'epoch'"
+        self.schedule_mode = arg[0]
+        super().__init__(mode="train", outputs=self.model.model_name + "_lr")
+
+    def on_epoch_begin(self, data: Data) -> None:
+        if self.schedule_mode == "epoch":
+            new_lr = np.float32(self.lr_fn(self.system.epoch_idx))
+            set_lr(self.model, new_lr)
+
+    def on_batch_begin(self, data: Data) -> None:
+        if self.schedule_mode == "step":
+            new_lr = np.float32(self.lr_fn(self.system.global_step))
+            set_lr(self.model, new_lr)
+
+    def on_batch_end(self, data: Data) -> None:
+        if self.system.log_steps and (self.system.global_step % self.system.log_steps == 0
+                                      or self.system.global_step == 1):
+            current_lr = np.float32(get_lr(self.model))
+            data.write_with_log(self.outputs[0], current_lr)
