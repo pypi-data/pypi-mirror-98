d28b0bccc93374f21463de34d883d0df6c4a1f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:18 +0000

    Add package cg/20.5.4/cg-20.5.4.tar.gz

---
+++ packages/cg/cg-20.5.4.tar.gz/cg-20.5.4/cg/constants/gene_panel.py
@@ -0,0 +1,24 @@
+"""Gene panel specific constants"""
+
+MASTER_LIST = (
+    "BRAIN",
+    "Cardiology",
+    "CTD",
+    "ENDO",
+    "EP",
+    "IBMFS",
+    "IEM",
+    "IF",
+    "NEURODEG",
+    "NMD",
+    "mcarta",
+    "MHT",
+    "MIT",
+    "MOVE",
+    "mtDNA",
+    "PEDHEP",
+    "PID",
+    "PIDCAD",
+    "OMIM-AUTO",
+    "SKD",
+)
