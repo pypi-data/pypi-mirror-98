d10dc10cd3c9df3dfd7991996d6ebd4f4e8fc85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package cellfinder/0.4.11rc5/cellfinder-0.4.11rc5.tar.gz

---
+++ packages/cellfinder/cellfinder-0.4.11rc5.tar.gz/cellfinder-0.4.11rc5/setup.py
@@ -0,0 +1,84 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = [
+    "cellfinder-core",
+    "numpy",
+    "scikit-learn",
+    "configparser",
+    "pandas",
+    "packaging",
+    "scikit-image",
+    "tifffile",
+    "natsort",
+    "tqdm",
+    "multiprocessing-logging",
+    "psutil",
+    "configobj",
+    "tensorflow>=2.4.0",
+    "napari[pyside2]>=0.3.7",
+    "napari-cellfinder>=0.1.1",
+    "slurmio>=0.0.4",
+    "fancylog>=0.0.7",
+    "imlib>=0.0.26",
+    "brainreg",
+    "imio",
+]
+
+
+setup(
+    name="cellfinder",
+    version="0.4.11-rc5",
+    description="Automated 3D cell detection and registration of "
+    "whole-brain images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage>=5.0.3",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+        ]
+    },
+    setup_requires=["cython"],
+    python_requires=">=3.7",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "cellfinder = cellfinder.main:main",
+            "cellfinder_curate_new = cellfinder.train.curation:main",
+            "cellfinder_curate = cellfinder.train.curation_old:main",
+        ]
+    },
+    url="https://cellfinder.info",
+    project_urls={
+        "Source Code": "https://github.com/brainglobe/cellfinder",
+        "Bug Tracker": "https://github.com/brainglobe/cellfinder/issues",
+        "Documentation": "https://docs.brainglobe.info/cellfinder",
+    },
+    author="Adam Tyson, Christian Niedworok, Charly Rousseau",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
