ae2d9507f4c5f8d6c25cf59a83b1db37e1f86d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package vigorish/0.4.7/vigorish-0.4.7-py3-none-any.whl

---
+++ packages/vigorish/vigorish-0.4.7-py3-none-any.whl/vigorish/scrape/bbref_boxscores/models/umpire.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BBRefUmpire:
+    """Name and field location of one umpire for a single game."""
+
+    field_location: str = ""
+    umpire_name: str = ""
+
+    def as_dict(self):
+        """Convert umpire name and field location to a dictionary."""
+        dict = {
+            "field_location": f"{self.field_location}",
+            "umpire_name": f"{self.umpire_name}",
+        }
+        return dict
