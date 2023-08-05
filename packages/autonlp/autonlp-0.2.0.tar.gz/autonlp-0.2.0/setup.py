59812c522226d9d86d24b0b4bb405b277c5bbbda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package autonlp/0.2.0/autonlp-0.2.0.tar.gz

---
+++ packages/autonlp/autonlp-0.2.0.tar.gz/autonlp-0.2.0/setup.py
@@ -0,0 +1,66 @@
+# Lint as: python3
+""" HuggingFace/AutoNLP
+"""
+
+from setuptools import find_packages, setup
+
+
+DOCLINES = __doc__.split("\n")
+
+INSTALL_REQUIRES = [
+    "loguru==0.5.3",
+    "requests==2.25.1",
+    "tqdm==4.56.0",
+    "prettytable==2.0.0",
+    "huggingface_hub<0.1.0",
+]
+
+QUALITY_REQUIRE = [
+    "black",
+    "isort",
+    "flake8==3.7.9",
+]
+
+
+EXTRAS_REQUIRE = {
+    "dev": INSTALL_REQUIRES + QUALITY_REQUIRE,
+    "quality": INSTALL_REQUIRES + QUALITY_REQUIRE,
+    "docs": [
+        "recommonmark",
+        "sphinx==3.1.2",
+        "sphinx-markdown-tables",
+        "sphinx-rtd-theme==0.4.3",
+        "sphinx-copybutton",
+    ],
+}
+
+setup(
+    name="autonlp",
+    version="0.2.0",
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    author="HuggingFace Inc.",
+    author_email="abhishek@huggingface.co",
+    url="https://github.com/huggingface/autonlp",
+    download_url="https://github.com/huggingface/autonlp/tags",
+    license="Apache 2.0",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    scripts=["scripts/autonlp"],
+    extras_require=EXTRAS_REQUIRE,
+    install_requires=INSTALL_REQUIRES,
+    entry_points={"console_scripts": ["autonlp=autonlp.cli.autonlp:main"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="automl autonlp huggingface",
+)
---
+++ packages/autonlp/autonlp-0.2.0.tar.gz/autonlp-0.2.0/src/autonlp/cli/autonlp.py
@@ -0,0 +1,44 @@
+import argparse
+
+from .create_project import CreateProjectCommand
+from .estimator import EstimatorCommand
+from .list_projects import ListProjectsCommand
+from .login import LoginCommand
+from .metrics import MetricsCommand
+from .predict import PredictCommand
+from .project_info import ProjectInfoCommand
+from .train import TrainCommand
+from .upload import UploadCommand
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        "AutoNLP CLI",
+        usage="autonlp <command> [<args>]",
+        epilog="For more information about a command, run: `autonlp <command> --help`",
+    )
+    commands_parser = parser.add_subparsers(help="commands")
+
+    # Register commands
+    LoginCommand.register_subcommand(commands_parser)
+    CreateProjectCommand.register_subcommand(commands_parser)
+    ProjectInfoCommand.register_subcommand(commands_parser)
+    UploadCommand.register_subcommand(commands_parser)
+    TrainCommand.register_subcommand(commands_parser)
+    MetricsCommand.register_subcommand(commands_parser)
+    ListProjectsCommand.register_subcommand(commands_parser)
+    PredictCommand.register_subcommand(commands_parser)
+    EstimatorCommand.register_subcommand(commands_parser)
+
+    args = parser.parse_args()
+
+    if not hasattr(args, "func"):
+        parser.print_help()
+        exit(1)
+
+    command = args.func(args)
+    command.run()
+
+
+if __name__ == "__main__":
+    main()
