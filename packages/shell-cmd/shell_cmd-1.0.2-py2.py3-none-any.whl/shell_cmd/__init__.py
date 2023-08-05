a6d3e080d007bcca00061b2a5a9fceae2c8d5e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:46 +0000

    Add package shell-cmd/1.0.2/shell_cmd-1.0.2-py2.py3-none-any.whl

---
+++ packages/shell-cmd/shell_cmd-1.0.2-py2.py3-none-any.whl/shell_cmd/__init__.py
@@ -0,0 +1,31 @@
+from subprocess import check_output, CalledProcessError
+
+
+class RunShellException(Exception):
+    pass
+
+
+def __version__():
+    print('1.0.2')
+
+
+def sh(cmd, output_list=False):
+    '''
+    Returns a string or list with the result of executing given shell command
+
+    Parameters:
+    cmd (string): Shell command to be executed
+    output_list (boolean): If True then returns a list with the result, otherwise returns a string
+
+    Returns:
+    string or list with the output of the command. If `CalledProcessError` is catched,
+    then Custom `RunShellException` exception is raised
+    '''
+    output = ''
+    try:
+        output = check_output(cmd, shell=True).decode('utf-8')
+        if output_list:
+            output = output.split('\n')[:-1]
+    except CalledProcessError:
+        raise RunShellException
+    return output
