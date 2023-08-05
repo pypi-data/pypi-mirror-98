4189280bcf6b135c96e852e58221c28d069c87b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package bigquery-sql-parser/0.0.3/bigquery-sql-parser-0.0.3.tar.gz

---
+++ packages/bigquery-sql-parser/bigquery-sql-parser-0.0.3.tar.gz/bigquery-sql-parser-0.0.3/tests/test_line.py
@@ -0,0 +1,38 @@
+from bigquery_sql_parser.line import Line
+
+
+def test_identation():
+    string = "    column_name"
+
+    line = Line(string)
+    assert len(line.identation) == 4
+
+
+def test_no_identation():
+    string = "column_name"
+
+    line = Line(string)
+    assert len(line.identation) == 0
+
+
+def test_from_clause():
+    string = "  from table"
+
+    line = Line(string)
+    assert line.is_from_clause
+
+
+def test_select_clause():
+    string = "select * from table"
+
+    line = Line(string)
+    assert line.is_select_clause
+    assert not line.is_only_select_clause
+
+
+def test_add_comma():
+    string = "select name"
+
+    line = Line(string)
+    line.add_comma()
+    assert line.syntax == "select name,"
