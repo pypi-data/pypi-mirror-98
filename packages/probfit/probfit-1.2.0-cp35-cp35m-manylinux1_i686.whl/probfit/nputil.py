b0e082dac7fcf7f30fc08e026f7f2cd5f0d07a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package probfit/1.2.0/probfit-1.2.0-cp35-cp35m-manylinux1_i686.whl

---
+++ packages/probfit/probfit-1.2.0-cp35-cp35m-manylinux1_i686.whl/probfit/nputil.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from ._libstat import _vector_apply
+
+
+def mid(x):
+    return (x[:-1] + x[1:]) / 2
+
+
+def minmax(x):
+    return min(x), max(x)
+
+
+# for converting numpy array to double
+def float2double(a):
+    if a is None or a.dtype == np.float64:
+        return a
+    else:
+        return a.astype(np.float64)
+
+
+def vector_apply(f, x, *arg):
+    """
+    Apply **f** to array **x** with given arguments fast.
+
+    This is a fast version of::
+
+        np.array([f(xi,*arg) for xi in x ])
+
+    useful when you try to plot something.
+    """
+    return _vector_apply(f, x, arg)
