e5e81c3e77bf597303b215052c861b09469110b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:11 +0000

    Add package j-funcutils/0.0.6/j-funcutils-0.0.6.tar.gz

---
+++ packages/j-funcutils/j-funcutils-0.0.6.tar.gz/j-funcutils-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='j-funcutils',  
+     version='0.0.6',
+     author="Juan Manuel Camara Diaz",
+     author_email="juanma.caaz@gmail.com",
+     description="A basic cost time calculator",
+     url="https://github.com/juanmacaaz/FuncUtils",
+     packages=setuptools.find_packages(),
+     install_requires=[
+          'hwcounter',
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
