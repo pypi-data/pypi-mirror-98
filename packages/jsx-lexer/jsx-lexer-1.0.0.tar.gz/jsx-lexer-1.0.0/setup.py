2da690ea462a7ce567e2a0b67fb7c211215b0ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:03 +0000

    Add package jsx-lexer/1.0.0/jsx-lexer-1.0.0.tar.gz

---
+++ packages/jsx-lexer/jsx-lexer-1.0.0.tar.gz/jsx-lexer-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import io
+import os
+
+from setuptools import setup, find_packages
+
+
+VERSION = "1.0.0"
+here = os.path.abspath(os.path.dirname(__file__))
+README = io.open(os.path.join(here, "README.rst"), encoding="utf8").read()
+
+setup(
+    name="jsx-lexer",
+    description="A JSX lexer for Pygments",
+    long_description=README,
+    version=VERSION,
+    url="https://github.com/fcurella/jsx-lexer",
+    author="Flavio Curella",
+    author_email="flavio.curella@gmail.com",
+    classifiers=[
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="pygments highlight jsx react",
+    install_requires=["Pygments >= 2.7"],
+    test_suite="tests",
+    license="MIT License",
+    packages=find_packages(exclude=["docs", "tests", "tests.*"]),
+    entry_points="""
+        [pygments.lexers]
+        jsx=jsx:JsxLexer
+    """,
+)
