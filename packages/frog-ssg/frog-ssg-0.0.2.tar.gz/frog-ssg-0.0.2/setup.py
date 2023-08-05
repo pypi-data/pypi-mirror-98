6396ac4462f905649787356d7e27bb6be27806d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:45 +0000

    Add package frog-ssg/0.0.2/frog-ssg-0.0.2.tar.gz

---
+++ packages/frog-ssg/frog-ssg-0.0.2.tar.gz/frog-ssg-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frog-ssg",
+    version="0.0.2",
+    packages=find_packages(),
+    author="Leo Peltola",
+    author_email="leopeltola@gmail.com",
+    description="A Python static site generator",
+    classifiers= [
+        "Programming Language :: Python :: 3",
+    ],
+    
+    keywords=['python', 'static site generator', 'ssg'],
+    entry_points={
+        "console_scripts": [
+            "frog_build = frog.__main__:build"
+        ]
+    },
+)
