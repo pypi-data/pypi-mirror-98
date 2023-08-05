deab40c806e6f65b8316af3cb5a4cd364ad91b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:20 +0000

    Add package py-bpmn/0.0.4/py_bpmn-0.0.4-py3-none-any.whl

---
+++ packages/py-bpmn/py_bpmn-0.0.4-py3-none-any.whl/pybpmn/user_task.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import datetime
+
+from .task import Task
+
+logger = logging.getLogger(__name__)
+
+class UserTask(Task):
+
+    def execute(self,context,payload):
+        self._execute(context,payload)
+
+        name = self.name
+
+        task = self.task_context
+        task["status"] = "STARTED"
+
+        payload_task = self.payload_task
+
+        getattr(self.process_instance.handler,f"on_enter_task")(context = context,task = payload_task, payload = payload)
+        getattr(self.process_instance.handler,f"on_enter_{name}")(context = context,task = payload_task, payload = payload)
+        
+    def complete(self,input_task_context):
+        name = self.name
+        task = self.task_context
+        context = self.context
+        payload = self.payload
+        payload_task = self.payload_task
+
+        getattr(self.process_instance.handler,f"on_exit_{name}")(context = context,task = payload_task, payload = payload)
+        getattr(self.process_instance.handler,f"on_exit_task")(context = context,task = payload_task, payload = payload)
+        task["end_time"] = datetime.now()
+        task["status"] = "COMPLETED"
+        task.update(input_task_context)
+
+        self.process_instance.evaluate_results(self.get_outgoing_activities())
