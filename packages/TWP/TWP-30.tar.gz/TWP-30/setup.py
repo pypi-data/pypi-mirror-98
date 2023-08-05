504a7140fab5cb5414dadd32f4f9a5bcef0282a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:16 +0000

    Add package TWP/30/TWP-30.tar.gz

---
+++ packages/TWP/TWP-30.tar.gz/TWP-30/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='TWP',
+      version='30',
+      description='tweet with picture',
+      author='Kerota',
+      author_email='hifuna2727@gmail.com',
+      url='https://twitter.com',
+      install_requires=["json","glob","os","sys"],
+
+     
+     )
