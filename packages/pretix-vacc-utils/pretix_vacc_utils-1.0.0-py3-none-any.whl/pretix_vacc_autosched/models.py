277b217376fc95cbb42d95bf976743efc47ceee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:29 +0000

    Add package pretix-vacc-utils/1.0.0/pretix_vacc_utils-1.0.0-py3-none-any.whl

---
+++ packages/pretix-vacc-utils/pretix_vacc_utils-1.0.0-py3-none-any.whl/pretix_vacc_autosched/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from pretix.base.models import OrderPosition
+
+
+class ItemConfig(models.Model):
+    item = models.OneToOneField(
+        "pretixbase.Item",
+        related_name="vacc_autosched_config",
+        on_delete=models.CASCADE,
+    )
+    days = models.IntegerField(
+        verbose_name=_("Scheduling of second dose: Number of days"),
+        help_text=_("Keep empty to turn feature off"),
+    )
+    event = models.ForeignKey(
+        "pretixbase.Event",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name=_("Scheduling of second dose: Event series to choose date from"),
+        help_text=_("If empty, the current series will be used"),
+    )
+
+
+class LinkedOrderPosition(models.Model):
+    base_position = models.ForeignKey(
+        OrderPosition, related_name="vacc_autosched_linked", on_delete=models.PROTECT
+    )
+    child_position = models.OneToOneField(
+        OrderPosition, related_name="vacc_autosched_link", on_delete=models.PROTECT
+    )
