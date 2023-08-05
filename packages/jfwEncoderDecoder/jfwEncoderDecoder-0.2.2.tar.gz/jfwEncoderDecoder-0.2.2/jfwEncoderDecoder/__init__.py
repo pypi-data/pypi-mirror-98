7b81d04f79a246f166b76771a7fe550ede288eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:32 +0000

    Add package jfwEncoderDecoder/0.2.2/jfwEncoderDecoder-0.2.2.tar.gz

---
+++ packages/jfwEncoderDecoder/jfwEncoderDecoder-0.2.2.tar.gz/jfwEncoderDecoder-0.2.2/jfwEncoderDecoder/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .jfw_generator import *
+from .jfw_structs import *
+from .jfw_deserializer import deserializer
+
+# Version of the jfw-encoder-decoder package
+__version__ = "1.1.2"
