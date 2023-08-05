29a51b86b1a1e58d24a5f15d900598fabde6564d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:01 +0000

    Add package onfido-python/1.3.1/onfido_python-1.3.1-py3-none-any.whl

---
+++ packages/onfido-python/onfido_python-1.3.1-py3-none-any.whl/onfido/resources/live_photos.py
@@ -0,0 +1,16 @@
+from ..resource import Resource
+
+
+class LivePhotos(Resource):
+    def upload(self, sample_file:bytes, request_body):
+        return self._upload_request("live_photos/", sample_file, **request_body)
+
+    def find(self, live_photo_id:str):
+        return self._get(f"live_photos/{live_photo_id}")
+
+    def all(self, applicant_id:str):
+        payload = {"applicant_id": applicant_id}
+        return self._get("live_photos/", payload=payload)
+
+    def download(self, live_photo_id:str):
+        return self._download_request(f"live_photos/{live_photo_id}/download")
