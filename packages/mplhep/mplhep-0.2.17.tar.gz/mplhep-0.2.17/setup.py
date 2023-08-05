a3634995f2d3b4a4ff05b98dad16547000ef306b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package mplhep/0.2.17/mplhep-0.2.17.tar.gz

---
+++ packages/mplhep/mplhep-0.2.17.tar.gz/mplhep-0.2.17/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+extras_require = {
+    "test": [
+        "pytest",
+        "pytest-mpl",
+        "pytest-mock",
+        "papermill~=1.0",
+        "nteract-scrapbook~=0.3",
+        "uproot",
+        "uproot4",
+        "boost_histogram",
+        "scikit-hep-testdata",
+    ],
+    "dev": ["flake8", "jupyter", "bumpversion", "twine", "black", "pre-commit"],
+}
+extras_require["all"] = sorted(set(sum(extras_require.values(), [])))
+
+
+setup(
+    extras_require=extras_require,
+)
+
+# To push on pypi
+# python setup.py sdist
+# twine upload dist/*
