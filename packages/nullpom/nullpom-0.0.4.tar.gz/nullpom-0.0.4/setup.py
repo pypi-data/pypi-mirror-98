9c55bf2dba85cbd5f0f1eb98bbccad83f81fa682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package nullpom/0.0.4/nullpom-0.0.4.tar.gz

---
+++ packages/nullpom/nullpom-0.0.4.tar.gz/nullpom-0.0.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nullpom']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lightgbm>=3.1.1,<4.0.0',
+ 'matplotlib>=3.3.4,<4.0.0',
+ 'pandas>=1.2.3,<2.0.0',
+ 'scikit-learn>=0.24.1,<0.25.0']
+
+setup_kwargs = {
+    'name': 'nullpom',
+    'version': '0.0.4',
+    'description': 'test',
+    'long_description': None,
+    'author': 'tenajima',
+    'author_email': 'tenajima@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
