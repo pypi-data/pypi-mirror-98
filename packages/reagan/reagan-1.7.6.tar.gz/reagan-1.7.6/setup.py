c66d7880ccc1e63dca6cf7514a90ed19c929e590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:43 +0000

    Add package reagan/1.7.6/reagan-1.7.6.tar.gz

---
+++ packages/reagan/reagan-1.7.6.tar.gz/reagan-1.7.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="reagan",
+    version="1.7.6",
+    description="Package for streamlining credentials, connections, and data flow",
+    url="https://github.com/schustda/reagan",
+    author="Douglas Schuster",
+    author_email="douglas.schuster@carat.com",
+    packages=["reagan"],
+    zip_safe=False,
+    install_requires=requirements,
+)
+
