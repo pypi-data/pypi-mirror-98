c145da07bcefa52ca14b0d13f1d38bc706dc1d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:12 +0000

    Add package firex-bundle-ci/1.3.7/firex-bundle-ci-1.3.7.tar.gz

---
+++ packages/firex-bundle-ci/firex-bundle-ci-1.3.7.tar.gz/firex-bundle-ci-1.3.7/setup.py
@@ -0,0 +1,23 @@
+import versioneer
+from setuptools import setup
+
+setup(name='firex-bundle-ci',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='FireX CI services',
+      url='https://github.com/FireXStuff/firex-bundle-ci.git',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='BSD-3-Clause',
+      packages=['firex_bundle_ci'],
+      zip_safe=True,
+      install_requires=[
+          "firexapp",
+          "firex-keeper",
+          "firex-flame",
+          "lxml",
+          "xunitmerge",
+          "unittest-xml-reporting"
+      ],
+      entry_points={'firex.bundles': 'firex-bundle-ci = firex_bundle_ci'},
+      )
