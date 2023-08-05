06b0060ccca6fa347c7e4f848b85a440aea0cbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package gene-normalizer/0.1.2/gene-normalizer-0.1.2.tar.gz

---
+++ packages/gene-normalizer/gene-normalizer-0.1.2.tar.gz/gene-normalizer-0.1.2/README.md
@@ -0,0 +1,119 @@
+# Gene Normalization
+Services and guidelines for normalizing gene terms
+
+## Developer instructions
+Following are sections include instructions specifically for developers.
+
+### Installation
+For a development install, we recommend using Pipenv. See the 
+[pipenv docs](https://pipenv-fork.readthedocs.io/en/latest/#install-pipenv-today) 
+for direction on installing pipenv in your compute environment.
+ 
+Once installed, from the project root dir, just run:
+
+```commandline
+pipenv sync
+```
+
+Gene Normalization relies on [SeqRepo](https://github.com/biocommons/biocommons.seqrepo) data. 
+
+From the _gene_ directory of the repository:
+```commandline
+pip install seqrepo
+mkdir -p data/seqrepo
+seqrepo -r data/seqrepo pull -i 2020-11-27
+chmod -R u+w data/seqrepo
+cd data/seqrepo
+seqrepo_date_dir=$(ls -d */)
+mv $seqrepo_date_dir 2020-11-27
+```
+
+### Deploying DynamoDB Locally
+
+We use Amazon DynamoDB for our database. To deploy locally, follow [these instructions](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html).
+
+### Init coding style tests
+
+Code style is managed by [flake8](https://github.com/PyCQA/flake8) and checked prior to commit.
+
+We use [pre-commit](https://pre-commit.com/#usage) to run conformance tests.
+
+This ensures:
+
+* Check code style
+* Check for added large files
+* Detect AWS Credentials
+* Detect Private Key
+
+Before first commit run:
+
+```commandline
+pre-commit install
+```
+
+
+### Running unit tests
+
+Running unit tests is as easy as pytest.
+
+```commandline
+pipenv run pytest
+```
+
+### Updating the gene normalization database
+
+Before you use the CLI to update the database, run the following in a separate terminal to start a local DynamoDB service on `port 8000`:
+
+```
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+```
+
+To change the port, simply add `-port value`.
+
+#### Update source(s)
+The sources we currently use are: HGNC, Ensembl, and NCBI.
+
+To update one source, simply set `--normalizer` to the source you wish to update. 
+
+From the project root, run the following to update the HGNC source:
+
+```commandline
+python3 -m gene.cli --normalizer="hgnc"
+```
+
+To update multiple sources, you can use the `--normalizer` flag with the source names separated by spaces.
+
+#### Update all sources
+
+To update all sources, use the `--update_all` flag. 
+
+From the project root, run the following to update all sources:
+
+```commandline
+python3 -m gene.cli --update_all
+```
+
+#### Specifying the database URL endpoint
+The default URL endpoint is `http://localhost:8000`.
+There are two different ways to specify the database URL endpoint. 
+
+The first way is to set the `--db_url` flag to the URL endpoint.
+```commandline
+python3 -m gene.cli --update_all --db_url="http://localhost:8001"
+```
+ 
+The second way is to set the `GENE_NORM_DB_URL` to the URL endpoint.
+```commandline
+export GENE_NORM_DB_URL="http://localhost:8001"
+python3 -m gene.cli --update_all
+```
+
+### Starting the gene normalization service
+From the project root, run the following:
+```commandline
+ uvicorn gene.main:app --reload
+```
+
+Next, view the OpenAPI docs on your local machine: 
+
+http://127.0.0.1:8000/gene
---
+++ packages/gene-normalizer/gene-normalizer-0.1.2.tar.gz/gene-normalizer-0.1.2/gene/schemas.py
@@ -0,0 +1,461 @@
+"""This module contains data models for representing VICC normalized
+gene records.
+"""
+from typing import Type, List, Optional, Dict, Union, Any
+from pydantic import BaseModel, StrictBool
+from enum import Enum, IntEnum
+from pydantic.fields import Field
+
+
+class SymbolStatus(str, Enum):
+    """Define string constraints for symbol status attribute."""
+
+    WITHDRAWN = "withdrawn"
+    APPROVED = "approved"
+
+
+class Strand(str, Enum):
+    """Define string constraints for strand attribute."""
+
+    FORWARD = "+"
+    REVERSE = "-"
+
+
+class CytobandInterval(BaseModel):
+    """GA4GH cytoband interval definition."""
+
+    end: str
+    start: str
+    type = "CytobandInterval"
+
+    class Config:
+        """Configure model"""
+
+        orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['CytobandInterval']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for p in schema.get('properties', {}).values():
+                p.pop('title', None)
+            schema['example'] = {
+                "end": "q22.2",
+                "start": "q22.3",
+                "type": "CytobandInterval"
+            }
+
+
+class SimpleInterval(BaseModel):
+    """GA4GH simple interval definition."""
+
+    end: int
+    start: int
+    type = "SimpleInterval"
+
+    class Config:
+        """Configure model"""
+
+        orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['SimpleInterval']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for p in schema.get('properties', {}).values():
+                p.pop('title', None)
+            schema['example'] = {
+                "end": 44908822,
+                "start": 44908821,
+                "type": "SimpleInterval"
+            }
+
+
+class LocationType(str, Enum):
+    """Define string constraints for location type attribute."""
+
+    CHROMOSOME = "ChromosomeLocation"
+    SEQUENCE = "SequenceLocation"
+
+
+class Annotation(str, Enum):
+    """Define string constraints for annotations when gene location
+    is absent.
+    """
+
+    NOT_FOUND_ON_REFERENCE = "not on reference assembly"
+    UNPLACED = "unplaced"
+    RESERVED = "reserved"
+    ALT_LOC = "alternate reference locus"
+
+
+class Chromosome(str, Enum):
+    """Define string constraints for chromosomes."""
+
+    MITOCHONDRIA = 'MT'
+
+
+class Location(BaseModel):
+    """Define string constraints for the location attribute."""
+
+    id: str = Field(..., alias='_id')
+    type: LocationType
+
+
+class ChromosomeLocation(Location):
+    """GA4GH Chromosome Location definition."""
+
+    species_id: str
+    chr: str
+    interval: CytobandInterval
+
+    class Config:
+        """Configure model"""
+
+        orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['ChromosomeLocation']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for p in schema.get('properties', {}).values():
+                p.pop('title', None)
+            schema['example'] = {
+                "chr": "11",
+                "interval": {
+                    "end": "q22.2",
+                    "start": "q22.3",
+                    "type": "CytobandInterval"
+                },
+                "species_id": "taxonomy:9606",
+                "type": "ChromosomeLocation"
+            }
+
+
+class SequenceLocation(Location):
+    """GA4GH Sequence Location definition."""
+
+    sequence_id: str
+    interval: SimpleInterval
+
+    class Config:
+        """Configure model"""
+
+        orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['SequenceLocation']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for p in schema.get('properties', {}).values():
+                p.pop('title', None)
+            schema['example'] = {
+                "interval": {
+                    "end": 44908822,
+                    "start": 44908821,
+                    "type": "SimpleInterval"
+                },
+                "sequence_id": "ga4gh:SQ.IIB53T8CNeJJdUqzn9V_JnRtQadwWCbl",
+                "type": "SequenceLocation"
+            }
+
+
+class Gene(BaseModel):
+    """Gene"""
+
+    concept_id: str
+    symbol: str
+    symbol_status: Optional[SymbolStatus]
+    label: Optional[str]
+    strand: Optional[Strand]
+    location_annotations: Optional[List[str]]
+    locations: Optional[List[Union[ChromosomeLocation, SequenceLocation]]]
+    aliases: Optional[List[str]]
+    previous_symbols: Optional[List[str]]
+    other_identifiers: Optional[List[str]]
+    xrefs: Optional[List[str]]
+
+    class Config:
+        """Configure model"""
+
+        orm_mode = True
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['Gene']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for p in schema.get('properties', {}).values():
+                p.pop('title', None)
+            schema['example'] = {
+                "label": None,
+                "concept_id": "ensembl:ENSG00000157764",
+                "symbol": "BRAF",
+                "previous_symbols": [],
+                "aliases": [],
+                "other_identifiers": [],
+                "symbol_status": None,
+                "strand": "-",
+                "location": []
+            }
+
+
+class GeneGroup(Gene):
+    """A grouping of genes based on common attributes."""
+
+    description: str
+    type_identifier: str
+    genes: List[Gene]
+
+
+class MatchType(IntEnum):
+    """Define string constraints for use in Match Type attributes."""
+
+    CONCEPT_ID = 100
+    SYMBOL = 100
+    PREV_SYMBOL = 80
+    ALIAS = 60
+    OTHER_ID = 60
+    FUZZY_MATCH = 20
+    NO_MATCH = 0
+
+
+class SourceName(Enum):
+    """Define string constraints to ensure consistent capitalization."""
+
+    HGNC = "HGNC"
+    ENSEMBL = "Ensembl"
+    NCBI = "NCBI"
+
+
+class SourceIDAfterNamespace(Enum):
+    """Define string constraints after namespace."""
+
+    HGNC = ""
+    ENSEMBL = "ENSG"
+    NCBI = ""
+
+
+class NamespacePrefix(Enum):
+    """Define string constraints for namespace prefixes on concept IDs."""
+
+    HGNC = "hgnc"
+    ENSEMBL = "ensembl"
+    NCBI = "ncbigene"
+    ENTREZ = NCBI
+    VEGA = "vega"
+    UCSC = "ucsc"
+    ENA = "ena.embl"
+    REFSEQ = "refseq"
+    CCDS = "ccds"
+    UNIPROT = "uniprot"
+    PUBMED = "pubmed"
+    COSMIC = "cosmic"
+    OMIM = "omim"
+    MIRBASE = "mirbase"
+    HOMEODB = "homeodb"
+    SNORNABASE = "snornabase"
+    ORPHANET = "orphanet"
+    PSEUDOGENE = "pseudogene.org"
+    HORDE = "hordedb"
+    MEROPS = "merops"
+    IUPHAR = "iuphar"
+    KZNF = "knzfgc"
+    MAMIT = "mamittrnadb"
+    CD = "hcdmdb"
+    LNCRNADB = "lncrnadb"
+    IMGT = "imgt"  # .hla? .ligm? leave as is?
+    IMGT_GENE_DB = "imgt/gene-db"  # redundant w/ above?
+    RFAM = "rfam"
+
+
+class DataLicenseAttributes(BaseModel):
+    """Define constraints for data license attributes."""
+
+    non_commercial: StrictBool
+    share_alike: StrictBool
+    attribution: StrictBool
+
+
+class Meta(BaseModel):
+    """Metadata for a given source to return in response object."""
+
+    data_license: str
+    data_license_url: str
+    version: str
+    data_url: Optional[str]
+    rdp_url: Optional[str]
+    data_license_attributes: Dict[str, StrictBool]
+    genome_assemblies: Optional[List[str]]
+
+    class Config:
+        """Enables orm_mode"""
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['Meta']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "data_license": "custom",
+                "data_license_url": "https://www.ncbi.nlm.nih.gov/home/about/policies/",  # noqa: E501
+                "version": "20201215",
+                "data_url": "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/",
+                "rdp_url": "https://reusabledata.org/ncbi-gene.html",
+                'data_license_attributes': {
+                    "non_commercial": False,
+                    "share_alike": False,
+                    "attribution": False
+                },
+                "genome_assemblies": None
+            }
+
+
+class MatchesKeyed(BaseModel):
+    """Container for matching information from an individual source.
+    Used when matches are requested as an object, not an array.
+    """
+
+    match_type: MatchType
+    records: List[Gene]
+    meta_: Meta
+
+    class Config:
+        """Enables orm_mode"""
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['MatchesKeyed']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "NCBI": {
+                    "match_type": 0,
+                    "records": [],
+                    "meta_": {
+                        "data_license": "custom",
+                        "data_license_url": "https://www.ncbi.nlm.nih.gov/home/about/policies/",  # noqa: E501
+                        "version": "20201215",
+                        "data_url": "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/",
+                        "rdp_url": "https://reusabledata.org/ncbi-gene.html",
+                        "data_license_attributes": {
+                            "non_commercial": False,
+                            "share_alike": False,
+                            "attribution": False
+                        },
+                        "genome_assemblies": None
+                    }
+                }
+            }
+
+
+class MatchesListed(BaseModel):
+    """Container for matching information from an individual source.
+    Used when matches are requested as an array, not an object.
+    """
+
+    source: SourceName
+    match_type: MatchType
+    records: List[Gene]
+    meta_: Meta
+
+    class Config:
+        """Enables orm_mode"""
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['MatchesListed']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "source": "NCBI",
+                "match_type": 0,
+                "records": [],
+                "meta_": {
+                    "data_license": "custom",
+                    "data_license_url": "https://www.ncbi.nlm.nih.gov/home/about/policies/",  # noqa: E501
+                    "version": "20201215",
+                    "data_url": "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/",
+                    "rdp_url": "https://reusabledata.org/ncbi-gene.html",
+                    "data_license_attributes": {
+                        "non_commercial": False,
+                        "share_alike": False,
+                        "attribution": False
+                    },
+                    "genome_assemblies": None
+                }
+            }
+
+
+class Service(BaseModel):
+    """Core response schema containing matches for each source"""
+
+    query: str
+    warnings: Optional[List]
+    source_matches: Union[Dict[SourceName, MatchesKeyed], List[MatchesListed]]
+
+    class Config:
+        """Enables orm_mode"""
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['Service']) -> None:
+            """Configure OpenAPI schema"""
+            if 'title' in schema.keys():
+                schema.pop('title', None)
+            for prop in schema.get('properties', {}).values():
+                prop.pop('title', None)
+            schema['example'] = {
+                "query": "BRAF",
+                "warnings": [],
+                "source_matches": [
+                    {
+                        "source": "Ensembl",
+                        "match_type": 100,
+                        "records": [
+                            {
+                                "label": None,
+                                "concept_id": "ensembl:ENSG00000157764",
+                                "symbol": "BRAF",
+                                "previous_symbols": [],
+                                "aliases": [],
+                                "other_identifiers": [],
+                                "symbol_status": None,
+                                "strand": "-",
+                                "locations": []
+                            }
+                        ],
+                        "meta_": {
+                            "data_license": "custom",
+                            "data_license_url": "https://uswest.ensembl.org/info/about/legal/index.html",  # noqa: E501
+                            "version": "102",
+                            "data_url": "http://ftp.ensembl.org/pub/",
+                            "rdp_url": None,
+                            "data_license_attributes": {
+                                "non_commercial": False,
+                                "share_alike": False,
+                                "attribution": False
+                            },
+                            "genome_assemblies": "GRCh38"
+                        }
+                    }
+                ]
+            }
