8646475e22f25f895d135bf5a4349f2e297f47e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:36 +0000

    Add package ergo/0.4.12a0/ergo-0.4.12a0.tar.gz

---
+++ packages/ergo/ergo-0.4.12a0.tar.gz/ergo-0.4.12a0/src/flask_http_invoker.py
@@ -0,0 +1,43 @@
+"""Summary."""
+from typing import List
+
+from flask import Flask, request  # , abort
+
+from src.http_invoker import HttpInvoker
+from src.types import TYPE_PAYLOAD
+
+
+class FlaskHttpInvoker(HttpInvoker):
+    """Summary."""
+
+    def start(self) -> int:
+        """Summary.
+
+        Returns:
+            int: Description
+
+        """
+        app: Flask = Flask(__name__)
+
+        @app.route(self.route)
+        def handler() -> str:
+            """Summary.
+
+            Returns:
+                str: Description
+
+            """
+            data_out: List[TYPE_PAYLOAD] = []
+            data_in: TYPE_PAYLOAD = dict(request.args)
+            # data_in(f'route: {str(request.url_rule)}')
+            # try:
+            for result in self._invocable.invoke(data_in):
+                data_out.append(result)
+            # except Exception as err:
+            #     print(err)
+            #     abort(400)
+
+            return str(data_out)
+
+        app.run(host='0.0.0.0', port=self._port)
+        return 0
