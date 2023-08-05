d00dfd34d59f392a3a77908320ffb233fa98a9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:52 +0000

    Add package wasdi/0.6.2/wasdi-0.6.2.tar.gz

---
+++ packages/wasdi/wasdi-0.6.2.tar.gz/wasdi-0.6.2/setup.py
@@ -0,0 +1,44 @@
+"""
+Waspy - WASDI Python Library
+
+Created on 11 Jun 2018
+
+@author: p.campanella - FadeOut Software
+@author: c.nattero - FadeOut Software
+"""
+import setuptools
+import io
+
+with io.open("README.md", "r", encoding="utf8") as oFh:
+    sLong_description = oFh.read()
+
+
+setuptools.setup(
+    name="wasdi",
+    version="0.6.2",
+    author="FadeOut Software",
+    author_email="info@fadeout.biz",
+    description="The Wasdi Python library",
+    long_description=sLong_description,
+    long_description_content_type="text/markdown",
+    url="http://www.wasdi.net",
+    packages=setuptools.find_packages(),
+    install_requires = [
+        'requests', 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Software Development :: Libraries"
+    ],
+    project_urls={
+        'Source': 'https://github.com/fadeoutsoftware/WASDI/tree/develop/libraries/waspy',
+        'Tracker': 'https://github.com/fadeoutsoftware/WASDI/issues'
+    }
+)
