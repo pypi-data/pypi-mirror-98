15f36d118d6f0f5209af7a6df02be92621bfb9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:34 +0000

    Add package dask-kubernetes/2021.3.0/dask-kubernetes-2021.3.0.tar.gz

---
+++ packages/dask-kubernetes/dask-kubernetes-2021.3.0.tar.gz/dask-kubernetes-2021.3.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name="dask-kubernetes",
+    cmdclass=versioneer.get_cmdclass(),
+    version=versioneer.get_version(),
+    description="Native Kubernetes integration for Dask",
+    url="https://github.com/dask/dask-kubernetes",
+    keywords="dask,kubernetes,distributed",
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=(open("README.rst").read() if exists("README.rst") else ""),
+    zip_safe=False,
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    python_requires=">=3.7",
+)
