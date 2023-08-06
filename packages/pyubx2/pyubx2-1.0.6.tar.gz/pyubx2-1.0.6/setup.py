33bf793e06f1369884be9192c1c1a226c2f857f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package pyubx2/1.0.6/pyubx2-1.0.6.tar.gz

---
+++ packages/pyubx2/pyubx2-1.0.6.tar.gz/pyubx2-1.0.6/setup.py
@@ -0,0 +1,53 @@
+'''
+Created on 1 Oct 2020
+
+@author: semuadmin
+'''
+import setuptools
+
+from pyubx2 import version as VERSION
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyubx2",
+    version=VERSION,
+    author="semuadmin",
+    author_email="semuadmin@semuconsulting.com",
+    description="UBX Protocol Parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/semuconsulting/pyubx2",
+    packages=setuptools.find_packages(exclude=['tests', 'examples', 'docs']),
+    license="BSD 3-Clause 'Modified' License",
+    keywords="pyubx2 GNSS GPS GLONASS UBX GIS u-blox",
+    platforms="Windows, MacOS, Linux",
+    project_urls={
+        "Bug Tracker": "https://github.com/semuconsulting/pyubx2",
+        "Documentation": "https://github.com/semuconsulting/pyubx2",
+        "Sphinx API Documentation": "http://semuconsulting.com/pyubx2",
+        "Source Code": "https://github.com/semuconsulting/pyubx2",
+    },
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    python_requires='>=3.6',
+)
