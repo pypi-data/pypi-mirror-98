63c037f55775c9ad8d025f5ede53dca0541b986c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package scrapy-rabbit/0.2.3/scrapy_rabbit-0.2.3-py3-none-any.whl

---
+++ packages/scrapy-rabbit/scrapy_rabbit-0.2.3-py3-none-any.whl/scrapy_rabbit/http/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from scrapy_rabbit.http.req import random_ua
+from scrapy_rabbit.http.req import Request
---
+++ packages/scrapy-rabbit/scrapy_rabbit-0.2.3-py3-none-any.whl/scrapy_rabbit/utils/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from scrapy_rabbit.utils.project import get_project_settings
+from scrapy_rabbit.utils.loadspider import SpiderLoader
+from scrapy_rabbit.utils.pipelines import Pipeline
