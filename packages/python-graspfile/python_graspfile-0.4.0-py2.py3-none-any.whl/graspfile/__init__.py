a9b690a9290eb4380ef68edcc3f3781c998541e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:27 +0000

    Add package python-graspfile/0.4.0/python_graspfile-0.4.0-py2.py3-none-any.whl

---
+++ packages/python-graspfile/python_graspfile-0.4.0-py2.py3-none-any.whl/graspfile/__init__.py
@@ -0,0 +1,19 @@
+"""This is main graspfile module for manipulating files from TICRA Tools, GRASP and CHAMP
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    cut
+    grid
+    torfile
+    torparser
+    analysis
+    plot
+"""
+
+__version__ = '0.4.0'
+
+__all__ = ['cut', 'grid', 'plot', 'torfile', 'torparser', 'analysis']
