75c6aecdbf0881c5f82fa2ae44bf59c459fb4cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:52 +0000

    Add package quicktrash/0.0.0/quicktrash-0.0.0.tar.gz

---
+++ packages/quicktrash/quicktrash-0.0.0.tar.gz/quicktrash-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='quicktrash',
+    packages=["quicktrash"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
