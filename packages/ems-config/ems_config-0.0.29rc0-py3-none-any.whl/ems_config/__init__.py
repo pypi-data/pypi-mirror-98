17db638ba889a4987643cdaf743d1195cb856835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:39 +0000

    Add package ems-config/0.0.29rc0/ems_config-0.0.29rc0-py3-none-any.whl

---
+++ packages/ems-config/ems_config-0.0.29rc0-py3-none-any.whl/ems_config/__init__.py
@@ -0,0 +1,4 @@
+from ems_config.config import parse_config, parse_remote_config
+from ems_config.helpers import *
+from ems_config.vars import *
+
---
+++ packages/ems-config/ems_config-0.0.29rc0-py3-none-any.whl/ems_config/vars.py
@@ -0,0 +1,3 @@
+import os
+
+APP_DATA_DIR = os.getenv("APP_DATA_PATH", "app_data")
