71810ff7e2678071f9cdfd3bca9e353f67ddd360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:14 +0000

    Add package TimeConvert/1.5.3/TimeConvert-1.5.3.tar.gz

---
+++ packages/TimeConvert/TimeConvert-1.5.3.tar.gz/TimeConvert-1.5.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '1.5.3'
+
+
+setup(
+    name='TimeConvert',
+    version=version,
+    keywords='',
+    description='Time Convert for Humans™',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/xxx-convert/TimeConvert',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['TimeConvert'],
+    py_modules=[],
+    install_requires=['python-dateutil', 'pytz', 'tzlocal'],
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
