07ac1293675b213463749b67856becb88f1d449f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:19 +0000

    Add package twacapic/0.2.0/twacapic-0.2.0.tar.gz

---
+++ packages/twacapic/twacapic-0.2.0.tar.gz/twacapic-0.2.0/twacapic/main.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+
+from twacapic.auth import save_credentials
+from twacapic.collect import UserGroup
+
+
+def run():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--userlist',
+                        help='path to list of user IDs, one per line. \
+                        Required for first run only. Can be used to add users to a group')
+    parser.add_argument('-g', '--groupname',
+                        help='name of the group to collect.\
+                        Results will be saved in folder `results/GROUPNAME/`.\
+                        Can be used to poll for new tweets of a group.\
+                        Default: "users"',
+                        default='users')
+    args = parser.parse_args()
+
+    print("Hello friend …")
+
+    if not os.path.isfile('twitter_keys.yaml'):
+        print("Please enter your API key:")
+        consumer_key = input()
+        print("Please enter your API secret:")
+        consumer_secret = input()
+
+        save_credentials('twitter_keys.yaml', consumer_key, consumer_secret)
+
+    try:
+        path = args.userlist
+    except IndexError:
+        path = None
+
+    user_group = UserGroup(path=path, name=args.groupname)
+    user_group.collect()
+
+    print("Finished")
+
+
+if __name__ == '__main__':
+
+    run()
