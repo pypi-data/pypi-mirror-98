1b34144254b79b18b19213594780df21f195bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:34 +0000

    Add package datazen/1.6.4/datazen-1.6.4.tar.gz

---
+++ packages/datazen/datazen-1.6.4.tar.gz/datazen-1.6.4/tests/test_entry.py
@@ -0,0 +1,64 @@
+
+"""
+datazen - Test the program's entry-point.
+"""
+
+# built-in
+import os
+
+# module under test
+from datazen import PKG_NAME
+from datazen.entry import main as datazen_main
+
+# internal
+from .resources import get_resource
+
+
+def test_entry_basic():
+    """
+    Build the default target to provide a useful test when isolating
+    individual tests.
+    """
+
+    manifest = get_resource("manifest.yaml", True)
+    args = [PKG_NAME, "-m", manifest]
+    assert datazen_main(args) == 0
+    assert datazen_main(args + ["-c"]) == 0
+
+
+def test_entry():
+    """ Test some basic command-line argument scenarios. """
+
+    manifest = get_resource("manifest.yaml", True)
+    manifest_dir = os.path.dirname(manifest)
+    args = [PKG_NAME, "-m", manifest]
+
+    assert datazen_main(args + ["--not-an-option", "asdf"]) != 0
+
+    assert datazen_main(args) == 0
+    assert datazen_main(args + ["a", "b", "c"]) == 0
+    assert datazen_main(args + ["not_a_target"]) != 0
+    assert datazen_main([PKG_NAME, "-C", manifest_dir, "a", "b", "c"]) == 0
+    assert datazen_main(args + ["--sync", "-d"]) == 0
+
+    # change a file to test describe's miss detection
+    data_file = get_resource(os.path.join("configs", "a.yaml"), True)
+    with open(data_file) as manifest_file:
+        manifest_data = manifest_file.read()
+    with open(data_file, "w") as manifest_file:
+        manifest_file.write("a: {{a}}")
+
+    assert datazen_main(args + ["-d"]) == 0
+
+    # remove a file to additionally test describe's miss detection
+    os.remove(data_file)
+    assert datazen_main(args + ["-d"]) == 0
+
+    # restore the changed file
+    with open(data_file, "w") as manifest_file:
+        manifest_data = manifest_file.write(manifest_data)
+
+    assert datazen_main(args + ["-c"]) == 0
+
+    # test inability to load a manifest
+    assert datazen_main(args + ["-m", "not-a-manifest"]) != 0
---
+++ packages/datazen/datazen-1.6.4.tar.gz/datazen-1.6.4/tests/test_schemas.py
@@ -0,0 +1,27 @@
+
+"""
+datazen - Tests for the 'schemas' API.
+"""
+
+# module under test
+from datazen.schemas import validate
+
+# internal
+from . import ENV
+
+
+def test_load_schemas():
+    """ Test that the schemas can be loaded. """
+
+    assert ENV.get_schemas(True, True)
+
+
+def test_validate_configs():
+    """ Test that schema data can successfully validate configuration data. """
+
+    schema_data = ENV.get_schemas(True, True)
+    assert schema_data
+    config_data = ENV.get_configs(True)
+    assert config_data
+
+    assert validate(schema_data, config_data)
