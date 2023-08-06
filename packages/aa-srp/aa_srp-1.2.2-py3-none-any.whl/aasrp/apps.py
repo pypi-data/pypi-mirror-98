8cb6651c9cb0020c9e82c2ea2e616065ff3c8cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:31 +0000

    Add package aa-srp/1.2.2/aa_srp-1.2.2-py3-none-any.whl

---
+++ packages/aa-srp/aa_srp-1.2.2-py3-none-any.whl/aasrp/apps.py
@@ -0,0 +1,17 @@
+"""
+app config
+"""
+
+from django.apps import AppConfig
+
+from aasrp import __version__
+
+
+class AaSrpConfig(AppConfig):
+    """
+    application config
+    """
+
+    name = "aasrp"
+    label = "aasrp"
+    verbose_name = "AA Ship Replacement v{}".format(__version__)
