54b682bc0420dd88639b43e43d9e3d82407cd94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:58 +0000

    Add package py-Ultroid/13.4b0/py_Ultroid-13.4b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-13.4b0-py3-none-any.whl/pyUltroid/functions/__init__.py
@@ -0,0 +1,17 @@
+from pyUltroid import *
+from ..dB.database import Var
+
+DANGER = [
+    "APP_ID",
+    "API_HASH",
+    "SESSION",
+    "BOT_TOKEN",
+    "HEROKU_API",
+    "base64",
+    "base32",
+    "get_me",
+    "NewMessage",
+    "REDIS_PASSWORD",
+    "load_addons",
+    "load_plugins",
+]
