5bf1376304fb043a3e9f9e5b8b12bfacdb582ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:32 +0000

    Add package pointercratepy/0.0.2/pointercratepy-0.0.2.tar.gz

---
+++ packages/pointercratepy/pointercratepy-0.0.2.tar.gz/pointercratepy-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pointercratepy',
+    version='0.0.2',
+    packages=find_packages(),
+    url='https://github.com/bretheskevin/pointercrate.py',
+    license='MIT License',
+    author='Hikudo',
+    author_email='bretheskevin@gmail.com',
+    description='A pointercrate API wrapper for Python',
+    long_description=open("README.md").read() + '\n\n' + open("CHANGELOG.txt").read(),
+    long_description_content_type = "text/markdown",
+    keywords='pointercratepy',
+    install_requires=['requests~=2.25.1']
+)
