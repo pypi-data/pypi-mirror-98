1af70c8d025f2b7ac8d67d6c0b1a3ccf8506b365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:27 +0000

    Add package django-po2xls/0.9.9/django-po2xls-0.9.9.zip

---
+++ packages/django-po2xls/django-po2xls-0.9.9.zip/django-po2xls-0.9.9/po2xls/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# django-po2xls
+# po2xls/__init__.py
+
+
+from typing import List  # pylint: disable=W0611
+
+
+__all__ = ["default_app_config"]  # type: List[str]
+
+
+default_app_config = "po2xls.apps.DjangoPoToXlsConfig"
