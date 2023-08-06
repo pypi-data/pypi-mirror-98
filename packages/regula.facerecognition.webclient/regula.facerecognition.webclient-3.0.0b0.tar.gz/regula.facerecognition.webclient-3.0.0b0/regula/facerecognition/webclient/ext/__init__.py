4ae15b09263a31acfee68e7c6ec7e07879892c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:32 +0000

    Add package regula.facerecognition.webclient/3.0.0b0/regula.facerecognition.webclient-3.0.0b0.tar.gz

---
+++ packages/regula.facerecognition.webclient/regula.facerecognition.webclient-3.0.0b0.tar.gz/regula.facerecognition.webclient-3.0.0b0/regula/facerecognition/webclient/ext/__init__.py
@@ -0,0 +1,5 @@
+from regula.facerecognition.webclient.ext.api import MatchingApi, FaceSdk
+from regula.facerecognition.webclient.ext.common import Base64String
+from regula.facerecognition.webclient.ext.models import CompareRequest
+from regula.facerecognition.webclient.ext.models import CompareImage
+from regula.facerecognition.webclient.ext.models import DetectRequest
---
+++ packages/regula.facerecognition.webclient/regula.facerecognition.webclient-3.0.0b0.tar.gz/regula.facerecognition.webclient-3.0.0b0/setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="regula.facerecognition.webclient",
+    version=os.getenv("PACKAGE_VERSION_TO_PUBLISH", "unknown"),
+    python_requires=">=3.5",
+    description="Regula's Face Recognition python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Regula Forensics, Inc.",
+    author_email="support@regulaforensics.com",
+    url="https://mobile.regulaforensics.com",
+    keywords=[
+        "face-recognition-client",
+        "face recognition",
+        "facer",
+        "regulaforensics",
+        "regula",
+    ],
+    install_requires=[
+        "certifi==2020.12.5",
+        "future==0.18.2",
+        "python-dateutil==2.8.1",
+        "six==1.15.0",
+        "urllib3==1.26.3",
+    ],
+    packages=find_packages(exclude=["test", "tests", "example"]),
+    include_package_data=True,
+)
