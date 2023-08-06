499858f9b63cc0ea76456c6c4d9231d444edc2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:51 +0000

    Add package Products.MIMETools/3.2/Products.MIMETools-3.2.tar.gz

---
+++ packages/Products.MIMETools/Products.MIMETools-3.2.tar.gz/Products.MIMETools-3.2/setup.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(name='Products.MIMETools',
+      version='3.2',
+      url='https://github.com/zopefoundation/Products.MIMETools',
+      project_urls={
+        'Issue Tracker': ('https://github.com/zopefoundation'
+                          '/Products.MIMETools/issues'),
+        'Sources': 'https://github.com/zopefoundation/Products.MIMETools',
+      },
+      license='ZPL 2.1',
+      description='MIMETools provides the ``dtml-mime`` tag for '
+                  'DocumentTemplate.',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.txt').read()),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Web Environment',
+        'Framework :: Zope',
+        'Framework :: Zope :: 4',
+        'Framework :: Zope :: 5',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Communications :: Email',
+      ],
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+      install_requires=[
+        'setuptools',
+        'six',
+        'ExtensionClass>=4.1a1',
+        'DocumentTemplate>=3',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
