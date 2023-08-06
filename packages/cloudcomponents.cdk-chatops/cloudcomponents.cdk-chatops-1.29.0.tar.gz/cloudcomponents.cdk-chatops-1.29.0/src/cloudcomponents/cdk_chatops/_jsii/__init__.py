d91d2c3843aa5925dc0b797be190b636b236281b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:24 +0000

    Add package cloudcomponents.cdk-chatops/1.29.0/cloudcomponents.cdk-chatops-1.29.0.tar.gz

---
+++ packages/cloudcomponents.cdk-chatops/cloudcomponents.cdk-chatops-1.29.0.tar.gz/cloudcomponents.cdk-chatops-1.29.0/src/cloudcomponents/cdk_chatops/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_chatbot._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_event_sources._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-chatops",
+    "1.29.0",
+    __name__[0:-6],
+    "cdk-chatops@1.29.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
