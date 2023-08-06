eb007cf1c862aec9aca20ede631cf8c26a939fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:01 +0000

    Add package aiolyric/1.0.6/aiolyric-1.0.6.tar.gz

---
+++ packages/aiolyric/aiolyric-1.0.6.tar.gz/aiolyric-1.0.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import io
+
+from setuptools import setup, find_packages
+
+version = "1.0.6"
+
+setup(
+    name="aiolyric",
+    version=version,
+    description="AIO package for the Honeywell Lyric Platform.",
+    long_description=io.open("README.md", encoding="UTF-8").read(),
+    keywords="honeywell lyric thermostat",
+    author="Aidan Timson (Timmo)",
+    author_email="contact@timmo.xyz",
+    license="MIT",
+    url="https://github.com/timmo001/aiolyric",
+    packages=find_packages(exclude=["tests", "generator"]),
+    install_requires=["aiohttp>=3.7.3"],
+)
