cdea0f8c8d34dcad3934fc2590564acb75298aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:07 +0000

    Add package pylint-json2html/0.3.0/pylint_json2html-0.3.0-py3-none-any.whl

---
+++ packages/pylint-json2html/pylint_json2html-0.3.0-py3-none-any.whl/tests/craps.py
@@ -0,0 +1,16 @@
+
+def something():
+    if True:
+        print('It is true.')
+        pass
+    g = 'this is not a good variable'
+    result = 60 * 60 * 24 * 365.5 * 100
+    return True
+
+def iPreferCamelCase(what):
+    what_are_you_going_to_do_against_such_long_variable_name = 'i know right?'
+    return something() is not what
+
+THIS_LINE_IS_TOO_LONG = ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.'
+
+not_uppercase = 'IT IS NOT'
---
+++ packages/pylint-json2html/pylint_json2html-0.3.0-py3-none-any.whl/tests/other.py
@@ -0,0 +1,19 @@
+
+
+class SomeClass:
+    def class_method(self, not_relevant):
+        print('this should be a class method')
+
+
+def non_latin_code():
+    п = 'a'
+    text = 'природа'
+
+
+def duplicate_something():
+    if True:
+        print('It is true.')
+        pass
+    g = 'this is not a good variable'
+    result = 60 * 60 * 24 * 365.5 * 100
+    return True
