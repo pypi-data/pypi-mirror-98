bfc34d1f77cd20ff7ebd717147e3add29268dba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package cpymad/1.7.0/cpymad-1.7.0-cp39-cp39-win_amd64.whl

---
+++ packages/cpymad/cpymad-1.7.0-cp39-cp39-win_amd64.whl/cpymad/__init__.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+__title__ = 'cpymad'
+__version__ = '1.7.0'
+
+__summary__ = 'Cython binding to MAD-X'
+__uri__ = 'https://github.com/hibtc/cpymad'
+
+__credits__ = """
+Current cpymad maintainer:
+
+    - Thomas Gläßle <t_glaessle@gmx.de>
+
+Initial pymad creators:
+
+    - Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
+    - Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
+"""
+
+
+def get_copyright_notice():
+    from importlib_resources import read_text
+    return read_text('cpymad.COPYING', 'cpymad.rst', encoding='utf-8')
