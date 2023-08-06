a00bd9b343e521d1816e6e7248a6d8a95ad074ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:30 +0000

    Add package pykalimux/1.0/pykalimux-1.0.tar.gz

---
+++ packages/pykalimux/pykalimux-1.0.tar.gz/pykalimux-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pykalimux',
+version='1.0',
+description='This is a python script by which you can install Kali Nethunter (Kali Linux) in your termux application without rooted phone.',
+url='https://github.com/RaynerSec/pykalimux',
+author='RC Chuah',
+author_email='raynersec@gmail.com',
+license='GPL',
+scripts=['pykalimux'],
+zip_safe=False)
