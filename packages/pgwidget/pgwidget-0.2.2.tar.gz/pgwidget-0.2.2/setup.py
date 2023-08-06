c1e60685ae9764dcb2bdf0881fe3e7f8cf83c443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:59 +0000

    Add package pgwidget/0.2.2/pgwidget-0.2.2.tar.gz

---
+++ packages/pgwidget/pgwidget-0.2.2.tar.gz/pgwidget-0.2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='pgwidget',
+    version='0.2.2',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='Widgets for pygame',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DovaX/pgwidget',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pygame','pandas'
+     ],
+    python_requires='>=3.6',
+)
+    
