87b46ebd8d38a474d01795a7ab99214cb2d7d257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:19 +0000

    Add package comlib/0.4.2/comlib-0.4.2.tar.gz

---
+++ packages/comlib/comlib-0.4.2.tar.gz/comlib-0.4.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='comlib',
+      version='0.4.2',
+      description='Randomization and communities',
+      packages=['comlib'],
+      author_email='sergey-vorobyof@mail.ru',
+      zip_safe=False)
