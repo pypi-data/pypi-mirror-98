0d62d6a95cd0da8466e600b8348b923f9762b871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:27 +0000

    Add package crude.dev/0.1.0/crude.dev-0.1.0.tar.gz

---
+++ packages/crude.dev/crude.dev-0.1.0.tar.gz/crude.dev-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crude_dev']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tabulate>=0.8.9,<0.9.0', 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['crude = crude_dev.cli:main', 'run = crude_dev.cli:main']}
+
+setup_kwargs = {
+    'name': 'crude.dev',
+    'version': '0.1.0',
+    'description': 'crude.dev api client',
+    'long_description': None,
+    'author': 'Janto Dreijer',
+    'author_email': 'jantod@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://crude.dev/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
