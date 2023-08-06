159aa9fb5bd1860979bb0aa76b75b27994af6c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:38 +0000

    Add package hiko2233/1.0.0/hiko2233-1.0.0.tar.gz

---
+++ packages/hiko2233/hiko2233-1.0.0.tar.gz/hiko2233-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    description='test',
+    long_description=open('read.txt').read(),
+    version='1.0.0',
+    packages=['test1'],
+    name='hiko2233'
+    )
