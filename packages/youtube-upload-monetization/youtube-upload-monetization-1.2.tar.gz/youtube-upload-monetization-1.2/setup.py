ca1222601e85e84775b17cdece172357363cc732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:01 +0000

    Add package youtube-upload-monetization/1.2/youtube-upload-monetization-1.2.tar.gz

---
+++ packages/youtube-upload-monetization/youtube-upload-monetization-1.2.tar.gz/youtube-upload-monetization-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='youtube-upload-monetization',
+   version='1.2',
+   description='Uploads videos to youtube through the use of selenium',
+   author='Chance Vodnoy',
+   author_email='chancevodnoy129@gmail.com',
+   packages=['youtube-upload-monetization'],
+   install_requires=[]
+)
