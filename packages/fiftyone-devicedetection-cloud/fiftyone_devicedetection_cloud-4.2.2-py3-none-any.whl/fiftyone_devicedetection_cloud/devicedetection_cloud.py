2137b72fbb9de643c150fb5d94fa03c21fe75dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:19 +0000

    Add package fiftyone-devicedetection-cloud/4.2.2/fiftyone_devicedetection_cloud-4.2.2-py3-none-any.whl

---
+++ packages/fiftyone-devicedetection-cloud/fiftyone_devicedetection_cloud-4.2.2-py3-none-any.whl/fiftyone_devicedetection_cloud/devicedetection_cloud.py
@@ -0,0 +1,37 @@
+# *********************************************************************
+# This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+# Copyright 2019 51 Degrees Mobile Experts Limited, 5 Charlotte Close,
+# Caversham, Reading, Berkshire, United Kingdom RG4 7BY.
+#
+# This Original Work is licensed under the European Union Public Licence (EUPL) 
+# v.1.2 and is subject to its terms as set out below.
+#
+# If a copy of the EUPL was not distributed with this file, You can obtain
+# one at https://opensource.org/licenses/EUPL-1.2.
+#
+# The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+# amended by the European Commission) shall be deemed incompatible for
+# the purposes of the Work and the provisions of the compatibility
+# clause in Article 5 of the EUPL shall not apply.
+# 
+# If using the Work as, or as part of, a network application, by 
+# including the attribution notice(s) required under Article 5 of the EUPL
+# in the end user terms of the application under an appropriate heading, 
+# such notice(s) shall fulfill the requirements of that article.
+# ********************************************************************
+
+from __future__ import absolute_import
+from fiftyone_pipeline_cloudrequestengine.cloudengine import CloudEngine
+
+class DeviceDetectionCloud(CloudEngine):
+    """!
+    The Device Detction cloud engine requires the 51Degrees
+    cloudRequestEngine to be placed in a pipeline before it.
+    It takes that raw JSON response and parses it to extract the
+    device part. It also uses this data to generate a list of properties
+    """
+    def __init__(self):
+
+        super(DeviceDetectionCloud, self).__init__()
+
+        self.datakey = "device"
