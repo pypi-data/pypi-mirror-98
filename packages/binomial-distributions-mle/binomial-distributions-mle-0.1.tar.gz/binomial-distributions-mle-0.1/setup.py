c48985fc1d38d59ddc8c6f602467fde62e98c04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:58 +0000

    Add package binomial-distributions-mle/0.1/binomial-distributions-mle-0.1.tar.gz

---
+++ packages/binomial-distributions-mle/binomial-distributions-mle-0.1.tar.gz/binomial-distributions-mle-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial-distributions-mle',
+      version='0.1',
+      description='Binomial distributions',
+      packages=['binomial-distributions-mle'],
+      zip_safe=False)
