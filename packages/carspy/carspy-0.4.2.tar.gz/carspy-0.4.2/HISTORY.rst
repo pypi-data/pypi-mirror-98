83eff0a54908e7dc20d7654d57418752d7a1e49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:04 +0000

    Add package carspy/0.4.2/carspy-0.4.2.tar.gz

---
+++ packages/carspy/carspy-0.4.2.tar.gz/carspy-0.4.2/HISTORY.rst
@@ -0,0 +1,44 @@
+==========
+Change Log
+==========
+
+0.4.2 (2021-03-12)
+------------------
+
+* Add missing json data files to the package.
+
+0.4.1 (2021-03-10)
+------------------
+
+* Implement an asymmetric "super" Voigt function for better fitting slit function.
+
+0.4.0 (2021-03-03)
+------------------
+
+* Implement least-square fitting routine with ``lmfit`` (optional).
+* Add documentations for the least-square fitting module.
+* Add usage examples for both synthesizing and fitting CARS spectra.
+
+0.3.0 (2021-02-25)
+------------------
+
+* Implement optional function for calculating local gas composition at chemical equilibrium using ``cantera``.
+* Add documentations for secondary modules.
+* Add bibtex style references in documentations.
+
+0.2.1 (2021-02-14)
+------------------
+
+* Change Sphinx theme.
+* Add existing modules to docs.
+* Fix format errors in docstrings.
+
+0.2.0 (2021-02-13)
+------------------
+
+* Implement modules for synthesizing N2 CARS spectra.
+
+0.1.0 (2021-02-13)
+------------------
+
+* First release on PyPI.
---
+++ packages/carspy/carspy-0.4.2.tar.gz/carspy-0.4.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['numpy']
+
+setup_requirements = []
+
+test_requirements = ['numpy']
+
+setup(
+    author="Zhiyao Yin",
+    author_email='zhiyao.yin@dlr.de',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description=("Synthesizing and fitting coherent anti-Stokes Raman spectra "
+                 "(CARS) in Python"),
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='carspy',
+    name='carspy',
+    packages=find_packages(include=['carspy', 'carspy.*']),
+    package_data={'carspy': ['_constants/*.json']},
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/chuckedfromspace/carspy',
+    version='0.4.2',
+    zip_safe=False,
+)
