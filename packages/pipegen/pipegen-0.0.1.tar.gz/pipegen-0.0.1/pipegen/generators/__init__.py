6dd7ed8a7f58a7e7b8368fe05a06db1659432ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:05 +0000

    Add package pipegen/0.0.1/pipegen-0.0.1.tar.gz

---
+++ packages/pipegen/pipegen-0.0.1.tar.gz/pipegen-0.0.1/pipegen/generators/__init__.py
@@ -0,0 +1,32 @@
+from . import codebuild, codepipeline, iam
+
+
+def generate(config):
+    """Generate all config elements"""
+
+    resources = {}
+
+    definition, codebuild_role_logical_name = iam.codebuild_role(config)
+    resources.update(definition)
+
+    codebuild_projects = codebuild.get_codebuild_projects(config)
+    codebuild_logical_ids = []
+    for codebuild_project in codebuild_projects:
+        definition, codebuild_project_logical_name = codebuild.project(
+            codebuild_project,
+            config.get("config", {}),
+            codebuild_role_logical_name,
+        )
+
+        resources.update(definition)
+        codebuild_logical_ids.append(codebuild_project_logical_name)
+
+    definition, codepipeline_role_logical_name = iam.codepipeline_role(
+        config, codebuild_logical_ids
+    )
+    resources.update(definition)
+
+    definition, _ = codepipeline.pipeline(config, codepipeline_role_logical_name)
+    resources.update(definition)
+
+    return resources
