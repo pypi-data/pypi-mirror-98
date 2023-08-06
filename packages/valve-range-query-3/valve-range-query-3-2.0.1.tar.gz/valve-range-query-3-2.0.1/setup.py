b18e4d913706a115ccd786933d6b868b75f636d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:31 +0000

    Add package valve-range-query-3/2.0.1/valve-range-query-3-2.0.1.tar.gz

---
+++ packages/valve-range-query-3/valve-range-query-3-2.0.1.tar.gz/valve-range-query-3-2.0.1/setup.py
@@ -0,0 +1,52 @@
+"""
+Python library for Query of Valve Servers over a range of IPs
+
+"""
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='valve-range-query-3',
+
+	version='2.0.1',
+
+	description='Python library for Query of Valve Servers over a range of IPs',
+	long_description=long_description,
+
+	# The project's main homepage.
+	url='https://github.com/incontestableness/valve-range-query-3',
+
+	# Author details
+	author='incontestableness',
+	author_email='whyistherumg0ne@protonmail.com',
+
+	packages = ['valverangequery'],
+
+
+	license='MIT',
+
+	classifiers=[
+		'Development Status :: 4 - Beta',
+
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 3.7',
+	],
+
+	keywords='valve query servers counter strike csgo team fortress tf2 left for dead l4d2'
+
+)
