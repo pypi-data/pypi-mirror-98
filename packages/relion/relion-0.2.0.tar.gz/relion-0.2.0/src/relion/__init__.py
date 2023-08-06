f6a7f34952b94d06d29cc2264242f94d31d22bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:51 +0000

    Add package relion/0.2.0/relion-0.2.0.tar.gz

---
+++ packages/relion/relion-0.2.0.tar.gz/relion-0.2.0/src/relion/__init__.py
@@ -0,0 +1,80 @@
+"""
+Relion Python API
+https://github.com/DiamondLightSource/python-relion
+"""
+
+import functools
+import pathlib
+from relion._parser.ctffind import CTFFind
+from relion._parser.motioncorrection import MotionCorr
+from relion._parser.class2D import Class2D
+from relion._parser.class3D import Class3D
+
+__all__ = []
+__author__ = "Diamond Light Source - Scientific Software"
+__email__ = "scientificsoftware@diamond.ac.uk"
+__version__ = "0.2.0"
+__version_tuple__ = tuple(int(x) for x in __version__.split("."))
+
+
+class Project:
+    """
+    Reads information from a Relion project directory and makes it available in
+    a structured, object-oriented, and pythonic fashion.
+    """
+
+    def __init__(self, path):
+        """
+        Create an object representing a Relion project.
+        :param path: A string or file system path object pointing to the root
+                     directory of an existing Relion project.
+        """
+        self.basepath = pathlib.Path(path)
+        if not self.basepath.is_dir():
+            raise ValueError(f"path {self.basepath} is not a directory")
+
+    def __eq__(self, other):
+        if isinstance(other, Project):
+            return self.basepath == other.basepath
+        return False
+
+    def __hash__(self):
+        return hash(("relion.Project", self.basepath))
+
+    def __repr__(self):
+        return f"relion.Project({repr(str(self.basepath))})"
+
+    def __str__(self):
+        return f"<relion.Project at {self.basepath}>"
+
+    @property
+    @functools.lru_cache(maxsize=1)
+    def ctffind(self):
+        """access the CTFFind stage of the project.
+        Returns a dictionary-like object with job names as keys,
+        and lists of CTFMicrograph namedtuples as values."""
+        return CTFFind(self.basepath / "CtfFind")
+
+    @property
+    @functools.lru_cache(maxsize=1)
+    def motioncorrection(self):
+        """access the motion correction stage of the project.
+        Returns a dictionary-like object with job names as keys,
+        and lists of MCMicrograph namedtuples as values."""
+        return MotionCorr(self.basepath / "MotionCorr")
+
+    @property
+    @functools.lru_cache(maxsize=1)
+    def class2D(self):
+        """access the 2D classification stage of the project.
+        Returns a dictionary-like object with job names as keys,
+        and lists of Class2DParticleClass namedtuples as values."""
+        return Class2D(self.basepath / "Class2D")
+
+    @property
+    @functools.lru_cache(maxsize=1)
+    def class3D(self):
+        """access the 3D classification stage of the project.
+        Returns a dictionary-like object with job names as keys,
+        and lists of Class3DParticleClass namedtuples as values."""
+        return Class3D(self.basepath / "Class3D")
---
+++ packages/relion/relion-0.2.0.tar.gz/relion-0.2.0/src/relion/_parser/class3D.py
@@ -0,0 +1,174 @@
+import collections.abc
+from gemmi import cif
+import os
+import functools
+from collections import namedtuple
+from collections import Counter
+
+
+Class3DParticleClass = namedtuple(
+    "Class3DParticleClass",
+    [
+        "particle_sum",
+        "reference_image",
+        "class_distribution",
+        "accuracy_rotations",
+        "accuracy_translations_angst",
+        "estimated_resolution",
+        "overall_fourier_completeness",
+    ],
+)
+
+Class3DParticleClass.__doc__ = "3D Classification stage."
+Class3DParticleClass.particle_sum.__doc__ = "Sum of all particles in the class. Gives a tuple with the class number first, then the particle sum."
+Class3DParticleClass.reference_image.__doc__ = "Reference image."
+Class3DParticleClass.class_distribution.__doc__ = (
+    "Class Distribution. Proportional to the number of particles per class."
+)
+Class3DParticleClass.accuracy_rotations.__doc__ = "Accuracy rotations."
+Class3DParticleClass.accuracy_translations_angst.__doc__ = (
+    "Accuracy translations angst."
+)
+Class3DParticleClass.estimated_resolution.__doc__ = "Estimated resolution."
+Class3DParticleClass.overall_fourier_completeness.__doc__ = (
+    "Overall Fourier completeness."
+)
+
+
+class Class3D(collections.abc.Mapping):
+    def __eq__(self, other):
+        if isinstance(other, Class3D):
+            return self._basepath == other._basepath
+        return False
+
+    def __hash__(self):
+        return hash(("relion._parser.Class3D", self._basepath))
+
+    def __init__(self, path):
+        self._basepath = path
+        self._jobcache = {}
+
+    def __iter__(self):
+        return iter(self.jobs)
+
+    def __len__(self):
+        return len(self.jobs)
+
+    def __repr__(self):
+        return f"Class3D({repr(str(self._basepath))})"
+
+    def __str__(self):
+        return f"<Class3D parser at {self._basepath}>"
+
+    @property
+    def jobs(self):
+        return sorted(
+            d.stem
+            for d in self._basepath.iterdir()
+            if d.is_dir() and not d.is_symlink()
+        )
+
+    def __getitem__(self, key):
+        if not isinstance(key, str):
+            raise KeyError(f"Invalid argument {key!r}, expected string")
+        if key not in self._jobcache:
+            job_path = self._basepath / key
+            if not job_path.is_dir():
+                raise KeyError(
+                    f"no job directory present for {key} in {self._basepath}"
+                )
+            self._jobcache[key] = self._load_job_directory(key)
+        return self._jobcache[key]
+
+    @property
+    def job_number(self):
+        jobs = sorted(x.name for x in self._basepath.iterdir())
+        return jobs
+
+    def _load_job_directory(self, jobdir):
+
+        dfile, mfile = self._final_data_and_model(self._basepath / jobdir)
+
+        sdfile = self._read_star_file(jobdir, dfile)
+        smfile = self._read_star_file(jobdir, mfile)
+
+        class_distribution = self.parse_star_file("_rlnClassDistribution", smfile, 1)
+        accuracy_rotations = self.parse_star_file("_rlnAccuracyRotations", smfile, 1)
+        accuracy_translations_angst = self.parse_star_file(
+            "_rlnAccuracyTranslationsAngst", smfile, 1
+        )
+        estimated_resolution = self.parse_star_file(
+            "_rlnEstimatedResolution", smfile, 1
+        )
+        overall_fourier_completeness = self.parse_star_file(
+            "_rlnOverallFourierCompleteness", smfile, 1
+        )
+        reference_image = self.parse_star_file("_rlnReferenceImage", smfile, 1)
+
+        class_numbers = self.parse_star_file("_rlnClassNumber", sdfile, 1)
+        particle_sum = self._sum_all_particles(class_numbers)
+        int_particle_sum = [(int(name), value) for name, value in particle_sum.items()]
+        checked_particle_list = self._class_checker(
+            sorted(int_particle_sum), len(reference_image)
+        )
+
+        particle_class_list = []
+        for j in range(len(reference_image)):
+            particle_class_list.append(
+                Class3DParticleClass(
+                    checked_particle_list[j],
+                    reference_image[j],
+                    float(class_distribution[j]),
+                    accuracy_rotations[j],
+                    accuracy_translations_angst[j],
+                    estimated_resolution[j],
+                    overall_fourier_completeness[j],
+                )
+            )
+        return particle_class_list
+
+    def _final_data_and_model(self, job_path):
+        number_list = [entry.stem[6:9] for entry in job_path.glob("run_it*.star")]
+        last_iteration_number = max(
+            (int(n) for n in number_list if n.isnumeric()), default=0
+        )
+        if not last_iteration_number:
+            raise ValueError(f"No result files found in {job_path}")
+        data_file = job_path / f"run_it{last_iteration_number:03d}_data.star"
+        model_file = job_path / f"run_it{last_iteration_number:03d}_model.star"
+        for check_file in (data_file, model_file):
+            if not check_file.exists():
+                raise ValueError(f"File {check_file} missing from job directory")
+        return data_file, model_file
+
+    @functools.lru_cache(maxsize=None)
+    def _read_star_file(self, job_num, file_name):
+        full_path = self._basepath / job_num / file_name
+        gemmi_readable_path = os.fspath(full_path)
+        star_doc = cif.read_file(gemmi_readable_path)
+        return star_doc
+
+    def parse_star_file(self, loop_name, star_doc, block_number):
+        data_block = star_doc[block_number]
+        values = data_block.find_loop(loop_name)
+        values_list = list(values)
+        if not values_list:
+            print("Warning - no values found for", loop_name)
+        return values_list
+
+    def _class_checker(
+        self, tuple_list, length
+    ):  # Makes sure every class has a number of associated particles
+        for i in range(1, length):
+            if i not in tuple_list[i - 1]:
+                tuple_list.insert(i - 1, (i, 0))
+                print("No values found for class", i)
+        return tuple_list
+
+    def _count_all(self, list):
+        count = Counter(list)
+        return count
+
+    def _sum_all_particles(self, list):
+        counted = self._count_all(list)
+        return counted
