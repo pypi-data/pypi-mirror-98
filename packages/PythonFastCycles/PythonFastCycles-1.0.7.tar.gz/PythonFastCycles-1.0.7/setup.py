8b96aa24f561e0a0426e3e695cb71360fa563212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:27 +0000

    Add package PythonFastCycles/1.0.7/PythonFastCycles-1.0.7.tar.gz

---
+++ packages/PythonFastCycles/PythonFastCycles-1.0.7.tar.gz/PythonFastCycles-1.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PythonFastCycles", 
+    version="1.0.7",
+    author="Roxane Ferry",
+    author_email='roxane.ferry@ens.fr',
+    description="Package to interact with FastCycles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rferry/PythonFastCycles",
+    keywords="fast cycles earthquake romanet",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'': ['img/*.png']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
