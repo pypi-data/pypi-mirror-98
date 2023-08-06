b802f23c652a2537679e665ba0f23360f384788b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:24 +0000

    Add package gpboost/0.5.0/gpboost-0.5.0-py3-none-manylinux1_x86_64.whl

---
+++ packages/gpboost/gpboost-0.5.0-py3-none-manylinux1_x86_64.whl/gpboost/__init__.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+""" GPBoost
+ Original work Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+ Modified work Copyright (c) 2020 Fabio Sigrist. All rights reserved.
+"""
+from .basic import Booster, Dataset, register_logger, GPModel
+from .callback import (early_stopping, print_evaluation, record_evaluation,
+                       reset_parameter)
+from .engine import cv, train, CVBooster, grid_search_tune_parameters
+
+import os
+
+try:
+    from .sklearn import GPBoostModel, GPBoostRegressor, GPBoostClassifier, GPBoostRanker
+except ImportError:
+    pass
+try:
+    from .plotting import (plot_importance, plot_split_value_histogram, plot_metric,
+                           plot_tree, create_tree_digraph)
+except ImportError:
+    pass
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+if os.path.isfile(os.path.join(dir_path, 'VERSION.txt')):
+    with open(os.path.join(dir_path, 'VERSION.txt')) as version_file:
+        __version__ = version_file.read().strip()
+
+__all__ = ['Dataset', 'Booster', 'CVBooster',
+           'register_logger',
+           'train', 'cv', 'grid_search_tune_parameters',
+           'GPBoostModel', 'GPBoostRegressor', 'GPBoostClassifier', 'GPBoostRanker',
+           'print_evaluation', 'record_evaluation', 'reset_parameter', 'early_stopping',
+           'plot_importance', 'plot_split_value_histogram', 'plot_metric', 'plot_tree', 'create_tree_digraph']
