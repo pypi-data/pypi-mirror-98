1a45cb308e81a0321f517a0cdfbc678d5aad1a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:14 +0000

    Add package vice/1.2.0/vice-1.2.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/vice/vice-1.2.0-cp39-cp39-macosx_10_9_x86_64.whl/vice/toolkit/__init__.py
@@ -0,0 +1,48 @@
+r""" 
+VICE Toolkit : General utilities to maximize VICE's computational power and 
+user-friendliness. 
+
+.. versionadded:: 1.2.0 
+
+Contents 
+--------
+hydrodisk : <module> 
+	Utilities for simulating migration in disk galaxies. 
+interpolation : <module> 
+	Interpolation schema. 
+J21_sf_law : <module> 
+	The observationally motivated star formation law from Johnson et al. (2021) 
+	[1]_, intended for use as the attribute ``tau_star`` of the ``singlezone`` 
+	class. 
+
+.. [1] Johnson et al. (2021), in prep 
+""" 
+
+from __future__ import absolute_import 
+try: 
+	__VICE_SETUP__ 
+except NameError: 
+	__VICE_SETUP__ = False 
+
+if not __VICE_SETUP__: 
+
+	__all__ = ["hydrodisk", "interpolation", "J21_sf_law", "test"] 
+	from ..testing import moduletest 
+	from .J21_sf_law import J21_sf_law 
+	from . import interpolation 
+	from . import hydrodisk 
+
+	@moduletest 
+	def test(): 
+		r""" 
+		vice.toolkit module test 
+		""" 
+		return ["vice.toolkit", 
+			[
+				hydrodisk.test(run = False), 
+				interpolation.test(run = False) 
+			] 
+		] 
+
+else: 
+	pass 
