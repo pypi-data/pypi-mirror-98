f17e273a684e18cf06097f7f3e8a9278ba6018a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:20 +0000

    Add package teo-files/0.2/teo-files-0.2.tar.gz

---
+++ packages/teo-files/teo-files-0.2.tar.gz/teo-files-0.2/README.md
@@ -0,0 +1,32 @@
+# NOTE: THIS REPOSITORY IS NOT FINISHED AND NOT HAVE A RELEASE
+
+# What is this ? 🧐
+
+![File Image](https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Text-x-generic.svg/1024px-Text-x-generic.svg.png)
+
+A library for make many operations for files Move Delete Name Filter ETC
+
+## Instalation 
+
+![Python Logo](https://1000logos.net/wp-content/uploads/2020/08/Python-Logo.png)
+
+`pip install teo-files`
+
+`py -m pip install teo-files`
+
+## Contribute
+
+1.- Make a fork
+
+2.- Use the dev branch
+
+3.- Write the changelog
+
+4.- Make the pull request with the description
+
+5.- Accepted the pull request and add New Release
+
+## Credits
+
+Made with :heart: in Ecuador by TeoDev1611
+
---
+++ packages/teo-files/teo-files-0.2.tar.gz/teo-files-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+
+with open('./README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+DESCRIPTION = 'MAKE FILE OPERATIONS WITH PYTHON'
+VERSION = '0.2'
+setup(
+    name="teo-files",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    author="Mateo Hurtado",
+    author_email='teodev1611@gmail.com', 
+    license="MIT",
+    url="https://github.com/TeoDev1611/teo-files",
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    packages=find_packages(),
+    keywords=['python', 'file', 'system', 'copy', 'delete', 'os', 'file', 'py-files'],
+        classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
