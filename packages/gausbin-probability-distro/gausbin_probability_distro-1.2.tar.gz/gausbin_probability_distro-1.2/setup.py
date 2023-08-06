0e7362611d86837dac6353345366d891b4d38b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package gausbin-probability-distro/1.2/gausbin_probability_distro-1.2.tar.gz

---
+++ packages/gausbin-probability-distro/gausbin_probability_distro-1.2.tar.gz/gausbin_probability_distro-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='gausbin_probability_distro',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Doylee',
+      author_email='myemail@something.com',
+      zip_safe=False)
