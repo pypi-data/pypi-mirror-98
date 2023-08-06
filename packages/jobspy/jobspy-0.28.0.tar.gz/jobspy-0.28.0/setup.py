afd762508d4185f0d49f4976ff96ce74975c267a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:20 +0000

    Add package jobspy/0.28.0/jobspy-0.28.0.tar.gz

---
+++ packages/jobspy/jobspy-0.28.0.tar.gz/jobspy-0.28.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+#from distutils.core import setup
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = ''
+
+try:
+    version = open('VERSION').read()
+except:
+    VERSION = '??'
+
+setup(
+    name='jobspy',
+    version=version,
+    description='Use Redis as a job input/output coordinator.',
+    author='Josiah Carlson',
+    author_email='josiah.carlson@gmail.com',
+    url='https://github.com/josiahcarlson/jobs',
+    py_modules=['jobs'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    license='GNU LGPL v2.1',
+    long_description=long_description,
+    install_requires=['redis>=3.0.0'],
+)
