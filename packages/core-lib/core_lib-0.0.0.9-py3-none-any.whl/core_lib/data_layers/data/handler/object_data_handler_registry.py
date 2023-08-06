238a78e08162ce96a2748eb64d866258631a1ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:47 +0000

    Add package core-lib/0.0.0.9/core_lib-0.0.0.9-py3-none-any.whl

---
+++ packages/core-lib/core_lib-0.0.0.9-py3-none-any.whl/core_lib/data_layers/data/handler/object_data_handler_registry.py
@@ -0,0 +1,17 @@
+from core_lib.data_layers.data.handler.data_handler_registry import DataHandlerRegistry
+from core_lib.data_layers.data.handler.object_data_handler import ObjectDataHandler
+
+
+class ObjectDataHandlerRegistry(DataHandlerRegistry):
+
+    def __init__(self, obj, new_session_callback=None, close_session_callback=None):
+        self.obj = obj
+        self.new_session_callback = new_session_callback
+        self.close_session_callback = close_session_callback
+
+    def get(self, *args, **kwargs):
+        obj = self.obj
+        if self.new_session_callback:
+            obj = self.new_session_callback(self.obj)
+        return ObjectDataHandler(obj, self.close_session_callback)
+
