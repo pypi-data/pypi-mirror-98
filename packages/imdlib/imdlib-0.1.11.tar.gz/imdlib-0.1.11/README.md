cb7570f7cc533e36a0cf57bb5bfaa7b0bbe6c871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:25 +0000

    Add package imdlib/0.1.11/imdlib-0.1.11.tar.gz

---
+++ packages/imdlib/imdlib-0.1.11.tar.gz/imdlib-0.1.11/README.md
@@ -0,0 +1,40 @@
+# imdlib
+
+[![Build Status](https://travis-ci.org/iamsaswata/imdlib.svg?branch=master)](https://travis-ci.org/iamsaswata/imdlib)
+![GitHub](https://img.shields.io/github/license/iamsaswata/imdlib)
+![PyPI](https://img.shields.io/pypi/v/imdlib)
+![Conda](https://img.shields.io/conda/v/iamsaswata/imdlib)
+![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/iamsaswata/imdlib)  
+[![Downloads](https://pepy.tech/badge/imdlib)](https://pepy.tech/project/imdlib)
+
+
+This is a python package to download and handle binary grided data from Indian Meterological department (IMD).
+
+## Installation
+
+> pip install imdlib
+ 
+ or
+
+> conda install -c iamsaswata imdlib
+
+or 
+
+> pip install git+https://github.com/iamsaswata/imdlib.git
+
+
+## Documentation
+
+[Tutorial](https://saswatanandi.github.io/softwares/imdlib)
+[Tutorial](https://pratiman-91.github.io/blog.html)
+
+## License
+
+imdlib is available under the open source [MIT](https://opensource.org/licenses/MIT).
+
+## Citation
+
+If you are using imdlib and would like to cite it in academic publication, we would certainly appreciate it. We recommend to use the zenodo DOI for this purpose:
+
+
+[![DOI](https://zenodo.org/badge/235463327.svg)](https://zenodo.org/badge/latestdoi/235463327)
---
+++ packages/imdlib/imdlib-0.1.11.tar.gz/imdlib-0.1.11/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:copyright: (c) 2020 by Saswata Nandi
+:license: MIT, see LICENSE for more details.
+"""
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+# imdlib version
+VERSION = "0.1.11"
+
+
+def readme():
+  """print long description"""
+  with open('README.md') as f:
+    return f.read()
+
+
+setup(
+    name="imdlib",
+    version="0.1.11",
+    author="Saswata Nandi",
+    author_email="iamsaswata@yahoo.com",
+    description="A tool for handling and downloading IMD gridded data",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamsaswata/",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Development Status :: 4 - Beta",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Topic :: Scientific/Engineering :: Hydrology",
+    ],
+    python_requires='>=3.0',
+
+    keywords='imd, India, rainfall, data, hydrology, IMD, grid, grided, gridded',
+    # packages=['':'cct_nn'],
+    install_requires=['matplotlib',
+                      'numpy',
+                      'pandas',
+                      'six',
+                      'pandas',
+                      'python-dateutil',
+                      'pytz',
+                      'urllib3',
+                      'scipy',
+                      'xarray',
+                      'requests', ]
+)
