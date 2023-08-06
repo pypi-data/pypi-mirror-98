62529326aac936a051d2a69d20653f888411cf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:23 +0000

    Add package congo-lab/0.6/congo-lab-0.6.tar.gz

---
+++ packages/congo-lab/congo-lab-0.6.tar.gz/congo-lab-0.6/README.md
@@ -0,0 +1,7 @@
+# ConGo Lab
+
+A fork of the Octopus project [LINK](https://github.com/richardingham/octopus) stripped down to only the octopus python library.
+
+## Documentation
+
+[Legacy documentation](doc/Octopus%20Documentation.md)
---
+++ packages/congo-lab/congo-lab-0.6.tar.gz/congo-lab-0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="congo-lab",
+    version="0.6",
+    author="Richard Ingham, Gary Short",
+    description="Real-time laboratory automation and monitoring in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gar-syn/congo-lab",
+    packages=find_packages(where='src', exclude=['*.test']),
+    package_dir={'': 'src'},
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
