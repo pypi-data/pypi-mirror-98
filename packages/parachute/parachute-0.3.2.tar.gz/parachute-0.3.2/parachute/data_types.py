86e6e8728b5b5afd8bb2757f084a59987def028e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:10 +0000

    Add package parachute/0.3.2/parachute-0.3.2.tar.gz

---
+++ packages/parachute/parachute-0.3.2.tar.gz/parachute-0.3.2/parachute/data_types.py
@@ -0,0 +1,98 @@
+import re
+from dataclasses import dataclass
+from dataclasses import field
+from datetime import datetime
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+
+
+@dataclass(order=True)
+class Parameter:
+    """Represent a MAVLink parameter."""
+
+    name: str
+    value: Any = None
+    index: int = 0
+    type: int = 10
+
+    def __post_init__(self):
+        # Convert the value according to the type:
+        # https://mavlink.io/en/messages/common.html#MAV_PARAM_TYPE
+        if self.value is not None:
+            if self.is_float:
+                self.value = float(self.value)
+            else:
+                self.value = int(self.value)
+        self.name = self.name.upper()
+
+    @classmethod
+    def from_message(cls, message) -> "Parameter":
+        return Parameter(
+            name=message["param_id"],
+            value=message["param_value"],
+            index=message["param_index"],
+            type=message["param_type"],
+        )
+
+    @property
+    def is_float(self):
+        """Return whether the value of this parameter is a float."""
+        return self.type in (9, 10)
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {
+            "value": self.value,
+            "index": self.index,
+            "type": self.type,
+        }
+
+
+@dataclass()
+class Backup:
+    """Represent an entire backup file."""
+
+    status: List[str] = field(default_factory=list)
+    schema: int = 1
+    timestamp: datetime = field(default_factory=datetime.now)
+    parameters: Dict[str, Parameter] = field(default_factory=dict)
+
+    @property
+    def status_str(self) -> str:
+        """Construct a status comment string from a list of status messages."""
+        if not self.status:
+            return ""
+        return "// " + "\n// ".join(self.status) + "\n\n"
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "Backup":
+        d["timestamp"] = datetime.fromisoformat(d["timestamp"])
+        d["parameters"] = {
+            name: Parameter(name=name, **data) for name, data in d["parameters"].items()
+        }
+        backup = cls(**d)
+        return backup
+
+    def as_dict(self) -> Dict[str, Any]:
+        return {
+            "schema": self.schema,
+            "timestamp": self.timestamp.isoformat(),
+            "status": self.status,
+            "parameters": {
+                name: param.as_dict() for name, param in sorted(self.parameters.items())
+            },
+        }
+
+    def filter(self, regex: Optional[re.Pattern]) -> None:
+        """
+        Filter parameters based on a regex.
+
+        Modifies the backup in-place.
+        """
+        if not regex:
+            return
+
+        self.parameters = {
+            name: param for name, param in self.parameters.items() if regex.search(name)
+        }
---
+++ packages/parachute/parachute-0.3.2.tar.gz/parachute-0.3.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['parachute']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0',
+ 'json5>=0.9.5,<0.10.0',
+ 'pymavlink>=2.4.14,<3.0.0',
+ 'pyserial>=3.5,<4.0']
+
+entry_points = \
+{'console_scripts': ['parachute = parachute.cli:cli']}
+
+setup_kwargs = {
+    'name': 'parachute',
+    'version': '0.3.2',
+    'description': 'A lifeline for ArduPilot craft.',
+    'long_description': 'Parachute\n=========\n\nParachute is a swiss army knife for ArduPilot settings. It helps you quickly and\neasily back up all your parameters to a file (and restore them). It also lets\nyou get/set them, filter them, diff them, restore them or convert them to\nparameter files compatible with Mission Planner/QGroundControl.\n\n\nInstallation\n------------\n\nInstalling Parachute is simple. You can use `pipx` (recommended):\n\n```\n$ pipx install parachute\n```\n\nOr `pip` (less recommended):\n\n```\n$ pip install parachute\n```\n\n\nUsage\n-----\n\nParachute is called like so:\n\n```\n$ parachute backup <craft name>\n```\n\nFor example:\n\n```\n$ parachute backup Mini-Drak\n```\n\nTo restore:\n\n```\n$ parachute restore backup.chute\n```\n\n\nConversion\n----------\n\nYou can also convert a Parachute file to a file compatible with Mission Planner or QGroundControl:\n\n```\n$ parachute convert qgc Mini-Drak_2021-03-02_02-29.chute Mini-Drak.params\n```\n\n\nFiltering\n---------\n\nYou can filter parameters based on a regular expression:\n\n```\n$ parachute filter "serial[123]_" Mini-Drak_2021-03-02_02-29.chute filtered.chute\n```\n\nSince all parameter names are uppercase, the regex is case-insensitive, for convenience.\n\nYou can also filter when converting:\n\n```\n$ parachute convert --filter=yaw mp Mini-Drak_2021-03-02_02-29.chute -\n```\n\n\nComparing\n---------\n\nYou can compare parameters in a backup with parameters on the craft:\n\n```\n$ parachute compare backup.chute\n```\n\n\nGetting/setting\n---------------\n\nYou can get and set parameters:\n\n```\n$ parachute get BATT_AMP_OFFSET BATT_AMP_PERVLT\n```\n\n```\n$ parachute set BATT_AMP_OFFSET=-0.0135 BATT_AMP_PERVLT=63.8826\n```\n',
+    'author': 'Stavros Korokithakis',
+    'author_email': 'hi@stavros.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/stavros/parachute',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
