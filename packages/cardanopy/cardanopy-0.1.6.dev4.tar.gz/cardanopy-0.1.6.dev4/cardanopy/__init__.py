04e93243faefd8c75b36faf95651377e4a07b084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:21 +0000

    Add package cardanopy/0.1.6.dev4/cardanopy-0.1.6.dev4.tar.gz

---
+++ packages/cardanopy/cardanopy-0.1.6.dev4.tar.gz/cardanopy-0.1.6.dev4/cardanopy/__init__.py
@@ -0,0 +1,30 @@
+__version__ = "0.1.6-dev4"
+__license__ = "Apache-2.0 License"
+__title__ = "cardanopy"
+
+import click
+
+from .run import run
+from .create import create
+from .cli import cli
+from .docker import docker
+from .k8s import k8s
+from .generate import generate_cmd
+# from .config import config
+
+@click.group()
+@click.version_option(version=__version__)
+@click.pass_context
+def main(ctx):
+    pass
+
+
+# export
+main.add_command(run)
+main.add_command(create)
+main.add_command(cli)
+main.add_command(docker)
+main.add_command(k8s)
+main.add_command(generate_cmd)
+# disable config cli for now (might remove)
+# main.add_command(config)
