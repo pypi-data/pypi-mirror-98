25e71aadb525297dfc45674673fa3863f1bf0e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:55 +0000

    Add package jiujit-probability/0.2/jiujit_probability-0.2.tar.gz

---
+++ packages/jiujit-probability/jiujit_probability-0.2.tar.gz/jiujit_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jiujit_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['jiujit_probability'],
+      author = 'Michael Ledesma',
+      author_email = 'mxledesma@gmail.com',
+      zip_safe=False)
