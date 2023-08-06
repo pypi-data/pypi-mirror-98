62972f11d5c5861e83974095a9c5992907a6dc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:14 +0000

    Add package MatDisp/0.0.3/MatDisp-0.0.3.tar.gz

---
+++ packages/MatDisp/MatDisp-0.0.3.tar.gz/MatDisp-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='MatDisp',
+    version='0.0.3',
+    
+    packages=find_packages(),
+
+    license="GPL Licence",
+
+    author='Algebra-FUN',
+    author_email="g.creator.fan@gmail.com"
+)
