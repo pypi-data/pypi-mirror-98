b9e195bb0f9b3c170c74e8216ad0728a848aa60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:28 +0000

    Add package bebi103/0.1.8/bebi103-0.1.8-py2.py3-none-any.whl

---
+++ packages/bebi103/bebi103-0.1.8-py2.py3-none-any.whl/bebi103/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bebi103."""
+
+# Force showing deprecation warnings.
+import re
+import warnings
+
+warnings.filterwarnings(
+    "always", category=DeprecationWarning, module="^{}\.".format(re.escape(__name__))
+)
+
+try:
+    import multiprocess
+except:
+    warnings.warn(
+        "Unable to import multiprocess. Using multiprocessing (note the"
+        " ing) instead. Depending on your operating system, Python"
+        " version, and whether or not you are running in Jupyter, "
+        " IPython, the Python REPL, etc., your execution may stall if"
+        " you try to run jobs on more than one core. See discussion"
+        " here: https://github.com/ipython/ipython/issues/12396. As a"
+        " workaround, you can install multiprocess"
+        " (pip install multiprocess) and everything should work as"
+        " expected.",
+        ImportWarning,
+    )
+
+from . import hv
+
+from . import viz
+
+from . import image
+
+from . import bootstrap
+
+from . import gp
+
+try:
+    from . import stan
+except:
+    warnings.warn(
+        "Could not import `stan` submodule. Perhaps ArviZ or PyStan or CmdStanPy is/are"
+        " not properly installed."
+    )
+
+
+__author__ = """Justin Bois"""
+__email__ = "bois@caltech.edu"
+__version__ = "0.1.8"
