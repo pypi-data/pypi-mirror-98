9b685db335012d646c3b05852f6aad38a7bb51d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:59 +0000

    Add package odahu-flow-mlflow-runner/1.5.0rc3/odahu-flow-mlflow-runner-1.5.0rc3.tar.gz

---
+++ packages/odahu-flow-mlflow-runner/odahu-flow-mlflow-runner-1.5.0rc3.tar.gz/odahu-flow-mlflow-runner-1.5.0rc3/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+#
+#    Copyright 2019 EPAM Systems
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+import os
+import re
+
+from setuptools import setup, find_namespace_packages
+
+PACKAGE_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+VERSION_FILE = os.path.join(PACKAGE_ROOT_PATH, 'version.py')
+
+
+def extract_version() -> str:
+    """
+    Extract version from .py file using regex
+
+    :return: Odahuflow version
+    """
+    with open(VERSION_FILE, 'rt') as version_file:
+        file_content = version_file.read()
+        VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        mo = re.search(VSRE, file_content, re.M)
+        if mo:
+            return mo.group(1)
+        else:
+            raise RuntimeError("Unable to find version string in %s." % (file_content,))
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='odahu-flow-mlflow-runner',
+    author='Vlad Tokarev, Vitalik Solodilov',
+    author_email='vlad.tokarev.94@gmail.com, mcdkr@yandex.ru',
+    license='Apache v2',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='mlflow odahu-flow',
+    python_requires='>=3.6',
+    packages=find_namespace_packages(),
+    data_files=[('', ["README.md"])],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'odahu-flow-mlflow-runner=odahuflow.trainer.mlflow_full.runner:main',
+            'odahu-flow-mlflow-project-runner=odahuflow.trainer.mlflow_projects.runner:main',
+            'odahu-flow-mlflow-wrapper=odahuflow.trainer.helpers.wrapper.wrapper:main',
+            'odahu-flow-mlflow-gppi-converter=odahuflow.trainer.helpers.mlflow_helper:mlflow_to_gppi_cli',
+        ],
+    },
+    install_requires=requirements,
+    extras_require={
+        'testing': [
+            'pytest>=5.1.2',
+            'pytest-mock>=1.10.4',
+            'pytest-cov>=2.7.1',
+            'pylint>=2.3.0'
+        ]
+    },
+    version=extract_version()
+)
