d80f42ae97a2cf61293cdffc675dab6ef61e9645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:47 +0000

    Add package torchnmf/0.3.3/torchnmf-0.3.3.tar.gz

---
+++ packages/torchnmf/torchnmf-0.3.3.tar.gz/torchnmf-0.3.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from torchnmf import __version__, __email__, name, __maintainer__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=name,
+    version=__version__,
+    author=__maintainer__,
+    author_email=__email__,
+    description="A pytorch package for Non-negative Matrix Factorization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yoyololicon/pytorch-NMFs",
+    packages=setuptools.find_packages(),
+    install_requires=['torch>=1.4',
+                      'tqdm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
