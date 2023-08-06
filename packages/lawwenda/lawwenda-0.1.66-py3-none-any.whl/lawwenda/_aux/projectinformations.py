c406e811e7e23915168aec978850c795facd1dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:19 +0000

    Add package lawwenda/0.1.66/lawwenda-0.1.66-py3-none-any.whl

---
+++ packages/lawwenda/lawwenda-0.1.66-py3-none-any.whl/lawwenda/_aux/projectinformations.py
@@ -0,0 +1,9 @@
+# This file is automatically created by Anise in order to make some
+# information available at runtime. Do not change stuff here.
+#   Information about Anise:
+#   https://pseudopolis.eu/wiki/pino/projs/anise
+version = '0.1.64'
+buildtimeutc = '2021-03-16 20:18:01'
+summary = 'web file manager'
+homepage = 'https://pseudopolis.eu/wiki/pino/projs/lawwenda'
+license = 'AGPLv3'
