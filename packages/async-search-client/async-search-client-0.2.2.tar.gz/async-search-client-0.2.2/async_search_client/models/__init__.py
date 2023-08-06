b776f9732dcf15b5aafcec073ee8396ec5080525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:19 +0000

    Add package async-search-client/0.2.2/async-search-client-0.2.2.tar.gz

---
+++ packages/async-search-client/async-search-client-0.2.2.tar.gz/async-search-client-0.2.2/async_search_client/models/__init__.py
@@ -0,0 +1,8 @@
+from async_search_client.models.base_config import BaseConfig  # noqa: F401
+from async_search_client.models.client import ClientStats, Keys  # noqa: F401
+from async_search_client.models.dump import DumpInfo  # noqa: F401
+from async_search_client.models.index import IndexInfo, IndexStats  # noqa: F401
+from async_search_client.models.search import SearchResults  # noqa: F401
+from async_search_client.models.settings import MeiliSearchSettings  # noqa: F401
+from async_search_client.models.update import UpdateId, UpdateStatus  # noqa: F401
+from async_search_client.models.version import Version  # noqa: F401
