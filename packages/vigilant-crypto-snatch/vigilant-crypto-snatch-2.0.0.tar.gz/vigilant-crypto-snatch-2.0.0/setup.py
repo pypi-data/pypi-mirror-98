48b1b23ceedf3c4363dc4920bd1a65e95ea4b3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:51 +0000

    Add package vigilant-crypto-snatch/2.0.0/vigilant-crypto-snatch-2.0.0.tar.gz

---
+++ packages/vigilant-crypto-snatch/vigilant-crypto-snatch-2.0.0.tar.gz/vigilant-crypto-snatch-2.0.0/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['vigilant_crypto_snatch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['BitstampClient>=2.2.8,<3.0.0',
+ 'clikraken>=0.8.3,<0.9.0',
+ 'coloredlogs>=15.0,<16.0',
+ 'pyyaml>=5.4.1,<6.0.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'sqlalchemy>=1.3.23,<2.0.0',
+ 'urllib3>=1.26.3,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['vigilant-crypto-snatch = '
+                     'vigilant_crypto_snatch.cli:main']}
+
+setup_kwargs = {
+    'name': 'vigilant-crypto-snatch',
+    'version': '2.0.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Martin Ueding',
+    'author_email': 'mu@martin-ueding.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/vigilant-crypto-snatch/vigilant-crypto-snatch-2.0.0.tar.gz/vigilant-crypto-snatch-2.0.0/src/vigilant_crypto_snatch/clikraken_adaptor_api.py
@@ -0,0 +1,61 @@
+import datetime
+
+import clikraken.api.api_utils
+import clikraken.api.public.ticker
+import clikraken.api.private.place_order
+import clikraken.clikraken_utils
+import clikraken.global_vars
+
+from . import capture
+from . import datamodel
+from . import marketplace
+
+
+class KrakenArgs(object):
+    def __init__(self):
+        self.raw = False
+        self.csv = True
+
+
+class KrakenMarketplace(marketplace.Marketplace):
+    def __init__(self):
+        clikraken.clikraken_utils.load_config()
+        clikraken.api.api_utils.load_api_keyfile()
+        clikraken.global_vars.CSV_SEPARATOR = ';'
+
+    def place_order(self, coin: str, fiat: str, volume: float) -> None:
+        args = KrakenArgs()
+        args.pair = make_asset_pair(coin, fiat)
+        args.type = 'buy'
+        args.ordertype = 'market'
+        args.volume = volume
+        args.starttm = 0
+        args.expiretm = 0
+        args.leverage = 'none'
+        args.price = None
+        args.userref = None
+        args.viqc = None
+        args.validate = False
+        clikraken.api.private.place_order.place_order(args)
+
+    def get_spot_price(self, coin: str, fiat: str) -> datamodel.Price:
+        args = KrakenArgs()
+        args.pair = make_asset_pair(coin, fiat)
+        with capture.Capturing() as output:
+            clikraken.api.public.ticker.ticker(args)
+        header, data = output
+        ticker = {h: d for h, d in zip(header.split(';'), data.split(';'))}
+        now = datetime.datetime.now()
+        price = datamodel.Price(timestamp=now, last=float(ticker['last']), coin=coin, fiat=fiat)
+        return price
+
+    def get_name(self) -> str:
+        return "Kraken"
+
+
+def make_asset_pair(coin: str, fiat: str) -> str:
+    kraken_coin = coin
+    if coin == 'BTC':
+        kraken_coin = 'XBT'
+    pair = '{}{}'.format(kraken_coin, fiat)
+    return pair
