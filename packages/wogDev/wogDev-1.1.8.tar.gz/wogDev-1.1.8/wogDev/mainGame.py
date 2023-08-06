dee89066673941eb596792c0e718a59e97dd5362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package wogDev/1.1.8/wogDev-1.1.8.tar.gz

---
+++ packages/wogDev/wogDev-1.1.8.tar.gz/wogDev-1.1.8/wogDev/mainGame.py
@@ -0,0 +1,13 @@
+from utils import get_user_name, welcome, load_game, number_to_game
+from Factory import Factory
+
+
+def mainGame:
+    user_name = get_user_name()
+    welcome(user_name)
+    game, level = load_game()
+    obj_game = Factory.create_game(number_to_game[game], user_name, level, game)
+    obj_game.print_data()
+    obj_game.play
+
+#m = mainGame()
