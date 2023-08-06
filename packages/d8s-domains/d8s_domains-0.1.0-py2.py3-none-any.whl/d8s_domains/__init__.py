448bd35e509e782080ec68802926ef46b4ba6779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:25 +0000

    Add package d8s-domains/0.1.0/d8s_domains-0.1.0-py2.py3-none-any.whl

---
+++ packages/d8s-domains/d8s_domains-0.1.0-py2.py3-none-any.whl/d8s_domains/__init__.py
@@ -0,0 +1,20 @@
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+from .domains import *
+
+try:
+    __version__ = version('democritus_domains')
+except PackageNotFoundError:
+    message = (
+        'Unable to find a version number for "democritus_domains". '
+        + 'This likely means the library was not installed '
+        + 'properly. Please re-install it and, if the problem persists, '
+        + 'raise an issue here: https://github.com/democritus-project/democritus-domains/issues.'
+    )
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
