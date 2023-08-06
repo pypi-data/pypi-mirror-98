b2fe243702fba57bad15dbd1faaea18bbd070f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:18 +0000

    Add package vsmetaEncoder/0.9/vsmetaEncoder-0.9-py3-none-any.whl

---
+++ packages/vsmetaEncoder/vsmetaEncoder-0.9-py3-none-any.whl/vsmetaEncoder/__init__.py
@@ -0,0 +1,5 @@
+from .vsmetaBase import VsMetaBase
+from .vsmetaInfo import VsMetaInfo, VsMetaImageInfo
+from .vsmetaListInfo import VsMetaListInfo
+from .vsmetaMovieEncoder import VsMetaMovieEncoder
+from .vsmetaSeriesEncoder import VsMetaSeriesEncoder
---
+++ packages/vsmetaEncoder/vsmetaEncoder-0.9-py3-none-any.whl/vsmetaEncoder/vsmetaListInfo.py
@@ -0,0 +1,7 @@
+
+class VsMetaListInfo():
+    def __init__(self):
+        self.cast = {''}
+        self.genre = []
+        self.director = []
+        self.writer = []
