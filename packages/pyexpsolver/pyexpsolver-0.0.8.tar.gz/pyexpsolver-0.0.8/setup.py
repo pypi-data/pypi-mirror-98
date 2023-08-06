4fe2ecda17950196a9c17fa5cb7a2a07f1245286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:14 +0000

    Add package pyexpsolver/0.0.8/pyexpsolver-0.0.8.tar.gz

---
+++ packages/pyexpsolver/pyexpsolver-0.0.8.tar.gz/pyexpsolver-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyexpsolver",
+    version="0.0.8",
+    author="Alberto Castronovo",
+    author_email="alberto.castronovo@hotmail.it",
+    description="Automatically solve algebraic expression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albertocastronovo/pyexpsolver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
