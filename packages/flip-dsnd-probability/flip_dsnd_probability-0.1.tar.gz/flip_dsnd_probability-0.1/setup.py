3ef6325a43bab8474bfd34bbb9209ebc64481102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:14 +0000

    Add package flip-dsnd-probability/0.1/flip_dsnd_probability-0.1.tar.gz

---
+++ packages/flip-dsnd-probability/flip_dsnd_probability-0.1.tar.gz/flip_dsnd_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'flip_dsnd_probability',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['flip_dsnd_probability'],
+      author = 'Felipe Gonçalves',
+      zip_safe = False)
