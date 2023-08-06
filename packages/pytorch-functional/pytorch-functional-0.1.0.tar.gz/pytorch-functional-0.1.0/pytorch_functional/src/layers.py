5382d2007cbf525286b0cfc8395133004770ae43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:50 +0000

    Add package pytorch-functional/0.1.0/pytorch-functional-0.1.0.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.1.0.tar.gz/pytorch-functional-0.1.0/pytorch_functional/src/layers.py
@@ -0,0 +1,62 @@
+import torch
+from torch import nn
+
+
+class AddOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a + b
+
+
+class SubOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a - b
+
+
+class MulOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a * b
+
+
+class MatmulOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a @ b
+
+
+class AnyOpLayer(nn.Module):
+    def __init__(self, op):
+        super().__init__()
+        self.forward = op
+
+
+class ConcatLayer(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *elements):
+        return torch.cat(tensors=elements, dim=self.dim)
+
+
+class StackLayer(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *elements):
+        return torch.stack(tensors=elements, dim=self.dim)
---
+++ packages/pytorch-functional/pytorch-functional-0.1.0.tar.gz/pytorch-functional-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# setup(
+#    name='pytorch_functional',
+#    version='0.1.1',
+#    description='Provides functional API for model creation in PyTorch',
+#    author='gahaalt',
+#    author_email='gahaalt@gmail.com',
+#    packages=['pytorch_functional'],
+#    url='https://github.com/gahaalt/pytorch-functional-api',
+#    license='MIT',
+# )
+
+setup()
