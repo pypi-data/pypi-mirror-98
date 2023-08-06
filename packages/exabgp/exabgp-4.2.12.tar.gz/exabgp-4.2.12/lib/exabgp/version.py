5ae3a92860936dbf03fb22842ac35ba49b315890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:56 +0000

    Add package exabgp/4.2.12/exabgp-4.2.12.tar.gz

---
+++ packages/exabgp/exabgp-4.2.12.tar.gz/exabgp-4.2.12/lib/exabgp/version.py
@@ -0,0 +1,13 @@
+import os
+
+commit = "cf4309a8"
+release = "4.2.12"
+json = "4.0.1"
+text = "4.0.1"
+version = os.environ.get('EXABGP_VERSION',release)
+
+# Do not change the first line as it is parsed by scripts
+
+if __name__ == '__main__':
+    import sys
+    sys.stdout.write(version)
