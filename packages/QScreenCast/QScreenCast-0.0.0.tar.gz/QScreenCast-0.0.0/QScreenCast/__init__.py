1afacaf10480d16fdc6db0c6bee775f792f856e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:19 +0000

    Add package QScreenCast/0.0.0/QScreenCast-0.0.0.tar.gz

---
+++ packages/QScreenCast/QScreenCast-0.0.0.tar.gz/QScreenCast-0.0.0/QScreenCast/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © Tom Hören
+
+# Licensed under the terms of the MIT License
+# ----------------------------------------------------------------------------
+"""
+Python QtScreenCaster.
+"""
+from .QtScreenCast import ScreenCastToolButton
+
+__version__ = "0.0.0"
