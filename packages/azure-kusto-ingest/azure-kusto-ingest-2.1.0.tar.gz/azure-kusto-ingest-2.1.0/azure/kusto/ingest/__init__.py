7cc05055902df1d432d77dd46ae2a47fbf33b1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:49 +0000

    Add package azure-kusto-ingest/2.1.0/azure-kusto-ingest-2.1.0.tar.gz

---
+++ packages/azure-kusto-ingest/azure-kusto-ingest-2.1.0.tar.gz/azure-kusto-ingest-2.1.0/azure/kusto/ingest/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License
+from ._version import VERSION as __version__
+from .descriptors import BlobDescriptor, FileDescriptor, StreamDescriptor
+from .exceptions import KustoMissingMappingReferenceError
+from .ingest_client import QueuedIngestClient
+from .ingestion_properties import (
+    DataFormat,
+    ValidationPolicy,
+    ValidationImplications,
+    ValidationOptions,
+    ReportLevel,
+    ReportMethod,
+    IngestionProperties,
+    IngestionMappingType,
+    ColumnMapping,
+    TransformationMethod,
+)
+from .streaming_ingest_client import KustoStreamingIngestClient
