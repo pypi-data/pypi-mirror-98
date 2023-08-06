79fb5bec5e74fdfd43bfd29d3cf279d9be77390a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:42 +0000

    Add package fordfulkerson-prueba/0.0.2/fordfulkerson_prueba-0.0.2.tar.gz

---
+++ packages/fordfulkerson-prueba/fordfulkerson_prueba-0.0.2.tar.gz/fordfulkerson_prueba-0.0.2/README.txt
@@ -0,0 +1,1 @@
+Este paquete implementa el método de Ford Fulkerson para resolver el problema de redes de encontrar el flujo máximo.
---
+++ packages/fordfulkerson-prueba/fordfulkerson_prueba-0.0.2.tar.gz/fordfulkerson_prueba-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='fordfulkerson_prueba',
+  version='0.0.2',
+  description='Implementación del método Fork Fulkerson',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',
+  author='Equipo 2',
+  author_email='',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='max flow', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
