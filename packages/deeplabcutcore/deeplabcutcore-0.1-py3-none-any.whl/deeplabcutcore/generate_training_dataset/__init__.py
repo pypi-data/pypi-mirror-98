3a6ac1fc9239a3a92a911da1a8a325c84be3f24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package deeplabcutcore/0.1/deeplabcutcore-0.1-py3-none-any.whl

---
+++ packages/deeplabcutcore/deeplabcutcore-0.1-py3-none-any.whl/deeplabcutcore/generate_training_dataset/__init__.py
@@ -0,0 +1,13 @@
+"""
+deeplabcutcore2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/deeplabcut
+
+Please see AUTHORS for contributors.
+https://github.com/AlexEMG/deeplabcut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+from deeplabcutcore import DEBUG
+from deeplabcutcore.generate_training_dataset.frame_extraction import *
+from deeplabcutcore.generate_training_dataset.trainingsetmanipulation import *
