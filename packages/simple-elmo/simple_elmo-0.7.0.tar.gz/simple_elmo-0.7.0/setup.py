28433927fe726302ec4a265c50c847234e5ece68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:50 +0000

    Add package simple-elmo/0.7.0/simple_elmo-0.7.0.tar.gz

---
+++ packages/simple-elmo/simple_elmo-0.7.0.tar.gz/simple_elmo-0.7.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_elmo",
+    version="0.7.0",
+    author="Andrey Kutuzov",
+    author_email="andreku@ifi.uio.no",
+    description="Handy library to work with pre-trained ELMo embeddings in TensorFlow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ltgoslo/simple_elmo",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=["h5py", "numpy", "smart_open>1.8.1", "pandas", "scipy"],
+    extras_require={
+        "tf": ["tensorflow>=1.14.0"],
+        "tf_gpu": ["tensorflow-gpu>=1.14.0"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Utilities"
+    ],
+)
---
+++ packages/simple-elmo/simple_elmo-0.7.0.tar.gz/simple_elmo-0.7.0/simple_elmo/__init__.py
@@ -0,0 +1,5 @@
+name = "simple_elmo"
+from simple_elmo.elmo_helpers import ElmoModel, divide_chunks
+from simple_elmo.data import Batcher
+from simple_elmo.model import BidirectionalLanguageModel
+from simple_elmo.elmo import weight_layers
