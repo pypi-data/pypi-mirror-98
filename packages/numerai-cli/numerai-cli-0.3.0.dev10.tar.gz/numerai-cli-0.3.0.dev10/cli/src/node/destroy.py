33b6f9a6e091e0b81698b2abfaa3f69001ecdf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:27 +0000

    Add package numerai-cli/0.3.0.dev10/numerai-cli-0.3.0.dev10.tar.gz

---
+++ packages/numerai-cli/numerai-cli-0.3.0.dev10.tar.gz/numerai-cli-0.3.0.dev10/cli/src/node/destroy.py
@@ -0,0 +1,79 @@
+import numerapi
+
+from cli.src.constants import *
+from cli.src.util.files import \
+    format_path_if_mingw,\
+    load_or_init_nodes,\
+    store_config
+from cli.src.util.docker import terraform
+from cli.src.util.keys import get_provider_keys, get_numerai_keys
+
+
+@click.command()
+@click.option('--verbose', '-v', is_flag=True)
+@click.pass_context
+def destroy(ctx, verbose):
+    """
+    Uses Terraform to destroy Numerai Compute cluster in AWS.
+    This will delete everything, including:
+        - lambda url
+        - docker container and associated task
+        - all logs
+    This command is idempotent and safe to run multiple times.
+    """
+    ctx.ensure_object(dict)
+    node = ctx.obj['node']
+    if not os.path.exists(CONFIG_PATH):
+        click.secho(f".numerai directory not setup, "
+                    f"run 'numerai setup'...",
+                    fg='red')
+        return
+    numerai_dir = format_path_if_mingw(CONFIG_PATH)
+
+    try:
+        nodes_config = load_or_init_nodes()
+        node_config = nodes_config[node]
+        provider_keys = get_provider_keys(node)
+    except (KeyError, FileNotFoundError) as e:
+        click.secho(f"make sure you run `numerai setup` and "
+                    f"`numerai node -n {node} create` first...", fg='red')
+        return
+
+    try:
+        click.secho(f"deleting node configuration...")
+        del nodes_config[node]
+        store_config(NODES_PATH, nodes_config)
+
+        click.secho(f"deleting cloud resources for node...")
+        terraform(f'apply -auto-approve -var="node_config_file=nodes.json"',
+                  numerai_dir, verbose, env_vars=provider_keys)
+
+    except Exception as e:
+        click.secho(e.__str__(), fg='red')
+        nodes_config[node] = node_config
+        store_config(NODES_PATH, nodes_config)
+        return
+
+    if 'model_id' in node_config:
+        napi = numerapi.NumerAPI(*get_numerai_keys())
+        model_id = node_config['model_id']
+        webhook_url = node_config['webhook_url']
+        click.echo(f'deregistering webhook {webhook_url} for model {model_id}...')
+        napi.raw_query(
+            '''
+            mutation (
+                $modelId: String!
+                $newSubmissionWebhook: String!
+            ) {
+                setSubmissionWebhook(
+                    modelId: $modelId
+                    newSubmissionWebhook: $newSubmissionWebhook
+                )
+            }
+            ''',
+            variables={
+                'modelId': model_id,
+                'newSubmissionWebhook': None
+            },
+            authorization=True
+        )
---
+++ packages/numerai-cli/numerai-cli-0.3.0.dev10.tar.gz/numerai-cli-0.3.0.dev10/cli/src/upgrade.py
@@ -0,0 +1,128 @@
+from configparser import ConfigParser, MissingSectionHeaderError
+import json
+
+from cli.src.constants import *
+from cli.src.util.debug import confirm
+from cli.src.util.docker import terraform
+from cli.src.util.files import \
+    maybe_create,\
+    copy_files
+from cli.src.util.keys import \
+    load_or_init_keys, \
+    config_numerai_keys, \
+    config_provider_keys
+
+
+@click.command()
+@click.option('--verbose', '-v', is_flag=True)
+def upgrade(verbose):
+    """
+    Upgrades configuration from <=0.2 format to >=0.3 format
+    """
+    home = str(Path.home())
+    old_key_path = os.path.join(home, '.numerai')
+    old_config_path = os.path.join(os.getcwd(), '.numerai/')
+
+    if str(old_key_path) == str(old_config_path):
+        click.secho('You cannot run this command from your home directory.')
+        return
+
+    if not os.path.exists(old_config_path):
+        click.secho(
+            'Run this command from the directory in which you first ran '
+            '"numerai setup" (it should have the .numerai folder in it)'
+        )
+        return
+
+    click.secho(f"Upgrading, do not interrupt or else "
+                f"your environment may be corrupted.", fg='yellow')
+    # MOVE KEYS FILE
+    if os.path.isfile(old_key_path):
+        temp_key_path = os.path.join(old_config_path, '.keys')
+        click.secho(f"\tmoving '{old_key_path}' to '{temp_key_path}'",)
+        os.rename(old_key_path, temp_key_path)
+
+    # MOVE CONFIG FILE
+    if os.path.exists(old_config_path):
+        click.secho(f"\tmoving {old_config_path} to {CONFIG_PATH}",)
+        os.rename(old_config_path, CONFIG_PATH)
+
+    # REFORMAT OLD KEYS
+    try:
+        config = ConfigParser()
+        config.read(KEYS_PATH)
+        click.secho(f"Old keyfile format found, reformatting...", fg='yellow')
+
+        new_config = {
+            'aws': {
+                'AWS_ACCESS_KEY_ID': config['default']['AWS_ACCESS_KEY_ID'],
+                'AWS_SECRET_ACCESS_KEY': config['default']['AWS_SECRET_ACCESS_KEY']
+            },
+            'numerai': {
+                'NUMERAI_PUBLIC_ID': config['default']['NUMERAI_PUBLIC_ID'],
+                'NUMERAI_SECRET_KEY': config['default']['NUMERAI_SECRET_KEY']
+            }
+        }
+
+        del config['default']
+        with open(os.open(KEYS_PATH, os.O_CREAT | os.O_WRONLY, 0o600), 'w') as f:
+            config.write(f)
+            json.dump(new_config, f, indent=2)
+
+    # if this file is already a json file skip
+    except MissingSectionHeaderError:
+        pass
+
+    # Double check keys file exists
+    if not os.path.exists(KEYS_PATH) or 'aws' not in load_or_init_keys() or 'numerai' not in load_or_init_keys():
+        click.secho(f"Keys missing from {KEYS_PATH}, you must re-initialize your keys:")
+        config_numerai_keys()
+        config_provider_keys(PROVIDER_AWS)
+
+    # DELETE OLD CONFIG FILES
+    click.secho('Checking for old config output files...', fg='yellow')
+    old_suburl_path = os.path.join(CONFIG_PATH, 'submission_url.txt')
+    if os.path.exists(old_suburl_path):
+        click.secho(f"\tdeleting {old_suburl_path}, you can create the "
+                    f"new config file with 'numerai node create'")
+        os.remove(old_suburl_path)
+    old_docker_path = os.path.join(CONFIG_PATH, 'docker_repo.txt')
+    if os.path.exists(old_docker_path):
+        click.secho(f"\tdeleting {old_docker_path}, you can create the "
+                    f"new config file with 'numerai node create'")
+        os.remove(old_docker_path)
+
+    # RENAME AND UPDATE TERRAFORM FILES
+    click.secho('Upgrading terraform files...', fg='yellow')
+    tf_files_map = {
+        'main.tf': '-main.tf',
+        'variables.tf': '-inputs.tf',
+        'outputs.tf': '-outputs.tf'
+    }
+    for old_file, new_file in tf_files_map.items():
+        old_file = os.path.join(CONFIG_PATH, old_file)
+        new_file = os.path.join(CONFIG_PATH, new_file)
+        if not os.path.exists(old_file):
+            continue
+        click.secho(f'\trenaming {old_file} to {new_file} to prep for upgrade...')
+        os.rename(old_file, new_file)
+    copy_files(
+        TERRAFORM_PATH,
+        CONFIG_PATH,
+        force=True,
+        verbose=verbose
+    )
+    # terraform init
+    click.secho("Re-initializing terraform...", fg='yellow')
+    terraform("init -upgrade", CONFIG_PATH, verbose=verbose)
+
+    if confirm("It's recommended you destroy your current Compute Node. Continue?"):
+        click.secho("Removing old cloud infrastructure...", fg='yellow')
+        terraform(
+            'destroy -auto-approve -var="node_config_file=nodes.json"',
+            CONFIG_PATH, verbose, env_vars=load_or_init_keys('aws'),
+        )
+
+    click.secho('Upgrade complete!', fg='green')
+    click.secho('run "numerai node create --help" to learn how to '
+                'register this directory as a prediction node')
