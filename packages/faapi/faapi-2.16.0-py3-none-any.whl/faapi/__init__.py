f9f54e9fbba756fa588a06862da2e6ed1a1d13cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:18 +0000

    Add package faapi/2.16.0/faapi-2.16.0-py3-none-any.whl

---
+++ packages/faapi/faapi-2.16.0-py3-none-any.whl/faapi/__init__.py
@@ -0,0 +1,7 @@
+from .__version__ import __version__
+from .base import FAAPI
+from .exceptions import *
+from .journal import Journal
+from .submission import Submission
+from .submission import SubmissionPartial
+from .user import User
