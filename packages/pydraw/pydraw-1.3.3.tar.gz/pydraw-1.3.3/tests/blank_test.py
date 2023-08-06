e2f633fca270a3d31602fe9930cde8f8df134d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package pydraw/1.3.3/pydraw-1.3.3.tar.gz

---
+++ packages/pydraw/pydraw-1.3.3.tar.gz/pydraw-1.3.3/tests/blank_test.py
@@ -0,0 +1,47 @@
+from pydraw import *;
+
+screen = Screen(800, 600);
+
+screen.toggle_grid();
+# screen.clear();
+screen.grid(helpers=True);
+
+
+p1 = Polygon(screen, 5, 150, 50, 50, 50)
+poly = Triangle(screen, 50, 50, 50, 50);
+image = Image(screen, '../images/pacman.gif', 100, 50, 50, 50);
+image.load();
+
+# poly.width(500, ratio=True)
+count = 5
+
+def keydown(key):
+    global count;
+
+    if key == 'r':
+        p1.rotate(1);
+    elif key == 'w':
+        p1.width(p1.width() + 10)
+    elif key == 'h':
+        p1.height(p1.height() + 10)
+    elif key == 'x':
+        p1.x(p1.x() + 10)
+    elif key == 'y':
+        p1.y(p1.y() + 10)
+    elif key == 'g':
+        image.next();
+    elif key == 'n':
+        poly.forward(3);
+    elif key == 'b':
+        # poly.backward(3);
+        poly.border(Color('red'), width=count)
+        count += 3;
+
+
+screen.listen();
+
+fps = 30;
+running = True;
+while running:
+    screen.update();
+    screen.sleep(1 / fps);
