68374630f61fcf63b9cdb66e753f6101dee9dcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package rolling-quantiles/0.2.1/rolling_quantiles-0.2.1-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/rolling-quantiles/rolling_quantiles-0.2.1-cp38-cp38-macosx_10_15_x86_64.whl/rolling_quantiles/__init__.py
@@ -0,0 +1,12 @@
+# A Python module is basically a file. A Python package is a directory that acts as a parent module with many submodules.
+
+__version__ = "0.2.1"
+
+from .triton import *
+
+# expose a rolling-median convenience method as a direct replacement to scipy.signal.medfilt
+def medfilt(signal, window_size):
+  import numpy as np # don't pollute the top-level namespace
+  pipeline = Pipeline(
+    LowPass(window=window_size, quantile=0.5, subsample_rate=1))
+  return pipeline.feed(np.array(signal))
