ea7254d34c85007bf4b462bfed6b2730f53b5de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:52 +0000

    Add package consign/0.1.2/consign-0.1.2.tar.gz

---
+++ packages/consign/consign-0.1.2.tar.gz/consign-0.1.2/consign/adapters.py
@@ -0,0 +1,38 @@
+import csv
+import json
+
+
+class StoreAdapter():
+    '''
+    '''
+
+    def __init__(self, method):
+        self.method = method
+
+
+    def store(self, data, url, delimiter, overwrite):
+        if self.method == 'CSV':
+            self.to_csv(data, url, delimiter)
+        elif self.method == 'JSON':
+            self.to_json(data, url, overwrite)
+
+
+    def to_csv(self, data, url, delimiter):
+        with open(url, 'w', newline='', encoding='utf-8') as output_file:
+            writer = csv.writer(output_file, delimiter=delimiter)
+            for row in data:
+                writer.writerow(row)
+
+
+    def to_json(self, data, url, overwrite):
+        if not overwrite:
+            d = self.load_json(url)
+            data.update(d)
+        with open(url, 'w') as output_file:
+            json.dump(data, output_file)
+
+
+    def load_json(self, url):
+        with open(url, 'r') as input_file:
+            d = json.load(input_file)
+        return d
---
+++ packages/consign/consign-0.1.2.tar.gz/consign-0.1.2/consign/exceptions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+consign.exceptions
+~~~~~~~~~~~~~~~~~~~
+This module contains the set of Consign's exceptions.
+"""
+
+
+class InvalidDataType(TypeError):
+    """The data provided was somehow invalid."""
+
+
+class InvalidPath(ValueError):
+    """The path provided was somehow invalid."""
+
+
+# Warnings
+
+
+class ConsignWarning(Warning):
+    """Base warning for Consign."""
