3202518ea8bb0c52a623161840c604f1aa7a43f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:02 +0000

    Add package premise/0.2.1/premise-0.2.1.tar.gz

---
+++ packages/premise/premise-0.2.1.tar.gz/premise-0.2.1/premise/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "NewDatabase",
+    "Geomap",
+    "DATA_DIR",
+    "INVENTORY_DIR"
+)
+__version__ = (0, 2, 1)
+
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+INVENTORY_DIR = Path(__file__).resolve().parent / "data" / "additional_inventories"
+
+from .ecoinvent_modification import NewDatabase
+from .geomap import Geomap
