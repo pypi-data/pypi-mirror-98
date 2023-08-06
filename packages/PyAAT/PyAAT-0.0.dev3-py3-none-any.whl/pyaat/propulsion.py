753ec465e24c32aae131c251976893b51997c698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:35 +0000

    Add package PyAAT/0.0.dev3/PyAAT-0.0.dev3-py3-none-any.whl

---
+++ packages/PyAAT/PyAAT-0.0.dev3-py3-none-any.whl/pyaat/propulsion.py
@@ -0,0 +1,37 @@
+"""
+Python Aerospace Analysis Toolbox - PyAAT
+Copyright (c) 2021 Kenedy Matiasso Portella
+Distributed under MIT License
+
+propulsion model for turbo-propeller engine
+"""
+
+from numpy import array, cross
+from pyaat.constants import RHO_SEA
+
+class SimpleModel(object):
+    def __init__(self):
+        # control
+        self.delta_p = 0.0
+        
+        # States
+        self.TAS = 0.0
+        self._rho = RHO_SEA
+        
+        # parameters
+        self.Fmaxi = 70e3
+        self.nrho = 0.775
+        self.rhoi = 0.41271
+        self.nv = 0
+        self.Vi = 200.0
+        self.position = array([0,0,1.42])
+        self.attitude = array([0.,0.,0.])
+        
+    @property
+    def Forces(self):
+        Fx = self.delta_p*self.Fmaxi*(self._rho/self.rhoi)**(self.nrho)
+        return array([Fx, 0.0, 0.0])
+    
+    @property
+    def Moments(self):
+        return cross(self.Forces, self.position)
