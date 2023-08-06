c69dfb8e30835209bfdf00fce096ec2217314b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package jupyterlab-geojson/3.1.1/jupyterlab-geojson-3.1.1.tar.gz

---
+++ packages/jupyterlab-geojson/jupyterlab-geojson-3.1.1.tar.gz/jupyterlab-geojson-3.1.1/jupyterlab-geojson/labextension/static/style_index_js.314e97ccb960e4f7bfbd.js
@@ -0,0 +1,92 @@
+(self["webpackChunk_jupyterlab_geojson_extension"] = self["webpackChunk_jupyterlab_geojson_extension"] || []).push([["style_index_js"],{
+
+/***/ "../../node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!******************************************************************!*\
+  !*** ../../node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \******************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "../../node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/getUrl.js */ "../../node_modules/css-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _geojson_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./geojson.svg */ "./style/geojson.svg");
+/* harmony import */ var _geojson_svg__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_geojson_svg__WEBPACK_IMPORTED_MODULE_3__);
+// Imports
+
+
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()((_geojson_svg__WEBPACK_IMPORTED_MODULE_3___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/**\n  Copyright (c) Jupyter Development Team.\n  Distributed under the terms of the Modified BSD License.\n*/\n\n/* Add CSS variables to :root */\n:root {\n  --jp-icon-geojson: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\n}\n\n/* Base styles */\n.jp-RenderedGeoJSON.leaflet-container {\n  width: 100%;\n  height: 100%;\n  padding: 0;\n  overflow: hidden;\n}\n\n/* Document styles */\n.jp-MimeDocument .jp-RenderedGeoJSON {\n}\n\n/* Document styles */\n.jp-OutputArea .jp-RenderedGeoJSON {\n  height: 360px;\n}\n\n/* Document icon */\n.jp-GeoJSONIcon {\n  background-image: var(--jp-icon-geojson);\n}\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;;;CAGC;;AAED,+BAA+B;AAC/B;EACE,0DAAuC;AACzC;;AAEA,gBAAgB;AAChB;EACE,WAAW;EACX,YAAY;EACZ,UAAU;EACV,gBAAgB;AAClB;;AAEA,oBAAoB;AACpB;AACA;;AAEA,oBAAoB;AACpB;EACE,aAAa;AACf;;AAEA,kBAAkB;AAClB;EACE,wCAAwC;AAC1C","sourcesContent":["/**\n  Copyright (c) Jupyter Development Team.\n  Distributed under the terms of the Modified BSD License.\n*/\n\n/* Add CSS variables to :root */\n:root {\n  --jp-icon-geojson: url('./geojson.svg');\n}\n\n/* Base styles */\n.jp-RenderedGeoJSON.leaflet-container {\n  width: 100%;\n  height: 100%;\n  padding: 0;\n  overflow: hidden;\n}\n\n/* Document styles */\n.jp-MimeDocument .jp-RenderedGeoJSON {\n}\n\n/* Document styles */\n.jp-OutputArea .jp-RenderedGeoJSON {\n  height: 360px;\n}\n\n/* Document icon */\n.jp-GeoJSONIcon {\n  background-image: var(--jp-icon-geojson);\n}\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../../node_modules/css-loader/dist/cjs.js!./base.css */ "../../node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default, options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});
+
+/***/ }),
+
+/***/ "./style/geojson.svg":
+/*!***************************!*\
+  !*** ./style/geojson.svg ***!
+  \***************************/
+/***/ ((module) => {
+
+module.exports = "data:image/svg+xml,%3Csvg width='12px' height='12px' viewBox='0 0 12 12' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E %3C!-- Generator: Sketch 42 (36781) - http://www.bohemiancoding.com/sketch --%3E %3Ctitle%3Egeojson%3C/title%3E %3Cdesc%3ECreated with Sketch.%3C/desc%3E %3Cdefs%3E%3C/defs%3E %3Cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E %3Cg id='geojson' transform='translate(-6.000000, -6.000000)'%3E %3Cpath d='M17.6666667,6 L17.56,6.02 L14,7.4 L10,6 L6.24,7.26666667 C6.1,7.31333333 6,7.43333333 6,7.58666667 L6,17.6666667 C6,17.8533333 6.14666667,18 6.33333333,18 L6.44,17.98 L10,16.6 L14,18 L17.76,16.7333333 C17.9,16.6866667 18,16.5666667 18,16.4133333 L18,6.33333333 C18,6.14666667 17.8533333,6 17.6666667,6 Z M14,16.6666667 L10,15.26 L10,7.33333333 L14,8.74 L14,16.6666667 Z' id='Shape' fill='%231BB233' fill-rule='nonzero'%3E%3C/path%3E %3Cpolygon id='Shape' points='0 0 24 0 24 24 0 24'%3E%3C/polygon%3E %3C/g%3E %3C/g%3E %3C/svg%3E"
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.314e97ccb960e4f7bfbd.js.map
