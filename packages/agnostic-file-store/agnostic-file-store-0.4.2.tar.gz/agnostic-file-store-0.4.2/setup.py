5ee8566532c1f32c0d0603f7155e6c9b3c84c410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:52 +0000

    Add package agnostic-file-store/0.4.2/agnostic-file-store-0.4.2.tar.gz

---
+++ packages/agnostic-file-store/agnostic-file-store-0.4.2.tar.gz/agnostic-file-store-0.4.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+KEYWORDS = [
+    'agnostic',
+    'AMS',
+    'CIFS',
+    'NAS',
+    'S3',
+    'Samba',
+    'SAN',
+    'SMB',
+    'storage',
+]
+
+DESCRIPTION = (
+    'An agnostic, easy-to-use module for different file systems'
+    ' (Memory | local | Samba (SMB) | NFS | Amazon S3)'
+)
+
+setup(
+    name='agnostic-file-store',
+    version='0.4.2',
+    packages=['afs'],
+    package_dir={"": "src"},    
+    description=DESCRIPTION,
+    author='Juan Ignacio Rodríguez de León',
+    author_email='euribates@gmail.com',
+    url='https://github.com/euribates/agnostic-file-store/',
+    download_url='https://github.com/euribates/agnostic-file-store/archive/master.zip',
+    keywords=KEYWORDS,
+    )
