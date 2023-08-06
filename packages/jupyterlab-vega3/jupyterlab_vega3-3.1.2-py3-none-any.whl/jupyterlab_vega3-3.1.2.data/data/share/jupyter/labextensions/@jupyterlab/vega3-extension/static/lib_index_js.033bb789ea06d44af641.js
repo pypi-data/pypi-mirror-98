8217df2f60aaa1fd9c64d0f43c4eb389f45e9b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package jupyterlab-vega3/3.1.2/jupyterlab_vega3-3.1.2-py3-none-any.whl

---
+++ packages/jupyterlab-vega3/jupyterlab_vega3-3.1.2-py3-none-any.whl/jupyterlab_vega3-3.1.2.data/data/share/jupyter/labextensions/@jupyterlab/vega3-extension/static/lib_index_js.033bb789ea06d44af641.js
@@ -0,0 +1,247 @@
+(self["webpackChunk_jupyterlab_vega3_extension"] = self["webpackChunk_jupyterlab_vega3_extension"] || []).push([["lib_index_js"],{
+
+/***/ "../../node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!******************************************************************!*\
+  !*** ../../node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \******************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "../../node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/*-----------------------------------------------------------------------------\n| Copyright (c) Jupyter Development Team.\n| Distributed under the terms of the Modified BSD License.\n|----------------------------------------------------------------------------*/\n\n.jp-RenderedVegaCommon3 {\n  margin-left: 8px;\n  margin-top: 8px;\n}\n\n.jp-MimeDocument .jp-RenderedVegaCommon3 {\n  padding: 16px;\n}\n\n.vega canvas {\n  background: var(--jp-vega-background);\n}\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;;;8EAG8E;;AAE9E;EACE,gBAAgB;EAChB,eAAe;AACjB;;AAEA;EACE,aAAa;AACf;;AAEA;EACE,qCAAqC;AACvC","sourcesContent":["/*-----------------------------------------------------------------------------\n| Copyright (c) Jupyter Development Team.\n| Distributed under the terms of the Modified BSD License.\n|----------------------------------------------------------------------------*/\n\n.jp-RenderedVegaCommon3 {\n  margin-left: 8px;\n  margin-top: 8px;\n}\n\n.jp-MimeDocument .jp-RenderedVegaCommon3 {\n  padding: 16px;\n}\n\n.vega canvas {\n  background: var(--jp-vega-background);\n}\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "../../node_modules/css-loader/dist/cjs.js!./style/index.css":
+/*!*******************************************************************!*\
+  !*** ../../node_modules/css-loader/dist/cjs.js!./style/index.css ***!
+  \*******************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "../../node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "../../node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! -!../../../node_modules/css-loader/dist/cjs.js!./base.css */ "../../node_modules/css-loader/dist/cjs.js!./style/base.css");
+// Imports
+
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+___CSS_LOADER_EXPORT___.i(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_2__.default);
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/*-----------------------------------------------------------------------------\n| Copyright (c) Jupyter Development Team.\n| Distributed under the terms of the Modified BSD License.\n|----------------------------------------------------------------------------*/\n", "",{"version":3,"sources":["webpack://./style/index.css"],"names":[],"mappings":"AAAA;;;8EAG8E","sourcesContent":["/*-----------------------------------------------------------------------------\n| Copyright (c) Jupyter Development Team.\n| Distributed under the terms of the Modified BSD License.\n|----------------------------------------------------------------------------*/\n\n@import url('base.css');\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/index.css":
+/*!*************************!*\
+  !*** ./style/index.css ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../../node_modules/css-loader/dist/cjs.js!./index.css */ "../../node_modules/css-loader/dist/cjs.js!./style/index.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default, options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});
+
+/***/ }),
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.rendererFactory = exports.RenderedVega3 = exports.VEGALITE_MIME_TYPE = exports.VEGA_MIME_TYPE = void 0;
+const widgets_1 = __webpack_require__(/*! @lumino/widgets */ "webpack/sharing/consume/default/@lumino/widgets");
+const vega_embed_1 = __importStar(__webpack_require__(/*! vega-embed */ "webpack/sharing/consume/default/vega-embed/vega-embed"));
+__webpack_require__(/*! ../style/index.css */ "./style/index.css");
+/**
+ * The CSS class to add to the Vega and Vega-Lite widget.
+ */
+const VEGA_COMMON_CLASS = 'jp-RenderedVegaCommon3';
+/**
+ * The CSS class to add to the Vega.
+ */
+const VEGA_CLASS = 'jp-RenderedVega3';
+/**
+ * The CSS class to add to the Vega-Lite.
+ */
+const VEGALITE_CLASS = 'jp-RenderedVegaLite2';
+/**
+ * The MIME type for Vega.
+ *
+ * #### Notes
+ * The version of this follows the major version of Vega.
+ */
+exports.VEGA_MIME_TYPE = 'application/vnd.vega.v3+json';
+/**
+ * The MIME type for Vega-Lite.
+ *
+ * #### Notes
+ * The version of this follows the major version of Vega-Lite.
+ */
+exports.VEGALITE_MIME_TYPE = 'application/vnd.vegalite.v2+json';
+/**
+ * A widget for rendering Vega or Vega-Lite data, for usage with rendermime.
+ */
+class RenderedVega3 extends widgets_1.Widget {
+    /**
+     * Create a new widget for rendering Vega/Vega-Lite.
+     */
+    constructor(options) {
+        super();
+        this._mimeType = options.mimeType;
+        this._resolver = options.resolver;
+        this.addClass(VEGA_COMMON_CLASS);
+        this.addClass(this._mimeType === exports.VEGA_MIME_TYPE ? VEGA_CLASS : VEGALITE_CLASS);
+    }
+    /**
+     * Render Vega/Vega-Lite into this widget's node.
+     */
+    renderModel(model) {
+        const data = model.data[this._mimeType];
+        const metadata = model.metadata[this._mimeType];
+        const embedOptions = metadata && metadata.embed_options ? metadata.embed_options : {};
+        const mode = this._mimeType === exports.VEGA_MIME_TYPE ? 'vega' : 'vega-lite';
+        return this._resolver.resolveUrl('').then((path) => {
+            return this._resolver.getDownloadUrl(path).then(baseURL => {
+                const loader = vega_embed_1.vega.loader({
+                    baseURL
+                });
+                const options = Object.assign(Object.assign({ actions: true }, embedOptions), { mode,
+                    loader });
+                const el = document.createElement('div');
+                this.node.innerHTML = ''; // clear the output before attaching a chart
+                this.node.appendChild(el);
+                return vega_embed_1.default(el, data, options).then(result => {
+                    // Add png representation of vega chart to output
+                    if (!model.data['image/png']) {
+                        return result.view.toImageURL('png').then(imageData => {
+                            const data = Object.assign(Object.assign({}, model.data), { 'image/png': imageData.split(',')[1] });
+                            model.setData({ data });
+                        });
+                    }
+                    return void 0;
+                });
+            });
+        });
+    }
+}
+exports.RenderedVega3 = RenderedVega3;
+/**
+ * A mime renderer factory for vega data.
+ */
+exports.rendererFactory = {
+    safe: true,
+    mimeTypes: [exports.VEGA_MIME_TYPE, exports.VEGALITE_MIME_TYPE],
+    createRenderer: options => new RenderedVega3(options)
+};
+const extension = {
+    id: '@jupyterlab/vega3-extension:factory',
+    rendererFactory: exports.rendererFactory,
+    rank: 59,
+    dataType: 'json',
+    documentWidgetFactoryOptions: [
+        {
+            name: 'Vega 3',
+            primaryFileType: 'vega3',
+            fileTypes: ['vega3', 'json'],
+            defaultFor: ['vega3']
+        },
+        {
+            name: 'Vega-Lite 2',
+            primaryFileType: 'vega-lite2',
+            fileTypes: ['vega-lite2', 'json'],
+            defaultFor: ['vega-lite2']
+        }
+    ],
+    fileTypes: [
+        {
+            mimeTypes: [exports.VEGA_MIME_TYPE],
+            name: 'vega3',
+            extensions: ['.vg', '.vg.json', '.vega'],
+            iconClass: 'jp-MaterialIcon jp-VegaIcon'
+        },
+        {
+            mimeTypes: [exports.VEGALITE_MIME_TYPE],
+            name: 'vega-lite2',
+            extensions: ['.vl', '.vl.json', '.vegalite'],
+            iconClass: 'jp-MaterialIcon jp-VegaIcon'
+        }
+    ]
+};
+exports.default = extension;
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.033bb789ea06d44af641.js.map
