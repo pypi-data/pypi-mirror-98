9ae3ab54c79b54053d451e5e7f574d759289b678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:32 +0000

    Add package cmarkgfm/0.5.3/cmarkgfm-0.5.3.tar.gz

---
+++ packages/cmarkgfm/cmarkgfm-0.5.3.tar.gz/cmarkgfm-0.5.3/setup.py
@@ -0,0 +1,64 @@
+import platform
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+class custom_build_ext(build_ext):
+    """Custom build_ext command that uses mingw32 when building on Python2.7
+    in Windows."""
+
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        is_windows = platform.system() == 'Windows'
+        is_py2 = sys.version_info[0] < 3
+        if self.compiler is None and is_windows and is_py2:
+            self.compiler = 'mingw32'
+
+
+setup(
+    name='cmarkgfm',
+    version='0.5.3',
+    description="Minimal bindings to GitHub's fork of cmark",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/theacodes/cmarkgfm',
+    author='The Python Packaging Authority',
+    author_email='me@thea.codes, pypa-dev@googlegroups.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    cffi_modules=["src/cmarkgfm/build_cmark.py:ffibuilder"],
+    setup_requires=["cffi>=1.0.0"],
+    install_requires=["cffi>=1.0.0"],
+    project_urls={
+        'Bug Reports': 'https://github.com/theacodes/cmarkgfm/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Source': 'https://github.com/theacodes/cmarkgfm',
+    },
+    zip_safe=False,
+    include_package_data=True,
+    cmdclass={
+        'build_ext': custom_build_ext,
+    },
+)
---
+++ packages/cmarkgfm/cmarkgfm-0.5.3.tar.gz/cmarkgfm-0.5.3/tests/test_cmark.py
@@ -0,0 +1,93 @@
+from __future__ import unicode_literals
+
+import textwrap
+
+from cmarkgfm import cmark
+
+
+def _normalize_ws(html):
+    return textwrap.dedent(html.strip("\n")).strip("\n")
+
+
+def test_markdown_to_html():
+    text = u"Hello, **world**!"
+    result = cmark.markdown_to_html(text)
+    assert result == '<p>Hello, <strong>world</strong>!</p>\n'
+
+
+def test_render_html_with_extensions():
+    text = u"Hello, https://pypa.io!"
+    result = cmark.markdown_to_html_with_extensions(
+        text, extensions=['autolink'])
+    expected = """
+        <p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
+
+
+def test_github_flavored_markdown_to_html():
+    text = u"Hello, https://pypa.io!"
+    result = cmark.github_flavored_markdown_to_html(text)
+    expected = """
+        <p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
+
+
+def test_github_flavored_markdown_to_html_pre_tag():
+    text = u"```python\nprint('hello')\n```"
+    result = cmark.github_flavored_markdown_to_html(text)
+    expected = """
+        <pre><code class="language-python">print('hello')
+        </code></pre>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
+
+
+def test_github_flavored_markdown_to_html_tasklist():
+    text = u"- [X] Task 1 Done\n- [ ] Task 2 Incomplete"
+    result = cmark.github_flavored_markdown_to_html(text)
+    expected = """
+        <ul>
+        <li><input type="checkbox" disabled="" /> Task 1 Done</li>
+        <li><input type="checkbox" disabled="" /> Task 2 Incomplete</li>
+        </ul>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
+
+
+def test_parse_document():
+    text = u"Hello, **world**!"
+    result = cmark.parse_document(text)
+    assert result is not None
+
+
+def test_render_html():
+    text = u"Hello, **world**!"
+    root = cmark.parse_document(text)
+    result = cmark.render_html(root)
+    assert result == '<p>Hello, <strong>world</strong>!</p>\n'
+
+
+def test_parser_interface():
+    text = u"Hello, **world**!"
+    parser = cmark.parser_new()
+    cmark.parser_feed(parser, text)
+    root = cmark.parser_finish(parser)
+    result = cmark.render_html(root)
+    cmark.parser_free(parser)
+    assert result == '<p>Hello, <strong>world</strong>!</p>\n'
+
+
+def test_core_extensions_ensure_registered():
+    cmark.core_extensions_ensure_registered()
+
+
+def test_find_syntax_extension():
+    extension = cmark.find_syntax_extension('table')
+    assert extension is not None
+
+
+def test_find_syntax_extension_doesnt_exist():
+    extension = cmark.find_syntax_extension('notarealext')
+    assert extension is None
