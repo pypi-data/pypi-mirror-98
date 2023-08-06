f58c62a333949b169d1a3ef06212d6cffd80c89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:01 +0000

    Add package traviscli/0.3.0/traviscli-0.3.0.tar.gz

---
+++ packages/traviscli/traviscli-0.3.0.tar.gz/traviscli-0.3.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="traviscli",
+    version="0.3.0",
+    author_email='Humberto.A.Sanchez.II@gmail.com',
+    description='A build help tool',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hasii2011/travisci-cli",
+    packages=[
+        'travisci',
+        'travisci.exceptions',
+        'travisci.resources'
+    ],
+    package_data={'travisci.resources': ['loggingConfiguration.json', 'loggingConfiguration.json']},
+    include_package_data=True,
+    install_requires=['click', 'PyTravisCI'],
+    entry_points='''
+        [console_scripts]
+        traviscli=travisci.TravisCli:commandHandler
+    ''',
+)
