939c81e9fcacc8d55517f931d6cc6d7cb4101d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package fast-ft/0.1.2/fast-ft-0.1.2.tar.gz

---
+++ packages/fast-ft/fast-ft-0.1.2.tar.gz/fast-ft-0.1.2/fast_ft/static/js/list.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var clipboard = new ClipboardJS('#copy_path', {
+        target: function(e) {
+            return e.parentNode;
+        }
+    });
+    clipboard.on('success', function(e) {
+        // alert('复制成功！');
+    });
+    clipboard.on('error', function(e) {
+        // alert('复制失败！');
+    });
+    // 监听iframe点击时间
+    let iframe = document.getElementById('chatIframe'); 
+    iframe.onload = function onload (){
+        $(iframe.contentDocument.getElementById('close')).on('click', function () {
+            $('div#webchat').removeClass('webchat');
+            $('div#webchat').addClass('chat-close');
+        });
+    }
+    // 监听消息按钮
+    $('div.tool-button').on('click', function () {
+        $('div#webchat').addClass('webchat');
+        $('div#webchat').removeClass('chat-close');
+    });
+});
