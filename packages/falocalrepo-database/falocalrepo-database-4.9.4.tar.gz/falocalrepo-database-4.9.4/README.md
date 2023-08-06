668302ee8b0c415d1ca3fa1a577e7c622b017b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:33 +0000

    Add package falocalrepo-database/4.9.4/falocalrepo-database-4.9.4.tar.gz

---
+++ packages/falocalrepo-database/falocalrepo-database-4.9.4.tar.gz/falocalrepo-database-4.9.4/README.md
@@ -0,0 +1,84 @@
+# FALocalRepo-Database
+
+[![version_pypi](https://img.shields.io/pypi/v/falocalrepo-database?logo=pypi)](https://pypi.org/project/falocalrepo-database/)
+[![version_gitlab](https://img.shields.io/badge/dynamic/json?logo=gitlab&color=orange&label=gitlab&query=%24%5B%3A1%5D.name&url=https%3A%2F%2Fgitlab.com%2Fapi%2Fv4%2Fprojects%2Fmatteocampinoti94%252Ffalocalrepo-database%2Frepository%2Ftags)](https://gitlab.com/MatteoCampinoti94/falocalrepo-database)
+[![version_python](https://img.shields.io/pypi/pyversions/falocalrepo-database?logo=Python)](https://www.python.org)
+
+Database functionality for [falocalrepo](https://gitlab.com/MatteoCampinoti94/falocalrepo).
+
+## Usage
+
+_Detailed usage and components documentation will be added in a future patch_ 
+
+## Tables
+
+To store its information, the database uses four tables: `SETTINGS`, `USERS`, `SUBMISSIONS` and `JOURNALS`.
+
+**Note**: bar-separated lists are formatted as `|item1||item2|` to properly isolate all elements
+
+### Settings
+
+The settings table contains settings for the program and statistics of the database.
+
+* `HISTORY` list of executed commands in the format `[[<time1>, "<command1>"], ..., [<timeN>, "<commandN>"]]` (UNIX time in seconds)
+* `COOKIES` cookies for the scraper, stored in JSON format
+* `FILESFOLDER` location of downloaded submission files
+* `VERSION` database version
+
+### Users
+
+The users table contains a list of all the users that have been download with the program, the folders that have been downloaded, and the submissions found in each of those.
+
+Each entry contains the following fields:
+
+* `USERNAME` The URL username of the user (no underscores or spaces)
+* `FOLDERS` the folders downloaded for that specific user, sorted and bar-separated
+
+### Submissions
+
+The submissions table contains the metadata of the submissions downloaded by the program and information on their files 
+
+* `ID` the id of the submission
+* `AUTHOR` the username of the author (uploader) in full format
+* `TITLE`
+* `DATE` upload date in the format YYYY-MM-DD
+* `DESCRIPTION` description in html format
+* `TAGS` keywords sorted alphanumerically and bar-separated
+* `CATEGORY`
+* `SPECIES`
+* `GENDER`
+* `RATING`
+* `TYPE` image, text, music, or flash
+* `FILEURL` the remote URL of the submission file
+* `FILEEXT` the extensions of the downloaded file. Can be empty if the file contained errors and could not be recognised upon download
+* `FILESAVED` 1 if the file was successfully downloaded and saved, 0 if there was an error during download
+* `FAVORITE` a bar-separated list of users that have "faved" the submission
+* `MENTIONS` a bar-separated list of users that are mentioned in the submission description as links
+* `FOLDER` the folder of the submission (`gallery` or `scraps`)
+* `USERUPDATE` whether the submission was added as a user update or favorite/single entry
+
+### Journals
+
+The journals table contains the metadata of the journals downloaded by the program.
+
+* `ID` the id of the journal
+* `AUTHOR` the username of the author (uploader) in full format
+* `TITLE`
+* `DATE` upload date in the format YYYY-MM-DD
+* `CONTENT` content in html format
+* `MENTIONS` a bar-separated list of users that are mentioned in the journal content as links
+* `USERUPDATE` whether the journal was added as a user update or favorite/single entry
+
+## Submission Files
+
+The `save_submission` functions saves the submission metadata in the database and stores the files.
+
+Submission files are saved in a tiered tree structure based on their submission ID. ID's are zero-padded to 10 digits and then broken up in 5 segments of 2 digits; each of this segments represents a folder tha will be created in the tree.
+
+For example, a submission `1457893` will be padded to `0001457893` and divided into `00`, `01`, `45`, `78`, `93`. The submission file will then be saved as `00/01/45/78/93/submission.file` with the correct extension extracted from the file itself - FurAffinity links do not always contain the right extension and often confuse jpg and png.
+
+## Upgrading Database
+
+The `FADatabase.upgrade` function allows to upgrade the database to the current version.
+
+_Note:_ Versions before 2.7.0 are not supported by falocalrepo-database version 3.0.0 and above. To update from those to the new version use [falocalrepo](https://gitlab.com/MatteoCampinoti94/FALocalRepo/-/releases/v2.11.2) version 2.11.2 to update the database to version 2.7.0
---
+++ packages/falocalrepo-database/falocalrepo-database-4.9.4.tar.gz/falocalrepo-database-4.9.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['falocalrepo_database']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['chardet>=4.0.0,<5.0.0', 'filetype>=1.0.7,<2.0.0']
+
+setup_kwargs = {
+    'name': 'falocalrepo-database',
+    'version': '4.9.4',
+    'description': 'Database functionality for falocalrepo.',
+    'long_description': '# FALocalRepo-Database\n\n[![version_pypi](https://img.shields.io/pypi/v/falocalrepo-database?logo=pypi)](https://pypi.org/project/falocalrepo-database/)\n[![version_gitlab](https://img.shields.io/badge/dynamic/json?logo=gitlab&color=orange&label=gitlab&query=%24%5B%3A1%5D.name&url=https%3A%2F%2Fgitlab.com%2Fapi%2Fv4%2Fprojects%2Fmatteocampinoti94%252Ffalocalrepo-database%2Frepository%2Ftags)](https://gitlab.com/MatteoCampinoti94/falocalrepo-database)\n[![version_python](https://img.shields.io/pypi/pyversions/falocalrepo-database?logo=Python)](https://www.python.org)\n\nDatabase functionality for [falocalrepo](https://gitlab.com/MatteoCampinoti94/falocalrepo).\n\n## Usage\n\n_Detailed usage and components documentation will be added in a future patch_ \n\n## Tables\n\nTo store its information, the database uses four tables: `SETTINGS`, `USERS`, `SUBMISSIONS` and `JOURNALS`.\n\n**Note**: bar-separated lists are formatted as `|item1||item2|` to properly isolate all elements\n\n### Settings\n\nThe settings table contains settings for the program and statistics of the database.\n\n* `HISTORY` list of executed commands in the format `[[<time1>, "<command1>"], ..., [<timeN>, "<commandN>"]]` (UNIX time in seconds)\n* `COOKIES` cookies for the scraper, stored in JSON format\n* `FILESFOLDER` location of downloaded submission files\n* `VERSION` database version\n\n### Users\n\nThe users table contains a list of all the users that have been download with the program, the folders that have been downloaded, and the submissions found in each of those.\n\nEach entry contains the following fields:\n\n* `USERNAME` The URL username of the user (no underscores or spaces)\n* `FOLDERS` the folders downloaded for that specific user, sorted and bar-separated\n\n### Submissions\n\nThe submissions table contains the metadata of the submissions downloaded by the program and information on their files \n\n* `ID` the id of the submission\n* `AUTHOR` the username of the author (uploader) in full format\n* `TITLE`\n* `DATE` upload date in the format YYYY-MM-DD\n* `DESCRIPTION` description in html format\n* `TAGS` keywords sorted alphanumerically and bar-separated\n* `CATEGORY`\n* `SPECIES`\n* `GENDER`\n* `RATING`\n* `TYPE` image, text, music, or flash\n* `FILEURL` the remote URL of the submission file\n* `FILEEXT` the extensions of the downloaded file. Can be empty if the file contained errors and could not be recognised upon download\n* `FILESAVED` 1 if the file was successfully downloaded and saved, 0 if there was an error during download\n* `FAVORITE` a bar-separated list of users that have "faved" the submission\n* `MENTIONS` a bar-separated list of users that are mentioned in the submission description as links\n* `FOLDER` the folder of the submission (`gallery` or `scraps`)\n* `USERUPDATE` whether the submission was added as a user update or favorite/single entry\n\n### Journals\n\nThe journals table contains the metadata of the journals downloaded by the program.\n\n* `ID` the id of the journal\n* `AUTHOR` the username of the author (uploader) in full format\n* `TITLE`\n* `DATE` upload date in the format YYYY-MM-DD\n* `CONTENT` content in html format\n* `MENTIONS` a bar-separated list of users that are mentioned in the journal content as links\n* `USERUPDATE` whether the journal was added as a user update or favorite/single entry\n\n## Submission Files\n\nThe `save_submission` functions saves the submission metadata in the database and stores the files.\n\nSubmission files are saved in a tiered tree structure based on their submission ID. ID\'s are zero-padded to 10 digits and then broken up in 5 segments of 2 digits; each of this segments represents a folder tha will be created in the tree.\n\nFor example, a submission `1457893` will be padded to `0001457893` and divided into `00`, `01`, `45`, `78`, `93`. The submission file will then be saved as `00/01/45/78/93/submission.file` with the correct extension extracted from the file itself - FurAffinity links do not always contain the right extension and often confuse jpg and png.\n\n## Upgrading Database\n\nThe `FADatabase.upgrade` function allows to upgrade the database to the current version.\n\n_Note:_ Versions before 2.7.0 are not supported by falocalrepo-database version 3.0.0 and above. To update from those to the new version use [falocalrepo](https://gitlab.com/MatteoCampinoti94/FALocalRepo/-/releases/v2.11.2) version 2.11.2 to update the database to version 2.7.0\n',
+    'author': 'Matteo Campinoti',
+    'author_email': 'matteo.campinoti94@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/MatteoCampinoti94/falocalrepo-database',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
