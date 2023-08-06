6d6104f672667a4fa3561442b19b0cee08c75502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:27 +0000

    Add package synonym-dict/0.2.1/synonym_dict-0.2.1-py3-none-any.whl

---
+++ packages/synonym-dict/synonym_dict-0.2.1-py3-none-any.whl/synonym_dict/__init__.py
@@ -0,0 +1,6 @@
+from .lower_dict import LowerDict
+from .synonym_dict import SynonymDict, MergeError, TermExists
+from .synonym_set import SynonymSet
+
+from .compartments import Compartment, CompartmentManager, InconsistentLineage, NonSpecificCompartment
+from .flowables import Flowable, FlowablesDict
