89e00335190e9c7d739f446c99cf264290ed0743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:52 +0000

    Add package mlnd-EPC-distributions/0.1/mlnd-EPC-distributions-0.1.tar.gz

---
+++ packages/mlnd-EPC-distributions/mlnd-EPC-distributions-0.1.tar.gz/mlnd-EPC-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlnd-EPC-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mlnd-EPC-distributions'],
+      zip_safe=False,
+      author='emile',
+      author_email='fake@email.com')
