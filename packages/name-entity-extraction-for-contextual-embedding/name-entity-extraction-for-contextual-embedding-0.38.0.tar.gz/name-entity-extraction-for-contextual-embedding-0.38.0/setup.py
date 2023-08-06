9fd0ae2c1c77bd032b3c5ca24ad632eb46015818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:50 +0000

    Add package name-entity-extraction-for-contextual-embedding/0.38.0/name-entity-extraction-for-contextual-embedding-0.38.0.tar.gz

---
+++ packages/name-entity-extraction-for-contextual-embedding/name-entity-extraction-for-contextual-embedding-0.38.0.tar.gz/name-entity-extraction-for-contextual-embedding-0.38.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+print(find_packages())
+setup(
+    name="name-entity-extraction-for-contextual-embedding",
+    packages=find_packages(),
+    version="0.38.0",
+    description="a machine learning project for mlm task for contextual embedding",
+    author="Simon Meoni",
+    license="MIT",
+    install_requires=requirements,
+    entry_points="""
+    [console_scripts]
+    camembert_ner_ft=contextual_ner.models.camembert_ner_ft:main
+    make_dataset=contextual_ner.data.make_dataset:main
+    """,
+    package_data={
+        "contextual_ner": [
+            "resources/log_config.ini",
+        ]
+    },
+)
