1426f5631fe173bf5fd9c5c201711939845c1c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package ironic-lib/4.2.2/ironic-lib-4.2.2.tar.gz

---
+++ packages/ironic-lib/ironic-lib-4.2.2.tar.gz/ironic-lib-4.2.2/doc/source/conf.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+
+# -- General configuration ----------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'openstackdocstheme',
+              ]
+
+wsme_protocols = ['restjson']
+
+# autodoc generation is a bit aggressive and a nuisance when doing heavy
+# text edit cycles.
+# execute "export SPHINX_DEBUG=1" in your terminal to disable
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+copyright = u'OpenStack Foundation'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from ironic_lib import version as il_version
+# The full version, including alpha/beta/rc tags.
+release = il_version.version_info.release_string()
+# The short X.Y version.
+version = il_version.version_info.version_string()
+
+# A list of ignored prefixes for module index sorting.
+modindex_common_prefix = ['ironic_lib']
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output --------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+#html_theme_path = ["."]
+#html_static_path = ['_static']
+
+html_theme = 'openstackdocs'
+
+# openstackdocstheme options
+repository_name = 'openstack/ironic-lib'
+use_storyboard = True
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'ironic-libdoc'
+
+latex_use_xindy = False
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass
+# [howto/manual]).
+latex_documents = [
+    (
+        'index',
+        'doc-ironic-lib.tex',
+        u'Ironic Lib Documentation',
+        u'OpenStack Foundation',
+        'manual'
+    ),
+]
