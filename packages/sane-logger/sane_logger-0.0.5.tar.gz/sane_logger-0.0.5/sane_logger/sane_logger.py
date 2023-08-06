3df644f2c024dfc1d16f56c9241b910d87d225c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package sane-logger/0.0.5/sane_logger-0.0.5.tar.gz

---
+++ packages/sane-logger/sane_logger-0.0.5.tar.gz/sane_logger-0.0.5/sane_logger/sane_logger.py
@@ -0,0 +1,19 @@
+import logging, sys
+
+
+def sane_logger(log_level=logging.INFO):
+  logger = logging.getLogger()
+  logger.setLevel(log_level)
+
+  sh = logging.StreamHandler(sys.stdout)
+  formatter = logging.Formatter(
+    '[%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s',
+    datefmt='%a, %d %b %Y %H:%M:%S %Z'
+  )
+  sh.setFormatter(formatter)
+  logger.addHandler(sh)
+  return logger
+
+if __name__ == '__main__':
+  logger = sane_logger(logging.DEBUG)
+  logger.info('test log')
