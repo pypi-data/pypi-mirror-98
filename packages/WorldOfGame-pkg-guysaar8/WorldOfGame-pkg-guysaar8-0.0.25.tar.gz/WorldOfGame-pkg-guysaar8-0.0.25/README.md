55cf76fd5e711ba479eb13aa9d32b21ea5a7e95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:07 +0000

    Add package WorldOfGame-pkg-guysaar8/0.0.25/WorldOfGame-pkg-guysaar8-0.0.25.tar.gz

---
+++ packages/WorldOfGame-pkg-guysaar8/WorldOfGame-pkg-guysaar8-0.0.25.tar.gz/WorldOfGame-pkg-guysaar8-0.0.25/README.md
@@ -0,0 +1,9 @@
+# A place For Many Games
+
+Guess Game Memory Game Currency Rullete<br/>
+More now and exiting games are coming soon!! <br/>
+
+# to start the game: "from worldofgames import log"
+
+
+
---
+++ packages/WorldOfGame-pkg-guysaar8/WorldOfGame-pkg-guysaar8-0.0.25.tar.gz/WorldOfGame-pkg-guysaar8-0.0.25/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="WorldOfGame-pkg-guysaar8",
+    version="0.0.25",
+    description="Python Games package",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Guy Saar",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=['worldofgames'],
+    package_data={'': ['account_db.json']},
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts":[
+            "worldofgames=worldofgames.MainGame:log",
+        ]
+    },
+)
+
