3e670f2bc466436fc7d7593109fc76c743829b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:14 +0000

    Add package easyhttp/0.0.2/easyhttp-0.0.2.tar.gz

---
+++ packages/easyhttp/easyhttp-0.0.2.tar.gz/easyhttp-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+"""
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+"""
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Basic HTTP server package'
+LONG_DESCRIPTION = 'A simple HTTP server made for study purposes.'
+
+# Setting up
+setup(
+    name="easyhttp",
+    version=VERSION,
+    author="luisbrandino (Luis Brandino)",
+    author_email="<luisbrandino.contato@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'webserver', 'http', 'http server', 'sockets'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
