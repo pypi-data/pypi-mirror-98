b61f2c20f092b763822d655fd2bf5c9bcad2144d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:29 +0000

    Add package apollo-python-client/0.0.1/apollo-python-client-0.0.1.tar.gz

---
+++ packages/apollo-python-client/apollo-python-client-0.0.1.tar.gz/apollo-python-client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='apollo-python-client',
+    version='0.0.1',
+    author='xhrg',
+    author_email='634789257@qq.com',
+    url='https://github.com/xhrg-product/apollo-client-python',
+    packages=['apollo'],
+)
