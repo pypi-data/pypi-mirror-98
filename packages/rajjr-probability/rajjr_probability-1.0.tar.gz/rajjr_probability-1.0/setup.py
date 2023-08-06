315c2687bbe584e2618bd7a5ff91b066badc3e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:36 +0000

    Add package rajjr-probability/1.0/rajjr_probability-1.0.tar.gz

---
+++ packages/rajjr-probability/rajjr_probability-1.0.tar.gz/rajjr_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rajjr_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rajjr_probability'],
+      author= 'Robert Johnson',
+      author_email = 'info@junkmail.com',
+      zip_safe=False)
