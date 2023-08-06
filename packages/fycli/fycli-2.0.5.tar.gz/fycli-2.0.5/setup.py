e82d8c4779e8ea4fa0fcb06b42d48f9001d61758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:08 +0000

    Add package fycli/2.0.5/fycli-2.0.5.tar.gz

---
+++ packages/fycli/fycli-2.0.5.tar.gz/fycli-2.0.5/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fycli',
+ 'fycli.dependencies',
+ 'fycli.environment',
+ 'fycli.infra',
+ 'fycli.kubernetes',
+ 'fycli.skeleton',
+ 'fycli.terraform',
+ 'fycli.vault']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyyaml>=5.3,<6.0', 'sh>=1.12.14,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['fy = fycli.__main__:main']}
+
+setup_kwargs = {
+    'name': 'fycli',
+    'version': '2.0.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Rob Wilson',
+    'author_email': 'roobert@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
