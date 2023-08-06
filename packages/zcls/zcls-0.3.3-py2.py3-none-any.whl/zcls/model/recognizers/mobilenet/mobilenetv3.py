b11d6e016cdf292ef931bc2f92cd3bb986343a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:51 +0000

    Add package zcls/0.3.3/zcls-0.3.3-py2.py3-none-any.whl

---
+++ packages/zcls/zcls-0.3.3-py2.py3-none-any.whl/zcls/model/recognizers/mobilenet/mobilenetv3.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/12/24 下午7:38
+@file: shufflenetv1.py
+@author: zj
+@description: 
+"""
+
+import torch.nn as nn
+from torchvision.models.utils import load_state_dict_from_url
+
+from zcls.model import registry
+from ..base_recognizer import BaseRecognizer
+
+
+@registry.RECOGNIZER.register('MobileNetV3')
+class MobileNetV3(BaseRecognizer):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    def init_weights(self, pretrained, pretrained_num_classes, num_classes):
+        if pretrained != "":
+            state_dict = load_state_dict_from_url(pretrained, progress=True)
+            self.load_state_dict(state_dict=state_dict, strict=False)
+        if num_classes != pretrained_num_classes:
+            in_channels = self.head.conv2.in_channels
+            conv2 = nn.Conv2d(in_channels, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
+
+            nn.init.kaiming_normal_(conv2.weight, mode="fan_out", nonlinearity="relu")
+            nn.init.zeros_(conv2.bias)
+
+            self.head.conv2 = conv2
