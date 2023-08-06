ee58f56f72fd354ccee812fd1526ba9ce1f92d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:55 +0000

    Add package dpt-xml/1.0.3/dpt-xml-1.0.3.tar.gz

---
+++ packages/dpt-xml/dpt-xml-1.0.3.tar.gz/dpt-xml-1.0.3/src/dpt_xml/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+direct Python Toolbox
+All-in-one toolbox to encapsulate Python runtime variants
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?dpt;xml
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+v1.0.3
+dpt_xml/__init__.py
+"""
+
+from .xml_parser import XmlParser
+from .xml_resource import XmlResource
