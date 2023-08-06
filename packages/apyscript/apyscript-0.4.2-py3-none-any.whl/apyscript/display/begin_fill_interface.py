49bd9fb786bd2650e41b695bf8264ec39efcba89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package apyscript/0.4.2/apyscript-0.4.2-py3-none-any.whl

---
+++ packages/apyscript/apyscript-0.4.2-py3-none-any.whl/apyscript/display/begin_fill_interface.py
@@ -0,0 +1,70 @@
+"""Class implementation for begin_fill method related interface.
+
+See Also
+--------
+- graphics_clear_interface
+"""
+
+
+from typing import Optional
+from typing import Union
+
+from apyscript.color import color_util
+from apyscript.converter import cast
+from apyscript.type import Number
+from apyscript.validation import color_validation
+from apyscript.validation import number_validation
+
+
+class BiginFillInterface:
+
+    _fill_color: Optional[str] = None
+    _fill_alpha: Number = Number(1.0)
+
+    def begin_fill(
+            self, color: str,
+            alpha: Union[float, Number] = 1.0) -> None:
+        """
+        Set single color value for fill.
+
+        Parameters
+        ----------
+        color : str
+            Hexadecimal color string. e.g., '#00aaff'
+        alpha : float or Number, default 1.0
+            Color opacity (0.0 to 1.0).
+        """
+        color = color_util.complement_hex_color(hex_color_code=color)
+        self._fill_color = color
+        number_validation.validate_num(num=alpha)
+        if not isinstance(alpha, Number):
+            alpha = cast.to_float_from_int(int_or_float=alpha)
+        else:
+            alpha = alpha.value
+        color_validation.validate_alpha_range(alpha=alpha)
+        self._fill_alpha = Number(value=alpha)
+
+    @property
+    def fill_color(self) -> Optional[str]:
+        """
+        Get current fill color.
+
+        Returns
+        -------
+        fill_color : str or None
+            Current fill color (hexadecimal string, e.g., '#00aaff').
+            If not be set, None will be returned.
+        """
+        return self._fill_color
+
+    @property
+    def fill_alpha(self) -> Number:
+        """
+        Get current fill color opacity.
+
+        Returns
+        -------
+        fill_alpha : Number
+            Current fill color opacity (0.0 to 1.0).
+        """
+        return self._fill_alpha
---
+++ packages/apyscript/apyscript-0.4.2-py3-none-any.whl/apyscript/display/graphics_clear_interface.py
@@ -0,0 +1,32 @@
+"""Class implementation for graphics clear method related interface.
+"""
+
+
+from typing import List
+from typing import Optional
+
+from apyscript.display.display_object import DisplayObject
+from apyscript.type import Int
+from apyscript.type import Number
+
+
+class GraphicsClearInterface:
+
+    _fill_color: Optional[str] = None
+    _fill_alpha: Number
+    _line_color: Optional[str] = None
+    _line_thickness: Int = Int(1)
+    _line_alpha: Number
+    _childs: List[DisplayObject]
+
+    def clear(self) -> None:
+        """
+        Clear all graphics and reset fill and line settings.
+        """
+        self._fill_color = None
+        self._fill_alpha = Number(1.0)
+        self._line_color = None
+        self._line_thickness = Int(1)
+        self._line_alpha = Number(1.0)
+        while self._childs:
+            self._childs[0].remove_from_parent()
