e4b78f1ce1d4bfa620d94c743c4d6018dfae04df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package gallerist/0.0.5/gallerist-0.0.5.tar.gz

---
+++ packages/gallerist/gallerist-0.0.5.tar.gz/gallerist-0.0.5/gallerist/abc.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+
+class FileInfo:
+
+    __slots__ = ("mime", "extension")
+
+    def __init__(self, mime: str, extension: str):
+        self.mime = mime
+        self.extension = extension
+
+    def __repr__(self):
+        return f'<FileInfo mime="{self.mime}" extension="{self.extension}">'
+
+    def to_dict(self):
+        return {"mime": self.mime, "extension": self.extension}
+
+
+class FileStore(ABC):
+    """A store that operates on files asynchronously."""
+
+    @abstractmethod
+    async def read_file(self, file_path: str) -> bytes:
+        """Returns file contents by its unique path"""
+
+    @abstractmethod
+    async def write_file(self, file_path: str, data: bytes, info: FileInfo):
+        """Writes a file to a given unique path"""
+
+    @abstractmethod
+    async def delete_file(self, file_path: str):
+        """Deletes a file"""
+
+
+class SyncFileStore(ABC):
+    """A store that operates on files synchronously."""
+
+    @abstractmethod
+    def read_file(self, file_path: str) -> bytes:
+        """Returns file contents by its unique path"""
+
+    @abstractmethod
+    def write_file(self, file_path: str, data: bytes, info: FileInfo):
+        """Writes a file to a given unique path"""
+
+    @abstractmethod
+    def delete_file(self, file_path: str):
+        """Deletes a file"""
+
+
+FileStoreType = Union[FileStore, SyncFileStore]
