7b5bccc95071dbb6268bfbefd102cb398edf6e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:22 +0000

    Add package done-xblock/2.0.4/done-xblock-2.0.4.tar.gz

---
+++ packages/done-xblock/done-xblock-2.0.4.tar.gz/done-xblock-2.0.4/setup.py
@@ -0,0 +1,66 @@
+"""Setup for done XBlock."""
+
+
+import os
+
+from setuptools import setup
+
+
+def package_data(pkg, root):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for dirname, _, files in os.walk(os.path.join(pkg, root)):
+        for fname in files:
+            data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+setup(
+    name='done-xblock',
+    version='2.0.4',
+    description='done XBlock',   # TODO: write a better description.
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=[
+        'done',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    entry_points={
+        'xblock.v1': [
+            'done = done:DoneXBlock',
+        ]
+    },
+    package_data=package_data("done", "static"),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
