03fac599644316db93d70181afca7d0e7bf2ad54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:27 +0000

    Add package hypatie/2.5.1/hypatie-2.5.1.tar.gz

---
+++ packages/hypatie/hypatie-2.5.1.tar.gz/hypatie-2.5.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hypatie",
+    version="2.5.1",
+    author="Behrouz Safari",
+    author_email="behrouz.safari@gmail.com",
+    description="A python package for astronomical calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/behrouzz/hypatie",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["hypatie"],
+    include_package_data=True,
+    install_requires=["numpy", "matplotlib","pandas"],
+    python_requires='>=3.4',
+)
