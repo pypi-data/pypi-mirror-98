3b9d3d379effa3d0beff96205b005934c4e832e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:28 +0000

    Add package play-sounds/0.4.3/play_sounds-0.4.3.tar.gz

---
+++ packages/play-sounds/play_sounds-0.4.3.tar.gz/play_sounds-0.4.3/play_sounds/base.py
@@ -0,0 +1,161 @@
+from contextlib import contextmanager, asynccontextmanager, \
+    AbstractContextManager, AbstractAsyncContextManager
+from typing import Callable, AsyncContextManager, Any, \
+    ContextManager, Awaitable, Optional, Set
+from multiprocessing import Process
+from platform import platform
+from pathlib import Path
+from asyncio import sleep
+import logging
+import sys
+
+from .wrap import to_thread
+from .proc import play_process, kill_process
+
+
+BLOCK_WHILE_PLAYING = True
+PROCS = 1
+DEFAULT_WAIT: float = 0.25
+
+
+def get_assets_dir() -> Path:
+  filename = __loader__.get_filename()
+  return Path(filename).parent / 'assets'
+
+
+DEFAULT_ASSETS = get_assets_dir()
+DEFAULT_SONG = DEFAULT_ASSETS / 'song.mp3'
+DEFAULT_SOUND = DEFAULT_ASSETS / 'ding.mp3'
+
+PLATFORM = platform().lower()
+MAJOR, MINOR, *_ = sys.version_info
+
+
+if 'windows' in PLATFORM or 'nt' in PLATFORM:
+  from playsound import playsound
+
+  def play_file(file: Path, block: bool = BLOCK_WHILE_PLAYING):
+    filename = str(file.absolute())
+    playsound(filename, block=block)
+
+    if not block:
+      logging.warning("Playback must block on Windows.")
+
+else:
+  from boombox import BoomBox
+
+  def play_file(file: Path, block: bool = BLOCK_WHILE_PLAYING):
+    player = BoomBox(file, wait=block)
+    player.play()
+
+
+def play_loop(file: Path, block: bool = True):
+  try:
+    while True:
+      play_file(file, block)
+
+  except Exception as e:
+    logging.error(f"Error while trying to play {file}: {e}")
+
+
+@contextmanager
+def play_while_running(
+  file: Path,
+  block: bool = BLOCK_WHILE_PLAYING,
+  loop: bool = True
+) -> ContextManager[Process]:
+  play_func = play_loop if loop else play_file
+  proc = play_process(file, target=play_func, block=block)
+
+  try:
+    yield proc
+
+  finally:
+    kill_process(proc)
+
+
+@contextmanager
+def play_after(
+  file: Path,
+  block: bool = BLOCK_WHILE_PLAYING
+) -> ContextManager[Path]:
+  try:
+    yield file
+
+  finally:
+    play_file(file, block)
+
+
+async def play_file_async(
+  file: Path,
+  block: bool = BLOCK_WHILE_PLAYING,
+  loop: bool = False,
+  interval: float = DEFAULT_WAIT,
+):
+  play_func = play_loop if loop else play_file
+
+  try:
+    proc = await to_thread(play_process, file, block=block, target=play_func)
+
+    while proc.is_alive():
+      await sleep(interval)
+
+  finally:
+    await to_thread(kill_process, proc)
+
+
+@asynccontextmanager
+async def play_while_running_async(
+  file: Path,
+  block: bool = BLOCK_WHILE_PLAYING,
+  loop: bool = True
+) -> AsyncContextManager[Process]:
+  play_func = play_loop if loop else play_file
+
+  try:
+    proc = await to_thread(play_process, file, block=block, target=play_func)
+    yield proc
+
+  finally:
+    await to_thread(kill_process, proc)
+
+
+@asynccontextmanager
+async def play_after_async(
+  file: Path,
+  block: bool = BLOCK_WHILE_PLAYING,
+  loop: bool = False,
+  interval: float = DEFAULT_WAIT,
+) -> AsyncContextManager[Path]:
+  try:
+    yield file
+
+  finally:
+    await play_file_async(file, block, loop, interval)
+
+
+#@dataclass
+#class play_sound(AbstractContextManager, AbstractAsyncContextManager):
+  #def __call__(self, func: Callable) -> Callable:
+    #if iscoroutinefunction(func):
+      #@wraps(func)
+      #async def new_coro(*args, **kwargs) -> Awaitable[Any]:
+        #async with self as proc:
+          #pass
+    #with play_while_running(file) as proc:
+      #return func
+
+  #def __enter__(self) -> ContextManager[Process]:
+    #with play_while_running(file) as proc:
+      #return proc
+
+  #def __exit__(self, *args):
+    #pass
+
+  #async def __aenter__(self) -> Awaitable[AsyncContextManager[Process]]:
+    #with play_while_running_async(file) as proc:
+      #return proc
+
+
+  #async def __aexit__(self, *args):
+    #pass
---
+++ packages/play-sounds/play_sounds-0.4.3.tar.gz/play_sounds-0.4.3/play_sounds/proc.py
@@ -0,0 +1,103 @@
+from typing import Callable, Optional, Set, Any
+from functools import partial
+from multiprocessing import Process
+from platform import platform
+from pathlib import Path
+from weakref import finalize as finalizer
+from sys import exit
+import signal
+import atexit
+
+PLATFORM: str = platform().lower()
+_PROCS: Set[Process] = set()
+
+_SIGINT: Callable = signal.getsignal(signal.SIGINT)
+
+
+def play_process(
+  file: Path,
+  target: Callable,
+  *args,
+  finalize: bool = True,
+  running_procs: Optional[Set[Process]] = _PROCS,
+  **kwargs,
+) -> Process:
+  proc = Process(
+    target=target,
+    args=(file, *args),
+    kwargs=kwargs,
+    daemon=True
+  )
+
+  if finalize:
+    finalizer(proc, kill_process, proc=proc, running_procs=running_procs)
+
+  if running_procs is not None:
+    running_procs.add(proc)
+
+  proc.start()
+
+  return proc
+
+
+def kill_process(
+  proc: Process,
+  running_procs: Optional[Set[Process]] = _PROCS
+):
+  proc.kill()
+  proc.join()
+
+  if running_procs and proc in running_procs:
+    running_procs.remove(proc)
+
+
+def kill_child_procs(
+  signum: Optional[int] = None,
+  frame: Optional[Any] = None,
+  perform_exit: bool = True
+):
+  if _PROCS:
+    for proc in _PROCS.copy():
+      try:
+        kill_process(proc)
+
+      except:
+        pass
+
+  if perform_exit:
+    exit()
+
+
+kill_procs_no_exit: Callable = \
+  partial(kill_child_procs, perform_exit=False)
+
+
+def handle_sigint(
+  signum: Optional[int] = None,
+  frame: Optional[Any] = None,
+):
+  kill_procs_no_exit()
+  _SIGINT(signum, frame)
+
+
+def register_handlers():
+  # handle graceful shutdown
+  atexit.register(kill_procs_no_exit)
+
+  # allow users to catch KeyboardInterrupt without exiting
+  signal.signal(signal.SIGINT, handle_sigint)
+
+  # handle ungraceful shutdown
+  signal.signal(signal.SIGTERM, kill_child_procs)
+  signal.signal(signal.SIGSEGV, kill_child_procs)
+  signal.signal(signal.SIGABRT, kill_child_procs)
+  signal.signal(signal.SIGILL, kill_child_procs)
+
+  if 'windows' not in PLATFORM:
+    signal.signal(signal.SIGPIPE, kill_child_procs)
+    signal.signal(signal.SIGQUIT, kill_child_procs)
+    signal.signal(signal.SIGBUS, kill_child_procs)
+    signal.signal(signal.SIGHUP, kill_child_procs)
+
+
+register_handlers()
