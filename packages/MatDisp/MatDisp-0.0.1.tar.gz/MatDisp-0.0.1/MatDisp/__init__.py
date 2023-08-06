1d70adf404e1e74df57b38e30b0164a11001e2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:32 +0000

    Add package MatDisp/0.0.1/MatDisp-0.0.1.tar.gz

---
+++ packages/MatDisp/MatDisp-0.0.1.tar.gz/MatDisp-0.0.1/MatDisp/__init__.py
@@ -0,0 +1,19 @@
+from IPython.core.interactiveshell import InteractiveShell
+import numpy as np
+
+IShell = InteractiveShell.instance()
+
+def vec2str_lis(vector):
+    return map(lambda x:str(round(x)),vector)
+
+def mat2md(M:np.ndarray):
+    head = r'\begin{bmatrix}'
+    tail = r'\end{bmatrix}'
+    if M.ndim == 1:
+        content = r'\\'.join(vec2str_lis(M))
+        return f'{head}{content}{tail}'
+    if M.ndim == 2:
+        content = r'\\'.join(['&'.join(vec2str_lis(vec)) for vec in M])
+        return f'{head}{content}{tail}'
+    return repr(M)
+    
