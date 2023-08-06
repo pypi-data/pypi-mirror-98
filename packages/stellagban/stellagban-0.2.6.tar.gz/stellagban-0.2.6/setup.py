ceac39aa7ed34a83487ed3fc8e0f4450712ecf55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:36 +0000

    Add package stellagban/0.2.6/stellagban-0.2.6.tar.gz

---
+++ packages/stellagban/stellagban-0.2.6.tar.gz/stellagban-0.2.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from stellagban import __version__
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='stellagban',
+    version=__version__,
+    description='StellaGban API Python Wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='Anil Chauhan',
+    author_email='anilchauhanxda@gmail.com',
+    url='https://github.com/SpookyGang/StellaGban-py',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Typing :: Typed'
+    ],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.6'
+)
