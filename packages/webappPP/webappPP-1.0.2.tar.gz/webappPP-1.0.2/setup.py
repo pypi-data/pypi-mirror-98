ddb9fd3e7e410b4afd65d96a095a03c7878a6e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:51 +0000

    Add package webappPP/1.0.2/webappPP-1.0.2.tar.gz

---
+++ packages/webappPP/webappPP-1.0.2.tar.gz/webappPP-1.0.2/setup.py
@@ -0,0 +1,15 @@
+import pathlib
+from setuptools import setup
+HERE = pathlib.Path(__file__).parent
+README = (HERE/"README.md").read_text()
+setup(
+    name="webappPP",
+    version = "1.0.2",
+    description="Algorithm for finance",
+    long_description =README,
+    long_description_content_type ="text/markdown",
+    license ="MIT",
+    packages = ["backtest"],
+    install_requires=["pandas","numpy","pandas_datareader", "scipy","Flask", "seaborn"],
+    entry_points={"console_scripts":["finance=finance.__main__:main"]}
+)
