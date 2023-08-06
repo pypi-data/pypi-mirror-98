b13442c575abd1aad51d45b6b031a5a929965e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:10 +0000

    Add package SQLAlchemy-FullText-Search-Ngram/0.1.1/SQLAlchemy-FullText-Search-Ngram-0.1.1.tar.gz

---
+++ packages/SQLAlchemy-FullText-Search-Ngram/SQLAlchemy-FullText-Search-Ngram-0.1.1.tar.gz/SQLAlchemy-FullText-Search-Ngram-0.1.1/setup.py
@@ -0,0 +1,32 @@
+"""
+SQLAlchemy FullText Search
+"""
+
+from setuptools import setup, Command
+
+
+setup(
+        name='SQLAlchemy-FullText-Search-Ngram',
+        version='0.1.1',
+        url='https://github.com/mengzhuo/sqlalchemy-fulltext-search',
+        license='BSD',
+        author='Meng Zhuo, Alejandro Mesa',
+        author_email='mengzhuo1203@gmail.com, alejom99@gmail.com',
+        description=('Provide FullText for MYSQL & SQLAlchemy model'),
+        long_description = __doc__,
+        packages=['sqlalchemy_fulltext'],
+        zip_safe=False,
+        include_package_data=True,
+        platforms='any',
+        install_requires=['SQLAlchemy>=1.0',],
+            classifiers=[
+                        'Environment :: Web Environment',
+                        'Intended Audience :: Developers',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3.4',
+                        'Programming Language :: Python :: 3.6',
+                        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                        'Topic :: Software Development :: Libraries :: Python Modules'            ]
+)
