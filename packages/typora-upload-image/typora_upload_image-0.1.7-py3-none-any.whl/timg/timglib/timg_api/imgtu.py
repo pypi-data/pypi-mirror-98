707bc2c7ff43a83ab7df37adf33a17bece24427e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:54 +0000

    Add package typora-upload-image/0.1.7/typora_upload_image-0.1.7-py3-none-any.whl

---
+++ packages/typora-upload-image/typora_upload_image-0.1.7-py3-none-any.whl/timg/timglib/timg_api/imgtu.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: thepoy
+# @Email: thepoy@aliyun.com
+# @File Name: chr.py
+# @Created: 2021-02-13 09:04:37
+# @Modified: 2021-02-27 14:53:01
+
+import sys
+import os
+import re
+import time
+import json
+import mimetypes
+import requests
+
+from urllib import parse
+from typing import List, Optional, Tuple, Union
+
+from requests_toolbelt import MultipartEncoder
+
+from timg.timglib.timg_api import Base
+from timg.timglib.utils import Login, check_image_exists
+from timg.timglib.constants import IMGTU
+
+
+class Imgtu(Base):
+    def __init__(self,
+                 conf_file: Optional[str] = None,
+                 auto_compress: bool = False):
+        if not conf_file:
+            super().__init__(IMGTU)
+        else:
+            super().__init__(IMGTU, conf_file)
+        self.base_url: str = "https://imgtu.com/"
+        self.max_size = 10 * 1024 * 1024
+        self.auto_compress: bool = auto_compress
+
+        self.headers = {
+            "Accept":
+            "application/json",
+            "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0"
+        }
+
+        self.cookie: Optional[str] = None
+        self.token: Optional[str] = None
+        if self.auth_info:
+            self.cookie = self.auth_info["cookie"]
+            self.token = self.auth_info["token"]
+            self.username = self.auth_info["username"]
+            self.headers["Cookie"] = self.cookie
+
+    def login(self, username: str, password: str):
+        url = self._url("login")
+        auth_token, cookie = self._parse_auth_token()
+
+        headers = self.headers.copy()
+        headers["Cookie"] = cookie
+
+        data = {
+            "login-subject": username,
+            "password": password,
+            "auth_token": auth_token,
+        }
+
+        resp = requests.post(url,
+                             headers=headers,
+                             data=data,
+                             allow_redirects=False)
+        if resp.status_code == 301:
+            # If there is a KEEPLOGIN field in the cookie,
+            # pictures will be uploaded as a normal user
+            self.cookie = cookie + "; " + resp.headers["Set-Cookie"].split(
+                "; ")[0]
+
+            # If there is no KEEPLOGIN field in the cookie,
+            # pictures will be uploaded as a tourist
+            # self.cookie = cookie
+
+            auth_info = {
+                "token": auth_token,
+                "cookie": self.cookie,
+                "username": username,
+                "password": password
+            }
+            self._save_auth_info(auth_info)
+        else:
+            print("Error: Login failed.")
+            sys.exit(1)
+
+    def _parse_auth_token(self) -> Tuple[Optional[str], Optional[str]]:
+        url = self._url("login")
+        resp = requests.get(url, headers=self.headers)
+        if resp.status_code == 200:
+            auth_token = re.search(
+                r'PF.obj.config.auth_token = "([a-f0-9]{40})"', resp.text)
+            resp_set_cookie = resp.headers["Set-Cookie"].split("; ")[0]
+            return auth_token.group(1), resp_set_cookie
+        else:
+            print("响应错误:", resp.status_code)
+            return None, None
+
+    def _update_auth_token(self):
+        headers = {
+            "Accept": "application/json",
+            "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+            "Cookie": self.cookie,
+        }
+        resp = requests.get(self.base_url, headers=headers)
+        auth_token = re.search(r'PF.obj.config.auth_token = "([a-f0-9]{40})"',
+                               resp.text).group(1)
+        self.auth_info["token"] = self.token = auth_token
+        self._save_auth_info(self.auth_info)
+
+    @Login
+    def upload_image(self, image_path: str) -> Union[str, dict]:
+        image_path = self._compress_image(image_path)
+        url = self._url("json")
+        headers = {
+            "Accept": "application/json",
+            "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+            "Cookie": self.cookie,
+        }
+
+        filename_with_suffix = os.path.basename(image_path)
+        filename_without_suffix, suffix = os.path.splitext(
+            filename_with_suffix)
+        if suffix.lower() == '.apng':
+            suffix = ".png"
+        filename = filename_without_suffix + suffix
+
+        mime_type = mimetypes.guess_type(image_path)[0]
+
+        timestamp = int(time.time() * 1000)
+        data = MultipartEncoder({
+            "source": (filename, open(image_path, "rb"), mime_type),
+            "type":
+            "file",
+            "action":
+            "upload",
+            "timestamp":
+            str(timestamp),
+            "auth_token":
+            self.token,
+            "nsfw":
+            "0",
+        })
+        headers.update({"Content-Type": data.content_type})
+        resp = requests.post(url, headers=headers, data=data)
+        resp.encoding = "utf-8"
+        try:
+            return resp.json()["image"]["image"]["url"]
+        except KeyError:
+            if resp.json()["error"]["message"] == '请求被拒绝 (auth_token)':
+                print(
+                    "Warning: `auth_token` has expired, "
+                    "the program will try to update `auth_token` automatically."
+                )
+                self._update_auth_token()
+                return self.upload_image(image_path)
+            else:
+                return resp.json()
+
+    @Login
+    def upload_images(self, images_path: List[str]) -> list:
+        check_image_exists(images_path)
+
+        self._check_images_valid(images_path)
+
+        images_url = []
+        for img in images_path:
+            result = self.upload_image(img)
+            if type(result) == str:
+                images_url.append(result)
+            elif type(result) == dict:
+                images_url.append({
+                    "image_path": img,
+                    "status_code": result["status_code"],
+                    "error": result["error"]["message"],
+                })
+
+        for i in images_url:
+            print(i)
+
+        self._clear_cache()
+        return images_url
+
+    @Login
+    def get_all_images(self):
+        url = self._url(self.username)
+
+        # 集合去重
+        images = set()
+
+        def visit_next_page(url):
+            resp = requests.get(url, headers=self.headers)
+            resp.encoding = "utf-8"
+
+            # 只获取默认的公开相册里的图片
+            # images_object = re.findall(
+            #     r"data-privacy=\"public\" data-object='(.+?)'", resp.text)
+            # 获取用户所有图片
+            images_object = re.findall(r"data-object='(.+?)'", resp.text)
+            images_object = [
+                json.loads(parse.unquote(i)) for i in images_object
+            ]
+            for image in images_object:
+                images.add((
+                    image["url"],
+                    image["display_url"],
+                    image["id_encoded"],
+                    image["width"],
+                    image["height"],
+                ))
+                # images.append({
+                #     "url": image["url"],
+                #     "display_url": image["display_url"],
+                #     "id": image["id_encoded"],
+                #     "width": image["width"],
+                #     "height": image["height"],
+                # })
+
+            next_page_url = re.search(
+                r'data-pagination="next" href="(.+?)" ><span', resp.text)
+            if next_page_url:
+                next_page_url = next_page_url.group(1)
+            else:
+                next_page_url = ""
+
+            if next_page_url:
+                visit_next_page(next_page_url)
+
+        visit_next_page(url)
+
+        result = []
+
+        for item in images:
+            result.append({
+                "url": item[0],
+                "display_url": item[1],
+                "id": item[2],
+                "width": item[3],
+                "height": item[4],
+            })
+
+        return result
+
+    @Login
+    def delete_image(self, img_id: str):
+        url = self._url("json")
+        data = {
+            "auth_token": self.token,
+            "action": "delete",
+            "single": "true",
+            "delete": "image",
+            "deleting[id]": img_id,
+        }
+        resp = requests.post(url, headers=self.headers, data=data)
+        if resp.status_code == 400:
+            if resp.json()["error"]["message"] == '请求被拒绝 (auth_token)':
+                print(
+                    "Warning: `auth_token` has expired, "
+                    "the program will try to update `auth_token` automatically."
+                )
+                self._update_auth_token()
+                return self.delete_image(img_id)
+        return resp.json()
+
+    @Login
+    def delete_images(self, imgs_id: List[str]):
+        url = self._url("json")
+        data = {
+            "auth_token": self.token,
+            "action": "delete",
+            "from": "list",
+            "multiple": "true",
+            "delete": "images",
+            "deleting[ids][]": imgs_id,
+        }
+        resp = requests.post(url, headers=self.headers, data=data)
+        if resp.status_code == 400:
+            if resp.json()["error"]["message"] == '请求被拒绝 (auth_token)':
+                print(
+                    "Warning: `auth_token` has expired, "
+                    "the program will try to update `auth_token` automatically."
+                )
+                self._update_auth_token()
+                return self.delete_images(imgs_id)
+        return resp.json()
+
+    def _url(self, path: str) -> str:
+        return self.base_url + path
+
+    def __str__(self):
+        return "imgtu.com"
---
+++ packages/typora-upload-image/typora_upload_image-0.1.7-py3-none-any.whl/timg/timglib/timg_api/sm.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: thepoy
+# @Email: thepoy@aliyun.com
+# @File Name: sm.py
+# @Created: 2021-02-13 09:04:07
+# @Modified: 2021-02-25 18:38:54
+
+import requests
+
+from typing import List, Optional, Dict, Any
+
+from timg.timglib.timg_api import Base
+from timg.timglib.utils import Login, check_image_exists
+from timg.timglib import errors
+from timg.timglib.constants import SM_MS
+from timg.timglib.custom_types import DeletedResponse
+
+
+class SM(Base):
+    def __init__(self,
+                 conf_file: Optional[str] = None,
+                 auto_compress: bool = False):
+        if not conf_file:
+            super().__init__(SM_MS)
+        else:
+            super().__init__(SM_MS, conf_file)
+        self.base_url = "https://sm.ms/api/v2/"
+        self.max_size = 5 * 1024 * 1024
+        self.auto_compress: bool = auto_compress
+
+        if self.auth_info:
+            self.token: str = self.auth_info["token"]
+
+            self.headers = {
+                "Content-Type": "multipart/form-data",
+                "Authorization": self.token
+            }
+
+    def login(self, username: str, password: str):
+        token = self._get_api_token(username, password)
+        self._save_auth_info({
+            "token": token,
+            "username": username,
+            "password": password
+        })
+
+    def _get_api_token(self, username: str, password: str) -> str:
+        url = self._url("token")
+        data = {"username": username, "password": password}
+        resp = requests.post(url, data=data).json()
+        return resp["data"]["token"]
+
+    def _get_user_profile(self) -> Dict[str, str]:
+        url = self._url("profile")
+        resp = requests.post(url, headers=self.headers).json()
+        if resp["success"]:
+            return resp["data"]
+        else:
+            if self._login_expired(resp):
+                self._auto_login()
+                self.token = self.auth_info["token"]
+
+    @Login
+    def upload_image(self, image_path: str):
+        # sm.ms不管出不出错，返回的状态码都是200
+        url = self._url("upload")
+        headers = {"Authorization": self.token}
+        files = {"smfile": open(image_path, "rb")}
+        resp = requests.post(url, headers=headers, files=files).json()
+        if resp["success"]:
+            return resp["data"]["url"]
+        else:
+            if resp["code"] == "image_repeated":
+                # 如果图片重复，会返回重复的图片的链接，所以此处不报错
+                return resp["images"]
+            elif self._login_expired(resp):
+                self._auto_login()
+                self.token = self.auth_info["token"]
+            else:
+                raise errors.UploadFailed(resp["message"])
+
+    @Login
+    def upload_images(self, images_path: List[str]) -> List[str]:
+        if len(images_path) > 10:
+            raise errors.OverSizeError(
+                "You can only upload up to 10 pictures, but you uploaded %d pictures."
+                % len(images_path))
+
+        check_image_exists(images_path)
+
+        self._check_images_valid(images_path)
+
+        images_url = []
+        for img in images_path:
+            try:
+                result = self.upload_image(img)
+            except errors.UploadFailed as e:
+                result = {
+                    "image_path": img,
+                    "status_code": 400,
+                    "error": f"{e}",
+                }
+
+            images_url.append(result)
+
+        for i in images_url:
+            print(i)
+
+        self._clear_cache()
+
+        return images_url
+
+    @Login
+    def history(self) -> Dict[str, Any]:
+        """
+        Temporary History - IP Based Temporary Upload History
+        """
+        url = self._url("history")
+        resp = requests.get(url, headers=self.headers)
+        return resp.json()
+
+    @Login
+    def clear(self) -> Dict[str, Any]:
+        """
+        Clear Temporary History - Clear IP Based Temporary Upload History
+        """
+        url = self._url("clear")
+        resp = requests.get(url, headers=self.headers)
+        return resp.json()
+
+    @Login
+    def upload_history(self) -> dict:
+        url = self._url("upload_history")
+        resp = requests.get(url, headers=self.headers)
+        return resp.json()
+
+    @Login
+    def get_all_images(self) -> List[Dict[str, str]]:
+        images = []
+        for file in self.upload_history()["data"]:
+            images.append({
+                "url": file["url"],
+                "delete_url": file["delete"],
+                "width": file["width"],
+                "height": file["height"],
+            })
+
+        return images
+
+    @Login
+    def delete_image(self, delete_url: str) -> DeletedResponse:
+        resp = requests.get(delete_url, headers=self.headers)
+        # TODO: sm.ms 删除图片后本应返回json，但实际返回的是html。而且删除链接不需要认证，任何人get都能删除图片
+        return resp.status_code == 200
+
+    @Login
+    def delete_images(self, urls: List[str]) -> dict:
+        result = {}
+        for url in urls:
+            r = self.delete_image(url)
+            # if not r["success"]:
+            #     result[hash] = {"message": r["message"], "code": r["code"]}
+            result[url] = r
+        return result
+
+    @Login
+    def get_all_images_in_image_bed(self) -> List[str]:
+        pass
+
+    def _url(self, path: str) -> str:
+        return self.base_url + path
+
+    def _login_expired(self, resp: dict):
+        return resp[
+            "message"] == "Get user profile failed, invalid Authorization."
+
+    def __str__(self):
+        return "sm.ms"
