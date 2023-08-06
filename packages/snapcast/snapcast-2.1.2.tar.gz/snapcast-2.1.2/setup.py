d1ac569a83be8253af33ec2b9113059b91af2e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:29 +0000

    Add package snapcast/2.1.2/snapcast-2.1.2.tar.gz

---
+++ packages/snapcast/snapcast-2.1.2.tar.gz/snapcast-2.1.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='snapcast',
+    version='2.1.2',
+    description='Control Snapcast.',
+    url='https://github.com/happyleavesaoc/python-snapcast/',
+    license='MIT',
+    author='happyleaves',
+    author_email='happyleaves.tfr@gmail.com',
+    packages=['snapcast', 'snapcast.control', 'snapcast.client'],
+    install_requires=[
+        'construct>=2.5.2',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
