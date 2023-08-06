483e5c5fac9b53da3a24c0d4b00d31f19288608f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package umm/0.5.0/umm-0.5.0.tar.gz

---
+++ packages/umm/umm-0.5.0.tar.gz/umm-0.5.0/src/umm/C.py
@@ -0,0 +1,33 @@
+import os
+
+import yaml
+
+# Package meta-data.
+VERSION = '0.5.0'
+NAME = 'umm'
+DESCRIPTION = 'A toolkit to manager the fastest mirror of various tools, such as pip, npm, composer and etc.'
+URL = 'https://github.com/xiyusullos/umm'
+EMAIL = 'i@aponder.top'
+AUTHOR = 'aponder'
+
+# What packages are required for this module to be executed?
+REQUIRES = [
+    'click',
+    'PyYAML',
+]
+# Package meta-data. #end#
+
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.getenv('HOME')
+VIRTUAL_ENV = os.getenv('VIRTUAL_ENV', HOME)
+
+MIRRORS = yaml.safe_load(open(os.sep.join([HERE, 'mirrors.yml'])))
+
+
+# some constants
+INVALID_MIRROR_NAME = 'Invalid mirror name'
+
+if __name__ == '__main__':
+    print(MIRRORS)
