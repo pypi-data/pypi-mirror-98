5573ba648583172e85219559361e696b8bb6b195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package setuptools-odoo/2.7.0/setuptools_odoo-2.7.0-py2.py3-none-any.whl

---
+++ packages/setuptools-odoo/setuptools_odoo-2.7.0-py2.py3-none-any.whl/setuptools_odoo/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2015 ACSONE SA/NV
+# License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+
+from .core import get_addon_metadata
+
+
+__all__ = [get_addon_metadata.__name__]
