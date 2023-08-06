c259834fd05b5d84cd84d2b1a6bee612b5fa3e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:53 +0000

    Add package streamlit-tags/1.0.3/streamlit_tags-1.0.3-py3-none-any.whl

---
+++ packages/streamlit-tags/streamlit_tags-1.0.3-py3-none-any.whl/streamlit_tags/frontend/build/precache-manifest.2cda96ed130385a747d1f267b941955a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d962c17f87601be9c6857e963b0f96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e08877bf3cc3e4b7d548",
+    "url": "./static/css/main.f4ca8bb4.chunk.css"
+  },
+  {
+    "revision": "1b1e1264d4791e068a2e",
+    "url": "./static/js/2.7303cafd.chunk.js"
+  },
+  {
+    "revision": "9bf7777849ff7c0e7b1f4ddb70e6eb95",
+    "url": "./static/js/2.7303cafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08877bf3cc3e4b7d548",
+    "url": "./static/js/main.509236ff.chunk.js"
+  },
+  {
+    "revision": "4b3ba0d9c283ff9f3fb6",
+    "url": "./static/js/runtime-main.488f8084.js"
+  }
+]);
