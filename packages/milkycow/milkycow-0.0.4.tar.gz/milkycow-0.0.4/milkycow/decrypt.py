e374b26713a4a6065f16c4e9a17f79fb0373554b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:41 +0000

    Add package milkycow/0.0.4/milkycow-0.0.4.tar.gz

---
+++ packages/milkycow/milkycow-0.0.4.tar.gz/milkycow-0.0.4/milkycow/decrypt.py
@@ -0,0 +1,34 @@
+from hashlib import sha1
+from random import randint, choice
+
+def dec(string, passwd='pass ', rotate=500):
+    try:
+        passwd = sha1((str(passwd)+"#@").encode()).hexdigest()
+        pas = 0
+        mkvsp = 1
+        tmp = ""
+        mkstr = string[14:-6]
+
+        for i in range(1, 11):
+            if(rotate%i == 0):
+                mkvsp = i
+
+        for i in passwd:
+            if(i.isdigit()):
+                if(i == mkvsp):
+                    continue
+                pas += ord(i)
+            else:
+                pas -= ord(i)
+            while(pas <= 0):
+                pas += len(passwd)+ord(i)
+
+        for i in range(len(mkstr)):
+            if(i%(mkvsp+1) == 0):
+                tmp += chr(ord(mkstr[i])-rotate-pas)
+
+    except:
+        tmp = enc("".join([chr(randint(40, 255)) for x in range(len(str(string))-20)]), ""*randint(1, 10), rotate)
+    return tmp
+
+
