af36d2f768a40d860dc4b217dce47d18ba23a270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:13 +0000

    Add package geoserver-rest/1.5.4/geoserver-rest-1.5.4.tar.gz

---
+++ packages/geoserver-rest/geoserver-rest-1.5.4.tar.gz/geoserver-rest-1.5.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geoserver-rest",
+    version="1.5.4",
+    author="Tek Kshetri",
+    author_email="iamtekson@gmail.com",
+    description="Package for GeoServer rest API",
+    py_modules=['geoserver-rest-python'],
+    # package_dir={'':'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamtekson/geoserver-rest-python",
+    packages=['geo'],
+    keywords=['geoserver-rest-python', 'geoserver rest', 'python geoserver', 'geoserver api',
+              'api', 'rest geoserver', 'python', 'geoserver python', 'geoserver rest'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pycurl',
+        'seaborn',
+        'gdal',
+        'psycopg2',
+    ],
+    python_requires='>=3.6',
+)
