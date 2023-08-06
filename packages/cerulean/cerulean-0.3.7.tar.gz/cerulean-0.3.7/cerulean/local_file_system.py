38708971a653bcc2f519f3a2c9c8506ff5ad20bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package cerulean/0.3.7/cerulean-0.3.7.tar.gz

---
+++ packages/cerulean/cerulean-0.3.7.tar.gz/cerulean-0.3.7/cerulean/local_file_system.py
@@ -0,0 +1,221 @@
+import errno
+import os
+import pathlib
+from typing import cast, Any, Generator, Iterable, Optional
+
+from cerulean.file_system import FileSystem
+from cerulean.file_system_impl import FileSystemImpl
+from cerulean.path import AbstractPath, EntryType, Path, Permission
+
+
+class LocalFileSystem(FileSystemImpl):
+    """Represents the local file system.
+
+    To create an instance, just call `LocalFileSystem()`.
+
+    LocalFileSystem support a single operation:
+
+    .. code-block:: python
+
+      fs / 'path'
+
+    which produces a :class:`Path`, through which you can do things \
+    with local files.
+
+    LocalFileSystem is a context manager, so you can use it in a \
+    ``with`` statement, and it has a :meth:`close` method, but since \
+    it doesn't hold any resources, you do not need to use them. It may \
+    be good to do so anyway, to avoid leaks if you ever replace it with \
+    a different :class:`FileSystem` that does.
+
+    """
+    def __eq__(self, other: Any) -> bool:
+        """Return True iff this file system equals other."""
+        if not isinstance(other, FileSystem):
+            return NotImplemented
+        return isinstance(other, LocalFileSystem)
+
+    def root(self) -> Path:
+        return Path(self, pathlib.Path('/'))
+
+    def __truediv__(self, segment: str) -> Path:
+        # TODO: segment: Union[str, pathlib.Path]?
+        absseg = '/' + segment.strip('/')
+        path = pathlib.Path(absseg)
+        return Path(self, path)
+
+    def _supports(self, feature: str) -> bool:
+        if feature not in self._features:
+            raise ValueError('Invalid argument for "feature"')
+        return True
+
+    def _exists(self, path: AbstractPath) -> bool:
+        lpath = cast(pathlib.Path, path)
+        try:
+            return lpath.exists()
+        except OSError as e:
+            if e.errno == errno.ELOOP:
+                return False
+            raise
+
+    def _mkdir(self,
+              path: AbstractPath,
+              mode: Optional[int] = None,
+              parents: bool = False,
+              exists_ok: bool = False) -> None:
+        lpath = cast(pathlib.Path, path)
+        if mode is not None:
+            lpath.mkdir(mode, parents, exists_ok)
+        else:
+            lpath.mkdir(parents=parents, exist_ok=exists_ok)
+
+    def _iterdir(self, path: AbstractPath) -> Generator[AbstractPath, None, None]:
+        lpath = cast(pathlib.Path, path)
+        for entry in lpath.iterdir():
+            yield entry
+
+    def _rmdir(self, path: AbstractPath, recursive: bool = False) -> None:
+        lpath = cast(pathlib.Path, path)
+        if not lpath.is_dir():
+            raise RuntimeError('Path must refer to a directory')
+
+        if recursive:
+            for entry in lpath.iterdir():
+                if entry.is_symlink():
+                    entry.unlink()
+                elif entry.is_dir():
+                    self._rmdir(entry, True)
+                else:
+                    entry.unlink()
+
+        lpath.rmdir()
+
+    def _touch(self, path: AbstractPath) -> None:
+        lpath = cast(pathlib.Path, path)
+        lpath.touch()
+
+    def _streaming_read(self, path: AbstractPath) -> Generator[bytes, None, None]:
+        # Buffer size vs. speed (MB/s) against localhost
+        #       up      down        local
+        # 8k    33      56          159
+        # 16k   52      56          145
+        # 24k   66      57          150
+        # 32k   24      57          149
+        # 2M    24      48
+        # scp   120     110
+        # cp                        172
+        lpath = cast(pathlib.Path, path)
+        with lpath.open('rb') as f:
+            data = f.read(24576)
+            while len(data) > 0:
+                yield data
+                data = f.read(24576)
+
+    def _streaming_write(self, path: AbstractPath, data: Iterable[bytes]) -> None:
+        lpath = cast(pathlib.Path, path)
+        with lpath.open('wb') as f:
+            for chunk in data:
+                f.write(chunk)
+
+    def _rename(self, path: AbstractPath, target: AbstractPath) -> None:
+        lpath = cast(pathlib.Path, path)
+        ltarget = cast(pathlib.Path, target)
+        lpath.replace(pathlib.Path(ltarget))
+
+    def _unlink(self, path: AbstractPath) -> None:
+        lpath = cast(pathlib.Path, path)
+        lpath.unlink()
+
+    def _is_dir(self, path: AbstractPath) -> bool:
+        lpath = cast(pathlib.Path, path)
+        return lpath.is_dir()
+
+    def _is_file(self, path: AbstractPath) -> bool:
+        lpath = cast(pathlib.Path, path)
+        return lpath.is_file()
+
+    def _is_symlink(self, path: AbstractPath) -> bool:
+        lpath = cast(pathlib.Path, path)
+        return lpath.is_symlink()
+
+    def _entry_type(self, path: AbstractPath) -> EntryType:
+        lpath = cast(pathlib.Path, path)
+        # Note: symlink goes first, because is_dir() and is_file() will
+        # dereference and return true, while we want to say it's a
+        # symlink and leave it at that.
+        pred_to_type = [(pathlib.Path.is_symlink, EntryType.SYMBOLIC_LINK),
+                        (pathlib.Path.is_dir, EntryType.DIRECTORY),
+                        (pathlib.Path.is_file, EntryType.FILE),
+                        (pathlib.Path.is_char_device,
+                            EntryType.CHARACTER_DEVICE),
+                        (pathlib.Path.is_block_device, EntryType.BLOCK_DEVICE),
+                        (pathlib.Path.is_fifo, EntryType.FIFO),
+                        (pathlib.Path.is_socket, EntryType.SOCKET)]
+
+        if not self._exists(lpath):
+            raise OSError(errno.ENOENT, 'No such file or directory',
+                          str(lpath))
+        for pred, entry_type in pred_to_type:
+            if pred(lpath):
+                return entry_type
+        raise RuntimeError('Object is of unknown type, please report a'
+                           'Cerulean bug')
+
+    def _size(self, path: AbstractPath) -> int:
+        lpath = cast(pathlib.Path, path)
+        return lpath.stat().st_size
+
+    def _uid(self, path: AbstractPath) -> int:
+        lpath = cast(pathlib.Path, path)
+        return lpath.stat().st_uid
+
+    def _gid(self, path: AbstractPath) -> int:
+        lpath = cast(pathlib.Path, path)
+        return lpath.stat().st_gid
+
+    def _has_permission(self, path: AbstractPath, permission: Permission) -> bool:
+        lpath = cast(pathlib.Path, path)
+        return bool(lpath.stat().st_mode & permission.value)
+
+    def _set_permission(self, path: AbstractPath, permission: Permission,
+                       value: bool = True) -> None:
+        lpath = cast(pathlib.Path, path)
+        mode = lpath.stat().st_mode
+        if value:
+            mode = mode | permission.value
+        else:
+            mode = mode & ~permission.value
+
+        self._chmod(lpath, mode)
+
+    def _chmod(self, path: AbstractPath, mode: int) -> None:
+        lpath = cast(pathlib.Path, path)
+        lpath.chmod(mode)
+
+    def _symlink_to(self, path: AbstractPath, target: AbstractPath) -> None:
+        lpath = cast(pathlib.Path, path)
+        ltarget = cast(pathlib.Path, target)
+        lpath.symlink_to(ltarget)
+
+    def _readlink(self, path: AbstractPath, recursive: bool) -> Path:
+        lpath = cast(pathlib.Path, path)
+        if recursive:
+            # pathlib.Path.resolve() raises if the link is broken
+            # we don't want that, so use our own implementation
+            max_iter = 32
+            cur_path = lpath
+            iter_count = 0
+            while cur_path.is_symlink() and iter_count < max_iter:
+                target = pathlib.Path(os.readlink(str(cur_path)))
+                if not target.is_absolute():
+                    target = cur_path.parent / target
+                cur_path = target
+                iter_count += 1
+
+            if iter_count == max_iter:
+                raise RuntimeError('Too many symbolic links detected')
+
+            return Path(self, cur_path)
+        else:
+            return Path(self, pathlib.Path(
+                os.readlink(str(lpath))))
---
+++ packages/cerulean/cerulean-0.3.7.tar.gz/cerulean-0.3.7/cerulean/slurm_scheduler.py
@@ -0,0 +1,215 @@
+import logging
+import time
+from typing import Optional, Union
+
+from cerulean.job_description import JobDescription
+from cerulean.job_status import JobStatus
+from cerulean.scheduler import Scheduler
+from cerulean.terminal import Terminal
+
+
+logger = logging.getLogger(__name__)
+
+
+class SlurmScheduler(Scheduler):
+    """Represents a Slurm scheduler.
+
+    This class represents a Slurm scheduler, to which it talks through \
+    a :class:`Terminal`.
+
+    On some machines, an additional command is needed to make Slurm
+    available to the user, e.g. 'module load slurm'. If you specify a
+    prefix, it will be prepended to any Slurm command run by this
+    class. Note that this is a plain string concatenation, so you'll
+    probably need something like 'module load slurm;', with a
+    semicolon to separate the commands.
+    """
+    def __init__(self, terminal: Terminal, prefix: str = '') -> None:
+        """Create ea SlurmScheduler.
+
+        Arguments:
+            terminal: The terminal to use to talk to the scheduler.
+            prefix: A string to prefix the SLURM commands with.
+
+        """
+        self.__terminal = terminal
+        self.__prefix = prefix
+
+        command = self.__prefix + ' sbatch'
+        exit_code, output, error = self.__terminal.run(10, command,
+                                                       ['--version'])
+        logger.debug('sbatch --version exit code: %s', exit_code)
+        logger.debug('sbatch --version output: %s', output)
+        logger.debug('sbatch --version error: %s', error)
+
+    def submit(self, job_description: JobDescription) -> str:
+        if job_description.command is None:
+            raise ValueError('Job description is missing a command')
+
+        job_script = _job_desc_to_job_script(job_description)
+        logger.debug('Submitting job script: %s', job_script)
+
+        command = self.__prefix + ' sbatch'
+        exit_code, output, error = self.__terminal.run(10, command, [],
+                                                       job_script, None)
+
+        logger.debug('sbatch exit code: %s', exit_code)
+        logger.debug('sbatch output: %s', output)
+        logger.debug('sbatch error: %s', error)
+        if exit_code != 0:
+            raise RuntimeError('Error running sbatch: {}'.format(error))
+
+        job_id = output.strip().split(' ')[-1]
+        return job_id
+
+    def get_status(self, job_id: str) -> JobStatus:
+        logger.debug('Calling squeue -j %s -h -o %%T', job_id)
+        command = self.__prefix + ' squeue'
+        exit_code, output, error = self.__terminal.run(
+            10, command, ['-j', job_id, '-h', '-o', '%T'], None, None)
+        logger.debug('squeue exit code: %s', exit_code)
+        logger.debug('squeue output: %s', output)
+        logger.debug('squeue error: %s', error)
+
+        status = output.strip()
+        if status == '':
+            # Seems like SLURM sometimes does not show the job, possibly
+            # because it's transitioning to COMPLETING. So
+            # if we don't find it, try again to be a bit more robust.
+            time.sleep(2.0)
+            logger.debug('No answer from Slurm, trying again...')
+            exit_code, output, error = self.__terminal.run(
+                10, command, ['-j', job_id, '-h', '-o', '%T'])
+            status = output.strip()
+            logger.debug('squeue output 2: %s', status)
+
+        status_map = {
+            'PENDING': JobStatus.WAITING,
+            'CONFIGURING': JobStatus.WAITING,
+            'RUNNING': JobStatus.RUNNING,
+            'SUSPENDED': JobStatus.RUNNING,
+            'COMPLETING': JobStatus.RUNNING,
+            'BOOT_FAIL': JobStatus.DONE,
+            'CANCELLED': JobStatus.DONE,
+            'COMPLETED': JobStatus.DONE,
+            'FAILED': JobStatus.DONE,
+            'TIMEOUT': JobStatus.DONE,
+            'PREEMPTED': JobStatus.DONE,
+            'NODE_FAIL': JobStatus.DONE,
+            'REVOKED': JobStatus.DONE,
+            'SPECIAL_EXIT': JobStatus.DONE
+        }
+        try:
+            job_status = status_map[status]
+        except KeyError:
+            job_status = JobStatus.DONE
+
+        logger.debug('get_status returning %s', job_status.name)
+
+        return job_status
+
+    def get_exit_code(self, job_id: str) -> Optional[int]:
+        if self.get_status(job_id) != JobStatus.DONE:
+            return None
+
+        logger.debug('get_exit_code() running sacct -j %s --noheader'
+                     ' --format=ExitCode', job_id)
+        command = self.__prefix + ' sacct'
+        err, output, error = self.__terminal.run(
+            10, command, ['-j', job_id, '--noheader', '--format=ExitCode'])
+        logger.debug('sacct exit code: %s', err)
+        logger.debug('sacct output: %s', output)
+        logger.debug('sacct error: %s', error)
+        if err != 0:
+            raise RuntimeError('Error running sacct: {}'.format(error))
+
+        if output.lstrip() == '':
+            return None
+        exit_code = int(output.lstrip().split(':')[0])
+        return exit_code
+
+    def cancel(self, job_id: str) -> None:
+        logger.debug('Running scancel %s', job_id)
+        command = self.__prefix + ' scancel'
+        self.__terminal.run(10, command, [job_id])
+
+
+def _job_desc_to_job_script(job_description: JobDescription) -> str:
+    job_script = '#!/bin/bash\n'
+    job_script = _add_option(job_script, 'job-name', job_description.name)
+
+    if job_description.time_reserved is not None:
+        job_script += '#SBATCH --time={}\n'.format(
+            _seconds_to_time(job_description.time_reserved))
+
+    job_script = _add_option(job_script, 'partition',
+                             job_description.queue_name)
+    job_script = _add_option(job_script, 'nodes', job_description.num_nodes)
+    job_script = _add_option(job_script, 'ntasks-per-node',
+                             job_description.mpi_processes_per_node)
+    if job_description.mpi_processes_per_node is not None:
+        job_script = _add_option(job_script, 'overcommit', '')
+
+    if job_description.system_out_file is not None:
+        job_script += '#SBATCH -o {}\n'.format(
+                job_description.system_out_file)
+
+    if job_description.system_err_file is not None:
+        job_script += '#SBATCH -e {}\n'.format(
+                job_description.system_err_file)
+
+    if job_description.extra_scheduler_options is not None:
+        job_script += '#SBATCH {}\n'.format(
+                job_description.extra_scheduler_options)
+
+    for name, value in job_description.environment.items():
+        job_script += "export {}='{}'\n".format(name, value)
+
+    if job_description.working_directory is not None:
+        job_script += 'cd {}\n'.format(job_description.working_directory)
+
+    job_script += '{}'.format(job_description.command)
+    job_script += ' {}'.format(' '.join(job_description.arguments))
+
+    if job_description.stdout_file is not None:
+        job_script += ' >{}'.format(job_description.stdout_file)
+
+    if job_description.stderr_file is not None:
+        job_script += ' 2>{}'.format(job_description.stderr_file)
+    return job_script
+
+
+def _add_option(job_script: str, option: str,
+                value: Optional[Union[int, str]]) -> str:
+    if value is not None:
+        if value is not '':
+            return job_script + '#SBATCH --{}={}\n'.format(option, value)
+        else:
+            return job_script + '#SBATCH --{}\n'.format(option)
+    else:
+        return job_script
+
+
+def _seconds_to_time(seconds: int) -> str:
+    """Converts seconds to a SLURM allocation duration.
+
+    Args:
+        seconds: The number of seconds to reserve.
+
+    Returns:
+        A string of the form DD:HH:MM:SS.
+
+    """
+    seconds_per_day = 60 * 60 * 24
+    seconds_per_hour = 60 * 60
+    seconds_per_minute = 60
+
+    days = seconds // seconds_per_day
+    seconds -= days * seconds_per_day
+
+    hours = seconds // seconds_per_hour
+    seconds -= hours * seconds_per_hour
+
+    minutes = seconds // seconds_per_minute
+    seconds -= minutes * seconds_per_minute
+    return '{:02d}-{:02d}:{:02d}:{:02d}'.format(days, hours, minutes, seconds)
