c0859a810a4ba0cfd802bb45f62371cd946a31ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:58 +0000

    Add package effmass/2.0.0/effmass-2.0.0.tar.gz

---
+++ packages/effmass/effmass-2.0.0.tar.gz/effmass-2.0.0/setup.py
@@ -0,0 +1,43 @@
+import os
+from effmass import __version__
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+try:
+	import pypandoc
+	long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+	long_description = open('README.md').read()
+
+config = {
+    'description': 'An effective mass package',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'author': 'Lucy Whalley',
+    'author_email': 'lucywhalley@gmail.com',
+    'url': 'https://github.com/lucydot/effmass',
+    'download_url': "https://github.com/lucydot/effmass/archive/%s.tar.gz" % (__version__),
+    'version': __version__,
+    'install_requires': [ 'vasppy>=0.5.0.0', 
+			  'scipy',
+			  'numpy', 
+                          'matplotlib',
+                          'adjustText',
+                          'codeclimate-test-reporter',
+                          'pytest-lazy-fixture',
+                          'coverage==4.3.4',
+                          'ase>=3.21.1',
+                          'questionary>=1.9.0',
+                          'prettytable>=2.1.0'],
+    'python_requires': '>=3.6',
+    'license': 'MIT',
+    'packages': [ 'effmass' ],
+    'scripts': [],
+    'name': 'effmass',
+    'entry_points': {"console_scripts": ["effmass = effmass.cli:cli"]}
+}
+
+setup(**config)
