32f2ebc2c0be10374cb7e728f88a27e57efbf124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:55 +0000

    Add package pikitlib/0.0.19/pikitlib-0.0.19.tar.gz

---
+++ packages/pikitlib/pikitlib-0.0.19.tar.gz/pikitlib-0.0.19/pikitlib/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+if os.path.isdir('/home/pi'):
+    # If we are running on a raspberry pi (or any linux os that can install smbus)
+    from .xboxcontroller import XboxController
+    from .buzzer import IllegalBuzzer
+    from .differentialdrive import DifferentialDrive
+    from .pca_motor import PCA9685
+    from .speedcontroller import SpeedController
+    from .speedcontrollergroup import SpeedControllerGroup
+    from .analoginput import analogInput
+    from .ultrasonic import Ultrasonic
+    from .timer import Timer
+from .timedrobot import TimedRobot
+from .iterativerobotbase import IterativeRobotBase
+from .buffer import Buffer
+from .run import run
