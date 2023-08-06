8fd7207433d9252cdac3db3cb35c75b0267fac9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package firepyer/0.0.4/firepyer-0.0.4.tar.gz

---
+++ packages/firepyer/firepyer-0.0.4.tar.gz/firepyer-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+from firepyer import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="firepyer",
+    version=__version__,
+    author="Marcus Cockerill",
+    author_email="marcus@certa.network",
+    license="Apache License 2.0",
+    description="Interacting with Cisco FTD devices via the FDM REST API in Python",
+    keywords='cisco firepower fdm ftd rest api python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/certanet/firepyer",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.8',
+)
