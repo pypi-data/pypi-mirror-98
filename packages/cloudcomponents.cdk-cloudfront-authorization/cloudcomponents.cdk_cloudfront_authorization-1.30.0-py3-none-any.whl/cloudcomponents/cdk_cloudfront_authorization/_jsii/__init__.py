c454ed19dc70a549b81c89c20e4c1f16316bbcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:29 +0000

    Add package cloudcomponents.cdk-cloudfront-authorization/1.30.0/cloudcomponents.cdk_cloudfront_authorization-1.30.0-py3-none-any.whl

---
+++ packages/cloudcomponents.cdk-cloudfront-authorization/cloudcomponents.cdk_cloudfront_authorization-1.30.0-py3-none-any.whl/cloudcomponents/cdk_cloudfront_authorization/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_cloudfront_origins._jsii
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import cloudcomponents.cdk_lambda_at_edge_pattern._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-cloudfront-authorization",
+    "1.30.0",
+    __name__[0:-6],
+    "cdk-cloudfront-authorization@1.30.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
