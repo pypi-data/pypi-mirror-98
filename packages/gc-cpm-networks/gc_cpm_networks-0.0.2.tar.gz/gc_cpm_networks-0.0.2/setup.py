b6bbaf5246e40fe424313170ad37c2ba6bfcad7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package gc-cpm-networks/0.0.2/gc_cpm_networks-0.0.2.tar.gz

---
+++ packages/gc-cpm-networks/gc_cpm_networks-0.0.2.tar.gz/gc_cpm_networks-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='gc_cpm_networks',
+    version='0.0.2',
+    description='Global Context Convolutional Pose Machine Network',
+    author='Kacper Kulczak',
+    author_email='k.kulczak@xberry.tech',
+    packages=find_packages(),
+    python_requires=">=3.8.*",
+)
