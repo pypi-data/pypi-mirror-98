05975b89d0cf5fdd8ca7d8578b0a35c4a2ba2248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:20 +0000

    Add package LoopStructural/1.0.9/LoopStructural-1.0.9-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/LoopStructural/LoopStructural-1.0.9-cp38-cp38-manylinux2010_x86_64.whl/LoopStructural/modelling/fold/__init__.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+from .fold import FoldEvent
+from .svariogram import SVariogram
+from .fold_rotation_angle_feature import FoldRotationAngleFeature, fourier_series
+from .foldframe import FoldFrame
+from .fold_rotation_angle import FoldRotationAngle
