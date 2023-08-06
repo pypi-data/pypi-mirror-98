910352362571eb4ac0401c11c5627d4475b2b26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package deeplabcutcore/0.1/deeplabcutcore-0.1.tar.gz

---
+++ packages/deeplabcutcore/deeplabcutcore-0.1.tar.gz/deeplabcutcore-0.1/deeplabcutcore/refine_training_dataset/__init__.py
@@ -0,0 +1,11 @@
+"""
+deeplabcutcorecore2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/AlexEMG/deeplabcut
+Please see AUTHORS for contributors.
+
+https://github.com/AlexEMG/deeplabcut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+from deeplabcutcore.refine_training_dataset.outlier_frames import *
