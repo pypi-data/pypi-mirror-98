4ffd45abad75e8bf493c802240724931c2de1bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package texta-parsers/2.7.8/texta-parsers-2.7.8.tar.gz

---
+++ packages/texta-parsers/texta-parsers-2.7.8.tar.gz/texta-parsers-2.7.8/texta_parsers/settings.py
@@ -0,0 +1,8 @@
+import os
+#field which is used to store meta information about the file in ElasticSearch, e.g. path, extension, type
+META_FIELD = "properties"
+ES_URL = os.getenv("DOCPARSER_ES_URL", "http://localhost:9200")
+BROKER_URL = "redis://localhost:6379/0"
+RESULT_BACKEND_URL = "redis://localhost:6379/0"
+
+FACE_VECTOR_FIELD_NAME = "texta_face_vectors"
