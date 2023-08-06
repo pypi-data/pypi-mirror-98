657518782dba11323fed11b13451c913df200850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:31 +0000

    Add package rai-core-flask/0.2.1/rai_core_flask-0.2.1.tar.gz

---
+++ packages/rai-core-flask/rai_core_flask-0.2.1.tar.gz/rai_core_flask-0.2.1/rai_core_flask/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+"""Module for wrapped-flask service."""
+
+from .flask_helper import FlaskHelper
+
+__all__ = ['FlaskHelper']
