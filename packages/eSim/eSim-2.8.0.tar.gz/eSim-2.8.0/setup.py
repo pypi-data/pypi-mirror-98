48958532163c7e4b2964c8d3ceb35ce60a73c9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:19 +0000

    Add package eSim/2.8.0/eSim-2.8.0.tar.gz

---
+++ packages/eSim/eSim-2.8.0.tar.gz/eSim-2.8.0/setup.py
@@ -0,0 +1,23 @@
+import __init__
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eSim",
+    version= "2.8.0",
+    author="e-sim-python",
+    description="E-sim python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/e-sim-python/scripts",
+    packages=setuptools.find_packages(),
+    install_requires=["lxml", "discord.py", "pytz"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
