6d7a7dc5f38697866ee5db4c92905fe88f795b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:49 +0000

    Add package cdft1d/0.1.0/cdft1d-0.1.0.tar.gz

---
+++ packages/cdft1d/cdft1d-0.1.0.tar.gz/cdft1d-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cdft1d',
+    version='0.1.0',
+    url='',
+    license='',
+    author='Marat Valiev and Gennady Chuev',
+    author_email='marat.valiev@gmail.com',
+    description='Classical density functional theory code',
+    packages=find_packages(include=['cdft1d', 'cdft1d.*']),
+    include_package_data=True,
+    package_data={'': ['data/*']},
+    install_requires=[
+            'scipy>=1.6.1',
+            'numpy>=1.20.1',
+            'matplotlib>=3.3.4',
+            'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'rism = cdft1d.cli:rism'
+        ]
+    }
+)
