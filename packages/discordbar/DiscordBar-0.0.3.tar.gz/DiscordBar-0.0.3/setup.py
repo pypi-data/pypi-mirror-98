30691d60b0fa6056ccb3275e601fbc5f09000e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:17 +0000

    Add package discordbar/0.0.3/DiscordBar-0.0.3.tar.gz

---
+++ packages/discordbar/DiscordBar-0.0.3.tar.gz/DiscordBar-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='DiscordBar',
+      version='0.0.3',
+      description='Progressbar for discord, docs here https://github.com/Animatea/DiscordProgressbar',
+      long_description='Documentation - https://github.com/Animatea/DiscordProgressbar',
+      packages=['DiscordBar'],
+      author_email='den25340@gmail.com',
+      zip_safe=False)
