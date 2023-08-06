05622ff3b5d440dcfed9caf7dc1d196c6a2b2f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:25 +0000

    Add package cloudcomponents.cdk-codepipeline-slack/1.29.0/cloudcomponents.cdk_codepipeline_slack-1.29.0-py3-none-any.whl

---
+++ packages/cloudcomponents.cdk-codepipeline-slack/cloudcomponents.cdk_codepipeline_slack-1.29.0-py3-none-any.whl/cloudcomponents/cdk_codepipeline_slack/_jsii/__init__.py
@@ -0,0 +1,36 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_codebuild._jsii
+import aws_cdk.aws_codepipeline._jsii
+import aws_cdk.aws_codepipeline_actions._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-codepipeline-slack",
+    "1.29.0",
+    __name__[0:-6],
+    "cdk-codepipeline-slack@1.29.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
