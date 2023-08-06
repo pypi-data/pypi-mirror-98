8b7c91073a50ba5cfe7145a0240a4beaa74b5710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package python-arq/0.3/Python_ARQ-0.3.tar.gz

---
+++ packages/python-arq/Python_ARQ-0.3.tar.gz/Python_ARQ-0.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'Python_ARQ',
+  packages = ['Python_ARQ'],
+  version = '0.3', 
+  license='MIT',  
+  description = 'Asynchronous Python Wrapper For A.R.Q API. ',
+  author = 'TheHamkerCat',
+  author_email = 'thehamkercat@gmail.com',
+  url = 'https://github.com/thehamkercat/Python_ARQ',
+  download_url = '',
+  keywords = ['API', 'ARQ_API', 'Universal API'],
+  install_requires=[
+          'aiohttp',
+          'dotmap',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
