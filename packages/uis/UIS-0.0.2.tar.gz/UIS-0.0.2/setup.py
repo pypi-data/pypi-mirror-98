0cf88c63cd5a28f19e84730c42749325409fa50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package uis/0.0.2/UIS-0.0.2.tar.gz

---
+++ packages/uis/UIS-0.0.2.tar.gz/UIS-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='UIS',
+      version='0.0.2',
+      description='UIS',
+      packages=['UIS'],
+      author_email='v.zagumennikov@uiscom.ru',
+      zip_safe=False)
