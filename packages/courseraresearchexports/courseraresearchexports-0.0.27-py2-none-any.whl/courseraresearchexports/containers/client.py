f967024f89c4aaa1c6424b8ef6cd5c5c41050f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package courseraresearchexports/0.0.27/courseraresearchexports-0.0.27-py2-none-any.whl

---
+++ packages/courseraresearchexports/courseraresearchexports-0.0.27-py2-none-any.whl/courseraresearchexports/containers/client.py
@@ -0,0 +1,231 @@
+# Copyright 2016 Coursera
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Coursera's tools for managing docker containers configured with a
+postgres database.
+"""
+
+import logging
+import os
+import shutil
+import time
+
+from courseraresearchexports import exports
+from courseraresearchexports.constants.api_constants import \
+    EXPORT_TYPE_TABLES
+from courseraresearchexports.constants.container_constants import \
+    COURSERA_DOCKER_LABEL, COURSERA_LOCAL_FOLDER, POSTGRES_DOCKER_IMAGE, \
+    POSTGRES_INIT_MSG, POSTGRES_READY_MSG
+from courseraresearchexports.containers import utils as container_utils
+from courseraresearchexports.exports import utils as export_utils
+from courseraresearchexports.models.ContainerInfo import ContainerInfo
+
+
+def list_all(docker_client):
+    """
+    Return all containers that have Coursera label
+    :param docker_client:
+    :return containers_info: [ContainerInfo]
+    """
+    return [ContainerInfo.from_container(container['Id'], docker_client)
+            for container in docker_client.containers(
+            all=True, filters={'label': COURSERA_DOCKER_LABEL})]
+
+
+def start(container_name, docker_client):
+    """
+    Start a docker container containing a research export database. Waits until
+    """
+    try:
+        logging.debug('Starting container {}...'.format(container_name))
+        docker_client.start(container_name)
+
+        # poll logs to see if database is ready to accept connections
+        while POSTGRES_READY_MSG not in docker_client.logs(
+                container_name, tail=4):
+
+            logging.debug('Polling container for database connection...')
+            if not container_utils.is_container_running(
+                    container_name, docker_client):
+                raise RuntimeError('Container failed to start.')
+
+            time.sleep(10)
+
+        logging.info('Started container {}.'.format(container_name))
+
+    except:
+        logging.error(
+            """Container failed to start, check log for errors:\n{}"""
+            .format(docker_client.logs(container_name, tail=20)))
+        raise
+
+
+def stop(container_name, docker_client):
+    """
+    Stops a docker container
+    """
+    docker_client.stop(container_name)
+
+
+def remove(container_name, docker_client):
+    """
+    Remove a stopped container
+    """
+    docker_client.remove_container(container_name)
+
+
+def initialize(container_name, docker_client):
+    """
+    Initialize a docker container. Polls database for completion of
+    entrypoint tasks.
+    """
+    try:
+        logging.info('Initializing container {}...'.format(
+            container_name))
+
+        docker_client.start(container_name)
+        while POSTGRES_INIT_MSG not in docker_client.logs(
+                container_name, tail=20):
+
+            logging.debug('Polling data for entrypoint initialization...')
+            if not container_utils.is_container_running(container_name,
+                                                        docker_client):
+                raise RuntimeError('Container initialization failed.')
+
+            time.sleep(10)
+
+        logging.info('Initialized container {}.'.format(container_name))
+
+    except:
+        logging.error(
+            """Container initialization failed, check log for errors:\n{}"""
+            .format(docker_client.logs(container_name, tail=20)))
+        logging.error(
+            """If error persists, consider restarting your docker engine.""")
+        raise
+
+
+def create_from_folder(export_data_folder, docker_client,
+                       container_name='coursera-exports',
+                       database_name='coursera-exports'):
+    """
+    Using a folder containing a Coursera research export, create a docker
+     container with the export data loaded into a data base and start the
+     container
+    :param export_data_folder: folder where export data/scripts is stored
+    :param docker_client:
+    :param container_name:
+    :param database_name:
+    :return container_id:
+    """
+    logging.debug('Creating containers from {folder}'.format(
+        folder=export_data_folder))
+    create_container_args = {
+        'volumes': ['/mnt/exportData'],
+        'host_config': docker_client.create_host_config(
+            binds=['{}:/mnt/exportData:ro'.format(export_data_folder)],
+            port_bindings={
+                5432: ('127.0.0.1',
+                       container_utils.get_next_available_port(list_all(
+                           docker_client)))
+            })
+    }
+    container = create_postgres_container(
+        docker_client, container_name, database_name, create_container_args)
+
+    container_id = container['Id']
+
+    # copy containers initialization script to entrypoint
+    database_setup_script = """
+        createdb -U {user} {db}
+        cd /mnt/exportData
+        psql -e -U {user} -d {db} -f setup.sql
+        psql -e -U {user} -d {db} -f load.sql
+    """.format(user='postgres', db=database_name)
+
+    docker_client.put_archive(
+        container_id,  # using a named argument causes NullResource error
+        path='/docker-entrypoint-initdb.d/',
+        data=container_utils.create_tar_archive(
+            database_setup_script, name='init-user-db.sh'))
+
+    logging.info('Created container with id: {}'.format(container_id))
+
+    initialize(container_id, docker_client)
+
+    return container_id
+
+
+def create_postgres_container(docker_client, container_name, database_name,
+                              create_container_args):
+    if not docker_client.images(name=POSTGRES_DOCKER_IMAGE):
+        logging.info('Downloading image: {}'.format(POSTGRES_DOCKER_IMAGE))
+        docker_client.import_image(image=POSTGRES_DOCKER_IMAGE)
+
+    for existing_container in docker_client.containers(
+            all=True, filters={'name': container_name}):
+        logging.info('Removing existing container with name: {}'.format(
+            container_name))
+        docker_client.stop(existing_container)
+        docker_client.remove_container(existing_container)
+    create_container_args['image'] = POSTGRES_DOCKER_IMAGE
+    create_container_args['name'] = container_name
+    create_container_args['labels'] = {
+        COURSERA_DOCKER_LABEL: None,
+        'database_name': database_name
+    }
+    return docker_client.create_container(**create_container_args)
+
+
+def create_from_export_request_id(export_request_id, docker_client,
+                                  container_name=None, database_name=None):
+    """
+    Create a docker container containing the export data from a given
+    export request. Container and database name will be inferred as the
+    course slug or partner short name from export_request if not provided.
+    :param export_request_id:
+    :param docker_client:
+    :param container_name:
+    :param database_name:
+    :return container_id:
+    """
+    export_request = exports.api.get(export_request_id)[0]
+
+    if export_request.export_type != EXPORT_TYPE_TABLES:
+        raise ValueError('Invalid Export Type. (Only tables exports supported.'
+                         'Given [{}])'.format(export_request.export_type))
+
+    logging.info('Downloading export {}'.format(export_request_id))
+    downloaded_files = export_utils.download(
+        export_request, dest=COURSERA_LOCAL_FOLDER)
+    dest = os.path.join(COURSERA_LOCAL_FOLDER, export_request_id)
+    for f in downloaded_files:
+        container_utils.extract_zip_archive(
+            archive=f,
+            dest=dest,
+            delete_archive=True)
+
+    container_id = create_from_folder(
+        export_data_folder=dest,
+        docker_client=docker_client,
+        database_name=(database_name if database_name
+                       else export_request.scope_name),
+        container_name=(container_name if container_name
+                        else export_request.scope_name)
+    )
+
+    shutil.rmtree(dest)
+
+    return container_id
---
+++ packages/courseraresearchexports/courseraresearchexports-0.0.27-py2-none-any.whl/courseraresearchexports/db/db.py
@@ -0,0 +1,175 @@
+# Copyright 2016 Coursera
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import logging
+import pkg_resources
+import subprocess
+
+from courseraresearchexports.constants.container_constants import \
+    POSTGRES_DOCKER_IMAGE
+from courseraresearchexports.models.ContainerInfo import ContainerInfo
+from courseraresearchexports.models.ExportDb import ExportDb
+from courseraresearchexports.constants.db_constants import \
+    HASHED_USER_ID_COLUMN_TO_SOURCE_TABLE
+
+
+def replace_user_id_placeholders(export_db, sql_text):
+    """
+    Replace placeholders with actual user_id column names
+    :param export_db:
+    :param sql_text:
+    :return sql_text_with_inferred_columns:
+    """
+    hashed_user_id_columns_dict = infer_hashed_user_id_columns(export_db)
+
+    for placeholder, column_name in hashed_user_id_columns_dict.items():
+        sql_text = sql_text.replace(placeholder, column_name)
+
+    return sql_text
+
+
+def infer_hashed_user_id_columns(export_db):
+    """
+    Infer hashed_user_id_columns from database using known placeholders
+    :param export_db:
+    :return:
+    """
+    hashed_user_id_columns_dict = {}
+
+    for placeholder, table in HASHED_USER_ID_COLUMN_TO_SOURCE_TABLE.items():
+        if table in export_db.tables:
+            columns = export_db.get_columns(table)
+            inferred_column = infer_user_id_column(columns)
+            if inferred_column:
+                hashed_user_id_columns_dict[placeholder] = inferred_column
+
+    return hashed_user_id_columns_dict
+
+
+def infer_user_id_column(columns):
+    """
+    Infer partner_short_name
+    :param columns:
+    :return:
+    """
+    return next((column for column in columns
+                 if column.endswith('user_id')), None)
+
+
+def connect(container_name, docker_client):
+    """
+    Create psql shell to container databaise
+    :param container_name:
+    :param docker_client:
+    """
+    container_info = ContainerInfo.from_container(
+        container_name, docker_client)
+
+    subprocess.call([
+        'docker', 'run', '-it', '--rm',
+        '--link', container_info.name,
+        POSTGRES_DOCKER_IMAGE, 'psql',
+        '-h', container_info.name,
+        '-d', container_info.database_name,
+        '-U', 'postgres'
+    ], shell=False)
+
+
+def get_table_names(container_name, docker_client):
+    """
+    Returns table names present in containerized database.
+    :param container_name:
+    :param docker_client:
+    :return table_names:
+    """
+    export_db = ExportDb.from_container(container_name, docker_client)
+
+    return export_db.tables
+
+
+def get_view_names(container_name, docker_client):
+    """
+    Returns view names present in containerized database.
+    :param container_name:
+    :param docker_client:
+    :return table_names:
+    """
+    export_db = ExportDb.from_container(container_name, docker_client)
+
+    return export_db.views
+
+
+def unload_relation(container_name, dest, relation, docker_client):
+    """
+    Unloads a table or view to a csv file.
+    :param container_name:
+    :param dest_file:
+    :param relation:
+    :param docker_client:
+    :return:
+    """
+    if not os.path.exists(dest):
+        logging.debug('Creating destination folder: {}'.format(dest))
+        os.makedirs(dest)
+
+    export_db = ExportDb.from_container(container_name, docker_client)
+    output_filename = os.path.join(dest, '{}.csv'.format(relation))
+    rowcount = export_db.unload_relation(relation, output_filename)
+    return rowcount
+
+
+def create_registered_view(container_name, view_name, docker_client):
+    """
+    Create a prepackaged view
+    :param container_name:
+    :param view_name:
+    :param partner_short_name:
+    :param docker_client:
+    :return view_name:
+    """
+    export_db = ExportDb.from_container(container_name, docker_client)
+
+    sql_text = pkg_resources.resource_string(
+        __name__.split('.')[0], 'sql/{}.sql'.format(view_name))
+    sql_text_with_inferred_columns = replace_user_id_placeholders(
+        export_db, sql_text)
+
+    export_db.create_view(view_name, sql_text_with_inferred_columns)
+
+    return view_name
+
+
+def create_view_from_file(container_name, sql_file, docker_client):
+    """
+    Create a view from a sql file.
+    :param container_name:
+    :param sql_file:
+    :param partner_short_name:
+    :param docker_client:
+    :return view_name:
+    """
+    export_db = ExportDb.from_container(container_name, docker_client)
+
+    with open(sql_file, 'r') as sf:
+        sql_text = sf.read()
+
+    view_name = os.path.splitext(os.path.basename(sql_file))[0]
+
+    sql_text_with_inferred_columns = replace_user_id_placeholders(
+        export_db, sql_text)
+
+    export_db.create_view(view_name, sql_text_with_inferred_columns)
+
+    return view_name
