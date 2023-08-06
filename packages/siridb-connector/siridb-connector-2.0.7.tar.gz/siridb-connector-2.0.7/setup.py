940a58992717ad82dbb8246b946e91b31ae3f26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:11 +0000

    Add package siridb-connector/2.0.7/siridb-connector-2.0.7.tar.gz

---
+++ packages/siridb-connector/siridb-connector-2.0.7.tar.gz/siridb-connector-2.0.7/setup.py
@@ -0,0 +1,55 @@
+"""
+Upload to PyPI
+
+python3 setup.py sdist
+twine upload --repository pypitest dist/siridb-connector-X.X.X.tar.gz
+twine upload --repository pypi dist/siridb-connector-X.X.X.tar.gz
+"""
+
+from distutils.core import setup
+import setuptools
+from siridb.connector import __version__
+
+
+VERSION = __version__
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='siridb-connector',
+    packages=[
+        'siridb',
+        'siridb.connector',
+        'siridb.connector.lib'],
+    version=VERSION,
+    description='SiriDB Connector',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/SiriDB/siridb-connector',
+    download_url=
+        'https://github.com/SiriDB/'
+        'siridb-connector/tarball/{}'.format(VERSION),
+    keywords=['siridb', 'connector', 'database', 'client'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Database',
+        'Topic :: Software Development'
+    ],
+    install_requires=['qpack']
+)
---
+++ packages/siridb-connector/siridb-connector-2.0.7.tar.gz/siridb-connector-2.0.7/siridb/connector/lib/__init__.py
@@ -0,0 +1,5 @@
+'''SiriDB Connector - Communication with SiriDB
+
+:copyright: 2017, Jeroen van der Heijden (Transceptor Technology)
+:license: MIT
+'''
