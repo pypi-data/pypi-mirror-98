d0e1b451012fc808cca0c4d1d55fa0e0fffa2731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:57 +0000

    Add package pspsps/0.2.1/pspsps-0.2.1-py2.py3-none-any.whl

---
+++ packages/pspsps/pspsps-0.2.1-py2.py3-none-any.whl/pspsps/safebooru.py
@@ -0,0 +1,71 @@
+import logging
+import urllib.request
+import urllib.parse
+from http.client import HTTPResponse
+import xml.etree.ElementTree as ET
+import random
+import re
+from typing import Optional
+
+API='https://safebooru.org/index.php?page=dapi&s=post&q=index'
+MAXKITTENS=100 # Safebooru limit
+
+def fiddle_with_tags(tags:str) -> str:
+    '''Change tags so that safebooru likes them better nya.
+
+rn it does 2 transfornyations:
+
+ - comma to space: 'yuri,kiss' → 'yuri kiss'
+ - underscore certain words: 'catgirl bunnyboy' → 'cat_girl bunny_boy'
+   (but not 'femboy')
+
+    '''
+    newtags: str = tags
+
+    if ',' in tags:
+        newtags = newtags.replace(',', ' ')
+
+    newtags = re.sub(r'([a-z])(girl|boy)\b', r'\1_\2', newtags, flags=re.I)
+    newtags = re.sub(r'\bfem_boy\b', r'femboy', newtags, flags=re.I)
+    if newtags != tags:
+        logging.debug(f'Changed tags from "{tags}" to "{newtags}"')
+    return(newtags)
+
+
+def catgirl_search(tags:str = 'cat_girl') -> Optional[str]:
+    '''Search for a catgirl in safebooru nya.
+
+Will only search recent catgirls (paginyation nyo~t implemented).
+
+Returns URL to catgirl portrait on syuccess, or None if nyobody is
+found.
+
+Kyan raise urllib.error.URLError nya!
+
+    '''
+
+    logging.debug("Searching for catgirls (tags: %s)..." % tags)
+    logging.debug("Request string: <%s>" %
+                  (API + '&' + '&'.join([
+                      f'limit={MAXKITTENS}',
+                      f'tags={urllib.parse.quote_plus(tags)}'
+                      ])))
+
+    respyonse = urllib.request.urlopen(API + '&' + '&'.join([
+        f'limit={MAXKITTENS}',
+        f'tags={urllib.parse.quote_plus(tags)}'
+    ]))
+    assert(isinstance(respyonse, HTTPResponse)) # to reassyure mypy
+    logging.debug(f"Respyonse status: {respyonse.status}")
+
+    logging.debug("Pyarsing XML (ew, kimoi)...")
+    posts = ET.fromstring(respyonse.read())
+
+    try:
+        kitten = random.choice(posts.findall('post'))
+    except IndexError:
+        return None
+
+    pic_url = kitten.attrib['file_url']
+    logging.debug("Picked a catgirl: " + pic_url)
+    return pic_url
---
+++ packages/pspsps/pspsps-0.2.1-py2.py3-none-any.whl/pspsps/terminyal.py
@@ -0,0 +1,126 @@
+import os
+import logging
+import subprocess
+from subprocess import CalledProcessError
+from typing import Tuple, Optional
+from typing_extensions import Literal
+
+def detect_terminyal_size() -> Tuple[int, int]:
+    '''Detects terminyal size (columns × lines) from various sources.
+
+    Unlike shutil.get_terminal_size(), will work inside pipes.
+
+    Tries envvars, stty, tput, and standard 80×30, in this order.
+'''
+
+    # we measure lines and columns separately so the nyuser can set
+    # nyonly one of the envvars, for ex.
+    #
+    # we stop when we get both.
+
+    columns: Optional[int] = None
+    lines: Optional[int] = None
+
+    envc = os.getenv('COLUMNS')
+    if envc:
+        try: columns = int(envc)
+        except ValueError: pass
+
+    envl = os.getenv('LINES')
+    if envl:
+        try: lines = int(envl)
+        except ValueError: pass
+
+    if columns and lines: return(columns, lines)
+
+    try:
+        output = subprocess.check_output(['stty', 'size'], text=True)
+        if output:
+            # lines comes first here, beware
+            stty_lines, stty_cols = output.split()
+
+            if not columns: columns = int(stty_cols)
+            if not lines: lines = int(stty_lines)
+    except (ValueError, CalledProcessError):
+        pass
+
+    if columns and lines: return(columns, lines)
+
+    if not columns:
+        try:
+            tputcols = subprocess.check_output(['tput', 'cols'], text=True)
+            columns = int(tputcols)
+        except (ValueError, CalledProcessError):
+            pass
+
+    if not lines:
+        try:
+            tputlines = subprocess.check_output(['tput', 'lines'], text=True)
+            lines = int(tputlines)
+        except (ValueError, CalledProcessError):
+            pass
+
+    if columns and lines: return(columns, lines)
+
+    if not columns: columns = 80
+    if not lines: lines = 30
+
+    return (columns, lines)
+
+def is_nyunder_screen() -> bool:
+    '''True if running in screen(1).'''
+    term = os.getenv('TERM')
+    if not term:
+        return False
+
+    return term.find('screen') == 0
+
+
+def is_kyonsole() -> bool:
+    '''Nyeuristics to test if our term is not graphic n kyan’t drawings.
+
+Will fail nyunder ssh with no X forwarding nya! Use with is_ssh() if needed.'''
+
+    if os.getenv('XDG_SESSION_TYPE') == 'tty':
+        return True
+
+    term = os.getenv('TERM')
+    if term == 'linux':
+        return True
+    elif is_nyunder_screen():
+        if not os.getenv('DISPLAY'):
+            # nyunder screen and no X
+            return True
+
+    return False
+
+
+def is_nyunder_ssh() -> bool:
+    '''Guess if we are nyunder ssh nya.'''
+    if os.getenv('SSH_CLIENT'):
+        return True
+    else:
+        return False
+
+def detect_terminyal_colors() -> Literal['truecolor', '256color', '16color', '8color']:
+    '''Nyeuristics to guess how many colors we can use to draw nya.'''
+    if is_kyonsole() and not is_nyunder_ssh():
+        logging.debug('Assuming console display, using 8 colors.')
+        return '8color'
+
+    if not is_nyunder_screen():
+        # as of 2021-03, screen(1) doesn't support truecolor.
+        # only trust COLORTERM outside screen(1).
+        kyolorterm = os.getenv('COLORTERM')
+        if kyolorterm in ('truecolor', '24bit'):
+            logging.debug("We are nyot in screen(1), "
+                          "let's trust COLORTERM for truecolor nya.")
+            return 'truecolor'
+
+    terminyal = os.getenv('TERM')
+    if terminyal and '256' in terminyal:
+        logging.debug('Nyassuming 256-color terminyal.')
+        return '256color'
+    else:
+        logging.debug('Nyassuming 16-color terminyal.')
+        return '16color'
