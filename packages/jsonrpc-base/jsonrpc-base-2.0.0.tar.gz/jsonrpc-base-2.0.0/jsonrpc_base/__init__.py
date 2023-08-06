3ffd952b4db06772a558c92b895b3e90181841df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:25 +0000

    Add package jsonrpc-base/2.0.0/jsonrpc-base-2.0.0.tar.gz

---
+++ packages/jsonrpc-base/jsonrpc-base-2.0.0.tar.gz/jsonrpc-base-2.0.0/jsonrpc_base/__init__.py
@@ -0,0 +1,3 @@
+from .jsonrpc import (   # noqa: F401, F403
+    Server, Message, Request, Response,
+    JSONRPCError, ProtocolError, TransportError)
---
+++ packages/jsonrpc-base/jsonrpc-base-2.0.0.tar.gz/jsonrpc-base-2.0.0/setup.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+try:
+    from setuptools import setup
+except ImportError:
+    import sys
+    print("Please install the `setuptools` package in order to install this library", file=sys.stderr)
+    raise
+
+setup(
+    name='jsonrpc-base',
+    version='2.0.0',
+    author='Emily Mills',
+    author_email='emily@emlove.me',
+    packages=('jsonrpc_base',),
+    license='BSD',
+    keywords='json-rpc base',
+    url='http://github.com/emlove/jsonrpc-base',
+    description='''A JSON-RPC client library base interface''',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
