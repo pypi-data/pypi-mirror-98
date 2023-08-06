20ae52b72f65d59a0373abf0f6ad4f5f34ae4d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:20 +0000

    Add package utilsbib/27.4/utilsbib-27.4.tar.gz

---
+++ packages/utilsbib/utilsbib-27.4.tar.gz/utilsbib-27.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(     name='utilsbib',
+
+     version='27.4',
+     author="Habibou Sissoko",
+     author_email="habibou.s@outlook.fr",
+     description="An recurrent utility algorithm package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Bibjuju/utils-bib",
+     packages=setuptools.find_packages(),
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+    #py_modules = ['utils-b'],
+
+ )
