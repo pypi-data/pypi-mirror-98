bd204d358f79f3f697d66b47fd124c8c499c94ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:28 +0000

    Add package climetlab/0.3.10/climetlab-0.3.10.tar.gz

---
+++ packages/climetlab/climetlab-0.3.10.tar.gz/climetlab-0.3.10/climetlab/core/ipython.py
@@ -0,0 +1,59 @@
+# (C) Copyright 2020 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+"""
+ipython is not None when running a notebook
+"""
+
+import logging
+import sys
+
+ipython_active = None
+try:
+    from IPython import get_ipython
+
+    ipython_active = get_ipython()
+except Exception:
+    pass
+
+
+def enable_ipython_login(level=logging.INFO):
+    class Filter(logging.Filter):
+        def filter(self, message):
+            return message.levelno < logging.WARNING
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+
+    stdout = logging.StreamHandler(sys.stdout)
+    stdout.setLevel(level)
+    stdout.addFilter(Filter())
+    logger.addHandler(stdout)
+
+    stderr = logging.StreamHandler(sys.stderr)
+    stderr.setLevel(logging.WARNING)
+    logger.addHandler(stderr)
+
+
+def _identity(x, **kwargs):
+    return x
+
+
+if ipython_active:
+    from IPython.display import HTML, SVG, Image, Markdown, display
+
+    # enable_ipython_login()
+else:
+    Image = _identity
+    SVG = _identity
+    HTML = _identity
+    Markdown = _identity
+    display = _identity
+
+__all__ = ["display", "Image", "SVG", "HTML", "Markdown"]
