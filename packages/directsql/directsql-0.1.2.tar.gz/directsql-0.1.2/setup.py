9402d48188d902c30a56270ca88f7e9e2a324fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:33 +0000

    Add package directsql/0.1.2/directsql-0.1.2.tar.gz

---
+++ packages/directsql/directsql-0.1.2.tar.gz/directsql-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages  
+  
+setup(  
+    name = 'directsql',  
+    version = '0.1.2',
+    # keywords = ('chinesename',),  
+    description = 'add init.py',  
+    license = 'MIT License',  
+    packages = ['directsql'],  # 要打包的项目文件夹
+    include_package_data=True,   # 自动打包文件夹内所有数据
+    author = 'surecanlee',  
+    author_email = 'lishukan@qq.com',
+    url = 'https://github.com/lishukan/directsql',
+    # packages = find_packages(include=("*"),),  
+    install_requires=[
+        'DBUtils>=1.13.0',
+    ]
+)
+"""
+python3 setup.py bdist_wheel --universal    #编译 纯python的wheel包
+python3 setup.py sdist   #编译 源码压缩包
+任选其一 
+
+之后 使用  twine upload dist/* 上传 ，用户名是lishukan
+dist/ 文件夹下只保留一个文件
+"""
+
