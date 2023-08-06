7047dc24ac564b4438e9297c61cb93a2964d5592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:35 +0000

    Add package md-click/1.0.1/md-click-1.0.1.tar.gz

---
+++ packages/md-click/md-click-1.0.1.tar.gz/md-click-1.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from md_click import __version__
+
+with open('requirements.txt', 'r') as req:
+    install_requires = req.readlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='md-click',
+    description="A command line tool for automatically generating .md files from click's CLI command.",
+    author='Rivery',
+    version="1.0.1",
+    include_package_data=False,
+    zip_safe=True,
+    packages=find_packages(),
+    py_modules=[
+        'md-click'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RiveryIo/md-click",
+    project_urls={
+        "Bug Tracker": "https://github.com/RiveryIo/md-click/issues",
+    },
+    install_requires=install_requires,
+    entry_points="""
+        [console_scripts]
+        mdclick=md_click.main:cli
+    """,
+    license_files=("LICENSE", ),
+    license="BSD 3.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Documentation",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup :: Markdown"
+    ],
+    python_required=">=3.6",
+)
