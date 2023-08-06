7013a9283e103d1bbb778c18465a5b9a2b69a4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:25 +0000

    Add package pytire/0.3.0/pytire-0.3.0-py3-none-any.whl

---
+++ packages/pytire/pytire-0.3.0-py3-none-any.whl/pytire/__init__.py
@@ -0,0 +1,9 @@
+"""
+:author: Alex Robinson <girotobial@gmail.com>
+:copyright: Copyright (c) Alex Robinson, 2021-2021.
+:license: MIT
+"""
+
+from .tire import Tire
+
+__all__ = ["Tire"]
---
+++ packages/pytire/pytire-0.3.0-py3-none-any.whl/pytire/enums.py
@@ -0,0 +1,13 @@
+"""
+:author: Alex Robinson <girotobial@gmail.com>
+:copyright: Copyright (c) Alex Robinson, 2021-2021.
+:license: MIT
+"""
+
+import enum
+
+
+class Unit(enum.Enum):
+    MILLIMETRE = enum.auto()
+    METRE = enum.auto()
+    INCH = enum.auto()
