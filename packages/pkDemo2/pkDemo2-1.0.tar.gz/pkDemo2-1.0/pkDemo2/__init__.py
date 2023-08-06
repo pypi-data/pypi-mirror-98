1a7cf3e5e1d85c601f7b3223a8f37746309ed17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:59 +0000

    Add package pkDemo2/1.0/pkDemo2-1.0.tar.gz

---
+++ packages/pkDemo2/pkDemo2-1.0.tar.gz/pkDemo2-1.0/pkDemo2/__init__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# _*_coding:utf-8 _*_
+import time
+
+
+def demo():
+    for i in range(4):
+        print("This is %d demo package!" % i)
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    demo()
---
+++ packages/pkDemo2/pkDemo2-1.0.tar.gz/pkDemo2-1.0/setup.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python
+# _*_coding:utf-8 _*_
+
+from setuptools import find_packages, setup
+setup(
+    name='pkDemo2',    # 程序名称
+    version='1.0',    # 程序版本号
+    packages=find_packages(),  # 包含的包列表
+)
