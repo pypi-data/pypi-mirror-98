ff06e3f823df18a40fda4420734d0d6e6af2b7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:14 +0000

    Add package OBITools3/3.0.1b1/OBITools3-3.0.1b1-py3.8-linux-x86_64.egg

---
+++ packages/OBITools3/OBITools3-3.0.1b1-py3.8-linux-x86_64.egg/obitools3/dms/column/typed_column/__init__.py
@@ -0,0 +1,3 @@
+#from .bool import Column_bool
+#from .int import Column_int
+# TODO why is this needed?
---
+++ packages/OBITools3/OBITools3-3.0.1b1-py3.8-linux-x86_64.egg/obitools3/version.py
@@ -0,0 +1,5 @@
+major = 3
+minor = 0
+serial= '1b1'
+
+version ="%d.%d.%s" % (major,minor,serial)
