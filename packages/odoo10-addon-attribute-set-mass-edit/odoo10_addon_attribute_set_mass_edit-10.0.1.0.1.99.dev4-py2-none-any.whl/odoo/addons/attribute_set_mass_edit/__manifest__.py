20afa8d93480e66b0586e880d0f1b3dbf8a36c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:30 +0000

    Add package odoo10-addon-attribute-set-mass-edit/10.0.1.0.1.99.dev4/odoo10_addon_attribute_set_mass_edit-10.0.1.0.1.99.dev4-py2-none-any.whl

---
+++ packages/odoo10-addon-attribute-set-mass-edit/odoo10_addon_attribute_set_mass_edit-10.0.1.0.1.99.dev4-py2-none-any.whl/odoo/addons/attribute_set_mass_edit/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Attribute Set Mass Edit",
+    "version": "10.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-pim",
+    "depends": ["attribute_set", "mass_editing"],
+    "data": ["views/attribute_attribute.xml"],
+    "demo": [],
+}
