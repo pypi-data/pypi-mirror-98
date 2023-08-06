b87fad4cbd56f5638f037de29c51c7c83207985a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:51 +0000

    Add package umm/0.5.1/umm-0.5.1.tar.gz

---
+++ packages/umm/umm-0.5.1.tar.gz/umm-0.5.1/src/umm/C.py
@@ -0,0 +1,35 @@
+import os
+import platform
+
+import yaml
+
+# Package meta-data.
+VERSION = '0.5.1'
+NAME = 'umm'
+DESCRIPTION = 'A toolkit to manager the fastest mirror of various tools, such as pip, npm, composer and etc.'
+URL = 'https://github.com/xiyusullos/umm'
+EMAIL = 'i@aponder.top'
+AUTHOR = 'aponder'
+
+# What packages are required for this module to be executed?
+REQUIRES = [
+    'click',
+    'PyYAML',
+]
+# Package meta-data. #end#
+
+
+HERE = os.path.dirname(__file__)
+
+_HOME_str = 'HOMEPATH' if platform.system() == 'Windows' else 'HOME'
+HOME = os.getenv(_HOME_str)
+VIRTUAL_ENV = os.getenv('VIRTUAL_ENV', HOME)
+
+MIRRORS = yaml.safe_load(open(os.sep.join([HERE, 'mirrors.yml'])))
+
+
+# some constants
+INVALID_MIRROR_NAME = 'Invalid mirror name'
+
+if __name__ == '__main__':
+    print(MIRRORS)
---
+++ packages/umm/umm-0.5.1.tar.gz/umm-0.5.1/src/umm/commands/npm.py
@@ -0,0 +1,39 @@
+from click import echo
+
+from .base import MirrorCommand
+from .. import C
+from ..utils import run_cmd, set_locale
+
+
+# https://docs.npmjs.com/cli/v6/commands/npm-config
+# https://docs.npmjs.com/cli/v6/using-npm/config
+
+# npm config set registry https://registry.npm.taobao.org
+
+class Npm(MirrorCommand):
+    NAME = 'npm'
+
+    def current_mirror_name(self):
+        is_ok, out = run_cmd('npm config get registry')
+        found_mirror = self.mirror.find_by_url(out)
+        return found_mirror[0] if found_mirror else self.DEFAULT
+
+    def cmd_use(self, mirror_name, is_local=False):
+        if mirror_name not in self.mirror:
+            return echo(C.INVALID_MIRROR_NAME)
+
+        mirror_url = self.mirror[mirror_name]
+        cmd = 'npm config set registry ' + mirror_url
+        is_ok, out = run_cmd(cmd)
+
+
+def test_npm():
+    cli = Npm()
+    print(cli.current_mirror_name())
+
+
+cli = Npm()
+
+if __name__ == '__main__':
+    set_locale()
+    cli()
