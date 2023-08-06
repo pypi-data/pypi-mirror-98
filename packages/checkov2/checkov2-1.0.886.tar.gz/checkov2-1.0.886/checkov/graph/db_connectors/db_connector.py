a3623ce84059b32d936688eb325a175d24d520a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:55 +0000

    Add package checkov2/1.0.886/checkov2-1.0.886.tar.gz

---
+++ packages/checkov2/checkov2-1.0.886.tar.gz/checkov2-1.0.886/checkov/graph/db_connectors/db_connector.py
@@ -0,0 +1,15 @@
+class DBConnector:
+    def save_graph(self, local_graph):
+        pass
+
+    def get_reader_endpoint(self):
+        pass
+
+    def get_writer_endpoint(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def get_vertices_attributes(self):
+        pass
