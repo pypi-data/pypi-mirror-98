6ff30a1e9b37dfa93ec7390d8e11dfa594324d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:57 +0000

    Add package pcpca/0.1/pcpca-0.1.tar.gz

---
+++ packages/pcpca/pcpca-0.1.tar.gz/pcpca-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pcpca",
+    version="0.1",
+    description="Probabilistic contrastive principal component analysis",
+    url="https://github.com/andrewcharlesjones/pcpca",
+    download_url="https://github.com/andrewcharlesjones/pcpca/archive/v_01.tar.gz",
+    author="Andy Jones",
+    author_email="ajones788@gmail.com",
+    packages=find_packages(),
+)
