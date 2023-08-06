babbf6504209d46cedccfccd196496f55d5dc968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:30 +0000

    Add package tpimporter/0.3.3/tpimporter-0.3.3-py2.py3-none-any.whl

---
+++ packages/tpimporter/tpimporter-0.3.3-py2.py3-none-any.whl/tpimporter/adapters/Basic_Adapter.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+
+
+class Basic_Adapter:
+    """
+    abstract base class that needs to be subclassed for every special adapter
+    each special class should have an instantiated transformer_dict in the following structure:
+    {'db_field_name_for_program': 'key_from_tp_importer', ...}
+    """
+    transformer_dict = dict()
+
+    def transform(self, tp_data: List[Dict], transformer_dict: dict = None) -> List[Dict]:
+        """
+        transforms the given dictionaries to a form defined in the class variable
+        :param tp_data: a list of dictionaries coming from the TP-Importer
+        :param transformer_dict: [OPTIONAL] a dictionary with the format {KEY_FOR_APP:KEY_FROM_TPIMPORTER} -
+        used for separated importing of further data that is not covered within the original transformer_dict
+        :return: List[Dict] (with adapted keys fitting the specific service)
+        """
+        if not transformer_dict:
+            transformer_dict = self.transformer_dict
+        tp_data_mam = []
+        for title in tp_data:
+            title_dict_mam = dict()
+            for key_mam, key_tpimporter in transformer_dict.items():
+                title_dict_mam[key_mam] = title.get(key_tpimporter)
+            tp_data_mam.append(title_dict_mam)
+        return tp_data_mam
