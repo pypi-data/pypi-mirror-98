db0370f365fd3f25fd35bc1698ac9b4df602fb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:02 +0000

    Add package ffexport/0.1.8/ffexport-0.1.8.tar.gz

---
+++ packages/ffexport/ffexport-0.1.8.tar.gz/ffexport-0.1.8/setup.py
@@ -0,0 +1,41 @@
+import io
+from setuptools import setup, find_packages
+
+requirements = ["click", "logzero", "IPython"]
+
+# Use the README.md content for the long description:
+with io.open("README.md", encoding="utf-8") as fo:
+    long_description = fo.read()
+
+pkg = "ffexport"
+setup(
+    name=pkg,
+    version="0.1.8",
+    url="https://github.com/seanbreckenridge/ffexport",
+    author="Sean Breckenridge",
+    author_email="seanbrecke@gmail.com",
+    description=("""export/interface with firefox history/site metadata"""),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(include=["ffexport"]),
+    package_data={pkg: ['py.typed']},
+    install_requires=requirements,
+    extras_require={
+        "testing": [
+            "pytest",
+            "mypy",
+        ],
+    },
+    keywords="firefox history backup data",
+    entry_points={"console_scripts": ["ffexport = ffexport.cli:cli"]},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
