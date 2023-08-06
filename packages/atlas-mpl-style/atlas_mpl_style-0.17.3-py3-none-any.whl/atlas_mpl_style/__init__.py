41d0781255fa88039d46b435ea0f9b60df8a79f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:57 +0000

    Add package atlas-mpl-style/0.17.3/atlas_mpl_style-0.17.3-py3-none-any.whl

---
+++ packages/atlas-mpl-style/atlas_mpl_style-0.17.3-py3-none-any.whl/atlas_mpl_style/__init__.py
@@ -0,0 +1,247 @@
+import matplotlib as _mpl
+import matplotlib.style as _style
+import pkg_resources as _pkg
+import atexit as _atexit
+import shutil as _shutil
+import warnings as _warn
+import cycler as _cycler
+import atlas_mpl_style.plot as plot
+import atlas_mpl_style.utils as utils
+import atlas_mpl_style.uhi as uhi
+from atlas_mpl_style.plot import set_xlabel, set_ylabel, set_zlabel, draw_atlas_label
+
+_stylesheets = _pkg.resource_filename(__name__, "stylesheets")
+_atexit.register(_pkg.cleanup_resources)
+_style.core.USER_LIBRARY_PATHS.append(_stylesheets)
+_style.core.reload_library()
+
+_EXTRA_COLORS = {
+    "paper:bg": "#eeeeee",
+    "paper:fg": "#444444",
+    "paper:bgAlt": "#e4e4e4",
+    "paper:red": "#af0000",
+    "paper:green": "#008700",
+    "paper:blue": "#005f87",
+    "paper:yellow": "#afaf00",
+    "paper:orange": "#d75f00",
+    "paper:pink": "#d70087",
+    "paper:purple": "#8700af",
+    "paper:lightBlue": "#0087af",
+    "paper:olive": "#5f7800",
+    "on:bg": "#1b2b34",
+    "on:fg": "#cdd3de",
+    "on:bgAlt": "#343d46",
+    "on:fgAlt": "#d8dee9",
+    "on:red": "#ec5f67",
+    "on:orange": "#f99157",
+    "on:yellow": "#fac863",
+    "on:green": "#99c794",
+    "on:cyan": "#5fb3b3",
+    "on:blue": "#6699cc",
+    "on:pink": "#c594c5",
+    "on:brown": "#ab7967",
+    "series:cyan": "#54c9d1",
+    "series:orange": "#eca89a",
+    "series:blue": "#95bced",
+    "series:olive": "#ceb776",
+    "series:purple": "#d3a9ea",
+    "series:green": "#9bc57f",
+    "series:pink": "#f0a1ca",
+    "series:turquoise": "#5fcbaa",
+    "atlas:onesigma": "#00ff26",
+    "atlas:twosigma": "#fbff1f",
+    "series2:green": "#00ff26",
+    "series2:yellow": "#fbff1f",
+    "series2:blue": "#00a1e0",
+    "series2:red": "#a30013",
+    "series2:purple": "#5100c2",
+    "transparent": "#ffffff00",
+}
+
+_mpl.colors.EXTRA_COLORS = _EXTRA_COLORS
+_mpl.colors.colorConverter.colors.update(_EXTRA_COLORS)
+
+
+def set_color_cycle(pal=None, n=4):
+    """
+    Sets a different color cycle.
+
+    The ATLAS palette includes the standard green and yellow.
+
+    Parameters
+    ----------
+    pal : {'ATLAS', 'Paper', 'Oceanic', 'MPL', None}
+      The palette to use. None resets to default palette.
+      The ATLAS palette is suitable for histograms, not lines.
+       'MPL' (alias 'Tab') provides the default matplotlib palette.
+    n : int, optional
+      Number of lines or histograms.
+    """
+    if n < 2:
+        n = 2
+    if pal.upper() == "ATLAS":
+        if n > 5:
+            n = 5
+        colors = reversed(
+            ["series2:yellow", "series2:green", "series2:blue", "series2:purple"][
+                0 : n - 1
+            ]
+            + ["series2:red"]
+        )
+    elif pal.lower() == "paper":
+        if n > 8:
+            n = 8
+        colors = [
+            "paper:green",
+            "paper:red",
+            "paper:blue",
+            "paper:orange",
+            "paper:purple",
+            "paper:yellow",
+            "paper:lightblue",
+            "paper:olive",
+        ][0:n]
+    elif pal.lower() == "oceanic":
+        colors = [
+            "on:green",
+            "on:red",
+            "on:blue",
+            "on:cyan",
+            "on:orange",
+            "on:pink",
+            "on:yellow",
+        ]
+    elif pal.lower() == "tab" or pal.lower() == "tableau" or pal.lower() == "mpl":
+        colors = [
+            "tab:blue",
+            "tab:orange",
+            "tab:green",
+            "tab:red",
+            "tab:purple",
+            "tab:brown",
+            "tab:pink",
+            "tab:gray",
+            "tab:olive",
+            "tab:cyan",
+        ]
+    else:
+        colors = [
+            "series:cyan",
+            "series:orange",
+            "series:blue",
+            "series:olive",
+            "series:purple",
+            "series:green",
+            "series:pink",
+        ]
+
+    _mpl.rcParams["axes.prop_cycle"] = _cycler.cycler(color=colors)
+    plot._hist_colors = _mpl.rcParams["axes.prop_cycle"]()
+
+
+def use_atlas_style(atlasLabel="ATLAS", fancyLegend=True, usetex=True):
+    """
+    Setup ATLAS style.
+
+    Parameters
+    ----------
+    atlasLabel : str, optional
+       Replace ATLAS with a custom label
+    fancyLegend : bool, optional
+       Use matplotlib's fancy legend frame (defaults to True)
+    usetex : bool, optional
+       Use LaTeX installation to set text (defaults to True)
+       If no LaTeX installation is found, this package will fallback to usetex=False.
+       This is on a best-effort basis, since the detected LaTeX installation may be incomplete.
+    """
+    if usetex:
+        if (
+            _shutil.which("pdflatex") is None
+            or _shutil.which("dvipng") is None
+            or _shutil.which("gs") is None
+        ):
+            _warn.warn(
+                "No LaTeX installation found -- atlas-mpl-style is falling back to usetex=False"
+            )
+            usetex = False
+
+    _style.use("atlas")
+    set_color_cycle("ATLAS")
+    plot._atlas_label = atlasLabel
+    _mpl.rcParams["xtick.minor.visible"] = True
+    _mpl.rcParams["ytick.minor.visible"] = True
+    if not usetex:
+        _mpl.rcParams["text.usetex"] = False
+        _mpl.rcParams["mathtext.default"] = "regular"
+    else:
+        plot._usetex = True
+        _mpl.rcParams["text.latex.preamble"] = "\n".join(
+            [
+                r"\usepackage[LGR,T1]{fontenc}",
+                r"\usepackage{tgheros}",
+                r"\renewcommand{\familydefault}{\sfdefault}",
+                r"\usepackage{amsmath}",
+                r"\usepackage[symbolgreek,symbolmax]{mathastext}",
+                r"\usepackage{physics}",
+                r"\usepackage{siunitx}",
+                r"\setlength{\parindent}{0pt}",
+                r"\def\mathdefault{}",
+            ]
+        )
+    if not fancyLegend:
+        _mpl.rcParams["legend.frameon"] = False
+        _mpl.rcParams["legend.fancybox"] = False
+        _mpl.rcParams["legend.framealpha"] = 0.75
+
+
+def ratio_axes(extra_axes=None):
+    """
+    Splits axes for ratio plots.
+
+    Parameters
+    -----------
+    extra_axes : int, optional
+       Number of additional axes. If not given, defaults to one.
+    square : bool, optional
+       Whether the plot should be square or tall. Defaults to True (square)
+
+    Returns
+    -------
+    fig : figure
+    main_ax : axes
+    ratio_ax : axes or list of axes
+       Returns list if extra_axes is passed
+    """
+    hgt = (6 + 2 * (1 if extra_axes is None else extra_axes)) * (100/72)
+    fig = _mpl.pyplot.figure(figsize=(hgt, hgt))
+    if extra_axes is None:
+        gs = _mpl.gridspec.GridSpec(4, 1, hspace=0.0, wspace=0.0)
+        ax1 = fig.add_subplot(gs[0:3])
+        ax1.tick_params(labelbottom=False)
+        ax2 = fig.add_subplot(gs[3], sharex=ax1)
+        ax2.yaxis.set_major_locator(
+            _mpl.ticker.MaxNLocator(
+                symmetric=True, prune="both", min_n_ticks=5, nbins=4
+            )
+        )
+        ax2.autoscale(axis="x", tight=True)
+        _mpl.pyplot.sca(ax1)
+        return fig, ax1, ax2
+    else:
+        gs = _mpl.gridspec.GridSpec(3 + extra_axes, 1, hspace=0.0, wspace=0.0)
+        ax1 = fig.add_subplot(gs[0:3])
+        ax1.tick_params(labelbottom=False)
+        axs = []
+        for i in range(3, extra_axes + 3):
+            ax = fig.add_subplot(gs[i], sharex=ax1)
+            if i != extra_axes + 2:
+                ax.tick_params(labelbottom=False)
+            ax.yaxis.set_major_locator(
+                _mpl.ticker.MaxNLocator(
+                    symmetric=True, prune="both", min_n_ticks=5, nbins=4
+                )
+            )
+            ax.autoscale(axis="x", tight=True)
+            axs.append(ax)
+        _mpl.pyplot.sca(ax1)
+        return fig, ax1, axs
---
+++ packages/atlas-mpl-style/atlas_mpl_style-0.17.3-py3-none-any.whl/atlas_mpl_style/uhi.py
@@ -0,0 +1,303 @@
+"""This module contains versions of the histogram plotting functions that take PlottableHistograms
+
+These are in a separate module to preserve backward compatibility since the array versions of these
+functions take the array of bins before the histogram.
+
+:class:`atlas_mpl_style.plot.Background` can be constructed using a ``PlottableHistogram`` and therefore
+there is no ``atlas_mpl_style.uhi.plot_backgrounds`` function.
+"""
+
+import numpy as _np
+import atlas_mpl_style.plot as _amplplt
+
+
+class LabeledBinsError(Exception):
+    "Labeled bins when edges expected (or vice versa)"
+
+    def __init__(self, msg):
+        "Labeled bins when edges expected (or vice versa)"
+        super().__init__(self, msg)
+
+
+def _bins(axis):
+    a = list(axis)
+    if isinstance(a[0], str):
+        raise LabeledBinsError("Bins are labeled. Perhaps you may want plot_cutflow.")
+    return _np.array([i for (i, _) in a] + [a[-1][1]])
+
+
+def plot_data(hist, ignore_variances=False, color="k", label="Data", ax=None):
+    """
+    Plot data from PlottableHistogram
+
+    Parameters
+    ----------
+    label : str, optional
+        Label for legend (default: "Data")
+    hist : PlottableHistogram
+        Histogram
+    ignore_variances : bool
+        Ignore variances and substitute ``hist``. Defaults to False.
+    color : color, optional
+        Point color, defaults to black
+    ax : mpl.axes.Axes, optional
+        Axes to draw on (defaults to current axes)
+
+    Returns
+    --------
+    hist : array_like
+        Data histogram
+    stat_errs : array_like
+        Statistical errors
+    """
+    if (
+        not hasattr(hist, "axes")
+        or not hasattr(hist, "values")
+        or not hasattr(hist, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "hist violates PlottableHistogram protocol"
+        )
+    if len(hist.axes) != 1:
+        raise _amplplt.DimensionError("Only 1D histograms are supported here")
+    hist_obj = hist
+    bins = _bins(hist.axes[0])
+    hist = hist_obj.values()
+    if ignore_variances:
+        stat_errs = _np.sqrt(hist)
+    else:
+        stat_errs = (
+            None if hist_obj.variances() is None else _np.sqrt(hist_obj.variances())
+        )
+    return _amplplt.plot_data(bins, hist, stat_errs, color, label, ax)
+
+
+def plot_signal(
+    hist, label, ignore_variances=False, syst_errs=None, color=None, ax=None
+):
+    """
+    Plot signal histogram from PlottableHistogram
+
+    Parameters
+    ----------
+    hist : PlottableHistogram
+        Histogram
+    label : str
+        Label for legend
+    ignore_variances : bool
+        Ignore variances and substitute ``hist``. Defaults to False.
+    syst_errs : array_like or PlottableHistogram, optional
+        Systematic errors
+    color : color
+        Line color
+    ax : mpl.axes.Axes, optional
+        Axes to draw on (defaults to current axes)
+    """
+    if (
+        not hasattr(hist, "axes")
+        or not hasattr(hist, "values")
+        or not hasattr(hist, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "hist violates PlottableHistogram protocol"
+        )
+    if len(hist.axes) != 1:
+        raise _amplplt.DimensionError("Only 1D histograms are supported here")
+    hist_obj = hist
+    bins = _bins(hist_obj.axes[0])
+    hist = hist_obj.values()
+    if ignore_variances:
+        stat_errs = _np.sqrt(hist)
+    else:
+        stat_errs = (
+            None if hist_obj.variances() is None else _np.sqrt(hist_obj.variances())
+        )
+    if syst_errs is not None and hasattr(syst_errs, "axes"):
+        if not hasattr(syst_errs, "values") or not hasattr(syst_errs, "variances"):
+            raise _amplplt.ViolatesPlottableHistogramError(
+                "syst_errs violates PlottableHistogram protocol"
+            )
+        if len(syst_errs.axes) != 1:
+            raise _amplplt.DimensionError("Only 1D histograms are supported here")
+        syst_errs_obj = syst_errs
+        if len(syst_errs_obj.axes[0]) != len(hist_obj.axes[0]):
+            raise _amplplt.BinningMismatchError(
+                "Binning mismatch between syst_errs and hist"
+            )
+        syst_errs = syst_errs_obj.values()
+    _amplplt.plot_signal(label, bins, hist, stat_errs, syst_errs, color, ax)
+
+
+def plot_ratio(data, total_bkg, ratio_ax, max_ratio=None, plottype="diff"):
+    """
+    Plot ratio plot from PlottableHistogram
+
+    Parameters
+    ----------
+    data : PlottableHistogram
+        Data histogram
+    total_bkg : (array_like, array_like)
+        Tuple returned from :func:`atlas_mpl_style.plot.plot_backgrounds`
+    ratio_ax : mpl.axes.Axes
+        Ratio axes (produced using :func:`atlas_mpl_style.ratio_axes()`)
+    max_ratio : float, optional
+        Maximum ratio (defaults to 0.2 for "diff", 1.2 for "raw", 3 for "significances")
+    plottype : {"diff", "raw", "significances"}
+        | Type of ratio to plot.
+        | "diff" : (data - bkg) / bkg
+        | "raw" : data / bkg
+        | "significances" : Significances (from `ampl.utils.significance()`)
+    """
+    if (
+        not hasattr(data, "axes")
+        or not hasattr(data, "values")
+        or not hasattr(data, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "data violates PlottableHistogram protocol"
+        )
+    if len(data.axes) != 1:
+        raise _amplplt.DimensionError("Only 1D histograms are supported here")
+    data_obj = data
+    bins = data_obj.axes[0].edges()
+    data = data_obj.values()
+    if data_obj.variances() is None:
+        data_errs = _np.sqrt(data)
+    else:
+        data_errs = _np.sqrt(data_obj.variances())
+    if not isinstance(total_bkg, tuple) or len(total_bkg) != 2:
+        raise TypeError(
+            "total_bkg should be a two element tuple of arrays, as returned by plot_backgrounds"
+        )
+    bkg = total_bkg[0]
+    bkg_errs = total_bkg[1]
+    _amplplt.plot_ratio(
+        bins, data, data_errs, bkg, bkg_errs, ratio_ax, max_ratio, plottype
+    )
+
+
+def plot_1d(
+    hist, label, ignore_variances=False, stat_err=True, color=None, ax=None, **kwargs
+):
+    """
+    Plot single 1D histogram from PlottableHistogram
+
+    Parameters
+    ----------
+    hist : PlottableHistogram
+        Histogram
+    label : str
+        Label for legend
+    ignore_variances : bool
+        Ignore variances and substitute ``hist``. Defaults to False.
+    stat_err : bool
+        Draw statistical errors. Defaults to True.
+    color : color, optional
+        Line color
+    ax : mpl.axes.Axes, optional
+        Axes to draw on (defaults to current axes)
+    **kwargs
+        Extra parameters passed to ``plt.hist``
+    """
+    if (
+        not hasattr(hist, "axes")
+        or not hasattr(hist, "values")
+        or not hasattr(hist, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "hist violates PlottableHistogram protocol"
+        )
+    if len(hist.axes) != 1:
+        raise _amplplt.DimensionError("Only 1D histograms are supported here")
+    hist_obj = hist
+    bins = _bins(hist_obj.axes[0])
+    hist = hist_obj.values()
+    if stat_err:
+        if ignore_variances:
+            stat_errs = _np.sqrt(hist)
+        else:
+            stat_errs = (
+                None if hist_obj.variances() is None else _np.sqrt(hist_obj.variances())
+            )
+    else:
+        stat_errs = None
+    _amplplt.plot_1d(label, bins, hist, stat_errs, color, ax, **kwargs)
+
+
+def plot_2d(hist, ax=None, pad=0.05, **kwargs):
+    """
+    Plot 2D histogram from PlottableHistogram
+
+    Parameters
+    ----------
+    hist : PlottableHistogram
+        Histogram
+    ax : mpl.axes.Axes, optional
+        Axes to draw on (defaults to current axes)
+    pad : float, optional
+        Padding for colorbar in inches (defaults to 0.05)
+    **kwargs
+        Extra parameters passed to ``pcolormesh``
+
+    Returns
+    -------
+    mesh : QuadMesh
+    cbar : mpl.colorbar.Colorbar
+    """
+    if (
+        not hasattr(hist, "axes")
+        or not hasattr(hist, "values")
+        or not hasattr(hist, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "hist violates PlottableHistogram protocol"
+        )
+    if len(hist.axes) != 2:
+        raise _amplplt.DimensionError("Only 2D histograms are supported here")
+    xbins = _bins(hist.axes[0])
+    ybins = _bins(hist.axes[1])
+    h = hist.values()
+    return _amplplt.plot_2d(xbins, ybins, h, ax, pad, **kwargs)
+
+
+def plot_cutflow(hist, ax=None, text=True, textcolor="w", horizontal=True, **kwargs):
+    """
+    Plot cutflow from PlottableHistogram
+
+    Parameters
+    ----------
+    hist : PlottableHistogram
+        Cutflow histogram
+    ax : mpl.axes.Axes, optional
+        Axes to draw on (defaults to current axes)
+    text : bool, optional
+        Whether to label bars (default: True)
+    textcolor: str, optional
+        Text color
+    horizontal : bool, optional
+        Whether to draw horizontal bars (default: True)
+    **kwargs
+        Extra parameters passed to ``bar`` or ``barh``
+    """
+    if (
+        not hasattr(hist, "axes")
+        or not hasattr(hist, "values")
+        or not hasattr(hist, "variances")
+    ):
+        raise _amplplt.ViolatesPlottableHistogramError(
+            "hist violates PlottableHistogram protocol"
+        )
+    if len(hist.axes) != 1:
+        raise _amplplt.DimensionError("Cutflow should be 1D")
+    labels = list(hist.axes[0])
+    if not isinstance(labels[0], str):
+        raise LabeledBinsError("Bins are not labeled")
+    _amplplt.plot_cutflow(
+        labels,
+        hist.values(),
+        ax=ax,
+        text=text,
+        textcolor=textcolor,
+        horizontal=horizontal,
+        **kwargs
+    )
