68b38ff11610b4f6492eecacc574141d40c9d7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:22 +0000

    Add package enrichwrap/0.0.1/enrichwrap-0.0.1.tar.gz

---
+++ packages/enrichwrap/enrichwrap-0.0.1.tar.gz/enrichwrap-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='enrichwrap',
+      version='0.0.1',
+      description='Sample enrichment wrapper',
+      #url='http://github.com/storborg/funniest',
+      author='Sue Hannum',
+      author_email='Sue.Hannum@sas.com',
+      license='MIT',
+      packages=['enrichwrap'],
+      zip_safe=False)
