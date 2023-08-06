8123fec77e5575bce82a04fa79fd21fda57731d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:48 +0000

    Add package PyHyphen/4.0.1/PyHyphen-4.0.1-cp37-abi3-macosx_10_14_x86_64.whl

---
+++ packages/PyHyphen/PyHyphen-4.0.1-cp37-abi3-macosx_10_14_x86_64.whl/hyphen/__init__.py
@@ -0,0 +1,10 @@
+'''
+hyphen - hyphenation for Python
+
+This package contains the 'Hyphenator' class, which is a wrapper class for
+libhyphen. Each instance uses its own hyphenation dictionary.
+'''
+
+__version__ = '4.0.1'
+
+from .hyphenator import Hyphenator
