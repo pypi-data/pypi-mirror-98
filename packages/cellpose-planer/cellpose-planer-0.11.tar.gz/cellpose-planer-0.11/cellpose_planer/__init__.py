90933b60c00b6b60d816824e1a31904d1d463dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package cellpose-planer/0.11/cellpose-planer-0.11.tar.gz

---
+++ packages/cellpose-planer/cellpose-planer-0.11.tar.gz/cellpose-planer-0.11/cellpose_planer/__init__.py
@@ -0,0 +1,68 @@
+import planer
+from . import cellpose
+from .cellpose import load_model, get_flow, tile_flow, flow2msk
+from .render import msk2edge, rgb_mask
+from urllib.request import urlretrieve, urlopen
+
+from glob import glob
+import os, os.path as osp
+from tqdm import tqdm
+
+def engine(core, nimg):
+    global _asnumpy
+    cellpose.np, cellpose.ndimg = core, nimg
+    _asnumpy = planer.core(core)
+    print('\nuser switch engine:', core.__name__)
+
+def asnumpy(arr): return _asnumpy.cpu(arr)
+
+try:
+    import cupy
+    from cupyx.scipy import ndimage as ndimg
+    engine(cupy, ndimg)
+    print('using cupy engine, gpu powered!')
+except:
+    import numpy as np
+    import scipy.ndimage as ndimg
+    engine(np, ndimg)
+    print('using numpy engine, install cupy would be faster.')
+
+root = osp.abspath(osp.dirname(__file__))
+if not osp.exists(root+'/models'): os.mkdir(root+'/models')
+
+models = {}
+
+def search_models():
+    url = 'http://release.imagepy.org/cellpose-planer/README.md'
+    lines = urlopen(url).read().decode('utf-8').split('\n')
+    global models
+    for line in lines:
+        if not '[download](' in line: continue
+        ss = line.split('|')
+        url = ss[-2].split('(')[-1].split(')')[0]
+        name = osp.split(url)[1]
+        models[ss[1].strip()] = url
+    fs = glob(root+'/models/*.npy')
+    has = [osp.split(i)[1][:-4] for i in fs]
+    for i in sorted(models):
+        print(i, ':', 'installed' if i in has else '--')
+    
+def list_models():
+    fs = glob(root+'/models/*.npy')
+    print([osp.split(i)[1][:-4] for i in fs])
+
+def progress(a, b, c, bar):
+    per = int(min(100.0 * a * b / c, 100))
+    bar.update(round(per)-bar.n)
+
+def download(names=['cyto_0','cyto_1','cyto_2','cyto_3']):
+    assert len(models)>0, 'please search_models first!'
+    if names=='all': names = list(models.keys())
+    if isinstance(names, str): names = [names]
+    for name in names:
+        print('download %s from %s'%(name, models[name]))
+        bar = tqdm(total=100)
+        urlretrieve(models[name], root+'/models/'+name+'.npy',
+            lambda a,b,c,bar=bar: progress(a,b,c,bar))
+        urlretrieve(models[name][:-3]+'json',
+                    root+'/models/'+name+'.json')
---
+++ packages/cellpose-planer/cellpose-planer-0.11.tar.gz/cellpose-planer-0.11/cellpose_planer/demo.py
@@ -0,0 +1,96 @@
+import sys; sys.path.append('../')
+import cellpose_planer as cellpp
+import matplotlib.pyplot as plt
+from time import time, sleep
+import numpy as np
+
+from skimage.data import coins, gravel
+
+def search_models():
+    cellpp.search_models()
+    cellpp.download(['cyto_0', 'cyto_1', 'cyto_1', 'cyto_2'])
+    
+def show(img, msk):
+    rgb = cellpp.rgb_mask(img, msk)
+    plt.subplot(121).imshow(img)
+    plt.subplot(122).imshow(rgb)
+    plt.show()
+    
+def np_backend_test():
+    print('numpy backend test:')
+    import numpy as np
+    import scipy.ndimage as ndimg
+    cellpp.engine(np, ndimg)
+
+    img = gravel()
+    x = img.astype(np.float32)/255
+
+    net = cellpp.load_model('cyto_0')
+    start = time()
+    flow, prob, style = cellpp.get_flow(net, x, [0,0], 480)
+    print('\tnet time:', time()-start)
+
+    start = time()
+    msk = cellpp.flow2msk(flow * (5/1.5), None, 1, 20, 100)
+    print('\tflow time:', time()-start)
+
+    show(img, msk)
+
+def cp_backend_test():
+    print('cupy backend test:')
+    import cupy as cp
+    import cupyx.scipy.ndimage as cpimg
+    cellpp.engine(cp, cpimg)
+
+    img = np.tile(np.tile(gravel(), 2).T, 2).T
+    x = img.astype(np.float32)/255
+
+    net = cellpp.load_model('cyto_0')
+    start = time()
+    flow, prob, style = cellpp.get_flow(net, x, [0,0])
+    print('\tnet time first time (need preheat):', time()-start)
+    start = time()
+    flow, prob, style = cellpp.get_flow(net, x, [0,0])
+    print('\tnet time second time (faster):', time()-start)
+    
+    start = time()
+    msk = cellpp.flow2msk(flow * (5/1.5), None, 1, 20, 100)
+    print('\tflow time first time (need preheat):', time()-start)
+    start = time()
+    msk = cellpp.flow2msk(flow * (5/1.5), None, 1, 20, 100)
+    print('\tflow time second time (faster):', time()-start)
+
+    show(img, cellpp.asnumpy(msk))
+
+def resize_test():
+    print('\nresize test:')
+    img = coins()
+    x = img.astype(np.float32)/255
+    print('if the size is not 64 x n, we need resize it.')
+    print('also could be used for speeding up when large image')
+    
+    net = cellpp.load_model('cyto_0')
+    flow, prob, style = cellpp.get_flow(net, x, [0,0], size=0)
+    msk = cellpp.flow2msk(flow * (5/1.5), None, 1, 20, 100)
+    show(img, cellpp.asnumpy(msk))
+
+def large_tile_test():
+    print('\nlarge tile test:')
+    img = np.tile(np.tile(coins(), 10).T, 10).T
+    x = img.astype(np.float32)/255
+    print('if the image is too large, we need tile.')
+    print('sample: resample the image if it is not 1')
+    print('size: tile size, should be 64x, 512/768/1024 recommend')
+    print('work: multi thread, useful for multi-core cpu (gpu insensitive)')
+    net = cellpp.load_model('cyto_0')
+    flow, prob, style = cellpp.tile_flow(net, x, sample=1, size=768, work=4)
+    msk = cellpp.flow2msk(flow * (5/1.5), None, 1, 20, 100)
+    show(img, cellpp.asnumpy(msk))
+    
+if __name__ == '__main__':
+    #np_backend_test()
+    #cp_backend_test()
+    #resize_test()
+    #large_tile_test()
+
+    pass
