4b3a2b68528cd8d341b2898936066d0210c5140d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:27 +0000

    Add package d8s-html/0.1.0/d8s_html-0.1.0.tar.gz

---
+++ packages/d8s-html/d8s_html-0.1.0.tar.gz/d8s_html-0.1.0/d8s_html/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  #type: ignore
+
+try:
+    __version__ = version('democritus_html')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "democritus_html". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-html/issues.'
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+
+from .html_data import *
