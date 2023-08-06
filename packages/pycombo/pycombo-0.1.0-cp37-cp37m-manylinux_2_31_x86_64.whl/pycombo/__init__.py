5e4643397b116b7fc53f3fcf26ad37b1e781bd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package pycombo/0.1.0/pycombo-0.1.0-cp37-cp37m-manylinux_2_31_x86_64.whl

---
+++ packages/pycombo/pycombo-0.1.0-cp37-cp37m-manylinux_2_31_x86_64.whl/pycombo/__init__.py
@@ -0,0 +1,10 @@
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
+from .pyCombo import execute
+
+__all__ = ["execute"]
