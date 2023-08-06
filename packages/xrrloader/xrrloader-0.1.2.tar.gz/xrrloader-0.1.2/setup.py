c7e4be6a694efebffdf0df6f988f13ceb2d8d718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:59 +0000

    Add package xrrloader/0.1.2/xrrloader-0.1.2.tar.gz

---
+++ packages/xrrloader/xrrloader-0.1.2.tar.gz/xrrloader-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as file:
+    requirements = file.readlines()
+
+setup(
+    name='xrrloader',
+    version='0.1.2',
+    author='Alessandro Greco',
+    author_email='alessandro.greco@uni-tuebingen.de',
+    licence='GPL3',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+    python_requires='>=3.6'
+)
