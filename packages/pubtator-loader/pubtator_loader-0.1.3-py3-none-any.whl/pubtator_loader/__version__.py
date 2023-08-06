ccf0982eadf3872c2a67fdcf74e0a2d523a7273e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:59 +0000

    Add package pubtator-loader/0.1.3/pubtator_loader-0.1.3-py3-none-any.whl

---
+++ packages/pubtator-loader/pubtator_loader-0.1.3-py3-none-any.whl/pubtator_loader/__version__.py
@@ -0,0 +1,14 @@
+
+"""
+Variables for setup.py are defined here, allowing them to be accessed from
+within the package:
+Example:
+    >>> from pubtator_loader.__version__ import __version__
+"""
+__name__ = "pubtator_loader"
+__version__ = "0.1.3"
+__description__ = "Parse PubTator files with ease"
+__url__ = "https://github.com/arshsekhon/pubtator_loader"
+__author__ = "Arsh Sekhon"
+__author_email__ = "arshs3khon@gmail.com"
+__license__ = "GNU GPL v3"
