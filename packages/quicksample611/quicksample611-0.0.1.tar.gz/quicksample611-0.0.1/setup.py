de20c36db4abcd014bccebc77916f28b11859689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package quicksample611/0.0.1/quicksample611-0.0.1.tar.gz

---
+++ packages/quicksample611/quicksample611-0.0.1.tar.gz/quicksample611-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+
+    name="quicksample611",
+    version="0.0.1",
+    author="anis dhouieb",
+    description="Quicksample test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["quicksample"],
+    package_dir={'':'quicksample/src'},
+    install_requires=[]
+
+)
