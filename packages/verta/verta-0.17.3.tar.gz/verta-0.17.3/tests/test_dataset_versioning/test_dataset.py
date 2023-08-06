cdd029f80c4d405d8ba6def0332cd79dcd2589c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:28 +0000

    Add package verta/0.17.3/verta-0.17.3.tar.gz

---
+++ packages/verta/verta-0.17.3.tar.gz/verta-0.17.3/tests/test_dataset_versioning/test_dataset.py
@@ -0,0 +1,137 @@
+import collections
+
+import pytest
+import requests
+
+import verta
+
+
+class TestMetadata:
+    def test_description(self, client, created_entities, strs):
+        first_desc, second_desc = strs[:2]
+
+        dataset = client.create_dataset(desc=first_desc)
+        created_entities.append(dataset)
+        assert dataset.get_description() == first_desc
+
+        dataset.set_description(second_desc)
+        assert dataset.get_description() == second_desc
+
+        assert client.get_dataset(id=dataset.id).get_description() == second_desc
+
+    def test_tags(self, client, created_entities, strs):
+        tag1, tag2, tag3, tag4 = strs[:4]
+
+        dataset = client.create_dataset(tags=[tag1])
+        created_entities.append(dataset)
+        assert set(dataset.get_tags()) == {tag1}
+
+        dataset.add_tag(tag2)
+        assert set(dataset.get_tags()) == {tag1, tag2}
+
+        dataset.add_tags([tag3, tag4])
+        assert set(dataset.get_tags()) == {tag1, tag2, tag3, tag4}
+
+        dataset.del_tag(tag3)
+        dataset.del_tag(tag3)  # no error if nonexistent
+        assert set(dataset.get_tags()) == {tag1, tag2, tag4}
+
+        assert set(client.get_dataset(id=dataset.id).get_tags()) == {tag1, tag2, tag4}
+
+    def test_attributes(self, client, created_entities):
+        Attr = collections.namedtuple('Attr', ['key', 'value'])
+        attr1 = Attr('key1', {'a': 1})
+        attr2 = Attr('key2', ['a', 1])
+        attr3 = Attr('key3', 'a')
+        attr4 = Attr('key4', 1)
+
+        dataset = client.create_dataset(attrs=dict([attr1]))
+        created_entities.append(dataset)
+        assert dataset.get_attributes() == dict([attr1])
+
+        dataset.add_attribute(*attr2)
+        assert dataset.get_attributes() == dict([attr1, attr2])
+
+        dataset.add_attributes(dict([attr3, attr4]))
+        assert dataset.get_attributes() == dict([attr1, attr2, attr3, attr4])
+
+        dataset.del_attribute(attr3.key)
+        dataset.del_attribute(attr3.key)  # no error if nonexistent
+        assert dataset.get_attributes() == dict([attr1, attr2, attr4])
+
+        assert client.get_dataset(id=dataset.id).get_attributes() == dict([attr1, attr2, attr4])
+
+        for attr in [attr1, attr2, attr4]:
+            assert dataset.get_attribute(attr.key) == attr.value
+
+        # overwrite
+        new_val = 'b'
+        dataset.add_attribute(attr1.key, new_val)
+        assert dataset.get_attribute(attr1.key) == new_val
+
+
+class TestCreateGet:
+    def test_create(self, client, created_entities):
+        dataset = client.set_dataset()
+        assert dataset
+        created_entities.append(dataset)
+
+        name = verta._internal_utils._utils.generate_default_name()
+        dataset = client.create_dataset(name)
+        assert dataset
+        created_entities.append(dataset)
+
+        with pytest.raises(requests.HTTPError, match="already exists"):
+            assert client.create_dataset(name)
+        with pytest.warns(UserWarning, match="already exists"):
+            client.set_dataset(name=dataset.name, time_created=123)
+
+    def test_get(self, client, created_entities):
+        name = verta._internal_utils._utils.generate_default_name()
+
+        with pytest.raises(ValueError):
+            client.get_dataset(name)
+
+        dataset = client.set_dataset(name)
+        created_entities.append(dataset)
+
+        assert dataset.id == client.get_dataset(dataset.name).id
+        assert dataset.id == client.get_dataset(id=dataset.id).id
+
+        # Deleting non-existing key:
+        dataset.del_attribute("non-existing")
+
+    def test_find(self, client, created_entities, strs):
+        name1, name2, name3, name4, tag1, tag2 = (
+            s + str(verta._internal_utils._utils.now())
+            for s in strs[:6]
+        )
+
+        dataset1 = client.create_dataset(name1, tags=[tag1])
+        dataset2 = client.create_dataset(name2, tags=[tag1])
+        dataset3 = client.create_dataset(name3, tags=[tag2])
+        dataset4 = client.create_dataset(name4, tags=[tag2])
+        created_entities.extend([dataset1, dataset2, dataset3, dataset4])
+
+        datasets = client.datasets.find("name == {}".format(name3))
+        assert len(datasets) == 1
+        assert datasets[0].id == dataset3.id
+
+        datasets = client.datasets.find("tags ~= {}".format(tag1))
+        assert len(datasets) == 2
+        assert set(dataset.id for dataset in datasets) == {dataset1.id, dataset2.id}
+
+    def test_repr(self, client, created_entities):
+        description = "this is a cool dataset"
+        tags = [u"tag1", u"tag2"]
+        dataset = client.set_dataset(desc=description, tags=tags)
+        created_entities.append(dataset)
+
+        str_repr = repr(dataset)
+
+        assert "name: {}".format(dataset.name) in str_repr
+        assert "id: {}".format(dataset.id) in str_repr
+        assert "time created" in str_repr
+        assert "time updated" in str_repr
+        assert "description: {}".format(description) in str_repr
+        assert "tags: {}".format(tags) in str_repr
---
+++ packages/verta/verta-0.17.3.tar.gz/verta-0.17.3/tests/test_permissions/test_visibility_api.py
@@ -0,0 +1,216 @@
+"""
+Basic tests to make sure the client passes `visibility` without errors.
+
+"""
+import pytest
+
+import requests
+
+from verta._protos.public.common import CommonService_pb2 as _CommonCommonService
+from verta._protos.public.modeldb import DatasetService_pb2 as _DatasetService
+from verta._protos.public.modeldb import ProjectService_pb2 as _ProjectService
+from verta._protos.public.modeldb.versioning import VersioningService_pb2 as _VersioningService
+
+from verta._internal_utils import _utils
+from verta.visibility import (
+    OrgCustom,
+    Private,
+)
+
+
+pytestmark = pytest.mark.not_oss
+
+
+def assert_visibility(entity, visibility, entity_name):
+    if not entity._msg.HasField('custom_permission'):
+        pytest.skip("backend does not support new visibility")
+
+    assert entity._msg.custom_permission == visibility._custom_permission
+    if entity_name == "registered_model":
+        assert entity._msg.resource_visibility == visibility._visibility
+    else:
+        assert entity._msg.visibility == visibility._visibility
+
+
+def assert_endpoint_visibility(endpoint, visibility):
+    endpoint_json = endpoint._get_json_by_id(endpoint._conn, endpoint.workspace, endpoint.id)
+    if 'custom_permission' not in endpoint_json['creator_request']:
+        pytest.skip("backend does not support new visibility")
+
+    assert endpoint_json['creator_request']['custom_permission']['collaborator_type'] == visibility._collaborator_type_str
+    assert endpoint_json['creator_request']['resource_visibility'] == visibility._visibility_str
+
+
+def assert_repository_visibility(repo, visibility):
+    repo_msg = repo._get_proto_by_id(repo._conn, repo.id)
+
+    if not repo_msg.HasField('custom_permission'):
+        pytest.skip("backend does not support new visibility")
+
+    assert repo_msg.custom_permission == visibility._custom_permission
+    assert repo_msg.visibility == visibility._visibility
+
+
+class TestCreate:
+    @pytest.mark.parametrize(
+        ("entity_name", "visibility"),
+        [
+            ("dataset", OrgCustom(write=True)),
+            ("project", OrgCustom(write=True, deploy=True)),
+            ("registered_model", OrgCustom(write=True, deploy=True)),
+        ]
+    )
+    def test_mdb_entity(self, client, organization, entity_name, visibility):
+        create_entity = getattr(client, "create_{}".format(entity_name))
+
+        entity = create_entity(workspace=organization.name, visibility=visibility)
+        try:
+            assert_visibility(entity, visibility, entity_name)
+        finally:
+            entity.delete()
+            client._ctx.proj = None  # otherwise client teardown tries to delete
+
+    def test_endpoint(self, client, organization):
+        visibility = OrgCustom(write=True)
+
+        endpoint = client.create_endpoint(
+            path=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            assert_endpoint_visibility(endpoint, visibility)
+        finally:
+            endpoint.delete()
+
+    @pytest.mark.skip(reason="client.create_repository() does not yet exist")
+    def test_repository(self, client, organization):
+        raise NotImplementedError
+
+
+class TestSet:
+    @pytest.mark.parametrize(
+        ("entity_name", "visibility"),
+        [
+            ("dataset", OrgCustom(write=True)),
+            ("project", OrgCustom(write=True, deploy=True)),
+            ("registered_model", OrgCustom(write=True, deploy=True)),
+        ]
+    )
+    def test_mdb_entity(self, client, organization, entity_name, visibility):
+        set_entity = getattr(client, "set_{}".format(entity_name))
+
+        entity = set_entity(workspace=organization.name, visibility=visibility)
+        try:
+            assert_visibility(entity, visibility, entity_name)
+
+            # second set ignores visibility
+            with pytest.warns(UserWarning, match="cannot set"):
+                entity = set_entity(entity.name, workspace=organization.name, visibility=Private())
+            assert_visibility(entity, visibility, entity_name)
+        finally:
+            entity.delete()
+            client._ctx.proj = None  # otherwise client teardown tries to delete
+
+    def test_endpoint(self, client, organization):
+        visibility = OrgCustom(write=True)
+
+        endpoint = client.set_endpoint(
+            path=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            assert_endpoint_visibility(endpoint, visibility)
+
+            # second set ignores visibility
+            with pytest.warns(UserWarning, match="cannot set"):
+                endpoint = client.set_endpoint(path=endpoint.path, workspace=organization.name, visibility=Private())
+            assert_endpoint_visibility(endpoint, visibility)
+        finally:
+            endpoint.delete()
+
+    def test_repository(self, client, organization):
+        visibility = OrgCustom(write=True)
+
+        repo = client.set_repository(
+            name=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            assert_repository_visibility(repo, visibility)
+
+            # second set ignores visibility
+            repo = client.set_repository(name=repo.name, workspace=organization.name, visibility=Private())
+            assert_repository_visibility(repo, visibility)
+        finally:
+            repo.delete()
+
+
+class TestPublicWithinOrg:
+    """
+    `visibility` gets translated to an equivalent `public_within_org` value for
+    compatibility with older backends.
+
+    """
+    def test_dataset(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_dataset(workspace=organization.name, visibility=visibility)
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.dataset_visibility == _DatasetService.DatasetVisibilityEnum.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.dataset_visibility == _DatasetService.DatasetVisibilityEnum.PRIVATE
+        finally:
+            entity.delete()
+
+    def test_endpoint(self, client, organization):
+        visibility = OrgCustom(write=True)
+        endpoint = client.set_endpoint(
+            path=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            endpoint_json = endpoint._get_json_by_id(endpoint._conn, endpoint.workspace, endpoint.id)
+            if visibility._to_public_within_org():
+                assert endpoint_json['creator_request']['visibility'] == "ORG_SCOPED_PUBLIC"
+            else:
+                assert endpoint_json['creator_request']['visibility'] == "PRIVATE"
+        finally:
+            endpoint.delete()
+
+    def test_project(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_project(workspace=organization.name, visibility=visibility)
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.project_visibility == _ProjectService.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.project_visibility == _ProjectService.PRIVATE
+        finally:
+            entity.delete()
+            client._ctx.proj = None  # otherwise client teardown tries to delete
+
+    def test_registered_model(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_registered_model(workspace=organization.name, visibility=visibility)
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.visibility == _CommonCommonService.VisibilityEnum.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.visibility == _CommonCommonService.VisibilityEnum.PRIVATE
+        finally:
+            entity.delete()
+
+    def test_repository(self, client, organization):
+        visibility = OrgCustom(write=True)
+        repo = client.set_repository(
+            name=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            retrieved_visibility = repo._get_proto_by_id(repo._conn, repo.id).repository_visibility
+            if visibility._to_public_within_org():
+                assert retrieved_visibility == _VersioningService.RepositoryVisibilityEnum.ORG_SCOPED_PUBLIC
+            else:
+                assert retrieved_visibility == _VersioningService.RepositoryVisibilityEnum.PRIVATE
+        finally:
+            repo.delete()
