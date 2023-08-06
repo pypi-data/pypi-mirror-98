2ffbc1a42b831a18e6c7d95f918082dc6d266dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package midas/0.4.3b1/midas-0.4.3b1.tar.gz

---
+++ packages/midas/midas-0.4.3b1.tar.gz/midas-0.4.3b1/setup.py
@@ -0,0 +1,36 @@
+"""Python driver and command line tool for Honeywell Midas gas detectors."""
+from setuptools import setup
+
+with open('README.md', 'r') as in_file:
+    long_description = in_file.read()
+
+setup(
+    name="midas",
+    version="0.4.3b1",
+    description="Python driver for Honeywell Midas gas detectors.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="http://github.com/numat/midas/",
+    author="Patrick Fuller",
+    author_email="pat@numat-tech.com",
+    packages=['midas'],
+    package_data={'midas': ['faults.csv']},
+    install_requires=[
+        'pymodbus==2.5.0'
+    ],
+    entry_points={
+        'console_scripts': [('midas = midas:command_line')]
+    },
+    license='GPLv2',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces'
+    ]
+)
