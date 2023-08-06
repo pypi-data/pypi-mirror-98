9b59df2b0fc2d2b03865b8917c2f06d01af12a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:01 +0000

    Add package path4gmns/0.5.0/path4gmns-0.5.0.tar.gz

---
+++ packages/path4gmns/path4gmns-0.5.0.tar.gz/path4gmns-0.5.0/path4gmns/__init__.py
@@ -0,0 +1,5 @@
+from .classes import *
+from .path import *
+from .util import *
+from .colgen import *
+from .dtaapi import *
---
+++ packages/path4gmns/path4gmns-0.5.0.tar.gz/path4gmns-0.5.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="path4gmns", 
+    version="0.5.0",
+    author="Dr. Xuesong Zhou, Dr. Peiheng Li",
+    author_email="xzhou74@asu.edu, jdlph@hotmail.com",
+    description="dev",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdlph/PATH4GMNS",
+    packages=['path4gmns'],
+    package_dir={'path4gmns': 'path4gmns'},
+    package_data={'path4gmns': ['bin/*', 'data/*/*.csv']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
