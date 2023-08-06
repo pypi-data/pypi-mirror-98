2c1ec7833a0db0d9b01a7ef7d36b3523c26b68fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:54 +0000

    Add package pp.server/3.0.0.7/pp.server-3.0.0.7.tar.gz

---
+++ packages/pp.server/pp.server-3.0.0.7.tar.gz/pp.server-3.0.0.7/pp/server/logger.py
@@ -0,0 +1,8 @@
+################################################################
+# pp.server - Produce & Publish Server
+# (C) 2021, ZOPYX,  Tuebingen, Germany
+################################################################
+
+""" package wide logger module """
+
+from loguru import logger as LOG # noqa
