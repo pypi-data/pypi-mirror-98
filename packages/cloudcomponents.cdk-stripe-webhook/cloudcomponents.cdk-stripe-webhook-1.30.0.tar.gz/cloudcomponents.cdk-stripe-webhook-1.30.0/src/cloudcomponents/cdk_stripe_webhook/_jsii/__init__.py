90639161862be5bd30e9f51befc28cdd26ad55bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:16 +0000

    Add package cloudcomponents.cdk-stripe-webhook/1.30.0/cloudcomponents.cdk-stripe-webhook-1.30.0.tar.gz

---
+++ packages/cloudcomponents.cdk-stripe-webhook/cloudcomponents.cdk-stripe-webhook-1.30.0.tar.gz/cloudcomponents.cdk-stripe-webhook-1.30.0/src/cloudcomponents/cdk_stripe_webhook/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import cloudcomponents.cdk_secret_key._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-stripe-webhook",
+    "1.30.0",
+    __name__[0:-6],
+    "cdk-stripe-webhook@1.30.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
