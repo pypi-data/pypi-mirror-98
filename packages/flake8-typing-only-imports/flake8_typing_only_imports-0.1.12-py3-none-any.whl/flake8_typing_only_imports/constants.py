91091788768e2c1a065d72cdf48650b043dd24d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:40 +0000

    Add package flake8-typing-only-imports/0.1.12/flake8_typing_only_imports-0.1.12-py3-none-any.whl

---
+++ packages/flake8-typing-only-imports/flake8_typing_only_imports-0.1.12-py3-none-any.whl/flake8_typing_only_imports/constants.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+TYO100 = "TYO100: Import '{module}' should be moved to a type-checking block"
+TYO101 = "TYO101: Third-party import '{module}' should be moved to a type-checking block"
+TYO200 = "TYO200: Missing 'from __future__ import annotations' import"
+TYO201 = "TYO201: Annotation '{annotation}' is wrapped in unnecessary quotes"
+TYO300 = "TYO300: Annotation '{annotation}' should be wrapped in quotes"
+TYO301 = "TYO301: Annotation '{annotation}' is wrapped in unnecessary quotes"
+
+disabled_by_default: list[str] = ['TYO101', 'TYO300', 'TYO301', 'TYO200', 'TYO201']
