d648f92473381cd92f92efb164fadb5034e7c93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:05 +0000

    Add package carson/1.0.5/carson-1.0.5-py3-none-any.whl

---
+++ packages/carson/carson-1.0.5-py3-none-any.whl/carson/__main__.py
@@ -0,0 +1,96 @@
+
+import argparse
+import asyncio
+import warnings
+
+from getpass import getpass
+
+from . import auth, logging, Session, TeslaCredentialError, __version__ as _version
+
+
+def main():
+    parser = argparse.ArgumentParser('carson', description=f'Command line utility for carson/{_version}.')
+    parser.add_argument('--email', '-e', help='The email associated with your Tesla account.')
+    parser.add_argument('--password', '-p', metavar='P', help=argparse.SUPPRESS)
+    parser.add_argument('--access-token', '-t', '--token', help='An access token that can be used in lieue of email/password.')
+    parser.add_argument('--list', '-l', default=False, action='store_true', help='List vehicles associated with the account and exit.')
+    parser.add_argument('--name', '-n', '--car-name', '--car', '--display-name', help='The vehicle display name')
+    parser.add_argument('--stream', '-s', default=False, action='store_true', help='Start streaming telemetry.')
+    parser.add_argument('--command', '-c', default=[], action='append', help='Perform the data request or command specified.')
+    parser.add_argument('--wake', dest='wake', default=False, action='store_true', help='Wake the car if necessary.')
+    parser.add_argument('--no-wake', default=None, action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('--verbose', '-v', default=0, action='count')
+    args = parser.parse_args()
+
+    if args.no_wake:
+        warnings.warn('The --no-wake argument is deprecated.  You can explicitly wake using the --wake argument.')
+        if args.wake:
+            raise SystemExit('Cannot specify both --wake and --no-wake')
+
+    if not any([args.list, args.command, args.stream, args.name]):
+        return parser.print_help()
+
+    logging.initLogging(debug=args.verbose)
+    try:
+        asyncio.run(start(args), debug=args.verbose)
+    except TeslaCredentialError as err:
+        print(err, '\n')
+        parser.print_help()
+    except KeyboardInterrupt:
+        pass
+
+
+async def start(args):
+
+    session = Session(email=args.email, password=args.password, access_token=args.access_token, verbose=args.verbose)
+    if session.email and not session.password and not session.access_token:
+        session.password = getpass(f'Password for {session.email}: ')
+    try:
+
+        result = await session.vehicles(name=args.name) if args.name else await session.car
+        if not result:
+            logging.error('Nothing associated with this account!')
+            return
+
+        if args.list or isinstance(result, list):
+            result = result if isinstance(result, list) else [result,]
+            for carnbr, car in enumerate(result, start=1):
+                logging.info('Car #%d %r', carnbr, car)
+            return
+
+        car = result[0] if isinstance(result, list) else result
+        if car.in_service:
+            logging.info('%r', car)
+            if args.wake or args.stream or args.command:
+                logging.error('Cannot perform any more commands while car is in Tesla Service.')
+            return
+
+        if car.state != 'online' and args.wake:
+            logging.debug('Waking up car...')
+            await car.wake_up()
+
+        if args.stream:
+            return await car.stream()
+
+        if car.state == 'online':
+            await car.data()
+
+        logging.info('%r', car)
+
+        for cmd in args.command:
+            if 'stream' in cmd.lower():
+                continue
+            func = getattr(car, cmd.lower())
+            if asyncio.iscoroutinefunction(func):
+                logging.info('Result of `await %s()` is %s', cmd, await func())
+            elif callable(func):
+                logging.info('Result of `%s()` is %s', cmd, func())
+            else:
+                logging.info('Result of %r is %s', cmd, func)
+
+    finally:
+        await session.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/carson/carson-1.0.5-py3-none-any.whl/carson/utils.py
@@ -0,0 +1,37 @@
+
+import json
+from datetime import datetime, timedelta, timezone
+
+_ONE_YEAR_MILLIS = timedelta(days=365).total_seconds() * 1000
+
+
+class JSONEncoderHelper(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            if not obj.tzinfo:
+                obj = datetime.fromtimestamp(obj.timestamp(), tz=timezone.utc)
+            return obj.isoformat(timespec='microseconds')
+        if hasattr(obj, 'dump') and callable(obj.dump):
+            return obj.dump()
+        return super().default(obj)
+
+
+def _json_timestamp_decoder(obj):
+    for attr in ('timestamp', 'scheduled_charging_start_time', 'gps_as_of'):
+        if attr in obj:
+            val = obj.get(attr)
+            if (not isinstance(val, int) and not isinstance(val, float)) or val <= 0:
+                val = None
+            else:
+                val = val / 1000 if val > _ONE_YEAR_MILLIS else val
+                val = datetime.fromtimestamp(val, tz=timezone.utc)
+            obj[attr] = val
+    return obj
+
+
+def json_loads(txt):
+    return json.loads(txt, object_hook=_json_timestamp_decoder)
+
+
+def json_dumps(obj):
+    return json.dumps(obj, cls=JSONEncoderHelper)
