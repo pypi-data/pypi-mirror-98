e715bc05b549edacbfc550fe38de80e68162f395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:18 +0000

    Add package vsmetaEncoder/0.9/vsmetaEncoder-0.9.tar.gz

---
+++ packages/vsmetaEncoder/vsmetaEncoder-0.9.tar.gz/vsmetaEncoder-0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='vsmetaEncoder',
+      version='0.9',
+      description='A module to encode media information with Synology\'s vsmeta file format.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/TomMeHo/vsMetaFileEncoder',
+      author='Thomas Meder',
+      author_email='tom@tommho.net',
+      license='BSD',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      install_requires=[
+            'datetime >= 4.3'
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Logging"
+      ],
+      python_requires='>=3.6'
+)
