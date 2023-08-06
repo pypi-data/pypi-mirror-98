785e021f0792bd54073c9b19a6429966e978173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:38 +0000

    Add package aioradio/0.12.1/aioradio-0.12.1.tar.gz

---
+++ packages/aioradio/aioradio-0.12.1.tar.gz/aioradio-0.12.1/setup.py
@@ -0,0 +1,54 @@
+"""Python utility for NRCCUA common generic functions to reuse across
+projects."""
+
+from setuptools import setup
+
+with open('README.md', 'r') as fileobj:
+    long_description = fileobj.read()
+
+setup(name='aioradio',
+    version='0.12.1',
+    description='Generic asynchronous i/o python utilities for AWS services (SQS, S3, DynamoDB, Secrets Manager), Redis, MSSQL (pyodbc), JIRA and more',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nrccua/aioradio',
+    author='NRCCUA Architects',
+    author_email='architecture@nrccua.org',
+    license="MIT",
+    packages=[
+        'aioradio',
+        'aioradio/aws',
+    ],
+    install_requires=[
+        'aiobotocore',
+        'aioboto3',
+        'aiojobs',
+        'aioredis',
+        'boto3',
+        'ddtrace',
+        'httpx',
+        'mandrill',
+        'orjson',
+        'psycopg2-binary',
+        'pysmb',
+        'python-json-logger',
+        'xlrd'
+    ],
+    dependency_links=['git+https://github.com/aio-libs/aioredis@sean/aioredis-redis-py-compliance'],
+    include_package_data=True,
+    tests_require=[
+        'flask',
+        'moto',
+        'pre-commit',
+        'pylint',
+        'pytest',
+        'pytest-asyncio',
+        'pytest-cov'
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
