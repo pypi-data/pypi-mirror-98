c9b6ff0bae42057dc9c021efc4c296969f9f0b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package b2bapi/2.0.0/b2bapi-2.0.0.tar.gz

---
+++ packages/b2bapi/b2bapi-2.0.0.tar.gz/b2bapi-2.0.0/b2bapi/wrapper.py
@@ -0,0 +1,83 @@
+import requests, aiohttp, asyncio
+from time import sleep
+from .meme import Meme
+from .madlib import Madlib
+from .text import Text
+from .word import Word
+from .errors.response_check import response_check, token_check
+
+class BytesToBits:
+
+    def __init__(self, token: str) -> None:
+        self.auth_header = {
+            'Authorization': token
+        }
+        self.BASE_URL = 'https://api.bytestobits.dev'
+        token_check(token)
+        return
+
+    def get_word(self) -> Word:
+        "Returns a random word from the API"
+        ret = requests.get(f'{self.BASE_URL}/word/', headers=self.auth_header)
+        response_check(ret)
+        return Word(ret.json())
+        
+    def get_text(self) -> Text:
+        "Returns a random paragraph from the API"
+        ret = requests.get(f'{self.BASE_URL}/text/', headers=self.auth_header)
+        response_check(ret)
+        return Text(ret.json())
+
+    def get_meme(self) -> Meme:
+        "Returns a random meme from a random subreddit through the API"
+        ret = requests.get(f'{self.BASE_URL}/meme/', headers=self.auth_header)
+        response_check(ret)
+        ret = ret.json()
+        return Meme(ret['title'], ret['url'], ret['link'], ret['subreddit'])
+
+    def get_madlib(self) -> Madlib:
+        "Returns a random madlib from the API"
+        ret = requests.get(f'{self.BASE_URL}/madlibs/', headers=self.auth_header)
+        response_check(ret)
+        ret = ret.json()
+        return Madlib(ret['title'], ret['text'], ret['questions'], ret['variables'])
+
+class AsynchronousBytesToBits:
+
+    def __init__(self, token: str) -> None:
+        self.auth_header = {
+            'Authorization': token
+        }
+        token_check(token)
+        self.BASE_URL = 'https://api.bytestobits.dev'
+        return
+
+    async def get_word(self) -> Word:
+        "Returns a random word from the API in an asynchronous context"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.BASE_URL}/word/', headers=self.auth_header) as request:
+                response_check(request)
+                return Word(await request.json())
+    
+    async def get_text(self) -> Text:
+        "Returns a random paragraph from the API in an asynchronous context"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.BASE_URL}/text/', headers=self.auth_header) as request:
+                response_check(request)
+                return Text(await request.json())
+
+    async def get_meme(self) -> Meme:
+        "Returns a random meme from a random subreddit through the API in an asynchronous context"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.BASE_URL}/meme/', headers=self.auth_header) as request:
+                response_check(request)
+                ret = await request.json()
+                return Meme(ret['title'], ret['url'], ret['link'], ret['subreddit'])
+
+    async def get_madlib(self) -> Madlib:
+        "Returns a random madlib from the API in an asynchronous context"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.BASE_URL}/madlibs/', headers=self.auth_header) as request:
+                response_check(request)
+                ret = await request.json()
+                return Madlib(ret['title'], ret['text'], ret['questions'], ret['variables'])
