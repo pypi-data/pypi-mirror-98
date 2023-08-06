3481a88428b0a056dc616b85be95707fc66d0cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package custom-menu/0.1.0/custom-menu-0.1.0.tar.gz

---
+++ packages/custom-menu/custom-menu-0.1.0.tar.gz/custom-menu-0.1.0/src/index.ts
@@ -0,0 +1,61 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import { Menu } from '@lumino/widgets';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the main menu example.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'main-menu',
+  autoStart: true,
+  requires: [ICommandPalette, IMainMenu],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    mainMenu: IMainMenu
+  ) => {
+    const { commands } = app;
+
+    // Add a command
+    const command = 'jlab-examples:main-menu';
+    commands.addCommand(command, {
+      label: 'Execute jlab-examples:main-menu Command',
+      caption: 'Execute jlab-examples:main-menu Command',
+      execute: (args: any) => {
+        console.log(
+          `jlab-examples:main-menu has been called ${args['origin']}.`
+        );
+        window.alert(
+          `jlab-examples:main-menu has been called ${args['origin']}.`
+        );
+      }
+    });
+
+    // Add the command to the command palette
+    const category = 'Extension Examples';
+    palette.addItem({
+      command,
+      category,
+      args: { origin: 'from the palette' }
+    });
+
+    // Create a menu
+    const tutorialMenu: Menu = new Menu({ commands });
+    tutorialMenu.title.label = 'About nmdev';
+    mainMenu.addMenu(tutorialMenu, { rank: 80 });
+
+    // Add the command to the menu
+    tutorialMenu.addItem({ command, args: { origin: 'from the menu' } });
+  }
+};
+
+
+
+export default extension;
