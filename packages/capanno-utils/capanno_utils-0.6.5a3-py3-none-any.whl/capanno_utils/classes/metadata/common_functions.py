927398f48a68e33c59501133fb01d334e940ebcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:23 +0000

    Add package capanno-utils/0.6.5a3/capanno_utils-0.6.5a3-py3-none-any.whl

---
+++ packages/capanno-utils/capanno_utils-0.6.5a3-py3-none-any.whl/capanno_utils/classes/metadata/common_functions.py
@@ -0,0 +1,210 @@
+#
+# * This file is subject to the terms and conditions defined in
+# * file 'LICENSE.txt', which is part of this source code package.
+
+from .shared_properties import Publication, Person, CodeRepository, WebSite, Keyword, SoftwareVersion, object_attributes
+from hashlib import md5
+import uuid
+
+
+
+
+def _mk_hashes(arg1, *args):
+    arg1 = str(arg1)
+    hashes = [md5(arg1.encode(encoding='utf-8')).hexdigest()]
+    for arg in args:
+        arg = str(arg)
+        hashes.append(md5(arg.encode(encoding='utf-8')).hexdigest())
+    return hashes
+
+
+def mk_empty_prop_object(property_name):
+    """
+    Should only call this to override a None value for a property.
+    :param property_name:
+    :return:
+    """
+    prop_map = {'codeRepository': {'name': None}, 'publication': [{'identifier': None}],
+                'contactPoint': [{'name': None}], 'creator': [{'name': None}], 'WebSite': [{'name': None}], 'keywords': [Keyword()], 'applicationSuite': {'name': None, 'softwareVersion': None, 'identifier': None}}
+    if not property_name in prop_map:
+        return None
+
+    return prop_map[property_name]
+
+
+def is_attr_empty(attribute):
+    # Need to check for lists.
+    if isinstance(attribute, list):
+        is_empty = True
+        for item in attribute:
+            if not is_attr_empty(item):
+                is_empty = False
+                break
+    elif isinstance(attribute, object_attributes):
+        is_empty = attribute.is_empty()
+    else:
+        if attribute:
+            is_empty = False
+        else:
+            is_empty = True
+    return is_empty
+
+
+class CommonPropsMixin:
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not new_name:
+            raise ValueError(f"'name'' must be set.")
+        self._name = new_name
+
+    @property
+    def softwareVersion(self):
+        return (self._softwareVersion)
+
+    @softwareVersion.setter
+    def softwareVersion(self, software_version_info):
+
+        if not software_version_info:  # softwareVersion not defined. Check in parentMetadata
+            raise ValueError(f"'softwareVersion must be set.")
+        if isinstance(software_version_info, SoftwareVersion):
+            software_version = software_version_info
+        elif isinstance(software_version_info, dict):
+            software_version = SoftwareVersion(**software_version_info)
+            # software_version = SoftwareVersion() # Don't initialize directly with **software_version_info. Skips setters.
+            # software_version.versionName, software_version.includedVersions = software_version_info['versionName'], software_version_info['includedVersions']
+        else:
+            raise TypeError(f"Cannot create softwareVersion with {software_version_info}")
+        self._softwareVersion = software_version
+        return
+
+    @property
+    def publication(self):
+        return self._publication
+
+    @publication.setter
+    def publication(self, publication_list):
+        if publication_list:
+            publications = [Publication(**pub) for pub in publication_list]
+        else:
+            publications = None
+        self._publication = publications
+
+    @property
+    def contactPoint(self):
+        return self._contactPoint
+
+    @contactPoint.setter
+    def contactPoint(self, person_list):
+        if person_list:
+            if isinstance(person_list[0], dict):
+                people = [Person(**person) for person in person_list]
+            elif isinstance(person_list[0], Person):
+                people = person_list
+        else:
+            people = None
+        self._contactPoint = people
+
+    @property
+    def creator(self):
+        return self._creator
+
+    @creator.setter
+    def creator(self, person_list):
+        if person_list:
+            if isinstance(person_list[0], dict):
+                people = [Person(**person) for person in person_list]
+            elif isinstance(person_list[0], Person):
+                people = person_list
+            else:
+                raise NotImplementedError
+        else:
+            people = None
+        self._creator = people
+
+    @property
+    def codeRepository(self):
+        return self._codeRepository
+
+    @codeRepository.setter
+    def codeRepository(self, code_repo_info):
+        if code_repo_info:
+            if isinstance(code_repo_info, CodeRepository):
+                code_repo = code_repo_info
+            elif isinstance(code_repo_info, dict):
+                code_repo = CodeRepository(**code_repo_info)
+            else:
+                raise TypeError(f"Cannot create codeRepository with {code_repo_info}")
+        else:
+            code_repo = None
+        self._codeRepository = code_repo
+
+    @property
+    def keywords(self):
+        return self._keywords
+
+    @keywords.setter
+    def keywords(self, keywords_list):
+        if keywords_list:
+            keywords = []
+            for keyword in keywords_list:
+                if isinstance(keyword, Keyword):
+                    new_keyword = keyword
+                else:
+                    if isinstance(keyword, dict):
+                        new_keyword  =  Keyword(**keyword)
+                    else:
+                        new_keyword = Keyword(keyword)  # Should be a uri.
+                keywords.append(new_keyword)
+        else:
+            keywords = None
+        self._keywords = keywords
+
+    @property
+    def WebSite(self):
+        return self._website
+
+    @WebSite.setter
+    def WebSite(self, website_list):
+        if website_list:
+            if isinstance(website_list[0], WebSite):
+                websites = website_list
+            elif isinstance(website_list[0], dict):
+                websites = [WebSite(**website_dict) for website_dict in website_list]
+            else:
+                raise TypeError(f"Cannot create WebSite with {website_list}")
+        else:
+            websites = None
+        self._website = websites
+
+    @property
+    def cwlStatus(self):
+        return self._cwlStatus
+
+    @cwlStatus.setter
+    def cwlStatus(self, cwl_status):
+        allowed_statuses = ('Incomplete', 'Draft', 'Released')
+        if not cwl_status:
+            raise ValueError("cwlStatus must be set.")
+        elif cwl_status not in allowed_statuses:
+            raise ValueError(f"cwlStatus must be on of  {allowed_statuses}, not {cwl_status}")
+        else:
+            self._cwlStatus = cwl_status
+
+    @property
+    def metadataStatus(self):
+        return self._metadataStatus
+
+    @metadataStatus.setter
+    def metadataStatus(self, metadata_status):
+        allowed_statuses = ('Incomplete', 'Draft', 'Released')
+        if not metadata_status:
+            raise ValueError("cwlStatus must be set.")
+        elif metadata_status not in allowed_statuses:
+            raise ValueError(f"cwlStatus must be on of  {allowed_statuses}, not {metadata_status}")
+        else:
+            self._metadataStatus = metadata_status
+
---
+++ packages/capanno-utils/capanno_utils-0.6.5a3-py3-none-any.whl/tests/test_validate_all_metadata_in_maps.py
@@ -0,0 +1,50 @@
+import os
+from tests.test_base import TestBase
+from pathlib import Path
+from ruamel.yaml import safe_load
+from capanno_utils.repo_config import config
+from capanno_utils.helpers.get_paths import get_metadata_path
+from capanno_utils.validate_content import main
+
+class TestValidateContent(TestBase):
+
+    def test_validate_tool_metadata(self):
+        self.update_tool_maps()
+        with self.get_content_map_paths()['tool_maps'].open('r') as tm:
+            tool_map_dict = safe_load(tm)
+        base_path = config[os.environ['CONFIG_KEY']]['base_path']
+        for identifier, values in tool_map_dict.items():
+            path = base_path / values['path']
+
+            tool_type = values['type']
+            if tool_type == 'parent':
+                if not 'common' in path.parts:  # values[type] would be better test.
+                    raise ValueError(f"Have a parent tool that is not in a common directory {path}")
+                meta_path = path
+            else:  # either a subtool or standalone tool.
+                meta_path = get_metadata_path(path)
+            main([str(meta_path), '-q'])
+        return
+
+    def test_validate_script_metadata(self):
+        self.update_script_maps()
+        script_map_path = self.get_content_map_paths()['script_maps']
+        with script_map_path.open('r') as sm:
+            script_map = safe_load(sm)
+        base_path = config[os.environ['CONFIG_KEY']]['base_path']
+        for script_identifier, script_values in script_map.items():
+            metadata_path = base_path / get_metadata_path(Path(script_values['path']))
+            main([str(metadata_path), '-q'])
+        return
+
+
+    def test_validate_workflow_metadata(self):
+        self.update_workflow_maps()
+        with self.get_content_map_paths()['workflow_maps'].open('r') as wm:
+            workflow_map = safe_load(wm)
+            base_path = config[os.environ['CONFIG_KEY']]['base_path']
+
+        for workflow_identifier, workflow_values in workflow_map.items():
+            metadata_path = base_path / get_metadata_path(workflow_values['path'])
+            main([str(metadata_path), '-q'])
+        return
