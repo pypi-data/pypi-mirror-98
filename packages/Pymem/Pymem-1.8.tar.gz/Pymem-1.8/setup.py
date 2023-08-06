ed838759a8367ee2ee95a5d2ddfb5adc303dea86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package Pymem/1.8/Pymem-1.8.tar.gz

---
+++ packages/Pymem/Pymem-1.8.tar.gz/Pymem-1.8/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+import setuptools
+
+ROOT = str(pathlib.Path(__file__).parent)
+
+extras = {}
+
+with open(ROOT + '/requirements-doc.txt', encoding='utf-8') as fp:
+    extras['doc'] = fp.read().splitlines()
+
+with open(ROOT + '/requirements-test.txt', encoding='utf-8') as fp:
+    extras['test'] = fp.read().splitlines()
+
+with open(ROOT + '/PYPI-README.md', encoding="utf-8") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name='Pymem',
+    version='1.8',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='pymem: python memory access made easy',
+    author='Fabien Reboia',
+    author_email='srounet@gmail.com',
+    maintainer='Fabien Reboia',
+    maintainer_email='srounet@gmail.com',
+    url='http://pymem.readthedocs.org/en/latest/',
+    license="mit",
+    packages=["pymem", "pymem.ressources"],
+    platforms=["windows"],
+    keywords='memory win32 windows process',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    extras_require=extras,
+)
