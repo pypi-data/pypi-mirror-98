cfc45c3bad8204b5aa19e2c9b61b97aed036a386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:34 +0000

    Add package unomi-query-language/0.1.4/unomi_query_language-0.1.4-py3-none-any.whl

---
+++ packages/unomi-query-language/unomi_query_language-0.1.4-py3-none-any.whl/unomi_query_language/query/mappers/operation_mapper.py
@@ -0,0 +1,15 @@
+operation_mapper = {
+    'exists': "exists",
+    "between": "between",
+    "=": "equals",
+    "!=": "notEquals",
+    '>=': 'greaterThanOrEqualTo',
+    '<=': 'lessThanOrEqualTo',
+    '=>': 'greaterThanOrEqualTo',
+    '=<': 'lessThanOrEqualTo',
+    '>': 'greaterThan',
+    '<': 'lessThan',
+    'is null': 'isNull',
+    'not exists': 'missing',
+    'contains': 'contains'
+}
