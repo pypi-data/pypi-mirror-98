eb983a073f46a072e67f3abe80ff9b8a78d83920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package cpymad/1.7.0/cpymad-1.7.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/cpymad/cpymad-1.7.0-cp35-cp35m-manylinux1_x86_64.whl/cpymad/stream.py
@@ -0,0 +1,78 @@
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+# The following code makes sure to read all available stdout lines before
+# sending more input to MAD-X (ensure the real chronological order!), see:
+#       linux:   https://stackoverflow.com/q/375427/650222
+#       windows: https://stackoverflow.com/a/34504971/650222
+#                https://gist.github.com/techtonik/48c2561f38f729a15b7b
+
+try:                        # Linux
+    import fcntl
+    from os import O_NONBLOCK
+
+    def set_nonblocking(pipe):
+        fd = pipe.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, fl | O_NONBLOCK)
+
+except ImportError:         # Windows
+    import msvcrt
+    from ctypes import windll, byref, WinError
+    try:
+        from ctypes.wintypes import HANDLE, DWORD, LPDWORD, BOOL
+    except ImportError:     # py27
+        from ctypes.wintypes import HANDLE, DWORD, LPVOID as LPDWORD, BOOL
+
+    PIPE_NOWAIT = DWORD(0x00000001)
+
+    # NOTE: SetNamedPipeHandleState works for anonymous pipes as well.
+    SetNamedPipeHandleState = windll.kernel32.SetNamedPipeHandleState
+    SetNamedPipeHandleState.argtypes = [HANDLE, LPDWORD, LPDWORD, LPDWORD]
+    SetNamedPipeHandleState.restype = BOOL
+
+    def set_nonblocking(pipe):
+        fd = pipe.fileno()
+        hd = msvcrt.get_osfhandle(fd)
+        if SetNamedPipeHandleState(hd, byref(PIPE_NOWAIT), None, None) == 0:
+            raise OSError(WinError())
+
+
+class AsyncReader:
+
+    """Read stream asynchronously in a worker thread. Note that the worker
+    thread will only be active while have entered the `with` context."""
+
+    def __init__(self, stream, callback):
+        set_nonblocking(stream)
+        self.pool = ThreadPool(1)
+        self.stream = stream
+        self.callback = callback
+
+    def __enter__(self):
+        self.stop = False
+        self.result = self.pool.apply_async(self._read_thread)
+
+    def __exit__(self, *exc_info):
+        self.stop = True
+        output_lines = self.result.get()
+        if output_lines:
+            self.callback(b''.join(output_lines))
+
+    def _read_thread(self):
+        lines = []
+        stop = False
+        while True:
+            try:
+                line = self.stream.readline()
+            except IOError:
+                if stop:
+                    return lines
+                # do one more iteration, this prevents missing output due to
+                # unfortunate thread scheduling:
+                if self.stop:
+                    stop = True
+                continue
+            if not line:
+                return lines
+            lines.append(line)
---
+++ packages/cpymad/cpymad-1.7.0-cp35-cp35m-manylinux1_x86_64.whl/cpymad/types.py
@@ -0,0 +1,95 @@
+"""
+Python type analogues for MAD-X data structures.
+"""
+
+from collections import namedtuple
+
+__all__ = [
+    'Range',
+    'Parameter',
+    'Constraint',
+]
+
+
+PARAM_TYPE_LOGICAL       = 0
+PARAM_TYPE_INTEGER       = 1
+PARAM_TYPE_DOUBLE        = 2
+PARAM_TYPE_STRING        = 3
+PARAM_TYPE_CONSTRAINT    = 4
+PARAM_TYPE_LOGICAL_ARRAY = 10
+PARAM_TYPE_INTEGER_ARRAY = 11
+PARAM_TYPE_DOUBLE_ARRAY  = 12
+PARAM_TYPE_STRING_ARRAY  = 13
+
+VAR_TYPE_CONST    = 0
+VAR_TYPE_DIRECT   = 1
+VAR_TYPE_DEFERRED = 2
+VAR_TYPE_STRING   = 3
+
+dtype_to_native = {
+    PARAM_TYPE_LOGICAL: bool,
+    PARAM_TYPE_INTEGER: int,
+    PARAM_TYPE_DOUBLE: float,
+    PARAM_TYPE_STRING: str,
+    PARAM_TYPE_LOGICAL_ARRAY: list,
+    PARAM_TYPE_INTEGER_ARRAY: list,
+    PARAM_TYPE_DOUBLE_ARRAY: list,
+    PARAM_TYPE_STRING_ARRAY: list,
+}
+
+
+Range = namedtuple('Range', ['first', 'last'])
+
+AlignError = namedtuple('AlignError', [
+    'dx', 'dy', 'ds',
+    'dphi', 'dtheta', 'dpsi',
+    'mrex', 'mrey', 'mredx', 'mredy',
+    'arex', 'arey', 'mscalx', 'mscaly',
+])
+FieldError = namedtuple('FieldError', ['dkn', 'dks'])
+PhaseError = namedtuple('PhaseError', ['dpn', 'dps'])
+
+
+class Parameter(object):
+
+    __slots__ = ('name', 'value', 'expr', 'dtype', 'inform', 'var_type')
+
+    def __init__(self, name, value, expr, dtype, inform, var_type=None):
+        self.name = name
+        self.value = value
+        self.expr = expr
+        self.dtype = dtype
+        self.inform = inform
+        if var_type is None:
+            if isinstance(value, str):
+                var_type = VAR_TYPE_STRING
+            else:
+                has_expr = expr and (not isinstance(value, list) or any(expr))
+                var_type = VAR_TYPE_DEFERRED if has_expr else VAR_TYPE_DIRECT
+        self.var_type = var_type
+
+    def __call__(self):
+        return self.definition
+
+    @property
+    def definition(self):
+        """Return command argument as should be used for MAD-X input to
+        create an identical element."""
+        if isinstance(self.value, list):
+            return [e or v for v, e in zip(self.value, self.expr)]
+        else:
+            return self.expr or self.value
+
+    def __str__(self):
+        return str(self.definition)
+
+
+class Constraint(object):
+
+    """Represents a MAD-X constraint, which has either min/max/both/value."""
+
+    def __init__(self, val=None, min=None, max=None):
+        """Just store the values"""
+        self.val = val
+        self.min = min
+        self.max = max
