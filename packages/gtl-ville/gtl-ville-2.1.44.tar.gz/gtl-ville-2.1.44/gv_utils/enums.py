4b956b4ddaec366ba5b92c83ac7e0571904465a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:08 +0000

    Add package gtl-ville/2.1.44/gtl-ville-2.1.44.tar.gz

---
+++ packages/gtl-ville/gtl-ville-2.1.44.tar.gz/gtl-ville-2.1.44/gv_utils/enums.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+
+class AttId:
+    att = 'att'
+    centerxy = 'centerxy'
+    datapointeid = 'datapointeid'
+    datapointseids = 'datapointseids'
+    datatypeeid = 'datatypeeid'
+    eid = 'eid'
+    ffspeed = 'ffspeed'
+    fow = 'fow'
+    frc = 'frc'
+    fromno = 'fromno'
+    geom = 'geom'
+    geomxy = 'geomxy'
+    id = 'id'
+    length = 'length'
+    mainroad = 'mainroad'
+    maxspeed = 'maxspeed'
+    name = 'name'
+    nlanes = 'nlanes'
+    no = 'no'
+    roadeid = 'roadeid'
+    roads = 'roads'
+    tono = 'tono'
+    sample = 'sample'
+    validfrom = 'validfrom'
+    validto = 'validto'
+    webatt = 'webatt'
+    zoneeid = 'zoneeid'
+
+
+class CsvData:
+    samples = 'samples'
+    timestamp = 'timestamp'
+
+    confidence = 'confidence'
+    density = 'density'
+    flow = 'flow'
+    fluidity = 'fluidity'
+    occupancy = 'occupancy'
+    speed = 'speed'
+    status = 'status'
+    traveltime = 'traveltime'
+    vehicle = 'vehicle'
+
+
+class DataTypeId:
+    gka = 'gka'
+    karrusrd = 'karrusrd'
+    metropme = 'metropme'
+    zonepoints = 'zonepoints'
+    roads = 'roads'
+    tomtomfcd = 'tomtomfcd'
+    zones = 'zones'
+
+    dataquality = 'dataquality'
+    datapoints = 'datapoints'
+    mappingroadsdatapoints = 'mappingroadsdatapoints'
+    zonestraveltime = 'zonestraveltime'
+
+    imputedprefixe = 'imputed'
+
+
+class NetworkObjId:
+    datapointsroadsmap = 'datapointsroadsmap'
+    frcroadsmap = 'frcroadsmap'
+    lonlatnodesmatrix = 'lonlatnodesmatrix'
+    mainclustersgeom = 'mainclustersgeom'
+    newdpmappings = 'newdpmappings'
+    newzonemappings = 'newzonemappings'
+    omiteddatapoints = 'omiteddatapoints'
+    roadclustermap = 'roadclustermap'
+    roadsdatamap = 'roadsdatamap'
+    roadsffspeedmap = 'roadsffspeedmap'
+    roadszonesmap = 'roadszonesmap'
+    voronoiroadmap = 'voronoiroadmap'
+    zonesdatamap = 'zonesdatamap'
+
+
+INDICATORS_DATA_TYPES = (DataTypeId.karrusrd, DataTypeId.metropme, DataTypeId.tomtomfcd, DataTypeId.roads,
+                         DataTypeId.zones)
+
+IMPUTED_INDICATORS_DATA_TYPES = tuple(('-'.join((DataTypeId.imputedprefixe, dt)) for dt in INDICATORS_DATA_TYPES))
+
+MISSING_VALUE = -1
+NO_VALUE = ''
