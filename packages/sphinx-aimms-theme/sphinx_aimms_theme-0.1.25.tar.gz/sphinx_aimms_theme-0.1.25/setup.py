f1bcbd2a6e893a511f88d592e09944695f5c64e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:15 +0000

    Add package sphinx-aimms-theme/0.1.25/sphinx_aimms_theme-0.1.25.tar.gz

---
+++ packages/sphinx-aimms-theme/sphinx_aimms_theme-0.1.25.tar.gz/sphinx_aimms_theme-0.1.25/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+import setuptools
+import sys
+
+
+setup(
+    
+    name = "sphinx_aimms_theme",
+    version = '0.1.25',
+    license = "MIT",
+    packages= ['sphinx_aimms_theme'],
+    url = "https://gitlab.com/ArthurdHerbemont/sphinx-aimms-theme",
+    description = 'AIMMS theme for Sphinx',
+    long_description='Please refer to https://gitlab.com/ArthurdHerbemont/sphinx-aimms-theme',
+    author = "AIMMS User Support",
+    author_email = "support@aimms.com",
+
+    
+    entry_points = {
+        'sphinx.html_themes': [
+            'sphinx_aimms_theme = sphinx_aimms_theme',
+        ]        
+    },
+    install_requires=[
+       'sphinx',
+       'sphinx_rtd_theme',
+    ],
+    package_data={'sphinx_aimms_theme': [
+        'theme.conf',
+        '*.html',
+        'static/aimms_css/*.*',
+        'static/*.*',
+        'static/icons/*.*'
+    ]},
+    include_package_data=True,
+
+)
