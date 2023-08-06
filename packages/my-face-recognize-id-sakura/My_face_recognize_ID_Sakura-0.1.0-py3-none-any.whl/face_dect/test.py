4c38bc16373a4164f9a5b7e2c6a2c3d32b87d775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:40 +0000

    Add package my-face-recognize-id-sakura/0.1.0/My_face_recognize_ID_Sakura-0.1.0-py3-none-any.whl

---
+++ packages/my-face-recognize-id-sakura/My_face_recognize_ID_Sakura-0.1.0-py3-none-any.whl/face_dect/test.py
@@ -0,0 +1,6 @@
+from function import compare
+
+save_path = './'
+img1_path = './photo/Michael_Douglas_0001.jpg'
+img2_path = './photo/George_W_Bush_0003.jpg'
+compare(img1_path, img2_path, save_path,mask=False)
