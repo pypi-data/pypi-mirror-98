8f2d6e0ee92279922954812a1b02d44e5ca51874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:56 +0000

    Add package parasut-cli/0.2.0/parasut_cli-0.2.0-py2.py3-none-any.whl

---
+++ packages/parasut-cli/parasut_cli-0.2.0-py2.py3-none-any.whl/parasut_cli/commands/switch.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import Optional
+
+from parasut_cli.utils.receiver import Receiver
+from parasut_cli.utils.command import Command
+
+
+class SwitchCommand(Command):
+    def __init__(
+        self,
+        receiver: Receiver,
+        target_repo: Optional[str] = None,
+        target_addling: Optional[str] = None,
+    ) -> None:
+        self._receiver: Receiver = receiver
+        self._target_repo: Optional[str] = target_repo
+        self._target_addling: Optional[str] = target_addling
+
+    def execute(self) -> None:
+        if self._target_repo:
+            self._receiver.switch_server_rails_frontend(self._target_repo)
+        if self._target_addling:
+            self._receiver.switch_server_rails_addling(self._target_addling)
