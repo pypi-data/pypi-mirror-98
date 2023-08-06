ad04bcb1ac9db1f7cb5a232b0aa8dc1484ad6442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:55 +0000

    Add package ewah/0.4.22/ewah-0.4.22.tar.gz

---
+++ packages/ewah/ewah-0.4.22.tar.gz/ewah-0.4.22/ewah/ewah_utils/airflow_provider_info.py
@@ -0,0 +1,11 @@
+from ewah.hooks import hook_class_names
+
+
+def get_provider_info():
+    return {
+        "package-name": "ewah",
+        "name": "ewah",
+        "description": "",
+        "hook-class-names": hook_class_names,
+        "versions": ["1.0.0"],
+    }
