f92ba537ebbe9860eb4ba45883d0001e9e703940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package pandas-stubs/1.0.4.7/pandas-stubs-1.0.4.7.tar.gz

---
+++ packages/pandas-stubs/pandas-stubs-1.0.4.7.tar.gz/pandas-stubs-1.0.4.7/setup.py
@@ -0,0 +1,44 @@
+"""
+Setup for Pandas type annotations.
+For the sake of convenience the package installation
+will coexist with Pandas installation.
+If this is a problem - download the source
+and add it to PYTHONPATH manually.
+"""
+
+from setuptools import setup
+import os
+
+# find_packages might not work with stub files
+src_path = os.path.join("third_party", "3")
+
+
+def list_packages(source_path: str = src_path) -> None:
+    for root, _, _ in os.walk(os.path.join(source_path, "pandas")):
+        yield ".".join(os.path.relpath(root, source_path).split(os.path.sep))
+
+
+setup(
+    name="pandas-stubs",
+    package_dir={"": src_path},
+    version="1.0.4.7",
+    description="Type annotations for Pandas",
+    long_description=(open("README.md").read()
+                      if os.path.exists("README.md") else ""),
+    long_description_content_type='text/markdown',
+    url="https://github.com/VirtusLab/pandas-stubs",
+    packages=list(list_packages()),
+    package_data={"": ["*.pyi", "py.typed"]},
+    license="MIT",
+    author="Zbigniew Królikowski",
+    author_email="zkrolikowski@virtuslab.com",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Typing :: Typed",
+    ],
+)
