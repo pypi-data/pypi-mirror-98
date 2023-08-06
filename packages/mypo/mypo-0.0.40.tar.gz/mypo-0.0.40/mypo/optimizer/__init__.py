b0b92dce0f0c95328b3a7c5dfbcc720b76981630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:29 +0000

    Add package mypo/0.0.40/mypo-0.0.40.tar.gz

---
+++ packages/mypo/mypo-0.0.40.tar.gz/mypo-0.0.40/mypo/optimizer/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+from mypo.optimizer.minimum_variance_optimizer import MinimumVarianceOptimizer
+from mypo.optimizer.optimizer import Optimizer
+from mypo.optimizer.sharp_ratio_optimizer import SharpRatioOptimizer
