1143b2e9bedacf656a7dbc36f54031115a0dd2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:51 +0000

    Add package elabjournal/0.0.19/elabjournal-0.0.19.tar.gz

---
+++ packages/elabjournal/elabjournal-0.0.19.tar.gz/elabjournal-0.0.19/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+exec(open("elabjournal/_version.py").read())
+
+setup(
+    name="elabjournal",
+    version=__version__,
+    author="Matthijs Brouwer",
+    url="https://github.com/matthijsbrouwer/elabjournal-python",
+    packages=find_packages(),
+    license="Apache License 2.0",
+    long_description=open("README.txt").read(),
+    install_requires=[
+        "requests >= 2.18.4",
+        "pandas",
+        "keyring",
+        "IPython",
+        "rjson",
+        "xlrd",
+        "matplotlib",
+        "Pillow",
+        "ipywidgets",
+        "graphviz",
+        "openpyxl"
+    ],
+    classifiers=[
+        "Framework :: Jupyter",
+        "Intended Audience :: Science/Research",
+    ],
+)
+
