75c410aaa75e518ef8b4d6073dd92f06e70f5cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package hello-009/1.0.0/hello_009-1.0.0.tar.gz

---
+++ packages/hello-009/hello_009-1.0.0.tar.gz/hello_009-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="hello_009",
+    author="xxx",
+    version="1.0.0",
+    author_email="xxx@gmail.com",
+    py_modules=['hellopy.hello'],
+    url="http://www.xxx.com"
+)
