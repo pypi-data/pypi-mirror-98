75030401afe6b8020dedf70f0ec77f20d159395d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:26 +0000

    Add package awsimple/0.14.4/awsimple-0.14.4-py3-none-any.whl

---
+++ packages/awsimple/awsimple-0.14.4-py3-none-any.whl/awsimple/__version__.py
@@ -0,0 +1,8 @@
+__application_name__ = "awsimple"
+__title__ = __application_name__
+__author__ = "abel"
+__version__ = "0.14.4"
+__author_email__ = "j@abel.co"
+__url__ = "https://github.com/jamesabel/awsimple"
+__download_url__ = "https://github.com/jamesabel/awsimple"
+__description__ = "Simple AWS API for S3, DynamoDB, SNS, and SQS"
