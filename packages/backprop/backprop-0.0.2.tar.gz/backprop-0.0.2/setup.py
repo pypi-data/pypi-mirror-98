c39419fb7229d660f1fea4888dd571175c453267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package backprop/0.0.2/backprop-0.0.2.tar.gz

---
+++ packages/backprop/backprop-0.0.2.tar.gz/backprop-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="backprop",
+    version="0.0.2",
+    author="Backprop",
+    author_email="hello@backprop.co",
+    description="Backprop",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/backprop-ai/backprop",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "transformers>=4.3.2",
+        "sentence_transformers>=0.4.1.2",
+        "torch<1.8.0",
+        "torchvision",
+        "ftfy",
+        "pytorch_lightning==1.1.0",
+        "dill"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
