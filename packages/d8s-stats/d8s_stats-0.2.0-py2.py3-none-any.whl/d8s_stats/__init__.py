4bc6e9d6d9f8614524dcf867eed2dcf412b3e617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package d8s-stats/0.2.0/d8s_stats-0.2.0-py2.py3-none-any.whl

---
+++ packages/d8s-stats/d8s_stats-0.2.0-py2.py3-none-any.whl/d8s_stats/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  #type: ignore
+
+try:
+    __version__ = version('d8s_stats')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "d8s_stats". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-stats/issues.'
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+
+from .stats import *
