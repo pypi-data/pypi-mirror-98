2d98cea1b9d649bfe53476dea1720feb2df2dbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:18 +0000

    Add package spmat/0.0.5/spmat-0.0.5-cp38-cp38-manylinux1_i686.whl

---
+++ packages/spmat/spmat-0.0.5-cp38-cp38-manylinux1_i686.whl/spmat/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "spmat"
+__summary__ = "spmat: A collection of tools for special matrices"
+__uri__ = "https://github.com/zhengp0/spmat"
+
+__version__ = "0.0.5"
+
+__author__ = "Peng Zheng"
+__email__ = "zhengp@uw.edu"
+
+__license__ = "BSD 2-Clause License"
+__copyright__ = f"Copyright 2021 {__author__}"
