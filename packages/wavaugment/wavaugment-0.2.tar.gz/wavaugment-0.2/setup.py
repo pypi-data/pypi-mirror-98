0847b5080453bed5bfc228fb9ca20152faa264a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:51 +0000

    Add package wavaugment/0.2/wavaugment-0.2.tar.gz

---
+++ packages/wavaugment/wavaugment-0.2.tar.gz/wavaugment-0.2/setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree
+
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+
+# Creating the version file
+cwd = os.path.dirname(os.path.abspath(__file__))
+version = '0.2'
+sha = 'Unknown'
+
+try:
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+except Exception:
+    pass
+
+setup(
+    name="wavaugment",
+    version=version,
+    # Exclude the build files.
+    packages=find_packages(exclude=["build"]),
+    install_requires=['torch', 'torchaudio']
+)
