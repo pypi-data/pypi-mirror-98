a0990d85844f944dd0dedbcd14b68cb7c23044cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:58 +0000

    Add package bitn/0.0.41/bitn-0.0.41.tar.gz

---
+++ packages/bitn/bitn-0.0.41.tar.gz/bitn-0.0.41/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bitn",
+    version="0.0.41",
+    author="fu-corp",
+    author_email="spam@futzu.com",
+    description="Bitwise Bit Slicing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/futzu/bitn",
+    py_modules=["bitn"],
+    platforms="all",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
