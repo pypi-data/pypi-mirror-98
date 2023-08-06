b8a0ebc7edc872eeb999223b4cce4bd9e23653c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:30 +0000

    Add package adamspy/0.29.6/adamspy-0.29.6-py3-none-any.whl

---
+++ packages/adamspy/adamspy-0.29.6-py3-none-any.whl/adamspy/__init__.py
@@ -0,0 +1,16 @@
+"""
+adamspy is a set of python tools for manipulating MSC Adams files and data
+
+Available subpackages
+---------------------
+adripy
+    A set of python tools for manipulating MSC Adams Drill files and data
+
+Available modules
+-----------------
+adamspy
+    Functions for manipulating adams files and data
+"""
+from . import adripy
+from . import waterfall
+name = 'adamspy'
