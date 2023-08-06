d8d6157fc92eec94ebd93cb474c7a1f551365fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:21 +0000

    Add package odoo8-addon-l10n-es-aeat-mod303/8.0.1.17.0/odoo8_addon_l10n_es_aeat_mod303-8.0.1.17.0-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-es-aeat-mod303/odoo8_addon_l10n_es_aeat_mod303-8.0.1.17.0-py2-none-any.whl/odoo_addons/l10n_es_aeat_mod303/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# © 2013 Alberto Martín Cortada (Guadaltech)
+# © 2014-2015 Pedro M. Baeza
+# © 2015 AvanzOSC - Ainara Galdona
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
+
+{
+    "name": "AEAT modelo 303",
+    "version": "8.0.1.17.0",
+    'category': "Accounting & Finance",
+    'author': "Guadaltech,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Antiun Ingeniería S.L.,"
+              "Comunitea,"
+              "Odoo Community Association (OCA),"
+              "Otherway Creatives S.L.",
+    'website': "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es",
+        "l10n_es_aeat",
+    ],
+    "data": [
+        "data/tax_code_map_mod303_data.xml",
+        "data/aeat_export_mod303_data.xml",
+        "data/aeat_export_mod303_2017_data.xml",
+        "data/aeat_export_mod303_2018_data.xml",
+        "data/aeat_export_mod303_2021_data.xml",
+        "data/l10n_es_aeat_mod303_report_activity_code_data.xml",
+        "views/mod303_view.xml",
+        "views/l10n_es_aeat_mod303_report_activity_code_data_views.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml"
+    ],
+    "installable": True,
+}
