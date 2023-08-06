942c1fefa4e63082a806c5283c99b256be4fbe72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:50 +0000

    Add package iatiflattener/0.1.5b6/iatiflattener-0.1.5b6.tar.gz

---
+++ packages/iatiflattener/iatiflattener-0.1.5b6.tar.gz/iatiflattener-0.1.5b6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+requirements = """
+lxml>=4.6.1
+requests>=2.24.0
+exchangerates>=0.3.4
+pandas>=1.1.4
+numpy>=1.19.4
+PyExcelerate>=0.9.0
+openpyxl>=3.0.5
+iatikit>=2.3.0
+"""
+
+setup(
+    name="iatiflattener",
+    packages=find_packages(exclude=['ez_setup', 'examples']),
+    version='0.1.5b6',
+    description="A set of tools to flatten IATI data.",
+    author="Mark Brough",
+    author_email="mark@brough.io",
+    url="https://github.com/iati-data-access/iati-flattener",
+    license="AGPLv3+",
+    install_requires=requirements.strip().splitlines(),
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.9'
+    ),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+    }
+)
