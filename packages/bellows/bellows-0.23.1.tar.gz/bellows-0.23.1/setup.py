9bef663ad91f96104f871879e698990d0a8bd9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:38 +0000

    Add package bellows/0.23.1/bellows-0.23.1.tar.gz

---
+++ packages/bellows/bellows-0.23.1.tar.gz/bellows-0.23.1/setup.py
@@ -0,0 +1,28 @@
+"""Setup module for bellows"""
+
+from setuptools import find_packages, setup
+
+import bellows
+
+setup(
+    name="bellows",
+    version=bellows.__version__,
+    description="Library implementing EZSP",
+    url="http://github.com/zigpy/bellows",
+    author="Russell Cloran",
+    author_email="rcloran@gmail.com",
+    license="GPL-3.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    entry_points={"console_scripts": ["bellows=bellows.cli.main:main"]},
+    install_requires=[
+        "click",
+        "click-log>=0.2.1",
+        "dataclasses;python_version<'3.7'",
+        "pure_pcapy3==1.0.1",
+        "pyserial-asyncio",
+        "voluptuous",
+        "zigpy>=0.21.0",
+    ],
+    dependency_links=["https://codeload.github.com/rcloran/pure-pcapy-3/zip/master"],
+    tests_require=["asynctest", "pytest", "pytest-asyncio"],
+)
