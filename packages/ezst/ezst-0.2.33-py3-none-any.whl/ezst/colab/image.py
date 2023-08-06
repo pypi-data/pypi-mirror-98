f20e1cfedda6a9f4b17d13e2dafa8bb7c48e53ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:28 +0000

    Add package ezst/0.2.33/ezst-0.2.33-py3-none-any.whl

---
+++ packages/ezst/ezst-0.2.33-py3-none-any.whl/ezst/colab/image.py
@@ -0,0 +1,11 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+def show(file_path, grid=False):
+  img = mpimg.imread(file_path)
+  plt.grid(True)
+  plt.imshow(img)
+  plt.show()
+
+
