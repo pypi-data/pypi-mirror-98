4618ee3fb4c25640a21990d4fd495acf44cd24e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package pycurl-requests/0.2.0/pycurl-requests-0.2.0.tar.gz

---
+++ packages/pycurl-requests/pycurl-requests-0.2.0.tar.gz/pycurl-requests-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='pycurl-requests',
+    version='0.2.0',
+    description='A Requests-compatible interface for pycURL',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/dcoles/pycurl-requests',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pycurl',
+        'chardet',
+    ],
+)
