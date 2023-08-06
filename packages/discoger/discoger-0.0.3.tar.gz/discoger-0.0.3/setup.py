62fcbf381dcd3d200217969c3366c6ec1153f590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package discoger/0.0.3/discoger-0.0.3.tar.gz

---
+++ packages/discoger/discoger-0.0.3.tar.gz/discoger-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+import discoger._info as package_info
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name=package_info.__title__,  # How you named your package folder (MyLib)
+    packages=setuptools.find_packages(),
+    version=package_info.__version__,  # Start with a small number and increase it with every
+    license=package_info.__license__,
+    description=package_info.__description__,  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=package_info.__author__,  # Type in your name
+    author_email=package_info.__author_email__,  # Type in your E-Mail
+    url=f'https://github.com/{package_info.__github_username__}/{package_info.__title__}',  #
+    download_url=f'https://github.com/{package_info.__github_username__}/{package_info.__title__}/archive/{package_info.__version__}.tar.gz',
+    keywords=package_info.__keywords__,
+    install_requires=requirements,
+    entry_points={"console_scripts": ["discoger = discoger.discoger:main"]},
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
