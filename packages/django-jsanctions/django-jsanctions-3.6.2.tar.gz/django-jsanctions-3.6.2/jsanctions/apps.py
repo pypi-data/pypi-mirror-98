943d349802a27570cbf8311dd992f0400813de4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:23 +0000

    Add package django-jsanctions/3.6.2/django-jsanctions-3.6.2.tar.gz

---
+++ packages/django-jsanctions/django-jsanctions-3.6.2.tar.gz/django-jsanctions-3.6.2/jsanctions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class JsanctionsConfig(AppConfig):
+    name = "jsanctions"
+    verbose_name = _("Sanction Lists")
