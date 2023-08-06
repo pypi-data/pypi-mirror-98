071c46c44793417be7420dc81a0b7ded5db72f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:04 +0000

    Add package distributions-tarun/0.1/distributions_tarun-0.1.tar.gz

---
+++ packages/distributions-tarun/distributions_tarun-0.1.tar.gz/distributions_tarun-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tarun',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tarun'],
+      zip_safe=False)
