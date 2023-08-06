2437823dfc63237152a8d3230ddff02d5f602b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:47 +0000

    Add package argparse-best-practice/0.1.3/argparse_best_practice-0.1.3-py3-none-any.whl

---
+++ packages/argparse-best-practice/argparse_best_practice-0.1.3-py3-none-any.whl/argparse_best_practice/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    bool as _bool, 
+    string as _string, 
+    number as _number, 
+    list as _list
+)
+
+
+def best_practice():
+    _bool.best_practice_bool()
+    _string.best_practice_string()
+    _number.best_practice_number()
+    _list.best_practice_list()
