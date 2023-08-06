25ea307a96211d564b3233f96f3196d958881ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:35 +0000

    Add package p6-namer/0.7.43/p6-namer-0.7.43.tar.gz

---
+++ packages/p6-namer/p6-namer-0.7.43.tar.gz/p6-namer-0.7.43/src/p6_namer/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "p6-namer", "0.7.43", __name__[0:-6], "p6-namer@0.7.43.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
