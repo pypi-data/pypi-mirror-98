5a042d35b0cd7ac80e78e4d4073547c5b7009b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package discord-ext-paginator/0.3.6/discord-ext-paginator-0.3.6.tar.gz

---
+++ packages/discord-ext-paginator/discord-ext-paginator-0.3.6.tar.gz/discord-ext-paginator-0.3.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = "0.3.6"
+
+with open("README.md", "r") as f:
+	long_desc = f.read()
+
+setup(
+name="discord-ext-paginator",
+author="Alex Hutz",
+author_email="frostiiflames@gmail.com",
+keywords=["discord.py", "paginator"],
+version=version,
+packages=['discord.ext.paginator'],
+license='MIT',
+long_description=long_desc,
+long_description_content_type="text/markdown",
+description="An package for discord pagination.",
+install_requires=['discord.py>=1.5.1'],
+python_requires='>=3.7.1'
+)
