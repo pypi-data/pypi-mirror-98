a8e7d2b1cb0817b1602b5414f635f2dc864ba1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package hdwallet/1.0.0/hdwallet-1.0.0-py3-none-any.whl

---
+++ packages/hdwallet/hdwallet-1.0.0-py3-none-any.whl/hdwallet/symbols.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Available Mainnet & Testnet symbols
+"""
+
+# Bitcoin
+BTC, BTCTEST = "BTC", "BTCTEST"
+# Bitcoin Cash
+BCH = "BCH"
+# Bitcoin Gold
+BTG = "BTG"
+# Ethereum
+ETH, ETHTEST = "ETH", "ETHTEST"
+# Dogecoin
+DOGE, DOGETEST = "DOGE", "DOGETEST"
+# XinFin
+XDC, XDCTEST = "XDC", "XDCTEST"
+# Litecoin
+LTC, LTCTEST = "LTC", "LTCTEST"
+# Navcoin
+NAV = "NAV"
+# Shadow Cash
+SDC, SDCTEST = "SDC", "SDCTEST"
+# Viacoin
+VIA, VIATEST = "VIA", "VIATEST"
+# Omni
+OMNI, OMNITEST = "OMNI", "OMNITEST"
+# Dash
+DASH, DASHTEST = "DASH", "DASHTEST"
+# Qtum
+QTUM, QTUMTEST = "QTUM", "QTUMTEST"
+
+
+__all__ = [
+    "BTC", "BTCTEST",
+    "BCH",
+    "BTG",
+    "ETH", "ETHTEST",
+    "DOGE", "DOGETEST",
+    "XDC", "XDCTEST",
+    "LTC", "LTCTEST",
+    "NAV",
+    "SDC", "SDCTEST",
+    "VIA", "VIATEST",
+    "OMNI", "OMNITEST",
+    "DASH", "DASHTEST",
+    "QTUM", "QTUMTEST"
+]
