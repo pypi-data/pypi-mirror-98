5a96b8bd62fc8641548c1a31d400e3b900b3a60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package tradingfeatures/0.5.2/tradingfeatures-0.5.2-py3-none-any.whl

---
+++ packages/tradingfeatures/tradingfeatures-0.5.2-py3-none-any.whl/tradingfeatures/apis/binance/main.py
@@ -0,0 +1,28 @@
+import time
+import requests
+import numpy as np
+import pandas as pd
+
+from tradingfeatures import apiBase
+from tradingfeatures.apis.binance.base import binanceBase
+from tradingfeatures.apis.binance.funding import binanceFunding
+
+
+class binance(binanceBase):
+
+    def __init__(self):
+        super(binance, self).__init__()
+
+        self.funding = binanceFunding()
+
+    def update(self, path, update_all=False, *args, **kwargs):        
+        if update_all:
+            return self.update_all(*args, **kwargs)
+        else:
+            return super(binance, self).update(path, *args, **kwargs)
+
+    def update_all(self):
+        """
+            Update everything that api offers.
+        """
+        raise NotImplementedError
