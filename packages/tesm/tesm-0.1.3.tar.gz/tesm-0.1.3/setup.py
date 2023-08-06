5823101f0cfc3017da15c974bbdcd1787c8f98e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package tesm/0.1.3/tesm-0.1.3.tar.gz

---
+++ packages/tesm/tesm-0.1.3.tar.gz/tesm-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tesm']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tesm',
+    'version': '0.1.3',
+    'description': 'Pseudo-assembly programming-like language',
+    'long_description': None,
+    'author': 'Ruthenic',
+    'author_email': 'mdrakea3@tutanota.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/tesm/tesm-0.1.3.tar.gz/tesm-0.1.3/tesm/__init__.py
@@ -0,0 +1,1 @@
+from tesm import *
