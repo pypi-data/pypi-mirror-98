a6345fb60d1d78b9110e93963b5a3b2c8a39f434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package midiroute/0.2.0/midiroute-0.2.0.tar.gz

---
+++ packages/midiroute/midiroute-0.2.0.tar.gz/midiroute-0.2.0/setup.py
@@ -0,0 +1,42 @@
+"""A command line utility for routing and monitoring MIDI ports."""
+
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).absolute().parent
+
+# Get the long description from the README file
+with open(str(pathlib.Path(here, "README.md")), encoding="utf-8") as f:
+    long_description = f.read()
+
+PROJECT_NAME = "midiroute"
+DESCRIPTION = __doc__
+INSTALL_REQUIRES = ["click", "mido", "python-rtmidi", "tabulate"]
+
+setup(
+    name=PROJECT_NAME,
+    version="1.0.0",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atticave/midiroute",
+    author="atticave",
+    author_email="atticave@blueheaps.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="midi interface router monitor diagnostics",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6, <4",
+    install_requires=INSTALL_REQUIRES,
+    entry_points={"console_scripts": ["midiroute=midiroute.main:main"]},
+)
