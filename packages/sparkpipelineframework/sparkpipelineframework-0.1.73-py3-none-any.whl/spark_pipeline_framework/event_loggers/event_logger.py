e18db11e9baf399bf9f2430f08712039bf697625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package sparkpipelineframework/0.1.73/sparkpipelineframework-0.1.73-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-0.1.73-py3-none-any.whl/spark_pipeline_framework/event_loggers/event_logger.py
@@ -0,0 +1,11 @@
+class EventLogger:
+    def log_progress_event(
+        self, event_name: str, current: int, total: int, event_format_string: str
+    ) -> None:
+        pass
+
+    def log_event(self, event_name: str, event_text: str) -> None:
+        pass
+
+    def log_exception(self, event_name: str, event_text: str, ex: Exception) -> None:
+        pass
