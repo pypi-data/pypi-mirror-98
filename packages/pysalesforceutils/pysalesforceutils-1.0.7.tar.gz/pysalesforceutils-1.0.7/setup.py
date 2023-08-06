888e81da6312dc63c2b1954c789c7d04c3f01ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:03 +0000

    Add package pysalesforceutils/1.0.7/pysalesforceutils-1.0.7.tar.gz

---
+++ packages/pysalesforceutils/pysalesforceutils-1.0.7.tar.gz/pysalesforceutils-1.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pysalesforceutils',
+    version='1.0.7',
+    author='Glen Barger',
+    author_email='gbarger@gmail.com',
+    description='Python module to wrap the Salesforce APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/gbarger/PySalesforce',
+    py_modules=[
+        'pysalesforceutils',
+        'webservice'
+        ],
+    install_requires=[
+        'requests',
+        'urllib3',
+        'zeep'
+    ])
