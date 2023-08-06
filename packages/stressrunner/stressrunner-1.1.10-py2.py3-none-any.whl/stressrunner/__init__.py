edbca0c73075927abf7a2fa91950bea40e6c7fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:03 +0000

    Add package stressrunner/1.1.10/stressrunner-1.1.10-py2.py3-none-any.whl

---
+++ packages/stressrunner/stressrunner-1.1.10-py2.py3-none-any.whl/stressrunner/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@file  : __init__.py.py
+@Time  : 2020/11/17 10:15
+@Author: Tao.Xu
+@Email : tao.xu2008@outlook.com
+"""
+
+from .runner import *
+from .mail import *
+
+__all__ = ['StressRunner', 'send_mail']
+
+"""
+StressRunner - A stressrunner similar as TextTestRunner for stress test, support for html report.
+FYI: http://tungwaiyip.info/software/HTMLTestRunner.html
+===============
+Based on unittest.
+Support iterative exection, html report, send html report, etc.
+"""
+
+if __name__ == '__main__':
+    pass
