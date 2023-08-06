5dc708d141047e81fc67e6f0c4a40f0281eee096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:18 +0000

    Add package pytprot/0.51/pytprot-0.51.tar.gz

---
+++ packages/pytprot/pytprot-0.51.tar.gz/pytprot-0.51/setup.py
@@ -0,0 +1,25 @@
+
+# pytprot SETUP FILE
+##############################
+
+
+from distutils.core import setup, Extension
+import setuptools
+
+setup(name = "pytprot",
+      version='0.51',
+      description="Macrocomplex builder from PPIs",
+      long_description_content_type='text/markdown',
+      long_description=open('README.md').read(),
+      author="Maria Sopena, Joana Llauradó, Othmane Hayoun",
+      license='LICENSE.txt',
+      url='https://github.com/mariasr3/pytprot',
+      packages=setuptools.find_packages(),
+      scripts=['./pytprot/main.py', './pytprot/__init__.py', './pytprot/parser.py', './pytprot/functions.py',
+               './pytprot/test_pyprot_dash.py'],
+      install_requires=["BioPython", "numpy", "seaborn", "matplotlib", "setuptools", "dash"])
+
+
+
+
+
