dc860d3aaa6e312c218b7627588f4be435210727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:26 +0000

    Add package uarizona-ece275-outputfileTester/0.0.17/uarizona-ece275-outputfileTester-0.0.17.tar.gz

---
+++ packages/uarizona-ece275-outputfileTester/uarizona-ece275-outputfileTester-0.0.17.tar.gz/uarizona-ece275-outputfileTester-0.0.17/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uarizona-ece275-outputfileTester", # Replace with your own username
+    version="0.0.17",
+    author="Connor Fuhrman",
+    author_email="connorfuhrman@email.arizona.edu",
+    description="Packaage used to test and compare output files for ECE 275 @ the University of Arizona",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/connorfuhrman/OutputFileTester",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
