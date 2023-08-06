e6bd00d7b936683fd1caec946d558e34bc2bd727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package labml-nn/0.4.90/labml-nn-0.4.90.tar.gz

---
+++ packages/labml-nn/labml-nn-0.4.90.tar.gz/labml-nn-0.4.90/labml_nn/transformers/__init__.py
@@ -0,0 +1,60 @@
+"""
+---
+title: Transformers
+summary: >
+  This is a collection of PyTorch implementations/tutorials of
+  transformers and related techniques.
+---
+
+# Transformers
+
+This module contains [PyTorch](https://pytorch.org/)
+implementations and explanations of original transformer
+from paper [Attention Is All You Need](https://arxiv.org/abs/1706.03762),
+and derivatives and enhancements of it.
+
+* [Multi-head attention](mha.html)
+* [Transformer Encoder and Decoder Models](models.html)
+* [Fixed positional encoding](positional_encoding.html)
+
+## [Transformer XL](xl/index.html)
+This implements Transformer XL model using
+[relative multi-head attention](xl/relative_mha.html)
+
+## [Compressive Transformer](compressive/index.html)
+
+This is an implementation of compressive transformer
+that extends upon [Transformer XL](xl/index.html) by compressing
+oldest memories to give a longer attention span.
+
+## [GPT Architecture](gpt)
+
+This is an implementation of GPT-2 architecture.
+
+## [GLU Variants](glu_variants/simple.html)
+
+This is an implementation of the paper
+[GLU Variants Improve Transformer](https://arxiv.org/abs/2002.05202).
+
+## [kNN-LM](knn)
+
+This is an implementation of the paper
+[Generalization through Memorization: Nearest Neighbor Language Models](https://arxiv.org/abs/1911.00172).
+
+## [Feedback Transformer](feedback)
+
+This is an implementation of the paper
+[Accessing Higher-level Representations in Sequential Transformers with Feedback Memory](https://arxiv.org/abs/2002.09402).
+
+## [Switch Transformer](switch)
+
+This is a miniature implementation of the paper
+[Switch Transformers: Scaling to Trillion Parameter Models with Simple and Efficient Sparsity](https://arxiv.org/abs/2101.03961).
+Our implementation only has a few million parameters and doesn't do model parallel distributed training.
+It does single GPU training but we implement the concept of switching as described in the paper.
+"""
+
+from .configs import TransformerConfigs
+from .models import TransformerLayer, Encoder, Decoder, Generator, EncoderDecoder
+from .mha import MultiHeadAttention
+from labml_nn.transformers.xl.relative_mha import RelativeMultiHeadAttention
---
+++ packages/labml-nn/labml-nn-0.4.90.tar.gz/labml-nn-0.4.90/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("readme.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='labml-nn',
+    version='0.4.90',
+    author="Varuna Jayasiri, Nipun Wijerathne",
+    author_email="vpjayasiri@gmail.com, hnipun@gmail.com",
+    description="A collection of PyTorch implementations of neural network architectures and layers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lab-ml/nn",
+    project_urls={
+        'Documentation': 'https://lab-ml.com/'
+    },
+    packages=setuptools.find_packages(exclude=('labml', 'labml.*',
+                                               'labml_samples', 'labml_samples.*',
+                                               'labml_helpers', 'labml_helpers.*',
+                                               'test',
+                                               'test.*')),
+    install_requires=['labml>=0.4.103',
+                      'labml-helpers>=0.4.76',
+                      'torch',
+                      'einops',
+                      'numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='machine learning',
+)
