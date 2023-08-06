ed4a6c567f4a7ac8f1cd4ae1e973f0ddf3232ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:11 +0000

    Add package seplot/2.2.4/seplot-2.2.4-py3-none-any.whl

---
+++ packages/seplot/seplot-2.2.4-py3-none-any.whl/seplot-2.2.4.data/scripts/seploter.py
@@ -0,0 +1,19 @@
+#!python
+# -*- coding: utf-8 -*-
+#
+# Copyright Serge Dmitrieff
+# www.biophysics.fr
+#
+# Based on Python Pyx
+""" Just a script to bridge the module and the executable """
+
+import sys
+from seplot.seplot import Splotter
+
+if __name__ == "__main__":
+
+    nargs=len(sys.argv)
+    dargs=sys.argv[1:]
+
+    seplot=Splotter(arguments=dargs)
+    seplot.make_and_save()
