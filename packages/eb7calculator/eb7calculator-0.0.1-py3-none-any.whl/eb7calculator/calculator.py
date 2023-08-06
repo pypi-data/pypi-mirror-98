58cf2725aa03b31ab58cc6b727187c1e76edcbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:38 +0000

    Add package eb7calculator/0.0.1/eb7calculator-0.0.1-py3-none-any.whl

---
+++ packages/eb7calculator/eb7calculator-0.0.1-py3-none-any.whl/eb7calculator/calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    """ Adds two number """
+    return a+b
+
+def sub(a,b):
+    """ subtracts a from b """
+    return a-b
+
+def mul(a,b):
+    """ gives the product of a , b """
+    return a*b
+
+def div(a,b):
+    """ divide a by b """
+    return a/b 
