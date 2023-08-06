62266d342dae8e0474574174d107d965fc71f63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:40 +0000

    Add package moonlistclient/0.6.1/moonlistclient-0.6.1.tar.gz

---
+++ packages/moonlistclient/moonlistclient-0.6.1.tar.gz/moonlistclient-0.6.1/moonlistclient/__init__.py
@@ -0,0 +1,7 @@
+from .client import MoonlistClient
+from .exceptions import *
+from .http import ApiClient
+from .webhook_manager import *
+from moonlistclient import models
+from moonlistclient.models.api import *
+from moonlistclient.models.webhooks import *
