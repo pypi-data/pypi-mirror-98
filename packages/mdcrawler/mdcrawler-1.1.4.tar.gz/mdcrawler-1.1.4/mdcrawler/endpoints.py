32656088ef929d53982762b4bcce06c166034b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package mdcrawler/1.1.4/mdcrawler-1.1.4.tar.gz

---
+++ packages/mdcrawler/mdcrawler-1.1.4.tar.gz/mdcrawler-1.1.4/mdcrawler/endpoints.py
@@ -0,0 +1,8 @@
+PROFILE_URL = 'https://cloudac.mildom.com/nonolive/gappserv/user/profileV2'
+PLAYBACK_LIST_URL = 'https://cloudac.mildom.com/nonolive/videocontent/profile/playbackList'
+
+CHAT_URL = 'https://cloudac.mildom.com/nonolive/videocontent/chat/replay'
+PLAYBACK_URL = 'https://cloudac.mildom.com/nonolive/videocontent/playback/getPlaybackDetail'
+LIVE_URL = 'wss://jp-room1.mildom.com'
+LIVE_META_URL = 'https://cloudac.mildom.com/nonolive/gappserv/live/enterstudio'
+
