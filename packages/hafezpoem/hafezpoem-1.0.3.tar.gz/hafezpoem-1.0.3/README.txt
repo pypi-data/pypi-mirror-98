a13c78508d3252886726fdd60b679d3c18034312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:31 +0000

    Add package hafezpoem/1.0.3/hafezpoem-1.0.3.tar.gz

---
+++ packages/hafezpoem/hafezpoem-1.0.3.tar.gz/hafezpoem-1.0.3/README.txt
@@ -0,0 +1,6 @@
+give you random poem of hafez for factor and other staff
+
+USAGE:
+from hafezpoem import get_random_fal
+fal = get_random_fal()
+print(fal)
---
+++ packages/hafezpoem/hafezpoem-1.0.3.tar.gz/hafezpoem-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='hafezpoem',
+    version='1.0.3',
+    description="Random poem of hafez for restaurant factor and other staff",
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Radesh Farokhmanesh',
+    author_email='radesh.farokhmanesh@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='hafez fal poem factor restaurant',
+    packages=find_packages(),
+    install_requires=['']
+)
