57fa49b8c2e93c58cce656588f32bddfe4c697af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:25 +0000

    Add package probability-distributions-v1-0-mlnd/1.0/probability_distributions_v1_0_mlnd-1.0.tar.gz

---
+++ packages/probability-distributions-v1-0-mlnd/probability_distributions_v1_0_mlnd-1.0.tar.gz/probability_distributions_v1_0_mlnd-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_v1_0_mlnd',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_v1_0_mlnd'],
+      author='Megha Rana',
+      author_email='megha14492@gmail.com',
+      zip_safe=False)
