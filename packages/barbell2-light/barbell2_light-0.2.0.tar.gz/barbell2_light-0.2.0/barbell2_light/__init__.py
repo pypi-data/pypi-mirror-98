7dcafbbcc4b430aa45c71de0f1d44597c79e291e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package barbell2-light/0.2.0/barbell2_light-0.2.0.tar.gz

---
+++ packages/barbell2-light/barbell2_light-0.2.0.tar.gz/barbell2_light-0.2.0/barbell2_light/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for barbell2_light."""
+
+__author__ = """Ralph Brecheisen"""
+__email__ = 'ralph.brecheisen@gmail.com'
+__version__ = '0.2.0'
+
+from .castorclient import CastorClient
