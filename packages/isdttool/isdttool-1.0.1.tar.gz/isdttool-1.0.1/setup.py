288ae3b5e8b66350371d230b9191c8628fa20553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:46 +0000

    Add package isdttool/1.0.1/isdttool-1.0.1.tar.gz

---
+++ packages/isdttool/isdttool-1.0.1.tar.gz/isdttool-1.0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""Standard setup.py. Speaks for itself."""
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='isdttool',
+    version='1.0.1',
+    packages=find_packages(),
+    url='https://github.com/maxried/isdt',
+    license='GPLv3',
+    install_requires='hidapi~=0.10.1',
+    author='Max Ried',
+    author_email='maxried@posteo.de',
+    description='Tool to retrieve information from ISDT chargers with USB connection.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={'console_scripts': ['isdttool=isdttool.cli_tool:main']},
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+
+)
