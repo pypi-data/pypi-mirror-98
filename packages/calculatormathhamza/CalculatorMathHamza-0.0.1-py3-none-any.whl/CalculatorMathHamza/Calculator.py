52d2358ffb1a3e392ce43edcb8a7eb14e084ac94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:22 +0000

    Add package calculatormathhamza/0.0.1/CalculatorMathHamza-0.0.1-py3-none-any.whl

---
+++ packages/calculatormathhamza/CalculatorMathHamza-0.0.1-py3-none-any.whl/CalculatorMathHamza/Calculator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 10:27:51 2021
+
+@author: hamza_shoukat
+"""
+
+class Calculator:
+    def add(self, number1, number2):
+        return number1 + number2
+    def subtract(self, number1, number2):
+        return number1 - number2
