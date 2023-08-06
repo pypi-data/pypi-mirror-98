bbbca8128e3cd1d5eeb0c9c32a7a17c7a43bb227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:42 +0000

    Add package invenio-records/1.5.0a3/invenio_records-1.5.0a3-py2.py3-none-any.whl

---
+++ packages/invenio-records/invenio_records-1.5.0a3-py2.py3-none-any.whl/invenio_records/errors.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Errors for Invenio-Records module."""
+
+
+class RecordsError(Exception):
+    """Base class for errors in Invenio-Records module."""
+
+
+class MissingModelError(RecordsError):
+    """Error raised when a record has no model."""
