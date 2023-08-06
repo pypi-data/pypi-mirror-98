02c2a328b8135e12cffc7450b68bc1e7acda6b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:28 +0000

    Add package edgeiq/1.2.0/edgeiq-1.2.0-py3-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-1.2.0-py3-none-win_amd64.whl/edgeiq/__init__.pyi
@@ -0,0 +1,14 @@
+from edgeiq.constants import *
+from edgeiq.object_detection import *
+from edgeiq.image_classification import *
+from edgeiq.edge_tools import *
+from edgeiq.object_tracking import *
+from edgeiq.bounding_box import *
+from edgeiq.pose_estimation import *
+from edgeiq.camera_blockage import *
+from edgeiq.semantic_segmentation import *
+from edgeiq.realsense import *
+from edgeiq.eyecloud import *
+from edgeiq.oak import *
+from edgeiq.mjpg_video_writer import MjpgVideoWriter as MjpgVideoWriter
+from edgeiq.streamer import Streamer as Streamer
