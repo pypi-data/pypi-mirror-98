9eb79c65f5b10ed0d7269858361629bb5ea6db62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:31 +0000

    Add package burp/1.0/burp-1.0.tar.gz

---
+++ packages/burp/burp-1.0.tar.gz/burp-1.0/burp/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .burpy import *
+
+import base64
+
+__version__ = "1.0"
+__author__ = "Erlend Leiknes"
+__author_email__ = base64.b64decode("ZGV2QGxlaWtuLmVz")
+__license__ = "MIT"
+__url__ = "https://github.com/elnerd/burpy"
+
+
