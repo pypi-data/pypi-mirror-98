900c2123a59515b6506f3f12c8f846f21770437f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:39 +0000

    Add package scrapfly-sdk/0.7.5/scrapfly_sdk-0.7.5-py3-none-any.whl

---
+++ packages/scrapfly-sdk/scrapfly_sdk-0.7.5-py3-none-any.whl/scrapfly/api_response.py
@@ -0,0 +1,374 @@
+import re
+import logging as logger
+import shutil
+
+from base64 import b64decode
+from contextlib import suppress
+from datetime import datetime
+from functools import partial, cached_property
+from http.cookiejar import Cookie
+from http.cookies import SimpleCookie
+from io import BytesIO
+from json import JSONDecoder, loads
+
+from dateutil.parser import parse
+from requests import Request, Response, HTTPError
+from typing import Dict, Optional, Iterable, Union, TextIO
+
+from requests.structures import CaseInsensitiveDict
+
+from .scrape_config import ScrapeConfig
+from .errors import ErrorFactory, EncoderError, ApiHttpClientError, ApiHttpServerError, \
+    HttpError
+from .frozen_dict import FrozenDict
+
+logger.getLogger('scrapfly')
+
+_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def _date_parser(value):
+    if isinstance(value, Dict):
+        over = value.items()
+    else:
+        over = enumerate(value)
+
+    for k, v in over:
+        if isinstance(v, str):
+            if len(v) <= 26:
+                try:
+                    value[k] = datetime.strptime(v, _DATE_FORMAT)
+                except ValueError:
+                    value[k] = v
+            else:
+                value[k] = v
+        elif isinstance(v, Iterable):
+            value[k] = _date_parser(v)
+        else:
+            value[k] = v
+
+    return value
+
+
+class ResponseBodyHandler:
+
+    SUPPORTED_COMPRESSION = ['gzip', 'deflate']
+    SUPPORTED_CONTENT_TYPES = ['application/msgpack', 'application/json']
+
+    class JSONDateTimeDecoder(JSONDecoder):
+        def __init__(self, *args, **kargs):
+            JSONDecoder.__init__(self, *args, object_hook=_date_parser, **kargs)
+
+    def __init__(self):
+        try:
+            import brotli
+            self.SUPPORTED_COMPRESSION.insert(0, 'br')
+        except ImportError:
+            pass
+
+        self.content_encoding = ', '.join(self.SUPPORTED_COMPRESSION)
+
+        try:  # automatically use msgpack if available https://msgpack.org/
+            import msgpack
+            self.accept = 'application/msgpack'
+            self.content_type = 'application/msgpack'
+            self.content_loader = partial(msgpack.loads, object_hook=_date_parser, strict_map_key=False)
+        except ImportError:
+            self.accept = 'application/json;charset=utf-8'
+            self.content_type = 'application/json;charset=utf-8'
+            self.content_loader = partial(loads, cls=self.JSONDateTimeDecoder)
+
+    def support(self, headers:Dict) -> bool:
+        if 'content-type' not in headers:
+            return False
+
+        for content_type in self.SUPPORTED_CONTENT_TYPES:
+            if headers['content-type'].find(content_type) != -1:
+                return True
+
+        return False
+
+    def __call__(self, content: bytes) -> Union[str, Dict]:
+        try:
+            return self.content_loader(content)
+        except Exception as e:
+            raise EncoderError(content=content.decode('utf-8')) from e
+
+
+class ScrapeApiResponse:
+
+    def __init__(self, request: Request, response: Response, scrape_config: ScrapeConfig, api_result: Optional[Dict] = None):
+        self.request = request
+        self.response = response
+        self.scrape_config = scrape_config
+
+        if self.scrape_config.method == 'HEAD':
+            api_result = {
+                'result': {
+                    'request_headers': {},
+                    'success': 200 >= self.response.status_code < 300,
+                    'response_headers': self.response.headers,
+                    'status_code': self.response.status_code,
+                    'reason': self.response.reason,
+                    'format': 'text',
+                    'content': ''
+                },
+                'context': {},
+                'config': self.scrape_config.__dict__
+            }
+
+            if 'X-Scrapfly-Reject-Code' in response.headers:
+                api_result['result']['error'] = {
+                    'code': response.headers['X-Scrapfly-Reject-Code'],
+                    'http_code': response.headers['X-Scrapfly-Reject-Http-Code'],
+                    'message': response.headers['X-Scrapfly-Reject-Description'],
+                    'error_id': response.headers['X-Scrapfly-Reject-ID'],
+                    'retryable': True if response.headers['X-Scrapfly-Reject-Retryable'] == 'yes' else False
+                }
+
+                if 'X-Scrapfly-Reject-Doc' in response.headers:
+                    api_result['result']['error']['doc_url'] = response.headers['X-Scrapfly-Reject-Doc']
+
+        self.result = self.handle_api_result(api_result=api_result)
+
+    @property
+    def scrape_result(self) -> Dict:
+        return self.result['result']
+
+    @property
+    def config(self) -> Dict:
+        return self.result['config']
+
+    @property
+    def context(self) -> Dict:
+        return self.result['context']
+
+    @property
+    def content(self) -> str:
+        return self.scrape_result['content']
+
+    @property
+    def success(self) -> bool:
+        """
+            /!\ Success means Scrapfly api reply correctly to the call, but the scrape can be unsuccessful if the upstream reply with error status code
+        """
+        return 200 >= self.response.status_code <= 299
+
+    @property
+    def scrape_success(self) -> bool:
+        return self.scrape_result['success']
+
+    @property
+    def error(self) -> Optional[Dict]:
+        if self.scrape_success is False:
+            return self.scrape_result['error']
+
+    @property
+    def status_code(self) -> int:
+        return self.response.status_code
+
+    @property
+    def headers(self) -> CaseInsensitiveDict:
+        return self.response.headers
+
+    def handle_api_result(self, api_result: Dict) -> Optional[FrozenDict]:
+        if self._is_api_error(api_result=api_result) is True:
+            return FrozenDict(api_result)
+
+        if isinstance(api_result['config']['headers'], list):
+            api_result['config']['headers'] = {}
+
+        with suppress(KeyError):
+            api_result['result']['request_headers'] = CaseInsensitiveDict(api_result['result']['request_headers'])
+            api_result['result']['response_headers'] = CaseInsensitiveDict(api_result['result']['response_headers'])
+
+        if api_result['result']['format'] == 'binary' and api_result['result']['content']:
+            api_result['result']['content'] = BytesIO(b64decode(api_result['result']['content']))
+
+        return FrozenDict(api_result)
+
+    @cached_property
+    def soup(self) -> 'BeautifulSoup':
+        try:
+            from bs4 import BeautifulSoup
+            soup = BeautifulSoup(self.content, "lxml")
+            return soup
+        except ImportError as e:
+            logger.error('You must install scrapfly[parser] to enable this feature')
+
+    @cached_property
+    def selector(self) -> 'Selector':
+        try:
+            from scrapy import Selector
+            return Selector(text=self.content)
+        except ImportError as e:
+            logger.error('You must install scrapfly[scrapy] to enable this feature')
+            raise e
+
+    def _is_api_error(self, api_result: Dict) -> bool:
+        if self.scrape_config.method == 'HEAD':
+            if 'X-Reject-Reason' in self.response.headers:
+                return True
+            return False
+
+        if api_result is None:
+            return True
+
+        return 'error_id' in api_result
+
+    def raise_for_result(self, raise_on_upstream_error: bool = True):
+
+        try:
+            self.response.raise_for_status()
+        except HTTPError as e:
+            if 'http_code' in self.result:
+                if e.response.status_code >= 500:
+                    raise ApiHttpServerError(
+                        request=e.request,
+                        response=e.response,
+                        message= self.result['message'],
+                        code='',
+                        resource='',
+                        http_status_code=e.response.status_code
+                    ) from e
+                else:
+                    raise ApiHttpClientError(
+                        request=e.request,
+                        response=e.response,
+                        message=self.result['message'],
+                        code='',
+                        resource='API',
+                        http_status_code=self.result['http_code']
+                    ) from e
+
+        if self.scrape_success is False:
+            error = ErrorFactory.create(api_response=self)
+
+            if error:
+                if isinstance(error, HttpError):
+                    if raise_on_upstream_error is True:
+                        raise error
+                else:
+                    raise error
+
+    def upstream_result_into_response(self, _class=Response) -> Optional[Response]:
+        if _class != Response:
+            raise RuntimeError('only Response from requests package is supported at the moment')
+
+        if self.result is None:
+            return None
+
+        if self.response.status_code != 200:
+            return None
+
+        response = Response()
+        response.status_code = self.scrape_result['status_code']
+        response.reason = self.scrape_result['reason']
+        response._content = self.scrape_result['content'].encode('utf-8') if self.scrape_result['content'] else None
+        response.headers.update(self.scrape_result['response_headers'])
+        response.url = self.scrape_result['url']
+
+        response.request = Request(
+            method=self.config['method'],
+            url=self.config['url'],
+            headers=self.scrape_result['request_headers'],
+            data=self.config['body'] if self.config['body'] else None
+        )
+
+        if 'set-cookie' in response.headers:
+            for raw_cookie in response.headers['set-cookie']:
+                for name, cookie in SimpleCookie(raw_cookie).items():
+                    expires = cookie.get('expires')
+
+                    if expires == '':
+                        expires = None
+
+                    if expires:
+                        try:
+                            expires = parse(expires).timestamp()
+                        except ValueError:
+                            expires = None
+
+                    if type(expires) == str:
+                        if '.' in expires:
+                            expires = float(expires)
+                        else:
+                            expires = int(expires)
+
+                    response.cookies.set_cookie(Cookie(
+                        version=cookie.get('version') if cookie.get('version') else None,
+                        name=name,
+                        value=cookie.value,
+                        path=cookie.get('path', ''),
+                        expires=expires,
+                        comment=cookie.get('comment'),
+                        domain=cookie.get('domain', ''),
+                        secure=cookie.get('secure'),
+                        port=None,
+                        port_specified=False,
+                        domain_specified=cookie.get('domain') is not None and cookie.get('domain') != '',
+                        domain_initial_dot=bool(cookie.get('domain').startswith('.')) if cookie.get('domain') is not None else False,
+                        path_specified=cookie.get('path') != '' and cookie.get('path') is not None,
+                        discard=False,
+                        comment_url=None,
+                        rest={
+                            'httponly': cookie.get('httponly'),
+                            'samesite': cookie.get('samesite'),
+                            'max-age': cookie.get('max-age')
+                        }
+                    ))
+
+        return response
+
+    def sink(self, path: Optional[str] = None, name: Optional[str] = None, file: Optional[Union[TextIO, BytesIO]] = None, content:Optional[Union[str, bytes]]=None):
+        file_content = content or self.scrape_result['content']
+        file_path = None
+        file_extension = None
+
+        if name:
+            name_parts = name.split('.')
+            if len(name_parts) > 1:
+                file_extension = name_parts[-1]
+
+        if not file:
+            if file_extension is None:
+                try:
+                    mime_type = self.scrape_result['response_headers']['content-type']
+                except KeyError:
+                    mime_type = 'application/octet-stream'
+
+                if ';' in mime_type:
+                    mime_type = mime_type.split(';')[0]
+
+                file_extension = '.' + mime_type.split('/')[1]
+
+            if not name:
+                name = self.config['url'].split('/')[-1]
+
+            if name.find(file_extension) == -1:
+                name += file_extension
+
+            file_path = path + '/' + name if path is not None else name
+
+            if file_path == file_extension:
+                url = re.sub(r'(https|http)?://', '', self.config['url']).replace('/', '-')
+
+                if url[-1] == '-':
+                    url = url[:-1]
+
+                url += file_extension
+
+                file_path = url
+
+            file = open(file_path, 'wb')
+
+        if isinstance(file_content, str):
+            file_content = BytesIO(file_content.encode('utf-8'))
+        elif isinstance(file_content, bytes):
+            file_content = BytesIO(file_content)
+
+        file_content.seek(0)
+        with file as f:
+            shutil.copyfileobj(file_content, f, length=131072)
+
+        logger.info('file %s created' % file_path)
---
+++ packages/scrapfly-sdk/scrapfly_sdk-0.7.5-py3-none-any.whl/scrapfly/client.py
@@ -0,0 +1,368 @@
+from collections import Iterable
+from concurrent.futures.thread import ThreadPoolExecutor
+
+import asyncio
+import http
+import platform
+import re
+import shutil
+from functools import partial, cached_property
+from io import BytesIO
+
+import backoff
+from requests import Session, Response
+from requests import exceptions as RequestExceptions
+from typing import TextIO, Union, List, Dict, Optional, Set
+import requests
+import urllib3
+import logging as logger
+
+from .errors import *
+from .api_response import ResponseBodyHandler
+from .scrape_config import ScrapeConfig
+from . import __version__ as version, ScrapeApiResponse
+
+logger.getLogger('scrapfly')
+
+NetworkError = (
+    ConnectionError,
+    RequestExceptions.ConnectionError,
+    RequestExceptions.ReadTimeout
+)
+
+
+class ScrapflyClient:
+
+    HOST = 'https://api.scrapfly.io'
+    DEFAULT_CONNECT_TIMEOUT = 30
+    DEFAULT_READ_TIMEOUT = 150
+
+    host:str
+    key:str
+    max_concurrency:int
+    verify:bool
+    debug:bool
+    distributed_mode:bool
+    connect_timeout:int
+    read_timeout:int
+
+    def __init__(
+        self,
+        key: str,
+        host: Optional[str] = HOST,
+        verify=True,
+        debug: bool = False,
+        max_concurrency:int=1,
+        distributed_mode = False,
+        connect_timeout:int = DEFAULT_CONNECT_TIMEOUT,
+        read_timeout:int = DEFAULT_READ_TIMEOUT
+    ):
+        if host[-1] == '/':  # remove last '/' if exists
+            host = host[:-1]
+
+        self.host = host
+        self.key = key
+        self.verify = verify
+        self.debug = debug
+        self.connect_timeout = connect_timeout
+        self.read_timeout = read_timeout
+        self.max_concurrency = max_concurrency
+        self.distributed_mode = distributed_mode
+        self.body_handler = ResponseBodyHandler()
+        self.async_executor = ThreadPoolExecutor()
+        self.http_session = None
+        self.ua = 'ScrapflySDK/%s (Python %s, %s, %s)' % (
+            version,
+            platform.python_version(),
+            platform.uname().system,
+            platform.uname().machine
+        )
+
+        if not self.verify and not self.HOST.endswith('.local'):
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        if self.debug is True:
+            http.client.HTTPConnection.debuglevel = 5
+
+    @cached_property
+    def _http_handler(self):
+        return partial(self.http_session.request if self.http_session else requests.request)
+
+    @property
+    def http(self):
+        return self._http_handler
+
+    def _scrape_request(self, scrape_config:ScrapeConfig):
+
+        if self.distributed_mode is True and scrape_config.correlation_id is None:
+            scrape_config.generate_distributed_correlation_id()
+
+        return {
+            'method': scrape_config.method,
+            'url': self.host + '/scrape',
+            'data': scrape_config.body,
+            'verify': self.verify,
+            'timeout': (self.connect_timeout, self.read_timeout),
+            'headers': {
+                'content-type': scrape_config.headers['content-type'] if scrape_config.method in ['POST', 'PUT', 'PATCH'] else self.body_handler.content_type,
+                'accept-encoding': self.body_handler.content_encoding,
+                'accept': self.body_handler.accept,
+                'user-agent': self.ua
+            },
+            'params': scrape_config.to_api_params(key=self.key)
+        }
+
+    def account(self) -> Union[str, Dict]:
+        response = self._http_handler('GET', self.host + '/account', params={'key': self.key})
+
+        response.raise_for_status()
+
+        if self.body_handler.support(response.headers):
+            return self.body_handler(response.content)
+
+        return response.content.decode('utf-8')
+
+    def resilient_scrape(
+        self,
+        scrape_config:ScrapeConfig,
+        retry_on_errors:Optional[Set[Exception]]=None,
+        tries: int = 5,
+        delay: int = 20,
+    ) -> ScrapeApiResponse:
+
+        if not isinstance(retry_on_errors, Set) and isinstance(retry_on_errors, Iterable): # keep compat
+            retry_on_errors = set(retry_on_errors)
+
+        retryable_errors = retry_on_errors or {ScrapflyError}
+
+        if retry_on_errors is not None:
+            retryable_errors += retry_on_errors
+
+        @backoff.on_exception(backoff.expo, exception=retryable_errors, max_tries=tries, max_time=delay)
+        def inner() -> ScrapeApiResponse:
+            return self.scrape(scrape_config=scrape_config)
+
+        return inner()
+
+    def open(self):
+        if self.http_session is None:
+            self.http_session = Session()
+            self.http_session.verify = self.verify
+            self.http_session.timeout = (self.connect_timeout, self.read_timeout)
+            self.http_session.params['key'] = self.key
+            self.http_session.headers['accept-encoding'] = self.body_handler.content_encoding
+            self.http_session.headers['accept'] = self.body_handler.accept
+            self.http_session.headers['user-agent'] = self.ua
+
+    def close(self):
+        self.http_session.close()
+        self.http_session = None
+
+    def __enter__(self) -> 'ScrapflyClient':
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    async def async_scrape(self, scrape_config:ScrapeConfig) -> ScrapeApiResponse:
+        return await asyncio.get_running_loop().run_in_executor(self.async_executor, self.scrape, scrape_config)
+
+    async def concurrent_scrape(self, scrape_configs:List[ScrapeConfig], concurrency:Optional[int]=None) -> List[ScrapeApiResponse]:
+
+        try:
+            from asyncio_pool import AioPool
+        except ImportError:
+            print('You must run pip install scrapfly-sdk[concurrency]')
+            raise
+
+        if concurrency is None:
+            concurrency = self.max_concurrency
+
+        futures = []
+
+        async def call(scrape_config:ScrapeConfig) -> ScrapeApiResponse:
+            return await self.async_scrape(scrape_config=scrape_config)
+
+        async with AioPool(size=concurrency) as pool:
+            for index, scrape_config in enumerate(scrape_configs):
+                # handle concurrent session access correctly to prevent 429 session concurrent access
+                if (scrape_config.session is not None or scrape_config.asp is True) and not scrape_config.correlation_id:
+                    scrape_config.correlation_id = 'concurrent_slot_' + str(index)
+
+                futures.append(await pool.spawn(call(scrape_config)))
+
+        return [future.result() for future in futures]
+
+    @backoff.on_exception(backoff.expo, exception=NetworkError, max_tries=5)
+    def scrape(self, scrape_config:ScrapeConfig) -> ScrapeApiResponse:
+        logger.debug('--> %s Scrapping %s' % (scrape_config.method, scrape_config.url))
+        request_data = self._scrape_request(scrape_config=scrape_config)
+        response = self._http_handler(**request_data)
+        return self._handle_response(response=response, scrape_config=scrape_config)
+
+    def _handle_response(self, response:Response, scrape_config:ScrapeConfig) -> ScrapeApiResponse:
+        try:
+            api_response = self._handle_api_response(
+                response=response,
+                scrape_config=scrape_config,
+                raise_on_upstream_error=scrape_config.raise_on_upstream_error
+            )
+
+            if scrape_config.method == 'HEAD':
+                logger.debug('<-- [%s %s] %s | %ss' % (
+                    api_response.response.status_code,
+                    api_response.response.reason,
+                    api_response.response.request.url,
+                    0
+                ))
+            else:
+                logger.debug('<-- [%s %s] %s | %ss' % (
+                    api_response.result['result']['status_code'],
+                    api_response.result['result']['reason'],
+                    api_response.result['config']['url'],
+                    api_response.result['result']['duration'])
+                )
+
+            return api_response
+        except UpstreamHttpClientError as e:
+            if scrape_config.method == 'HEAD':
+                logger.warning('<-- %s | %s' % (str(e), e.api_response.response.request.url))
+            else:
+                logger.warning('<-- %s | %s' % (str(e), e.api_response.result['result']['url']))
+            raise
+        except ApiHttpServerError as e:
+            logger.critical('<-- %s' % str(e))
+            raise
+        except ScrapflyError as e:
+            logger.critical('<-- - %s' % (str(e)))
+            raise
+
+    def save_screenshot(self, api_response:ScrapeApiResponse, name:str, path:Optional[str]=None):
+
+        if not api_response.scrape_result['screenshots']:
+            raise RuntimeError('Screenshot %s do no exists' % name)
+
+        try:
+            api_response.scrape_result['screenshots'][name]
+        except KeyError:
+            raise RuntimeError('Screenshot %s do no exists' % name)
+
+        screenshot_response = self._http_handler(
+            method='GET',
+            url=api_response.scrape_result['screenshots'][name]['url'],
+            params={'key': self.key},
+            verify=False
+        )
+
+        screenshot_response.raise_for_status()
+
+        if not name.endswith('.jpg'):
+            name += '.jpg'
+
+        api_response.sink(path=path, name=name, content=screenshot_response.content)
+
+    def screenshot(self, url:str, path:Optional[str]=None, name:Optional[str]=None) -> str:
+        # for advance configuration, take screenshots via scrape method with ScrapeConfig
+        api_response = self.scrape(scrape_config=ScrapeConfig(
+            url=url,
+            render_js=True,
+            screenshots={'main': 'fullpage'}
+        ))
+
+        name = name or 'main.jpg'
+
+        if not name.endswith('.jpg'):
+            name += '.jpg'
+
+        response = self._http_handler.request(
+            method='GET',
+            url=api_response.scrape_result['screenshots']['main']['url']
+        )
+
+        response.raise_for_status()
+
+        return self.sink(api_response, path=path, name=name, content=response.content)
+
+    def sink(self, api_response:ScrapeApiResponse, content:Optional[Union[str, bytes]]=None, path: Optional[str] = None, name: Optional[str] = None, file: Optional[Union[TextIO, BytesIO]] = None) -> str:
+        scrape_result = api_response.result['result']
+        scrape_config = api_response.result['config']
+
+        file_content = content or scrape_result['content']
+        file_path = None
+        file_extension = None
+
+        if name:
+            name_parts = name.split('.')
+            if len(name_parts) > 1:
+                file_extension = name_parts[-1]
+
+        if not file:
+            if file_extension is None:
+                try:
+                    mime_type = scrape_result['response_headers']['content-type']
+                except KeyError:
+                    mime_type = 'application/octet-stream'
+
+                if ';' in mime_type:
+                    mime_type = mime_type.split(';')[0]
+
+                file_extension = '.' + mime_type.split('/')[1]
+
+            if not name:
+                name = scrape_config['url'].split('/')[-1]
+
+            if name.find(file_extension) == -1:
+                name += file_extension
+
+            file_path = path + '/' + name if path else name
+
+            if file_path == file_extension:
+                url = re.sub(r'(https|http)?://', '', api_response.config['url']).replace('/', '-')
+
+                if url[-1] == '-':
+                    url = url[:-1]
+
+                url += file_extension
+
+                file_path = url
+
+            file = open(file_path, 'wb')
+
+        if isinstance(file_content, str):
+            file_content = BytesIO(file_content.encode('utf-8'))
+        elif isinstance(file_content, bytes):
+            file_content = BytesIO(file_content)
+
+        file_content.seek(0)
+        with file as f:
+            shutil.copyfileobj(file_content, f, length=131072)
+
+        logger.info('file %s created' % file_path)
+        return file_path
+
+    def _handle_api_response(
+        self,
+        response: Response,
+        scrape_config:ScrapeConfig,
+        raise_on_upstream_error: Optional[bool] = True
+    ) -> ScrapeApiResponse:
+
+        if scrape_config.method == 'HEAD':
+            body = None
+        else:
+            if self.body_handler.support(headers=response.headers):
+                body = self.body_handler(response.content)
+            else:
+                body = response.content.decode('utf-8')
+
+        api_response:ScrapeApiResponse = ScrapeApiResponse(
+            response=response,
+            request=response.request,
+            api_result=body,
+            scrape_config=scrape_config
+        )
+
+        api_response.raise_for_result(raise_on_upstream_error=raise_on_upstream_error)
+
+        return api_response
