d08b21bda0655af0779721621eb5b7d56f27c31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:34 +0000

    Add package plotvet/0.0.10/plotvet-0.0.10.tar.gz

---
+++ packages/plotvet/plotvet-0.0.10.tar.gz/plotvet-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='plotvet',
+    version='0.0.10',
+    url='https://github.com/marcos-de-sousa/plotvet',
+    license='MIT License',
+    author='Marcos Paulo Alves de Sousa',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='desousa.mpa@gmail.com',
+    keywords='Pacote',
+    description='Pacote python para plotagem de vetores em planos bidimensionais e espaços tridimensionais',
+    packages=['plotvet'],
+    install_requires=['numpy','matplotlib'])
