728fd536a43fe055ffa0bd665d332175cd714507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:08 +0000

    Add package aiohttp-retry/2.3.5/aiohttp_retry-2.3.5.tar.gz

---
+++ packages/aiohttp-retry/aiohttp_retry-2.3.5.tar.gz/aiohttp_retry-2.3.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='aiohttp_retry',
+    version='2.3.5',
+    description='Simple retry client for aiohttp',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='aiohttp retry client',
+    author='Dmitry Inyutin',
+    author_email='inyutin.da@gmail.com',
+    url='https://github.com/inyutin/aiohttp_retry',
+    license='MIT',
+    include_package_data=True,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    platforms=['any'],
+    install_requires=[
+        'aiohttp',
+    ]
+)
