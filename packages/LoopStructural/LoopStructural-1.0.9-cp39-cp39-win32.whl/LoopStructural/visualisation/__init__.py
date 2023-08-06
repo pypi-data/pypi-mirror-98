3e168f315481cdf65acde14e631719b8ff4470f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:24 +0000

    Add package LoopStructural/1.0.9/LoopStructural-1.0.9-cp39-cp39-win32.whl

---
+++ packages/LoopStructural/LoopStructural-1.0.9-cp39-cp39-win32.whl/LoopStructural/visualisation/__init__.py
@@ -0,0 +1,31 @@
+"""
+This is the base visualistion module for loop structural
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    map_viewer
+    model_visualisation
+    rotation_angle_plotter
+
+"""
+from ..utils import getLogger
+logger = getLogger(__name__)
+try:
+    import matplotlib
+    from .map_viewer import MapView
+    from .rotation_angle_plotter import RotationAnglePlotter
+except ImportError:
+    logger.warning('Cannot use MapView or RotationAnglePlotter as matplotlib is not installed. \n'\
+                   'Install matplotlib and try again. ')
+try:
+    from .sphinx_scraper import _get_loop_visualisation_scraper
+except:
+    logger.error('Cannot use sphinx scraper, pip install -r docs/requirements.txt')
+try:
+    from .model_visualisation import LavaVuModelViewer
+except:
+    logger.error("Missing lavavu, can't import LavaVuModelViewer")
