62f3e03a8bdd2b6786e51d12dac1129fd2c33572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package secure-password/0.5/secure_password-0.5.tar.gz

---
+++ packages/secure-password/secure_password-0.5.tar.gz/secure_password-0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='secure_password',
+  version='0.5',
+  description='A very easy password generator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Invisible Medusa',
+  author_email='invisiblemedusa10@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='password generator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
