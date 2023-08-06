0bf78bb9b1f74761f6b2ad23bb552a32ee23bfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package telegram-handler/1.1/telegram-handler-1.1.tar.gz

---
+++ packages/telegram-handler/telegram-handler-1.1.tar.gz/telegram-handler-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='telegram-handler',
+    version='1.1',
+    description='async telegram handler',
+    author='Guyshe',
+    url='https://github.com/guyshe/telegram_handler',
+    install_requieres=[
+        'requests'
+    ]
+)
