74ecce7adad9aac7a26164597e4f87a854af2650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:35 +0000

    Add package SDDB/0.0.3/SDDB-0.0.3.tar.gz

---
+++ packages/SDDB/SDDB-0.0.3.tar.gz/SDDB-0.0.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+with open("LICENSE", "r", encoding="utf-8") as fh:
+    license = fh.read()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SDDB",
+    version="0.0.3",
+    author="Kelzorz",
+    author_email="43789359+Kelzorz@users.noreply.github.com",
+    # description="Using Discord as a simple database",
+    # description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    license=license,
+    url="https://github.com/Kelzorz/SDDB",
+    project_urls={
+        "Issue tracker" : "https://github.com/Kelzorz/SDDB/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3"
+        "License :: BEERWARE"
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+    ],
+    install_requires=requirements,
+    packages=["SDDB"],
+    python_requires=">=3.5.3"
+)
