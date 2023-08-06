81625598714297a7ee4a95703873b54ff3260744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package zcls/0.3.1/zcls-0.3.1.tar.gz

---
+++ packages/zcls/zcls-0.3.1.tar.gz/zcls-0.3.1/zcls/config/configs/dataloader.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/11/25 下午6:50
+@file: dataloader.py
+@author: zj
+@description: 
+"""
+
+from yacs.config import CfgNode as CN
+
+
+def add_config(_C):
+    # ---------------------------------------------------------------------------- #
+    # DataLoader
+    # ---------------------------------------------------------------------------- #
+    _C.DATALOADER = CN()
+    _C.DATALOADER.TRAIN_BATCH_SIZE = 16
+    _C.DATALOADER.TEST_BATCH_SIZE = 16
+
+    # refert to [torch Dataloader中的num_workers](https://zhuanlan.zhihu.com/p/69250939)
+    _C.DATALOADER.NUM_WORKERS = 4
