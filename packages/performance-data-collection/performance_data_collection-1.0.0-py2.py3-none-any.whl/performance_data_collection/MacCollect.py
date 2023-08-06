5bae72810a9d361926fac15d665de36b5eefaa4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:45 +0000

    Add package performance-data-collection/1.0.0/performance_data_collection-1.0.0-py2.py3-none-any.whl

---
+++ packages/performance-data-collection/performance_data_collection-1.0.0-py2.py3-none-any.whl/performance_data_collection/MacCollect.py
@@ -0,0 +1,11 @@
+from performance_data.BaseCollect import BaseCollect
+
+
+class Mac(BaseCollect):
+
+    def __init__(self, process_name, collect_time):
+        super().__init__(process_name, collect_time)
+
+    def collect(self):
+        self.run()
+
