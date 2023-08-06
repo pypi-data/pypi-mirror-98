9682ed776b264712d66c7469378616eac0890ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:57 +0000

    Add package dicpp/0.1.3/dicpp-0.1.3-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/dicpp/dicpp-0.1.3-cp39-cp39-manylinux2010_x86_64.whl/dicpp/__init__.py
@@ -0,0 +1,20 @@
+r"""
+===============================================================
+Digital Image Correlation (:mod:`dicpp`)
+===============================================================
+
+.. currentmodule:: dicpp
+
+Post processing tools for DIC data of cylindrical shells.
+
+.. automodule:: dicpp.fit_data
+    :members:
+
+.. automodule:: dicpp.interpolate
+    :members:
+
+.. automodule:: dicpp.stitch
+    :members:
+
+"""
+pass
