f8e9790d189381aeeb6213fd827a8a4e9b17c76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:34 +0000

    Add package calchesoft/0.0.1/calchesoft-0.0.1.tar.gz

---
+++ packages/calchesoft/calchesoft-0.0.1.tar.gz/calchesoft-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 10:44:17 2021
+
+@author: hamza_shoukat
+"""
+
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "calchesoft",
+        version = "0.0.1",
+        description='Python Distribution Utilities',
+        author='Hamza shoukat',
+        author_email='hamza_shoukat@diyatech.com',
+        long_description = long_description,
+        long_description_context_type = "text/markdown",
+        url='https://github.com/hamzashoukat94/pythonAPP',
+        packages=setuptools.find_packages(),
+        python_requires = ">=3.6",
+        classifier = [
+                "Programming language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",]
+        )
