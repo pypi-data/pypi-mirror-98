bde9b9673075ce54da0492ad84c2a580710ad446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package pyrogis/0.3.0a0/pyrogis-0.3.0a0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/pyrogis/pyrogis-0.3.0a0-cp38-cp38-manylinux2014_x86_64.whl/pyrogis/ingredients/flip.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from .ingredient import Ingredient
+
+
+class Flip(Ingredient):
+    """
+    flip pixels about an axis
+    """
+
+    def prep(self, axis: int = 0, **kwargs):
+        """
+        :param axis: 0 to flip vertically, 1 to flip horizontally
+        """
+
+        self.axis = axis
+
+    def cook(self, pixels: np.ndarray):
+        """
+        flip the pixels
+        """
+        flipped_pixels = np.flip(pixels, axis=self.axis)
+
+        return flipped_pixels
