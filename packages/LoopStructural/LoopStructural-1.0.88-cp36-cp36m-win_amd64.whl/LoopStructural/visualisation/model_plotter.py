215323f2385e6d321de153ed5978fad57e587fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:40 +0000

    Add package LoopStructural/1.0.88/LoopStructural-1.0.88-cp36-cp36m-win_amd64.whl

---
+++ packages/LoopStructural/LoopStructural-1.0.88-cp36-cp36m-win_amd64.whl/LoopStructural/visualisation/model_plotter.py
@@ -0,0 +1,17 @@
+# import logging
+#
+#
+# from LoopStructural.utils import getLogger
+# logger = getLogger(__name__)
+#
+# class BaseModelPlotter:
+#     def __init__(self, model):
+#         """
+#
+#         Parameters
+#         ----------
+#         model
+#         """
+#         self.model = model
+#
+#     def
