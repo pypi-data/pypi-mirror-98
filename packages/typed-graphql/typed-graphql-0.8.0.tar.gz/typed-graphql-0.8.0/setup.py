d0a876857f9eea781e7da60471b05d0c03591143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:27 +0000

    Add package typed-graphql/0.8.0/typed-graphql-0.8.0.tar.gz

---
+++ packages/typed-graphql/typed-graphql-0.8.0.tar.gz/typed-graphql-0.8.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['typed_graphql']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphql-core>=3.1.2,<4.0.0']
+
+setup_kwargs = {
+    'name': 'typed-graphql',
+    'version': '0.8.0',
+    'description': 'Use types to have a simpler graphql implementation',
+    'long_description': None,
+    'author': 'Willem Thiart',
+    'author_email': 'himself@willemthiart.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
