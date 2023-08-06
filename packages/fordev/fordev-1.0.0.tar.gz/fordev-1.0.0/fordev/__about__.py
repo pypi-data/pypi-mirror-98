e5c7558082f023faf1d35168156efa15cf9056ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:55 +0000

    Add package fordev/1.0.0/fordev-1.0.0.tar.gz

---
+++ packages/fordev/fordev-1.0.0.tar.gz/fordev-1.0.0/fordev/__about__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Metadados do projeto."""
+
+__version__ = '1.0.0'
+__author__ = 'Matheus Felipe'
+__email__ = 'matheusfelipeog@protonmail.com'
+__author_github__ = 'https://github.com/matheusfelipeog'
+__project_github__ = 'https://github.com/matheusfelipeog/fordev'
