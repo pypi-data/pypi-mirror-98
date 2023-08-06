11b5c7b763c6e5364f0342b96b01494a4c9b5648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:38 +0000

    Add package wqxlib/3.0.0.3.4/wqxlib-3.0.0.3.4-py3-none-any.whl

---
+++ packages/wqxlib/wqxlib-3.0.0.3.4-py3-none-any.whl/wqxlib/Submission.py
@@ -0,0 +1,65 @@
+from io import BytesIO
+from typing import List
+from zipfile import ZipFile
+
+from .Document import Document
+from .ImportConfiguration import ImportConfiguration
+
+
+class Submission:
+  __document: Document
+  __importConfiguration: ImportConfiguration
+
+  def __init__(self, o:dict=None, *,
+    document:Document = None,
+    importConfiguration:ImportConfiguration = None,
+  ):
+    if isinstance(o, Document):
+      # Assign attributes from object without typechecking
+      self.__document = o.document
+      self.__importConfiguration = o.importConfiguration
+    elif isinstance(o, dict):
+      # Assign attributes from dictionary with typechecking
+      self.document = o.get('document')
+      self.importConfiguration = o.get('importConfiguration')
+    else:
+      # Assign attributes from named keywords with typechecking
+      self.document = document
+      self.importConfiguration = importConfiguration
+
+  @property
+  def document(self) -> Document:
+    return self.__document
+  @document.setter
+  def document(self, val:Document) -> None:
+    if val is not None and not isinstance(val, Document):
+      raise TypeError("Attribute 'document' must be a Document object.")
+    self.__document = None if val is None else Document(val)
+
+  @property
+  def importConfiguration(self) -> ImportConfiguration:
+    return self.__importConfiguration
+  @importConfiguration.setter
+  def importConfiguration(self, val:ImportConfiguration) -> None:
+    if val is not None and not isinstance(val, ImportConfiguration):
+      raise TypeError("Attribute 'importConfiguration' must be a ImportConfiguration object.")
+    self.__importConfiguration = None if val is None else ImportConfiguration(val)
+
+  def generateZIP(self, fileName:str=None):
+    if not isinstance(fileName, str):
+      raise TypeError("Parameter 'fileName' must be a string.")
+
+    mem = BytesIO()
+    zip = ZipFile(mem, mode='w')
+
+    results = self.__document.generateXML()
+    zip.writestr('results.xml', results)
+
+    # TODO: Add attachment files, if necessary. Example:
+    #   zip.writestr('rawdata.csv', self.data)
+
+    zip.close()
+    mem.seek(0)
+
+    with open(fileName, 'wb') as out:
+      out.write(mem.read())
