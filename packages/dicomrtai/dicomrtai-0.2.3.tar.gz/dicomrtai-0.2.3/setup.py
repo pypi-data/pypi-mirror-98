54d493ff4ae2294a6e8c3c0082ee75da06c3c294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:47 +0000

    Add package dicomrtai/0.2.3/dicomrtai-0.2.3.tar.gz

---
+++ packages/dicomrtai/dicomrtai-0.2.3.tar.gz/dicomrtai-0.2.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dicomrtai', 'dicomrtai.proto']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.4,<0.5.0', 'grpcio>=1.36.1,<2.0.0', 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['dicomrtai = dicomrtai.__main__:app']}
+
+setup_kwargs = {
+    'name': 'dicomrtai',
+    'version': '0.2.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
