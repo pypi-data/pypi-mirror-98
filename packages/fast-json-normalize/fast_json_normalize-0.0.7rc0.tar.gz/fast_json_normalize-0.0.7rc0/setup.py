63caa3b9d3a208284ff4a45257d4032cb0943ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:29 +0000

    Add package fast-json-normalize/0.0.7rc0/fast_json_normalize-0.0.7rc0.tar.gz

---
+++ packages/fast-json-normalize/fast_json_normalize-0.0.7rc0.tar.gz/fast_json_normalize-0.0.7rc0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name="fast_json_normalize",
+    version="0.0.7c",
+    packages=find_packages(),
+    author="Sam Purkis",
+    description="A classification library using a novel audio-inspired algorithm.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/smpurkis/fast_json_normalize",
+    ext_modules=cythonize(["fast_json_normalize/cythonized.pyx"]),
+    install_requires=[  # I get to this in a second
+        'pytest',
+        'cython',
+        'pandas',
+        'setuptools'
+    ],
+)
