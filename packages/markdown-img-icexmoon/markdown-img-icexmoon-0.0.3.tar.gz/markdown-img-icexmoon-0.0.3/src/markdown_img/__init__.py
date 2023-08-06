b53b009f8f97d3daba3ae5f037f103328b9e807c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:30 +0000

    Add package markdown-img-icexmoon/0.0.3/markdown-img-icexmoon-0.0.3.tar.gz

---
+++ packages/markdown-img-icexmoon/markdown-img-icexmoon-0.0.3.tar.gz/markdown-img-icexmoon-0.0.3/src/markdown_img/__init__.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    import os
+    import sys
+    part = __file__.rpartition('\\')
+    packageDirPath = part[0]
+    sys.path.append(packageDirPath)
+    from main import Main
+    mainProcess = Main()
+    mainProcess.main()
