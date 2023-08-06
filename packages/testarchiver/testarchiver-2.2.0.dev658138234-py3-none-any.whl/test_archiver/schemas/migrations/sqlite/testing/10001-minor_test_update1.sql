5479e1182d7298af75da05c25e12188994b68724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:45 +0000

    Add package testarchiver/2.2.0.dev658138234/testarchiver-2.2.0.dev658138234-py3-none-any.whl

---
+++ packages/testarchiver/testarchiver-2.2.0.dev658138234-py3-none-any.whl/test_archiver/schemas/migrations/sqlite/testing/10001-minor_test_update1.sql
@@ -0,0 +1,2 @@
+INSERT INTO schema_updates(schema_version, applied_by)
+VALUES (10001, '{applied_by}');
---
+++ packages/testarchiver/testarchiver-2.2.0.dev658138234-py3-none-any.whl/test_archiver/schemas/migrations/sqlite/testing/10002-minor_test_update2.sql
@@ -0,0 +1,2 @@
+INSERT INTO schema_updates(schema_version, applied_by)
+VALUES (10002, '{applied_by}');
