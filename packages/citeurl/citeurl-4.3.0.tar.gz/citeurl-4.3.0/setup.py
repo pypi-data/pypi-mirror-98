5138354ab30742bf44d2e141dea89dc3cfc32ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:31 +0000

    Add package citeurl/4.3.0/citeurl-4.3.0.tar.gz

---
+++ packages/citeurl/citeurl-4.3.0.tar.gz/citeurl-4.3.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='citeurl',
+    version='4.3.0',
+    description='an extensible tool to process legal citations in text',
+    author='Simon Raindrum Sherred',
+    author_email='simonraindrum@gmail.com',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://raindrum.github.io/citeurl",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'citeurl=citeurl.cli:main',
+            'citeurl-makejs=citeurl.makejs:main',
+        ],
+        'markdown.extensions': ['citeurl=citeurl.mdx:CiteURLExtension'],
+    },
+    include_package_data=True,
+    install_requires=['pyyaml', 'markdown'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Legal Industry',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: Markdown',
+    ],
+)
