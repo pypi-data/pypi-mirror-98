9acbe46a2dd0228274747055c44627f8bffb372b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:34 +0000

    Add package mud/0.0.23/mud-0.0.23.tar.gz

---
+++ packages/mud/mud-0.0.23.tar.gz/mud-0.0.23/tests/test_util.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import mud.util as mdu
+
+__author__ = "Mathematical Michael"
+__copyright__ = "Mathematical Michael"
+__license__ = "mit"
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_probability_high_for_tolerance(self):
+        # Arrange
+        tolerances = [0.01, 0.05, 0.1, 0.25]
+        result = []
+
+        # Act
+        for tol in tolerances:
+            result.append(mdu.std_from_equipment(tolerance=tol,
+                                                 probability=0.9999))
+
+        # Assert
+        for i, tol in enumerate(tolerances):
+            assert 3 * result[i] < tol
+
+    def test_probability_med_for_tolerance(self):
+        # Arrange
+        tolerances = [0.01, 0.05, 0.1, 0.25]
+        result = []
+
+        # Act
+        for tol in tolerances:
+            result.append(mdu.std_from_equipment(tolerance=tol,
+                                                 probability=0.999))
+
+        # Assert
+        for i, tol in enumerate(tolerances):
+            assert 3 * result[i] < tol
