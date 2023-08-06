2c446be478950b3d906f2086c750cf7739d8af2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:35 +0000

    Add package graph-ensembles/0.0.4/graph-ensembles-0.0.4.tar.gz

---
+++ packages/graph-ensembles/graph-ensembles-0.0.4.tar.gz/graph-ensembles-0.0.4/HISTORY.rst
@@ -0,0 +1,27 @@
+=======
+History
+=======
+
+0.0.4 (2021-03-15)
+------------------
+* Fixed issues with slow pandas index conversion
+
+0.0.3 (2021-03-14)
+------------------
+* Large changes in API with great improvements in usability
+* Added sampling function
+* Added RandomGraph model
+* Added Graph classes for ease of use
+
+
+0.0.2 (2020-11-13)
+------------------
+* Added steps for CI. 
+* Corrected broken links. 
+* Removed support for python 3.5 and 3.6
+
+0.0.1 (2020-10-28)
+------------------
+
+* First release on PyPI. StripeFitnessModel available, all other model classes still dummies.
+
---
+++ packages/graph-ensembles/graph-ensembles-0.0.4.tar.gz/graph-ensembles-0.0.4/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+with open('README.rst', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst', encoding='utf-8') as history_file:
+    history = history_file.read()
+
+setup(
+    name='graph-ensembles',
+    author="Leonardo Niccolò Ialongo",
+    author_email='leonardo.ialongo@gmail.com',
+    python_requires='>=3.0',
+    version='0.0.4',
+    url='https://github.com/LeonardoIalongo/graph-ensembles',
+    description=("The graph ensemble package contains a set of methods to"
+                 " build fitness based graph ensembles from marginal"
+                 " information."),
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    license="GNU General Public License v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    packages=['graph_ensembles', 'graph_ensembles.methods'],
+    package_dir={'': 'src'},
+    install_requires=["numpy>=1.20",
+                      "numba==0.53.rc3",
+                      "pandas>=1.1"
+                      ],
+    extras_require={
+        "dev": ["pytest==6.0.1",
+                "coverage==5.2.1",
+                "pytest-cov==2.10.1",
+                "flake8==3.8.3",
+                "wheel==0.35.1",
+                "matplotlib==3.3.2",
+                "networkx==2.5",
+                "check-manifest==0.44",
+                "setuptools==47.1.0",
+                "twine==3.2.0",
+                "tox==3.20.1"],
+        },
+    )
