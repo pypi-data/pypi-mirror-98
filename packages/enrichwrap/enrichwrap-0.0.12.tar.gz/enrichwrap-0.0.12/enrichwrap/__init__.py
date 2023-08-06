5c563b3f93f25fc3825cc96e60f9152a66fe37dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:21 +0000

    Add package enrichwrap/0.0.12/enrichwrap-0.0.12.tar.gz

---
+++ packages/enrichwrap/enrichwrap-0.0.12.tar.gz/enrichwrap-0.0.12/enrichwrap/__init__.py
@@ -0,0 +1,6 @@
+from .text import joke,checkfile
+from .steps import get_steps
+from .ccexception import CustomCallException
+from .indata import get_data,get_default_data
+from .read_mappings import get_mapping, entry
+
