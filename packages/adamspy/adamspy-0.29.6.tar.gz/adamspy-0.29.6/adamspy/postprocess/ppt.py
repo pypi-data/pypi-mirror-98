d1b357ce2b98f6461bf9dd55c57247670ca27b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:30 +0000

    Add package adamspy/0.29.6/adamspy-0.29.6.tar.gz

---
+++ packages/adamspy/adamspy-0.29.6.tar.gz/adamspy-0.29.6/adamspy/postprocess/ppt.py
@@ -0,0 +1,406 @@
+"""This module is for sending scripts through Adams/PPT
+
+"""
+import os
+import subprocess
+import re
+import time
+import platform
+
+import jinja2
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+from thornpy.signal import manually_clean_sig, remove_data_point, manually_clean_sigs, low_pass
+from thornpy.signal import _clean_sig as clean_sig
+
+
+LOG_COMPLETE_PATTERN = '! Command file is exhausted, batch run is finished.'
+
+LUNAR_SCRIPT_NAME = '.get_lunar_results.py'
+GET_RESULTS_SCRIPT_NAME = '.get_results.py'
+EDIT_RESULTS_SCRIPT_NAME = '.edit_results.py'
+TEMP_OUTPUT_FILENAME = '.results.tmp'
+
+LOG_NAME = 'aview.log'
+
+TMPLT_ENV = jinja2.Environment(
+    loader=jinja2.PackageLoader('adamspy.postprocess', 'aview_scripts'),
+    autoescape=False,
+    keep_trailing_newline=True,
+    trim_blocks=True,
+    lstrip_blocks=True
+)
+
+_TIMEOUT = 300
+LOG_FILE_ERROR_PATTERN = '! \\S*Error: '
+
+def get_results(res_file, reqs_to_get, t_min=None, t_max=None, _just_write_script=False, timeout=_TIMEOUT):
+    """Gets results from an Adams results (.res) file.
+	
+	Example
+	-------
+	>>> result_file = 'example.res'
+	>>> t_min = 70
+	>>> t_max = 80
+	>>> reqs_to_get = {}
+	>>> reqs_to_get['MSE'] = ['Instantaneous_Bottom_MSE', 'Filtered_Surface_MSE']
+	>>> reqs_to_get['ROP_controls'] = ['Command_ROP', 'True_WOB']
+	>>> requests, units = get_results(result_file, reqs_to_get, t_min, t_max)
+
+	Note
+	----
+	This funciton only works with Requests.  It does not work with Result Sets.
+
+	Note
+	----
+	This function only works with xml results files.
+	
+	Parameters
+	----------
+	result_file : str
+		Filename of an Adams results (.res) file
+	reqs_to_get : dict
+		Dictionary of requests to extract (the default is None, which gets all results)
+	t_min : float, optional
+		Minimum time for which to extract results (the default is None)
+	t_max : float, optional
+		Maximum time for which to extract results (the default is None)
+	
+	Returns
+	-------
+	dict
+		Dictionary of request data
+	
+	"""
+    template = TMPLT_ENV.from_string(open(os.path.join(os.path.dirname(__file__), 'aview_scripts', GET_RESULTS_SCRIPT_NAME)).read())
+    working_directory = os.path.dirname(res_file)
+
+    with open(os.path.join(working_directory, GET_RESULTS_SCRIPT_NAME), 'w') as fid:
+        fid.write(template.render({'res_file': os.path.split(res_file)[-1], 'reqs_to_get': reqs_to_get, 't_min': t_min, 't_max': t_max, 'output_file': TEMP_OUTPUT_FILENAME}))
+
+    if _just_write_script is False:
+        # Delete the aview.log file
+        try:
+            os.remove(os.path.join(working_directory, LOG_NAME))
+        except FileNotFoundError:
+            pass
+        except PermissionError:
+            pass
+
+        # Run the postprocessor
+        if platform.system() == 'Windows':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW        
+            subprocess.Popen('"{}" aview ru-s b {}'.format(os.environ['ADAMS_LAUNCH_COMMAND'], GET_RESULTS_SCRIPT_NAME), cwd=working_directory, startupinfo=startupinfo)
+        
+        else:
+            subprocess.Popen([os.environ['ADAMS_LAUNCH_COMMAND'], '-c', 'aview', 'ru-standard', 'b', GET_RESULTS_SCRIPT_NAME, 'exit'], cwd=working_directory)            
+
+        # Wait for complete
+        _wait(os.path.join(working_directory, LOG_NAME), timeout=timeout)
+
+        # Check the log file for errors
+        _get_log_errors(os.path.join(working_directory, LOG_NAME))
+
+        # Read and return the results
+        data = genfromtxt(os.path.join(working_directory, TEMP_OUTPUT_FILENAME), delimiter=',', names=True, dtype=None)
+
+        os.remove(os.path.join(working_directory, GET_RESULTS_SCRIPT_NAME))
+        os.remove(os.path.join(working_directory, TEMP_OUTPUT_FILENAME))
+
+        output_dict = {'time': list(data['time'])}
+        for res in reqs_to_get:
+            output_dict[res] = {}
+            for comp in reqs_to_get[res]:
+                output_dict[res][comp] = list(data[f'{res}_{comp}'])
+        
+        return output_dict
+
+def write_results(res_file, input_dict):
+    # TODO
+    return
+
+def edit_results(res_file, input_dict, new_res_file=None, _just_write_script=False, timeout=_TIMEOUT):
+    template = TMPLT_ENV.from_string(open(os.path.join(os.path.dirname(__file__), 'aview_scripts', EDIT_RESULTS_SCRIPT_NAME)).read())
+    working_directory = os.path.dirname(res_file)
+
+    new_res_file = os.path.split(res_file)[-1] if new_res_file is None else os.path.split(new_res_file)[-1]
+
+    with open(os.path.join(working_directory, EDIT_RESULTS_SCRIPT_NAME), 'w') as fid:
+        fid.write(template.render({'res_file': os.path.split(res_file)[-1], 'reqs_to_edit': input_dict, 'output_file': new_res_file}))
+
+    if _just_write_script is False:
+        # Delete the aview.log file
+        try:
+            os.remove(os.path.join(working_directory, LOG_NAME))
+        except FileNotFoundError:
+            pass
+        except PermissionError:
+            pass
+
+        if platform.system() == 'Windows':
+            # Run the postprocessor
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW        
+            subprocess.Popen('"{}" aview ru-s b {}'.format(os.environ['ADAMS_LAUNCH_COMMAND'], EDIT_RESULTS_SCRIPT_NAME), cwd=working_directory, startupinfo=startupinfo)
+        
+        else:
+            subprocess.Popen([os.environ['ADAMS_LAUNCH_COMMAND'], '-c', 'aview', 'ru-s', 'b', EDIT_RESULTS_SCRIPT_NAME, 'exit'], cwd=working_directory)
+
+        # Wait for complete
+        _wait(os.path.join(working_directory, LOG_NAME), timeout=timeout)
+
+        # Check the log file for errors
+        _get_log_errors(os.path.join(working_directory, LOG_NAME))
+
+def get_lunar_results(res_files, reqs_to_get, t_min, t_max, output_file, _just_write_script=False, timeout=_TIMEOUT):
+    
+    template = TMPLT_ENV.from_string(open(os.path.join(os.path.dirname(__file__), 'aview_scripts', LUNAR_SCRIPT_NAME)).read())
+    
+    working_directory = os.path.dirname(res_files[0])
+
+    with open(os.path.join(working_directory, LUNAR_SCRIPT_NAME), 'w') as fid:
+        fid.write(template.render({'res_files': res_files, 'reqs_to_get': reqs_to_get, 't_min': t_min, 't_max': t_max, 'output_suffix': output_file}))
+
+    if _just_write_script is False:
+        # Delete the aview.log file
+        try:
+            os.remove(os.path.join(working_directory, LOG_NAME))
+        except FileNotFoundError:
+            pass
+        
+        # Run the postprocessor
+        if platform.system() == 'Windows':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW        
+            subprocess.Popen('"{}" aview ru-s b {}'.format(os.environ['ADAMS_LAUNCH_COMMAND'], LUNAR_SCRIPT_NAME), cwd=working_directory, startupinfo=startupinfo)
+        
+        else:
+            subprocess.Popen([os.environ['ADAMS_LAUNCH_COMMAND'], 'aview', 'ru-s', 'b', LUNAR_SCRIPT_NAME], cwd=working_directory)
+
+        # Wait for complete
+        _wait(os.path.join(working_directory, LOG_NAME), timeout=timeout)
+
+        os.remove(os.path.join(working_directory, LUNAR_SCRIPT_NAME))
+
+        # Make a list of the files that are written
+        res_output_files = [os.path.splitext(output_file)[0] + '_time' + os.path.splitext(output_file)[-1]]
+        for res_name, res_comps in reqs_to_get.items():
+            for res_comp in res_comps:
+                full_res_name = f'{res_name}_{res_comp}'
+                res_output_files.append(os.path.splitext(output_file)[0] + '_' + full_res_name + os.path.splitext(output_file)[-1])
+
+        return res_output_files
+
+def _wait(log_file, sleep_time=0.2, timeout=300):
+    """Waits for the log file to write the last line of the macro
+    
+    Parameters
+    ----------
+    log_file : str
+        filename of log file
+    sleep_time : float, optional
+        Time between checks, by default 0.2
+    timeout : int, optional
+        During after which to time out, by default 300
+
+    """    
+
+    for _i in range(int(timeout/sleep_time)):
+        
+        ppt_log_file_exists = os.path.exists(log_file)       
+
+        if ppt_log_file_exists is True:
+            # If ppt.log exists, open it and see if the results have been loaded
+            with open(log_file, 'r') as fid:
+                text = fid.read()
+            if re.search(LOG_COMPLETE_PATTERN, text):
+                break
+                
+        time.sleep(sleep_time)
+
+def _get_log_errors(log_file):
+    """Checks the log file for errors of the type AviewError.
+    
+    Parameters
+    ----------
+    log_file : str
+        Filename of aview log file (usulally aview.log)
+
+    """
+    with open(log_file, 'r') as fid:
+        lines = fid.readlines()
+    
+    for line in lines:
+        if re.search(LOG_FILE_ERROR_PATTERN, line):
+            raise AviewError(line[2:])        
+
+def manually_remove_spikes(res_file, reqs_to_clean, reqs_to_check=None, t_min=None, t_max=None, _just_write_script=False, timeout=_TIMEOUT, _inplace=False):
+    """Allows the user to manually scan through the result sets to pick out points to eliminate.
+
+    Parameters
+    ----------
+    res_file : str
+        Adams Results (.res) filename 
+    reqs_to_clean : dict, optional
+        Nested dictionary of result sets and result components to clean
+    reqs_to_check : dict
+        Nested dictionary of result sets and result components to check for spikes, by default same as reqs_to_clean
+    t_min : float, optional
+        Minumum simulation time to clean, by default None
+    t_max : float, optional
+        Maximum simulation time to clean, by default None
+    timeout : float, optional
+        Number of seconds to wait for results to load before timing out, by default _TIMEOUT
+
+    Returns
+    -------
+    dict
+        Nested dictionary of cleaned results
+        
+    """
+    if reqs_to_check is None:
+        reqs_to_check = reqs_to_clean
+
+    results = get_results(res_file, reqs_to_clean, t_min=t_min, t_max=t_max, _just_write_script=_just_write_script, timeout=timeout)
+    
+    time_sig = results['time']
+    
+    # Remove the spikes
+    for (res, res_comps) in [(r, rc) for r, rc in results.items() if r in reqs_to_check]:
+        for (res_comp, values) in [(rc, v) for rc, v in res_comps.items() if rc in reqs_to_check[res]]:
+            results[res][res_comp], i_mod = manually_clean_sig(time_sig, values, indices=True)
+
+            # If a modification was made to the signal, make that modification to the rest of the signals
+            if i_mod != []:
+
+                # Loop over all the other results
+                for (other_res, other_res_comps) in [(r, rc) for r, rc in results.items() if r != 'time']:
+                    for (other_res_comp, other_values) in [(rc, v) for rc, v in other_res_comps.items() if not (other_res == res and rc == res_comp)]: #pylint: disable=no-member
+                        for i in i_mod:
+                            results[other_res][other_res_comp] = remove_data_point(time_sig, other_values, i)
+
+    # Update the analysis files
+    edit_results(res_file, results)
+
+    # Return the cleaned results
+    return results
+
+def filter_results(res_file, reqs_to_clean, freq_cutoff, N_filter=5, reqs_to_check=None, t_min=None, t_max=None, _just_write_script=False, timeout=_TIMEOUT, _inplace=False, return_raw=False):
+    """Similar to `manually_remove_spikes`, but allows user to plot the signals in batches.  Instead of passing a dictionary for the `reqs_to_check` argument, pass a list of dictionaries and the results in each dictionary in the list will be plotted together.
+
+    Parameters
+    ----------
+    res_file : str
+        Adams Results (.res) filename 
+    reqs_to_clean : dict
+        Nested dictionary of result sets and result components to clean
+    freq_cutoff : float
+        Cutoff freqency of filter in Hz
+    N_filter : int
+        Order of filter
+    reqs_to_check : list of dicts
+        list of nested dictionary of result sets and result components to check for spikes, by default same as reqs_to_clean
+    t_min : float, optional
+        Minumum simulation time to clean, by default None
+    t_max : float, optional
+        Maximum simulation time to clean, by default None
+    timeout : float, optional
+        Number of seconds to wait for results to load before timing out, by default _TIMEOUT
+
+    Returns
+    -------
+    dict
+        Nested dictionary of cleaned results
+        
+    """
+    if reqs_to_check is None:
+        reqs_to_check = [reqs_to_clean]
+
+    results = get_results(res_file, reqs_to_clean, t_min=t_min, t_max=t_max, _just_write_script=_just_write_script, timeout=timeout)    
+    time_sig = results.pop('time')
+
+    filtered_results = {}
+    for res_name, res_comps in results.items():
+        filtered_results[res_name] = {}
+        for res_comp, values in res_comps.items():
+
+            cleaned_sig, _, _ = clean_sig(values, 3)
+            
+            filtered_results[res_name][res_comp], _ = low_pass(cleaned_sig, time_sig, freq_cutoff, N=N_filter)
+
+    # Return the cleaned results
+    if return_raw is True:
+        return {'time': time_sig, **filtered_results}, {'time': time_sig, **results}
+    else:
+        return {'time': time_sig, **filtered_results} 
+
+def manually_remove_spikes_batch(res_file, reqs_to_clean, reqs_to_check=None, t_min=None, t_max=None, _just_write_script=False, timeout=_TIMEOUT, _inplace=False):
+    """Similar to `manually_remove_spikes`, but allows user to plot the signals in batches.  Instead of passing a dictionary for the `reqs_to_check` argument, pass a list of dictionaries and the results in each dictionary in the list will be plotted together.
+
+    Parameters
+    ----------
+    res_file : str
+        Adams Results (.res) filename 
+    reqs_to_clean : dict
+        Nested dictionary of result sets and result components to clean
+    reqs_to_check : list of dicts
+        list of nested dictionary of result sets and result components to check for spikes, by default same as reqs_to_clean
+    t_min : float, optional
+        Minumum simulation time to clean, by default None
+    t_max : float, optional
+        Maximum simulation time to clean, by default None
+    timeout : float, optional
+        Number of seconds to wait for results to load before timing out, by default _TIMEOUT
+
+    Returns
+    -------
+    dict
+        Nested dictionary of cleaned results
+        
+    """
+    if reqs_to_check is None:
+        reqs_to_check = [reqs_to_clean]
+
+    results = get_results(res_file, reqs_to_clean, t_min=t_min, t_max=t_max, _just_write_script=_just_write_script, timeout=timeout)
+    
+    time_sig = results['time']
+
+    # Create a flag indicating if the results have been modified and need to be rewritten
+    results_modified = False
+
+    for batch_to_check in reqs_to_check:
+        
+        # Make a list/batch of values to clean
+        values_to_check = []
+        for (res, res_comps) in [(r, rc) for r, rc in results.items() if r in batch_to_check]:
+            for (res_comp, values) in [(rc, v) for rc, v in res_comps.items() if rc in batch_to_check[res]]:
+                
+                values_to_check.append(values)
+
+        _, i_mod = manually_clean_sigs(time_sig, values_to_check, indices=True)
+
+        # If a modification was made to the signal, make that modification to the rest of the signals
+        if i_mod != []:
+            
+            # Loop over all the results
+            for (res, res_comps) in [(r, rc) for r, rc in results.items() if r != 'time']:
+                for (res_comp, values) in res_comps.items():
+                    for i in i_mod:
+                        results[res][res_comp] = remove_data_point(time_sig, values, i)
+
+            # Flag that the results have been modified
+            results_modified = True
+
+    # If the results were modified, update the analysis files
+    if results_modified is True:
+        edit_results(res_file, results)
+
+    # Return the cleaned results
+    return results
+    
+class AviewError(Exception):
+    """Raise this error to if a known error occurs in the log file.
+    
+    """
+    pass
---
+++ packages/adamspy/adamspy-0.29.6.tar.gz/adamspy-0.29.6/test/test_results.py
@@ -0,0 +1,307 @@
+import unittest
+import os
+import shutil
+import glob
+from adamspy.postprocess import xml
+from adamspy.postprocess import launch_ppt
+from adamspy.postprocess import _get_model_name_from_cmd
+from adamspy.postprocess.ppt import get_lunar_results, LUNAR_SCRIPT_NAME, get_results, edit_results
+
+from test import *
+
+import numpy as np
+
+class Test_ShrinkResults(unittest.TestCase):
+
+    def setUp(self):
+        # Delete existing shrunk results file
+        if os.path.exists(TEST_EXISTING_RES_FILE.replace('.res', '') + xml.SHRUNK_RES_SUFFIX +'.res'):
+            os.remove(TEST_EXISTING_RES_FILE.replace('.res', '') + xml.SHRUNK_RES_SUFFIX +'.res')
+        
+        # Create shrunk results file
+        xml.shrink_results(TEST_EXISTING_RES_FILE, TEST_REQUESTS_TO_GET, TEST_REQUEST_TIME_MIN, TEST_REQUEST_TIME_MAX)
+        
+    def test_shrink_results_exist(self):
+        """Tests if the shrunken results file exists
+        
+        """        
+        # Check that the shrunk results file exists
+        results_exist = os.path.exists(TEST_EXISTING_RES_FILE.replace('.res', '') + xml.SHRUNK_RES_SUFFIX +'.res')
+        
+        # Assert that the shrunk results file exists
+        self.assertTrue(results_exist)
+
+    def tearDown(self):
+        # Delete shrunk results file
+        if os.path.exists(TEST_EXISTING_RES_FILE.replace('.res', '') + xml.SHRUNK_RES_SUFFIX +'.res'):
+            os.remove(TEST_EXISTING_RES_FILE.replace('.res', '') + xml.SHRUNK_RES_SUFFIX +'.res')
+
+class Test_GetResultsPPT(unittest.TestCase):
+
+    TEST_REQUEST_FILE = os.path.join(os.getcwd(), 'test', 'files', 'test_request_file.req')
+    def setUp(self):
+        return
+
+    def test_get_request(self):
+        reqs_to_get = {'response_1_disp': ['X', 'MAG', 'R2', 'AMAG']}
+        results = get_results(self.TEST_REQUEST_FILE, reqs_to_get)
+        expected_results = {
+            'time': [13.0, 13.001000000000001, 13.002, 13.003, 13.004, 13.004999999999999, 13.006],
+            'response_1_disp': {
+                'X': [3.5, 3.4999599999999997, 3.4999599999999997, 3.4999599999999997, 3.4999599999999997, 3.4999499999999997, 3.4999499999999997],
+                'MAG': [3.500000000008821, 3.499960000008402, 3.4999600000085374, 3.4999600000085365, 3.499960000008617, 3.4999500000086563, 3.4999500000085777],
+                'R2': [2.3997399999999995e-07, 1.80854e-06, 1.6657699999999998e-06, 1.5456599999999998e-06, 1.44435e-06, 1.3732599999999998e-06, 1.3368999999999999e-06],
+                'AMAG': [38.76307707729096, 88.32065382287432, 85.85459677245011, 84.106517786495, 82.52081848120751, 81.60156028408772, 81.61501704525708]
+                }
+            }
+
+        self.assertDictEqual(results, expected_results)
+
+    def tearDown(self):
+        return
+
+
+class Test_GetResults(unittest.TestCase):
+
+    def setUp(self):
+        # Get results from a results file
+        self.requests, self.units = xml.get_results(TEST_EXISTING_RES_FILE, TEST_REQUESTS_TO_GET, TEST_REQUEST_TIME_MIN, TEST_REQUEST_TIME_MAX, return_units=True)
+
+    def test_get_results_requests(self):       
+        """Tests if `requests` returned by :meth:`xml.get_results` is as expected.
+
+        """
+        self.assertDictEqual(self.requests, TEST_EXPECTED_REQUESTS)
+
+    def test_get_results_units(self):       
+        """Tests if `units` returned by :meth:`xml.get_results` is as expected.
+
+        """
+        self.assertDictEqual(self.units, TEST_EXPECTED_REQUEST_UNITS)
+    
+    def test_get_results_all(self):    
+        _requests, _units = xml.get_results(TEST_EXISTING_RES_FILE, t_min=TEST_REQUEST_TIME_MIN, t_max=TEST_REQUEST_TIME_MAX, return_units=True)
+        
+    def tearDown(self):
+        return
+
+class Test_GetResultsIterparse(unittest.TestCase):
+
+    def setUp(self):
+        # Get results from a results file
+        self.requests, self.units = xml.get_results(TEST_EXISTING_RES_FILE, TEST_REQUESTS_TO_GET, TEST_REQUEST_TIME_MIN, TEST_REQUEST_TIME_MAX, return_units=True, use_iterparse=True)
+
+    def test_get_results_requests(self):       
+        """Tests if `requests` returned by :meth:`xml.get_results` is as expected.
+
+        """
+        self.assertDictEqual(self.requests, TEST_EXPECTED_REQUESTS)
+
+    def test_get_results_units(self):       
+        """Tests if `units` returned by :meth:`xml.get_results` is as expected.
+
+        """
+        self.assertDictEqual(self.units, TEST_EXPECTED_REQUEST_UNITS)
+    
+    def test_get_results_all(self):    
+        _requests, _units = xml.get_results(TEST_EXISTING_RES_FILE, t_min=TEST_REQUEST_TIME_MIN, t_max=TEST_REQUEST_TIME_MAX, return_units=True, use_iterparse=True)
+        
+    def tearDown(self):
+        return
+
+class Test_LaunchPPT(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def test_get_model_name_from_cmd(self):
+        mod_name = _get_model_name_from_cmd(TEST_EXISTING_CMD_FILE)
+        self.assertEqual(mod_name, TEST_EXPECTED_MODNAME_IN_CMD)
+
+    def test_pptAS_exists(self):
+        directory = launch_ppt(TEST_EXISTING_RES_FILE)
+        expected_directory = os.path.split(TEST_EXISTING_RES_FILE)[0]
+        self.assertEqual(directory, expected_directory)
+
+    def tearDown(self):
+        return
+        
+class Test_GetResultsAview(unittest.TestCase):
+
+    def setUp(self):
+        self.res_files = [TEST_EXISTING_GENERIC_RES_FILE]
+        self.output_file = os.path.join(os.path.dirname(TEST_EXISTING_GENERIC_RES_FILE), 'lunar_Y_file.csv')
+        self.reqs_to_get = {'JOINT_1': ['FY']}
+        self.t_min = 10
+        self.t_max = 19.9
+
+
+    def test_python_script_contents(self):
+        """Checks that the python script that gets passed to aview is written correctly.
+
+        """
+        get_lunar_results(self.res_files, self.reqs_to_get, self.t_min, self.t_max, self.output_file, _just_write_script=True)
+        directory = os.path.dirname(self.res_files[0])
+        
+        failures = check_file_contents(os.path.join(directory, LUNAR_SCRIPT_NAME), EXPECTED_LUNAR_PYTHON_SCRIPT.format(**{'reqs_to_get': self.reqs_to_get, 'res_files': self.res_files, 'output_file': self.output_file, 't_min': self.t_min, 't_max': self.t_max}))
+        self.assertListEqual(failures, [])
+
+    def test_lunar_y_file(self):
+        """Tests that the lunar Y file is written correctly.
+
+        """
+        self.res_output_files = get_lunar_results(self.res_files, self.reqs_to_get, self.t_min, self.t_max, self.output_file)
+
+        failures = []
+
+        for res_output_file, expected_contents in zip(self.res_output_files, EXPECTED_LUNAR_Y_FILES):
+            failures.append(check_file_contents(res_output_file, expected_contents))
+
+        self.assertListEqual(failures, [])
+
+    def tearDown(self):
+        for file in self.res_output_files:
+            try:
+                os.remove(file)
+            except FileNotFoundError:
+                pass
+            except PermissionError:
+                pass
+
+
+class Test_EditResults(unittest.TestCase):
+
+    def setUp(self):        
+        self.temp_ans_dir = TEST_EXISTING_GENERIC_ANS_DIR + '_tmp'        
+        self._setup_files()
+        self.res_to_edit = glob.glob(os.path.join(self.temp_ans_dir, '*.res'))[0]
+
+    def test_edit_results(self):
+        """Tests that edit_results works when time is **NOT** included in the reqs_to_edit dictionary.
+
+        """
+        reqs_to_edit = {
+            'JOINT_1': {
+                'FY': list(np.ones(201))
+            }
+        }
+
+        edit_results(self.res_to_edit, reqs_to_edit)
+        self.assertEqual(0,1)
+
+    def test_edit_results_with_time(self):
+        """Tests that edit_results works when time **IS** included in the reqs_to_edit dictionary.
+        
+        """
+        reqs_to_edit = {
+            'TIME': list(np.round(np.arange(5, 10.1, .1), 10)),
+            'JOINT_1': {
+                'FY': list(np.ones(51))
+            }
+        }
+
+        edit_results(self.res_to_edit, reqs_to_edit)
+        self.assertEqual(0,1)
+
+    def tearDown(self):
+        """Deletes the temporary working directory.
+
+        """
+        shutil.rmtree(self.temp_ans_dir)
+    
+    def _setup_files(self):
+        """Copies the test analysis files to a temporary working directory
+
+        """
+        if os.path.exists(self.temp_ans_dir) is True:
+            shutil.rmtree(self.temp_ans_dir)
+
+        os.makedirs(self.temp_ans_dir)
+        
+        for file in glob.glob(os.path.join(TEST_EXISTING_GENERIC_ANS_DIR, '*')):
+            shutil.copyfile(file, os.path.join(self.temp_ans_dir, os.path.split(file)[-1]))   
+
+EXPECTED_LUNAR_Y_FILES = [
+    '''
+''',
+    '''
+'''
+]
+
+#pylint: disable=anomalous-backslash-in-string
+EXPECTED_LUNAR_PYTHON_SCRIPT = '''import os
+
+import numpy as np
+import Adams
+
+def get_lunar_results(res_files, reqs_to_get, t_min, t_max, output_file):
+
+    res_dict = {{}}
+
+    for i_res, res_file in enumerate(res_files):
+        
+        Adams.execute_cmd('def com echo=on')
+
+        # Load Results
+        Adams.execute_cmd(f'file results read file_name="{{res_file}}"')
+
+        # Get the model object
+        mod = Adams.Models.get([mn for mn in Adams.Models][0])
+        
+        # Get the analysis object
+        ans = mod.Analyses.get([an for an in mod.Analyses][0])
+            
+        # Get time and set indices of results to export 
+        time = ans.results.get('TIME').values
+        _time_np = np.asarray(time)
+        i_min = np.argmax(_time_np >= t_min)
+        i_max = np.argmax(_time_np >= t_max)
+
+        if i_res == 0:
+            # Get a name for the output time file
+            time_output_file = os.path.splitext(output_file)[0] + '_time' + os.path.splitext(output_file)[-1]
+
+            # Write the time values
+            with open(time_output_file, 'w') as fid:
+                fid.write(f'{{time}}'.replace('[','').replace(']','').replace(' ',''))
+        
+        # Define first two columns of the output string
+        res_dict[os.path.split(res_file)[-1]] = {{}}
+        
+        for res_name, res_comps in reqs_to_get.items():
+
+            # Get the result object handle
+            res = ans.results.get(res_name)
+
+            for res_comp in res_comps:
+
+                values = res.get(res_comp).values[i_min:i_max]
+
+                full_res_name = f'{{res_name}}_{{res_comp}}'
+
+                # Get a name for the output time file
+                res_output_file = os.path.splitext(output_file)[0] + '_' + full_res_name + os.path.splitext(output_file)[-1]
+
+                with open(res_output_file, 'w' if i_res==0 else 'a') as fid:
+                    fid.write(f'{{values}}\\n'.replace('[','').replace(']','').replace(' ',''))    
+
+        # Delete the model and results
+        mod.destroy()          
+                
+if __name__ == 'aview_main':
+    
+    Adams.execute_cmd('def com echo=on')
+
+    res_files = {res_files}
+    reqs_to_get = {reqs_to_get}
+    t_min = {t_min}
+    t_max = {t_max}
+    output_suffix = r'{output_file}'
+
+    get_lunar_results(res_files, reqs_to_get, t_min, t_max, output_suffix)
+    
+    print('MESSAGE:  COMPLETE')
+    sys.stdout.flush()
+    Adams.execute_cmd('quit conf=no')
+
+'''
