ef5c2cc92e89ab96bae99f09e7c54fbc558b3da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:57 +0000

    Add package ryglfg/2.0.0/ryglfg-2.0.0-py3-none-any.whl

---
+++ packages/ryglfg/ryglfg-2.0.0-py3-none-any.whl/ryglfg/auth.py
@@ -0,0 +1,42 @@
+# Module docstring
+"""
+
+"""
+
+# Special imports
+from __future__ import annotations
+import royalnet.royaltyping as t
+
+# External imports
+import logging
+import pydantic as p
+import fastapi_cloudauth.auth0 as faca
+
+# Internal imports
+# from . import something
+
+# Special global objects
+log = logging.getLogger(__name__)
+
+
+# Code
+class Auth0CustomClaims(p.BaseModel):
+    iss: str
+    sub: str
+    aud: t.Union[t.List[str], str]
+    iat: int
+    exp: int
+    azp: str
+    scope: str
+    permissions: t.List[str]
+
+
+class Auth0CustomUser(faca.Auth0CurrentUser):
+    user_info = Auth0CustomClaims
+
+
+# Objects exported by this module
+__all__ = (
+    "Auth0CustomClaims",
+    "Auth0CustomUser",
+)
