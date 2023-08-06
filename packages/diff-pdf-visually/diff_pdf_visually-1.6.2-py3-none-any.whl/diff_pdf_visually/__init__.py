8f494349b32c4ccfbbb916cc8e4b515e0511a5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:11 +0000

    Add package diff-pdf-visually/1.6.2/diff_pdf_visually-1.6.2-py3-none-any.whl

---
+++ packages/diff-pdf-visually/diff_pdf_visually-1.6.2-py3-none-any.whl/diff_pdf_visually/__init__.py
@@ -0,0 +1,12 @@
+"""
+Quickly check whether there is a visible difference between two PDFs, using ImageMagick and pdftocairo.
+"""
+
+__version__ = '1.6.2'
+
+from typing import List
+
+__all__ = [] # type: List[str]
+
+from .diff import pdfdiff, pdfdiff_pages
+from .diff import pdftopng, imgdiff
