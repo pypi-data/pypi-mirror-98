f36aeb434da78ce08acfd1ec9e27936431ee6dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:34 +0000

    Add package Ga-Bi-Distributions/1.1/Ga-Bi-Distributions-1.1.tar.gz

---
+++ packages/Ga-Bi-Distributions/Ga-Bi-Distributions-1.1.tar.gz/Ga-Bi-Distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Ga-Bi-Distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Ga-Bi-Distributions'],
+      author = 'Moustafa Ashraf',
+      author_email = 'mosafehashf@gmail.com',
+      zip_safe=False)
