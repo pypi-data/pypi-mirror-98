9044d1d3dfd9262360c9574057994bd8d6e46345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:19 +0000

    Add package newton-polynomial/1.0.0/newton_polynomial-1.0.0-py3-none-any.whl

---
+++ packages/newton-polynomial/newton_polynomial-1.0.0-py3-none-any.whl/newton_polynomial/__init__.py
@@ -0,0 +1,34 @@
+from numpy import diagonal
+from numpy import zeros
+from sympy import simplify
+from sympy import symbols
+
+
+def coefficients(x, y):
+    assert len(x) == len(y), "arguments must have the same size"
+    size = len(x)
+    result = zeros((size, size))
+    result[:, 0] = y
+    for column in range(1, size):
+        result[column:, column] = (
+            (result[column:, column-1] - result[column-1:-1, column-1])
+            / (x[column:] - x[:-column])
+        )
+    return diagonal(result)
+
+
+def expression(a, x):
+    assert len(a) == len(x), "arguments must have the same size"
+    size = len(a)
+
+    x_ = symbols('x')
+    result = [a[0] for i in range(size)]
+    for i, x_i in enumerate(x[:-1], start=1):
+        result[i] = a[i] / (a[i-1] or 1) * result[i-1] * (x_ - x_i)
+
+    return simplify(sum(result))
+
+
+def polynomial(x, y):
+    a = coefficients(x, y)
+    return expression(a, x)
