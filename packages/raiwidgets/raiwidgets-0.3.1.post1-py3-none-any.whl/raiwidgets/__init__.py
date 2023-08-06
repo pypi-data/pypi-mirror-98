b6a671264bc42988a6ab02fe84b458695e53c245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:13 +0000

    Add package raiwidgets/0.3.1.post1/raiwidgets-0.3.1.post1-py3-none-any.whl

---
+++ packages/raiwidgets/raiwidgets-0.3.1.post1-py3-none-any.whl/raiwidgets/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+"""Package for the fairness, explanation, and error analysis widgets."""
+
+from .fairness_dashboard import FairnessDashboard
+from .explanation_dashboard import ExplanationDashboard
+from .error_analysis_dashboard import ErrorAnalysisDashboard
+from .model_performance_dashboard import ModelPerformanceDashboard
+from .__version__ import version
+
+__version__ = version
+
+__all__ = ['FairnessDashboard', 'ExplanationDashboard',
+           'ErrorAnalysisDashboard', 'ModelPerformanceDashboard']
