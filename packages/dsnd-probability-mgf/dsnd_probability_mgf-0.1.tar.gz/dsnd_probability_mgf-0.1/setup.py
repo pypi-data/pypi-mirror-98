644c1a526b5f036f61d74ffc801c82774317bcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:14 +0000

    Add package dsnd-probability-mgf/0.1/dsnd_probability_mgf-0.1.tar.gz

---
+++ packages/dsnd-probability-mgf/dsnd_probability_mgf-0.1.tar.gz/dsnd_probability_mgf-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_mgf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_mgf'],
+      zip_safe=False)
