971f6dfaf6b9f78831608ae7138dae72872cb3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:24 +0000

    Add package cloudcomponents.cdk-dynamodb-seeder/1.29.0/cloudcomponents.cdk-dynamodb-seeder-1.29.0.tar.gz

---
+++ packages/cloudcomponents.cdk-dynamodb-seeder/cloudcomponents.cdk-dynamodb-seeder-1.29.0.tar.gz/cloudcomponents.cdk-dynamodb-seeder-1.29.0/setup.py
@@ -0,0 +1,68 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-dynamodb-seeder",
+    "version": "1.29.0",
+    "description": "A seeder for dynamodb tables",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_dynamodb_seeder",
+        "cloudcomponents.cdk_dynamodb_seeder._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_dynamodb_seeder._jsii": [
+            "cdk-dynamodb-seeder@1.29.0.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_dynamodb_seeder": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-dynamodb>=1.93.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.93.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.93.0, <2.0.0",
+        "aws-cdk.aws-s3-assets>=1.93.0, <2.0.0",
+        "aws-cdk.aws-s3>=1.93.0, <2.0.0",
+        "aws-cdk.core>=1.93.0, <2.0.0",
+        "constructs>=3.2.0, <4.0.0",
+        "jsii>=1.24.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
---
+++ packages/cloudcomponents.cdk-dynamodb-seeder/cloudcomponents.cdk-dynamodb-seeder-1.29.0.tar.gz/cloudcomponents.cdk-dynamodb-seeder-1.29.0/src/cloudcomponents/cdk_dynamodb_seeder/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-dynamodb-seeder",
+    "1.29.0",
+    __name__[0:-6],
+    "cdk-dynamodb-seeder@1.29.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
