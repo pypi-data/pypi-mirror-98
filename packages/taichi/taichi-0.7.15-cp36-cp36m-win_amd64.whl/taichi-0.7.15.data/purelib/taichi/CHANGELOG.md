c7623a1c1df6d83b9873cf6947d64f6f67aecc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:23 +0000

    Add package taichi/0.7.15/taichi-0.7.15-cp36-cp36m-win_amd64.whl

---
+++ packages/taichi/taichi-0.7.15-cp36-cp36m-win_amd64.whl/taichi-0.7.15.data/purelib/taichi/CHANGELOG.md
@@ -0,0 +1,5 @@
+Highlights:
+
+Full changelog:
+   - [test] [ir] Add a simple unit test for IRBuilder (#2214) (by **Ye Kuang**)
+   - [ir] Add a constructor for the Kernel class to handle CHI IR (#2213) (by **xumingkuan**)
---
+++ packages/taichi/taichi-0.7.15-cp36-cp36m-win_amd64.whl/taichi-0.7.15.data/purelib/taichi/lang/kernel_arguments.py
@@ -0,0 +1,54 @@
+from .core import taichi_lang_core
+from .expr import Expr
+import numpy as np
+from .util import *
+
+
+class ArgExtArray:
+    def __init__(self, dim=1):
+        assert dim == 1
+
+    def extract(self, x):
+        return to_taichi_type(x.dtype), len(x.shape)
+
+
+ext_arr = ArgExtArray
+
+
+class Template:
+    def __init__(self, tensor=None, dim=None):
+        self.tensor = tensor
+        self.dim = dim
+
+    def extract(self, x):
+        import taichi as ti
+        if isinstance(x, ti.SNode):
+            return x.ptr
+        if isinstance(x, ti.Expr):
+            return x.ptr.get_underlying_ptr_address()
+        if isinstance(x, ti.core.Expr):
+            return x.get_underlying_ptr_address()
+        if isinstance(x, tuple):
+            return tuple(self.extract(item) for item in x)
+        return x
+
+
+template = Template
+
+
+def decl_scalar_arg(dtype):
+    dtype = cook_dtype(dtype)
+    id = taichi_lang_core.decl_arg(dtype, False)
+    return Expr(taichi_lang_core.make_arg_load_expr(id, dtype))
+
+
+def decl_ext_arr_arg(dtype, dim):
+    dtype = cook_dtype(dtype)
+    id = taichi_lang_core.decl_arg(dtype, True)
+    return Expr(taichi_lang_core.make_external_tensor_expr(dtype, dim, id))
+
+
+def decl_scalar_ret(dtype):
+    dtype = cook_dtype(dtype)
+    id = taichi_lang_core.decl_ret(dtype)
+    return id
