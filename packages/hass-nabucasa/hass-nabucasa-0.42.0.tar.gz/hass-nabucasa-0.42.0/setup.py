96737673ecd8831a2bbd807c10f6d8686e8544d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package hass-nabucasa/0.42.0/hass-nabucasa-0.42.0.tar.gz

---
+++ packages/hass-nabucasa/hass-nabucasa-0.42.0.tar.gz/hass-nabucasa-0.42.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+VERSION = "0.42.0"
+
+setup(
+    name="hass-nabucasa",
+    version=VERSION,
+    license="GPL v3",
+    author="Nabu Casa, Inc.",
+    author_email="opensource@nabucasa.com",
+    url="https://www.nabucasa.com/",
+    download_url="https://github.com/NabuCasa/hass-nabucasa/tarball/{}".format(VERSION),
+    description=("Home Assistant cloud integration by Nabu Casa, Inc."),
+    long_description=(""),
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: Proxy Servers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords=["homeassistant", "cloud"],
+    zip_safe=False,
+    platforms="any",
+    packages=["hass_nabucasa"],
+    install_requires=[
+        "pycognito==0.1.5",
+        "snitun==0.20",
+        "acme==1.12.0",
+        "cryptography>=2.8,<4.0",
+        "attrs>=19.3",
+        "pytz>=2019.3",
+        "aiohttp>=3.6.1",
+        "atomicwrites==1.4.0",
+    ],
+)
