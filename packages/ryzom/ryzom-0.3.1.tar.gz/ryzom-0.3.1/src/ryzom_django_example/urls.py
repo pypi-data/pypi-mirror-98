aef9eecf71feb124fa0412780c59ff4adc8d9d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:42 +0000

    Add package ryzom/0.3.1/ryzom-0.3.1.tar.gz

---
+++ packages/ryzom/ryzom-0.3.1.tar.gz/ryzom-0.3.1/src/ryzom_django_example/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('ryzom_django_example.views')),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path('bundles/', include('ryzom_django.bundle')),
+    )
+
+if settings.CHANNELS_ENABLE:
+    urlpatterns.append(
+        path('reactive/', include('ryzom_django_channels_example.views')),
+    )
