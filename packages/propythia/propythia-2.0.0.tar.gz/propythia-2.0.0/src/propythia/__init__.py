a215cbf637dc7d7d285e7f9c96f8de5f2eed1992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package propythia/2.0.0/propythia-2.0.0.tar.gz

---
+++ packages/propythia/propythia-2.0.0.tar.gz/propythia-2.0.0/src/propythia/__init__.py
@@ -0,0 +1,5 @@
+from . import sequence, descriptors, preprocess, clustering, manifold, linear_dim_reduction, feature_selection, \
+    shallow_ml, deep_ml, param_optimizer
+
+
+from . import adjuv_functions
