8c9928a50fe8ced60ae987e00d29aa4bc5a36a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:14 +0000

    Add package py2k/1.7.0/py2k-1.7.0.tar.gz

---
+++ packages/py2k/py2k-1.7.0.tar.gz/py2k-1.7.0/py2k/creators.py
@@ -0,0 +1,156 @@
+# Copyright 2021 ABSA Group Limited
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC
+from datetime import date
+from decimal import Decimal
+from typing import Any, Dict, List, Optional
+
+import pandas as pd
+from pydantic import Field, create_model, BaseModel
+
+
+class ModelCreator(ABC):
+    def create(self):
+        ...
+
+
+class PandasModelCreator(ModelCreator):
+    """Dynamically converts a Pandas dataframe to a Pydantic model.
+
+    The fields and types defaults can be used together, i.e. it is possible
+        to specify some defaults by field name and some defaults by type.
+
+    Currently, only strings, integers and floats are accepted.
+
+    If, for a given field, default values are not found either for its name
+        nor for its type, a default per type will be used.
+
+    The defaults per type are:
+        - str = ""
+        - int = -1
+        - float = -1.0
+        - Decimal not directly supported, will be converted to float,
+            so the default same as float (always)
+        - bool = False
+        - date = date.min
+        - pd.Timestamp = pd.Timestamp.min
+            Use when you need to set default datetime
+
+    They can be found in KafkaModel._SCHEMA_TYPES_DEFAULTS
+    """
+
+    _SCHEMA_TYPES_DEFAULTS = {
+        str: "",
+        int: -1,
+        float: -1.0,
+        bool: False,
+        Decimal: -1.0,
+        date: date.min,
+        pd.Timestamp: pd.Timestamp.min,
+    }
+
+    def __init__(self,
+                 df: pd.DataFrame,
+                 model_name: str,
+                 fields_defaults: Dict[str, object] = None,
+                 types_defaults: Dict[object, object] = None,
+                 optional_fields: List[str] = None,
+                 base: type = BaseModel):
+        """
+        Args:
+            df (pd.DataFrame): Pandas dataframe
+            model_name (str): destination Pydantic model
+            fields_defaults (Dict[str, object], optional): default values for
+                 fields in the dataframe. The keys are the fields names.
+                 Defaults to None.
+            types_defaults (Dict[object, object], optional): default values
+                 for the types in the dataframe. The keys are the types,
+                 e.g. int. Defaults to None.
+            optional_fields (List[str], optional): list of fields which should
+                 be marked as optional. Defaults to None.
+            base (type, optional): Pydantic model to create.
+                 Defaults to BaseModel.
+        """
+        self._df = df
+        self._model_name = model_name
+        self._fields_defaults = fields_defaults
+        self._types_defaults = types_defaults
+        self._optional_fields = optional_fields
+        self._base = base
+
+    def create(self):
+        records_as_dict_list = self._df.to_dict('records')
+        if records_as_dict_list:
+            sample_record = records_as_dict_list[0]
+            model = self._create_model(sample_record)
+            return model
+        else:
+            raise ValueError(
+                "Unable to create kafka model from an empty dataframe.")
+
+    def _create_model(self, record):
+        def field_definition(field_name: str, field_value: Any):
+            """Creates a field definition containing its
+                type, title and default value.
+            Args:
+                field_name ([str]): field name
+                field_value ([Any]): field value
+
+            Raises:
+                ValueError: if schema types aren't supported
+
+            Returns:
+                [Tuple like]: field type, Field(title, default value)
+            """
+            value_type = type(field_value)
+
+            if self._fields_defaults and field_name in self._fields_defaults:
+                # if there is a value_default for the field, use it
+                value_default = self._fields_defaults.get(field_name)
+            else:
+                # if there is preferred value_default for the type, use it
+                if self._types_defaults and value_type in self._types_defaults:
+                    value_default = self._types_defaults.get(value_type)
+                    # otherwise, use the value_default for the type
+                elif value_type in self._SCHEMA_TYPES_DEFAULTS:
+                    value_default = self._SCHEMA_TYPES_DEFAULTS.get(value_type)
+                else:
+
+                    message = f"""
+                        Invalid type for field '{field_name}': '{value_type}'.
+                        Supported types: '{self._SCHEMA_TYPES_DEFAULTS.keys()}'
+                    """
+                    raise ValueError(message)
+
+            # keep the title the same as the field name since the title is
+            # the value used when generating the Json schema from the record,
+            # which is also used to generate the Avro schema.
+            #
+            # If this is not done, the title will be different
+            # from the field name when the Avro serializer is producing
+            # the records, because the title uses 'snake case', therefore,
+            # there will be no matching between the fields
+            # which will take the serializer to assume the default values only
+            if self._optional_fields and field_name in self._optional_fields:
+                value_type = Optional[value_type]
+
+            return value_type, Field(title=field_name, default=value_default)
+
+        if self._types_defaults and self._types_defaults.get(float):
+            self._types_defaults[Decimal] = self._types_defaults.get(float)
+
+        template = {name: field_definition(
+            name, value) for name, value in record.items()}
+        return create_model(self._model_name, **template, __base__=self._base)
---
+++ packages/py2k/py2k-1.7.0.tar.gz/py2k-1.7.0/py2k/models.py
@@ -0,0 +1,131 @@
+# Copyright 2021 ABSA Group Limited
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import itertools
+import warnings
+from typing import Any, Dict, List, Type
+
+import pandas as pd
+from pydantic import BaseModel
+
+from py2k.creators import PandasModelCreator
+from py2k.utils import (process_properties,
+                        update_optional_schema)
+
+
+class IterableAdapter:
+    def __init__(self, iterator_factory):
+        self.iterator_factory = iterator_factory
+
+    def __iter__(self):
+        return self.iterator_factory()
+
+
+class KafkaModel(BaseModel):
+
+    @classmethod
+    def from_pandas(cls, df: pd.DataFrame) -> List['KafkaModel']:
+        records = df.to_dict('records')
+
+        if records:
+            return [cls(**item) for item in records]
+
+        warnings.warn(
+            "Unable to create kafka model from an empty dataframe.")
+        return []
+
+    @classmethod
+    def iter_from_pandas(cls, df: pd.DataFrame):
+        def iter_pandas(cls, df: pd.DataFrame):
+            for item in df.to_dict('records'):
+                yield cls(**item)
+        return IterableAdapter(lambda: iter_pandas(cls, df))
+
+    class Config:
+        json_encoders = {
+            datetime.date: lambda v: str(v),
+            datetime.datetime: lambda v: str(v),
+        }
+
+        @classmethod
+        def alias_generator(cls, string: str) -> str:
+            # this is the same as `alias_generator = to_camel` above
+            return string
+            # return ''.join(word.capitalize() for word in string.split('_'))
+
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any],
+                         model: Type['KafkaModel']) -> None:
+            schema['type'] = 'record'
+            schema['name'] = schema.pop('title')
+            schema['namespace'] = (f'python.kafka.'
+                                   f'{schema["name"].lower()}')
+            schema = process_properties(schema)
+            schema.pop('properties')
+
+            # Dynamically generated schemas might not have this field,
+            # which is removed anyway.
+            if 'required' in schema:
+                schema.pop('required')
+            update_optional_schema(schema=schema, model=model)
+
+    @staticmethod
+    def schema_from_iter(iterator: IterableAdapter):
+        return list(itertools.islice(iterator, 1))[0].schema_json()
+
+
+class DynamicKafkaModel:
+    """ class model for automatic serialization of Pandas DataFrame to
+    KafkaModel
+    """
+
+    def __init__(self, df: pd.DataFrame, model_name: str,
+                 fields_defaults: Dict[str, object] = None,
+                 types_defaults: Dict[object, object] = None,
+                 optional_fields: List[str] = None):
+        """
+        Args:
+            df (pd.DataFrame): Pandas dataframe to serialize
+            model_name (str): destination Pydantic model
+            fields_defaults (Dict[str, object], optional): default values for
+                 fields in the dataframe. The keys are the fields names.
+                 Defaults to None.
+            types_defaults (Dict[object, object], optional): default values
+                 for the types in the dataframe. The keys are the types,
+                 e.g. int. Defaults to None.
+            optional_fields (List[str], optional): list of fields which should
+                 be marked as optional. Defaults to None.
+        """
+        self._df = df
+
+        model_creator = PandasModelCreator(df, model_name, fields_defaults,
+                                           types_defaults, optional_fields,
+                                           KafkaModel)
+
+        self._model = model_creator.create()
+
+    def from_pandas(self, df: pd.DataFrame = None) -> List['KafkaModel']:
+        """create list of KafkaModel objects from a pandas DataFrame
+
+        Args:
+            df (pd.DataFrame): Pandas dataframe. Defaults to None.
+
+        Returns:
+            [List[KafkaModel]]: serialized list of KafkaModel objects
+        """
+        if df is not None:
+            return self._model.from_pandas(df)
+
+        return self._model.from_pandas(self._df)
