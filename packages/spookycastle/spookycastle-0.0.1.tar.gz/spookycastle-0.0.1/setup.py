6fb519911d8b67353bcfb79f68e002a6e874390d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package spookycastle/0.0.1/spookycastle-0.0.1.tar.gz

---
+++ packages/spookycastle/spookycastle-0.0.1.tar.gz/spookycastle-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__project__ = "spookycastle"
+__version__ = "0.0.1"
+__description__ = "A role playing game made using object-oriented in Python"
+__packages__ = ["spookycastle"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+)
