1081e69ab0f6da05381cc08fcc4e1d7e34fff628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package vplotter/1.2.4/vplotter-1.2.4-py3-none-any.whl

---
+++ packages/vplotter/vplotter-1.2.4-py3-none-any.whl/vplotter/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from .vplotter import Plotter
+
+from setuptools import setup, find_packages
+
+__all__ = (
+    '__version__',
+    'Plotter'
+)
+
+import pkg_resources
+__version__           = pkg_resources.get_distribution("Plotter").version
+
+__short_description__ = "Plotter class for any purpose, based on Veusz."
+__license__           = "MIT"
+__author__            = "Alexander D. Kazakov | Varvara M. Prokacheva"
+__author_email__      = "alexander.d.kazakov@gmail.com"
+__github_username__   = "AlexanderDKazakov"
