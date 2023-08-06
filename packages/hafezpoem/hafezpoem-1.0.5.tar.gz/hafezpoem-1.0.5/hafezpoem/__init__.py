a76e0f97dd23a140afbc8282bb2c953b7176edea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:52 +0000

    Add package hafezpoem/1.0.5/hafezpoem-1.0.5.tar.gz

---
+++ packages/hafezpoem/hafezpoem-1.0.5.tar.gz/hafezpoem-1.0.5/hafezpoem/__init__.py
@@ -0,0 +1,17 @@
+import csv
+import random
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+def get_random_fal():
+    with open('poems.csv', mode='r', encoding="utf8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        rnd = random.randrange(0, 495)
+        for row in csv_reader:
+            if rnd == line_count:
+                return row["poem"]
+            line_count += 1
