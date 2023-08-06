5e37deb552f0e4c5757786687552c994f38a7ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:14 +0000

    Add package nav-dcat/0.1.0/nav_dcat-0.1.0-py3-none-any.whl

---
+++ packages/nav-dcat/nav_dcat-0.1.0-py3-none-any.whl/nav_dcat/models.py
@@ -0,0 +1,24 @@
+from typing import Optional, Sequence
+from datetime import date
+from pydantic import BaseModel
+
+from nav_dcat import doc_fields
+
+
+class ESDocument(BaseModel):
+    id: str
+    title: str
+    description: str
+    type: doc_fields.Type
+    uri: str
+    format: Optional[doc_fields.Type]
+    accessRights: Optional[doc_fields.AccessRights] = doc_fields.AccessRights.INTERNAL.value
+    pii: Optional[doc_fields.Pii] = doc_fields.Pii.FALSE.value
+    keyword: Optional[Sequence] = []
+    theme: Optional[str] = ""
+    temporal: Optional[dict] = {"from": f"{date.today().year}", "to": f"{date.today().year}"}
+    language: Optional[str] = "NO"
+    creator: Optional[dict] = {}
+    publisher: Optional[dict] = {"name": "NAV"}
+    license: Optional[dict] = {}
+    rights: Optional[str] = f"Copyright {date.today().year}, NAV"
