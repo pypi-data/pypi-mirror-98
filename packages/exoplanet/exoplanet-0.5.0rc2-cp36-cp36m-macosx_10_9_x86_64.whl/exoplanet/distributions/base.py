44712a794c266a7b49423387729c505c9216f7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:34 +0000

    Add package exoplanet/0.5.0rc2/exoplanet-0.5.0rc2-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/exoplanet/exoplanet-0.5.0rc2-cp36-cp36m-macosx_10_9_x86_64.whl/exoplanet/distributions/base.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["UnitUniform", "UnitVector", "UnitDisk", "Angle", "Periodic"]
+
+from pymc3_ext.distributions import (
+    Angle,
+    Periodic,
+    UnitDisk,
+    UnitUniform,
+    UnitVector,
+)
