1fa3704c8390e002256e492863f5b643e70f3639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:40 +0000

    Add package gambaterm/0.9.4/gambaterm-0.9.4.tar.gz

---
+++ packages/gambaterm/gambaterm-0.9.4.tar.gz/gambaterm-0.9.4/setup.py
@@ -0,0 +1,92 @@
+import os
+import glob
+from pathlib import Path
+from setuptools import Extension, setup
+
+# Read the contents of the README file
+LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
+
+# List libgambatte sources, excluding `file_zip.cpp`
+libgambatte_sources = [
+    path
+    for path in glob.glob("libgambatte/**/*.cpp", recursive=True)
+    if not path.endswith("file_zip.cpp")
+]
+
+# List all directories containing `.h` files
+libgambatte_include_dirs = list(
+    set(
+        os.path.dirname(path)
+        for path in glob.glob("libgambatte/**/*.h", recursive=True)
+    )
+)
+
+
+# Defer call to `numpy.get_include`
+class NumpyIncludePath(os.PathLike):
+    def __str__(self):
+        return self.__fspath__()
+
+    def __fspath__(self):
+        import numpy
+
+        return os.fspath(numpy.get_include())
+
+
+# The gambatte extension, including libgambatte with the Cython wrapper
+gambatte_extension = Extension(
+    "gambaterm.libgambatte",
+    language="c++",
+    include_dirs=libgambatte_include_dirs + [NumpyIncludePath()],
+    extra_compile_args=["-DHAVE_STDINT_H"],
+    sources=libgambatte_sources + ["libgambatte_ext/libgambatte.pyx"],
+)
+
+
+# The termblit extension
+termblit_extension = Extension(
+    "gambaterm.termblit",
+    language="c",
+    include_dirs=[NumpyIncludePath()],
+    sources=["termblit_ext/termblit.pyx"],
+)
+
+setup(
+    name="gambaterm",
+    version="0.9.4",
+    packages=["gambaterm"],
+    setup_requires=["setuptools>=42", "Cython>=0.29.13", "numpy"],
+    ext_modules=[gambatte_extension, termblit_extension],
+    install_requires=[
+        "numpy>=1",
+        "asyncssh>=2",
+        "prompt_toolkit>=3",
+        "sounddevice",
+        "samplerate",
+        "xlib; sys_platform == 'linux'",
+        "pynput; sys_platform != 'linux'",
+        "pygame>=1.9.5",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "gambaterm = gambaterm:main",
+            "gambaterm-ssh = gambaterm.ssh:main",
+        ],
+    },
+    description="A terminal frontend for gambatte game boy color emulator ",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vxgmichel/gambatte-terminal",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    author="Vincent Michel",
+    author_email="vxgmichel@gmail.com",
+)
---
+++ packages/gambaterm/gambaterm-0.9.4.tar.gz/gambaterm-0.9.4/termblit_ext/termblit.pyx
@@ -0,0 +1,232 @@
+cimport numpy as np
+from libc.stdio cimport sprintf
+from libc.stdlib cimport malloc, free
+
+cdef char* move_absolute(char* buff, int x, int y):
+    buff += sprintf(buff, "\033[%d;%dH", x, y)
+    return buff
+
+
+cdef char* move_relative(char* buff, int dx, int dy):
+    # Vertical move
+    if dx < -1:
+        buff += sprintf(buff, "\033[%dA", -dx)
+    elif dx == -1:
+        buff += sprintf(buff, "\033[A")
+    elif dx == 1:
+        buff += sprintf(buff, "\033[B")
+    elif dx > 1:
+        buff += sprintf(buff, "\033[%dB", dx)
+    # Horizontal move
+    if dy < -1:
+        buff += sprintf(buff, "\033[%dD", -dy)
+    elif dy == -1:
+        buff += sprintf(buff, "\033[D")
+    elif dy == 1:
+        buff += sprintf(buff, "\033[C")
+    elif dy > 1:
+        buff += sprintf(buff, "\033[%dC", dy)
+    return buff
+
+
+
+cdef int scale_256_to_6_shift(int x):
+    x >>= 5
+    x -= x > 0
+    x -= x > 1
+    return x
+
+
+cdef int scale_256_to_6_closest(int x):
+    if x < 48:
+        return 0
+    if x < 115:
+        return 1
+    return (x - 35) // 40
+
+cdef int scale_256_to_6_spread(int x):
+    return x // 43
+
+
+cdef int scale_rgb_to_16_colors(int r, int g, int b):
+    r >>= 6
+    g >>= 6
+    b >>= 6
+    # Dark grey
+    if r == g == b == 1:
+        return 90
+    # Light grey
+    if r == g == b == 2:
+        return 37
+    # Standard colors
+    if r < 2 and g < 2 and b < 2:
+        return 30 + (b << 2 | g << 1 | r)
+    # Lower resolution
+    r >>= 1
+    g >>= 1
+    b >>= 1
+    # Bright colors
+    return 90 + (b << 2 | g << 1 | r)
+
+
+cdef int scale_rgb_to_4_colors(int r, int g, int b):
+    # Square the values
+    r *= r
+    g *= g
+    b *= b
+    # Divide the values by 8
+    r >>= 3
+    g >>= 3
+    b >>= 3
+    # Apply coefficients
+    cdef int l = 2 * r + 5 * g + b
+    # Black color
+    if l <= (64 - 40) ** 2:
+        return 30
+    # Dark grey color
+    if l <= (128 - 64) ** 2:
+        return 90
+    # Light grey color
+    if l <= (64 + 128 - 42) ** 2:
+        return 37
+    # White color
+    return 97
+
+cdef char* set_color(char* buff, int n, int color_mode, int foreground):
+    cdef int c
+    # Extract RGB components
+    cdef int b = n & 0xff
+    cdef int g = (n >> 8) & 0xff
+    cdef int r = (n >> 16) & 0xff
+    # Standard colors
+    if color_mode <= 2:
+        if color_mode == 1:
+            c = scale_rgb_to_4_colors(r, g, b)
+        elif color_mode == 2:
+            c = scale_rgb_to_16_colors(r, g, b)
+        if not foreground:
+            c += 10
+        buff += sprintf(buff, "\033[%dm", c)
+    # 256 colors
+    elif color_mode == 3:
+        b = scale_256_to_6_shift(b)
+        g = scale_256_to_6_shift(g)
+        r = scale_256_to_6_shift(r)
+        c = 16 + 36 * r + 6 * g + b
+        if foreground:
+            buff += sprintf(buff, "\033[38;5;%dm", c)
+        else:
+            buff += sprintf(buff, "\033[48;5;%dm", c)
+    # True colors
+    elif color_mode == 4:
+        if foreground:
+            buff += sprintf(buff, "\033[38;2;%d;%d;%dm", r, g, b)
+        else:
+            buff += sprintf(buff, "\033[48;2;%d;%d;%dm", r, g, b)
+    return buff
+
+
+cdef char* set_background(char* buff, int n, int color_mode):
+    return set_color(buff, n, color_mode, False)
+
+
+cdef char* set_foreground(char* buff, int n, int color_mode):
+    return set_color(buff, n, color_mode, True)
+
+
+cdef char* move_from_to(
+    char *buff, int from_x, int from_y, int to_x, int to_y
+):
+    return move_relative(buff, to_x - from_x, to_y - from_y)
+
+
+def blit(
+    np.ndarray[np.int32_t, ndim=2] image,
+    np.ndarray[np.int32_t, ndim=2] last,
+    int refx, int refy, int width, int height,
+    int color_mode,
+):
+
+    cdef int current_x = refx
+    cdef int current_y = refy
+    cdef int current_fg = -1
+    cdef int current_bg = -2
+    cdef int row_index, column_index
+    cdef int color1, color2
+    cdef int new_x, new_y
+    cdef int invert_print
+    cdef int image_height = image.shape[0]
+    cdef int image_width = image.shape[1]
+    cdef char* base = <char *> malloc(image_height * image_width * 30)
+    cdef char* result = base
+
+    # Move at reference point
+    result = move_absolute(result, refx, refy)
+
+    # Loop over terminal cells
+    for row_index in range(min(height - refx, image_height // 2)):
+        for column_index in range(min(width - refy, image_width)):
+
+            # Extract colors
+            color1 = image[2 * row_index + 0, column_index]
+            color2 = image[2 * row_index + 1, column_index]
+
+            # Skip if identical to last printed frame
+            if (
+                last is not None and
+                last[2 * row_index + 0, column_index] == color1 and
+                last[2 * row_index + 1, column_index] == color2
+            ):
+                continue
+
+            # Go to the new position
+            new_x, new_y = row_index + refx, column_index + refy
+            result = move_from_to(result, current_x, current_y, new_x, new_y)
+            current_x, current_y = new_x, new_y
+
+            # Print full block
+            if color1 == color2 == current_fg != current_bg:
+                result += sprintf(result, "\xe2\x96\x88")
+                current_y += 1
+                continue
+
+            # Print empty block (space)
+            if color1 == color2:
+                if color1 != current_bg:
+                    result = set_background(result, color1, color_mode)
+                    current_bg = color1
+                result += sprintf(result, " ")
+                current_y += 1
+                continue
+
+            # Detect print type
+            invert_print = (current_fg == color2 or current_bg == color1)
+
+            # Inverted print
+            if invert_print:
+                color1, color2 = color2, color1
+
+            # Set background and foreground colors if necessary
+            if current_fg != color1:
+                result = set_foreground(result, color1, color_mode)
+                current_fg = color1
+            if current_bg != color2:
+                result = set_background(result, color2, color_mode)
+                current_bg = color2
+
+            # Print lower half block
+            if invert_print:
+                result += sprintf(result, "\xe2\x96\x84")
+                current_y += 1
+
+            # Print upper half block
+            else:
+                result += sprintf(result, "\xe2\x96\x80")
+                current_y += 1
+
+    # Reset attributes before returning the buffer
+    result += sprintf(result, "\033[0m")
+    try:
+        return base[:result-base]
+    finally:
+        free(base)
