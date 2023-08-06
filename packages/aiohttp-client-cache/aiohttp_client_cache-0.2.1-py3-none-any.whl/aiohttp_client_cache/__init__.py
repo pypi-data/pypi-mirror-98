6faf445de61db933e18279f9a17c870f123ca5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:05 +0000

    Add package aiohttp-client-cache/0.2.1/aiohttp_client_cache-0.2.1-py3-none-any.whl

---
+++ packages/aiohttp-client-cache/aiohttp_client_cache-0.2.1-py3-none-any.whl/aiohttp_client_cache/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.2.1'
+
+try:
+    from aiohttp_client_cache.backends import *  # noqa
+    from aiohttp_client_cache.response import CachedResponse  # noqa
+    from aiohttp_client_cache.session import CachedSession  # noqa
+# When running setup.py outside a virtualenv
+except ImportError:
+    pass
