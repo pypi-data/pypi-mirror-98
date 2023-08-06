b003ebe078bb3274d3f141ee7c0222f19f04e8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:03 +0000

    Add package ezst/0.2.5/ezst-0.2.5-py3-none-any.whl

---
+++ packages/ezst/ezst-0.2.5-py3-none-any.whl/ezst/ai/cnn.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import numpy as np
+
+
+class CNN:
+  pass
+
+cnn = CNN()
+
+def init():
+  cnn.model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    # tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+
+  cnn.model.compile(optimizer=RMSprop(lr=0.001),
+              loss='categorical_crossentropy',
+              metrics = ['accuracy'])
+
+  print('CNN 모델 설정이 완료되었습니다.')
+
+def train(train_dir, repeat=5):
+  train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+  
+  train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='categorical',
+                                                    target_size=(150, 150))
+  history = cnn.model.fit(train_generator,
+                    steps_per_epoch=100,
+                    epochs=repeat,
+                    verbose=2)
+
+  return history
+
+def predict(target_path):
+  img=image.load_img(target_path , target_size=(150, 150))
+  x=image.img_to_array(img)
+  x=np.expand_dims(x, axis=0)
+  images = np.vstack([x])
+  classes = cnn.model.predict(images, batch_size=1)
+  return classes[0]
