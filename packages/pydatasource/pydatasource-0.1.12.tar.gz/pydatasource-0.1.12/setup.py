c2cea353ff53a5e57ba441ef467e2c19edcfecc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package pydatasource/0.1.12/pydatasource-0.1.12.tar.gz

---
+++ packages/pydatasource/pydatasource-0.1.12.tar.gz/pydatasource-0.1.12/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pydatasource',
+    version='0.1.12',
+    description='Easily manage a dataflow',
+    long_description=readme,
+    author='Dacker',
+    author_email='hello@dacker.co',
+    url='https://github.com/dacker-team/pydatasource',
+    keywords='easily manage dataflow',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3',
+    install_requires=[
+        "dbstream>=0.0.20",
+        "PyYAML>=5.1.2",
+        "tabulate>=0.8.7",
+        "dacktool>=0.0.7"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
