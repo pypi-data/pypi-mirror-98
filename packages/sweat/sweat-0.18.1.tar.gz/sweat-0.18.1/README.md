074874c2e9cb6f68a93e9f6c1e012dd17ec89772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:53 +0000

    Add package sweat/0.18.1/sweat-0.18.1.tar.gz

---
+++ packages/sweat/sweat-0.18.1.tar.gz/sweat-0.18.1/README.md
@@ -0,0 +1,14 @@
+# Sweatpy
+
+[![Downloads](https://pepy.tech/badge/sweat)](https://pepy.tech/project/sweat)
+
+> :warning: **Sweatpy is currently undergoing major revisions which will result in deprecations and backwards incompatible changes. We recommend pinning any dependencies with `sweat==0.4.0`.**
+
+Documentation can be found [here](https://sweatpy.gssns.io).
+
+## Contributors
+[Maksym Sladkov](https://github.com/sladkovm)
+[Aart Goossens](https://github.com/AartGoossens)
+
+## License
+See [LICENSE](LICENSE) file.
---
+++ packages/sweat/sweat-0.18.1.tar.gz/sweat-0.18.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sweat',
+ 'sweat.examples',
+ 'sweat.hrm',
+ 'sweat.io',
+ 'sweat.metrics',
+ 'sweat.pdm']
+
+package_data = \
+{'': ['*'], 'sweat.examples': ['data/*']}
+
+install_requires = \
+['fitparse>=1.1.0,<2.0.0',
+ 'lmfit>=1.0.0,<2.0.0',
+ 'pandas>=1.0.3,<2.0.0',
+ 'pydantic>=1.4,<2.0',
+ 'pyyaml>=5.3.1,<6.0.0',
+ 'requests>=2.23.0,<3.0.0',
+ 'scikit-learn>=0.23.1,<0.24.0',
+ 'scipy>=1.4.1,<2.0.0',
+ 'stravalib>=0.10.2,<0.11.0']
+
+setup_kwargs = {
+    'name': 'sweat',
+    'version': '0.18.1',
+    'description': 'Workout analysis',
+    'long_description': '# Sweatpy\n\n[![Downloads](https://pepy.tech/badge/sweat)](https://pepy.tech/project/sweat)\n\n> :warning: **Sweatpy is currently undergoing major revisions which will result in deprecations and backwards incompatible changes. We recommend pinning any dependencies with `sweat==0.4.0`.**\n\nDocumentation can be found [here](https://sweatpy.gssns.io).\n\n## Contributors\n[Maksym Sladkov](https://github.com/sladkovm)\n[Aart Goossens](https://github.com/AartGoossens)\n\n## License\nSee [LICENSE](LICENSE) file.\n',
+    'author': 'Aart Goossens',
+    'author_email': 'aart@goossens.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/goldencheetah/sweatpy',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
