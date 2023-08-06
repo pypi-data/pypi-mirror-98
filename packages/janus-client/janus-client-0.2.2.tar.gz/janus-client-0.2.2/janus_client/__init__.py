70c5c0731bbce987eeab2f8ba6619fddd738af49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:01 +0000

    Add package janus-client/0.2.2/janus-client-0.2.2.tar.gz

---
+++ packages/janus-client/janus-client-0.2.2.tar.gz/janus-client-0.2.2/janus_client/__init__.py
@@ -0,0 +1,5 @@
+
+from .core import JanusClient, JanusAdminMonitorClient
+from .session import JanusSession
+from .plugin_base import JanusPlugin
+from .plugin_video_room import JanusVideoRoomPlugin
