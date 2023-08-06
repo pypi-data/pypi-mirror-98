e79e4f85a5adf32348a597358b8f0237866aee73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:46 +0000

    Add package aptly-ctl/0.10/aptly_ctl-0.10-py3-none-any.whl

---
+++ packages/aptly-ctl/aptly_ctl-0.10-py3-none-any.whl/aptly_ctl/subcommands/__init__.py
@@ -0,0 +1,17 @@
+from aptly_ctl.subcommands import (
+        put,
+        remove,
+        search,
+        repo,
+        publish,
+        copy,
+        )
+
+__all__ = [
+        "put",
+        "remove",
+        "search",
+        "repo",
+        "publish",
+        "copy",
+        ]
