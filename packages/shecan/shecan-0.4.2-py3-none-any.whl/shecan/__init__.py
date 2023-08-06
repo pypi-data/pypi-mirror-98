455a381716bb82fc54804c0e18b6ac759c44c37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:22 +0000

    Add package shecan/0.4.2/shecan-0.4.2-py3-none-any.whl

---
+++ packages/shecan/shecan-0.4.2-py3-none-any.whl/shecan/__init__.py
@@ -0,0 +1,17 @@
+"""
+Shecan CLI
+~~~~~~~~~~~~~~~~~~~~~
+The Shecan CLI https://shecan.ir
+"""
+
+from .api import current_dns, list_dns, update
+from .utils import get_shecan_ips as get_ips
+
+__all__ = [
+    "current_dns",
+    "get_ips",
+    "list_dns",
+    "update",
+]
+
+version = "0.4.2"
