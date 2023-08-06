3e36386f003e65f4e3140a51f47202719e12dac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:22 +0000

    Add package hello-robot-stretch-body/0.0.23/hello_robot_stretch_body-0.0.23.tar.gz

---
+++ packages/hello-robot-stretch-body/hello_robot_stretch_body-0.0.23.tar.gz/hello_robot_stretch_body-0.0.23/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from os import listdir
+from os.path import isfile, join
+
+exec(open('stretch_body/version.py').read())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hello_robot_stretch_body",
+    version=__version__,
+    author="Hello Robot Inc.",
+    author_email="support@hello-robot.com",
+    description="Stretch RE1 low level API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hello-robot/stretch_body",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
+    ],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'ipython', 'jupyter', 'pandas', 'sympy', 'nose', 'PyYaml',
+                      'inputs', 'drawnow', 'rplidar-roboticia', 'snakeviz', 'pyusb', 'SpeechRecognition', 'pixel-ring',
+                      'click', 'cma', 'opencv-contrib-python', 'colorama', 'llvmlite==0.31.0', 'numba',
+                      'scikit-image', 'open3d', 'pyrealsense2', 'jsonschema==2.6.0', 'qtconsole==4.7.7']
+)
