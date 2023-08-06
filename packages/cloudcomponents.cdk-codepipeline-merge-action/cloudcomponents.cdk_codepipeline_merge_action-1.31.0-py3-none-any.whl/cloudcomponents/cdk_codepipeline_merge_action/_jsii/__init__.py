6a6d708383747c2da24bae6c90c4b975b7bdcc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:25 +0000

    Add package cloudcomponents.cdk-codepipeline-merge-action/1.31.0/cloudcomponents.cdk_codepipeline_merge_action-1.31.0-py3-none-any.whl

---
+++ packages/cloudcomponents.cdk-codepipeline-merge-action/cloudcomponents.cdk_codepipeline_merge_action-1.31.0-py3-none-any.whl/cloudcomponents/cdk_codepipeline_merge_action/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_codecommit._jsii
+import aws_cdk.aws_codepipeline._jsii
+import aws_cdk.aws_codepipeline_actions._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-codepipeline-merge-action",
+    "1.31.0",
+    __name__[0:-6],
+    "cdk-codepipeline-merge-action@1.31.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
