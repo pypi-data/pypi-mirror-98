7a5859cc03aa202cd6555729675a5db7d89bb56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:04 +0000

    Add package carspy/0.4.2/carspy-0.4.2-py2.py3-none-any.whl

---
+++ packages/carspy/carspy-0.4.2-py2.py3-none-any.whl/carspy/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for CARSpy."""
+from .line_strength import LineStrength
+from .cars_synth import CarsSpectrum
+from .cars_fit import CarsFit
+from .utils import *
+
+__author__ = """Zhiyao Yin"""
+__email__ = 'zhiyao.yin@dlr.de'
+__version__ = '0.4.2'
