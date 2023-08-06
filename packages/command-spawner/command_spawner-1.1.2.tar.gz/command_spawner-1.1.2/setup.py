9e9cbada8551a2d87173cb50223d07d767d65e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package command-spawner/1.1.2/command_spawner-1.1.2.tar.gz

---
+++ packages/command-spawner/command_spawner-1.1.2.tar.gz/command_spawner-1.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import pathlib
+
+VERSION = '1.1.2'
+DESCRIPTION = 'Command Spawner'
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Setting up
+setup(
+    name="command_spawner",
+    version=VERSION,
+    author="Isa Askin",
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    keywords=['python', 'command spawner', 'spawn', 'command', 'shell',
+              'bash', 'command spawn'],
+    lassifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
