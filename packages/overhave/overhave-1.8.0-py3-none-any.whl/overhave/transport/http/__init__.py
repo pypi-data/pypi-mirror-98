bd1336bcd0f97b83845539d580f9e8ebc6a4bc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:04 +0000

    Add package overhave/1.8.0/overhave-1.8.0-py3-none-any.whl

---
+++ packages/overhave/overhave-1.8.0-py3-none-any.whl/overhave/transport/http/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+from .base_client import BaseHttpClient, BaseHttpClientSettings
+from .stash_client import (
+    OverhaveStashClientSettings,
+    StashBranch,
+    StashErrorResponse,
+    StashHttpClient,
+    StashPrCreationResponse,
+    StashProject,
+    StashPrRequest,
+    StashRepository,
+    StashReviewer,
+    StashReviewerInfo,
+)
