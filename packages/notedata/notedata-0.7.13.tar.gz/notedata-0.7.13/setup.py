e072ac6bdfc775c3f4d876187fd40787c1a9da75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:50 +0000

    Add package notedata/0.7.13/notedata-0.7.13.tar.gz

---
+++ packages/notedata/notedata-0.7.13.tar.gz/notedata-0.7.13/setup.py
@@ -0,0 +1,24 @@
+import sys
+from os import path
+
+from notebuild.tool import get_version
+from setuptools import find_packages, setup
+
+version_path = path.join(path.abspath(
+    path.dirname(__file__)), 'script/__version__.md')
+
+version = get_version(sys.argv, version_path, step=16)
+
+install_requires = ['notetool', 'notedrive']
+
+setup(name='notedata',
+      version=version,
+      description='notedata',
+      author='euler',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+
+      packages=find_packages(),
+      package_data={"": ["*.db"]},
+      install_requires=install_requires
+      )
