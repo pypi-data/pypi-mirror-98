7c831a361211b10dbe272fc3e2334c38edeb0a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:08 +0000

    Add package hthPkg/0.0.29/hthPkg-0.0.29.tar.gz

---
+++ packages/hthPkg/hthPkg-0.0.29.tar.gz/hthPkg-0.0.29/hwp_convert_code/excute/file_converting/config.py
@@ -0,0 +1,25 @@
+import multiprocessing
+
+
+class test_path:
+    output_path = '/opt/HEQMS/convert/output/'
+    targetPattern = r"D:\\NCS\\hwp\\"
+    # lock_path = 'C:\\Python\\NCS\\log\\lock.txt'
+    # log_path = 'C:\\Python\\NCS\\log\\log.txt'
+
+
+class path:
+    output_path = '/opt/test/output/'
+    targetPattern = r"/opt/test/"
+    lock_path = '/opt/test/output/log/lock.txt'
+    log_path = '/opt/test/output/log/'
+
+class home_path:
+    output_path = 'C:\\Python\\NCS\\output\\'
+    targetPattern = r"C:\\Python\\NCS_data\\"
+    lock_path = 'C:\\Python\\NCS\\log\\lock.txt'
+    log_path = 'C:\\Python\\NCS\\log\\log.txt'
+
+
+class prcs:
+    cpu_num = multiprocessing.cpu_count()
