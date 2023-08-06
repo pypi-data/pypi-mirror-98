526e6a8f29e4426e53a7954816c9454048d60aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:09 +0000

    Add package secure-password/0.9/secure_password-0.9.tar.gz

---
+++ packages/secure-password/secure_password-0.9.tar.gz/secure_password-0.9/secure_password/__init__.py
@@ -0,0 +1,12 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456!£$%^&*(`)"
+
+def gen_password(password_len, password_count):
+        password = "" 
+        for x in range(0,password_count):
+            for x in range(0,password_len):
+                password_char = random.choice(chars)
+                password+=password_char
+            password+="\n" 
+        return password
