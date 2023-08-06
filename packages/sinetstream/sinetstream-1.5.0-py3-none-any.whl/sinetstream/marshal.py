0410c78cbe26ae9b8121a870e43bbcaaabbb8de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:25 +0000

    Add package sinetstream/1.5.0/sinetstream-1.5.0-py3-none-any.whl

---
+++ packages/sinetstream/sinetstream-1.5.0-py3-none-any.whl/sinetstream/marshal.py
@@ -0,0 +1,105 @@
+#!/usr/local/bin/python3.6
+# vim: expandtab shiftwidth=4
+
+# Copyright (C) 2020 National Institute of Informatics
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+import io
+import logging
+
+import avro.io
+import avro.schema
+
+logger = logging.getLogger(__name__)
+
+# timestamped message with Avro schema
+MESSAGE_SCHEMA_JSON = '''{
+  "namespace": "jp.ad.sinet.stream",
+  "type": "record",
+  "name": "message",
+  "fields": [
+    {
+      "name": "tstamp",
+      "type": {
+        "type": "long",
+        "logicalType": "timestamp-micros"
+      }
+    },
+    {
+      "name": "msg",
+      "type": "bytes"
+    }
+  ]
+}'''
+
+
+message_schema = avro.schema.Parse(MESSAGE_SCHEMA_JSON)
+message_schema_fingerprint = b'\x1f\x9c\x0c\x91\xeb\x33\x66\x4f'  # 64 bits
+
+
+avro_signle_object_format_marker = b"\xC3\x01"
+
+
+class Marshaller(object):
+    def __init__(self):
+        self._writer = avro.io.DatumWriter(message_schema)
+
+    def marshal(self, msg, tstamp):
+        assert type(tstamp) == int
+        assert type(msg) == bytes
+        bytes_writer = io.BytesIO()
+        bytes_writer.write(avro_signle_object_format_marker)
+        bytes_writer.write(message_schema_fingerprint)
+        encoder = avro.io.BinaryEncoder(bytes_writer)
+        rec = {"tstamp": tstamp, "msg": msg}
+        self._writer.write(rec, encoder)
+        return bytes_writer.getvalue()
+
+
+class Unmarshaller(object):
+    def __init__(self):
+        self._reader = avro.io.DatumReader(message_schema, message_schema)
+
+    def unmarshal(self, msg):
+        from sinetstream.api import InvalidMessageError
+
+        if len(msg) <= (2 + 8):
+            emsg = f"Unmarshaller: len(msg)={len(msg)}"
+            logger.error(emsg)
+            raise InvalidMessageError(emsg)
+        bytes_reader = io.BytesIO(msg)
+        marker = bytes_reader.read(2)
+        if marker != avro_signle_object_format_marker:
+            emsg = f"Unmarshaller: marker={marker}"
+            logger.error(emsg)
+            raise InvalidMessageError(emsg)
+        fingerprint = bytes_reader.read(8)
+        if fingerprint != message_schema_fingerprint:
+            emsg = f"Unmarshaller: fingerprint={fingerprint}"
+            logger.error(emsg)
+            raise InvalidMessageError(emsg)
+        decoder = avro.io.BinaryDecoder(bytes_reader)
+        try:
+            m = self._reader.read(decoder)
+        except AssertionError as ex:
+            raise InvalidMessageError(ex)
+        # rest = bytes_reader.read()
+        # assert len(rest) == 0
+        return m["tstamp"], m["msg"]
---
+++ packages/sinetstream/sinetstream-1.5.0-py3-none-any.whl/sinetstream/spi.py
@@ -0,0 +1,162 @@
+#!/usr/local/bin/python3.6
+# vim: expandtab shiftwidth=4
+
+# Copyright (C) 2019 National Institute of Informatics
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import logging
+from abc import ABCMeta, abstractmethod
+
+logger = logging.getLogger(__name__)
+
+
+def _subclasscheck(subclass, methods):
+    if all(any(method in cls.__dict__ for cls in subclass.__mro__)
+           for method in methods):
+        return True
+    return NotImplemented
+
+
+class PluginMessageWriter(metaclass=ABCMeta):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def metrics(self):
+        return None
+
+    def reset_metrics(self):
+        pass
+
+    @abstractmethod
+    def publish(self, message):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return _subclasscheck(subclass, ['open', 'close', 'metrics', 'reset_metrics', 'publish'])
+
+
+class PluginAsyncMessageWriter(metaclass=ABCMeta):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def metrics(self):
+        return None
+
+    def reset_metrics(self):
+        pass
+
+    @abstractmethod
+    def publish(self, message):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return _subclasscheck(subclass, ['open', 'close', 'metrics', 'reset_metrics', 'publish'])
+
+
+class PluginMessageReader(metaclass=ABCMeta):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def metrics(self):
+        return None
+
+    def reset_metrics(self):
+        pass
+
+    @abstractmethod
+    def __iter__(self):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return _subclasscheck(subclass, ['open', 'close', 'metrics', 'reset_metrics', '__iter__'])
+
+
+class PluginAsyncMessageReader(metaclass=ABCMeta):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+    def metrics(self):
+        return None
+
+    def reset_metrics(self):
+        pass
+
+    @property
+    @abstractmethod
+    def on_message(self):
+        pass
+
+    @on_message.setter
+    @abstractmethod
+    def on_message(self, on_message):
+        pass
+
+    @property
+    @abstractmethod
+    def on_failure(self):
+        pass
+
+    @on_failure.setter
+    @abstractmethod
+    def on_failure(self, on_failure):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return _subclasscheck(
+            subclass, ['open', 'close', 'metrics', 'reset_metrics', 'on_message', 'on_failure'])
+
+
+class PluginValueType(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def serializer(self):
+        pass
+
+    @property
+    @abstractmethod
+    def deserializer(self):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return _subclasscheck(subclass, ['serializer', 'deserializer'])
