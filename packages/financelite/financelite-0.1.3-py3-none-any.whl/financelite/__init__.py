55988a89bc4fee46a5fd058430696f5b044939a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:10 +0000

    Add package financelite/0.1.3/financelite-0.1.3-py3-none-any.whl

---
+++ packages/financelite/financelite-0.1.3-py3-none-any.whl/financelite/__init__.py
@@ -0,0 +1,1 @@
+from financelite.financelite import *
---
+++ packages/financelite/financelite-0.1.3-py3-none-any.whl/financelite/exceptions.py
@@ -0,0 +1,14 @@
+class NoNewsFoundException(BaseException):
+    pass
+
+
+class DataRequestException(BaseException):
+    pass
+
+
+class ItemNotValidException(BaseException):
+    pass
+
+
+class TickerNotInGroupException(BaseException):
+    pass
