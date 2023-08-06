040d3aad2e44ad19b328f53a670fb4577aaa6609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package OWNd/0.7.23/OWNd-0.7.23.tar.gz

---
+++ packages/OWNd/OWNd-0.7.23.tar.gz/OWNd-0.7.23/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OWNd",
+    version="0.7.23",
+    author="anotherjulien",
+    url="https://github.com/anotherjulien/OWNd",
+    author_email="yetanotherjulien@gmail.com",
+    description="Python interface for the OpenWebNet protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["aiohttp", "pytz", "python-dateutil"],
+    python_requires='>=3.8',
+)
