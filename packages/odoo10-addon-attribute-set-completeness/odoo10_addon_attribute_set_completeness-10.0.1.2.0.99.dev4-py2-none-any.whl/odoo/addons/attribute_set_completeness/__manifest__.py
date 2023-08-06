5828d2f73aa8051e2bbc1cdbe4efe8d04a23f43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:40 +0000

    Add package odoo10-addon-attribute-set-completeness/10.0.1.2.0.99.dev4/odoo10_addon_attribute_set_completeness-10.0.1.2.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo10-addon-attribute-set-completeness/odoo10_addon_attribute_set_completeness-10.0.1.2.0.99.dev4-py2-none-any.whl/odoo/addons/attribute_set_completeness/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Attribute Set Completeness",
+    "version": "10.0.1.2.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-pim",
+    "depends": ["attribute_set", "component_event"],
+    "data": [
+        "views/attribute_set.xml",
+        "security/attribute_set_completeness.xml",
+        "views/attribute_set_completeness.xml",
+    ],
+    "demo": [],
+}
