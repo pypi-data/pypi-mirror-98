e6102629707373c9e3ff6fadc5a3d21ddb985d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:48 +0000

    Add package clld-audio-plugin/0.1.2/clld-audio-plugin-0.1.2.tar.gz

---
+++ packages/clld-audio-plugin/clld-audio-plugin-0.1.2.tar.gz/clld-audio-plugin-0.1.2/src/clld_audio_plugin/datatables.py
@@ -0,0 +1,16 @@
+from clld.web.datatables.base import Col
+from clld.web.util.htmllib import HTML
+
+__all__ = ['AudioCol']
+
+
+class AudioCol(Col):
+    __kw__ = dict(bSearchable=False, bSortable=False)
+
+    def format(self, item):
+        if item.audio:
+            return HTML.audio(
+                HTML.source(src=item.audio, type="audio/mpeg"),
+                controls="controls"
+            )
+        return ''
