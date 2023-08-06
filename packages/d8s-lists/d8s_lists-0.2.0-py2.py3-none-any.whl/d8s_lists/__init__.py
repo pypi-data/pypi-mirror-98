916337c0e6a6f50ff77cea00def78092517b3e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:49 +0000

    Add package d8s-lists/0.2.0/d8s_lists-0.2.0-py2.py3-none-any.whl

---
+++ packages/d8s-lists/d8s_lists-0.2.0-py2.py3-none-any.whl/d8s_lists/__init__.py
@@ -0,0 +1,20 @@
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+from .iterables import *
+
+try:
+    __version__ = version('d8s_lists')
+except PackageNotFoundError:
+    message = (
+        'Unable to find a version number for "d8s_lists". '
+        + 'This likely means the library was not installed properly. '
+        + 'Please re-install it and, if the problem persists, '
+        + 'raise an issue here: https://github.com/democritus-project/democritus-lists/issues.'
+    )
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
