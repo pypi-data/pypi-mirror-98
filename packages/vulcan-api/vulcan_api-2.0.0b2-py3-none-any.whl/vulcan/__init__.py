5608177796150596feacffc5009b7cfb4dd7a6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:38 +0000

    Add package vulcan-api/2.0.0b2/vulcan_api-2.0.0b2-py3-none-any.whl

---
+++ packages/vulcan-api/vulcan_api-2.0.0b2-py3-none-any.whl/vulcan/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from ._account import Account
+from ._client import Vulcan
+from ._keystore import Keystore
+
+__version__ = "2.0.0-beta.2"
+__doc__ = "Unofficial API for UONET+ e-register"
+
+__all__ = ["Vulcan", "Keystore", "Account"]
