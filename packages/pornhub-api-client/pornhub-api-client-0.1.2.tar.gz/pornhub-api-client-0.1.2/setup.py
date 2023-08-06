14eedda52e41c743a0e13fd9c915329637c4b237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:50 +0000

    Add package pornhub-api-client/0.1.2/pornhub-api-client-0.1.2.tar.gz

---
+++ packages/pornhub-api-client/pornhub-api-client-0.1.2.tar.gz/pornhub-api-client-0.1.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pornhub', 'pornhub.tests']
+
+package_data = \
+{'': ['*'], 'pornhub.tests': ['responses/*', 'schemas/*']}
+
+entry_points = \
+{'console_scripts': ['phb = pornhub.client:main',
+                     'run-tests = pornhub.tests.unit_test:main']}
+
+setup_kwargs = {
+    'name': 'pornhub-api-client',
+    'version': '0.1.2',
+    'description': 'Simple lib to work with pornhub API',
+    'long_description': None,
+    'author': 'Misha Bystrov',
+    'author_email': 'drudkhmisha@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
