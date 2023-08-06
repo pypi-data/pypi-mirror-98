0ed1222765cb5293c7a9f45503a93d605eba6e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:06 +0000

    Add package deepcomp/1.2.1/deepcomp-1.2.1-py3-none-any.whl

---
+++ packages/deepcomp/deepcomp-1.2.1-py3-none-any.whl/deepcomp/env/multi_ue/central.py
@@ -0,0 +1,161 @@
+"""Multi-UE envs with single, centralized agent controlling all UEs at once."""
+import gym.spaces
+
+from deepcomp.env.single_ue.base import MobileEnv
+from deepcomp.env.single_ue.variants import NormDrMobileEnv, DatarateMobileEnv, RelNormEnv, MaxNormEnv
+
+
+class CentralBaseEnv(MobileEnv):
+    """
+    Abstract base env for central multi-UE coordination.
+    Variants need to inherit first from this class, 2nd from the single-UE base env.
+    Other than that, they only need to define the obs space accordingly.
+    All methods are adjusted to centralized by this wrapper.
+    """
+    def __init__(self, env_config):
+        super().__init__(env_config)
+        # how to aggregate rewards from multiple UEs (sum or min utility)
+        self.reward_agg = env_config['reward']
+
+        # if the number of UEs varies over time, the action space needs to be large enough to control all UEs
+        if self.max_ues > self.num_ue:
+            self.log.warning("Num. UEs varies over time. Setting action and observation space for max. number of UEs.",
+                             curr_num_ue=self.num_ue, max_ues=self.max_ues)
+
+        # observation space to be defined in sub classes!
+        # actions: FOR EACH UE: select a BS to be connected to/disconnect from or noop
+        self.action_space = gym.spaces.MultiDiscrete([self.num_bs + 1 for _ in range(self.max_ues)])
+
+    # overwrite modular functions used within step that are different in the centralized case
+    def get_obs(self):
+        """Get obs for all UEs based on the parent environment's obs definition"""
+        obs = {key: [] for key in self.observation_space.spaces.keys()}
+        for ue in self.ue_list:
+            # get properly normalized observations from NormDrMobileEnv for each UE
+            ue_obs = super().get_ue_obs(ue)
+            # extend central obs
+            for key in self.observation_space.spaces.keys():
+                # handle Discrete or Binary obs --> single number
+                if type(ue_obs[key]) is int:
+                    obs[key].append(ue_obs[key])
+                # remaining Box or MultiDiscrete/Binary obs --> vector/list
+                else:
+                    obs[key].extend(ue_obs[key])
+
+        # to support variable numbers of UEs, pad observations with zeros for all non-existing UEs
+        # since the observations need a fixed length, which is set to the max. number of UEs
+        if self.num_ue < self.max_ues:
+            num_ue_missing = self.max_ues - self.num_ue
+            for obs_name, obs_space in self.observation_space.spaces.items():
+                # get the number of obs per UE by looking at the shape and dividing it by num UEs
+                assert obs_space.shape[0] % self.max_ues == 0, f"{obs_name} does not have a fixed number of obs per UE"
+                obs_per_ue = obs_space.shape[0] / self.max_ues
+                # append zeros for the missing UEs
+                obs[obs_name].extend([0 for _ in range(int(num_ue_missing * obs_per_ue))])
+
+        return obs
+
+    def get_ue_actions(self, action):
+        """Apply action. Here: Actions for all UEs. Return unsuccessful connection attempts."""
+        assert self.action_space.contains(action), f"Action {action} does not fit action space {self.action_space}"
+        # get action for each UE based on index
+        return {ue: action[i] for i, ue in enumerate(self.ue_list)}
+
+    def step_reward(self, rewards):
+        """Return aggregated reward of all UEs as step reward"""
+        if self.reward_agg == 'sum':
+            return sum(rewards.values())
+        if self.reward_agg == 'min':
+            return min(rewards.values())
+        raise NotImplementedError(f"Unexpected reward aggregation: {self.reward_agg}")
+
+
+class CentralDrEnv(CentralBaseEnv, DatarateMobileEnv):
+    """
+    Env where all UEs move, observe and act at all time steps, controlled by a single central agent.
+    Otherwise similar to DatarateMobileEnv with auto dr_cutoff and sub_req_dr.
+    """
+    def __init__(self, env_config):
+        """Similar to DatarateMobileEnv but with multi-UEs controlled at once and fixed dr_cutoff, sub_req_dr"""
+        super().__init__(env_config)
+        self.curr_dr_obs = env_config['curr_dr_obs']
+        self.ues_at_bs_obs = env_config['ues_at_bs_obs']
+
+        # observations: FOR EACH UE: vector of BS dr + already connected BS + optionally: total curr dr per UE
+        obs_space = {}
+        # 1. Achievable data rate for given UE for all BS (normalized to [-1, 1]) --> Box;
+        obs_space['dr'] = gym.spaces.Box(low=-1, high=1, shape=(self.num_ue * self.num_bs,))
+        # 2. Connected BS --> MultiBinary
+        obs_space['connected'] = gym.spaces.MultiBinary(self.num_ue * self.num_bs)
+        # 3. Total curr. dr for each UE summed up over all BS connection. Normalized to [-1,1]. Optional
+        if self.curr_dr_obs:
+            obs_space['dr_total'] = gym.spaces.Box(low=-1, high=1, shape=(self.num_ue,))
+        # 4. number of connected UEs per BS --> help distribute UEs better. Optional
+        if self.ues_at_bs_obs:
+            # at each BS 0 up to all UEs can be connected (no normalization yet)
+            obs_space['ues_at_bs'] = gym.spaces.MultiDiscrete([self.num_ue+1 for _ in range(self.num_bs)])
+        if self.dist_obs:
+            obs_space['dist'] = gym.spaces.Box(low=0, high=1, shape=(self.num_ue * self.num_bs,))
+        if self.next_dist_obs:
+            obs_space['next_dist'] = gym.spaces.Box(low=0, high=1, shape=(self.num_ue * self.num_bs,))
+
+        self.observation_space = gym.spaces.Dict(obs_space)
+        # actions are defined in the parent class; they are the same for all central envs
+
+
+class CentralNormDrEnv(CentralBaseEnv, NormDrMobileEnv):
+    """
+    Variant of the central UE env with different observations (dr is normalized differently).
+    Inherits apply actions and action space from central base env, but get_obs from NormDrEnv
+    (bc not implemented in central base env);
+    """
+    def __init__(self, env_config):
+        # get action space and env basics from parent
+        super().__init__(env_config)
+        # same obs as NormDrMobileEnv, just for each UE
+        # clip & normalize data rates according to utility.
+        # we clip utility at +20, which is reached for a dr of 100
+        self.dr_cutoff = 100
+        obs_space = {
+            'dr': gym.spaces.Box(low=0, high=1, shape=(self.num_ue * self.num_bs,)),
+            'connected': gym.spaces.MultiBinary(self.num_ue * self.num_bs),
+            # 'can_connect': gym.spaces.MultiBinary(self.num_ue * self.num_bs),
+            # 'num_conn': gym.spaces.MultiDiscrete([self.num_bs + 1 for _ in range(self.num_ue)]),
+            # 'ues_at_bs': gym.spaces.MultiDiscrete([self.num_ue+1 for _ in range(self.num_bs)]),
+            # 'ues_at_bs': gym.spaces.Box(low=0, high=1, shape=(self.num_bs,)),
+            'dr_total': gym.spaces.Box(low=0, high=1, shape=(self.num_ue,)),
+            # 'unshared_dr': gym.spaces.Box(low=0, high=self.dr_cutoff, shape=(self.num_ue * self.num_bs,)),
+        }
+        self.observation_space = gym.spaces.Dict(obs_space)
+
+    # only overwrite this to only include num UEs per BS just once in obs instead of repeating it multiple times
+    # def get_obs(self):
+    #     # get basic obs from parent
+    #     obs = super().get_obs()
+    #     # only include first part of ues_at_bs obs; remove repetitions
+    #     obs['ues_at_bs'] = obs['ues_at_bs'][:self.num_bs]
+    #     return obs
+
+
+class CentralRelNormEnv(CentralBaseEnv, RelNormEnv):
+    def __init__(self, env_config):
+        super().__init__(env_config)
+        # use max. number of UEs instead of actual number to support varying numbers of UEs
+        obs_space = {
+            'connected': gym.spaces.MultiBinary(self.max_ues * self.num_bs),
+            'dr': gym.spaces.Box(low=0, high=1, shape=(self.max_ues * self.num_bs,)),
+            'utility': gym.spaces.Box(low=-1, high=1, shape=(self.max_ues,)),
+        }
+        self.observation_space = gym.spaces.Dict(obs_space)
+
+
+class CentralMaxNormEnv(CentralBaseEnv, MaxNormEnv):
+    def __init__(self, env_config):
+        super().__init__(env_config)
+        # use max. number of UEs instead of actual number to support varying numbers of UEs
+        obs_space = {
+            'connected': gym.spaces.MultiBinary(self.max_ues * self.num_bs),
+            'dr': gym.spaces.Box(low=-1, high=1, shape=(self.max_ues * self.num_bs,)),
+            'utility': gym.spaces.Box(low=-1, high=1, shape=(self.max_ues,)),
+        }
+        self.observation_space = gym.spaces.Dict(obs_space)
---
+++ packages/deepcomp/deepcomp-1.2.1-py3-none-any.whl/deepcomp/env/util/movement.py
@@ -0,0 +1,181 @@
+"""Utility functions for UE movement. Must inherit from abstract Movement class."""
+import random
+
+import structlog
+import numpy as np
+from shapely.geometry import Point
+
+
+class Movement:
+    """Abstract movement class that all subclasses must inherit from"""
+    def __init__(self, map):
+        self.map = map
+        # own RNG for reproducibility; global random shares state that's manipulated by RL during training
+        self.rng = random.Random()
+
+    def seed(self, seed=None):
+        self.rng.seed(seed)
+
+    def reset(self):
+        raise NotImplementedError("This function must be implemented in the subclass")
+
+    def step(self, curr_pos):
+        raise NotImplementedError("This function must be implemented in the subclass")
+
+
+class UniformMovement(Movement):
+    """
+    Uniformly move with same speed in one direction. When hitting the borders of the map, "bounce off" like a ball
+    """
+    def __init__(self, map, move_x=0, move_y=0):
+        """
+        Create object for uniform movement into given direction. Instantiate new movement object for each UE!
+
+        :param map: Map representing the area of movement
+        :param move_x: How far to move in x direction per step. Number or 'slow'/'fast'.
+        :param move_y: How far to move in y direction per step. Number or 'slow'/'fast'.
+        """
+        super().__init__(map)
+        self.init_move_x = move_x
+        self.init_move_y = move_y
+        self.move_x = None
+        self.move_y = None
+
+    def __str__(self):
+        return f"UniformMovement({self.move_x}, {self.move_y})"
+
+    def reset(self):
+        """Reset to original movement direction (may change when hitting a map border)"""
+        if self.init_move_x == 'slow':
+            self.move_x = self.rng.randint(1, 5)
+        elif self.init_move_x == 'fast':
+            self.move_x = self.rng.randint(10, 20)
+        else:
+            # assume init_move_x was a specific number for how to move
+            self.move_x = self.init_move_x
+        # same for move_y
+        if self.init_move_y == 'slow':
+            self.move_y = self.rng.randint(1, 5)
+        elif self.init_move_y == 'fast':
+            self.move_y = self.rng.randint(10, 20)
+        else:
+            # assume init_move_y was a specific number for how to move
+            self.move_y = self.init_move_y
+
+    def step(self, curr_pos):
+        """
+        Make a step from the current position into the given direction. Bounce off at map borders.
+
+        :param Point curr_pos: Current position
+        :returns Point: New position after taking one step
+        """
+        # seems like points are immutable --> replace by new point
+        new_pos = Point(curr_pos.x + self.move_x, curr_pos.y + self.move_y)
+        # reverse movement if otherwise moving out of map
+        if not new_pos.within(self.map.shape):
+            self.move_x = -self.move_x
+            self.move_y = -self.move_y
+            new_pos = Point(curr_pos.x + self.move_x, curr_pos.y + self.move_y)
+        return new_pos
+
+
+class RandomWaypoint(Movement):
+    """
+    Create random waypoints, move towards them, pause, and move towards new random waypoint
+    with new random velocity (within given range)
+    """
+    def __init__(self, map, velocity, pause_duration=2, border_buffer=10):
+        """
+        Create random waypoint movement utility object. Instantiate new movement object for each UE!
+
+        :param map: Map representing the area of movement that must not be left
+        :param velocity: Distance to move within one step. Number or 'slow'/'fast'.
+        :param pause_duration: Duration [in env steps] to pause after reaching each waypoint
+        :param border_buffer: Buffer to the map border in which no waypoints are placed
+        """
+        super().__init__(map)
+        self.init_velocity = velocity
+        self.velocity = None
+        self.waypoint = None
+        self.pause_duration = pause_duration
+        self.pausing = False
+        self.curr_pause = 0
+        assert border_buffer > 0, "Border Buffer must be >0 to avoid placing waypoints on or outside map borders."
+        self.border_buffer = border_buffer
+        self.log = structlog.get_logger()
+
+    def __str__(self):
+        return f"RandomWaypoint({self.init_velocity})"
+
+    def reset(self):
+        """Reset velocity and waypoint to new random values. Reset current pause."""
+        if self.init_velocity == 'slow':
+            self.velocity = self.rng.randint(1, 3)
+        elif self.init_velocity == 'fast':
+            self.velocity = self.rng.randint(5, 10)
+        else:
+            self.velocity = self.init_velocity
+
+        self.waypoint = self.random_waypoint()
+        self.pausing = False
+        self.curr_pause = 0
+        self.log.debug('Reset movement', new_velocity=self.velocity, new_waypoint=str(self.waypoint))
+
+    def random_waypoint(self):
+        """Return a new random waypoint inside the map"""
+        x = self.rng.randint(self.border_buffer, int(self.map.width - self.border_buffer))
+        y = self.rng.randint(self.border_buffer, int(self.map.height - self.border_buffer))
+        new_waypoint = Point(x, y)
+        assert new_waypoint.within(self.map.shape), f"Waypoint {str(new_waypoint)} is outside the map!"
+        return new_waypoint
+
+    def step_towards_waypoint(self, curr_pos):
+        """
+        Take one step from the current position towards the current waypoint.
+
+        :param Point curr_pos: Current position
+        :return: New position after moving
+        """
+        self.log = self.log.bind(prev_pos=str(curr_pos), waypoint=str(self.waypoint))
+
+        # if already close enough to waypoint, move directly onto waypoint (not past it)
+        if curr_pos.distance(self.waypoint) <= self.velocity:
+            self.log.debug('Waypoint reached')
+            return self.waypoint
+
+        # else move by self.velocity towards waypoint: https://math.stackexchange.com/a/175906/234077
+        # convert points to np arrays/vectors for calculation
+        np_curr = np.array([curr_pos.x, curr_pos.y])
+        np_waypoint = np.array([self.waypoint.x, self.waypoint.y])
+        v = np_waypoint - np_curr
+        norm_v = v / np.linalg.norm(v)
+        np_new_pos = np_curr + self.velocity * norm_v
+        new_pos = Point(np_new_pos)
+
+        self.log.debug('Step to waypoint', new_pos=str(new_pos))
+        return new_pos
+
+    def step(self, curr_pos):
+        """
+        Perform one movement step: Move towards waypoint, pause, select new waypoint & repeat.
+
+        :param Point curr_pos: Current position of the moving entity (eg, UE)
+        :return: New position after one step
+        """
+        assert curr_pos.within(self.map.shape) or curr_pos.touches(self.map.shape), \
+            f"Current position {str(curr_pos)} is outside the map!"
+
+        # start pausing when reaching the waypoint
+        if curr_pos == self.waypoint:
+            self.pausing = True
+
+        if self.pausing:
+            # continue pausing if pause duration is not yet reached
+            if self.curr_pause < self.pause_duration:
+                self.curr_pause += 1
+                return curr_pos
+            self.reset()
+
+        # move towards (new) waypoint
+        new_pos = self.step_towards_waypoint(curr_pos)
+        return new_pos
