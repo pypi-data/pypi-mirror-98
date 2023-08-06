de24668c6497e8af068efa79020b5cd0b8c77dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:51 +0000

    Add package excludarr/0.1.1/excludarr-0.1.1-py3-none-any.whl

---
+++ packages/excludarr/excludarr-0.1.1-py3-none-any.whl/excludarr/core/pyradarr/exceptions.py
@@ -0,0 +1,18 @@
+class RadarrTooManyVariablesException(Exception):
+    pass
+
+
+class RadarrInvalidIdSupplied(Exception):
+    pass
+
+
+class RadarrInvalidApiKey(Exception):
+    pass
+
+
+class RadarrMovieNotFound(Exception):
+    pass
+
+
+class RadarrValidationException(Exception):
+    pass
