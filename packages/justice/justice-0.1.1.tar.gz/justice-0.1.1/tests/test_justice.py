c0020471b951e567710225a9a5bdceeea25523b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:32 +0000

    Add package justice/0.1.1/justice-0.1.1.tar.gz

---
+++ packages/justice/justice-0.1.1.tar.gz/justice-0.1.1/tests/test_justice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `justice` package."""
+
+
+import unittest
+
+from justice import justice  # noqa: TODO Add unit-tests
+
+
+class TestJustice(unittest.TestCase):
+    """Tests for `justice` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
