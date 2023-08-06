b8c52c1ea4dfcf72433ef177b7aca0d79aa49fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:09 +0000

    Add package torchplus/0.2.91/torchplus-0.2.91-py3-none-any.whl

---
+++ packages/torchplus/torchplus-0.2.91-py3-none-any.whl/torchplus/nn/__init__.py
@@ -0,0 +1,17 @@
+#! python3.8 -u
+#  -*- coding: utf-8 -*-
+
+##############################
+## Project PyCTLib
+## Package torchplus.nn
+##############################
+
+from .modules import *
+from . import parameter
+from .parameter import Parameter
+from . import functional
+# from .. import basic_torch
+# from torch.nn import utils
+import torch as basic_torch
+
+from . import utils
