e484c01aee884c14af80803ea19ce8e7a2a60e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:48 +0000

    Add package deepspeed/0.3.13/deepspeed-0.3.13.tar.gz

---
+++ packages/deepspeed/deepspeed-0.3.13.tar.gz/deepspeed-0.3.13/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.3.13'
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'utils': False}
+compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'utils': True}
+torch_info={'version': '1.7', 'cuda_version': '10.2'}
