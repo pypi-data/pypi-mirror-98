87e36e9b493682d8a840b2df80746ae812752448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:26 +0000

    Add package XamPy/0.1.3.1/XamPy-0.1.3.1.tar.gz

---
+++ packages/XamPy/XamPy-0.1.3.1.tar.gz/XamPy-0.1.3.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="XamPy",
+    version="0.1.3.1",
+    author="Max Paul",
+    author_email="maxkpaul21@gmail.com",
+    description="A Data Science Package written in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requres=[
+        'nltk',
+        'pandas',
+        'vaderSentiment',
+        'numpy',
+        'sklearn',
+        'textblob',
+        'sklearn',
+        'datetime'
+    ]
+)
