0712db224849e353a93c08e48131b184f892ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:08 +0000

    Add package lir/0.0.6/lir-0.0.6-py3-none-any.whl

---
+++ packages/lir/lir-0.0.6-py3-none-any.whl/lir/util.py
@@ -0,0 +1,66 @@
+import collections
+
+import numpy as np
+
+
+LR = collections.namedtuple('LR', ['lr', 'p0', 'p1'])
+
+
+def get_classes_from_Xy(X, y, classes=None):
+    assert len(X.shape) >= 1, f'expected: X has at least 1 dimensions; found: {len(X.shape)} dimensions'
+    assert len(y.shape) == 1, f'expected: y is a 1-dimensional array; found: {len(y.shape)} dimensions'
+    assert X.shape[0] == y.size, f'dimensions of X and y do not match; found: {X.shape[0]} != {y.size}'
+
+    return np.unique(y) if classes is None else np.asarray(classes)
+
+
+def Xn_to_Xy(*Xn):
+    """
+    Convert Xn to Xy format.
+
+    Xn is a format where samples are divided into separate variables based on class.
+    Xy is a format where all samples are concatenated, with an equal length variable y indicating class."""
+    Xn = [np.asarray(X) for X in Xn]
+    X = np.concatenate(Xn)
+    y = np.concatenate([np.ones((X.shape[0],), dtype=np.int8) * i for i, X in enumerate(Xn)])
+    return X, y
+
+
+def Xy_to_Xn(X, y, classes=[0, 1]):
+    """
+    Convert Xy to Xn format.
+
+    Xn is a format where samples are divided into separate variables based on class.
+    Xy is a format where all samples are concatenated, with an equal length variable y indicating class."""
+
+    classes = get_classes_from_Xy(X, y, classes)
+    return [X[y == yvalue] for yvalue in classes]
+
+
+def to_probability(odds):
+    """
+    Converts odds to a probability
+
+    Returns
+    -------
+       1                , for odds values of inf
+       odds / (1 + odds), otherwise
+    """
+    inf_values = odds == np.inf
+    with np.errstate(invalid='ignore'):
+        p = np.divide(odds, (1 + odds))
+    p[inf_values] = 1
+    return p
+
+
+def to_odds(p):
+    """
+    Converts a probability to odds
+    """
+    with np.errstate(divide='ignore'):
+        return p / (1 - p)
+
+
+def to_log_odds(p):
+    odds = to_odds(p)
+    return np.nan_to_num(np.log10(odds))
