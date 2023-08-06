8f610a5833ebf9751191325514b303914c8f6cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:01 +0000

    Add package drf-query-filter/0.1.4/drf_query_filter-0.1.4-py3-none-any.whl

---
+++ packages/drf-query-filter/drf_query_filter-0.1.4-py3-none-any.whl/drf_query_filter/filters.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from rest_framework import filters
+from rest_framework.exceptions import ValidationError
+
+from drf_query_filter import fields
+
+
+class QueryParamFilter(filters.BaseFilterBackend):
+    # Defaults:
+    raise_exceptions = False
+    
+    # Look ups for attrs in view
+    query_param_attr = 'query_params'
+    query_param_call = 'get_query_params'
+    query_raise_exceptions = 'query_raise_exceptions'
+    
+    def get_raise_exceptions(self, view) -> bool:
+        return getattr(view, self.query_raise_exceptions, None) or self.raise_exceptions
+    
+    def get_query_params(self, view) -> List[fields.Node]:
+        get_query_params = getattr(view, self.query_param_call, None)
+        if callable(get_query_params):
+            return get_query_params()
+        else:
+            return getattr(view, self.query_param_attr, None)
+        
+    def filter_queryset(self, request, queryset, view):
+        query_fields = self.get_query_params(view)
+        errors = {}
+        query_params = request.query_params
+        if not query_params:
+            return queryset
+
+        if not query_fields:  # do nothing if no fields are found
+            return queryset
+            
+        for field in query_fields:
+            queryset, field_errors = field.filter(queryset, request.query_params, self.get_raise_exceptions(view))
+            if field_errors:
+                errors.update(field_errors)
+        if errors and self.get_raise_exceptions(view):
+            raise ValidationError(errors)
+        return queryset
