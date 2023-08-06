1e7857e6aaf7f6919bd1a659dd4b98ad00cd19d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:23 +0000

    Add package ukritlibrary/0.1.1/ukritlibrary-0.1.1.tar.gz

---
+++ packages/ukritlibrary/ukritlibrary-0.1.1.tar.gz/ukritlibrary-0.1.1/setup.py
@@ -0,0 +1,16 @@
+# print('Hello World !!')
+from setuptools import find_packages,setup
+
+setup(
+    name='ukritlibrary',
+    packages=find_packages(include=['libraly']),
+    version='0.1.1',
+    description='My libraly From ukrit',
+    author='ME',
+    license='MIT',
+    install_requirs=['numpy'],
+    setup_requires=['pytest-runner==4.4'],
+    tests_requires=['pytest==4.4.1'],
+    test_suite='tests' , ## ใช้ชื่อ Floder ที่เราใช้ในการ Test
+    download_url=""
+)
