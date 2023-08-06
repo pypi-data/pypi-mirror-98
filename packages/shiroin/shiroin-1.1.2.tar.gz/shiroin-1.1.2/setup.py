4c06624e51e33640508a55fe1d1a158b32ffcc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package shiroin/1.1.2/shiroin-1.1.2.tar.gz

---
+++ packages/shiroin/shiroin-1.1.2.tar.gz/shiroin-1.1.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup,find_packages
+
+setup(
+    name='shiroin',
+    version='1.1.2',
+    author='Grzegorz Adamski',
+    author_email='grzes-a1@o2.pl',
+    python_requires='>=3.5',
+    url='https://github.com/urojony/shiroin',
+    license='BSD',
+    description='Software for proving inequalities',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['scipy>=0.15','sympy'],
+    classifiers=[
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Mathematics"
+    ],
+)
