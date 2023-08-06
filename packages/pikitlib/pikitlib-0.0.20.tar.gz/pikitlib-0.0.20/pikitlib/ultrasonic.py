96bad9286093c453878b7dfbc109b1ee947a5e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:01 +0000

    Add package pikitlib/0.0.20/pikitlib-0.0.20.tar.gz

---
+++ packages/pikitlib/pikitlib-0.0.20.tar.gz/pikitlib-0.0.20/pikitlib/ultrasonic.py
@@ -0,0 +1,27 @@
+import socket
+
+class Ultrasonic:
+    def __init__(self, pingChannel = 27, echoChannel = 22):
+
+        self.pPin = pingChannel
+        self.ePin = echoChannel  
+        self.chirpLen = 0.00015
+        self.s = socket.socket()
+        
+
+    def connect():
+        host = 'localhost'
+        port = 1235
+        self.s.bind(('',port))
+        self.s.listen(5)
+        sc, addr = self.s.accept()
+
+    def getRangeInches(self):
+        
+        
+        
+        print("received info")
+        message = socketclient.recv(1024)
+        message = message.decode("utf-8")
+        return message
+        
