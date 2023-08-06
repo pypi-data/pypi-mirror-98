a648244d9a12a67172d4cc7d5583035529ca3ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package Typedtuples/1.0.4/Typedtuples-1.0.4-py3-none-any.whl

---
+++ packages/Typedtuples/Typedtuples-1.0.4-py3-none-any.whl/typedtuples/typedtuples.py
@@ -0,0 +1,47 @@
+from typedtuples.utils import toString
+
+class TypedTuple:
+    def __init__(self, *args, **kwargs):
+        self.initialize(args, kwargs)
+    
+    def initialize(self, args, kwargs):
+        a = self.__annotations__
+        names = list(a.keys())
+        types = list(a.values())
+        kwnames = list(kwargs.keys())
+        kwvalues = list(kwargs.values())
+        for i in range(len(a)):
+            n, t = names[i], types[i]
+            if i >= len(args):
+                kwn = kwnames[i]
+                it = kwvalues[i]
+                if kwn != n: raise Exception(
+                    f'Unknown name "{kwn}" found.'
+                )
+            else: it = args[i]
+            valid = isinstance(it, t)
+            if not valid: raise Exception(
+                f'Expect Type "{t.__name__}" but "{it.__class__.__name__}" found.'
+                f' (at name "{n}", Item {toString(it)})'
+            )
+            setattr(self, n, it)
+
+    def __str__(self):
+        res = []
+        a = self.__annotations__
+        for i in a:
+            res += [f'{i}={toString(getattr(self, i))}']
+        return f'{self.__class__.__name__}( {", ".join(res)} )'
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def of(name, info):
+        return type(name, (TypedTuple,), {'__annotations__': info})
+
+    @staticmethod
+    def apply(info):
+        def _(cls):
+            return type(cls.__name__, (cls, TypedTuple), {'__annotations__': info})
+        return _
