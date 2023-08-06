4fa2cb514d045472898ba986120ab76725ae90d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:23 +0000

    Add package d8s-xml/0.1.0/d8s_xml-0.1.0-py2.py3-none-any.whl

---
+++ packages/d8s-xml/d8s_xml-0.1.0-py2.py3-none-any.whl/d8s_xml/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  #type: ignore
+
+try:
+    __version__ = version('democritus_xml')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "democritus_xml". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-xml/issues.'
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+
+from .xml_data import *
