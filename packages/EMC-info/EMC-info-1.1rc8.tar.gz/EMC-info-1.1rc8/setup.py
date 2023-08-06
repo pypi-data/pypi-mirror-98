99a49e782feb8d24b0909b3647de2cc14fbaa86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:58 +0000

    Add package EMC-info/1.1rc8/EMC-info-1.1rc8.tar.gz

---
+++ packages/EMC-info/EMC-info-1.1rc8.tar.gz/EMC-info-1.1rc8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import listdir
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="EMC-info",
+    version="1.1-rc8",
+    description="EarthMC is a large Minecraft server this package lets you get info about things on that server.",
+    packages = ["emc"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Games/Entertainment",
+        "Typing :: Typed"
+    ],
+    url="https://github.com/TheSuperGamer20578/EMC-info/",
+    author="TheSuperGamer20578",
+    install_requires=["requests"],
+    extras_require = {
+        "async": ["aiohttp"]
+    }
+)
