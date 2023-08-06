dd92f1c3fd7ff4f435fff9314b7b0c94c6be12a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package expai/1.1/expai-1.1.tar.gz

---
+++ packages/expai/expai-1.1.tar.gz/expai-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='expai',
+    version='1.1',
+    description='User-friendly library to interact with EXPAI',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='EXPAI',
+    author_email='info@expai.io',
+    keywords=['EXPAI', 'ExplainableAI', 'Explainable Artificial Intelligence'],
+    url='https://expai.io',
+    download_url='https://pypi.org/project/expai/'
+)
+
+install_requires = ['requests', 'ipython', 'pandas', 'numpy']
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
