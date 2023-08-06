5b5bc362bd6fb8f86b585f20dc139ea3b8272bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:41 +0000

    Add package speech4t/0.0.4/speech4t-0.0.4-py3-none-any.whl

---
+++ packages/speech4t/speech4t-0.0.4-py3-none-any.whl/speech4t/common.py
@@ -0,0 +1,32 @@
+
+# tts
+
+SVSFDefault = 0
+SVSFlagsAsync = 1
+
+DEFAULT_VOLUME = 80
+DEFAULT_RATE = 1
+
+# recognization
+recognizer = None
+mic = None
+lock = None
+# recog listen program status
+
+recog_service = False # google or azure
+recog_key = ''
+recog_location = ''
+
+recog_text = ''
+
+recog_paused = False
+recog_discard = False
+
+recog_stopper = None
+#RECOG_COUNTDOWN = 10
+recog_countdown =  None
+
+### common var
+# spVoice object
+speaker = None
+
