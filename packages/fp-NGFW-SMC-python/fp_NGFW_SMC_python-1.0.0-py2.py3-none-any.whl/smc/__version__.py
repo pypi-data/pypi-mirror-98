f9051f19c48fd53ec06bdc754dfb779dc20925db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package fp-NGFW-SMC-python/1.0.0/fp_NGFW_SMC_python-1.0.0-py2.py3-none-any.whl

---
+++ packages/fp-NGFW-SMC-python/fp_NGFW_SMC_python-1.0.0-py2.py3-none-any.whl/smc/__version__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.0'
+__author__ = 'Forcepoint'
+__description__ = 'Python based API to Forcepoint NGFW Management Center'
+__url__ = 'http://github.com/Forcepoint/fp-NGFW-SMC-python'
+__author_email__ = 'PSIRT@forcepoint.com'
+__license__ = 'Apache 2.0'
