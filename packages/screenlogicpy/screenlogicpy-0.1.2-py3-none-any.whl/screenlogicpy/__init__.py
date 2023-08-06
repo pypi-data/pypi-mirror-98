1566ce828384648b01fcd47e57dd84c4f9aacd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:31 +0000

    Add package screenlogicpy/0.1.2/screenlogicpy-0.1.2-py3-none-any.whl

---
+++ packages/screenlogicpy/screenlogicpy-0.1.2-py3-none-any.whl/screenlogicpy/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.2"
+# flake8: noqa F401
+from screenlogicpy.gateway import ScreenLogicGateway
+from screenlogicpy.const import ScreenLogicError
+from screenlogicpy.discovery import discover
