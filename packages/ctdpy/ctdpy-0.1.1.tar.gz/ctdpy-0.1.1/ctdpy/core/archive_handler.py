0f19d310de9b0db6c2465bbca18f5ff876062ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:07 +0000

    Add package ctdpy/0.1.1/ctdpy-0.1.1.tar.gz

---
+++ packages/ctdpy/ctdpy-0.1.1.tar.gz/ctdpy-0.1.1/ctdpy/core/archive_handler.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Oct 31 10:26:30 2018
+
+@author: a002028
+"""
+from ctdpy.core import utils
+
+
+class Archive:
+    """
+
+    """
+    # TODO Fix archive name. Either copy archive structure from Phyche
+    #  (eg. SHARK_Profile_BAS_UMSC_2018, SHARK_Profile_GAV_UMSC_2018....)
+    #  or simply merge all PROJs as a complete delivery package ? SHARK_Profile_BAS_GAV_RNE_PBX_XXX_UMSC_2018
+    #  or just SHARK_Profile_UMSC_2018
+
+    def __init__(self, settings, data_path=''):
+        self.settings = settings
+        self.data_path = data_path
+        # self.archive_structure = self._get_standard_archive_structure()
+
+    def write_archive_package(self, data_path):
+        """
+        :param path: folder with data files
+        :return:
+        """
+        self._copy_standard_archive_structure()
+        self._copy_data_files_to_archive_path(data_path)
+
+    def _copy_data_files_to_archive_path(self, data_path):
+        """
+        :param data_path: Path to data files
+        :return: Copies datafiles to archive_data_path
+        """
+        archive_data_path = self._get_data_path_in_archive()
+        utils.copytree(data_path, archive_data_path)
+
+    def _get_data_path_in_archive(self):
+        """
+        :return:
+        """
+        return '/'.join([self.archive_path, 'processed_data', ''])
+
+    def _get_received_data_path_in_archive(self):
+        """
+        :return:
+        """
+        return '/'.join([self.archive_path, 'received_data', ''])
+
+    def _copy_standard_archive_structure(self):
+        """
+        :return:
+        """
+        source_path = self._get_standard_archive_structure()
+        self.archive_path = self._get_destination_path()
+        utils.copytree(source_path, self.archive_path)
+
+    def load_standard_archive_files(self):
+        """
+        :return:
+        """
+        raise NotImplementedError
+        # generator = utils.generate_strings_based_on_suffix(self.archive_structure, '.txt')
+        # files = [filename for filename in generator]
+
+    def _get_destination_path(self, archive_name='archive_'):
+        """
+        :return: Path to new archive
+        """
+        folder_time_stamp = utils.get_datetime_now(fmt='%Y%m%d_%H%M%S')
+        return ''.join([self.settings.settings_paths.get('export_path'),
+                        archive_name, folder_time_stamp])
+
+    def _get_standard_archive_structure(self):
+        """
+        :return: archive_structure_path
+        """
+        return self.settings.settings_paths.get('archive_structure_path')
+
+    def import_received_data(self, file_paths):
+        """
+        :param file_paths:
+        :return:
+        """
+        target_path = self._get_received_data_path_in_archive()
+        utils.copytree(None, target_path, file_paths=file_paths)
+
+    def _load_zipwriter_instance(self):
+        """
+        :return: Writer
+        """
+        writer_instance = self.settings.writers['zip']['writer'].get('writer')
+        return writer_instance()
+
+    def zip_archive(self):
+        """
+        :return: Zipped archive ready for sharkweb to load
+        """
+        zipzy = self._load_zipwriter_instance()
---
+++ packages/ctdpy/ctdpy-0.1.1.tar.gz/ctdpy-0.1.1/ctdpy/core/writers/xlsx_writer.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 13:38:35 2018
+
+@author: a002028
+"""
+import pandas as pd
+
+
+class XlsxWriter:
+    """ """
+    def __init__(self, with_style=False, in_template=None):
+        self.with_style = with_style
+        self.in_template = in_template
+        # self._load_xlsx_writer()
+
+    def _load_xlsx_writer(self, save_path, engine='openpyxl'):
+        """
+        Create a Pandas Excel writer using engine.
+        :param save_path: str, path to file
+        :param engine: Engine for the writer
+        :return: Pandas Excel writer using engine.
+        """
+        self.xlsx_writer = pd.ExcelWriter(save_path, engine=engine)
+
+    def write_multiple_sheets(self, save_path, dict_df=None, sheet_names=None, headers=None, start_rows=None,
+                              na_rep='', index=False, encoding='cp1252'):
+        """
+        :param dict_df: Dictionary with multiple pd.DataFrame
+        :param save_path: str
+        :param sheet_names: list of sheet names
+        :param headers: list of False or True
+        :param na_rep: str, np.nan, None
+        :param index: False or True
+        :param encoding: str
+        :return: Saved excel file
+        """
+        self._load_xlsx_writer(save_path)
+        for sheet, head, st_row in zip(sheet_names, headers, start_rows):
+            dict_df[sheet].to_excel(self.xlsx_writer,
+                                    sheet_name=sheet,
+                                    header=head,
+                                    startrow=st_row,
+                                    na_rep=na_rep,
+                                    index=index,
+                                    encoding=encoding)
+        self.close_writer()
+
+    def close_writer(self):
+        """
+        Close the Pandas Excel writer and save the Excel file.
+        :return:
+        """
+        self.xlsx_writer.save()
+
+    @staticmethod
+    def write(df, save_path, sheet_name='Data', na_rep='', index=False,  encoding='cp1252'):
+        """
+        :param df: pd.DataFrame
+        :param save_path: str
+        :param sheet_name: str
+        :param na_rep: str, np.nan, None
+        :param index: False or True
+        :param encoding: str
+        :return: Saved excel file
+        """
+        df.to_excel(save_path,
+                    na_rep=na_rep,
+                    sheet_name=sheet_name,
+                    index=index,
+                    encoding=encoding)
+
+    @staticmethod
+    def write_dataframe(df, save_path, sheet_name='Data', na_rep='', index=False,  encoding='cp1252'):
+        """
+        :param df: pd.DataFrame
+        :param save_path: str
+        :param sheet_name: str
+        :param na_rep: str, np.nan, None
+        :param index: False or True
+        :param encoding: str
+        :return: Saved excel file
+        """
+        df.to_excel(save_path,
+                    na_rep=na_rep,
+                    sheet_name=sheet_name,
+                    index=index,
+                    encoding=encoding)
