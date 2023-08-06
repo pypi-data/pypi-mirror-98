8ff3d537ae4611163860818a7730c7fd646ca0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:29 +0000

    Add package postgres-helper/0.0.1/postgres-helper-0.0.1.tar.gz

---
+++ packages/postgres-helper/postgres-helper-0.0.1.tar.gz/postgres-helper-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="postgres-helper",
+    version="0.0.1",
+    author="Tek Kshetri",
+    author_email="iamtekson@gmail.com",
+    description="Package for Postgres query on python",
+    py_modules=['postgres-helper'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamtekson/postgres-helper",
+    packages=['pg'],
+    keywords=['postgresql', 'postgres', 'database', 'sql', 'api', 'table', 'pg', 'postGIS'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['psycopg2'],
+    python_requires='>=3.6',
+)
