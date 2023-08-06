15cafb14448881e8fb0a2371d4c0e85b10cba99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:11 +0000

    Add package invenio-drafts-resources/0.10.2/invenio_drafts_resources-0.10.2-py2.py3-none-any.whl

---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.10.2-py2.py3-none-any.whl/invenio_drafts_resources/records/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Drafts data access layer API."""
+
+from .api import Draft, Record
+from .models import DraftMetadataBase, ParentRecordMixin, \
+    ParentRecordStateMixin
+
+__all__ = (
+    "Draft",
+    "DraftMetadataBase",
+    "ParentRecordMixin",
+    "ParentRecordStateMixin",
+    "Record",
+)
