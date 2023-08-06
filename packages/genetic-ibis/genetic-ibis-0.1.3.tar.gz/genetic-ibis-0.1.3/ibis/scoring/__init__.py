da70a684643852aeaf6c115fe8f777f8bbd4b14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:23 +0000

    Add package genetic-ibis/0.1.3/genetic-ibis-0.1.3.tar.gz

---
+++ packages/genetic-ibis/genetic-ibis-0.1.3.tar.gz/genetic-ibis-0.1.3/ibis/scoring/__init__.py
@@ -0,0 +1,9 @@
+"""
+--------------------------------------------------------------------------------
+<Circuit-Scoring Project>
+
+Written by W.R. Jackson, Ben Bremer, Eric South
+--------------------------------------------------------------------------------
+"""
+from .scorer import BaseScoring, BaseRequirement
+from .cello_score import CelloScoring
