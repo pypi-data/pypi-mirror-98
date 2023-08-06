a506069926402f6df45c7383647f5e48b46f5584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:41 +0000

    Add package tifffile/2021.3.17/tifffile-2021.3.17.tar.gz

---
+++ packages/tifffile/tifffile-2021.3.17.tar.gz/tifffile-2021.3.17/setup.py
@@ -0,0 +1,128 @@
+# tifffile/setup.py
+
+"""Tifffile package setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup
+
+buildnumber = ''
+
+with open('tifffile/tifffile.py') as fh:
+    code = fh.read()
+
+version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+version += ('.' + buildnumber) if buildnumber else ''
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
+
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+if 'sdist' in sys.argv:
+    # update README, LICENSE, and CHANGES files
+
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+    license = re.search(
+        r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+        code,
+        re.MULTILINE | re.DOTALL,
+    ).groups()[0]
+
+    license = license.replace('# ', '').replace('#', '')
+
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+
+    revisions = (
+        re.search(
+            r'(?:\r\n|\r|\n){2}(Revisions.*)   \.\.\.',
+            readme,
+            re.MULTILINE | re.DOTALL,
+        )
+        .groups()[0]
+        .strip()
+    )
+
+    with open('CHANGES.rst', 'r') as fh:
+        old = fh.read()
+
+    d = revisions.splitlines()[-1]
+    old = old.split(d)[-1]
+    with open('CHANGES.rst', 'w') as fh:
+        fh.write(revisions.strip())
+        fh.write(old)
+
+setup(
+    name='tifffile',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@uci.edu',
+    license='BSD',
+    url='https://www.lfd.uci.edu/~gohlke/',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/tifffile/issues',
+        'Source Code': 'https://github.com/cgohlke/tifffile',
+        # 'Documentation': 'https://',
+    },
+    packages=['tifffile'],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy>=1.15.1',
+        # 'imagecodecs>=2021.1.11',
+    ],
+    extras_require={
+        'all': [
+            'imagecodecs>=2021.1.11',
+            'matplotlib>=3.2',
+            'lxml',
+            # 'zarr>=2.5.0'
+        ]
+    },
+    tests_require=[
+        'pytest',
+        'imagecodecs',
+        'czifile',
+        'cmapfile',
+        'oiffile',
+        'lfdfiles',
+        'roifile',
+        'lxml',
+        'zarr>=2.5.0',
+        'fsspec',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tifffile = tifffile:main',
+            'tiffcomment = tifffile.tiffcomment:main',
+            'tiff2fsspec = tifffile.tiff2fsspec:main',
+            'lsm2bin = tifffile.lsm2bin:main',
+        ],
+        # 'napari.plugin': ['tifffile = tifffile.napari_tifffile'],
+    },
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
---
+++ packages/tifffile/tifffile-2021.3.17.tar.gz/tifffile-2021.3.17/tests/conftest.py
@@ -0,0 +1,34 @@
+# tifffile/tests/conftest.py
+
+import os
+import sys
+
+if os.environ.get('VSCODE_CWD'):
+    # work around pytest not using PYTHONPATH in VSCode
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    )
+
+if os.environ.get('SKIP_CODECS', None):
+    sys.modules['imagecodecs'] = None
+
+
+def pytest_report_header(config):
+    try:
+        from numpy import __version__ as numpy
+        from tifffile import __version__ as tifffile
+        from test_tifffile import config
+
+        try:
+            from imagecodecs import __version__ as imagecodecs
+        except ImportError:
+            imagecodecs = 'N/A'
+        return (
+            'versions: tifffile-{}, imagecodecs-{}, numpy-{}\n'
+            'test config: {}'.format(tifffile, imagecodecs, numpy, config())
+        )
+    except Exception:
+        pass
+
+
+collect_ignore = ['_tmp', 'data']
