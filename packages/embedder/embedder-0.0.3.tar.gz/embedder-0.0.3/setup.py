8aefcbdf6f05ea9fe4502f580bbbbd8c1abc34ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package embedder/0.0.3/embedder-0.0.3.tar.gz

---
+++ packages/embedder/embedder-0.0.3.tar.gz/embedder-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+parent_path = os.path.dirname(os.path.realpath(__file__))
+requirements_path = parent_path + '/requirements.txt'
+install_requires = []
+
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name="embedder",
+    version="0.0.3",
+    install_requires=install_requires,
+    packages=find_packages(exclude=("tests",)),
+)
