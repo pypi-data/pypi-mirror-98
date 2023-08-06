cc11c369e63ffc8e306aaaae7a805f024c3dcc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:18 +0000

    Add package pyFirmata2/2.1.1/pyFirmata2-2.1.1.tar.gz

---
+++ packages/pyFirmata2/pyFirmata2-2.1.1.tar.gz/pyFirmata2-2.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='pyFirmata2',
+    version='2.1.1',
+    description="Use your Arduino as a data acquisition card under Python",
+    long_description=long_description,
+    author='Bernd Porr',
+    author_email='mail@berndporr.me.uk',
+    packages=['pyfirmata2'],
+    include_package_data=True,
+    install_requires=['pyserial'],
+    zip_safe=False,
+    url='https://github.com/berndporr/pyFirmata2',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+)
