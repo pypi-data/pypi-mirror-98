72b86bb4928650232370a58b1be12cf8975f1602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:18 +0000

    Add package ssl-smtp-handler/0.0.10/ssl-smtp-handler-0.0.10.tar.gz

---
+++ packages/ssl-smtp-handler/ssl-smtp-handler-0.0.10.tar.gz/ssl-smtp-handler-0.0.10/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ssl_smtp_handler']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ssl-smtp-handler',
+    'version': '0.0.10',
+    'description': 'A Python standard library logging handler which uses SMTP_SSL to send its mail ',
+    'long_description': None,
+    'author': 'Derek Wan',
+    'author_email': 'd.wan@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
