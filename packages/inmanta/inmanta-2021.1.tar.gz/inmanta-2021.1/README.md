37bbd3b3b7dfdd25e4065d6d4286c8dcc7d3c9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package inmanta/2021.1/inmanta-2021.1.tar.gz

---
+++ packages/inmanta/inmanta-2021.1.tar.gz/inmanta-2021.1/README.md
@@ -0,0 +1,3 @@
+# inmanta
+Repository for the inmanta package. This package exists for backwards compatibility and depends on the `inmanta-core` package.
+For more information, have a look at [inmanta-core's GitHub page](https://github.com/inmanta/inmanta-core).
---
+++ packages/inmanta/inmanta-2021.1.tar.gz/inmanta-2021.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    version="2021.1",
+    python_requires=">=3.6",  # also update classifiers
+    # Meta data
+    name="inmanta",
+    description="Inmanta deployment tool",
+    author="Inmanta",
+    author_email="code@inmanta.com",
+    url="https://github.com/inmanta/inmanta",
+    license="Apache Software License 2",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="orchestrator orchestration configurationmanagement",
+    project_urls={
+        "Bug Tracker": "https://github.com/inmanta/inmanta-core/issues",
+        "Documentation": "https://docs.inmanta.com/community/latest/",
+    },
+    install_requires=[
+        "inmanta-core==5.0.0"
+    ],
+)
