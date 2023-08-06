10fb1b5d78c883cf61436276ca96dfbd6a9f5c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:12 +0000

    Add package oidcmsg/1.1.4/oidcmsg-1.1.4.tar.gz

---
+++ packages/oidcmsg/oidcmsg-1.1.4.tar.gz/oidcmsg-1.1.4/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Roland Hedberg, Sweden
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import re
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+__author__ = 'Roland Hedberg'
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+extra_install_requires = []
+
+with open('src/oidcmsg/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name="oidcmsg",
+    version=version,
+    description="Python implementation of OAuth2 and OpenID Connect messages",
+    author="Roland Hedberg",
+    author_email="roland@catalogix.se",
+    license="Apache 2.0",
+    url='https://github.com/IdentityPython/oidcmsg/',
+    packages=["oidcmsg", "oidcmsg/oauth2", "oidcmsg/oidc", 'oidcmsg/storage'],
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    install_requires=[
+        "cryptojwt>=1.1.0",
+        "pyOpenSSL",
+        "filelock>=3.0.12"
+    ],
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+    scripts=['tools/create_jwks.py']
+)
---
+++ packages/oidcmsg/oidcmsg-1.1.4.tar.gz/oidcmsg-1.1.4/src/oidcmsg/context.py
@@ -0,0 +1,88 @@
+import copy
+from urllib.parse import quote_plus
+
+from cryptojwt import KeyJar
+from cryptojwt.key_jar import init_key_jar
+
+from oidcmsg.message import Message
+from oidcmsg.storage.init import get_storage_conf
+from oidcmsg.storage.init import init_storage
+from oidcmsg.storage.init import storage_factory
+
+
+def add_issuer(conf, issuer):
+    res = {}
+    for key, val in conf.items():
+        if key == 'abstract_storage_cls':
+            res[key] = val
+        else:
+            _val = copy.copy(val)
+            _val['issuer'] = quote_plus(issuer)
+            res[key] = _val
+    return res
+
+
+class OidcContext:
+    def __init__(self, config=None, keyjar=None, entity_id=''):
+        if config is None:
+            config = {}
+
+        self.db_conf = config.get('db_conf')
+        if self.db_conf:
+            _iss = config.get('issuer')
+            if _iss:
+                self.db_conf = add_issuer(self.db_conf, _iss)
+
+            if self.db_conf.get('default'):
+                self.db = init_storage(self.db_conf)
+            else:
+                self.db = init_storage()
+        else:
+            self.db = init_storage()
+
+        self.keyjar = self._keyjar(keyjar, self.db_conf, config, entity_id=entity_id)
+
+    def add_boxes(self, boxes, db_conf):
+        for key, attr in boxes.items():
+            setattr(self, attr, init_storage(db_conf, key))
+
+    def _keyjar(self, keyjar=None, db_conf=None, conf=None, entity_id=''):
+        if keyjar is None:
+            _storage = None
+            if db_conf:
+                _cnf = get_storage_conf(db_conf, 'keyjar')
+                if _cnf:
+                    _storage = storage_factory(_cnf)
+
+            if 'keys' in conf:
+                args = {k: v for k, v in conf["keys"].items() if k != "uri_path"}
+                args.update({'storage': _storage})
+                _keyjar = init_key_jar(**args)
+            else:
+                _keyjar = KeyJar(storage=_storage)
+                if 'jwks' in conf:
+                    _keyjar.import_jwks(conf['jwks'], '')
+
+            if '' in _keyjar and entity_id:
+                # make sure I have the keys under my own name too (if I know it)
+                _keyjar.import_jwks_as_json(_keyjar.export_jwks_as_json(True, ''), entity_id)
+
+            _httpc_params = conf.get('httpc_params')
+            if _httpc_params:
+                _keyjar.httpc_params = _httpc_params
+
+            return _keyjar
+        else:
+            return keyjar
+
+    def set(self, item, value):
+        if isinstance(value, Message):
+            self.db[item] = value.to_dict()
+        else:
+            self.db[item] = value
+
+    def get(self, item):
+        if item == 'seed':
+            return bytes(self.db[item], 'utf-8')
+        else:
+            return self.db[item]
