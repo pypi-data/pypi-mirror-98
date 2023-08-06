f91c90b1a8157bc72a3376b73513b95d818204d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:36 +0000

    Add package odoo14-addon-pim/14.0.1.0.1.dev3/odoo14_addon_pim-14.0.1.0.1.dev3-py3-none-any.whl

---
+++ packages/odoo14-addon-pim/odoo14_addon_pim-14.0.1.0.1.dev3-py3-none-any.whl/odoo/addons/pim/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Information Management",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "https://github.com/shopinvader/odoo-pim",
+    "depends": ["product", "product_attribute_set"],
+    "data": [
+        "data/ir_module_category_data.xml",
+        "security/pim_security.xml",
+        "views/product_view.xml",
+        "views/pim_view.xml",
+        "views/attribute_set.xml",
+        "views/attribute_group.xml",
+        "views/attribute_attribute.xml",
+        "views/product_attribute_value.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "application": True,
+}
