881776cd1313185df3abc0e5d93b663b126696b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:58 +0000

    Add package eehelper/0.1.5/eehelper-0.1.5.zip

---
+++ packages/eehelper/eehelper-0.1.5.zip/eehelper-0.1.5/README.md
@@ -0,0 +1,9 @@
+## Earth Engine Helper v0.1.5
+
+[![DOI](https://zenodo.org/badge/252678920.svg)](https://zenodo.org/badge/latestdoi/252678920)
+
+Helper library for `Google Earth Engine` python API scripts
+
+Install using 
+
+`pip install eehelper`
---
+++ packages/eehelper/eehelper-0.1.5.zip/eehelper-0.1.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eehelper",
+    version="0.1.5",
+    author="Richard Massey",
+    author_email="rm885@nau.edu",
+    description="Helper library for Google Earth Engine python API scripts",
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/masseyr/eehelper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Topic :: Scientific/Engineering :: GIS',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'earthengine-api>=0.1.175',
+    ],
+    keywords='geospatial earthengine spatial google earth science satellite landsat modis',
+)
