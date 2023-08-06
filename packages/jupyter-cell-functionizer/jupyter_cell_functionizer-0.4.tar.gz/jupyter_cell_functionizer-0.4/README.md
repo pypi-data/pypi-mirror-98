7226a00eb18eadbf79e518937c7a5ed01e60fa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package jupyter-cell-functionizer/0.4/jupyter_cell_functionizer-0.4.tar.gz

---
+++ packages/jupyter-cell-functionizer/jupyter_cell_functionizer-0.4.tar.gz/jupyter_cell_functionizer-0.4/README.md
@@ -0,0 +1,125 @@
+# Jupyter/IPython Cell Functionizer
+A cell magic that converts the cell into a function for code reuse.
+
+# Installation
+```
+pip install jupyter-cell-functionizer
+```
+
+# Use case
+Imaging that you are working on a data processing project. You use the first cell to define the parameters:
+```python
+a = 1
+b = 2
+...
+```
+The processing logics are written in the next cell
+```python
+# process
+result = (a**2 + b**2) ** 1/2
+# visualize
+...
+```
+
+Later you want to try a different set of parameters. You have to copy and paste the cells and make the little changes.
+```python
+a = 2
+b = 3
+...
+```
+```python
+# process
+result = (a**2 + b**2) ** 1/2
+# visualize
+...
+```
+
+If you want to modify the processing logics, you have to apply the change to all duplications. The straightforward 
+solution is to wrapping the code in a function block
+```python
+def process(a, b):
+    result = (a**2 + b**2) ** 1/2
+    # visualize
+    ...
+    return ...
+a = 1
+b = 2
+result = process(a, b)
+a = 2
+b = 3
+result = process(a, b)
+```
+Debugging a function is much more difficult than debugging the flat code in a cell because the function will scope 
+the variables. Usually we write the cell. Test it. Then wrap it in a function. Later when something bad happens, we 
+remove the function header and remove the indent. After fixing the error, wrap it as a function again. This tedious 
+process can be automated by this magic.
+
+# Usage
+1. Load the extension
+```python
+%load_ext functionizer
+```
+2. Write some codes
+```python
+# parameter cell
+last_name = "W"
+first_name = "Y"
+```
+
+```python
+# processing cell
+name = first_name + " " + last_name
+name
+```
+3. Convert the processing cell with the magic. The first argument defines the function name. The `-a` defines the 
+   list of the function arguments. The `-r` defines the return list.
+```python
+%%functionize get_name -a first_name last_name -r name
+name = first_name + " " + last_name
+name
+```
+
+4. After this cell is executed, a function `get_name` is also defined that takes the arguments you defined and 
+   return the variable you requested.
+```python
+get_name("New name", "B")
+```
+
+# Arguments
+## function name (required)
+The name of the function to be defined in the global scope
+
+## Return as dictionary `--as_dict`
+The registered return values will be returned in a dictionary with their variable names as the keys
+
+## Input arguments `-a, --args`
+The variable names being injected as the arguments. Multiple arguments are separated by space. 
+
+If the name contains assignment such as `-a some_variable="125"`, it will be injected as a keyword argument and 
+being moved after the last positional argument. If the functionization cell is not `--skip`ped, this assignment will 
+be implicitly executed first to save the explicit assignment in the other cells.
+
+If the name ends with `!`, it will be expanded to the keyword argument using the variable of the same name in the 
+global scope. For example, `-a hello!` will be expaneded to `-a hello=hello`. This allows quick reuse of the 
+previously defined variables in the function's scope.
+
+## Return values `-r, --ret`
+The variable being returned. Multiple return values are separated by space.
+
+## Disable functionization `-d, --disable`
+Do not create the function of the cell. Only execute the cell as-is. Note that the keyword argument assignment will 
+still be effective in this mode to ensure the cell can execute consistently.
+
+## Skip cell execution `--skip`
+Do not run the cell but only create the function only.
+
+## Drop the last line `--skip_last`
+Drop the last non-blank line in the cell. This is useful the last line of the cell is an expression that outputs 
+some information, and you do not want to keep it after functionization.
+
+## Return last `--return_last`
+Use the return value of the last non-blank line as the return value. This will override the return values defined by 
+`-r, --ret`.
+
+
+
---
+++ packages/jupyter-cell-functionizer/jupyter_cell_functionizer-0.4.tar.gz/jupyter_cell_functionizer-0.4/functionizer/__init__.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+
+import ast
+
+from IPython.core.magic import Magics, magics_class, cell_magic
+from IPython.core import magic_arguments
+import re
+
+@magics_class
+class FunctionizerMagics(Magics):
+
+    def check_variable_exists(self, name, exception=False):
+        existing = name in self.shell.user_global_ns
+        if not existing and exception:
+            raise RuntimeError(f"{name} does not exist in the namespace")
+        return existing
+
+    def process_argument_list(self, arg_list):
+
+        kwargs = []
+        pargs = []
+        for arg in arg_list:
+            arg: str
+            if arg.endswith("!"):
+                # expand as keyword argument
+                arg = arg[:-1] # remove the trailing exclamation
+                kwargs.append(f"{arg}={arg}")
+                self.check_variable_exists(arg, True)
+
+            elif "=" in arg:
+                kwargs.append(arg)
+
+            elif arg.isidentifier():
+                pargs.append(arg)
+
+            else:
+                raise ValueError(f"'{arg}' is not a valid argument name.")
+        return pargs, kwargs
+
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument("--as_dict", action="store_true", default=False, help="return as a dictionary")
+    @magic_arguments.argument("-a", "--args", type=str, nargs="+", help="arguments")
+    @magic_arguments.argument("-r", "--ret", type=str, nargs="+", help="return values")
+    @magic_arguments.argument("-d", "--disable", action="store_true", default=False, help="disable")
+    @magic_arguments.argument("--skip", action="store_true", default=False, help="only define function, skip execution")
+    @magic_arguments.argument("--skip_last", action="store_true", default=False, help="drop last line")
+    @magic_arguments.argument("--return_last", action="store_true", default=False, help="return last line. will override -r")
+    @magic_arguments.argument("fn", type=str, help="function name")
+    @cell_magic
+    def functionize(self, line, cell):
+        args = magic_arguments.parse_argstring(self.functionize, line)
+        fn = args.fn
+        func_args = args.args or []
+        ret = args.ret or []
+        skip = args.skip
+        skip_last = args.skip_last
+        return_last = args.return_last
+        as_dict = args.as_dict
+        shell = self.shell
+
+        if skip_last and return_last:
+            raise UserWarning("Using skip_last and return_last together is not recommended.")
+
+        pargs, kwargs = self.process_argument_list(func_args)
+
+        # Apply the assignment of the kwargs for before executing the cell
+        assignment_code = "\n".join(kwargs) + "\n"
+
+        if args.disable:
+            shell.run_cell(assignment_code)
+            shell.run_cell(cell)
+
+        all_args = pargs + kwargs
+        arg_sig = ",".join(all_args)
+
+        lines = cell.splitlines()
+
+        # purge empty lines
+        while True:
+            last_line = lines[-1]
+            if re.search(r"\S+", last_line) or not lines:
+                # is not empty
+                break
+            lines.pop(-1)
+
+        if skip_last:
+            lines.pop(-1)
+
+        if not lines:
+            lines.append("pass")
+        else:
+            if return_last:
+                last_expr = lines.pop(-1)
+                ret_expr = f"return {last_expr}"
+            else:
+                if as_dict:
+                    ret_expr = f'return dict({",".join([f"{r}={r}" for r in ret])})'
+                else:
+                    ret_expr = f'return {",".join(ret)}'
+            lines.append(ret_expr)
+
+        indent_cell = "\n".join(["    " + l for l in lines])
+
+        function_def = (
+            f'def {fn}({arg_sig}):\n' 
+            f'{indent_cell}\n'
+        )
+
+        combined = function_def if skip else assignment_code + function_def + cell
+        shell.run_cell(combined)
+
+
+def load_ipython_extension(ipython):
+    ipython.register_magics(FunctionizerMagics)
