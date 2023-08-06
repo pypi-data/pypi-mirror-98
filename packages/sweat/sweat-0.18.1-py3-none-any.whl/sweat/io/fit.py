dc809746e81e9f67a634cc7083833a434d0c84f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:53 +0000

    Add package sweat/0.18.1/sweat-0.18.1-py3-none-any.whl

---
+++ packages/sweat/sweat-0.18.1-py3-none-any.whl/sweat/io/fit.py
@@ -0,0 +1,75 @@
+import pathlib
+
+from fitparse import FitFile
+import numpy as np
+import pandas as pd
+
+from .utils import remove_duplicate_indices, resample_data, semicircles_to_degrees
+
+
+def read_fit(fpath, resample: bool = False, interpolate: bool = False) -> pd.DataFrame:
+    """This method uses the Python fitparse library to load a FIT file into a Pandas DataFrame.
+    It is tested with a Garmin FIT file but will probably work with other FIT files too.
+    Columns names are translated to sweat terminology (e.g. "heart_rate" > "heartrate").
+
+    Args:
+        fpath: str, file-like or Path object
+        resample: whether or not the data frame needs to be resampled to 1Hz
+        interpolate: whether or not missing data in the data frame needs to be interpolated
+
+    Returns:
+        A pandas data frame with all the data.
+    """
+
+    if isinstance(fpath, pathlib.PurePath):
+        fpath = fpath.as_posix()
+
+    fitfile = FitFile(fpath)
+
+    records = []
+    lap = 0
+    session = -1
+    for record in fitfile.get_messages():
+        if record.mesg_type is None:
+            continue
+
+        if record.mesg_type.name == "record":
+            values = record.get_values()
+            values["lap"] = lap
+            values["session"] = session
+            records.append(values)
+        elif record.mesg_type.name == "lap":
+            lap += 1
+        elif record.mesg_type.name == "event":
+            if record.get_value("event_type") == "start":
+                # This happens whens an activity is (manually or automatically) paused or stopped and the resumed
+                session += 1
+        elif record.mesg_type.name == "sport":
+            # @TODO handle this to be able to return metadata
+            pass
+
+    fit_df = pd.DataFrame(records)
+
+    fit_df = fit_df.rename(
+        columns={
+            "heart_rate": "heartrate",
+            "position_lat": "latitude",
+            "position_long": "longitude",
+            "altitude": "elevation",
+            "left_right_balance": "left-right balance",
+        }
+    )
+
+    fit_df["datetime"] = pd.to_datetime(fit_df["timestamp"], utc=True)
+    fit_df = fit_df.drop(["timestamp"], axis="columns")
+    fit_df = fit_df.set_index("datetime")
+
+    for coordinate in ["latitude", "longitude"]:
+        if coordinate in fit_df.columns:
+            fit_df[coordinate] = semicircles_to_degrees(fit_df[coordinate])
+
+    fit_df = remove_duplicate_indices(fit_df)
+
+    fit_df = resample_data(fit_df, resample, interpolate)
+
+    return fit_df
---
+++ packages/sweat/sweat-0.18.1-py3-none-any.whl/sweat/io/tcx.py
@@ -0,0 +1,110 @@
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+import pandas as pd
+
+from .utils import remove_duplicate_indices, resample_data
+
+
+NAMESPACES = {
+    "default": "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2",
+    "ns5": "http://www.garmin.com/xmlschemas/ActivityGoals/v1",
+    "ns3": "http://www.garmin.com/xmlschemas/ActivityExtension/v2",
+    "ns2": "http://www.garmin.com/xmlschemas/UserProfile/v2",
+}
+
+
+def xml_find_value_or_none(element, match, namespaces=None):
+    e = element.find(match, namespaces=namespaces)
+    if e is None:
+        return e
+    else:
+        return e.text
+
+
+def read_tcx(fpath, resample: bool = False, interpolate: bool = False) -> pd.DataFrame:
+    """This method loads a TCX file into a Pandas DataFrame.
+    Columns names are translated to sweat terminology (e.g. "heart_rate" > "heartrate").
+
+    Args:
+        fpath: str, file-like or Path object
+        resample: whether or not the data frame needs to be resampled to 1Hz
+        interpolate: whether or not missing data in the data frame needs to be interpolated
+
+    Returns:
+        A pandas data frame with all the data.
+    """
+    tree = ET.parse(Path(fpath))
+    root = tree.getroot()
+    activities = root.find("default:Activities", NAMESPACES)
+
+    records = []
+    lap_no = -1
+    session = 0
+    for activity in activities.findall("default:Activity", NAMESPACES):
+        for lap in activity.findall("default:Lap", NAMESPACES):
+            lap_no += 1
+            track = lap.find("default:Track", NAMESPACES)
+            for trackpoint in track.findall("default:Trackpoint", NAMESPACES):
+                datetime = xml_find_value_or_none(
+                    trackpoint, "default:Time", NAMESPACES
+                )
+                elevation = xml_find_value_or_none(
+                    trackpoint, "default:AltitudeMeters", NAMESPACES
+                )
+                distance = xml_find_value_or_none(
+                    trackpoint, "default:DistanceMeters", NAMESPACES
+                )
+                cadence = xml_find_value_or_none(
+                    trackpoint, "default:Cadence", NAMESPACES
+                )
+
+                position = trackpoint.find("default:Position", NAMESPACES)
+                latitude = xml_find_value_or_none(
+                    position, "default:LatitudeDegrees", NAMESPACES
+                )
+                longitude = xml_find_value_or_none(
+                    position, "default:LongitudeDegrees", NAMESPACES
+                )
+
+                hr = trackpoint.find("default:HeartRateBpm", NAMESPACES)
+                heartrate = xml_find_value_or_none(hr, "default:Value", NAMESPACES)
+
+                extensions = trackpoint.find("default:Extensions", NAMESPACES)
+                if extensions:
+                    tpx = extensions.find("ns3:TPX", NAMESPACES)
+                    speed = xml_find_value_or_none(tpx, "ns3:Speed", NAMESPACES)
+                    power = xml_find_value_or_none(tpx, "ns3:Watts", NAMESPACES)
+                else:
+                    speed = None
+                    power = None
+
+                records.append(
+                    dict(
+                        datetime=datetime,
+                        latitude=latitude,
+                        longitude=longitude,
+                        elevation=elevation,
+                        heartrate=heartrate,
+                        cadence=cadence,
+                        distance=distance,
+                        speed=speed,
+                        power=power,
+                        lap=lap_no,
+                        session=session,
+                    )
+                )
+
+    tcx_df = pd.DataFrame(records)
+    tcx_df = tcx_df.dropna("columns", "all")
+    tcx_df["datetime"] = pd.to_datetime(tcx_df["datetime"], utc=True)
+    tcx_df = tcx_df.set_index("datetime")
+
+    # Convert columns to numeric if possible
+    tcx_df = tcx_df.apply(pd.to_numeric, errors="ignore")
+
+    tcx_df = remove_duplicate_indices(tcx_df)
+
+    tcx_df = resample_data(tcx_df, resample, interpolate)
+
+    return tcx_df
