1775a2676e480c05430c561068d3e2e872204de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:51 +0000

    Add package snirocr/0.1.1/snirocr-0.1.1.tar.gz

---
+++ packages/snirocr/snirocr-0.1.1.tar.gz/snirocr-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    include_package_data=True,
+    name='snirocr',
+    version='0.1.1',
+    description='automatic ocr',
+    long_description="""press the ctrl button two times, it will create a rectangle in the area you chose and ocr the image-the text will be copied to your clipboard""",
+    url='',
+    author='snir dekel',
+    author_email='snirdekel101@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='optical computer vision',
+    packages=find_packages(),
+    install_requires=['keyboard', 'pytesseract', 'pyperclip', 'Pillow', 'playsound', 'pypiwin32', 'pyautogui', 'mega.py', 'pyscreenshot']
+)
