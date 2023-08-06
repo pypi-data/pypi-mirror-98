37ec777b4ce20fa0a9c749c8c1273b388a364984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:24 +0000

    Add package 42Points/1.2.5/42Points-1.2.5-py3-none-any.whl

---
+++ packages/42Points/42Points-1.2.5-py3-none-any.whl/ftptsgame/__init__.py
@@ -0,0 +1,157 @@
+"""Main module of this project."""
+
+import datetime
+from .expr_utils import Node, build_node
+from .problem_utils import Problem
+
+
+class FTPtsGame(object):
+    """
+    The main game.
+
+    Available methods (+ means playing, - means not playing):
+    __init__(): initialization. (Entry point)
+    is_playing(): show the status of current game. (+-)
+    generate_problem(): generate a problem manually. (-)
+    get_elapsed_time(): get the time elapsed during the game. (+)
+    get_current_problem(): get current problem (tuple). (+)
+    get_current_solutions(): get current solutions (list). (+)
+    get_current_solution_number(): print current solution number. (+)
+    get_total_solution_number(): print total solution number. (+)
+    start(): start the game. (-)
+    stop(): stop the game. (+)
+    solve(): put forward a solution and show solution intervals. (+)
+    """
+
+    def __init__(self):
+        """Start the game session, serving as an initialization."""
+        self.__valid = []  # this list stores readable answers
+        self.__formula = []  # this list stores converted formulas
+        self.__players = []  # this list stores player statistics
+        self.__playing = False  # this stores playing status
+
+    def __status_check(self, required_status: bool = True):
+        """A status checker."""
+        if required_status != self.is_playing():
+            raise PermissionError('Required status: %s' % required_status)
+
+    def is_playing(self) -> bool:
+        """Indicate the game is started or not."""
+        return self.__playing
+
+    def get_elapsed_time(self) -> datetime.timedelta:
+        """Get elapsed time between solutions. Effective when playing."""
+        self.__status_check(required_status=True)
+        elapsed = datetime.datetime.now() - self.__timer
+        return elapsed
+
+    def generate_problem(self, problem, target=42):
+        """Generate a problem manually."""
+        self.__status_check(required_status=False)
+        self.__target = target
+        self.__problem = tuple(sorted(problem))
+        self.__problem_class = Problem(list(self.__problem))
+        self.__problem_class.generate_answers(self.__target)
+        if len(self.__problem_class.distinct_answer_table) == 0:
+            raise ValueError('No solution found.')
+
+    def get_current_problem(self) -> tuple:
+        """Get current problem. Effective when playing."""
+        self.__status_check(required_status=True)
+        return self.__problem
+
+    def get_current_solutions(self) -> list:
+        """Get current valid solutions. Effective when playing."""
+        self.__status_check(required_status=True)
+        return self.__valid
+
+    def get_current_solution_number(self) -> int:
+        """Get the number of current solutions. Effective when playing."""
+        self.__status_check(required_status=True)
+        return len(self.__valid)
+
+    def get_total_solution_number(self) -> int:
+        """Get the number of total solutions. Effective when playing."""
+        self.__status_check(required_status=True)
+        return len(self.__problem_class.distinct_answer_table)
+
+    def get_remaining_solutions(self) -> list:
+        """Get remaining solutions. Effective when playing."""
+        self.__status_check(required_status=True)
+        current_solution_set = set()
+        for expr_str in self.__valid:
+            node = build_node(expr_str)
+            current_solution_set.add(self.__problem_class.equivalence_dict[node.unique_id()])
+        return_list = []
+        for expr in self.__problem_class.distinct_answer_table:
+            if self.__problem_class.equivalence_dict[expr.unique_id()] not in current_solution_set:
+                return_list.append(str(expr))
+        return return_list
+
+    def get_current_player_statistics(self) -> list:
+        """Get current player statistics. Effective when playing."""
+        self.__status_check(required_status=True)
+        return self.__players
+
+    def __validate_repeated(self, node: Node):
+        """Validate distinguishing expressions. Private method."""
+        class_id = self.__problem_class.equivalence_dict[node.unique_id()]
+        for ind in range(0, len(self.__formula)):
+            cmp_node = self.__formula[ind]
+            cmp_class_id = self.__problem_class.equivalence_dict[cmp_node.unique_id()]
+            if cmp_class_id == class_id:
+                raise LookupError(self.__valid[ind])
+
+    def solve(self, math_expr: str, player_id: int = -1) -> datetime.timedelta:
+        """Put forward a solution and show solution intervals if correct."""
+        self.__status_check(required_status=True)
+
+        replace_table = [
+            ('×', '*'),
+            ('x', '*'),
+            ('÷', '/'),
+            (' ', ''),
+            ('\n', ''),
+            ('\r', ''),
+            ('（', '('),
+            ('）', ')'),
+        ]
+        for src, dest in replace_table:
+            math_expr = math_expr.replace(src, dest)
+
+        if len(math_expr) >= 30:
+            raise OverflowError('Maximum parsing length exceeded.')
+
+        node = build_node(math_expr)
+        math_expr_value = node.evaluate()
+        user_input_numbers = node.extract()
+        if math_expr_value != self.__target:
+            raise ArithmeticError(str(math_expr_value))
+        if tuple(sorted(user_input_numbers)) != self.__problem:
+            raise ValueError('Unmatched input numbers.')
+
+        self.__validate_repeated(node)
+        self.__formula.append(node)
+        self.__valid.append(math_expr)
+        elapsed = self.get_elapsed_time()
+        interval = elapsed - self.__last
+        self.__last = elapsed
+        self.__players.append((player_id, interval))
+        return interval
+
+    def start(self):
+        """Start the game. Effective when not playing."""
+        self.__status_check(required_status=False)
+        self.__valid = []
+        self.__formula = []
+        self.__players = []
+        self.__timer = datetime.datetime.now()
+        self.__last = datetime.timedelta(seconds=0)  # A tag for each solution.
+        self.__playing = True
+
+    def stop(self) -> datetime.timedelta:
+        """Stop the game. Effective when playing."""
+        self.__status_check(required_status=True)
+        elapsed = self.get_elapsed_time()
+        self.__playing = False
+        return elapsed
---
+++ packages/42Points/42Points-1.2.5-py3-none-any.whl/ftptsgame/expr_utils.py
@@ -0,0 +1,234 @@
+"""Expression utilities for 42 points."""
+
+import ast
+import itertools
+from copy import deepcopy
+from fractions import Fraction
+
+
+class Node(object):
+    """An expression tree."""
+
+    NODE_TYPE_NUMBER = 0
+    NODE_TYPE_OPERATOR = 1
+
+    def __init__(self, _type=NODE_TYPE_NUMBER, ch=None, left=None, right=None):
+        """Initialize the node."""
+        self.type = _type
+        self.left = left
+        self.right = right
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            self.value = Node.operation(ch, self.left.value, self.right.value)
+            self.ch = ch
+        else:
+            self.value = int(ch)
+            self.ch = '#'
+
+    @staticmethod
+    def operation(opt, x, y):
+        """Basic arithmetic operation between two numbers."""
+        if opt == '/' and y == 0:
+            raise ArithmeticError('x/0')
+        operation_list = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y,
+            '/': lambda x, y: Fraction(x, y)
+        }
+        return operation_list[opt](x, y)
+
+    def node_list(self) -> list:
+        """Get the list of a node."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return self.left.node_list() + [self] + self.right.node_list()
+        else:
+            return [self]
+
+    def unique_id(self) -> str:
+        """Return the unique id (postfix) of this expression."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return self.ch + self.left.unique_id() + self.right.unique_id()
+        else:
+            return '[' + str(self.value) + ']'
+
+    def __repr__(self) -> str:
+        """Return the string form of this expression."""
+        if self.type != Node.NODE_TYPE_OPERATOR:
+            return str(self.value)
+
+        deal_l = self.ch in '*/' and self.left.ch in '+-'
+        deal_r = (self.ch in '-*/' and self.right.ch in '+-') or (self.ch == '/' and self.right.ch in '*/')
+        left_string = '(' * deal_l + repr(self.left) + ')' * deal_l
+        right_string = '(' * deal_r + repr(self.right) + ')' * deal_r
+        return left_string + self.ch + right_string
+
+    def evaluate(self, values: dict = None) -> Fraction:
+        """Evaluate the value of this expression using substitution."""
+        if values is None:
+            return self.value
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return Node.operation(self.ch, self.left.evaluate(values), self.right.evaluate(values))
+        else:
+            return Fraction(values[int(self.value)])
+
+    def extract(self) -> list:
+        """Extract numbers from the node."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return self.left.extract() + self.right.extract()
+        else:
+            return [int(self.value)]
+
+    def reduce_negative_number(self):
+        """
+        Make all intermediate results of this expression not be negative.
+
+        The result of whole expression will become its absolute value.
+        """
+
+        def _neg(v1: Fraction, v2: Fraction) -> Fraction:
+            return v1 * (1 - 2 * (v2 < 0))
+
+        if self.type != Node.NODE_TYPE_OPERATOR:
+            return self.value
+
+        left_value = self.left.reduce_negative_number()
+        right_value = self.right.reduce_negative_number()
+        return_value = Node.operation(self.ch, left_value, right_value)
+
+        if self.ch not in '+-':
+            self.value = abs(return_value)
+            return return_value
+
+        char_map = {'+': 1, '-': -1, 1: '+', -1: '-'}
+
+        left_opt = 1
+        right_opt = char_map[self.ch]
+
+        left_opt = _neg(left_opt, left_value)
+        left_value = _neg(left_value, left_value)
+        right_opt = _neg(right_opt, right_value)
+        right_value = _neg(right_opt, right_value)
+        left_opt = _neg(left_opt, return_value)
+        right_opt = _neg(right_opt, return_value)
+
+        if left_opt == 1:
+            self.ch = char_map[right_opt]
+        else:
+            self.ch = '-'
+            self.left, self.right = self.right, self.left
+
+        self.value = abs(return_value)
+        return return_value
+
+    def all_equivalent_expression(self):
+        """
+        Return the list of all equivalent expression of an expression.
+
+        Rule 1 (equivalence by identical equation) is not considered.
+        If expression A induces expression B, B may not induce A.
+        """
+        if self.type != Node.NODE_TYPE_OPERATOR:
+            return
+
+        left_equal_list = self.left.all_equivalent_expression()
+        right_equal_list = self.right.all_equivalent_expression()
+        left_value, right_value = self.left.value, self.right.value
+        for new_left in left_equal_list:
+            yield Node(Node.NODE_TYPE_OPERATOR, self.ch, new_left, self.right)
+        for new_right in right_equal_list:
+            yield Node(Node.NODE_TYPE_OPERATOR, self.ch, self.left, new_right)
+
+        # Rule 2: x-0 --> x+0
+        #         x/1 --> x*1
+        #         0/x --> 0*x
+        if self.ch == '-' and right_value == 0:
+            yield Node(Node.NODE_TYPE_OPERATOR, '+', self.left, self.right)
+        if self.ch == '/' and right_value == 1:
+            yield Node(Node.NODE_TYPE_OPERATOR, '*', self.left, self.right)
+        if self.ch == '/' and left_value == 0:
+            yield Node(Node.NODE_TYPE_OPERATOR, '*', self.left, self.right)
+
+        # Rule 3: (x?y)+0 --> (x+0)?y, x?(y+0)
+        #         (x?y)*1 --> (x*1)?y, x?(y*1)
+        if ((self.ch == '+' and right_value == 0) or
+            (self.ch == '*' and right_value == 1)) \
+                and self.left.type == Node.NODE_TYPE_OPERATOR:
+            yield Node(Node.NODE_TYPE_OPERATOR, self.left.ch, Node(Node.NODE_TYPE_OPERATOR, self.ch, self.left.left,
+                                                                   self.right), self.left.right)
+            yield Node(Node.NODE_TYPE_OPERATOR, self.left.ch, self.left.left,
+                       Node(Node.NODE_TYPE_OPERATOR, self.ch, self.left.right, self.right))
+
+        # Rule 4: (y+z)/x --> (x-y)/z, (x-z)/y when x=y+z
+        if self.ch == '/' and self.left.ch == '+' and \
+                left_value == right_value and \
+                self.left.left.value != 0 and self.left.right.value != 0:
+            yield Node(Node.NODE_TYPE_OPERATOR, '/', Node(Node.NODE_TYPE_OPERATOR, '-', self.right, self.left.left),
+                       self.left.right)
+            yield Node(Node.NODE_TYPE_OPERATOR, '/', Node(Node.NODE_TYPE_OPERATOR, '-', self.right, self.left.right),
+                       self.left.left)
+
+        # Rule 5: x*(y/y) --> x+(y-y)
+        if self.ch == '*' and self.right.ch == '/' and right_value == 1:
+            yield Node(Node.NODE_TYPE_OPERATOR, '+', self.left,
+                       Node(Node.NODE_TYPE_OPERATOR, '-', self.right.left, self.right.right))
+
+        # Rule 6: x_1/x_2 --> x_2/x_1
+        if self.ch == '/' and left_value == right_value:
+            yield Node(Node.NODE_TYPE_OPERATOR, '/', self.right, self.left)
+
+        # Rule 7: Changing two sub-expressions which have the same result
+        #         doesn't change the equivalence class of this expression.
+        left_node_list = self.left.node_list()
+        right_node_list = self.right.node_list()
+        for nl, nr in itertools.product(left_node_list, right_node_list):
+            if nl.value == nr.value:
+                nl.type, nl.left, nl.right, nl.ch, nl.value, \
+                    nr.type, nr.left, nr.right, nr.ch, nr.value = \
+                    nr.type, nr.left, nr.right, nr.ch, nr.value, \
+                    nl.type, nl.left, nl.right, nl.ch, nl.value
+
+                yield deepcopy(self)
+
+                nl.type, nl.left, nl.right, nl.ch, nl.value, \
+                    nr.type, nr.left, nr.right, nr.ch, nr.value = \
+                    nr.type, nr.left, nr.right, nr.ch, nr.value, \
+                    nl.type, nl.left, nl.right, nl.ch, nl.value
+
+        # Rule 8: 2*2 --> 2+2
+        #         4/2 --> 4-2
+        if self.ch == '*' and left_value == 2 and right_value == 2:
+            yield Node(Node.NODE_TYPE_OPERATOR, '+', self.left, self.right)
+        if self.ch == '/' and left_value == 4 and right_value == 2:
+            yield Node(Node.NODE_TYPE_OPERATOR, '-', self.left, self.right)
+
+    def unique_id_for_rule_1(self, values_list: list) -> tuple:
+        """
+        Return the unique id of this expression.
+
+        Two expressions is equivalent by rule 1 iff they have the same id.
+        """
+        results = [self.evaluate(values) for values in values_list]
+        return tuple(results)
+
+
+def _build_node(node) -> Node:
+    """Convert an AST node to an expression node."""
+    node_ref = {type(ast.Add()): '+', type(ast.Sub()): '-', type(ast.Mult()): '*', type(ast.Div()): '/'}
+    if isinstance(node, ast.BinOp) and type(node.op) in node_ref:
+        built_node = Node(_type=Node.NODE_TYPE_OPERATOR,
+                          ch=node_ref[type(node.op)],
+                          left=_build_node(node.left),
+                          right=_build_node(node.right))
+    elif isinstance(node, ast.Num) and type(node.n) is int and node.n in list(range(0, 14)):
+        built_node = Node(_type=Node.NODE_TYPE_NUMBER, ch=node.n)
+    else:
+        raise SyntaxError('Unallowed operator or operands.')
+    return built_node
+
+
+def build_node(token: str) -> Node:
+    """Convert a token/string to an AST node."""
+    token_ast = ast.parse(token, mode='eval').body
+    node = _build_node(token_ast)
+    node.reduce_negative_number()
+    return node
