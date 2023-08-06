b045ac8945dd24aee98b7c2ccfc3b31ca911eeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package movekit/0.1.14/movekit-0.1.14.tar.gz

---
+++ packages/movekit/movekit-0.1.14.tar.gz/movekit-0.1.14/README.md
@@ -0,0 +1,42 @@
+MOVEKIT
+======
+
+<p align="center">
+    <img src="media/movement.png" height=150px>
+    <img src="media/voronoi.png" height=150px>
+    <img src="media/network.png" height=150px> 
+</p>
+
+`movekit` is an open-source software package for the processing and analysis of movement data.
+
+__Features:__
+
+* Data pre-processing (e.g. data checks, smoothing, duplicate removal, interpolation, outlier detection)
+* Feature extraction (e.g. speed, acceleration, heading)
+* Individual-level movement analysis (e.g. autocorrelation, speed distribution, environment exploration)
+* Group-level analysis (e.g. cohesion, polarisation, coordination, leadership)
+* Spatial data analysis (Voronoi, delaunay triangulation)
+* Network analysis with networkX
+
+---
+
+## Installation
+
+The easiest way to install *movekit* is by using `pip` :
+
+    pip install movekit
+
+---
+
+## Demo
+
+You can view a demo of common features here:
+[Jupyter Notebooks](examples/).
+
+---
+
+### License
+
+Released under a GNU General Public License. See the [LICENSE](LICENSE) file for details. List of [Authors](AUTHORS.rst)
+
+The package is funded by the Deutsche Forschungsgemeinschaft (DFG, German Research Foundation) under Germany's Excellence Strategy – EXC 2117 – 422037984.
---
+++ packages/movekit/movekit-0.1.14.tar.gz/movekit-0.1.14/src/movekit/__init__.py
@@ -0,0 +1,30 @@
+import sys
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+
+from .io import read_data  # noqa: E402
+from .preprocess import preprocess, filter_dataframe, replace_parts_animal_movement, resample_systematic, \
+    resample_random,split_trajectories,convert_measueres, interpolate, print_missing, print_duplicate, plot_missing_values  # noqa:
+# E402
+from .feature_extraction import extract_features, euclidean_dist  # noqa: E402
+from .feature_extraction import ts_feature, ts_all_features, explore_features_geospatial, centroid_medoid_computation, \
+    outlier_detection, group_movement
+from .clustering import dtw_matrix, ts_cluster, get_heading_difference, compute_polarization, get_spatial_objects, compute_centroid_direction
+# noqa: E402
+from .plot import plot_animal, plot_pace, plot_movement  # noqa: E402
+
+from .network import network_time_graphlist
