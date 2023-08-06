beb35f601397bf075f3262de4888d9816f8d5087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:36 +0000

    Add package jina/1.0.10.dev6/jina-1.0.10.dev6.tar.gz

---
+++ packages/jina/jina-1.0.10.dev6.tar.gz/jina-1.0.10.dev6/jina/hub/crafters/audio/AudioNormalizer/__init__.py
@@ -0,0 +1,24 @@
+import numpy as np
+from jina.executors.crafters import BaseCrafter
+
+
+class AudioNormalizer(BaseCrafter):
+    """:class:`AudioNormalizer` normalizes the audio signal on doc-level."""
+
+    def craft(self, blob: np.ndarray, *args, **kwargs):
+        """
+        Normalize signal from the audio signal.
+
+        Reads the `ndarray` of the audio signal,
+        normalizes the signal and saves the `ndarray` of the
+        normalized signal in the `blob` of the Document.
+
+        :param blob: the ndarray of the audio signal
+        :param args:  Additional positional arguments
+        :param kwargs: Additional keyword arguments
+        :return: a Document dict with the normalized audio signal
+        """
+        import librosa
+        signal_norm = librosa.util.normalize(blob)
+
+        return dict(offset=0, blob=signal_norm)
