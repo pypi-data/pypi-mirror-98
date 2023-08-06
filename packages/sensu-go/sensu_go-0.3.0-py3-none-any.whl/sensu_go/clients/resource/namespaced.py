0a70f8a2dc8a864f4198a0250fd663a08b78c35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:51 +0000

    Add package sensu-go/0.3.0/sensu_go-0.3.0-py3-none-any.whl

---
+++ packages/sensu-go/sensu_go-0.3.0-py3-none-any.whl/sensu_go/clients/resource/namespaced.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2020 XLAB Steampunk
+
+from typing import List, Optional, Type, TypeVar
+
+from sensu_go.clients.http.base import HTTPClient
+from sensu_go.clients.resource.base import ResourceClient
+from sensu_go.resources.base import Resource
+from sensu_go.resources.namespaced import NamespacedResource as NsResource
+from sensu_go.typing import JSONItem
+
+T = TypeVar("T", bound=Resource)
+
+
+class NamespacedClient(ResourceClient):
+    def __init__(
+        self,
+        client: HTTPClient,
+        resource_class: Type[NsResource],
+        default_namespace: str,
+    ) -> None:
+        super().__init__(client, resource_class)
+        self._default_ns = default_namespace
+
+    def create(
+        self,
+        spec: JSONItem,
+        metadata: JSONItem,
+        type: Optional[str] = None,
+    ) -> T:
+        if "namespace" not in metadata:
+            metadata = dict(metadata, namespace=self._default_ns)
+        return super().create(spec, metadata, type)
+
+    def _get_path(self, ns: Optional[str], name: Optional[str] = None) -> str:
+        return self.resource_class.get_path(namespace=ns or self._default_ns, name=name)
+
+    def list(self, namespace: Optional[str] = None) -> List[NsResource]:
+        return self.do_list(self._get_path(namespace))
+
+    def find(self, name: str, namespace: Optional[str] = None) -> Optional[NsResource]:
+        return self.do_find(self._get_path(namespace, name))
+
+    def get(self, name: str, namespace: Optional[str] = None) -> NsResource:
+        return self.do_get(self._get_path(namespace, name))
+
+    def delete(self, name: str, namespace: Optional[str] = None) -> None:
+        self.do_delete(self._get_path(namespace, name))
---
+++ packages/sensu-go/sensu_go-0.3.0-py3-none-any.whl/sensu_go/resources/hook.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2020 XLAB Steampunk
+
+from sensu_go.resources.namespaced import NamespacedResource
+from sensu_go.resources.v2 import V2Mixin
+
+
+class Hook(V2Mixin, NamespacedResource):
+    PATH_TEMPLATE = "/api/core/v2/namespaces/{namespace}/hooks"
+    TYPE = "HookConfig"
+    API_VERSION = "core/v2"
