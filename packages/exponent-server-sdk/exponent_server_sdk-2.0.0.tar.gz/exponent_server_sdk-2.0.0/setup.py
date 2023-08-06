8c84d8dc6bb26e3d38c5233c6d6d12307fd51a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:50 +0000

    Add package exponent-server-sdk/2.0.0/exponent_server_sdk-2.0.0.tar.gz

---
+++ packages/exponent-server-sdk/exponent_server_sdk-2.0.0.tar.gz/exponent_server_sdk-2.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+import re
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+README_PATH = os.path.join(HERE, 'README.md')
+try:
+    with open(README_PATH) as fd:
+        README = fd.read()
+except IOError:
+    README = ''
+
+
+setup(
+    name='exponent_server_sdk',
+    version='2.0.0',
+    description='Expo Server SDK for Python',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/expo/exponent-server-sdk-python',
+    author='Expo Team',
+    author_email='exponent.team@gmail.com',
+    license='MIT',
+    install_requires=[
+        'requests',
+        'six',
+    ],
+    packages=find_packages(),
+    zip_safe=False
+)
