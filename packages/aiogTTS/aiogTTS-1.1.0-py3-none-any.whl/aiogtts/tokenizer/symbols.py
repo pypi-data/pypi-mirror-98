48a23db325c2eaebdefea5d03f0f7575bf3df1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:51 +0000

    Add package aiogTTS/1.1.0/aiogTTS-1.1.0-py3-none-any.whl

---
+++ packages/aiogTTS/aiogTTS-1.1.0-py3-none-any.whl/aiogtts/tokenizer/symbols.py
@@ -0,0 +1,17 @@
+ABBREVIATIONS = [
+    'dr', 'jr', 'mr',
+    'mrs', 'ms', 'msgr',
+    'prof', 'sr', 'st']
+
+SUB_PAIRS = [
+    ('Esq.', 'Esquire'),
+    ('M.', 'Monsieur')
+]
+
+ALL_PUNC = '?!？！.,¡()[]¿…‥،;:—。，、：\n'
+
+TONE_MARKS = '?!？！'
+
+PERIOD_COMMA = '.,'
+
+COLON = ':'
