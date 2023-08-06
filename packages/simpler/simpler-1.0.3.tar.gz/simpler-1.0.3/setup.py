87b02fe3c02e9077e39e9f4ae4a55e5a16221631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:50 +0000

    Add package simpler/1.0.3/simpler-1.0.3.tar.gz

---
+++ packages/simpler/simpler-1.0.3.tar.gz/simpler-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+	long_description = fp.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+	requirements = f.read().strip().split()
+
+setup(
+	name='simpler',
+	version='1.0.3',
+	author='Juan C. Roldán',
+	author_email='juancarlos@sevilla.es',
+	description='Makes Python simpler.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/juancroldan/simpler',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+	],
+)
