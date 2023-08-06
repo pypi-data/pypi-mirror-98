ea4ac8e7abc475cc42657929a1f483dade80737f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package umm/0.6.0/umm-0.6.0-py2.py3-none-any.whl

---
+++ packages/umm/umm-0.6.0-py2.py3-none-any.whl/umm/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# This file is Python 2 compliant.
+
+import sys
+
+if sys.version_info[0] == 3:
+    pass
+    # from .__main__ import *
+
+else:
+    # Don't import anything.
+    pass
---
+++ packages/umm/umm-0.6.0-py2.py3-none-any.whl/umm/__main__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import click
+from click import echo
+
+from . import C
+from .commands import pip, npm, composer, brew, golang
+
+CONTEXT_SETTINGS = {
+    'ignore_unknown_options': True
+}
+
+
+@click.group(context_settings=CONTEXT_SETTINGS, help=C.DESCRIPTION)
+# @click.command()
+# @click.argument('tool')
+# @click.option('-v', '--version')
+def cli():
+    pass
+
+
+cli.add_command(pip.cli)
+cli.add_command(npm.cli)
+cli.add_command(composer.cli)
+cli.add_command(brew.cli)
+cli.add_command(golang.cli)
+
+
+@cli.command('v', short_help='Show %s version.' % C.NAME)
+def version():
+    return echo(C.VERSION)
+
+
+# @cli.command('selfupdate')
+# def selfupdate():
+#     '''Update umm'''
+#     pass
+
+
+if __name__ == '__main__':
+    pass
+    cli()
