01e88f46b82a3bd301a05bcb99c28c4810ba4c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:29 +0000

    Add package iiapds/0.0.2/iiapds-0.0.2.tar.gz

---
+++ packages/iiapds/iiapds-0.0.2.tar.gz/iiapds-0.0.2/iiapds/node.py
@@ -0,0 +1,9 @@
+class Node():
+	"""docstring for ClassName"""
+	def __init__(self,val=None,args=None):
+		# Basic Attributes
+		self.left = None
+		self.right = None
+		self.val = val
+		# Advanced Attributes
+		#self.name = None
