d6bfc15650538a62ba931021d87c58c7b9109611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package network-serializer/1.0.1/network_serializer-1.0.1.tar.gz

---
+++ packages/network-serializer/network_serializer-1.0.1.tar.gz/network_serializer-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+with open('README.md') as rf:
+    readme = rf.read()
+
+setup(
+  name = 'network_serializer',
+  packages = ['network_serializer'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'A Python module to help encode and decode network packets.',
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  author = 'Alvin Lin',
+  author_email = 'hungyeh.alvin.lin@gmail.com',
+  url = 'https://github.com/bubblemans/network-serializer',
+  download_url = 'https://github.com/bubblemans/network-serializer/archive/v1.0.1.tar.gz',
+  keywords = ['network', 'serilize', 'serializer', 'encode', 'decode', 'packet'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
