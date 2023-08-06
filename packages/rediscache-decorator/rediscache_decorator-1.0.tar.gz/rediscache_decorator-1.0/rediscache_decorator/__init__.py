b2d31959600dc198e6019f231f07f51c796f0e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package rediscache-decorator/1.0/rediscache_decorator-1.0.tar.gz

---
+++ packages/rediscache-decorator/rediscache_decorator-1.0.tar.gz/rediscache_decorator-1.0/rediscache_decorator/__init__.py
@@ -0,0 +1,7 @@
+from rediscache_decorator.CacheDecorator import Cache
+
+
+__title__ = 'rediscache_decorator'
+__author__ = 'Arthur RENAUD'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2021 Arthur RENAUD and Contributors'
