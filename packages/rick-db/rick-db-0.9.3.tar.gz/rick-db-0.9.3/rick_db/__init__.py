be066e4b0cd679f0efab9304f06da190e6b72a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:52 +0000

    Add package rick-db/0.9.3/rick-db-0.9.3.tar.gz

---
+++ packages/rick-db/rick-db-0.9.3.tar.gz/rick-db-0.9.3/rick_db/__init__.py
@@ -0,0 +1,3 @@
+from .version import get_version
+from .mapper import Record, fieldmapper, RecordError
+from .repository import Repository, RepositoryError
---
+++ packages/rick-db/rick-db-0.9.3.tar.gz/rick-db-0.9.3/rick_db/version.py
@@ -0,0 +1,5 @@
+RICK_DB_VERSION = ["0", "9", "3"]
+
+
+def get_version():
+    return ".".join(RICK_DB_VERSION)
