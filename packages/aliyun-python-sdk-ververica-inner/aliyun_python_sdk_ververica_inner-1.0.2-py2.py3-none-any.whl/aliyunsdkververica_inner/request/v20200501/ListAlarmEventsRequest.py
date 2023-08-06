8f13ac2b9705701e32286f95d506c8c7801fad71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:29 +0000

    Add package aliyun-python-sdk-ververica-inner/1.0.2/aliyun_python_sdk_ververica_inner-1.0.2-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-ververica-inner/aliyun_python_sdk_ververica_inner-1.0.2-py2.py3-none-any.whl/aliyunsdkververica_inner/request/v20200501/ListAlarmEventsRequest.py
@@ -0,0 +1,63 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RoaRequest
+
+class ListAlarmEventsRequest(RoaRequest):
+
+	def __init__(self):
+		RoaRequest.__init__(self, 'ververica-inner', '2020-05-01', 'ListAlarmEvents')
+		self.set_uri_pattern('/pop/workspaces/[workspace]/alert/v1/namespaces/[namespace]/deployments/[deploymentId]/events')
+		self.set_method('GET')
+
+	def get_workspace(self):
+		return self.get_path_params().get('workspace')
+
+	def set_workspace(self,workspace):
+		self.add_path_param('workspace',workspace)
+
+	def get_deploymentId(self):
+		return self.get_path_params().get('deploymentId')
+
+	def set_deploymentId(self,deploymentId):
+		self.add_path_param('deploymentId',deploymentId)
+
+	def get_namespace(self):
+		return self.get_path_params().get('namespace')
+
+	def set_namespace(self,namespace):
+		self.add_path_param('namespace',namespace)
+
+	def get_alarmId(self):
+		return self.get_query_params().get('alarmId')
+
+	def set_alarmId(self,alarmId):
+		self.add_query_param('alarmId',alarmId)
+
+	def get_endTime(self):
+		return self.get_query_params().get('endTime')
+
+	def set_endTime(self,endTime):
+		self.add_query_param('endTime',endTime)
+
+	def get_startTime(self):
+		return self.get_query_params().get('startTime')
+
+	def set_startTime(self,startTime):
+		self.add_query_param('startTime',startTime)
