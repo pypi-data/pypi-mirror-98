1c3a5aee4e441ce0fa20641720e9d826062121b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:30 +0000

    Add package energy-pandas/0.2.1/energy_pandas-0.2.1-py3-none-any.whl

---
+++ packages/energy-pandas/energy_pandas-0.2.1-py3-none-any.whl/energy_pandas/__init__.py
@@ -0,0 +1,18 @@
+"""energy-pandas Module."""
+
+from outdated import warn_if_outdated
+
+from .energypandas import EnergySeries, EnergyDataFrame
+from pkg_resources import get_distribution, DistributionNotFound
+
+# Version of the package
+try:
+    __version__ = get_distribution("energy-pandas").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "0.0.0"  # should happen only if package is copied, not installed.
+
+# warn if a newer version of archetypal is available
+warn_if_outdated("energy-pandas", __version__)
+
+__all__ = ["EnergySeries", "EnergyDataFrame"]
