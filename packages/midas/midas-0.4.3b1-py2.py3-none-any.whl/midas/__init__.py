eec32386fd2f682d7acf5d91a8ac744f46e2f3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package midas/0.4.3b1/midas-0.4.3b1-py2.py3-none-any.whl

---
+++ packages/midas/midas-0.4.3b1-py2.py3-none-any.whl/midas/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""
+A Python driver for Honeywell's Midas gas detector, using TCP/IP modbus.
+
+Distributed under the GNU General Public License v2
+Copyright (C) 2019 NuMat Technologies
+"""
+from midas.driver import GasDetector
+
+
+def command_line():
+    """Command-line tool for Midas gas detector communication."""
+    import argparse
+    import asyncio
+    import json
+
+    parser = argparse.ArgumentParser(description="Read a Honeywell Midas gas "
+                                     "detector state from the command line.")
+    parser.add_argument('address', help="The IP address of the gas detector.")
+    args = parser.parse_args()
+
+    async def get():
+        async with GasDetector(args.address) as detector:
+            print(json.dumps(await detector.get(), indent=4, sort_keys=True))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get())
+    loop.close()
+
+
+if __name__ == '__main__':
+    command_line()
