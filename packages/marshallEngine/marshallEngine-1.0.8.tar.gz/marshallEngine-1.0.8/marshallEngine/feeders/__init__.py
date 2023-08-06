341d810a67d4cfa8f07e7803153175e19e028233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:13 +0000

    Add package marshallEngine/1.0.8/marshallEngine-1.0.8.tar.gz

---
+++ packages/marshallEngine/marshallEngine-1.0.8.tar.gz/marshallEngine-1.0.8/marshallEngine/feeders/__init__.py
@@ -0,0 +1,6 @@
+"""
+*Import codes for the various transients surveys that 'feed' the marshall inbox*
+"""
+from __future__ import absolute_import
+from .data import data
+from .images import images
