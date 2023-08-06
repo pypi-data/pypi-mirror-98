1699af11b5d843a550969d941b7904cf8b9e0853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:43 +0000

    Add package validate-plist-xml/0.0.4/validate_plist_xml-0.0.4-py3-none-any.whl

---
+++ packages/validate-plist-xml/validate_plist_xml-0.0.4-py3-none-any.whl/validate_plist_xml/__main__.py
@@ -0,0 +1,7 @@
+"""
+To run this module directly
+"""
+# https://stackoverflow.com/questions/44977227/how-to-configure-main-py-init-py-and-setup-py-for-a-basic-package
+from validate_plist_xml import main
+
+main()
