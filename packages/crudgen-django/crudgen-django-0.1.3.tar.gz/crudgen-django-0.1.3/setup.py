26b7dc1763f9141bcd7eebf5d3c8bc78060491b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:58 +0000

    Add package crudgen-django/0.1.3/crudgen-django-0.1.3.tar.gz

---
+++ packages/crudgen-django/crudgen-django-0.1.3.tar.gz/crudgen-django-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crudgen_django', 'crudgen_django.console', 'crudgen_django.hacks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django<3.0', 'crudgen>=0.1.1,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['generate = crudgen_django.console.generate:main']}
+
+setup_kwargs = {
+    'name': 'crudgen-django',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'pouria',
+    'author_email': 'pooriazmn@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
