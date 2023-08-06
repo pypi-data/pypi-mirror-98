8ad7f56a691dce6cdcb24cb9990551343c4f2e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:30 +0000

    Add package ai-django-core/4.2.1/ai-django-core-4.2.1.tar.gz

---
+++ packages/ai-django-core/ai-django-core-4.2.1.tar.gz/ai-django-core-4.2.1/ai_django_core/utils/model.py
@@ -0,0 +1,31 @@
+from django.db.models import ForeignKey
+
+
+def object_to_dict(obj, blacklisted_fields: list = None, include_id: bool = False) -> dict:
+    """
+    Returns a dict with all data defined in the model class as a key-value-dict
+    Attention: Does not work for M2M fields!
+    """
+
+    # Default blacklist
+    blacklisted_fields = blacklisted_fields if blacklisted_fields else []
+
+    # Add default django primary key to blacklist
+    if not include_id:
+        blacklisted_fields.append('id')
+
+    data = vars(obj)
+    valid_data = {}
+
+    valid_fields = []
+    for f in obj.__class__._meta.get_fields():
+        if type(f) != ForeignKey:
+            valid_fields.append(f.name)
+        else:
+            valid_fields.append(f'{f.name}_id')
+
+    for key, value in list(data.items()):
+        if key in valid_fields and key not in blacklisted_fields:
+            valid_data[key] = value
+
+    return valid_data
