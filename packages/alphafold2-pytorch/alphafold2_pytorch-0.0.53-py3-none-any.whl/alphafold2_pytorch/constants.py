da361080898e37cf63494acbd2c5813dc917d9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package alphafold2-pytorch/0.0.53/alphafold2_pytorch-0.0.53-py3-none-any.whl

---
+++ packages/alphafold2-pytorch/alphafold2_pytorch-0.0.53-py3-none-any.whl/alphafold2_pytorch/constants.py
@@ -0,0 +1,14 @@
+import torch
+
+# constants
+
+MAX_NUM_MSA = 20
+MAX_NUM_TEMPLATES = 10
+NUM_AMINO_ACIDS = 21
+NUM_EMBEDDS_TR = 1280 # best esm model 
+DISTOGRAM_BUCKETS = 37
+
+# default device
+
+DEVICE_NAME = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = torch.device(DEVICE_NAME)
