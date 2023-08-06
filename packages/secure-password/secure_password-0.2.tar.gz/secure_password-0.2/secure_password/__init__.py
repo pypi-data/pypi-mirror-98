d2b5f13c43932fa58acdaaa5b6621ddc9116bbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:02 +0000

    Add package secure-password/0.2/secure_password-0.2.tar.gz

---
+++ packages/secure-password/secure_password-0.2.tar.gz/secure_password-0.2/secure_password/__init__.py
@@ -0,0 +1,14 @@
+import random
+
+def print(text):
+    return text
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456!£$%^&*(`)"
+
+def gen_password(password_len, password_count):
+        for x in range(0,password_count):
+            password  = ""
+            for x in range(0,password_len):
+                password_char = random.choice(chars)
+                password      = password + password_char
+            print(password) 
