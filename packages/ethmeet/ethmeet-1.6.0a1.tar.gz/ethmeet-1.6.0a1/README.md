0fee6a81bc7a89c6ff9418087334985436477351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:45 +0000

    Add package ethmeet/1.6.0a1/ethmeet-1.6.0a1.tar.gz

---
+++ packages/ethmeet/ethmeet-1.6.0a1.tar.gz/ethmeet-1.6.0a1/README.md
@@ -0,0 +1,28 @@
+# ethmeet
+'ethmeet' stands for 'Ethical Meeting'.
+>
+Video-communication webdriver library. API compatible with most famous platforms.
+
+## Installation
+Pip:
+```shell script
+pip3 install ethmeet && python3 /usr/local/bin/gecko_install.py
+```
+> NOTE: ethmeet uses Firefox as its standard browser
+
+## Docs
+- [ethmeet Basic Usage & API Reference](https://github.com/elleaech/ethmeet/blob/master/docs/ethmeet.md)
+
+## Contributing & Issue Tracker
+Branch & [Pull Request](https://github.com/elleaech/ethmeet/pulls)
+- [Issues](https://github.com/elleaech/ethmeet/issues)
+
+### Get source
+```shell script
+git clone git@github.com:elleaech/ethmeet.git && cd ethmeet
+
+python3 -m virtualenv . && pip3 install -r requirements.txt
+```
+
+## License
+[Apache License 2.0](https://github.com/elleaech/ethmeet/blob/master/LICENSE)
---
+++ packages/ethmeet/ethmeet-1.6.0a1.tar.gz/ethmeet-1.6.0a1/setup.py
@@ -0,0 +1,57 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ethmeet",
+    version="1.6.0-a.1",
+    author="Lo Han",
+    author_email="lohan.uchsa@protonmail.com",
+    description="'ethmeet' stands for 'Ethical Meeting'. A video-communication webdriver library. API compatible with most famous platforms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elleaech/ethmeet",
+    packages=setuptools.find_packages(),
+    keywords="bot firefox automation browser selenium meeting zoom google",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.8',
+    scripts=[
+        'scripts/gecko_install.py',
+    ],
+    install_requires=[
+        'bleach==3.3.0',
+        'build==0.2.1',
+        'certifi==2020.12.5',
+        'cffi==1.14.5',
+        'chardet==4.0.0',
+        'colorama==0.4.4',
+        'cryptography==3.4.4',
+        'docutils==0.16',
+        'idna==2.10',
+        'jeepney==0.6.0',
+        'keyring==22.0.1',
+        'packaging==20.9',
+        'pep517==0.9.1',
+        'pkginfo==1.7.0',
+        'pycparser==2.20',
+        'Pygments==2.7.4',
+        'pyparsing==2.4.7',
+        'readme-renderer==28.0',
+        'requests==2.25.1',
+        'requests-toolbelt==0.9.1',
+        'rfc3986==1.4.0',
+        'SecretStorage==3.3.1',
+        'selenium==3.141.0',
+        'six==1.15.0',
+        'toml==0.10.2',
+        'tqdm==4.56.2',
+        'twine==3.3.0',
+        'urllib3==1.26.3',
+        'webencodings==0.5.1'
+    ]
+)
