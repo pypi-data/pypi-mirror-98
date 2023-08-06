96f83e9a0f10dd58f57fecd3d195150035c93682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:06 +0000

    Add package album-sender/0.0.32/album_sender-0.0.32.tar.gz

---
+++ packages/album-sender/album_sender-0.0.32.tar.gz/album_sender-0.0.32/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="album_sender",
+    version="0.0.32",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Telegram album sender.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/telegram_album_sender",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'cached_url',
+        'telegram_util',
+        'pic_cut',
+    ],
+    python_requires='>=3.0',
+)
