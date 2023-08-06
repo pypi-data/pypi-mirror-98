fc11f6f4595a14624db865766d2a72d6a70ef7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:44 +0000

    Add package lapixdl/0.7.9/lapixdl-0.7.9.tar.gz

---
+++ packages/lapixdl/lapixdl-0.7.9.tar.gz/lapixdl-0.7.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lapixdl',
+    packages=find_packages(exclude=['tests']),
+    version='0.7.9',
+    description='Utils for Computer Vision Deep Learning research',
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lapix-ufsc/lapixdl",
+
+    author='LAPiX',
+    license='MIT',
+
+    install_requires=['numpy', 'tqdm', 'seaborn', 'pandas', 'matplotlib'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'cv2'],
+    test_suite='tests',
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
