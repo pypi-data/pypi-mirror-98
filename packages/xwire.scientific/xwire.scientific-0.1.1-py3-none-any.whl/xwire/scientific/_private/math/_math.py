5ffe2df971c86b17a856e10cae2bc919b144326a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:23 +0000

    Add package xwire.scientific/0.1.1/xwire.scientific-0.1.1-py3-none-any.whl

---
+++ packages/xwire.scientific/xwire.scientific-0.1.1-py3-none-any.whl/xwire/scientific/_private/math/_math.py
@@ -0,0 +1,14 @@
+import math
+
+
+def log_return(initial: float, final: float, period: float, decimals=None) -> float:
+    if initial == 0 and final == 0:
+        return 0
+    elif initial == 0 and final > 0:
+        return math.inf
+    else:
+        return_value = math.log(final/initial) / period
+        if decimals is not None:
+            return round(return_value, decimals)
+        else:
+            return return_value
