f8b58571305013e295efcbf8b50fe134ffb8aa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:49 +0000

    Add package Orange3-zh/3.27.4/Orange3_zh-3.27.4-cp37-cp37m-win_amd64.whl

---
+++ packages/Orange3-zh/Orange3_zh-3.27.4-cp37-cp37m-win_amd64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.27.4'
+version = '3.27.4'
+full_version = '3.27.4'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
---
+++ packages/Orange3-zh/Orange3_zh-3.27.4-cp37-cp37m-win_amd64.whl/Orange/widgets/model/tests/test_owlinearregression.py
@@ -0,0 +1,13 @@
+import unittest
+from Orange.widgets.model.owlinearregression import OWLinearRegression
+from Orange.widgets.tests.base import WidgetTest, WidgetLearnerTestMixin
+
+
+class TestOWLinearRegression(WidgetTest, WidgetLearnerTestMixin):
+    def setUp(self):
+        self.widget = self.create_widget(OWLinearRegression,
+                                         stored_settings={"auto_apply": False})
+        self.init()
+
+if __name__ == "__main__":
+    unittest.main()
