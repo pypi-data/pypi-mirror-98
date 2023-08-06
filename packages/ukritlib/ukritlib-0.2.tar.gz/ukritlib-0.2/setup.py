af6b70379be92debc772149ce805501579f87ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:45 +0000

    Add package ukritlib/0.2/ukritlib-0.2.tar.gz

---
+++ packages/ukritlib/ukritlib-0.2.tar.gz/ukritlib-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ukritlib',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/ukritfongsomboon/ukritlib',
+      author='ukrit Fongsomboon',
+      author_email='ukrit.fb@gmail.com',
+      license='MIT',
+      packages=['ukritlib'],
+      zip_safe=False)
