a2db99063dde686fdd19d5a2fbf322511a941472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package geoviews/1.9.0a2/geoviews-1.9.0a2.tar.gz

---
+++ packages/geoviews/geoviews-1.9.0a2.tar.gz/geoviews-1.9.0a2/geoviews/dist/lib/models/index.js
@@ -0,0 +1,6 @@
+export { CheckpointTool } from "./checkpoint_tool";
+export { ClearTool } from "./clear_tool";
+export { PolyVertexDrawTool } from "./poly_draw";
+export { PolyVertexEditTool } from "./poly_edit";
+export { RestoreTool } from "./restore_tool";
+//# sourceMappingURL=index.js.map
---
+++ packages/geoviews/geoviews-1.9.0a2.tar.gz/geoviews-1.9.0a2/geoviews/streams.py
@@ -0,0 +1,41 @@
+from holoviews.streams import PolyEdit, PolyDraw
+
+
+class PolyVertexEdit(PolyEdit):
+    """
+    Attaches a PolyVertexEditTool and syncs the datasource.
+
+    shared: boolean
+        Whether PolyEditTools should be shared between multiple elements
+
+    node_style: dict
+        A dictionary specifying the style options for the intermediate nodes.
+
+    feature_style: dict
+        A dictionary specifying the style options for the intermediate nodes.
+    """
+
+    def __init__(self, node_style={}, feature_style={}, **params):
+        self.node_style = node_style
+        self.feature_style = feature_style
+        super(PolyVertexEdit, self).__init__(**params)
+
+
+class PolyVertexDraw(PolyDraw):
+    """
+    Attaches a PolyVertexDrawTool and syncs the datasource.
+
+    shared: boolean
+        Whether PolyEditTools should be shared between multiple elements
+
+    node_style: dict
+        A dictionary specifying the style options for the intermediate nodes.
+
+    feature_style: dict
+        A dictionary specifying the style options for the intermediate nodes.
+    """
+
+    def __init__(self, node_style={}, feature_style={}, **params):
+        self.node_style = node_style
+        self.feature_style = feature_style
+        super(PolyVertexDraw, self).__init__(**params)
