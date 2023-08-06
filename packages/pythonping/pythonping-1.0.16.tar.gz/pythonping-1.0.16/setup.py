b3231a52ad206862d9c53d711e84ee26d8228697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:35 +0000

    Add package pythonping/1.0.16/pythonping-1.0.16.tar.gz

---
+++ packages/pythonping/pythonping-1.0.16.tar.gz/pythonping-1.0.16/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(name='pythonping',
+      version='1.0.16',
+      description='A simple way to ping in Python',
+      url='https://github.com/alessandromaggio/pythonping',
+      author='Alessandro Maggio',
+      author_email='me@alessandromaggio.com',
+      license='MIT',
+      packages=['pythonping'],
+      keywords=['ping', 'icmp', 'network'],
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: System Administrators',
+            'Natural Language :: English'
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
