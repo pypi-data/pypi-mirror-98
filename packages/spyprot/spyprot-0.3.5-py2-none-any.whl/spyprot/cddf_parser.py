67d2861826ff749d8e4c8ed245abea5897e9a205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:25 +0000

    Add package spyprot/0.3.5/spyprot-0.3.5-py2-none-any.whl

---
+++ packages/spyprot/spyprot-0.3.5-py2-none-any.whl/spyprot/cddf_parser.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib2 as urllib
+from urllib import urlretrieve
+from os import path
+from re import compile
+from spyprot import Command
+import os
+
+DEFAULT_DATA_FILE_PATH = path.join(path.expanduser("~"), ".local", "spyprot")
+hcath_tmp = DEFAULT_DATA_FILE_PATH + '/cath-domain-description-file.txt_tmp'
+hcath = DEFAULT_DATA_FILE_PATH + '/cath-domain-description-file.txt'
+cath_uri = \
+    'http://download.cathdb.info/cath/releases/latest-release/cath-classification-data/cath-domain-description-file.txt'
+
+
+# egrep -v "^NAME|^DLEN|^S|^VER|^NSE|^FOR|^DSEQ" CathDomainDescriptionFile_org >CathDomainDescriptionFile
+
+
+def getCath(pdbcode, chain):
+    '''Retrieve CATH annotations for a given PDBID and Chain
+       Supported attributes:
+       CLASS
+       CATHCODE
+       DOMAIN
+       ARCH
+       TOPOL
+       HOMOL
+
+       Annotations are parsed from a cath-domain-description file downloaded from cathdb and stored locally in:
+       $HOME/.local/spyprot/
+
+       Parameters
+       ==========
+       pdbcode: string
+       chain: string
+
+       Return
+       ======
+       results: list of CATH annotations
+       '''
+    if not path.isfile(hcath):
+        if not path.exists(DEFAULT_DATA_FILE_PATH):
+            os.makedirs(DEFAULT_DATA_FILE_PATH)
+        urlretrieve(cath_uri, hcath_tmp)
+        cmd = Command('egrep -v "^NAME|^DLEN|^S|^VER|^NSE|^FOR|^DSEQ" ' + hcath_tmp + '>' + hcath)
+        cmd.run(5000)
+        os.remove(hcath_tmp)
+    pdbcode = pdbcode.lower()
+    chain = chain
+    record = compile(r'^DOMAIN    ' + pdbcode + chain)
+    results = []
+    with open(hcath, 'r') as cddf:
+        found = False
+        for line in cddf:
+            if record.match(line):
+                found = True
+                recorddd = {'pdbcode': pdbcode, 'chain': chain}
+            elif 'ENDSEG' in line:
+                if found:
+                    results.append(recorddd)
+                found = False
+            if found:
+                key = line[:10].strip()
+                if key not in recorddd:  # only one line..
+                    recorddd[key] = line[10:].strip()
+    return results
+
+
+if __name__ == "__main__":
+    d = getCath("1j85", "A")
+    print(d)
