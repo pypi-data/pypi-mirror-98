3fd223059ce8128834b63526492344a8e6a3dbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package jina/1.0.9.dev26/jina-1.0.9.dev26.tar.gz

---
+++ packages/jina/jina-1.0.9.dev26.tar.gz/jina-1.0.9.dev26/jina/drivers/craft.py
@@ -0,0 +1,25 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import Optional
+
+from . import FlatRecursiveMixin, BaseExecutableDriver
+
+if False:
+    from .. import DocumentSet
+
+
+class CraftDriver(FlatRecursiveMixin, BaseExecutableDriver):
+    """Drivers inherited from this Driver will bind :meth:`craft` by default """
+
+    def __init__(
+        self, executor: Optional[str] = None, method: str = 'craft', *args, **kwargs
+    ):
+        super().__init__(executor, method, *args, **kwargs)
+
+    def _apply_all(self, docs: 'DocumentSet', *args, **kwargs):
+        for doc in docs:
+            _args_dict = doc.get_attrs(*self.exec.required_keys)
+            ret = self.exec_fn(**_args_dict)
+            if ret:
+                doc.set_attrs(**ret)
