03abeb3919a954c512e569f97bb8c2c51b5eec8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:01 +0000

    Add package celerite2/0.2.0/celerite2-0.2.0-cp39-cp39-win_amd64.whl

---
+++ packages/celerite2/celerite2-0.2.0-cp39-cp39-win_amd64.whl/celerite2/theano/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["terms", "GaussianProcess", "CeleriteNormal"]
+
+from . import terms
+from .celerite2 import GaussianProcess
+from .distribution import CeleriteNormal
