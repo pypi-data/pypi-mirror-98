b470176cd0d11bc9ad93d7ae065eb6e4b255b0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:32 +0000

    Add package netbox-cable-expansion/0.0.1/netbox-cable-expansion-0.0.1.tar.gz

---
+++ packages/netbox-cable-expansion/netbox-cable-expansion-0.0.1.tar.gz/netbox-cable-expansion-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netbox_cable_expansion']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'netbox-cable-expansion',
+    'version': '0.0.1',
+    'description': 'reservation',
+    'long_description': None,
+    'author': 'Jonas Krüger Svensson',
+    'author_email': 'jonas-ks@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
