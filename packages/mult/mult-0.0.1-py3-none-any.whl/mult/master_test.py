9be972f467e2af40cc52c8819dc8a191361d4346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package mult/0.0.1/mult-0.0.1-py3-none-any.whl

---
+++ packages/mult/mult-0.0.1-py3-none-any.whl/mult/master_test.py
@@ -0,0 +1,20 @@
+
+# ファイル単位並列計算 [mult]
+
+import sys
+from sout import sout
+from relpath import add_import_path
+add_import_path("../")
+from _develop_mult import master
+
+input_ls = [i for i in range(10)]
+
+# ファイル単位並列計算 (マスター側) [mult]
+output_ls = master.call(
+	"./slave_test/slave_test.py",	# スレーブファイル名
+	input_ls,	# 入力のリスト (ジョブごと)
+	n = 4,	# 並列数
+	progress = True	# 進捗表示
+)
+
+sout(output_ls, None)
