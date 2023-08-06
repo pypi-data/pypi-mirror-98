04b95c54cf14cacee59e903f4db8533729952f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:40 +0000

    Add package anal/0.2.0/anal-0.2.0.tar.gz

---
+++ packages/anal/anal-0.2.0.tar.gz/anal-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='anal',
+    packages=['anal'],
+    install_requires=[],
+
+    version='0.2.0',
+    license='MIT',
+
+    author='Tatsuya Abe',
+    author_email='abe12@mccc.jp',
+
+    url='https://github.com/averak/anal',
+
+    desription='Utility to standard output based on template and file.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='stdout',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
