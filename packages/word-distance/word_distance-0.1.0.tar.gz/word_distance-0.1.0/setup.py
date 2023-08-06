19a031c59e52cd03b1eff0b3f1eb2888393cfe72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package word-distance/0.1.0/word_distance-0.1.0.tar.gz

---
+++ packages/word-distance/word_distance-0.1.0.tar.gz/word_distance-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='word_distance',
+      version='0.1.0',
+      author='Rui Ren',
+      description='word distance wrapper for log analysis',
+      packages=['word_distance'],
+      zip_safe=False
+)
