fc806881c17ab0b5f3f72e529d211988203814e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:07 +0000

    Add package vpype-gcode/0.8.0/vpype-gcode-0.8.0.tar.gz

---
+++ packages/vpype-gcode/vpype-gcode-0.8.0.tar.gz/vpype-gcode-0.8.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="vpype-gcode",
+    version="0.8.0",
+    description="vpype gcode plugin",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Tatarize",
+    author_email="tatarize@gmail.com",
+    url="https://github.com/tatarize/vpype-gcode/",
+    packages=["vpype_gcode"],
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "vpype",
+        "numpy",
+    ],
+    entry_points="""
+            [vpype.plugins]
+            gwrite=vpype_gcode.gwrite:gwrite
+        """,
+)
