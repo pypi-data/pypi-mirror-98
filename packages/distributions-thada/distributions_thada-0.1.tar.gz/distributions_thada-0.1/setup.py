2856609afab95a5ceb4f1dbc0baea2fd888730a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:21 +0000

    Add package distributions-thada/0.1/distributions_thada-0.1.tar.gz

---
+++ packages/distributions-thada/distributions_thada-0.1.tar.gz/distributions_thada-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_thada',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_thada'],
+      zip_safe=False)
