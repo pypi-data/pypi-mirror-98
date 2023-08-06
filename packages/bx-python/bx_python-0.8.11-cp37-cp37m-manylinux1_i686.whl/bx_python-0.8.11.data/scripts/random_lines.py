78c506fd7cc4241eb72a33a2c89c92afa34e6237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:17 +0000

    Add package bx-python/0.8.11/bx_python-0.8.11-cp37-cp37m-manylinux1_i686.whl

---
+++ packages/bx-python/bx_python-0.8.11-cp37-cp37m-manylinux1_i686.whl/bx_python-0.8.11.data/scripts/random_lines.py
@@ -0,0 +1,16 @@
+#!python
+
+"""
+Script to select random lines from a file. Reads entire file into
+memory!
+
+TODO: Replace this with a more elegant implementation.
+"""
+
+import random
+import sys
+
+ndesired = int(sys.argv[1])
+
+for line in random.sample(sys.stdin.readlines(), ndesired):
+    print(line, end=' ')
