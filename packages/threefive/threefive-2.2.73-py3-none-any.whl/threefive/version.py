4e8aed758fb499822e4900b49a03e408cbc3354f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:36 +0000

    Add package threefive/2.2.73/threefive-2.2.73-py3-none-any.whl

---
+++ packages/threefive/threefive-2.2.73-py3-none-any.whl/threefive/version.py
@@ -0,0 +1,26 @@
+"""
+threeefive.version
+
+Used to set version in setup.py
+and as an easy way to check which
+version you have installed.
+"""
+version_tuple = (
+    "2.2.73",
+    "Two Two seventy-three",
+)
+
+
+def version():
+    """
+    version is set in the version_tuple
+    to make it immutable.
+    """
+    return version_tuple[0]
+
+
+def full_version():
+    """
+    returns full version tuple
+    """
+    return version_tuple
