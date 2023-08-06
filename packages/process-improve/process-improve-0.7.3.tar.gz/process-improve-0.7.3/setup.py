2467689504f8ed2c0c44239c0a1026cd03453ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:04 +0000

    Add package process-improve/0.7.3/process-improve-0.7.3.tar.gz

---
+++ packages/process-improve/process-improve-0.7.3.tar.gz/process-improve-0.7.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="process-improve",
+    version="0.7.3",
+    author="Kevin Dunn",
+    author_email="kgdunn@gmail.com",
+    description="Process Improvement using Data, particularly Designed Experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kgdunn/process_improve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "statsmodels",
+        "matplotlib",
+        "bokeh",
+        "sklearn",
+        "patsy",
+        "scikit-image",
+    ],
+)
