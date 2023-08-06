0047e53863f5a0a939cc14c72cc98a9841f0261e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:26 +0000

    Add package cloudcomponents.cdk-lambda-at-edge-pattern/1.29.0/cloudcomponents.cdk_lambda_at_edge_pattern-1.29.0-py3-none-any.whl

---
+++ packages/cloudcomponents.cdk-lambda-at-edge-pattern/cloudcomponents.cdk_lambda_at_edge_pattern-1.29.0-py3-none-any.whl/cloudcomponents/cdk_lambda_at_edge_pattern/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_ssm._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-lambda-at-edge-pattern",
+    "1.29.0",
+    __name__[0:-6],
+    "cdk-lambda-at-edge-pattern@1.29.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
