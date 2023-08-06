3fbd069f694ae49ba3630b01f812491c61354cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:03 +0000

    Add package akerbp.mlops/0.20210315135618/akerbp.mlops-0.20210315135618.tar.gz

---
+++ packages/akerbp.mlops/akerbp.mlops-0.20210315135618.tar.gz/akerbp.mlops-0.20210315135618/src/akerbp/mlops/cdf/handler.py
@@ -0,0 +1,27 @@
+# handler.py
+import traceback
+from importlib import import_module
+import warnings
+warnings.simplefilter("ignore")
+
+import akerbp.mlops.cdf.helpers as cdf
+from akerbp.mlops.core import logger, config
+service_name = config.envs.service_name
+logging=logger.get_logger("mlops_cdf")
+
+service = import_module(f"akerbp.mlops.services.{service_name}").service
+
+
+def handle(data, secrets, function_call_info):
+   try:
+      config.update_cdf_keys(secrets)
+      cdf.set_up_cdf_client(context='deploy')
+      output = service(data, secrets)
+      metadata = cdf.get_function_metadata(function_call_info)
+      output.update(metadata)
+      return output
+   except Exception:
+      trace = traceback.format_exc()
+      error_message = f"{service_name} service failed.\n{trace}"
+      logging.critical(error_message)
+      return dict(status='error', error_message=error_message)
