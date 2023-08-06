afb55bf51923738a2f30f698d04eed325df7b514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:36 +0000

    Add package pdm/1.4.1/pdm-1.4.1.tar.gz

---
+++ packages/pdm/pdm-1.4.1.tar.gz/pdm-1.4.1/pdm/_types.py
@@ -0,0 +1,14 @@
+from typing import Dict, List, NamedTuple, Tuple, Union
+
+Source = Dict[str, Union[str, bool]]
+RequirementDict = Union[str, Dict[str, Union[bool, str]]]
+CandidateInfo = Tuple[List[str], str, str]
+
+
+class Package(NamedTuple):
+    name: str
+    version: str
+    summary: str
+
+
+SearchResult = List[Package]
