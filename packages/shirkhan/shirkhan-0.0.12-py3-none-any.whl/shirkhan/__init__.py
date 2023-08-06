017b64296918a4216d23ff9d18e8e01382c00fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package shirkhan/0.0.12/shirkhan-0.0.12-py3-none-any.whl

---
+++ packages/shirkhan/shirkhan-0.0.12-py3-none-any.whl/shirkhan/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.12"
+__author__ = "shirkhan"
+
+from shirkhan.shir_encode_decode import *
+from shirkhan.shir_file import *
+from shirkhan.shir_string import *
+from shirkhan.shir_encryption import *
+
+from shirkhan.shir_syllable import *
+
+if __name__ == '__main__':
+    pass
