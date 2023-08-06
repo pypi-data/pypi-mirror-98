ae1e554f614299a266628f6d99e035b715d46ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:37 +0000

    Add package easy-pay/0.0.8/easy-pay-0.0.8.tar.gz

---
+++ packages/easy-pay/easy-pay-0.0.8.tar.gz/easy-pay-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+requires = [
+    "alipay-sdk-python>=3.3.398",
+    "pendulum>=2.1.2",
+    "requests>=2.25.1",
+    "pycryptodome==3.9.9",
+    "qrcode>=6.1",
+    "cryptography>=3.3.1"
+]
+
+setuptools.setup(
+    name="easy-pay",
+    version="0.0.8",
+    author="Steven Wang",
+    author_email="brightstar8284@icloud.com",
+    description="Easier integration with WeChat pay and Alipay.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StevenLianaL/easy-pay",
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
