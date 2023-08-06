9f65b99808c3c15c2c42eadb9b70c7bedad4aedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:24 +0000

    Add package Orange3/3.28.0/Orange3-3.28.0-cp37-cp37m-win32.whl

---
+++ packages/Orange3/Orange3-3.28.0-cp37-cp37m-win32.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.28.0'
+version = '3.28.0'
+full_version = '3.28.0'
+git_revision = '6cc3de0744af56092f8cd8d4e9d9c8636822cc53'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
