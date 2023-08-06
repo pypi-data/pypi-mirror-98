39d35c14aa9d5929a6932f3e3e4030ffdfa01120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package jina/1.0.9.dev37/jina-1.0.9.dev37.tar.gz

---
+++ packages/jina/jina-1.0.9.dev37.tar.gz/jina-1.0.9.dev37/jina/hub/rankers/LevenshteinRanker/__init__.py
@@ -0,0 +1,45 @@
+__copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import Dict
+
+import numpy as np
+from jina.executors.rankers import Match2DocRanker
+
+
+class LevenshteinRanker(Match2DocRanker):
+    """
+    :class:`LevenshteinRanker` Computes the negative Levenshtein distance
+        between a query and its matches. The distance is negative, in order to
+        achieve a bigger=better result, sort in the respective driver.
+    """
+
+    required_keys = {"text"}
+
+    def score(
+        self, query_meta: Dict, old_match_scores: Dict, match_meta: Dict
+    ) -> "np.ndarray":
+        """
+        Calculate the negative Levenshtein distance
+
+        :param query_meta: A Dict of queries to score
+        :param old_match_scores: Previously scored matches
+        :param match_meta: A Dict of matches of given query
+        :return: an `ndarray` of the size ``M x 2``.
+            `M`` is the number of new scores.
+            The columns correspond to the ``COL_MATCH_ID`` and ``COL_SCORE``.
+
+        """
+        from Levenshtein import distance
+
+        new_scores = [
+            (
+                match_id,
+                -distance(query_meta['text'], match_meta[match_id]['text']),
+            )
+            for match_id, old_score in old_match_scores.items()
+        ]
+        return np.array(
+            new_scores,
+            dtype=[(self.COL_MATCH_ID, np.object), (self.COL_SCORE, np.float64)],
+        )
