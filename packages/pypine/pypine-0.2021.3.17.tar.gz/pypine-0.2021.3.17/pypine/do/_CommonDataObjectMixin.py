4d4b06ea0be29f219554c2f16cd15f6c762b21f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:16 +0000

    Add package pypine/0.2021.3.17/pypine-0.2021.3.17.tar.gz

---
+++ packages/pypine/pypine-0.2021.3.17.tar.gz/pypine-0.2021.3.17/pypine/do/_CommonDataObjectMixin.py
@@ -0,0 +1,77 @@
+
+
+
+
+import os
+
+
+
+
+#
+# Expected existing variables or properties:
+# * str fileName
+# * str relFilePath
+#
+class _CommonDataObjectMixin():
+
+	################################################################################################################################
+	## Public Properties
+	################################################################################################################################
+
+	################################################################################################################################
+	## Helper Methods
+	################################################################################################################################
+
+	#
+	# * "" => ("", "")
+	# * "foo.txt" => ("foo", ".txt")
+	# * "foo." => ("foo", ".")
+	# * "foo.bar.txt" => ("foo.bar", ".txt")
+	# * ".bar" => (".bar", "")
+	# * "bar" => ("bar", "")
+	#
+	def __splitExt(self, fn:str) -> str:
+		if not fn:
+			return "", ""
+
+		pos = fn.rfind(".")
+		if pos > 0:
+			return fn[:pos], fn[pos:]
+		else:
+			return fn, ""
+	#
+
+	################################################################################################################################
+	## Public Methods
+	################################################################################################################################
+
+	@property
+	def fileNameWithoutExt(self) -> str:
+		ext = self.fileExt
+		return self.fileName[:-len(ext)]
+	#
+
+	@property
+	def fileExt(self) -> str:
+		baseFileName, ext = self.__splitExt(self.fileName)
+
+		if ext in ( ".gz", ".bz2", ".xz" ):
+			baseFileName2, ext2 = self.__splitExt(baseFileName)
+			if ext2 == ".tar":
+				return ".tar" + ext
+
+		return ext
+	#
+
+	@property
+	def relFilePathWithoutExt(self) -> str:
+		ext = self.fileExt
+		return self.relFilePath[:-len(ext)]
+	#
+
+#
+
+
+
+
+
---
+++ packages/pypine/pypine-0.2021.3.17.tar.gz/pypine-0.2021.3.17/pypine/utils/__init__.py
@@ -0,0 +1,8 @@
+﻿
+
+__version__ = "0.2021.3.17"
+
+
+
+from .RelFilePathBuilder import RelFilePathBuilder
+from .ObservableEvent import ObservableEvent
