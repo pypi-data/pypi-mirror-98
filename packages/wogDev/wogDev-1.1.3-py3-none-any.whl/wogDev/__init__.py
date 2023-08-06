ad0c2eef7741303dd747df0a79c5bb5b930123e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:28 +0000

    Add package wogDev/1.1.3/wogDev-1.1.3-py3-none-any.whl

---
+++ packages/wogDev/wogDev-1.1.3-py3-none-any.whl/wogDev/__init__.py
@@ -0,0 +1,17 @@
+#from .__main__ import main
+from mainGame import mainGame
+#from setuptools import setup, find_packages
+
+#setup(
+#    name='wog_pkg',
+#      version='0.2.0',
+#      description='World of Games'N,
+#      long_description=(HERE / "README.txt").read_text(),
+#      #long_description_content_type=LONG_DESC_TYPE,
+#      author='Oren Jacobovitz',
+#      license='Apache License 2.0',
+#      author_email='oj.gmbox@gmail.com',
+#      #url=URL,
+#      #install_requires=INSTALL_REQUIRES,
+#      packages=find_packages()
+#)
