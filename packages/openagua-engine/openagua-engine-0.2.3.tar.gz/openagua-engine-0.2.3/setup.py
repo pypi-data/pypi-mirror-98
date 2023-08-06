ae28191fd9e44a4fe3fb1ab7dcb5c1239de753c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:29 +0000

    Add package openagua-engine/0.2.3/openagua-engine-0.2.3.tar.gz

---
+++ packages/openagua-engine/openagua-engine-0.2.3.tar.gz/openagua-engine-0.2.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+VERSION = '0.2.3'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openagua-engine",
+    version=VERSION,
+    license="MIT",
+    author="David Rheinheimer",
+    author_email="david.rheinheimer@tec.mx",
+    description="Tools to connect a model engine to OpenAgua",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/openagua/openagua-engine",
+    packages=setuptools.find_packages(),
+    install_requires=["celery", "pubnub", "requests", "openagua_client"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
