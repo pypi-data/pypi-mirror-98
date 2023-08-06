7c3bc37841cb0c9cfbee45d9242a7c2b912b67f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:51 +0000

    Add package ycecream/1.1.9/ycecream-1.1.9.tar.gz

---
+++ packages/ycecream/ycecream-1.1.9.tar.gz/ycecream-1.1.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ycecream",
+    packages=["ycecream"],
+    version="1.1.9",
+    include_package_data=True,
+    long_description="ycecream",
+    description="ycecream",
+    author="Ruud van der Ham",
+    author_email="info@salabim.org",
+    url="https://github.com/salabim/ycecream",
+    download_url="https://github.com/salabim/ycecream",
+    keywords=["debugging", "utility", "tool"],
+    classifiers=[],
+)
