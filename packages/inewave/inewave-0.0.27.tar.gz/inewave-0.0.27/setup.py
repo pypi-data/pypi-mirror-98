555200eafc777937686d7a2dea714faf1e6abefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package inewave/0.0.27/inewave-0.0.27.tar.gz

---
+++ packages/inewave/inewave-0.0.27.tar.gz/inewave-0.0.27/setup.py
@@ -0,0 +1,30 @@
+import inewave
+from setuptools import setup, find_packages  # type: ignore
+
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.readlines()
+
+setup(
+    name="inewave",
+    version=inewave.__version__,
+    author="Rogerio Alves",
+    author_email="rogerioalves.ee@gmail.com",
+    description="Interface para arquivos do NEWAVE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rjmalves/inewave",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+    python_requires=">=3.5",
+    install_requires=requirements
+)
---
+++ packages/inewave/inewave-0.0.27.tar.gz/inewave-0.0.27/tests/newave/test_dger.py
@@ -0,0 +1,58 @@
+# Rotinas de testes associadas ao arquivo dger.dat do NEWAVE
+from inewave.newave.modelos.dger import DGer
+from inewave.newave.dger import LeituraDGer, EscritaDGer
+
+
+leitor = LeituraDGer("tests/_arquivos")
+leitor.le_arquivo()
+
+
+def test_leitura():
+    assert len(leitor.dger.nome_estudo) > 0
+
+
+def test_ano_inicio_estudo():
+    assert leitor.dger.ano_inicio_estudo == 1995
+
+
+def test_tolerancia():
+    assert leitor.dger.tolerancia == 95.0
+
+
+def test_risco_deficit():
+    assert leitor.dger.profundidade_risco_deficit == (1.0, 2.5)
+
+
+def test_reamostragem():
+    assert leitor.dger.reamostragem
+
+
+def test_dger_padrao():
+    dger_padrao = DGer.dger_padrao()
+    escritor = EscritaDGer("tests/_saidas")
+    escritor.escreve_arquivo(dger_padrao)
+    leitor_padrao = LeituraDGer("tests/_saidas")
+    leitor_padrao.le_arquivo()
+    assert leitor_padrao.dger == dger_padrao
+
+
+def test_escrita_e_leitura():
+    escritor = EscritaDGer("tests/_saidas")
+    escritor.escreve_arquivo(leitor.dger)
+    leitor2 = LeituraDGer("tests/_saidas")
+    leitor2.le_arquivo()
+    assert leitor.dger == leitor2.dger
+
+
+def test_eq_dger():
+    leitor2 = LeituraDGer("tests/_arquivos")
+    leitor2.le_arquivo()
+    assert leitor2.dger == leitor.dger
+
+
+def test_neq_dger():
+    leitor2 = LeituraDGer("tests/_arquivos")
+    leitor2.le_arquivo()
+    leitor2.dger.delta_zsup = -10.0
+    assert leitor2.dger != leitor.dger
+    assert leitor2.dger is not None
