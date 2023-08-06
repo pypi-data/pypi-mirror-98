05872ad9106763d084eeb4108110bf07a7f6fce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package kakao.py/0.0.1/kakao.py-0.0.1.tar.gz

---
+++ packages/kakao.py/kakao.py-0.0.1.tar.gz/kakao.py-0.0.1/kakao/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "kakao.py"
+__author__ = "jhleekr"
+__license__ = "MIT"
+__copyright__ = "Copyright (C) 2021 jhleekr"
+__version__ = "0.0.1"
+
+from .kakao import Client
+from .checkreg import check_reg
