08f9c90e7952cde176d62fe5c1ea937bb2d65454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:23 +0000

    Add package knlp/0.1.4/knlp-0.1.4.tar.gz

---
+++ packages/knlp/knlp-0.1.4.tar.gz/knlp-0.1.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+# -----------------------------------------------------------------------#
+# File Name: setup
+# Author: Junyi Li
+# Mail: 4ljy@163.com
+# Created Time: 2021-01-30
+# Description:
+# -----------------------------------------------------------------------#
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import distutils.text_file
+from pathlib import Path
+from typing import List
+
+import setuptools
+
+from version import __version__
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+def _parse_requirements(filename: str) -> List[str]:
+    """Return requirements from requirements file."""
+    # Ref: https://stackoverflow.com/a/42033122/
+    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
+
+
+setuptools.setup(
+    name='knlp',
+    version=__version__,
+    author='Junyi Li',
+    author_email='ljyduke@gmail.com',
+    description='KUAI SU(Quickly use) Python toolkit for Chinese Language Processing.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DukeEnglish/knlp',
+    package_data={'': ['*.md', '*.txt', '*.marshal', '*.marshal.3']},
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=('test*',)),
+    install_requires=_parse_requirements('requirements.txt'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'],
+)
---
+++ packages/knlp/knlp-0.1.4.tar.gz/knlp-0.1.4/test/test_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+# -----------------------------------------------------------------------#
+# File Name: test
+# Author: Junyi Li
+# Mail: 4ljy@163.com
+# Created Time: 2021-01-27
+# Description:
+# -----------------------------------------------------------------------#
+
+from knlp import Knlp
+
+
+def test_all():
+    with open("knlp/data/pytest_data.txt") as f:
+        text = f.read()
+    res = Knlp(text)
+    print("seg_result is", res.seg_result)
+    print("ner_result is", res.ner_result)
+    print("sentiment score is", res.sentiment)
+    print("key_words are", res.key_words)
+    print("key sentences are", res.key_sentences)
+    gt_string = '就读 于 中国人民大学 电视 上 的 电影 节目 项目 的 研究 角色 本人 将 会 参与 配音'
+    pred_string = '就读 于 中国 人民 大学 电视 上 的 电影 节目 项 目的 研究 角色 本人 将 会 参与 配音'
+    print("evaluation res are", res.evaluation_segment(gt_string, pred_string))
+
+
+if __name__ == '__main__':
+    test_all()
