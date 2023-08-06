b92d10f6d07d50a49d246e95c7d9007ad48771ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:48 +0000

    Add package telemetrix-rpi-pico/0.7/telemetrix-rpi-pico-0.7.tar.gz

---
+++ packages/telemetrix-rpi-pico/telemetrix-rpi-pico-0.7.tar.gz/telemetrix-rpi-pico-0.7/telemetrix_rpi_pico/telemetrix_neopixel.py
@@ -0,0 +1,31 @@
+"""
+ Copyright (c) 2021 Alan Yorinks All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,f
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+
+class TelemetrixNeoPixel:
+    """
+
+
+    """
+
+    def __init__(self, pin=28, num_pixels=8, bytes_per_pixel=3):
+        self.pin = pin
+        self.num_pixels = num_pixels
+        self.bytes_per_pixel = bytes_per_pixel
+
+
+
